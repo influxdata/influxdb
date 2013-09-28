38d588f5c01c68000da26ca7dc36381037de4b97
Xiang Li <xiang.li@coreos.com>
2013-09-28 10:06:52 -0700

    sort the commitindex of peers in descending order

--- sort.go
+++ sort.go
@@ -19,5 +19,5 @@ type uint64Slice []uint64
 //--------------------------------------
 
 func (p uint64Slice) Len() int           { return len(p) }
-func (p uint64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p uint64Slice) Less(i, j int) bool { return p[i] > p[j] } // descending order
 func (p uint64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
