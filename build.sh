59493d371497d26c1a70fb7f28a1436dd3654388
John Shahid <jvshahid@gmail.com>
2013-10-08 17:00:28 -0400

    build the packages that don't have tests.

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+. ./exports.sh
+
 go get code.google.com/p/goprotobuf/proto
+
+pushd src
+packages=$(ls -d * | egrep -v 'google|launchpad|github' | tr '\n' ' ')
+popd
+
+echo "packages: go build $packages"
+
+go build $packages
