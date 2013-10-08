8e87803ee52798ed062b66fc6bc397a9529dfd73
Todd Persen <todd.persen@gmail.com>
2013-10-08 12:43:46 -0400

    fix the test script on mac os

--- src/query/build_parser.sh
+++ src/query/build_parser.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "x`uname`" == "xLinux" ]; then
-    yacc -t -d query.yacc && lex -i query.lex
-fi
+yacc -t -d query.yacc && lex -i query.lex
--- test.sh
+++ test.sh
@@ -5,14 +5,15 @@ set -e
 cd `dirname $0`
 . exports.sh
 
-valgrind_result=0
-pushd src/query
-./build_parser.sh
-if ! ./test_memory_leaks.sh; then
-    echo "ERROR: memory leak detected"
-    exit 1
+if [ "x`uname`" == "xLinux" ]; then
+    pushd src/query
+    ./build_parser.sh
+    if ! ./test_memory_leaks.sh; then
+	echo "ERROR: memory leak detected"
+	exit 1
+    fi
+    popd
 fi
-popd
 
 go get launchpad.net/gocheck
 
