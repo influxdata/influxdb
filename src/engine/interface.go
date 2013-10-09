50227fe4a0b750d21e74ad96dc47bf0347cba6bc
John Shahid <jvshahid@gmail.com>
2013-10-09 13:16:20 -0400

    add the query engine interface.

--- src/engine/interface.go
+++ src/engine/interface.go
@@ -10,6 +10,14 @@ type EngineI interface {
 	RunQuery(query *parser.Query, yield func(*protocol.Series) error) error
 }
 
+type QueryEngine struct {
+	coordinator coordinator.Coordinator
+}
+
+func (self *QueryEngine) RunQuery(query *parser.Query, yield func(*protocol.Series) error) error {
+	return nil
+}
+
 func NewQueryEngine(c coordinator.Coordinator) (EngineI, error) {
-	return nil, nil
+	return &QueryEngine{c}, nil
 }
