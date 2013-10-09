8ef13d3c6fa603e560108a9c242281b79b19e626
John Shahid <jvshahid@gmail.com>
2013-10-09 17:04:17 -0400

    add a test for count with group by.

--- src/engine/engine_test.go
+++ src/engine/engine_test.go
@@ -278,3 +278,84 @@ func (self *EngineSuite) TestCountQueryWithRegexTables(c *C) {
 `)
 
 }
+
+func (self *EngineSuite) TestCountQueryWithGroupByClause(c *C) {
+	// make the mock coordinator return some data
+	engine := createEngine(c, `
+[
+  {
+    "points": [
+      {
+        "values": [
+          {
+            "string_value": "some_value"
+          }
+        ],
+        "timestamp": 1381346631,
+        "sequence_number": 1
+      },
+      {
+        "values": [
+          {
+            "string_value": "another_value"
+          }
+        ],
+        "timestamp": 1381346631,
+        "sequence_number": 1
+      }
+    ],
+    "name": "foo",
+    "fields": [
+      {
+        "type": "STRING",
+        "name": "column_one"
+      }
+    ]
+  }
+]
+`)
+
+	runQuery(engine, "select count(*), column_one from foo.* group by column_one;", c, `[
+  {
+    "points": [
+      {
+        "values": [
+          {
+            "int_value": 1
+          },
+          {
+            "string_value": "some_value"
+          }
+        ],
+        "timestamp": 1381346631,
+        "sequence_number": 1
+      },
+			{
+        "values": [
+          {
+            "int_value": 1
+          },
+          {
+            "string_value": "some_value"
+          }
+        ],
+        "timestamp": 1381346631,
+        "sequence_number": 1
+      }
+    ],
+    "name": "foo",
+    "fields": [
+      {
+        "type": "INT32",
+        "name": "count"
+      },
+      {
+        "type": "STRING",
+        "name": "column_one"
+      }
+    ]
+  }
+]
+`)
+
+}
