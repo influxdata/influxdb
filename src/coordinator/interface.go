8b65f7e728034db1f43dfc9140f70c57c831946c
John Shahid <jvshahid@gmail.com>
2013-10-09 13:16:10 -0400

    update the coordinator docs.

--- src/coordinator/interface.go
+++ src/coordinator/interface.go
@@ -6,6 +6,14 @@ import (
 )
 
 type Coordinator interface {
+	// Assumption about the returned data:
+	//   1. For any given time series, the points returned are in order
+	//   2. If the query involves more than one time series, there is no
+	//      guarantee on the order in whic they are returned
+	//   3. Data is filtered, i.e. where clause should be assumed to hold true
+	//      for all the data points that are returned
+	//   4. The end of a time series is signaled by returning a series with no data points
+	//   5. TODO: Aggregation on the nodes
 	DistributeQuery(query *parser.Query, yield func(*protocol.Series) error) error
 	WriteSeriesData(series *protocol.Series) error
 }
