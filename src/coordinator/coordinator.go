647d09cbd60b548cf7befb9d46883c5495f42c3d
Paul Dix <paul@pauldix.net>
2013-10-11 13:32:02 -0400

    Add basic non-distributed version of coordinator implementation to hook into datastore

--- src/coordinator/coordinator.go
+++ src/coordinator/coordinator.go
@@ -1,1 +1,25 @@
 package coordinator
+
+import (
+	"datastore"
+	"parser"
+	"protocol"
+)
+
+type CoordinatorImpl struct {
+	clusterConfiguration *ClusterConfiguration
+	raftServer           *RaftServer
+	datastore            datastore.Datastore
+}
+
+func NewCoordinatorImpl(datastore datastore.Datastore, raftServer *RaftServer, clusterConfiguration *ClusterConfiguration) Coordinator {
+	return &CoordinatorImpl{clusterConfiguration, raftServer, datastore}
+}
+
+func (self *CoordinatorImpl) DistributeQuery(query *parser.Query, yield func(*protocol.Series) error) error {
+	return self.datastore.ExecuteQuery(0, query, yield)
+}
+
+func (self *CoordinatorImpl) WriteSeriesData(series *protocol.Series) error {
+	return self.datastore.WriteSeriesData(0, series)
+}
