5fe7fa225f8628dd5b83bb0edfed785948c8dca2
John Shahid <jvshahid@gmail.com>
2013-10-11 10:55:35 -0400

    statically link leveldb if the build script ran on linux.

--- build.sh
+++ build.sh
@@ -7,6 +7,38 @@ go get github.com/goraft/raft
 go get github.com/gorilla/mux
 go get github.com/jmhodges/levigo
 
+# build snappy and leveldb
+if [ `uname` == "Linux" ]; then
+    snappy_version=1.1.0
+    snappy_file=snappy-$snappy_version.tar.gz
+    if [ ! -d $snappy_dir -o ! -e $snappy_dir/$snappy_file -o ! -e $snappy_dir/.libs/libsnappy.a ]; then
+        rm -rf $snappy_dir
+        mkdir -p $snappy_dir
+        pushd $snappy_dir
+        wget https://snappy.googlecode.com/files/$snappy_file
+        tar --strip-components=1 -xvzf $snappy_file
+        ./configure
+        make
+        popd
+    fi
+
+    leveldb_version=1.12.0
+    leveldb_file=leveldb-$leveldb_version.tar.gz
+    if [ ! -d $leveldb_dir -o ! -e $leveldb_dir/$leveldb_file -o ! -e $leveldb_dir/libleveldb.a ]; then
+        rm -rf $leveldb_dir
+        mkdir -p $leveldb_dir
+        pushd $leveldb_dir
+        wget https://leveldb.googlecode.com/files/$leveldb_file
+        tar --strip-components=1 -xvzf $leveldb_file
+        CXXFLAGS="-I$snappy_dir" LDFLAGS="-L$snappy_dir/.libs" make
+        popd
+    fi
+fi
+
+
+pushd src/github.com/jmhodges/levigo/
+find . -name \*.go | xargs sed -i 's/\/\/ #cgo LDFLAGS: -lleveldb\|#cgo LDFLAGS: -lleveldb//g'
+popd
 
 echo "packages: go build $packages"
 
--- exports.sh
+++ exports.sh
@@ -12,3 +12,12 @@ pushd src
 export packages=$(ls -d * | egrep -v 'google|launchpad|github' | tr '\n' ' ')
 popd
 
+snappy_dir=/tmp/snappychronosdb
+leveldb_dir=/tmp/leveldbchronosdb
+export LD_LIBRARY_PATH=/usr/local/lib
+if [ `uname` == "Linux" ]; then
+    export CGO_CFLAGS="-I$leveldb_dir/include"
+    export CGO_LDFLAGS="$leveldb_dir/libleveldb.a $snappy_dir/.libs/libsnappy.a -lstdc++"
+else
+    export CGO_LDFLAGS="-lleveldb -lsnappy -lstdc++"
+fi
