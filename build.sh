99d282ec00b61c9188df249873b33b05fe54725b
John Shahid <jvshahid@gmail.com>
2013-10-15 12:34:22 -0400

    build levigo and the other dependencies after building leveldb and snappy.

--- build.sh
+++ build.sh
@@ -4,11 +4,6 @@ set -e
 
 . ./exports.sh
 
-go get code.google.com/p/goprotobuf/proto
-go get github.com/goraft/raft
-go get github.com/gorilla/mux
-go get github.com/jmhodges/levigo
-
 # build snappy and leveldb
 if [ `uname` == "Linux" ]; then
     snappy_version=1.1.0
@@ -41,6 +36,10 @@ if [ `uname` == "Linux" ]; then
     popd
 fi
 
+go get code.google.com/p/goprotobuf/proto
+go get github.com/goraft/raft
+go get github.com/gorilla/mux
+go get github.com/jmhodges/levigo
 
 echo "packages: go build $packages"
 
