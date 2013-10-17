9ca1975b2c2bb15f400daf1dcb6268c37aefcff1
John Shahid <jvshahid@gmail.com>
2013-10-17 11:42:01 -0400

    add more dependencies.

--- build.sh
+++ build.sh
@@ -40,6 +40,10 @@ go get code.google.com/p/goprotobuf/proto
 go get github.com/goraft/raft
 go get github.com/gorilla/mux
 go get github.com/jmhodges/levigo
+go get github.com/bmizerany/pat
+go get github.com/fitstar/falcore
+go get github.com/fitstar/falcore/filter
+go get code.google.com/p/log4go
 
 echo "packages: go build $packages"
 
