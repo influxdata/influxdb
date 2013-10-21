81ea3dc31797259bcd329c8044da22e1c35bdc66
Paul Dix <paul@pauldix.net>
2013-10-21 14:55:28 -0400

    Fix configuration test

--- src/configuration/configuration_test.go
+++ src/configuration/configuration_test.go
@@ -16,5 +16,5 @@ var _ = Suite(&LoadConfigurationSuite{})
 
 func (self *LoadConfigurationSuite) TestConfig(c *C) {
 	config := LoadConfiguration("../../config.json.sample")
-	c.Assert(config.DataDir, Equals, "/tmp/influxdb/development")
+	c.Assert(config.DataDir, Equals, "/tmp/influxdb/development/db")
 }
