3d24c89a4b1cd6166d804f68cb872fd5ad84e6e5
John Shahid <jvshahid@gmail.com>
2013-11-06 13:48:48 -0500

    set CC only if it wasn't set. Set PYTHONPATH if it's not on mac.

--- exports.sh
+++ exports.sh
@@ -28,10 +28,15 @@ on_linux="no"
 if [ `uname` = "Linux" ]; then
     on_linux=yes
 
-elif [ `uname -v | cut -d' ' -f4` = "13.0.0:" ]; then
+elif [ "x$CC" == "x" -a `uname -v | cut -d' ' -f4` = "13.0.0:" ]; then
     # for mavericks use gcc instead of llvm
     export CC=gcc-4.2
 fi
+
+if [ "x$PYTHONPATH" == x -a $on_linux != yes ]; then
+    PYTHONPATH=/usr/local/lib/python2.7/site-packages/:$PYTHONPATH
+fi
+
 if [ $on_linux = yes ]; then
     export CGO_CFLAGS="-I$leveldb_dir/include"
     export CGO_LDFLAGS="$leveldb_dir/libleveldb.a $snappy_dir/.libs/libsnappy.a -lstdc++"
