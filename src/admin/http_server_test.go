87d71d7dd9233c179927c6ca551933e3d48fccb8
John Shahid <jvshahid@gmail.com>
2013-11-11 08:10:44 -0500

    fix the admin server test.

--- src/admin/http_server_test.go
+++ src/admin/http_server_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	. "launchpad.net/gocheck"
 	"net/http"
+	"os"
 	"testing"
 )
 
@@ -18,12 +19,21 @@ type HttpServerSuite struct {
 var _ = Suite(&HttpServerSuite{})
 
 func (self *HttpServerSuite) TestServesIndexByDefault(c *C) {
+	// prepare some dummy site files
+	err := os.Mkdir("./site/", 0755)
+	c.Assert(err, IsNil)
+	defer os.RemoveAll("./site/")
+	content := []byte("Welcome to Influxdb")
+	err = ioutil.WriteFile("./site/index.html", content, 0644)
+
 	s := NewHttpServer("./site/", ":8083")
+	c.Assert(err, IsNil)
 	go func() { s.ListenAndServe() }()
 	resp, err := http.Get("http://localhost:8083/")
 	c.Assert(err, IsNil)
 	defer resp.Body.Close()
 	c.Assert(resp.StatusCode, Equals, http.StatusOK)
-	_, err = ioutil.ReadAll(resp.Body)
+	actualContent, err := ioutil.ReadAll(resp.Body)
+	c.Assert(actualContent, DeepEquals, content)
 	c.Assert(err, IsNil)
 }
