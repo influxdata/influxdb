9d04ad6250c86fca696e6c5e0d6ec336f8ea9884
John Shahid <jvshahid@gmail.com>
2013-11-19 17:14:08 -0500

    fix the build to be version independent on Mac OSX

--- src/parser/build_parser.sh
+++ src/parser/build_parser.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
-export PATH=/usr/local/Cellar/bison/3.0/bin:/usr/local/Cellar/flex/2.5.37/bin:$PATH
+cd `dirname $0`
+
+source ../../exports.sh
+
+brew_dir=/usr/local/Cellar
+if [ $on_linux == no ]; then
+    if ! ls -l $brew_dir/bison/* >/dev/null 2>&1; then
+        echo "It looks like you don't have bison installed. Please run 'brew install bison'"
+        exit 1
+    fi
+    if ! ls -l $brew_dir/flex/* >/dev/null 2>&1; then
+        echo "It looks like you don't have flex installed. Please run 'brew install flex'"
+        exit 1
+    fi
+
+    bison_version=`ls -l $brew_dir/bison/* | sort -Vr | head -1`
+    flex_version=`ls -l $brew_dir/flex/* | sort -Vr | head -1`
+
+    export PATH=$brew_dir/bison/$bison_version/bin:$brew_dir/flex/$flex_version/bin:$PATH
+fi
 
 bison -t -d query.yacc -o y.tab.c --defines=y.tab.h && flex -o lex.yy.c -i query.lex
