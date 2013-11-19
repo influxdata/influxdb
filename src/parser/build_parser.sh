00ff292d970d7afc11880a281e8f73dab029033b
Todd Persen <todd.persen@gmail.com>
2013-11-19 17:22:11 -0500

    Update build_parser.sh to work on OS X.

--- src/parser/build_parser.sh
+++ src/parser/build_parser.sh
@@ -2,10 +2,13 @@
 
 cd `dirname $0`
 
-source ../../exports.sh
-
 brew_dir=/usr/local/Cellar
-if [ $on_linux == no ]; then
+on_linux="no"
+if [ `uname` = "Linux" ]; then
+    on_linux=yes
+fi
+
+if [ $on_linux = no ]; then
     if ! ls -l $brew_dir/bison/* >/dev/null 2>&1; then
         echo "It looks like you don't have bison installed. Please run 'brew install bison'"
         exit 1
@@ -15,10 +18,11 @@ if [ $on_linux == no ]; then
         exit 1
     fi
 
-    bison_version=`ls -l $brew_dir/bison/* | sort -Vr | head -1`
-    flex_version=`ls -l $brew_dir/flex/* | sort -Vr | head -1`
+    bison_path=`ls -d $brew_dir/bison/* | tail -1`
+    flex_path=`ls -d $brew_dir/flex/* | tail -1`
 
-    export PATH=$brew_dir/bison/$bison_version/bin:$brew_dir/flex/$flex_version/bin:$PATH
+    export PATH=$bison_path/bin:$flex_path/bin:$PATH
+    echo $PATH
 fi
 
 bison -t -d query.yacc -o y.tab.c --defines=y.tab.h && flex -o lex.yy.c -i query.lex
