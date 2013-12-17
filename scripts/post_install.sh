bcb0da5f54e8dd95806a8c6db8e0b8faa1cb2b51
John Shahid <jvshahid@gmail.com>
2013-12-17 14:44:05 -0500

    fix #141. Include the benchmark binary in the package

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -5,9 +5,11 @@ version=REPLACE_VERSION
 # create some symlinks
 ln -sfn $influx_dir/versions/$version $influx_dir/current
 [ -e /usr/bin/influxdb ] || ln -sfn $influx_dir/current/influxdb /usr/bin/influxdb
+[ -e /usr/bin/influxdb-benchmark ] || ln -sfn $influx_dir/current/influxdb-benchmark /usr/bin/influxdb-benchmark
 [ -e /usr/bin/influxdb-daemon ] || ln -sfn $influx_dir/current/scripts/influxdb-daemon.sh /usr/bin/influxdb-daemon
 [ -d $influx_dir/shared ] || mkdir $influx_dir/shared
 [ -e $influx_dir/shared/config.toml ] || cp $influx_dir/current/config.toml $influx_dir/shared/
+[ -e $influx_dir/shared/benchmark_config.toml ] || cp $influx_dir/current/benchmark_config.toml $influx_dir/shared/
 touch $influx_dir/shared/log.txt
 if [ ! -L /etc/init.d/influxdb ]; then
     ln -sfn $influx_dir/current/scripts/init.sh /etc/init.d/influxdb
