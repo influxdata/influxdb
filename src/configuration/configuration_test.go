a8f233081a1f1eb9e609923cde9aff0f3d7529d3
John Shahid <jvshahid@gmail.com>
2014-02-25 16:06:44 -0500

    fix the coniguration test suite

--- src/configuration/configuration_test.go
+++ src/configuration/configuration_test.go
@@ -40,8 +40,8 @@ func (self *LoadConfigurationSuite) TestConfig(c *C) {
 	c.Assert(config.DataDir, Equals, "/tmp/influxdb/development/db")
 
 	c.Assert(config.ProtobufPort, Equals, 8099)
-	c.Assert(config.ProtobufHeartbeatInterval.Duration, Equals, 10*time.Millisecond)
-	c.Assert(config.ProtobufTimeout.Duration, Equals, 1*time.Second)
+	c.Assert(config.ProtobufHeartbeatInterval.Duration, Equals, 200*time.Millisecond)
+	c.Assert(config.ProtobufTimeout.Duration, Equals, 2*time.Second)
 	c.Assert(config.SeedServers, DeepEquals, []string{"hosta:8090", "hostb:8090"})
 
 	c.Assert(config.WalDir, Equals, "/tmp/influxdb/development/wal")
