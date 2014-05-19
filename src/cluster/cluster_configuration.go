influxdata/influxdb

commit d593348283d83e742b4f07c44f205ec5f2ad7e1c

Fix #489. Remove replication factor from CreateDatabase command

src/cluster/cluster_configuration.go
@@ -57,7 +57,7 @@ const (
 */
 type ClusterConfiguration struct {
 	createDatabaseLock         sync.RWMutex
-	DatabaseReplicationFactors map[string]uint8
+	DatabaseReplicationFactors map[string]struct{}
 	usersLock                  sync.RWMutex
 	clusterAdmins              map[string]*ClusterAdmin
 	dbUsers                    map[string]map[string]*DbUser
@@ -92,8 +92,7 @@ type ContinuousQuery struct {
 }
 
 type Database struct {
-	Name              string `json:"name"`
-	ReplicationFactor uint8  `json:"replicationFactor"`
+	Name string `json:"name"`
 }
 
 func NewClusterConfiguration(
@@ -102,7 +101,7 @@ func NewClusterConfiguration(
 	shardStore LocalShardStore,
 	connectionCreator func(string) ServerConnection) *ClusterConfiguration {
 	return &ClusterConfiguration{
-		DatabaseReplicationFactors: make(map[string]uint8),
+		DatabaseReplicationFactors: make(map[string]struct{}),
 		clusterAdmins:              make(map[string]*ClusterAdmin),
 		dbUsers:                    make(map[string]map[string]*DbUser),
 		continuousQueries:          make(map[string][]*ContinuousQuery),
@@ -259,8 +258,8 @@ func (self *ClusterConfiguration) GetDatabases() []*Database {
 	defer self.createDatabaseLock.RUnlock()
 
 	dbs := make([]*Database, 0, len(self.DatabaseReplicationFactors))
-	for name, rf := range self.DatabaseReplicationFactors {
-		dbs = append(dbs, &Database{Name: name, ReplicationFactor: rf})
+	for name, _ := range self.DatabaseReplicationFactors {
+		dbs = append(dbs, &Database{Name: name})
 	}
 	return dbs
 }
@@ -273,14 +272,14 @@ func (self *ClusterConfiguration) DatabaseExists(name string) bool {
 	}
 }
 
-func (self *ClusterConfiguration) CreateDatabase(name string, replicationFactor uint8) error {
+func (self *ClusterConfiguration) CreateDatabase(name string) error {
 	self.createDatabaseLock.Lock()
 	defer self.createDatabaseLock.Unlock()
 
 	if _, ok := self.DatabaseReplicationFactors[name]; ok {
 		return common.NewDatabaseExistsError(name)
 	}
-	self.DatabaseReplicationFactors[name] = replicationFactor
+	self.DatabaseReplicationFactors[name] = struct{}{}
 	return nil
 }
 
@@ -481,7 +480,7 @@ func (self *ClusterConfiguration) SaveClusterAdmin(u *ClusterAdmin) {
 }
 
 type SavedConfiguration struct {
-	Databases         map[string]uint8
+	Databases         map[string]struct{}
 	Admins            map[string]*ClusterAdmin
 	DbUsers           map[string]map[string]*DbUser
 	Servers           []*ClusterServer

src/api/http/api.go
@@ -375,8 +375,7 @@ func (self *HttpServer) writePoints(w libhttp.ResponseWriter, r *libhttp.Request
 }
 
 type createDatabaseRequest struct {
-	Name              string `json:"name"`
-	ReplicationFactor uint8  `json:"replicationFactor"`
+	Name string `json:"name"`
 }
 
 func (self *HttpServer) listDatabases(w libhttp.ResponseWriter, r *libhttp.Request) {
@@ -400,12 +399,12 @@ func (self *HttpServer) createDatabase(w libhttp.ResponseWriter, r *libhttp.Requ
 		if err != nil {
 			return libhttp.StatusBadRequest, err.Error()
 		}
-		err = self.coordinator.CreateDatabase(user, createRequest.Name, createRequest.ReplicationFactor)
+		err = self.coordinator.CreateDatabase(user, createRequest.Name)
 		if err != nil {
 			log.Error("Cannot create database %s. Error: %s", createRequest.Name, err)
 			return errorToStatusCode(err), err.Error()
 		}
-		log.Debug("Created database %s with replication factor %d", createRequest.Name, createRequest.ReplicationFactor)
+		log.Debug("Created database %s", createRequest.Name)
 		return libhttp.StatusCreated, nil
 	})
 }

src/api/http/api_test.go
@@ -112,13 +112,13 @@ func (self *MockCoordinator) DeleteSeriesData(_ User, db string, query *parser.D
 	return nil
 }
 
-func (self *MockCoordinator) CreateDatabase(_ User, db string, _ uint8) error {
+func (self *MockCoordinator) CreateDatabase(_ User, db string) error {
 	self.db = db
 	return nil
 }
 
 func (self *MockCoordinator) ListDatabases(_ User) ([]*cluster.Database, error) {
-	return []*cluster.Database{&cluster.Database{"db1", 1}, &cluster.Database{"db2", 1}}, nil
+	return []*cluster.Database{&cluster.Database{"db1"}, &cluster.Database{"db2"}}, nil
 }
 
 func (self *MockCoordinator) DropDatabase(_ User, db string) error {
@@ -749,7 +749,7 @@ func (self *ApiSuite) TestDatabasesIndex(c *C) {
 		c.Assert(err, IsNil)
 		err = json.Unmarshal(body, &databases)
 		c.Assert(err, IsNil)
-		c.Assert(databases, DeepEquals, []*cluster.Database{&cluster.Database{"db1", uint8(1)}, &cluster.Database{"db2", uint8(1)}})
+		c.Assert(databases, DeepEquals, []*cluster.Database{&cluster.Database{"db1"}, &cluster.Database{"db2"}})
 	}
 }
 
@@ -768,7 +768,7 @@ func (self *ApiSuite) TestBasicAuthentication(c *C) {
 	c.Assert(err, IsNil)
 	err = json.Unmarshal(body, &databases)
 	c.Assert(err, IsNil)
-	c.Assert(databases, DeepEquals, []*cluster.Database{&cluster.Database{"db1", 1}, &cluster.Database{"db2", 1}})
+	c.Assert(databases, DeepEquals, []*cluster.Database{&cluster.Database{"db1"}, &cluster.Database{"db2"}})
 }
 
 func (self *ApiSuite) TestContinuousQueryOperations(c *C) {

src/coordinator/command.go
@@ -109,12 +109,11 @@ func (c *DropDatabaseCommand) Apply(server raft.Server) (interface{}, error) {
 }
 
 type CreateDatabaseCommand struct {
-	Name              string `json:"name"`
-	ReplicationFactor uint8  `json:"replicationFactor"`
+	Name string `json:"name"`
 }
 
-func NewCreateDatabaseCommand(name string, replicationFactor uint8) *CreateDatabaseCommand {
-	return &CreateDatabaseCommand{name, replicationFactor}
+func NewCreateDatabaseCommand(name string) *CreateDatabaseCommand {
+	return &CreateDatabaseCommand{name}
 }
 
 func (c *CreateDatabaseCommand) CommandName() string {
@@ -123,7 +122,7 @@ func (c *CreateDatabaseCommand) CommandName() string {
 
 func (c *CreateDatabaseCommand) Apply(server raft.Server) (interface{}, error) {
 	config := server.Context().(*cluster.ClusterConfiguration)
-	err := config.CreateDatabase(c.Name, c.ReplicationFactor)
+	err := config.CreateDatabase(c.Name)
 	return nil, err
 }
 

src/coordinator/coordinator.go
@@ -749,7 +749,7 @@ func (self *CoordinatorImpl) ListContinuousQueries(user common.User, db string) 
 	return series, nil
 }
 
-func (self *CoordinatorImpl) CreateDatabase(user common.User, db string, replicationFactor uint8) error {
+func (self *CoordinatorImpl) CreateDatabase(user common.User, db string) error {
 	if !user.IsClusterAdmin() {
 		return common.NewAuthorizationError("Insufficient permissions to create database")
 	}
@@ -758,7 +758,7 @@ func (self *CoordinatorImpl) CreateDatabase(user common.User, db string, replica
 		return fmt.Errorf("%s isn't a valid db name", db)
 	}
 
-	err := self.raftServer.CreateDatabase(db, replicationFactor)
+	err := self.raftServer.CreateDatabase(db)
 	if err != nil {
 		return err
 	}

src/coordinator/interface.go
@@ -18,7 +18,7 @@ type Coordinator interface {
 	//   5. TODO: Aggregation on the nodes
 	WriteSeriesData(user common.User, db string, series []*protocol.Series) error
 	DropDatabase(user common.User, db string) error
-	CreateDatabase(user common.User, db string, replicationFactor uint8) error
+	CreateDatabase(user common.User, db string) error
 	ForceCompaction(user common.User) error
 	ListDatabases(user common.User) ([]*cluster.Database, error)
 	DeleteContinuousQuery(user common.User, db string, id uint32) error
@@ -30,7 +30,7 @@ type Coordinator interface {
 }
 
 type ClusterConsensus interface {
-	CreateDatabase(name string, replicationFactor uint8) error
+	CreateDatabase(name string) error
 	DropDatabase(name string) error
 	CreateContinuousQuery(db string, query string) error
 	DeleteContinuousQuery(db string, id uint32) error

src/coordinator/raft_server.go
@@ -179,11 +179,8 @@ func SendCommandToServer(url string, command raft.Command) (interface{}, error) 
 
 }
 
-func (s *RaftServer) CreateDatabase(name string, replicationFactor uint8) error {
-	if replicationFactor == 0 {
-		replicationFactor = 1
-	}
-	command := NewCreateDatabaseCommand(name, replicationFactor)
+func (s *RaftServer) CreateDatabase(name string) error {
+	command := NewCreateDatabaseCommand(name)
 	_, err := s.doOrProxyCommand(command)
 	return err
 }

commit 1369ca75fbed364ccc75fb75bfc2602aa31c82ad

Fix #555. Fix a regression in d593348 where the snapshot format changed

src/cluster/cluster_configuration.go
@@ -480,7 +480,7 @@ func (self *ClusterConfiguration) SaveClusterAdmin(u *ClusterAdmin) {
 }
 
 type SavedConfiguration struct {
-	Databases         map[string]struct{}
+	Databases         map[string]uint8
 	Admins            map[string]*ClusterAdmin
 	DbUsers           map[string]map[string]*DbUser
 	Servers           []*ClusterServer
@@ -492,13 +492,17 @@ type SavedConfiguration struct {
 func (self *ClusterConfiguration) Save() ([]byte, error) {
 	log.Debug("Dumping the cluster configuration")
 	data := &SavedConfiguration{
-		Databases:         self.DatabaseReplicationFactors,
+		Databases:         make(map[string]uint8, len(self.DatabaseReplicationFactors)),
 		Admins:            self.clusterAdmins,
 		DbUsers:           self.dbUsers,
 		Servers:           self.servers,
 		ContinuousQueries: self.continuousQueries,
 		ShortTermShards:   self.convertShardsToNewShardData(self.shortTermShards),
 		LongTermShards:    self.convertShardsToNewShardData(self.longTermShards),
+	}
+
+	for k, _ := range self.DatabaseReplicationFactors {
+		data.Databases[k] = 0
 	}
 
 	b := bytes.NewBuffer(nil)
@@ -546,14 +550,18 @@ func (self *ClusterConfiguration) Recovery(b []byte) error {
 
 	err := gob.NewDecoder(bytes.NewReader(b)).Decode(&data)
 	if err != nil {
+		log.Error("Error while decoding snapshot: %s", err)
 		return err
 	}
 
-	self.DatabaseReplicationFactors = data.Databases
+	self.DatabaseReplicationFactors = make(map[string]struct{}, len(data.Databases))
+	for k, _ := range data.Databases {
+		self.DatabaseReplicationFactors[k] = struct{}{}
+	}
 	self.clusterAdmins = data.Admins
 	self.dbUsers = data.DbUsers
-
 	self.servers = data.Servers
+
 	for _, server := range self.servers {
 		log.Info("Checking whether %s is the local server %s", server.RaftName, self.LocalRaftName)
 		if server.RaftName == self.LocalRaftName {