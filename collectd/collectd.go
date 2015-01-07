f051aa641bb5c9d1b04ee7c26aed74a149ab8645
Cory LaNou <cory@lanou.com>
2015-01-07 16:10:27 -0700

    first round on unmarshalling a packet.  needs testing

--- collectd/collectd.go
+++ collectd/collectd.go
@@ -1,1 +1,64 @@
 package collectd
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math"
+	"time"
+
+	"github.com/kimor79/gollectd"
+)
+
+var ErrTimeOutOfBounds = errors.New("The time is to large to store")
+
+//TODO corylanou: This needs to be made a public `main.Point` so we can share this across packages.
+type Metrics []Metric
+type Metric struct {
+	Name      string
+	Tags      map[string]string
+	Value     interface{}
+	Timestamp time.Time
+}
+
+func Unmarshal(data *gollectd.Packet) (Metrics, error) {
+	// Prefer high resolution timestamp.  TimeHR is 2^-30 seconds, so shift
+	// right 30 to get seconds then convert to microseconds for InfluxDB
+	unixTime := (data.TimeHR >> 30) * 1000 * 1000
+
+	// Fallback on unix timestamp if high res is 0
+	if unixTime == 0 {
+		unixTime = data.Time * 1000 * 1000
+	}
+
+	// Check to see if the unixTime is too large.  If so, log an error
+	if unixTime > math.MaxInt64 {
+		log.Println("Collectd timestamp too large for InfluxDB.  Wrapping around to 0.")
+		return Metrics{}, ErrTimeOutOfBounds
+	}
+
+	// Collectd time is uint64 but influxdb expects int64
+	//ts := int64(unixTime % math.MaxInt64)
+
+	var m Metrics
+	for i, _ := range data.Values {
+		metric := Metric{Name: fmt.Sprintf("%s_%s", data.Plugin, data.Values[i].Name)}
+		metric.Value = data.Values[i].Value
+		metric.Timestamp = time.Unix(0, int64(unixTime)*int64(time.Millisecond))
+
+		if data.Hostname != "" {
+			metric.Tags["host"] = data.Hostname
+		}
+		if data.PluginInstance != "" {
+			metric.Tags["instance"] = data.PluginInstance
+		}
+		if data.Type != "" {
+			metric.Tags["type"] = data.Type
+		}
+		if data.TypeInstance != "" {
+			metric.Tags["type_instance"] = data.TypeInstance
+		}
+		m = append(m, metric)
+	}
+	return m, nil
+}
--- collectd/collectd_test.go
+++ collectd/collectd_test.go
@@ -1,1 +1,68 @@
 package collectd_test
+
+import (
+	"log"
+	"testing"
+
+	"github.com/influxdb/influxdb/collectd"
+	"github.com/kimor79/gollectd"
+)
+
+/*
+This is a sample of what data can be represented like in json
+[
+   {
+     "values": [197141504, 175136768],
+     "dstypes": ["counter", "counter"],
+     "dsnames": ["read", "write"],
+     "time": 1251533299,
+     "interval": 10,
+     "host": "leeloo.lan.home.verplant.org",
+     "plugin": "disk",
+     "plugin_instance": "sda",
+     "type": "disk_octets",
+     "type_instance": ""
+   },
+   …
+ ]
+
+
+*/
+
+func Test_UnmarshalPacket(t *testing.T) {
+	t.Skip()
+	var (
+		//overflowTimeHR uint64 = (((math.MaxInt64 + 1) / 1000) / 1000) << 30
+		//overflowTime   uint64 = (((math.MaxInt64 + 1) / 1000) / 1000) << 30
+		overflowTimeHR uint64
+		overflowTime   uint64
+	)
+
+	var tests = []struct {
+		name    string
+		packet  gollectd.Packet
+		metrics collectd.Metrics
+		err     error
+	}{
+		{
+			name: "Should error out if time is to large",
+			packet: gollectd.Packet{
+				Time:   overflowTime,
+				TimeHR: overflowTimeHR,
+			},
+			metrics: collectd.Metrics{},
+			err:     collectd.ErrTimeOutOfBounds,
+		},
+	}
+
+	for _, test := range tests {
+		log.Printf("%+v", test)
+		m, err := collectd.Unmarshal(&test.packet)
+		if err != test.err {
+			t.Errorf("error mismatch, expected %v, got %v", test.err, err)
+		}
+		_ = m
+
+	}
+
+}
