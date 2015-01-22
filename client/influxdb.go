86d632446976c4970a81f787e298b4afdca47681
Cory LaNou <cory@lanou.com>
2015-01-22 16:40:32 -0700

    ping added to client.  useful for testing/mocking

--- client/influxdb.go
+++ client/influxdb.go
@@ -1,11 +1,25 @@
 package client
 
-import "github.com/influxdb/influxdb"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/influxdb/influxdb"
+)
+
+const (
+	defaultAddr = "localhost:8086"
+)
 
 type Config struct {
+	Addr string
 }
 
 type Client struct {
+	addr       string
+	httpClient *http.Client
 }
 
 type Query struct {
@@ -15,7 +29,11 @@ type Write struct {
 }
 
 func NewClient(c Config) (*Client, error) {
-	return nil, nil
+	client := Client{
+		addr:       detect(c.Addr, defaultAddr),
+		httpClient: &http.Client{},
+	}
+	return &client, nil
 }
 
 func (c *Client) Query(queries ...Query) (influxdb.Results, error) {
@@ -24,4 +42,38 @@ func (c *Client) Query(queries ...Query) (influxdb.Results, error) {
 
 func (c *Client) Write(writes ...Write) (influxdb.Results, error) {
 	return nil, nil
+}
+
+func (c *Client) Ping() (time.Duration, error) {
+	now := time.Now()
+	u, err := c.urlFor("/ping")
+	if err != nil {
+		return 0, err
+	}
+	_, err = c.httpClient.Get(u.String())
+	if err != nil {
+		return 0, err
+	}
+	return time.Since(now), nil
+}
+
+// utility functions
+
+func (c *Client) Addr() string {
+	return c.addr
+}
+
+func (c *Client) urlFor(u string) (*url.URL, error) {
+	return url.Parse(fmt.Sprintf("%s/%s", c.addr, u))
+}
+
+// helper functions
+
+func detect(values ...string) string {
+	for _, v := range values {
+		if v != "" {
+			return v
+		}
+	}
+	return ""
 }
--- client/influxdb_test.go
+++ client/influxdb_test.go
@@ -1,6 +1,8 @@
 package client_test
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/influxdb/influxdb/client"
@@ -10,21 +12,53 @@ func TestNewClient(t *testing.T) {
 	config := client.Config{}
 	_, err := client.NewClient(config)
 	if err != nil {
-		t.Fatalf("unexpected error.  expected %s, got %s", nil, err)
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
 	}
+}
+
+func TestNewClient_Defaults(t *testing.T) {
+	config := client.Config{}
+	c, err := client.NewClient(config)
+	if err != nil {
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
+	}
+
+	defaultAddr := "localhost:8086"
+	if c.Addr() != defaultAddr {
+		t.Fatalf("unexpected addr: expected: %q, actual %q", defaultAddr, c.Addr())
+	}
+}
+
+func TestClient_Ping(t *testing.T) {
+	ts := emptyTestServer()
+	defer ts.Close()
+
+	config := client.Config{Addr: ts.URL}
+	c, err := client.NewClient(config)
+	if err != nil {
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
+	}
+	d, err := c.Ping()
+	if err != nil {
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
+	}
+	if d == 0 {
+		t.Fatalf("expected a duration greater than zero.  actual %v", d)
+	}
+
 }
 
 func TestClient_Query(t *testing.T) {
 	config := client.Config{}
 	c, err := client.NewClient(config)
 	if err != nil {
-		t.Fatalf("unexpected error.  expected %s, got %s", nil, err)
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
 	}
 
 	query := client.Query{}
 	_, err = c.Query(query)
 	if err != nil {
-		t.Fatalf("unexpected error.  expected %s, got %s", nil, err)
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
 	}
 }
 
@@ -32,12 +66,20 @@ func TestClient_Write(t *testing.T) {
 	config := client.Config{}
 	c, err := client.NewClient(config)
 	if err != nil {
-		t.Fatalf("unexpected error.  expected %s, got %s", nil, err)
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
 	}
 
 	write := client.Write{}
 	_, err = c.Write(write)
 	if err != nil {
-		t.Fatalf("unexpected error.  expected %s, got %s", nil, err)
+		t.Fatalf("unexpected error.  expected %v, actual %v", nil, err)
 	}
+}
+
+// helper functions
+
+func emptyTestServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		return
+	}))
 }
