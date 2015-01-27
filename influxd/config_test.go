d77c0f2d692c2ef92d77200b01b0037d2c7042f0
Cory LaNou <cory@lanou.com>
2015-01-27 10:31:04 -0700

    moving influxd to independant package to assist with intgration tests

--- influxd/config_test.go
+++ influxd/config_test.go
@@ -1,4 +1,4 @@
-package main_test
+package influxd_test
 
 import (
 	"reflect"
@@ -6,12 +6,12 @@ import (
 	"testing"
 	"time"
 
-	main "github.com/influxdb/influxdb/cmd/influxd"
+	"github.com/influxdb/influxdb/influxd"
 )
 
 // Ensure that megabyte sizes can be parsed.
 func TestSize_UnmarshalText_MB(t *testing.T) {
-	var s main.Size
+	var s influxd.Size
 	if err := s.UnmarshalText([]byte("200m")); err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	} else if s != 200*(1<<20) {
@@ -25,7 +25,7 @@ func TestSize_UnmarshalText_GB(t *testing.T) {
 		t.Skip("large gigabyte parsing on 64-bit arch only")
 	}
 
-	var s main.Size
+	var s influxd.Size
 	if err := s.UnmarshalText([]byte("10g")); err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	} else if s != 10*(1<<30) {
@@ -35,7 +35,7 @@ func TestSize_UnmarshalText_GB(t *testing.T) {
 
 // Ensure that a TOML configuration file can be parsed into a Config.
 func TestParseConfig(t *testing.T) {
-	c, err := main.ParseConfig(testFile)
+	c, err := influxd.ParseConfig(testFile)
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	} else if c.Hostname != "myserver.com" {
@@ -62,7 +62,7 @@ func TestParseConfig(t *testing.T) {
 		t.Fatalf("admin assets mismatch: %v", c.Admin.Assets)
 	}
 
-	if c.Data.Port != main.DefaultBrokerPort {
+	if c.Data.Port != influxd.DefaultBrokerPort {
 		t.Fatalf("data port mismatch: %v", c.Data.Port)
 	}
 
@@ -306,31 +306,31 @@ func TestCollectd_ConnectionString(t *testing.T) {
 		name             string
 		defaultBindAddr  string
 		connectionString string
-		config           main.Collectd
+		config           influxd.Collectd
 	}{
 		{
 			name:             "No address or port provided from config",
 			defaultBindAddr:  "192.168.0.1",
 			connectionString: "192.168.0.1:25826",
-			config:           main.Collectd{},
+			config:           influxd.Collectd{},
 		},
 		{
 			name:             "address provided, no port provided from config",
 			defaultBindAddr:  "192.168.0.1",
 			connectionString: "192.168.0.2:25826",
-			config:           main.Collectd{Addr: "192.168.0.2"},
+			config:           influxd.Collectd{Addr: "192.168.0.2"},
 		},
 		{
 			name:             "no address provided, port provided from config",
 			defaultBindAddr:  "192.168.0.1",
 			connectionString: "192.168.0.1:25827",
-			config:           main.Collectd{Port: 25827},
+			config:           influxd.Collectd{Port: 25827},
 		},
 		{
 			name:             "both address and port provided from config",
 			defaultBindAddr:  "192.168.0.1",
 			connectionString: "192.168.0.2:25827",
-			config:           main.Collectd{Addr: "192.168.0.2", Port: 25827},
+			config:           influxd.Collectd{Addr: "192.168.0.2", Port: 25827},
 		},
 	}
 
--- tests/integrations/server_single_test.go
+++ tests/integrations/server_single_test.go
@@ -1,18 +1,44 @@
 package integrations_test
 
-import (
-	"testing"
+//import (
+//"io/ioutil"
+//"net/url"
+//"os"
+//"testing"
 
-	"github.com/influxdb/influxdb/tests/integrations"
-)
+//"github.com/influxdb/influxdb/tests/integrations"
+//)
 
-func TestNewServer(t *testing.T) {
-	s, err := integrations.NewServer()
-	if err != nil {
-		t.Fatalf("Unable to create a new server: %v", err)
-	}
-	err = s.Start()
-	if err != nil {
-		t.Fatalf("Unable to start server: %v", err)
-	}
-}
+//func TestNewServer(t *testing.T) {
+//var err error
+//c := integrations.Config{}
+
+//c.BrokerURL = &url.URL{Scheme: "http:", Host: "localhost:9000"}
+//c.BrokerDir, err = ioutil.TempDir("", "broker_9000")
+//if err != nil {
+//t.Fatal(err)
+//}
+//// Be sure to clean up when we are dong
+//defer func() {
+//os.Remove(c.BrokerDir)
+//}()
+
+//c.ServerURL = &url.URL{Scheme: "http:", Host: "localhost:9000"}
+//c.ServerDir, err = ioutil.TempDir("", "data_9000")
+//if err != nil {
+//t.Fatal(err)
+//}
+//// Be sure to clean up when we are dong
+//defer func() {
+//os.Remove(c.ServerDir)
+//}()
+
+//s, err := integrations.NewServer(c)
+//if err != nil {
+//t.Fatalf("Unable to create a new server: %v", err)
+//}
+//err = s.Start()
+//if err != nil {
+//t.Fatalf("Unable to start server: %v", err)
+//}
+//}
