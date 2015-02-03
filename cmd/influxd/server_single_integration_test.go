72fb1486603ba9e71202414a8bdb041167432963
Cory LaNou <cory@lanou.com>
2015-02-02 17:20:34 -0700

    single server integration test is working

--- cmd/influxd/server_single_integration_test.go
+++ cmd/influxd/server_single_integration_test.go
@@ -10,19 +10,17 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"strconv"
 	"testing"
 	"time"
 
-	"github.com/influxdb/influxdb"
+	"github.com/influxdb/influxdb/client"
 	"github.com/influxdb/influxdb/influxql"
 
 	main "github.com/influxdb/influxdb/cmd/influxd"
 )
 
 func TestNewServer(t *testing.T) {
-	// Uncomment this to see the test fail when running for a second time in a row
-	//t.Skip()
-
 	var (
 		join    = ""
 		version = "x.x"
@@ -93,15 +91,15 @@ func TestNewServer(t *testing.T) {
 	t.Log("Creating database")
 
 	u := urlFor(c.BrokerURL(), "query", url.Values{"q": []string{"CREATE DATABASE foo"}})
-	client := http.Client{Timeout: 100 * time.Millisecond}
+	httpClient := http.Client{Timeout: 100 * time.Millisecond}
 
-	resp, err := client.Get(u.String())
+	resp, err := httpClient.Get(u.String())
 	if err != nil {
 		t.Fatalf("Couldn't create database: %s", err)
 	}
 	defer resp.Body.Close()
 
-	var results influxdb.Results
+	var results client.Results
 	err = json.NewDecoder(resp.Body).Decode(&results)
 	if err != nil {
 		t.Fatalf("Couldn't decode results: %v", err)
@@ -122,7 +120,7 @@ func TestNewServer(t *testing.T) {
 	// Query the database exists
 	u = urlFor(c.BrokerURL(), "query", url.Values{"q": []string{"SHOW DATABASES"}})
 
-	resp, err = client.Get(u.String())
+	resp, err = httpClient.Get(u.String())
 	if err != nil {
 		t.Fatalf("Couldn't query databases: %s", err)
 	}
@@ -141,21 +139,18 @@ func TestNewServer(t *testing.T) {
 		t.Fatalf("show databases failed.  Unexpected status code.  expected: %d, actual %d", http.StatusOK, resp.StatusCode)
 	}
 
-	if len(results.Results) != 1 {
-		t.Fatalf("show databases failed.  Unexpected results length.  expected: %d, actual %d", 1, len(results.Results))
+	expectedResults := client.Results{
+		Results: []client.Result{
+			{Rows: []influxql.Row{
+				influxql.Row{
+					Columns: []string{"name"},
+					Values:  [][]interface{}{{"foo"}},
+				},
+			}},
+		},
 	}
-
-	rows := results.Results[0].Rows
-	if len(rows) != 1 {
-		t.Fatalf("show databases failed.  Unexpected rows length.  expected: %d, actual %d", 1, len(rows))
-	}
-	row := rows[0]
-	expectedRow := &influxql.Row{
-		Columns: []string{"name"},
-		Values:  [][]interface{}{{"foo"}},
-	}
-	if !reflect.DeepEqual(row, expectedRow) {
-		t.Fatalf("show databases failed.  Unexpected row.  expected: %+v, actual %+v", expectedRow, row)
+	if !reflect.DeepEqual(results, expectedResults) {
+		t.Fatalf("show databases failed.  Unexpected results.  expected: %+v, actual %+v", expectedResults, results)
 	}
 
 	// Create a retention policy
@@ -163,7 +158,7 @@ func TestNewServer(t *testing.T) {
 
 	u = urlFor(c.BrokerURL(), "query", url.Values{"q": []string{"CREATE RETENTION POLICY bar ON foo DURATION 1h REPLICATION 1 DEFAULT"}})
 
-	resp, err = client.Get(u.String())
+	resp, err = httpClient.Get(u.String())
 	if err != nil {
 		t.Fatalf("Couldn't create retention policy: %s", err)
 	}
@@ -196,7 +191,7 @@ func TestNewServer(t *testing.T) {
 	buf := []byte(fmt.Sprintf(`{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "tags": {"host": "server01"},"timestamp": %d, "precision":"n","values": {"value": 100}}]}`, now.UnixNano()))
 	t.Logf("Writing raw data: %s", string(buf))
 
-	resp, err = client.Post(u.String(), "application/json", bytes.NewReader(buf))
+	resp, err = httpClient.Post(u.String(), "application/json", bytes.NewReader(buf))
 	if err != nil {
 		t.Fatalf("Couldn't write data: %s", err)
 	}
@@ -205,13 +200,14 @@ func TestNewServer(t *testing.T) {
 	}
 
 	// Need some time for server to get consensus and write data
+	// TODO corylanou query the status endpoint for the server and wait for the index to update to know the write was applied
 	time.Sleep(100 * time.Millisecond)
 
 	// Query the data exists
 	t.Log("Query data")
 	u = urlFor(c.BrokerURL(), "query", url.Values{"q": []string{`select value from "foo"."bar".cpu`}, "db": []string{"foo"}})
 
-	resp, err = client.Get(u.String())
+	resp, err = httpClient.Get(u.String())
 	if err != nil {
 		t.Fatalf("Couldn't query databases: %s", err)
 	}
@@ -238,24 +234,26 @@ func TestNewServer(t *testing.T) {
 		t.Fatalf("query databases failed.  Unexpected status code.  expected: %d, actual %d", http.StatusOK, resp.StatusCode)
 	}
 
-	if len(results.Results) != 1 {
-		t.Fatalf("query databases failed.  Unexpected results length.  expected: %d, actual %d", 1, len(results.Results))
-	}
-
-	rows = results.Results[0].Rows
-	if len(rows) != 1 {
-		t.Fatalf("query databases failed.  Unexpected rows length.  expected: %d, actual %d", 1, len(rows))
-	}
-	row = rows[0]
-	expectedRow = &influxql.Row{
-		Name:    "cpu",
-		Columns: []string{"time", "value"},
-		Values: [][]interface{}{
-			[]interface{}{now.UnixNano(), 100},
+	strNow := strconv.FormatInt(now.UnixNano(), 10)
+	expectedResults = client.Results{
+		Results: []client.Result{
+			{Rows: []influxql.Row{
+				{
+					Name:    "cpu",
+					Columns: []string{"time", "value"},
+					Values: [][]interface{}{
+						[]interface{}{json.Number(strNow), json.Number("100")},
+					},
+				}}},
 		},
 	}
-	if !reflect.DeepEqual(row, expectedRow) {
-		t.Fatalf("query databases failed.  Unexpected row.  expected: %+v, actual %+v", expectedRow, row)
+
+	if !reflect.DeepEqual(results, expectedResults) {
+		t.Logf("Expected:\n")
+		t.Logf("%#v\n", expectedResults)
+		t.Logf("Actual:\n")
+		t.Logf("%#v\n", results)
+		t.Fatalf("query databases failed.  Unexpected results.")
 	}
 }
 
