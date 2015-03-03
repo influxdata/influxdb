94d4287af991aec8fda9df4f4fda6735843017a6
Joseph Rothrock <rothrock@rothrock.org>
2015-03-03 12:57:01 -0800

    rewrite urlgen in go
    Fix outer enclosing json.
    
    Add date option.

--- tests/urlgen/urlgen.go
+++ tests/urlgen/urlgen.go
@@ -11,9 +11,10 @@ func main() {
 	intervalN := flag.Int("interval", 10, "interval")
 	seriesN := flag.Int("series", 1, "Number of unique series to generate.")
 	clientN := flag.Int("clients", 10, "Number of clients to simulate.")
+	startDate := flag.String("start", time.Now().Format(time.RFC3339), "Date to start with.")
 	flag.Parse()
 
-	t := time.Date(2010, time.January, 1, 8, 0, 0, 0, time.UTC)
+	t, _ := time.Parse(time.RFC3339, *startDate)
 	oneSecond := 1 * time.Second
 
 	for i := 0; i < *clientN; i++ {
@@ -28,11 +29,22 @@ func main() {
 					Fields:    map[string]interface{}{"value": 100},
 				})
 			}
-			buf, _ := json.Marshal(points)
+			batch := &Batch{
+				Database:        "db",
+				RetentionPolicy: "raw",
+				Points:          points,
+			}
+			buf, _ := json.Marshal(batch)
 
 			fmt.Printf("http://localhost:8086/write POST %s\n", buf)
 		}
 	}
+}
+
+type Batch struct {
+	Database        string   `json:"database"`
+	RetentionPolicy string   `json:"retentionPolicy"`
+	Points          []*Point `json:"points"`
 }
 
 type Point struct {
