6ceae8ba3ca610314aec6ad1f4631688311d0fab
Philip O'Toole <philip@influxdb.com>
2015-03-03 15:02:42 -0800

    Add WHERE field test, on 1 field

--- cmd/influxd/server_integration_test.go
+++ cmd/influxd/server_integration_test.go
@@ -270,6 +270,12 @@ func runTestsData(t *testing.T, testName string, nodes Cluster, database, retent
 		// WHERE fields queries
 		{
 			reset:    true,
+			name:     "WHERE fields",
+			write:    `{"database" : "%DB%", "retentionPolicy" : "%RP%", "points": [{"name": "cpu", "timestamp": "2015-02-28T01:03:36.703820946Z", "fields": {"alert_id": "alert", "tenant_id": "tenant"}}]}`,
+			query:    `SELECT alert_id FROM "%DB%"."%RP%".cpu WHERE alert_id='alert'`,
+			expected: `{"results":[{"series":[{"name":"cpu","columns":["time","alert_id"],"values":[["2015-02-28T01:03:36.703820946Z","alert"]]}]}]}`,
+		},
+		{
 			name:     "WHERE fields with AND query",
 			write:    `{"database" : "%DB%", "retentionPolicy" : "%RP%", "points": [{"name": "cpu", "timestamp": "2015-02-28T01:03:36.703820946Z", "fields": {"alert_id": "alert", "tenant_id": "tenant"}}]}`,
 			query:    `SELECT alert_id FROM "%DB%"."%RP%".cpu WHERE alert_id='alert' AND tenant_id='tenant'`,
