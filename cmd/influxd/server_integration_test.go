25a43a8f117e6cb0deb969eab54ce175f3072f28
Jason Wilder <jason@influxdb.com>
2015-04-21 10:26:46 -0600

    Change 6 node test to 5 node

--- cmd/influxd/server_integration_test.go
+++ cmd/influxd/server_integration_test.go
@@ -1459,9 +1459,9 @@ func Test3NodeServerFailover(t *testing.T) {
 
 // ensure that all queries work if there are more nodes in a cluster than the replication factor
 // and there is more than 1 shards
-func Test6NodeClusterPartiallyReplicated(t *testing.T) {
+func Test5NodeClusterPartiallyReplicated(t *testing.T) {
 	t.Parallel()
-	testName := "6-node server integration partial replication"
+	testName := "5-node server integration partial replication"
 	if testing.Short() {
 		t.Skip(fmt.Sprintf("skipping '%s'", testName))
 	}
@@ -1470,11 +1470,11 @@ func Test6NodeClusterPartiallyReplicated(t *testing.T) {
 		os.RemoveAll(dir)
 	}()
 
-	nodes := createCombinedNodeCluster(t, testName, dir, 6, nil)
+	nodes := createCombinedNodeCluster(t, testName, dir, 5, nil)
 	defer nodes.Close()
 
-	runTestsData(t, testName, nodes, "mydb", "myrp", 3)
-	runTest_rawDataReturnsInOrder(t, testName, nodes, "mydb", "myrp", 3)
+	runTestsData(t, testName, nodes, "mydb", "myrp", 2)
+	runTest_rawDataReturnsInOrder(t, testName, nodes, "mydb", "myrp", 2)
 }
 
 func TestClientLibrary(t *testing.T) {
