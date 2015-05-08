19ece442d8335db95462419fe1e461158e7b35d4
Ben Johnson <benbjohnson@yahoo.com>
2015-05-08 11:56:08 -0600

    Add meta.Store accessors.

--- meta/store.go
+++ meta/store.go
@@ -11,22 +11,28 @@ type Store interface {
 	CreateContinuousQuery(q *influxql.CreateContinuousQueryStatement) (*ContinuousQueryInfo, error)
 	DropContinuousQuery(q *influxql.DropContinuousQueryStatement) error
 
+	Node(id uint64) (*NodeInfo, error)
+	NodeByHost(host string) (*NodeInfo, error)
 	CreateNode(host string) (*NodeInfo, error)
 	DeleteNode(id uint64) error
 
-	CreateDatabase(name string) (*NodeInfo, error)
+	Database(name string) (*DatabaseInfo, error)
+	CreateDatabase(name string) (*DatabaseInfo, error)
 	CreateDatabaseIfNotExists(name string) (*DatabaseInfo, error)
 	DropDatabase(name string) error
 
+	RetentionPolicy(database, name string) (*RetentionPolicyInfo, error)
 	CreateRetentionPolicy(database string, rp *RetentionPolicyInfo) (*RetentionPolicyInfo, error)
 	CreateRetentionPolicyIfNotExists(database string, rp *RetentionPolicyInfo) (*RetentionPolicyInfo, error)
 	SetDefaultRetentionPolicy(database, name string) error
 	UpdateRetentionPolicy(database, name string, rpu *RetentionPolicyUpdate) (*RetentionPolicyInfo, error)
 	DeleteRetentionPolicy(database, name string) error
 
+	ShardGroup(database, policy string, timestamp time.Time) (*ShardGroupInfo, error)
 	CreateShardGroupIfNotExists(database, policy string, timestamp time.Time) (*ShardGroupInfo, error)
 	DeleteShardGroup(database, policy string, shardID uint64) error
 
+	User(username string) (*UserInfo, error)
 	CreateUser(username, password string, admin bool) (*UserInfo, error)
 	UpdateUser(username, password string) (*UserInfo, error)
 	DeleteUser(username string) error
