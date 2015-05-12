8ac6d80016ab687332b00ab78d8cfa761ffa3cbe
Philip O'Toole <philip@influxdb.com>
2015-05-12 11:58:18 -0700

    Numeric fields only when using numeric aggregates

--- tx.go
+++ tx.go
@@ -93,6 +93,21 @@ func (tx *tx) CreateMapReduceJobs(stmt *influxql.SelectStatement, tagKeys []stri
 			}
 		}
 
+		// If a numerical aggregate is requested, ensure it is only performed on numeric data.
+		for _, a := range stmt.FunctionCalls() {
+			lit, ok := a.Args[0].(*influxql.VarRef)
+			if !ok {
+				return nil, fmt.Errorf("aggregate call didn't contain a field %s", a.String())
+			}
+			if influxql.IsNumeric(a) {
+				f := m.FieldByName(lit.Val)
+				if f.Type != influxql.Float && f.Type != influxql.Integer {
+					return nil, fmt.Errorf("aggregate '%s' requires numerical field values. Field '%s' is of type %s",
+						a.Name, f.Name, f.Type)
+				}
+			}
+		}
+
 		// Grab time range from statement.
 		tmin, tmax := influxql.TimeRange(stmt.Condition)
 		if tmax.IsZero() {
