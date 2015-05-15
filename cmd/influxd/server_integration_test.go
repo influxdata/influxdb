851c0ab9aecc54a936c7ba00c90b9f7c010b9f60
Philip O'Toole <philip@influxdb.com>
2015-05-15 11:49:26 -0700

    Don't call InOrder

--- cmd/influxd/server_integration_test.go
+++ cmd/influxd/server_integration_test.go
@@ -1589,7 +1589,6 @@ func TestSingleServer(t *testing.T) {
 	defer nodes.Close()
 
 	runTestsData(t, testName, nodes, "mydb", "myrp", len(nodes))
-	runTest_rawDataReturnsInOrder(t, testName, nodes, "inorderdb", "inorderrp", len(nodes))
 }
 
 func Test3NodeServer(t *testing.T) {
@@ -1606,7 +1605,6 @@ func Test3NodeServer(t *testing.T) {
 	defer nodes.Close()
 
 	runTestsData(t, testName, nodes, "mydb", "myrp", len(nodes))
-	runTest_rawDataReturnsInOrder(t, testName, nodes, "inorderdb", "inorderrp", len(nodes))
 }
 
 func Test3NodeServerFailover(t *testing.T) {
@@ -1627,7 +1625,6 @@ func Test3NodeServerFailover(t *testing.T) {
 	defer nodes.Close()
 
 	runTestsData(t, testName, nodes, "mydb", "myrp", len(nodes))
-	runTest_rawDataReturnsInOrder(t, testName, nodes, "inorderdb", "inorderrp", len(nodes))
 }
 
 // ensure that all queries work if there are more nodes in a cluster than the replication factor
@@ -1645,7 +1642,6 @@ func Test5NodeClusterPartiallyReplicated(t *testing.T) {
 	defer nodes.Close()
 
 	runTestsData(t, testName, nodes, "mydb", "myrp", 2)
-	runTest_rawDataReturnsInOrder(t, testName, nodes, "inorderdb", "inorderrp", 2)
 }
 
 func TestClientLibrary(t *testing.T) {
