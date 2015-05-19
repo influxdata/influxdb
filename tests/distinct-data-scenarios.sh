531f1e72cefb92ffc7e3aebad05764cb4fff6248
Cory LaNou <cory@lanou.com>
2015-05-19 10:52:24 -0600

    add in non-distinct data to distinct seed

--- tests/distinct-data-scenarios.sh
+++ tests/distinct-data-scenarios.sh
@@ -8,18 +8,22 @@ echo "inserting data"
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-01T00:00:00.000Z","fields": {"value": 1.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-01T08:00:00.000Z","fields": {"value": 1.2}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-01T16:00:00.000Z","fields": {"value": 1.3}}]}' -H "Content-Type: application/json" http://localhost:8086/write
+curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-01T16:59:00.000Z","fields": {"value": 1.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-02T00:00:00.000Z","fields": {"value": 2.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-02T08:00:00.000Z","fields": {"value": 2.2}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-02T16:00:00.000Z","fields": {"value": 2.3}}]}' -H "Content-Type: application/json" http://localhost:8086/write
+curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-02T16:59:00.000Z","fields": {"value": 2.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-03T00:00:00.000Z","fields": {"value": 3.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-03T08:00:00.000Z","fields": {"value": 3.2}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-03T16:00:00.000Z","fields": {"value": 3.3}}]}' -H "Content-Type: application/json" http://localhost:8086/write
+curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-03T16:59:00.000Z","fields": {"value": 3.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-04T00:00:00.000Z","fields": {"value": 4.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-04T08:00:00.000Z","fields": {"value": 4.2}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-04T16:00:00.000Z","fields": {"value": 4.3}}]}' -H "Content-Type: application/json" http://localhost:8086/write
+curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "cpu", "time": "2015-05-04T16:59:00.000Z","fields": {"value": 4.1}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "names", "time": "2015-05-01T00:00:00.000Z","fields": {"first": "suzie", "last": "smith"}}]}' -H "Content-Type: application/json" http://localhost:8086/write
 curl -d '{"database" : "foo", "retentionPolicy" : "bar", "points": [{"name": "names", "time": "2015-05-01T08:00:00.000Z","fields": {"first": "frank", "last": "smith"}}]}' -H "Content-Type: application/json" http://localhost:8086/write
