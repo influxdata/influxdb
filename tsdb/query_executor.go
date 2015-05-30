97f9670fa363efae7882eec38d4d9f5e6f669846
Ben Johnson <benbjohnson@yahoo.com>
2015-05-30 07:19:19 -0600

    Fix influxql.Rows.

--- tsdb/query_executor.go
+++ tsdb/query_executor.go
@@ -531,7 +531,7 @@ func (q *QueryExecutor) executeShowMeasurementsStatement(stmt *influxql.ShowMeas
 
 	// Make a result.
 	result := &influxql.Result{
-		Series: []influxql.Row{row},
+		Series: []*influxql.Row{row},
 	}
 
 	return result
