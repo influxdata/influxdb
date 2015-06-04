df003cc0d784c76cb708db9504ce9892bc40f37a
Philip O'Toole <philip.otoole@yahoo.com>
2015-06-03 21:11:30 -0700

    Add retention policy enforcer

---
+++ meta/retention_policy_enforcer.go
@@ -0,0 +1,42 @@
+package meta
+
+import (
+	"time"
+)
+
+type RetentionPolicyEnforcer struct {
+	Store interface {
+		Databases() (dis []DatabaseInfo, err error)
+		DeleteShardGroup(database, policy string, id uint64) error
+	}
+}
+
+// Enforce removes shard groups that represent data older than the retention policies
+// allow. It then returns a slice of the shard IDs that must be deleted.
+func (r *RetentionPolicyEnforcer) Enforce() ([]uint64, error) {
+	dis, err := r.Store.Databases()
+	if err != nil {
+		return nil, err
+	}
+
+	// Check all shard groups.
+	var ids []uint64
+	for _, di := range dis {
+		for _, rp := range di.RetentionPolicies {
+			for _, g := range rp.ShardGroups {
+				if rp.Duration != 0 && g.EndTime.Add(rp.Duration).Before(time.Now().UTC()) {
+					for _, sh := range g.Shards {
+						ids = append(ids, sh.ID)
+					}
+
+					if err := r.Store.DeleteShardGroup(di.Name, rp.Name, g.ID); err != nil {
+						return nil, err
+					}
+
+				}
+			}
+		}
+	}
+
+	return ids, nil
+}
