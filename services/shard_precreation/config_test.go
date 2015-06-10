b5ad92be70801df0d7a33498eeb0e2e0600704e2
Philip O'Toole <philip@influxdb.com>
2015-06-10 10:30:23 -0700

    Create shard precreation service
    Initial commit includes basic configuration control.

---
+++ services/shard_precreation/config_test.go
@@ -0,0 +1,31 @@
+package shard_precreation_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	"github.com/influxdb/influxdb/services/shard_precreation"
+)
+
+func TestConfig_Parse(t *testing.T) {
+	// Parse configuration.
+	var c shard_precreation.Config
+	if _, err := toml.Decode(`
+enabled = true
+check-interval = "2m"
+advance-period = "10m"
+`, &c); err != nil {
+
+		t.Fatal(err)
+	}
+
+	// Validate configuration.
+	if !c.Enabled {
+		t.Fatalf("unexpected enabled state: %v", c.Enabled)
+	} else if time.Duration(c.CheckInterval) != 2*time.Minute {
+		t.Fatalf("unexpected check interval: %s", c.CheckInterval)
+	} else if time.Duration(c.AdvancePeriod) != 10*time.Minute {
+		t.Fatalf("unexpected advance period: %s", c.AdvancePeriod)
+	}
+}
