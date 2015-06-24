7037b2f027e85f4edf4461e538f01c9f5284fd29
Philip O'Toole <philip@influxdb.com>
2015-06-24 16:30:12 -0700

    Add very simple helper script for nightly builds

---
+++ nightly.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPO_DIR=`mktemp -d`
+echo "Using $REPO_DIR for all work..."
+
+cd $REPO_DIR
+export GOPATH=$PWD
+mkdir -p $GOPATH/src/github.com/influxdb
+cd $GOPATH/src/github.com/influxdb
+git clone https://github.com/influxdb/influxdb.git
+
+cd $GOPATH/src/github.com/influxdb/influxdb
+NIGHTLY_BUILD=true ./package.sh nightly
