259502fcb003397368e0b676bfbeb2431ddafd3f
Philip O'Toole <philip@influxdb.com>
2015-06-26 22:52:19 -0400

    Start fleshing out (local) shard mapper
    Does not build.

--- tsdb/query_engine.go
+++ tsdb/query_engine.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/boltdb/bolt"
 	"github.com/influxdb/influxdb/influxql"
 	"github.com/influxdb/influxdb/meta"
 )
@@ -39,6 +40,8 @@ type Planner struct {
 
 	// Local store for local shards. Would be elegant if all shards came from Cluster. REVISIT THIS!
 	// The TSDBSTORE interface in the cluster service could be expanded to "ShardMapper(shard ID)"
+	// this may not be possible due to circular imports. This file couldn't import Cluster, since
+	// imports this package.
 	store *Store
 
 	Logger *log.Logger
@@ -94,7 +97,7 @@ func (p *Planner) Plan(stmt *influxql.SelectStatement, chunkSize int) (*Executor
 				// In this case, since there is no data, don't make a mapper.
 				continue
 			}
-			mappers = append(mappers, &ShardMapper{shard})
+			mappers = append(mappers, NewShardMapper(shard))
 		} else {
 			mapper, err := p.Cluster.NewMapper(sh.ID)
 			if err != nil {
@@ -133,6 +136,9 @@ func (e *Executor) Execute() <-chan *influxql.Row {
 
 type ShardMapper struct {
 	shard *Shard
+
+	tx            *bolt.Tx                  // Read transaction for this shard.
+	tagSetCursors map[string][]*shardCursor // Multiple cursors per TagSet.
 }
 
 func NewShardMapper(shard *Shard) *ShardMapper {
@@ -140,14 +146,89 @@ func NewShardMapper(shard *Shard) *ShardMapper {
 }
 
 func (sm *ShardMapper) Open() error {
+	tx, err := sm.shard.DB().Begin(false)
+	if err != nil {
+		return err
+	}
+	sm.tx = tx
+
 	return nil
 }
 
 func (sm *ShardMapper) Close() {
+	if sm.tx != nil {
+		_ = sm.tx.Rollback()
+	}
 }
 
 func (sm *ShardMapper) Begin(stmt *influxql.SelectStatement, chunkSize int) error {
-	return nil
+	for _, src := range stmt.Sources {
+		mm := src.(*influxql.Measurement)
+
+		m := shard.index.Measurement(mm.Name)
+		if m == nil {
+			return nil, ErrMeasurementNotFound(influxql.QuoteIdent([]string{mm.Database, "", mm.Name}...))
+		}
+
+		// Validate the fields and tags asked for exist and keep track of which are in the select vs the where
+		var selectFields []string
+		var whereFields []string
+		var selectTags []string
+
+		for _, n := range stmt.NamesInSelect() {
+			if m.HasField(n) {
+				selectFields = append(selectFields, n)
+				continue
+			}
+			if !m.HasTagKey(n) {
+				return nil, fmt.Errorf("unknown field or tag name in select clause: %s", n)
+			}
+			selectTags = append(selectTags, n)
+		}
+		for _, n := range stmt.NamesInWhere() {
+			if n == "time" {
+				continue
+			}
+			if m.HasField(n) {
+				whereFields = append(whereFields, n)
+				continue
+			}
+			if !m.HasTagKey(n) {
+				return nil, fmt.Errorf("unknown field or tag name in where clause: %s", n)
+			}
+		}
+
+		if len(selectFields) == 0 && len(stmt.FunctionCalls()) == 0 {
+			return nil, fmt.Errorf("select statement must include at least one field or function call")
+		}
+
+		// Validate that group by is not a field
+		for _, d := range stmt.Dimensions {
+			switch e := d.Expr.(type) {
+			case *influxql.VarRef:
+				if !m.HasTagKey(e.Val) {
+					return nil, fmt.Errorf("can not use field in group by clause: %s", e.Val)
+				}
+			}
+		}
+
+		// Determine the interval and GROUP BY tag keys.
+		interval, tagKeys, err := stmt.Dimensions.Normalize()
+		if err != nil {
+			return nil, err
+		}
+
+		// get the sorted unique tag sets for this statement.
+		tagSets, err := m.TagSets(stmt, tagKeys)
+		if err != nil {
+			return nil, err
+		}
+
+		// -- looks like I need some sort of collection of cursors, grouped by tag set. Within each
+		// will be the codec, series keys, and a bunch of other stuff. Moving these cursors along
+		// will have to be driven by NextChunk().
+
+	}
 }
 
 func (sm *ShardMapper) NextChunk() (tagSet string, result interface{}, interval int, err error) {
