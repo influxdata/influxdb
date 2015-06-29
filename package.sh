aea509474e4445daf2868742b945f92e81c43903
Philip O'Toole <philip.otoole@yahoo.com>
2015-06-29 14:29:50 -0400

    Revert force of -x during packaging
    [ci skip]

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 
 ###########################################################################
 # Packaging script which creates debian and RPM packages. It optionally
