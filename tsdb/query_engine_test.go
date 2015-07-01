28d5ffa8b8d92e00fbbdb8e64d6df3b36c43a000
Philip O'Toole <philip@influxdb.com>
2015-07-01 14:27:58 -0400

    Extend unit test of ShardMapper

--- tsdb/query_engine_test.go
+++ tsdb/query_engine_test.go
@@ -1,6 +1,7 @@
 package tsdb
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -12,36 +13,62 @@ import (
 	"github.com/influxdb/influxdb/influxql"
 )
 
-func TestShardMapper_WriteAndSingleMapper(t *testing.T) {
+func TestShardMapper_WriteAndSingleMapperRawQuery(t *testing.T) {
 	tmpDir, _ := ioutil.TempDir("", "shard_test")
 	defer os.RemoveAll(tmpDir)
 	shard := mustCreateShard(tmpDir)
 
-	pt := NewPoint(
+	pt1 := NewPoint(
 		"cpu",
-		map[string]string{"host": "server"},
+		map[string]string{"host": "serverA"},
 		map[string]interface{}{"value": 42},
 		time.Unix(1, 2),
 	)
-	err := shard.WritePoints([]Point{pt})
+	pt2 := NewPoint(
+		"cpu",
+		map[string]string{"host": "serverB"},
+		map[string]interface{}{"value": 60},
+		time.Unix(1, 3),
+	)
+	err := shard.WritePoints([]Point{pt1, pt2})
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	stmt := mustParseSelectStatement("SELECT value FROM cpu")
-	mapper := NewShardMapper(shard)
-	if err := mapper.Open(); err != nil {
-		t.Fatalf("failed to open mapper: %s", err.Error())
-	}
-	if err := mapper.Begin(stmt, 1); err != nil {
-		t.Fatalf("failed to begin mapper: %s", err.Error())
+	var tests = []struct {
+		stmt     string
+		expected string
+	}{
+		{
+			stmt:     `SELECT value FROM eth`,
+			expected: `null`,
+		},
+		{
+			stmt:     `SELECT value FROM cpu`,
+			expected: `[{"Time":1000000002,"Values":42},{"Time":1000000003,"Values":60}]`,
+		},
+		{
+			stmt:     `SELECT value FROM cpu WHERE host='serverA'`,
+			expected: `[{"Time":1000000002,"Values":42}]`,
+		},
+		{
+			stmt:     `SELECT value FROM cpu WHERE host='serverB'`,
+			expected: `[{"Time":1000000003,"Values":60}]`,
+		},
+		{
+			stmt:     `SELECT value FROM cpu WHERE host='serverC'`,
+			expected: `null`,
+		},
 	}
 
-	if _, r, _, _ := mapper.NextChunk(); r == nil {
-		t.Fatalf("didn't receive result when expected: %v", r)
-	}
-	if _, r, _, _ := mapper.NextChunk(); r != nil {
-		t.Fatal("received result when unexpected")
+	for i, tt := range tests {
+		stmt := mustParseSelectStatement(tt.stmt)
+		mapper := beginMapperOrFail(t, shard, stmt)
+
+		got := nextChunkAsJson(t, mapper)
+		if got != tt.expected {
+			t.Fatalf("test %d got %s, expected %s", i, got, tt.expected)
+		}
 	}
 }
 
@@ -62,4 +89,27 @@ func mustParseSelectStatement(s string) *influxql.SelectStatement {
 		panic(err)
 	}
 	return stmt.(*influxql.SelectStatement)
+}
+
+func beginMapperOrFail(t *testing.T, shard *Shard, stmt *influxql.SelectStatement) *ShardMapper {
+	mapper := NewShardMapper(shard)
+	if err := mapper.Open(); err != nil {
+		t.Fatalf("failed to open mapper: %s", err.Error())
+	}
+	if err := mapper.Begin(stmt, 1); err != nil {
+		t.Fatalf("failed to begin mapper: %s", err.Error())
+	}
+	return mapper
+}
+
+func nextChunkAsJson(t *testing.T, mapper *ShardMapper) string {
+	_, r, _, err := mapper.NextChunk()
+	if err != nil {
+		t.Fatalf("failed to get next chunk from mapper: %s", err.Error())
+	}
+	b, err := json.Marshal(r)
+	if err != nil {
+		t.Fatalf("failed to marshal chunk as JSON: %s", err.Error())
+	}
+	return string(b)
 }
