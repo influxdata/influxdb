a84c48bff60bbdd7d05f14a430be8b6efa21049c
Philip O'Toole <philip@influxdb.com>
2015-07-02 12:32:28 -0400

    Allow the PointWriter timeout to be configurable

--- cluster/config_test.go
+++ cluster/config_test.go
@@ -13,12 +13,15 @@ func TestConfig_Parse(t *testing.T) {
 	var c cluster.Config
 	if _, err := toml.Decode(`
 shard-writer-timeout = "10s"
+write-timeout = "20s"
 `, &c); err != nil {
 		t.Fatal(err)
 	}
 
 	// Validate configuration.
 	if time.Duration(c.ShardWriterTimeout) != 10*time.Second {
-		t.Fatalf("unexpected bind address: %s", c.ShardWriterTimeout)
+		t.Fatalf("unexpected shard-writer timeout: %s", c.ShardWriterTimeout)
+	} else if time.Duration(c.WriteTimeout) != 20*time.Second {
+		t.Fatalf("unexpected write timeout s: %s", c.WriteTimeout)
 	}
 }
