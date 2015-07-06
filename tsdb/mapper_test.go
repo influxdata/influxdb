42c2b63d66fb7c9de3e4afb3880a4c1c93b2d336
Philip O'Toole <philip@influxdb.com>
2015-07-06 10:46:26 -0400

    Mapper tests working again

--- tsdb/mapper_test.go
+++ tsdb/mapper_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
@@ -13,7 +14,7 @@ import (
 	"github.com/influxdb/influxdb/influxql"
 )
 
-func TestShardMapper_WriteAndSingleMapperRawQuery(t *testing.T) {
+func TestShardMapper_WriteAndSingleMapperTagSets(t *testing.T) {
 	tmpDir, _ := ioutil.TempDir("", "shard_test")
 	defer os.RemoveAll(tmpDir)
 	shard := mustCreateShard(tmpDir)
@@ -43,50 +44,132 @@ func TestShardMapper_WriteAndSingleMapperRawQuery(t *testing.T) {
 	}{
 		{
 			stmt:     `SELECT value FROM cpu`,
-			expected: []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42},{"time":2000000000,"value":60}]}`},
+			expected: []string{"cpu"},
 		},
 		{
-			stmt: `SELECT value FROM cpu GROUP BY host`,
+			stmt:     `SELECT value FROM cpu GROUP BY host`,
+			expected: []string{"cpuhost|serverA", "cpuhost|serverB"},
+		},
+		{
+			stmt:     `SELECT value FROM cpu GROUP BY region`,
+			expected: []string{"cpuregion|us-east"},
+		},
+		{
+			stmt:     `SELECT value FROM cpu WHERE host='serverA'`,
+			expected: []string{"cpu"},
+		},
+		{
+			stmt:     `SELECT value FROM cpu WHERE host='serverB'`,
+			expected: []string{"cpu"},
+		},
+		{
+			stmt:     `SELECT value FROM cpu WHERE host='serverC'`,
+			expected: []string{},
+		},
+	}
+
+	for _, tt := range tests {
+		stmt := mustParseSelectStatement(tt.stmt)
+		mapper := openMapperOrFail(t, shard, stmt)
+		got := mapper.TagSets()
+		if !reflect.DeepEqual(got, tt.expected) {
+			t.Errorf("test '%s'\n\tgot      %s\n\texpected %s", tt.stmt, got, tt.expected)
+		}
+	}
+}
+
+func TestShardMapper_WriteAndSingleMapperRawQuery(t *testing.T) {
+	tmpDir, _ := ioutil.TempDir("", "shard_test")
+	defer os.RemoveAll(tmpDir)
+	shard := mustCreateShard(tmpDir)
+
+	pt1time := time.Unix(1, 0).UTC()
+	pt1 := NewPoint(
+		"cpu",
+		map[string]string{"host": "serverA", "region": "us-east"},
+		map[string]interface{}{"value": 42},
+		pt1time,
+	)
+	pt2time := time.Unix(2, 0).UTC()
+	pt2 := NewPoint(
+		"cpu",
+		map[string]string{"host": "serverB", "region": "us-east"},
+		map[string]interface{}{"value": 60},
+		pt2time,
+	)
+	err := shard.WritePoints([]Point{pt1, pt2})
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	var tests = []struct {
+		stmt      string
+		reqTagSet string
+		expected  []string
+	}{
+		{
+			stmt:      `SELECT value FROM cpu`,
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42},{"time":2000000000,"value":60}]}`},
+		},
+		{
+			stmt:      `SELECT value FROM cpu GROUP BY host`,
+			reqTagSet: "cpuhost|serverA",
 			expected: []string{
 				`{"Name":"cpu","tags":{"host":"serverA"},"values":[{"time":1000000000,"value":42}]}`,
+			},
+		},
+		{
+			stmt:      `SELECT value FROM cpu GROUP BY host`,
+			reqTagSet: "cpuhost|serverB",
+			expected: []string{
 				`{"Name":"cpu","tags":{"host":"serverB"},"values":[{"time":2000000000,"value":60}]}`,
 			},
 		},
 		{
-			stmt:     `SELECT value FROM cpu GROUP BY region`,
-			expected: []string{`{"Name":"cpu","tags":{"region":"us-east"},"values":[{"time":1000000000,"value":42},{"time":2000000000,"value":60}]}`},
+			stmt:      `SELECT value FROM cpu GROUP BY region`,
+			reqTagSet: "cpuregion|us-east",
+			expected:  []string{`{"Name":"cpu","tags":{"region":"us-east"},"values":[{"time":1000000000,"value":42},{"time":2000000000,"value":60}]}`},
 		},
 		{
-			stmt:     `SELECT value FROM cpu WHERE host='serverA'`,
-			expected: []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42}]}`},
+			stmt:      `SELECT value FROM cpu WHERE host='serverA'`,
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42}]}`},
 		},
 		{
-			stmt:     `SELECT value FROM cpu WHERE host='serverB'`,
-			expected: []string{`{"Name":"cpu","values":[{"time":2000000000,"value":60}]}`},
+			stmt:      `SELECT value FROM cpu WHERE host='serverB'`,
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":2000000000,"value":60}]}`},
 		},
 		{
-			stmt:     `SELECT value FROM cpu WHERE host='serverC'`,
-			expected: []string{`null`},
+			stmt:      `SELECT value FROM cpu WHERE host='serverC'`,
+			reqTagSet: "cpu",
+			expected:  []string{`null`},
 		},
 		{
-			stmt:     `SELECT value FROM cpu WHERE value = 60`,
-			expected: []string{`{"Name":"cpu","values":[{"time":2000000000,"value":60}]}`},
+			stmt:      `SELECT value FROM cpu WHERE value = 60`,
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":2000000000,"value":60}]}`},
 		},
 		{
-			stmt:     `SELECT value FROM cpu WHERE value != 60`,
-			expected: []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42}]}`},
+			stmt:      `SELECT value FROM cpu WHERE value != 60`,
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42}]}`},
 		},
 		{
-			stmt:     fmt.Sprintf(`SELECT value FROM cpu WHERE time = '%s'`, pt1time.Format(influxql.DateTimeFormat)),
-			expected: []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42}]}`},
+			stmt:      fmt.Sprintf(`SELECT value FROM cpu WHERE time = '%s'`, pt1time.Format(influxql.DateTimeFormat)),
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":1000000000,"value":42}]}`},
 		},
 		{
-			stmt:     fmt.Sprintf(`SELECT value FROM cpu WHERE time > '%s'`, pt1time.Format(influxql.DateTimeFormat)),
-			expected: []string{`{"Name":"cpu","values":[{"time":2000000000,"value":60}]}`},
+			stmt:      fmt.Sprintf(`SELECT value FROM cpu WHERE time > '%s'`, pt1time.Format(influxql.DateTimeFormat)),
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu","values":[{"time":2000000000,"value":60}]}`},
 		},
 		{
-			stmt:     fmt.Sprintf(`SELECT value FROM cpu WHERE time > '%s'`, pt2time.Format(influxql.DateTimeFormat)),
-			expected: []string{`{"Name":"cpu"}`},
+			stmt:      fmt.Sprintf(`SELECT value FROM cpu WHERE time > '%s'`, pt2time.Format(influxql.DateTimeFormat)),
+			reqTagSet: "cpu",
+			expected:  []string{`{"Name":"cpu"}`},
 		},
 	}
 
@@ -95,14 +178,11 @@ func TestShardMapper_WriteAndSingleMapperRawQuery(t *testing.T) {
 		mapper := openMapperOrFail(t, shard, stmt)
 
 		for _, s := range tt.expected {
-			got := nextChunkAsJson(t, mapper)
+			got := nextChunkAsJson(t, mapper, tt.reqTagSet)
 			if got != s {
 				t.Errorf("test '%s'\n\tgot      %s\n\texpected %s", tt.stmt, got, tt.expected)
 				break
 			}
-		}
-		if nextChunkAsJson(t, mapper) != `null` {
-			t.Errorf("test '%s' has more data when none expected", tt.stmt)
 		}
 	}
 }
@@ -135,8 +215,8 @@ func openMapperOrFail(t *testing.T, shard *Shard, stmt *influxql.SelectStatement
 	return mapper
 }
 
-func nextChunkAsJson(t *testing.T, mapper *RawMapper) string {
-	r, err := mapper.NextChunk("xxx")
+func nextChunkAsJson(t *testing.T, mapper *RawMapper, tagset string) string {
+	r, err := mapper.NextChunk(tagset)
 	if err != nil {
 		t.Fatalf("failed to get next chunk from mapper: %s", err.Error())
 	}
