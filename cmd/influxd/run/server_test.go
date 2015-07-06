efcfa78f54a6d25dddb1ac429192b1af35a09a5f
Philip O'Toole <philip@influxdb.com>
2015-07-06 15:20:27 -0400

    Correct server test expected error message

--- cmd/influxd/run/server_test.go
+++ cmd/influxd/run/server_test.go
@@ -831,7 +831,7 @@ func TestServer_Query_Tags(t *testing.T) {
 		&Query{
 			name:    "tag without field should return error",
 			command: `SELECT host FROM db0.rp0.cpu`,
-			exp:     `{"results":[{"error":"select statement must include at least one field or function call"}]}`,
+			exp:     `{"results":[{"error":"select statement must include at least one field"}]}`,
 		},
 		&Query{
 			name:    "field with tag should succeed",
