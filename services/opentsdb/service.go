3e29d8821a0cc51f209fcfff84c3621c7f1b2bc7
Nathaniel Cook <nathanielc@qualtrics.com>
2015-07-21 16:11:58 -0600

    catch opentsdb malformed tags if they are missing keys or values

--- services/opentsdb/service.go
+++ services/opentsdb/service.go
@@ -216,7 +216,7 @@ func (s *Service) handleTelnetConn(conn net.Conn) {
 		tags := make(map[string]string)
 		for t := range tagStrs {
 			parts := strings.SplitN(tagStrs[t], "=", 2)
-			if len(parts) != 2 {
+			if len(parts) != 2 || len(parts[0]) == 0 || len(parts[1]) == 0 {
 				s.Logger.Println("TSDBServer: malformed tag data", tagStrs[t])
 				continue
 			}
