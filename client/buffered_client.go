a2c5a0700c2974925e83fc3a8900fa174f8304c4
Marcus Westin <narcvs@gmail.com>
2015-07-24 14:26:13 -0400

    Document BufferedClient

--- client/buffered_client.go
+++ client/buffered_client.go
@@ -2,12 +2,17 @@ package client
 
 import "time"
 
+// BufferConfig is used to specify how frequently a BufferedClient should flush its dataset to the influxdb server.
+// Database: The Database to write points to (gets passed through to BatchPoints).
+// FlushMaxPoints: Buffer at most this many points in memory before flushing to the server.
+// FlushMaxWaitTime: Buffer points in memory for at most this amount of time before flushing to the server.
 type BufferConfig struct {
+	Database         string
 	FlushMaxPoints   int
 	FlushMaxWaitTime time.Duration
-	Database         string
 }
 
+// NewBufferedClient will instantiate and return a connected BufferedClient.
 func NewBufferedClient(clientConfig Config, bufferConfig BufferConfig) (bufferedClient *BufferedClient, err error) {
 	client, err := NewClient(clientConfig)
 	if err != nil {
@@ -26,6 +31,7 @@ func NewBufferedClient(clientConfig Config, bufferConfig BufferConfig) (buffered
 	return
 }
 
+// BufferedClient is used to buffer points in memory and periodically flush them to the server
 type BufferedClient struct {
 	*Client
 	bufferConfig BufferConfig
@@ -36,6 +42,8 @@ type BufferedClient struct {
 	pointsIndex  int
 }
 
+// Add a Point with the given values to the BufferedClient.
+// If the BufferedClient is closed, didAdd is false
 func (b *BufferedClient) Add(measurement string, val interface{}, tags map[string]string, fields map[string]interface{}) (didAdd bool) {
 	ingestChan := b.ingestChan
 	if ingestChan == nil {
@@ -55,6 +63,9 @@ func (b *BufferedClient) Add(measurement string, val interface{}, tags map[strin
 	return
 }
 
+// Close will close the BufferedClient. While closing, it will flush any points from Add()
+// This method executes asynchronously, but it returns a channel which can be read from to ensure that the buffered client
+// Once the client
 func (b *BufferedClient) Close() (didCloseChan chan bool) {
 	didCloseChan = make(chan bool)
 	b.closeChan <- didCloseChan
@@ -64,6 +75,8 @@ func (b *BufferedClient) Close() (didCloseChan chan bool) {
 // Async ingest and flush loop
 //////////////////////////////
 
+// Read ingested points, buffer them in memory, and periodically flush to server.
+// On Close(), drain ingested points, flush to server, and signal that Close has completed.
 func (b *BufferedClient) ingestAndFlushLoop() {
 	for b.ingestChan != nil {
 		select {
@@ -81,6 +94,7 @@ func (b *BufferedClient) ingestAndFlushLoop() {
 	}
 }
 
+// Drain the passed in ingest channel.
 func (b *BufferedClient) drainChan(ingestChan chan Point) {
 	for {
 		select {
@@ -92,6 +106,8 @@ func (b *BufferedClient) drainChan(ingestChan chan Point) {
 	}
 }
 
+// Buffer an ingested point into memory.
+// Flushes the batch if FlushMaxPoints has been reached.
 func (b *BufferedClient) processIngestedPoint(point Point) {
 	b.pointsBuf[b.pointsIndex] = point
 	b.pointsIndex += 1
@@ -100,6 +116,7 @@ func (b *BufferedClient) processIngestedPoint(point Point) {
 	}
 }
 
+// Flushes all buffered points to the server
 func (b *BufferedClient) flushBatch() {
 	if b.pointsIndex == 0 {
 		return
