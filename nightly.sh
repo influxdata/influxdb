56eb7e92ec9b09cd5bc3cace57e442355abc41d2
Todd Persen <toddboom@gmail.com>
2015-07-29 22:09:38 -0700

    Update nightly.sh to fix working directory and version string.

--- nightly.sh
+++ nightly.sh
@@ -4,11 +4,11 @@ REPO_DIR=`mktemp -d`
 echo "Using $REPO_DIR for all work..."
 
 cd $REPO_DIR
-export GOPATH=$PWD
+export GOPATH=`pwd`
 mkdir -p $GOPATH/src/github.com/influxdb
 cd $GOPATH/src/github.com/influxdb
 git clone https://github.com/influxdb/influxdb.git
 
 cd $GOPATH/src/github.com/influxdb/influxdb
-NIGHTLY_BUILD=true ./package.sh 0.9-nightly
+NIGHTLY_BUILD=true ./package.sh 0.9.3-nightly-`git log --pretty=format:'%h' -n 1`
 rm -rf $REPO_DIR
