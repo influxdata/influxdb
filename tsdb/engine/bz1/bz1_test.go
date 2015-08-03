426d6f6405d9b328d1b6383b8dfd38d33a95e8f6
Ben Johnson <benbjohnson@yahoo.com>
2015-08-03 14:30:26 -0600

    refactor bz1 to integrate with WAL

--- tsdb/engine/bz1/bz1_test.go
+++ tsdb/engine/bz1/bz1_test.go
@@ -1,15 +1,12 @@
 package bz1_test
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io/ioutil"
-	"math/rand"
 	"os"
 	"reflect"
-	"sort"
-	"strconv"
 	"testing"
-	"testing/quick"
 	"time"
 
 	"github.com/influxdb/influxdb/tsdb"
@@ -128,11 +125,11 @@ func TestEngine_WriteIndex_Append(t *testing.T) {
 	// Append points to index.
 	if err := e.WriteIndex(map[string][][]byte{
 		"cpu": [][]byte{
-			append(u64tob(1), []byte{0x10}...),
-			append(u64tob(2), []byte{0x20}...),
+			bz1.MarshalEntry(1, []byte{0x10}),
+			bz1.MarshalEntry(2, []byte{0x20}),
 		},
 		"mem": [][]byte{
-			append(u64tob(0), []byte{0x30}...),
+			bz1.MarshalEntry(0, []byte{0x30}),
 		},
 	}); err != nil {
 		t.Fatal(err)
@@ -146,125 +143,76 @@ func TestEngine_WriteIndex_Append(t *testing.T) {
 	c := tx.Cursor("cpu")
 	if k, v := c.Seek(u64tob(0)); !reflect.DeepEqual(k, []byte{0, 0, 0, 0, 0, 0, 0, 1}) || !reflect.DeepEqual(v, []byte{0x10}) {
 		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, v = c.Next(); !reflect.DeepEqual(k, []byte{0, 0, 0, 0, 0, 0, 0, 2}) || !reflect.DeepEqual(v, []byte{0x20}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, _ = c.Next(); k != nil {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	}
+
+	// Iterate over "mem" series.
+	c = tx.Cursor("mem")
+	if k, v := c.Seek(u64tob(0)); !reflect.DeepEqual(k, []byte{0, 0, 0, 0, 0, 0, 0, 0}) || !reflect.DeepEqual(v, []byte{0x30}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, _ = c.Next(); k != nil {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
 	}
 }
 
-// Ensure the engine read and write randomized points.
-func TestEngine_WritePoints_Quick(t *testing.T) {
-	if testing.Short() {
-		t.Skip("short mode")
-	}
-
-	t.Skip("refactoring...")
-
-	quick.Check(func(blockSize uint16, points Points) bool {
-		e := OpenDefaultEngine()
-		e.BlockSize = int(blockSize)
-		defer e.Close()
-
-		// Write points against two separate series.
-		if err := e.WritePoints([]tsdb.Point(points), nil, nil); err != nil {
-			t.Fatal(err)
-		}
-
-		// Validate each series.
-		for _, series := range points.Series() {
-			if keys, err := e.ReadSeriesPointKeys(series.Name, series.Tags); err != nil {
-				t.Fatal(err)
-			} else if exp := series.Keys(); !reflect.DeepEqual(exp, keys) {
-				t.Fatalf("series point keys:\n\nexp=%v\n\ngot=%v\n\n", exp, keys)
-			}
-		}
-
-		return true
-	}, nil)
-}
-
-func BenchmarkEngine_WritePoints_0b_1(b *testing.B)     { benchmarkEngine_WritePoints_0b(b, 1) }
-func BenchmarkEngine_WritePoints_0b_10(b *testing.B)    { benchmarkEngine_WritePoints_0b(b, 10) }
-func BenchmarkEngine_WritePoints_0b_100(b *testing.B)   { benchmarkEngine_WritePoints_0b(b, 100) }
-func BenchmarkEngine_WritePoints_0b_1000(b *testing.B)  { benchmarkEngine_WritePoints_0b(b, 1000) }
-func BenchmarkEngine_WritePoints_0b_10000(b *testing.B) { benchmarkEngine_WritePoints_0b(b, 10000) }
-func benchmarkEngine_WritePoints_0b(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 0, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_512b_100(b *testing.B)   { benchmarkEngine_WritePoints_512b(b, 100) }
-func BenchmarkEngine_WritePoints_512b_1000(b *testing.B)  { benchmarkEngine_WritePoints_512b(b, 1000) }
-func BenchmarkEngine_WritePoints_512b_10000(b *testing.B) { benchmarkEngine_WritePoints_512b(b, 10000) }
-func benchmarkEngine_WritePoints_512b(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 512, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_1KB_100(b *testing.B)   { benchmarkEngine_WritePoints_1KB(b, 100) }
-func BenchmarkEngine_WritePoints_1KB_1000(b *testing.B)  { benchmarkEngine_WritePoints_1KB(b, 1000) }
-func BenchmarkEngine_WritePoints_1KB_10000(b *testing.B) { benchmarkEngine_WritePoints_1KB(b, 10000) }
-func benchmarkEngine_WritePoints_1KB(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 1024, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_2KB_100(b *testing.B)   { benchmarkEngine_WritePoints_2KB(b, 100) }
-func BenchmarkEngine_WritePoints_2KB_1000(b *testing.B)  { benchmarkEngine_WritePoints_2KB(b, 1000) }
-func BenchmarkEngine_WritePoints_2KB_10000(b *testing.B) { benchmarkEngine_WritePoints_2KB(b, 10000) }
-func benchmarkEngine_WritePoints_2KB(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 2*1024, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_4KB_100(b *testing.B)   { benchmarkEngine_WritePoints_4KB(b, 100) }
-func BenchmarkEngine_WritePoints_4KB_1000(b *testing.B)  { benchmarkEngine_WritePoints_4KB(b, 1000) }
-func BenchmarkEngine_WritePoints_4KB_10000(b *testing.B) { benchmarkEngine_WritePoints_4KB(b, 10000) }
-func benchmarkEngine_WritePoints_4KB(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 4*1024, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_8KB_100(b *testing.B)   { benchmarkEngine_WritePoints_8KB(b, 100) }
-func BenchmarkEngine_WritePoints_8KB_1000(b *testing.B)  { benchmarkEngine_WritePoints_8KB(b, 1000) }
-func BenchmarkEngine_WritePoints_8KB_10000(b *testing.B) { benchmarkEngine_WritePoints_8KB(b, 10000) }
-func benchmarkEngine_WritePoints_8KB(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 8*1024, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_16KB_100(b *testing.B)   { benchmarkEngine_WritePoints_16KB(b, 100) }
-func BenchmarkEngine_WritePoints_16KB_1000(b *testing.B)  { benchmarkEngine_WritePoints_16KB(b, 1000) }
-func BenchmarkEngine_WritePoints_16KB_10000(b *testing.B) { benchmarkEngine_WritePoints_16KB(b, 10000) }
-func benchmarkEngine_WritePoints_16KB(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 16*1024, seriesN)
-}
-
-func BenchmarkEngine_WritePoints_32KB_100(b *testing.B)   { benchmarkEngine_WritePoints_32KB(b, 100) }
-func BenchmarkEngine_WritePoints_32KB_1000(b *testing.B)  { benchmarkEngine_WritePoints_32KB(b, 1000) }
-func BenchmarkEngine_WritePoints_32KB_10000(b *testing.B) { benchmarkEngine_WritePoints_32KB(b, 10000) }
-func benchmarkEngine_WritePoints_32KB(b *testing.B, seriesN int) {
-	benchmarkEngine_WritePoints(b, 32*1024, seriesN)
-}
-
-func benchmarkEngine_WritePoints(b *testing.B, blockSize, seriesN int) {
+// Ensure the engine can rewrite blocks that contain the new point range.
+func TestEngine_WriteIndex_Insert(t *testing.T) {
 	e := OpenDefaultEngine()
-	e.BlockSize = blockSize
 	defer e.Close()
 
-	// Generate data.
-	var points []tsdb.Point
-	for i := 0; i < b.N; i++ {
-		points = append(points, tsdb.NewPoint(
-			strconv.Itoa(i/seriesN),
-			tsdb.Tags{},
-			tsdb.Fields{"value": float64(i)},
-			time.Unix(0, 0).Add(time.Duration(i))))
-	}
-	b.ResetTimer()
-
-	// Write points against two separate series.
-	if err := e.WritePoints(points, nil, nil); err != nil {
-		b.Fatal(err)
+	// Write initial points to index.
+	if err := e.WriteIndex(map[string][][]byte{
+		"cpu": [][]byte{
+			bz1.MarshalEntry(10, []byte{0x10}),
+			bz1.MarshalEntry(20, []byte{0x20}),
+			bz1.MarshalEntry(30, []byte{0x30}),
+		},
+	}); err != nil {
+		t.Fatal(err)
 	}
 
-	// Stop timer and measure size per point.
-	b.StopTimer()
-	if stats, err := e.Stats(); err != nil {
-		b.Fatal(err)
-	} else {
-		b.Logf("n=%d, %.1f b/pt", b.N, float64(stats.Size)/float64(b.N))
+	// Write overlapping points to index.
+	if err := e.WriteIndex(map[string][][]byte{
+		"cpu": [][]byte{
+			bz1.MarshalEntry(9, []byte{0x09}),
+			bz1.MarshalEntry(10, []byte{0xFF}),
+			bz1.MarshalEntry(25, []byte{0x25}),
+			bz1.MarshalEntry(31, []byte{0x31}),
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Write overlapping points to index again.
+	if err := e.WriteIndex(map[string][][]byte{
+		"cpu": [][]byte{
+			bz1.MarshalEntry(31, []byte{0xFF}),
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Start transaction.
+	tx := e.MustBegin(false)
+	defer tx.Rollback()
+
+	// Iterate over "cpu" series.
+	c := tx.Cursor("cpu")
+	if k, v := c.Seek(u64tob(0)); btou64(k) != 9 || !bytes.Equal(v, []byte{0x09}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, v = c.Next(); btou64(k) != 10 || !bytes.Equal(v, []byte{0xFF}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, v = c.Next(); btou64(k) != 20 || !bytes.Equal(v, []byte{0x20}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, v = c.Next(); btou64(k) != 25 || !bytes.Equal(v, []byte{0x25}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, v = c.Next(); btou64(k) != 30 || !bytes.Equal(v, []byte{0x30}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
+	} else if k, v = c.Next(); btou64(k) != 31 || !bytes.Equal(v, []byte{0xFF}) {
+		t.Fatalf("unexpected key/value: %x / %x", k, v)
 	}
 }
 
@@ -317,27 +265,6 @@ func (e *Engine) MustBegin(writable bool) tsdb.Tx {
 	return tx
 }
 
-// ReadSeriesPointKeys returns the raw keys for all points store for a series.
-func (e *Engine) ReadSeriesPointKeys(name string, tags tsdb.Tags) ([][]byte, error) {
-	// Open transaction on engine.
-	tx, err := e.Begin(false)
-	if err != nil {
-		return nil, err
-	}
-	defer tx.Rollback()
-
-	// Create a cursor for the series.
-	c := tx.Cursor(string(tsdb.MakeKey([]byte(name), tags)))
-
-	// Collect all the keys.
-	var keys [][]byte
-	for k, _ := c.Seek([]byte{0, 0, 0, 0, 0, 0, 0, 0}); k != nil; k, _ = c.Next() {
-		keys = append(keys, copyBytes(k))
-	}
-
-	return keys, nil
-}
-
 // EnginePointsWriter represents a mock that implements Engine.PointsWriter.
 type EnginePointsWriter struct {
 	WritePointsFn func(points []tsdb.Point) error
@@ -358,79 +285,12 @@ func copyBytes(b []byte) []byte {
 	return other
 }
 
-// Points represents a list of points that implements quick.Generator.
-type Points []tsdb.Point
-
-// Generate generates a random list of point.
-func (Points) Generate(rand *rand.Rand, size int) reflect.Value {
-	points := make([]tsdb.Point, rand.Intn(1000))
-
-	for i := range points {
-		// Generate individual point fields.
-		name := strconv.Itoa(rand.Intn(3))
-		tags := tsdb.Tags{"host": strconv.Itoa(rand.Intn(3))}
-		value, _ := quick.Value(reflect.TypeOf(float64(0)), rand)
-		timestamp := time.Unix(int64(rand.Uint32()), int64(rand.Uint32()))
-
-		// Create point with random data.
-		points[i] = tsdb.NewPoint(name, tags, tsdb.Fields{"value": value}, timestamp)
-	}
-
-	return reflect.ValueOf(points)
-}
-
-// Series returns a list of unique series.
-func (a Points) Series() []Series {
-	m := make(map[string]Series)
-	for _, p := range a {
-		// Retrieve series.
-		key := string(p.Key())
-		s, ok := m[key]
-
-		// If not set already, set name and tags.
-		if !ok {
-			s.Name = p.Name()
-			s.Tags = p.Tags()
-		}
-
-		// Append point to series.
-		s.Points = append(s.Points, p)
-
-		// Update series.
-		m[key] = s
-	}
-
-	// Convert to slice of series and sort points.
-	var other []Series
-	for _, s := range m {
-		sort.Sort(tsdb.Points(s.Points))
-		other = append(other, s)
-	}
-
-	return other
-}
-
-// Series represents a name + tag series. It also includes a list of points.
-type Series struct {
-	Name   string
-	Tags   tsdb.Tags
-	Points []tsdb.Point
-}
-
-// Keys returns byte slice keys for each point.
-func (s *Series) Keys() [][]byte {
-	var a [][]byte
-	for _, p := range s.Points {
-		b := make([]byte, 8)
-		binary.BigEndian.PutUint64(b, uint64(p.UnixNano()))
-		a = append(a, b)
-	}
-	return a
-}
-
 // u64tob converts a uint64 into an 8-byte slice.
 func u64tob(v uint64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, v)
 	return b
 }
+
+// btou64 converts an 8-byte slice into an uint64.
+func btou64(b []byte) uint64 { return binary.BigEndian.Uint64(b) }
