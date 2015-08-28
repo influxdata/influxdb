38306857cd6b2efe82ad7c56cb97f69df0f911f8
Philip O'Toole <philip@influxdb.com>
2015-08-27 21:51:06 -0700

    Add tags to registration

--- monitor/service.go
+++ monitor/service.go
@@ -19,20 +19,20 @@ type Service struct {
 
 // Register registers a client with the given name. It is an error to register a client with
 // an already registered key.
-func (s *Service) Register(key string, client Client) error {
+func (s *Service) Register(name string, tags map[string]string, client Client) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	if _, ok := s.registrations[key]; ok {
-		return fmt.Errorf("existing client registered with key %s", key)
+	if _, ok := s.registrations[name]; ok {
+		return fmt.Errorf("existing client registered with name %s", name)
 	}
 
-	s.registrations[key] = client
+	s.registrations[name] = client
 	return nil
 }
 
-// Deregister deregisters any client previously registered with the existing key.
-func (s *Service) Deregister(key string) {
+// Deregister deregisters any client previously registered with the existing name.
+func (s *Service) Deregister(name string) {
 	s.mu.Lock()
 	defer s.mu.Unlock()
-	delete(s.registrations, key)
+	delete(s.registrations, name)
 }
