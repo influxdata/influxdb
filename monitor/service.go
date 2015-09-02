df864b2ec77ed9672fabcd6a32e436320c2a6f69
Philip O'Toole <philip@influxdb.com>
2015-09-01 21:28:00 -0700

    Set tags correctly for SHOW STATS

--- monitor/service.go
+++ monitor/service.go
@@ -125,13 +125,9 @@ func (s *Service) executeShowStatistics(q *influxql.ShowStatsStatement) *influxq
 	rows := make([]*influxql.Row, len(stats))
 
 	for n, stat := range stats {
-		row := &influxql.Row{Name: stat.Name}
-		values := make([]interface{}, 0, len(stat.Tags)+len(stat.Values))
+		row := &influxql.Row{Name: stat.Name, Tags: stat.Tags}
 
-		for _, k := range stat.tagNames() {
-			row.Columns = append(row.Columns, k)
-			values = append(values, stat.Tags[k])
-		}
+		values := make([]interface{}, 0, len(stat.Values))
 		for _, k := range stat.valueNames() {
 			row.Columns = append(row.Columns, k)
 			values = append(values, stat.Values[k])
