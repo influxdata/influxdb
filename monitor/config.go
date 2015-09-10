3cb29d92411251f58155d49d539425d43a22ff12
Philip O'Toole <philip@influxdb.com>
2015-09-10 15:03:51 -0700

    By default write stats to default retention policy
    This reverts to a previous design for storing stats, whereby if a
    non-default retention policy is configured as the destination retention
    policy, it is up the user to explicitly create it, with the desired
    replication factor and duration.

--- monitor/config.go
+++ monitor/config.go
@@ -14,37 +14,22 @@ const (
 	// DefaultStoreDatabase is the name of the database where gathered information is written
 	DefaultStoreDatabase = "_internal"
 
-	// DefaultStoreRetentionPolicy is the name of the retention policy for monitor data.
-	DefaultStoreRetentionPolicy = "monitor"
-
-	// DefaultRetentionPolicyDuration is the duration the data is retained.
-	DefaultStoreRetentionPolicyDuration = 168 * time.Hour
-
-	// DefaultStoreReplicationFactor is the default replication factor for the data.
-	DefaultStoreReplicationFactor = 1
-
 	// DefaultStoreInterval is the period between storing gathered information.
 	DefaultStoreInterval = 10 * time.Second
 )
 
 // Config represents the configuration for the monitor service.
 type Config struct {
-	StoreEnabled           bool          `toml:"store-enabled"`
-	StoreDatabase          string        `toml:"store-database"`
-	StoreRetentionPolicy   string        `toml:"store-retention-policy"`
-	StoreRetentionDuration toml.Duration `toml:"store-retention-duration"`
-	StoreReplicationFactor int           `toml:"store-replication-factor"`
-	StoreInterval          toml.Duration `toml:"store-interval"`
+	StoreEnabled  bool          `toml:"store-enabled"`
+	StoreDatabase string        `toml:"store-database"`
+	StoreInterval toml.Duration `toml:"store-interval"`
 }
 
 // NewConfig returns an instance of Config with defaults.
 func NewConfig() Config {
 	return Config{
-		StoreEnabled:           true,
-		StoreDatabase:          DefaultStoreDatabase,
-		StoreRetentionPolicy:   DefaultStoreRetentionPolicy,
-		StoreRetentionDuration: toml.Duration(DefaultStoreRetentionPolicyDuration),
-		StoreReplicationFactor: DefaultStoreReplicationFactor,
-		StoreInterval:          toml.Duration(DefaultStoreInterval),
+		StoreEnabled:  true,
+		StoreDatabase: DefaultStoreDatabase,
+		StoreInterval: toml.Duration(DefaultStoreInterval),
 	}
 }
