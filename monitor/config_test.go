32f58a59bd620bb65ac23888cdda7fd59751e944
Philip O'Toole <philip@influxdb.com>
2015-09-10 17:28:44 -0700

    Fix compile error in monitor unit tests

--- monitor/config_test.go
+++ monitor/config_test.go
@@ -24,12 +24,6 @@ store-interval="10m"
 		t.Fatalf("unexpected store-enabled: %v", c.StoreEnabled)
 	} else if c.StoreDatabase != "the_db" {
 		t.Fatalf("unexpected store-database: %s", c.StoreDatabase)
-	} else if c.StoreRetentionPolicy != "the_rp" {
-		t.Fatalf("unexpected store-retention-policy: %s", c.StoreRetentionPolicy)
-	} else if time.Duration(c.StoreRetentionDuration) != 1*time.Hour {
-		t.Fatalf("unexpected store-retention-duration: %s", c.StoreRetentionDuration)
-	} else if c.StoreReplicationFactor != 1234 {
-		t.Fatalf("unexpected store-replication-factor: %d", c.StoreReplicationFactor)
 	} else if time.Duration(c.StoreInterval) != 10*time.Minute {
 		t.Fatalf("unexpected store-interval:  %s", c.StoreInterval)
 	}
