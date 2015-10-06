16b3c310379bad73b7c17c70efd74f57d1a986f9
Philip O'Toole <philip@influxdb.com>
2015-10-05 21:51:11 -0700

    HH should not process inactive nodes
    It is possible that a node is dropped, while data is still pending in
    the HH queues for that node. If this happens, the HH processor should
    skip those nodes, or the system continually complains about unreachable
    nodes.
    
    Data deletion is still explicitly left the purge system.

--- services/hh/processor_test.go
+++ services/hh/processor_test.go
@@ -16,6 +16,14 @@ func (f *fakeShardWriter) WriteShard(shardID, nodeID uint64, points []models.Poi
 	return f.ShardWriteFn(shardID, nodeID, points)
 }
 
+type fakeMetaProvider struct {
+	NodesFn func() []uint64
+}
+
+func (f fakeMetaProvider) Nodes() []uint64 {
+	return f.NodesFn()
+}
+
 func TestProcessorProcess(t *testing.T) {
 	dir, err := ioutil.TempDir("", "processor_test")
 	if err != nil {
@@ -76,5 +84,53 @@ func TestProcessorProcess(t *testing.T) {
 	if exp := 1; count != exp {
 		t.Fatalf("Process() write count mismatch: got %v, exp %v", count, exp)
 	}
+}
 
+func TestProcessorProcessWithProvider(t *testing.T) {
+	dir, err := ioutil.TempDir("", "processor_test")
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %v", err)
+	}
+
+	// Active nodes and a point.
+	activeNodes := []uint64{1}
+	pt := models.NewPoint("cpu", models.Tags{"foo": "bar"}, models.Fields{"value": 1.0}, time.Unix(0, 0))
+
+	// This shard writer should only be called for active nodes.
+	count := 0
+	sh := &fakeShardWriter{
+		ShardWriteFn: func(shardID, nodeID uint64, points []models.Point) error {
+			if !contains(activeNodes, nodeID) {
+				t.Fatalf("proccess incorrectly processed inactive node ID %d", nodeID)
+			}
+			count++
+			return nil
+		},
+	}
+
+	// Create the processor for test.
+	p, err := NewProcessor(dir, sh, ProcessorOptions{MaxSize: 1024})
+	if err != nil {
+		t.Fatalf("Process() failed to create processor: %v", err)
+	}
+	p.MetaProvider = fakeMetaProvider{
+		NodesFn: func() []uint64 { return activeNodes },
+	}
+
+	// This should queue the writes, only the first should actually be processed.
+	if err := p.WriteShard(999, activeNodes[0], []models.Point{pt}); err != nil {
+		t.Fatalf("Process() failed to write points: %v", err)
+	}
+	if err := p.WriteShard(999, 999, []models.Point{pt}); err != nil {
+		t.Fatalf("Process() failed to write points: %v", err)
+	}
+
+	if err := p.Process(); err != nil {
+		t.Fatalf("Process() failed to process points: %v", err)
+	}
+
+	// Shard write should only have been called once.
+	if count != 1 {
+		t.Fatalf("Process() write count mismatch: got %v, exp 1", count)
+	}
 }
