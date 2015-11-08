d36a610a06e2ec1743868c88f10ae15a5ba72ef3
Steffen Windoffer <steffen@wind0r.de>
2015-11-08 13:13:01 +0100

    Complete lint for pkg/slices

--- pkg/slices/strings.go
+++ pkg/slices/strings.go
@@ -2,6 +2,7 @@ package slices
 
 import "strings"
 
+// Union combines two string sets
 func Union(setA, setB []string, ignoreCase bool) []string {
 	for _, b := range setB {
 		if ignoreCase {
@@ -17,6 +18,7 @@ func Union(setA, setB []string, ignoreCase bool) []string {
 	return setA
 }
 
+// Exists checks if a string is in a set
 func Exists(set []string, find string) bool {
 	for _, s := range set {
 		if s == find {
@@ -26,6 +28,7 @@ func Exists(set []string, find string) bool {
 	return false
 }
 
+// ExistsIgnoreCase checks if a string is in a set but ignores its case
 func ExistsIgnoreCase(set []string, find string) bool {
 	find = strings.ToLower(find)
 	for _, s := range set {
