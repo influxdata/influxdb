657877dd94d0bde045975048b29e1f5a66e1636a
David Norton <dgnorton@gmail.com>
2015-11-20 10:13:11 -0500

    make not found err messages more consistent

--- meta/store.go
+++ meta/store.go
@@ -21,6 +21,7 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/hashicorp/raft"
+	"github.com/influxdb/influxdb"
 	"github.com/influxdb/influxdb/influxql"
 	"github.com/influxdb/influxdb/meta/internal"
 	"golang.org/x/crypto/bcrypt"
@@ -1105,7 +1106,7 @@ func (s *Store) DefaultRetentionPolicy(database string) (rpi *RetentionPolicyInf
 	err = s.read(func(data *Data) error {
 		di := data.Database(database)
 		if di == nil {
-			return ErrDatabaseNotFound
+			return influxdb.ErrDatabaseNotFound(database)
 		}
 
 		for i := range di.RetentionPolicies {
@@ -1124,7 +1125,7 @@ func (s *Store) RetentionPolicies(database string) (a []RetentionPolicyInfo, err
 	err = s.read(func(data *Data) error {
 		di := data.Database(database)
 		if di != nil {
-			return ErrDatabaseNotFound
+			return influxdb.ErrDatabaseNotFound(database)
 		}
 		a = di.RetentionPolicies
 		return nil
