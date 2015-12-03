c2b8a24004fac366fc842cb757fc619363669eb9
Adarsha <mvadu@users.noreply.github.com>
2015-12-03 23:43:48 +0530

    Added mmap implementation for Windows
    Added mmap implementation for Windows based on MapViewOfFile. Used SliceHeader trick to change the pointer returned by MapViewOfFile to a byte slice. This will not call for any change in rest of tsm.
    
    However I am not sure where this mmap function is called, as go build is still complains about 
        
        tsdb\engine\tsm1\tsm1.go:1974: undefined: syscall.Mmap
        tsdb\engine\tsm1\tsm1.go:1974: undefined: syscall.PROT_READ
        tsdb\engine\tsm1\tsm1.go:1974: undefined: syscall.MAP_SHARED
        tsdb\engine\tsm1\tsm1.go:2033: undefined: syscall.Munmap

--- tsdb/engine/tsm1/mmap_windows.go
+++ tsdb/engine/tsm1/mmap_windows.go
@@ -1,14 +1,56 @@
 package tsm1
 
 import (
-	"fmt"
 	"os"
+	"syscall"
+	"unsafe"
+	"reflect"
 )
 
-func mmap(f *os.File, offset int64, length int) ([]byte, error) {
-	return nil, fmt.Errorf("mmap file not supported windows")
+// maxMapSize represents the largest mmap size supported by Bolt. 
+const maxMapSize = 0xFFFFFFFFFFFF // 256TB 
+
+// mmap implementation for Windows
+// Based on: https://github.com/edsrzf/mmap-go
+// Based on: https://github.com/boltdb/bolt/bolt_windows.go
+// Ref: https://groups.google.com/forum/#!topic/golang-nuts/g0nLwQI9www
+func mmap(f *os.File, offset int64, length int) (out []byte,err error) {
+	// Open a file mapping handle.
+	sizelo := uint32(length >> 32)
+	sizehi := uint32(length) & 0xffffffff
+	h, errno := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READONLY, sizelo, sizehi, nil)
+	if h == 0 {
+		return nil, os.NewSyscallError("CreateFileMapping", errno)
+	}
+
+	// Create the memory map.
+	addr, errno := syscall.MapViewOfFile(h, syscall.FILE_MAP_READ, 0, 0, uintptr(length))
+	if addr == 0 {
+		return nil, os.NewSyscallError("MapViewOfFile", errno)
+	}
+
+	// Close mapping handle.
+	if err := syscall.CloseHandle(syscall.Handle(h)); err != nil {
+		return nil, os.NewSyscallError("CloseHandle", err)
+	}
+
+	// Convert to a byte array.
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&out))
+	hdr.Data = uintptr(unsafe.Pointer(addr))
+	hdr.Len = length
+	hdr.Cap = length
+
+	return
 }
 
+// munmap Windows implementation
+// Based on: https://github.com/edsrzf/mmap-go
+// Based on: https://github.com/boltdb/bolt/bolt_windows.go
 func munmap(b []byte) (err error) {
-	return nil, fmt.Errorf("munmap file not supported on windows")
+	
+	addr := (uintptr)(unsafe.Pointer(&b[0]))
+	if err := syscall.UnmapViewOfFile(addr); err != nil {
+		return os.NewSyscallError("UnmapViewOfFile", err)
+	}
+	return nil
 }
