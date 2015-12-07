5482c6de03bebaed2777f2e91fafda7288690544
Adarsha <mvadu@users.noreply.github.com>
2015-12-07 23:30:19 +0530

    Avoid closing the handle in mmap
    Added mmap implementation for Windows. It uses MapViewOfFile similar to Bolt's implementation. MapViewOfFile  returns a pointer and not a byte array. Bolt changed their data structure to support it. 
    
    Instead of changing the implementation of tsm data structure, I used a trick shown in https://groups.google.com/forum/#!topic/golang-nuts/g0nLwQI9www to use SliceHeader to convert the pointer into a slice.
    
    Bolt's implementation also closes the file handle in mmap itself. It was resulting in a timeout, so implemented https://github.com/edsrzf/mmap-go/blob/master/mmap_windows.go logic to keep file handle open until munmap

--- tsdb/engine/tsm1/mmap_windows.go
+++ tsdb/engine/tsm1/mmap_windows.go
@@ -1,8 +1,10 @@
 package tsm1
 
 import (
+	"errors"
 	"os"
 	"reflect"
+	"sync"
 	"syscall"
 	"unsafe"
 )
@@ -11,25 +13,31 @@ import (
 // Based on: https://github.com/edsrzf/mmap-go
 // Based on: https://github.com/boltdb/bolt/bolt_windows.go
 // Ref: https://groups.google.com/forum/#!topic/golang-nuts/g0nLwQI9www
+
+// We keep this map so that we can get back the original handle from the memory address.
+var handleLock sync.Mutex
+var handleMap = map[uintptr]syscall.Handle{}
+
 func mmap(f *os.File, offset int64, length int) (out []byte, err error) {
 	// Open a file mapping handle.
 	sizelo := uint32(length >> 32)
 	sizehi := uint32(length) & 0xffffffff
+	//h, errno := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READWRITE, sizelo, sizehi, nil)
 	h, errno := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READONLY, sizelo, sizehi, nil)
 	if h == 0 {
 		return nil, os.NewSyscallError("CreateFileMapping", errno)
 	}
 
 	// Create the memory map.
+	//addr, errno := syscall.MapViewOfFile(h, syscall.FILE_MAP_READ | syscall.FILE_MAP_WRITE , 0, 0, uintptr(length))
 	addr, errno := syscall.MapViewOfFile(h, syscall.FILE_MAP_READ, 0, 0, uintptr(length))
 	if addr == 0 {
 		return nil, os.NewSyscallError("MapViewOfFile", errno)
 	}
 
-	// Close mapping handle.
-	if err := syscall.CloseHandle(syscall.Handle(h)); err != nil {
-		return nil, os.NewSyscallError("CloseHandle", err)
-	}
+	handleLock.Lock()
+	handleMap[addr] = h
+	handleLock.Unlock()
 
 	// Convert to a byte array.
 	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&out))
@@ -45,9 +53,23 @@ func mmap(f *os.File, offset int64, length int) (out []byte, err error) {
 // Based on: https://github.com/boltdb/bolt/bolt_windows.go
 func munmap(b []byte) (err error) {
 
+	handleLock.Lock()
+	defer handleLock.Unlock()
+
 	addr := (uintptr)(unsafe.Pointer(&b[0]))
 	if err := syscall.UnmapViewOfFile(addr); err != nil {
 		return os.NewSyscallError("UnmapViewOfFile", err)
 	}
+
+	handle, ok := handleMap[addr]
+	if !ok {
+		// should be impossible; we would've seen the error above
+		return errors.New("unknown base address")
+	}
+	delete(handleMap, addr)
+
+	e := syscall.CloseHandle(syscall.Handle(handle))
+	return os.NewSyscallError("CloseHandle", e)
+
 	return nil
 }
