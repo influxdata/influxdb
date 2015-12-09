07c0939fe1ffe721d84fa72503579e9b8e3610fd
Ady <Ady@adystech.com>
2015-12-10 01:07:50 +0530

    Added logic To let the memeory mapped files to renamed by OS. Now a copy is created in memory with SHARED_DELETE flag, so that OS is free to rename or delete original file

--- tsdb/engine/tsm1/mmap_windows.go
+++ tsdb/engine/tsm1/mmap_windows.go
@@ -17,19 +17,48 @@ import (
 // We keep this map so that we can get back the original handle from the memory address.
 var handleLock sync.Mutex
 var handleMap = map[uintptr]syscall.Handle{}
+var fileMap = map[uintptr]*os.File{}
+
+func openSharedFile(f *os.File) (file *os.File, err error) {
+
+	var access, createmode, sharemode uint32
+	var sa *syscall.SecurityAttributes
+
+	access = syscall.GENERIC_READ
+	sharemode = uint32(syscall.FILE_SHARE_READ | syscall.FILE_SHARE_WRITE | syscall.FILE_SHARE_DELETE)
+	createmode = syscall.OPEN_EXISTING
+	fileName := f.Name()
+
+	pathp, err := syscall.UTF16PtrFromString(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	h, e := syscall.CreateFile(pathp, access, sharemode, sa, createmode, syscall.FILE_ATTRIBUTE_NORMAL, 0)
+
+	if e != nil {
+		return nil, e
+	}
+	//NewFile does not add finalizer, need to close this manually
+	return os.NewFile(uintptr(h), fileName), nil
+}
 
 func mmap(f *os.File, offset int64, length int) (out []byte, err error) {
 	// Open a file mapping handle.
 	sizelo := uint32(length >> 32)
 	sizehi := uint32(length) & 0xffffffff
-	//h, errno := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READWRITE, sizelo, sizehi, nil)
-	h, errno := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READONLY, sizelo, sizehi, nil)
+
+	sharedHandle, errno := openSharedFile(f)
+	if errno != nil {
+		return nil, os.NewSyscallError("CreateFile", errno)
+	}
+
+	h, errno := syscall.CreateFileMapping(syscall.Handle(sharedHandle.Fd()), nil, syscall.PAGE_READONLY, sizelo, sizehi, nil)
 	if h == 0 {
 		return nil, os.NewSyscallError("CreateFileMapping", errno)
 	}
 
 	// Create the memory map.
-	//addr, errno := syscall.MapViewOfFile(h, syscall.FILE_MAP_READ | syscall.FILE_MAP_WRITE , 0, 0, uintptr(length))
 	addr, errno := syscall.MapViewOfFile(h, syscall.FILE_MAP_READ, 0, 0, uintptr(length))
 	if addr == 0 {
 		return nil, os.NewSyscallError("MapViewOfFile", errno)
@@ -37,6 +66,7 @@ func mmap(f *os.File, offset int64, length int) (out []byte, err error) {
 
 	handleLock.Lock()
 	handleMap[addr] = h
+	fileMap[addr] = sharedHandle
 	handleLock.Unlock()
 
 	// Convert to a byte array.
@@ -70,6 +100,16 @@ func munmap(b []byte) (err error) {
 
 	e := syscall.CloseHandle(syscall.Handle(handle))
 	return os.NewSyscallError("CloseHandle", e)
+
+	file, ok := fileMap[addr]
+	if !ok {
+		// should be impossible; we would've seen the error above
+		return errors.New("unknown base address")
+	}
+	delete(fileMap, addr)
+
+	e = file.Close()
+	return errors.New("Close File" + e.Error())
 
 	return nil
 }
