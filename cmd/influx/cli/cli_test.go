33de8789563d25b63be7b365123474c3557ddf4d
nick.grange <grange74@gmail.com>
2015-12-17 23:42:30 +1100

    Fixed #5078 to support Inserts in non-interactive mode. Changed non-interactive mode to send everything through the CLI's parser the same way the interactive mode works. Added multiline support for -execute flag.

--- cmd/influx/cli/cli_test.go
+++ cmd/influx/cli/cli_test.go
@@ -46,6 +46,21 @@ func TestRunCLI(t *testing.T) {
 	c.Run()
 }
 
+func TestRunCLI_ExecuteInsert(t *testing.T) {
+    t.Parallel()
+    ts := emptyTestServer()
+    defer ts.Close()
+
+    u, _ := url.Parse(ts.URL)
+    h, p, _ := net.SplitHostPort(u.Host)
+    c := cli.New(CLIENT_VERSION)
+    c.Host = h
+    c.Port, _ = strconv.Atoi(p)
+    c.Precision = "ms"
+    c.Execute = "INSERT sensor,floor=1 value=2"
+    c.Run() 
+}
+
 func TestConnect(t *testing.T) {
 	t.Parallel()
 	ts := emptyTestServer()
