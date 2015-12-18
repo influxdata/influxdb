b0d0668138c2e661dd55c8d255de1a2f3336ad17
Cory LaNou <cory@lanou.com>
2015-12-18 17:31:24 -0500

    wip

--- services/meta/service_test.go
+++ services/meta/service_test.go
@@ -1,12 +1,10 @@
 package meta
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"testing"
-	"time"
 )
 
 func TestService_Open(t *testing.T) {
@@ -48,58 +46,58 @@ func TestService_PingEndpoint(t *testing.T) {
 	}
 }
 
-func TestService_LongPollCache(t *testing.T) {
-	cfg := newConfig()
-	s := NewService(cfg)
-	if err := s.Open(); err != nil {
-		t.Fatal(err)
-	}
-
-	url, err := url.Parse(s.URL())
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ch := make(chan []byte)
-
-	reqFunc := func(index int) {
-		println("here1 ******")
-		u := fmt.Sprintf("http://%s?index=%d", url, index)
-		resp, err := http.Get(u)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer resp.Body.Close()
-		println("here2 ******")
-
-		buf := make([]byte, 10)
-		n, err := resp.Body.Read(buf)
-		if err != nil {
-			t.Fatal(err)
-		}
-		println(string(buf[:n]))
-		println("here3 ******")
-		ch <- buf[:n]
-	}
-
-	go reqFunc(0)
-	go reqFunc(1)
-	go func() {
-		time.Sleep(1 * time.Second)
-		s.handler.store.SetCache([]byte("world"))
-	}()
-
-	for n := 0; n < 2; n++ {
-		b := <-ch
-		t.Log(string(b))
-		println("client read cache update")
-	}
-	close(ch)
-
-	if err := s.Close(); err != nil {
-		t.Fatal(err)
-	}
-}
+//func TestService_LongPollCache(t *testing.T) {
+//	cfg := newConfig()
+//	s := NewService(cfg)
+//	if err := s.Open(); err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	url, err := url.Parse(s.URL())
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//
+//	ch := make(chan []byte)
+//
+//	reqFunc := func(index int) {
+//		println("here1 ******")
+//		u := fmt.Sprintf("http://%s?index=%d", url, index)
+//		resp, err := http.Get(u)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		defer resp.Body.Close()
+//		println("here2 ******")
+//
+//		buf := make([]byte, 10)
+//		n, err := resp.Body.Read(buf)
+//		if err != nil {
+//			t.Fatal(err)
+//		}
+//		println(string(buf[:n]))
+//		println("here3 ******")
+//		ch <- buf[:n]
+//	}
+//
+//	go reqFunc(0)
+//	go reqFunc(1)
+//	go func() {
+//		time.Sleep(1 * time.Second)
+//		s.handler.store.SetCache([]byte("world"))
+//	}()
+//
+//	for n := 0; n < 2; n++ {
+//		b := <-ch
+//		t.Log(string(b))
+//		println("client read cache update")
+//	}
+//	close(ch)
+//
+//	if err := s.Close(); err != nil {
+//		t.Fatal(err)
+//	}
+//}
 
 func newConfig() *Config {
 	cfg := NewConfig()
