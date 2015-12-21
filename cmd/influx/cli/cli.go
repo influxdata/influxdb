3f4949019db43c6229a199be1d006f7a3f92a602
Paulo Pires <pjpires@gmail.com>
2015-12-21 09:34:40 +0000

    USE databasename will work only for existing databases. Fixes #5174

--- cmd/influx/cli/cli.go
+++ cmd/influx/cli/cli.go
@@ -322,8 +322,41 @@ func (c *CommandLine) use(cmd string) {
 		return
 	}
 	d := args[1]
-	c.Database = d
-	fmt.Printf("Using database %s\n", d)
+
+	// validate if specified database exists
+	query := "SHOW DATABASES"
+	response, err := c.Client.Query(client.Query{Command: query})
+	if err != nil {
+		fmt.Printf("ERR: %s\n", err)
+		return
+	}
+
+	if err := response.Error(); err != nil {
+		fmt.Printf("ERR: %s\n", err)
+		return
+	}
+
+	// verify the provided database exists
+	databaseExists := false
+	for _, result := range response.Results {
+		for _, row := range result.Series {
+			if row.Name == "databases" {
+				for _, values := range row.Values {
+					for _, database := range values {
+						if database == d {
+							databaseExists = true
+						}
+					}
+				}
+			}
+		}
+	}
+	if databaseExists {
+		c.Database = d
+		fmt.Printf("Using database %s\n", d)
+	} else {
+		fmt.Printf("ERR: Database %s doesn't exist. Run SHOW DATABASES for a list of existing databases.\n", d)
+	}
 }
 
 // SetPrecision sets client precision
