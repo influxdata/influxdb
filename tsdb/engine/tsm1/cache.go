a41de6383220cde3d113fcf14b76c02e6b828018
Jason Wilder <mail@jasonwilder.com>
2016-02-22 22:59:14 -0700

    Simplify cache snapshotting
    The Cache had support for taking multiple snapshots to support writing
    multiple snapshots to TSM files concurrently if that happened to be
    a bottleneck.  In practice, this is never a bottleneck and we only
    run one snappshoting goroutine continously per shard which has worked
    well for all workloads.
    
    The multiple snapshot support introduces some unhandled failure scenarios
    where wal segments could be removed without writing them to TSM files.  If
    a snapshot compaction fails to write due to transient disk errors, subsequent
    snapshots will continue, but the failed one will not be retried.  When the
    subsequent ones succeeded, all closed wal segments are removed causing data
    loss.
    
    This change simplifies the snapshotting capability to ensure that there is only
    ever one snapshot.  If one fails, the next snapshot will update the existing
    snapshot and retry all of old and new data.
    
    Fixes #5686
    
    Rebased-to-0.10.x-by: Jon Seymour <jon@wildducktheories.com>

--- tsdb/engine/tsm1/cache.go
+++ tsdb/engine/tsm1/cache.go
@@ -68,8 +68,8 @@ type Cache struct {
 	// snapshots are the cache objects that are currently being written to tsm files
 	// they're kept in memory while flushing so they can be queried along with the cache.
 	// they are read only and should never be modified
-	snapshots     []*Cache
-	snapshotsSize uint64
+	snapshot     *Cache
+	snapshotSize uint64
 }
 
 // NewCache returns an instance of a cache which will use a maximum of maxSize bytes of memory.
@@ -88,7 +88,7 @@ func (c *Cache) Write(key string, values []Value) error {
 
 	// Enough room in the cache?
 	newSize := c.size + uint64(Values(values).Size())
-	if c.maxSize > 0 && newSize+c.snapshotsSize > c.maxSize {
+	if c.maxSize > 0 && newSize+c.snapshotSize > c.maxSize {
 		return ErrCacheMemoryExceeded
 	}
 
@@ -109,7 +109,7 @@ func (c *Cache) WriteMulti(values map[string][]Value) error {
 	// Enough room in the cache?
 	c.mu.RLock()
 	newSize := c.size + uint64(totalSz)
-	if c.maxSize > 0 && newSize+c.snapshotsSize > c.maxSize {
+	if c.maxSize > 0 && newSize+c.snapshotSize > c.maxSize {
 		c.mu.RUnlock()
 		return ErrCacheMemoryExceeded
 	}
@@ -131,17 +131,29 @@ func (c *Cache) Snapshot() *Cache {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	snapshot := NewCache(c.maxSize)
-	snapshot.store = c.store
-	snapshot.size = c.size
+	if c.snapshot == nil {
+		c.snapshot = &Cache{
+			store: make(map[string]*entry),
+		}
+	}
 
+	// Append the current cache values to the snapshot
+	for k, e := range c.store {
+		if _, ok := c.snapshot.store[k]; ok {
+			c.snapshot.store[k].add(e.values)
+		} else {
+			c.snapshot.store[k] = e
+		}
+		c.snapshotSize += uint64(Values(e.values).Size())
+	}
+
+	// Reset the cache
 	c.store = make(map[string]*entry)
 	c.size = 0
 
-	c.snapshots = append(c.snapshots, snapshot)
-	c.snapshotsSize += snapshot.size
+	c.snapshotSize += c.snapshot.size
 
-	return snapshot
+	return c.snapshot
 }
 
 // Deduplicate sorts the snapshot before returning it. The compactor and any queries
@@ -154,17 +166,13 @@ func (c *Cache) Deduplicate() {
 
 // ClearSnapshot will remove the snapshot cache from the list of flushing caches and
 // adjust the size
-func (c *Cache) ClearSnapshot(snapshot *Cache) {
+func (c *Cache) ClearSnapshot() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
-	for i, cache := range c.snapshots {
-		if cache == snapshot {
-			c.snapshots = append(c.snapshots[:i], c.snapshots[i+1:]...)
-			c.snapshotsSize -= snapshot.size
-			break
-		}
-	}
+	c.snapshotSize = 0
+	c.snapshot = nil
+
 }
 
 // Size returns the number of point-calcuated bytes the cache currently uses.
@@ -230,7 +238,7 @@ func (c *Cache) Delete(keys []string) {
 func (c *Cache) merged(key string) Values {
 	e := c.store[key]
 	if e == nil {
-		if len(c.snapshots) == 0 {
+		if c.snapshot == nil {
 			// No values in hot cache or snapshots.
 			return nil
 		}
@@ -242,13 +250,15 @@ func (c *Cache) merged(key string) Values {
 	// Calculate the required size of the destination buffer.
 	var entries []*entry
 	sz := 0
-	for _, s := range c.snapshots {
-		e := s.store[key]
-		if e != nil {
-			entries = append(entries, e)
-			sz += len(e.values)
+
+	if c.snapshot != nil {
+		snapshotEntries := c.snapshot.store[key]
+		if snapshotEntries != nil {
+			entries = append(entries, snapshotEntries)
+			sz += len(snapshotEntries.values)
 		}
 	}
+
 	if e != nil {
 		entries = append(entries, e)
 		sz += len(e.values)
--- tsdb/engine/tsm1/cache_test.go
+++ tsdb/engine/tsm1/cache_test.go
@@ -177,7 +177,7 @@ func TestCache_CacheSnapshot(t *testing.T) {
 	}
 
 	// Clear snapshot, ensuring non-snapshot data untouched.
-	c.ClearSnapshot(snapshot)
+	c.ClearSnapshot()
 	expValues = Values{v5, v4}
 	if deduped := c.Values("foo"); !reflect.DeepEqual(expValues, deduped) {
 		t.Fatalf("post-clear values for foo incorrect, exp: %v, got %v", expValues, deduped)
@@ -199,7 +199,7 @@ func TestCache_CacheEmptySnapshot(t *testing.T) {
 	}
 
 	// Clear snapshot.
-	c.ClearSnapshot(snapshot)
+	c.ClearSnapshot()
 	if deduped := c.Values("foo"); !reflect.DeepEqual(Values(nil), deduped) {
 		t.Fatalf("post-snapshot-clear values for foo incorrect, exp: %v, got %v", Values(nil), deduped)
 	}
@@ -222,13 +222,13 @@ func TestCache_CacheWriteMemoryExceeded(t *testing.T) {
 	}
 
 	// Grab snapshot, write should still fail since we're still using the memory.
-	snapshot := c.Snapshot()
+	_ = c.Snapshot()
 	if err := c.Write("bar", Values{v1}); err != ErrCacheMemoryExceeded {
 		t.Fatalf("wrong error writing key bar to cache")
 	}
 
 	// Clear the snapshot and the write should now succeed.
-	c.ClearSnapshot(snapshot)
+	c.ClearSnapshot()
 	if err := c.Write("bar", Values{v1}); err != nil {
 		t.Fatalf("failed to write key foo to cache: %s", err.Error())
 	}
