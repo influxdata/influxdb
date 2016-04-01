f45263da9150cb6f1dbec050867cb9bbeb64b676
Jon Seymour <jon@wildducktheories.com>
2016-04-01 21:17:15 +1100

    Use preferred go1.4.3 for docker images too.
    Some people (e.g. me!) use build-docker.sh to create docker images.
    
    I got burnt by the go 1.5 performance issue because I didn't override
    the default GO_VER variable.
    
    This change sets the default to the same as used in the main build.
    
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-GO_VER=${GO_VER:-1.5}
+GO_VER=${GO_VER:-1.4.3}
 
 docker run -it -v "${GOPATH}":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:$GO_VER sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o influxd ./cmd/influxd'
 
