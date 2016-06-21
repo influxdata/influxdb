2a1e6533b78c474a792888b64fecbfce85f5403d
Jonathan A. Sternberg <jonathan@influxdb.com>
2016-06-21 10:31:05 -0500

    Rename dumptsmdev to dumptsm in influx_inspect
    Removes the old implementation of `dumptsm`. It was for an older version
    of the tsm1 files that is no longer used and now just panics when used
    on a tsm1 file. dumptsmdev has been renamed to `dumptsm`, but the old
    `dumptsmdev` command still works for compatibility.

--- cmd/influx_inspect/main.go
+++ cmd/influx_inspect/main.go
@@ -16,8 +16,7 @@ Displays detailed information about InfluxDB data files.
 
 	println(`Commands:
   info - displays series meta-data for all shards.  Default location [$HOME/.influxdb]
-  dumptsm - dumps low-level details about tsm1 files.
-  dumptsmdev - dumps low-level details about tsm1dev files.`)
+  dumptsm - dumps low-level details about tsm1 files.`)
 	println()
 }
 
@@ -38,7 +37,7 @@ func main() {
 		fs.StringVar(&path, "dir", os.Getenv("HOME")+"/.influxdb", "Root storage path. [$HOME/.influxdb]")
 
 		fs.Usage = func() {
-			println("Usage: influx_inspect info [options]\n\n   Displays series meta-data for all shards..")
+			println("Usage: influx_inspect info [options]\n\n   Displays series meta-data for all shards.")
 			println()
 			println("Options:")
 			fs.PrintDefaults()
@@ -49,6 +48,9 @@ func main() {
 			os.Exit(1)
 		}
 		cmdInfo(path)
+	case "dumptsmdev":
+		fmt.Fprintf(os.Stderr, "warning: dumptsmdev is deprecated, use dumptsm instead.\n")
+		fallthrough
 	case "dumptsm":
 		var dumpAll bool
 		opts := &tsdmDumpOpts{}
@@ -81,38 +83,6 @@ func main() {
 		opts.dumpBlocks = opts.dumpBlocks || dumpAll || opts.filterKey != ""
 		opts.dumpIndex = opts.dumpIndex || dumpAll || opts.filterKey != ""
 		cmdDumpTsm1(opts)
-	case "dumptsmdev":
-		var dumpAll bool
-		opts := &tsdmDumpOpts{}
-		fs := flag.NewFlagSet("file", flag.ExitOnError)
-		fs.BoolVar(&opts.dumpIndex, "index", false, "Dump raw index data")
-		fs.BoolVar(&opts.dumpBlocks, "blocks", false, "Dump raw block data")
-		fs.BoolVar(&dumpAll, "all", false, "Dump all data. Caution: This may print a lot of information")
-		fs.StringVar(&opts.filterKey, "filter-key", "", "Only display index and block data match this key substring")
-
-		fs.Usage = func() {
-			println("Usage: influx_inspect dumptsm [options] <path>\n\n  Dumps low-level details about tsm1 files.")
-			println()
-			println("Options:")
-			fs.PrintDefaults()
-			os.Exit(0)
-		}
-
-		if err := fs.Parse(flag.Args()[1:]); err != nil {
-			fmt.Printf("%v", err)
-			os.Exit(1)
-		}
-
-		if len(fs.Args()) == 0 || fs.Args()[0] == "" {
-			fmt.Printf("TSM file not specified\n\n")
-			fs.Usage()
-			fs.PrintDefaults()
-			os.Exit(1)
-		}
-		opts.path = fs.Args()[0]
-		opts.dumpBlocks = opts.dumpBlocks || dumpAll || opts.filterKey != ""
-		opts.dumpIndex = opts.dumpIndex || dumpAll || opts.filterKey != ""
-		cmdDumpTsm1dev(opts)
 	case "verify":
 		var path string
 		fs := flag.NewFlagSet("verify", flag.ExitOnError)
