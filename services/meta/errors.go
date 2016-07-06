2c85d498ef444aaa17786af49e2b132a6b40840f
Edd Robinson <me@edd.io>
2016-07-06 15:04:39 +0100

    Remove unused errors

--- services/meta/errors.go
+++ services/meta/errors.go
@@ -11,28 +11,6 @@ var (
 
 	// ErrStoreClosed is returned when closing an already closed store.
 	ErrStoreClosed = errors.New("raft store already closed")
-
-	// ErrTooManyPeers is returned when more than 3 peers are used.
-	ErrTooManyPeers = errors.New("too many peers; influxdb v0.9.0 is limited to 3 nodes in a cluster")
-)
-
-var (
-	// ErrNodeExists is returned when creating an already existing node.
-	ErrNodeExists = errors.New("node already exists")
-
-	// ErrNodeNotFound is returned when mutating a node that doesn't exist.
-	ErrNodeNotFound = errors.New("node not found")
-
-	// ErrNodesRequired is returned when at least one node is required for an operation.
-	// This occurs when creating a shard group.
-	ErrNodesRequired = errors.New("at least one node required")
-
-	// ErrNodeIDRequired is returned when using a zero node id.
-	ErrNodeIDRequired = errors.New("node id must be greater than 0")
-
-	// ErrNodeUnableToDropFinalNode is returned if the node being dropped is the last
-	// node in the cluster
-	ErrNodeUnableToDropFinalNode = errors.New("unable to drop the final node in a cluster")
 )
 
 var (
