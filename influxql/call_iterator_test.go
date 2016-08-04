70c8c021acda433f33dddee819d6771394a86026
Ashish Gaurav <ashish@archsaber.com>
2016-08-04 08:02:19 +0530

    added benchmark tests for median aggrergator (Package: influxql,influxql_test)

--- influxql/call_iterator_test.go
+++ influxql/call_iterator_test.go
@@ -731,6 +731,33 @@ func benchmarkDistinctIterator(b *testing.B, pointN int) {
 	}
 }
 
+func BenchmarkMedianIterator_1K(b *testing.B)   { benchmarkMedianIterator(b, 1000) }
+func BenchmarkMedianIterator_100K(b *testing.B) { benchmarkMedianIterator(b, 100000) }
+func BenchmarkMedianIterator_1M(b *testing.B)   { benchmarkMedianIterator(b, 1000000) }
+
+func benchmarkMedianIterator(b *testing.B, pointN int) {
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		// Create a lightweight point generator.
+		p := influxql.FloatPoint{Name: "cpu"}
+		input := FloatPointGenerator{
+			N: pointN,
+			Fn: func(i int) *influxql.FloatPoint {
+				p.Value = float64(i % 10)
+				return &p
+			},
+		}
+
+		// Execute call against input.
+		itr, err := influxql.NewMedianIterator(&input, influxql.IteratorOptions{})
+		if err != nil {
+			b.Fatal(err)
+		}
+		influxql.DrainIterator(itr)
+	}
+}
+
 type FloatPointGenerator struct {
 	i  int
 	N  int
