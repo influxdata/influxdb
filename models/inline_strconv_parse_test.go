16c3bd20932945a7cd9ef7657b63ad985166fc21
rw <me@rwinslow.com>
2016-09-26 11:40:27 -0700

    Happy-path zero-alloc num parsing fuzz tests.

--- models/inline_strconv_parse_test.go
+++ models/inline_strconv_parse_test.go
@@ -31,7 +31,40 @@ func TestParseIntBytesEquivalenceFuzz(t *testing.T) {
 		return pred
 	}
 	cfg := &quick.Config{
-		MaxCount: 100000,
+		MaxCount: 10000,
+	}
+	if err := quick.Check(f, cfg); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParseIntBytesValid64bitBase10EquivalenceFuzz(t *testing.T) {
+	buf := []byte{}
+	f := func(n int64) bool {
+		buf = strconv.AppendInt(buf[:0], n, 10)
+
+		wantI, wantErr := strconv.ParseInt(string(buf), 10, 64)
+		gotI, gotErr := models.ParseIntBytes(buf, 10, 64)
+
+		pred := wantI == gotI
+
+		// error objects are heap allocated so naive equality checking
+		// won't work here. naive pointer dereferencing will panic
+		// in the case of a nil error.
+		if wantErr != nil && gotErr == nil {
+			pred = false
+		} else if wantErr == nil && gotErr != nil {
+			pred = false
+		} else if wantErr != nil && gotErr != nil {
+			if wantErr.Error() != gotErr.Error() {
+				pred = false
+			}
+		}
+
+		return pred
+	}
+	cfg := &quick.Config{
+		MaxCount: 10000,
 	}
 	if err := quick.Check(f, cfg); err != nil {
 		t.Fatal(err)
@@ -61,7 +94,40 @@ func TestParseFloatBytesEquivalenceFuzz(t *testing.T) {
 		return pred
 	}
 	cfg := &quick.Config{
-		MaxCount: 100000,
+		MaxCount: 10000,
+	}
+	if err := quick.Check(f, cfg); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestParseFloatBytesValid64bitEquivalenceFuzz(t *testing.T) {
+	buf := []byte{}
+	f := func(n float64) bool {
+		buf = strconv.AppendFloat(buf[:0], n, 'f', -1, 64)
+
+		wantI, wantErr := strconv.ParseFloat(string(buf), 64)
+		gotI, gotErr := models.ParseFloatBytes(buf, 64)
+
+		pred := wantI == gotI
+
+		// error objects are heap allocated so naive equality checking
+		// won't work here. naive pointer dereferencing will panic
+		// in the case of a nil error.
+		if wantErr != nil && gotErr == nil {
+			pred = false
+		} else if wantErr == nil && gotErr != nil {
+			pred = false
+		} else if wantErr != nil && gotErr != nil {
+			if wantErr.Error() != gotErr.Error() {
+				pred = false
+			}
+		}
+
+		return pred
+	}
+	cfg := &quick.Config{
+		MaxCount: 10000,
 	}
 	if err := quick.Check(f, cfg); err != nil {
 		t.Fatal(err)
