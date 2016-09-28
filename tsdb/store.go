3edbfb91970d0bf5beca6ed29a690814fbfdfe18
Edd Robinson <me@edd.io>
2016-09-28 13:44:57 +0100

    Prevent panic when shard nil

--- tsdb/store.go
+++ tsdb/store.go
@@ -630,6 +630,9 @@ func (s *Store) cardinalityEstimate(dbName string, getSketches func(*Shard) (est
 // SeriesCardinality returns the series cardinality for the provided database.
 func (s *Store) SeriesCardinality(database string) (int64, error) {
 	return s.cardinalityEstimate(database, func(sh *Shard) (estimator.Sketch, estimator.Sketch, error) {
+		if sh == nil {
+			return nil, nil, errors.New("shard nil, can't get cardinality")
+		}
 		return sh.engine.SeriesSketches()
 	})
 }
@@ -638,6 +641,9 @@ func (s *Store) SeriesCardinality(database string) (int64, error) {
 // database.
 func (s *Store) MeasurementsCardinality(database string) (int64, error) {
 	return s.cardinalityEstimate(database, func(sh *Shard) (estimator.Sketch, estimator.Sketch, error) {
+		if sh == nil {
+			return nil, nil, errors.New("shard nil, can't get cardinality")
+		}
 		return sh.engine.MeasurementsSketches()
 	})
 }
