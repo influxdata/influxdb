1030306d3e6f4c0fe80c4a661df8a3bf23abe915
Michael Desa <mjdesa@gmail.com>
2016-10-06 09:30:04 -0700

    Prevent writing auth command to influx_history

--- cmd/influx/cli/cli.go
+++ cmd/influx/cli/cli.go
@@ -200,7 +200,7 @@ func (c *CommandLine) mainLoop() error {
 				c.exit()
 				return e
 			}
-			if err := c.ParseCommand(l); err != ErrBlankCommand {
+			if err := c.ParseCommand(l); err != ErrBlankCommand && !strings.HasPrefix(l, "auth") {
 				c.Line.AppendHistory(l)
 				c.saveHistory()
 			}
