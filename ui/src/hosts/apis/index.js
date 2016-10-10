dbf1d36d35b5038c095ff891a2e1398ac9146381
Will Piers <willpiers@influxdb.com>
2016-10-10 14:34:07 -0700

    Remove extra promise nonsense

--- ui/src/hosts/apis/index.js
+++ ui/src/hosts/apis/index.js
@@ -2,28 +2,26 @@ import {proxy} from 'utils/queryUrlGenerator';
 import _ from 'lodash';
 
 export function getCpuAndLoadForHosts(proxyLink) {
-  return new Promise((resolve, reject) => {
-    proxy({
-      source: proxyLink,
-      query: `select mean(usage_user) from cpu where cpu = 'cpu-total' and time > now() - 10m group by host; select mean("load1") from "telegraf"."default"."system" where time > now() - 10m group by host`,
-      db: 'telegraf',
-    }).then((resp) => {
-      const hosts = {};
-      const precision = 100;
-      resp.data.results[0].series.forEach((s) => {
-        const meanIndex = s.columns.findIndex((col) => col === 'mean');
-        hosts[s.tags.host] = {
-          name: s.tags.host,
-          cpu: (Math.round(s.values[0][meanIndex] * precision) / precision).toFixed(2),
-        };
-      });
+  return proxy({
+    source: proxyLink,
+    query: `select mean(usage_user) from cpu where cpu = 'cpu-total' and time > now() - 10m group by host; select mean("load1") from "telegraf"."default"."system" where time > now() - 10m group by host`,
+    db: 'telegraf',
+  }).then((resp) => {
+    const hosts = {};
+    const precision = 100;
+    resp.data.results[0].series.forEach((s) => {
+      const meanIndex = s.columns.findIndex((col) => col === 'mean');
+      hosts[s.tags.host] = {
+        name: s.tags.host,
+        cpu: (Math.round(s.values[0][meanIndex] * precision) / precision).toFixed(2),
+      };
+    });
 
-      resp.data.results[1].series.forEach((s) => {
-        const meanIndex = s.columns.findIndex((col) => col === 'mean');
-        hosts[s.tags.host].load = (Math.round(s.values[0][meanIndex] * precision) / precision).toFixed(2);
-      });
+    resp.data.results[1].series.forEach((s) => {
+      const meanIndex = s.columns.findIndex((col) => col === 'mean');
+      hosts[s.tags.host].load = (Math.round(s.values[0][meanIndex] * precision) / precision).toFixed(2);
+    });
 
-      resolve(_.values(hosts));
-    }).catch(reject);
+    return _.values(hosts);
   });
 }
