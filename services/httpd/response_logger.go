931a6c6d089b3a394a4208d876c30631fff0c8a0
zhexuany <zhexuany@gmail.com>
2016-10-13 13:43:38 +0800

    fixed two typo

--- services/httpd/response_logger.go
+++ services/httpd/response_logger.go
@@ -112,7 +112,7 @@ func buildLogLine(l *responseLogger, r *http.Request, start time.Time) string {
 		int64(time.Since(start)/time.Microsecond))
 }
 
-// detect detects the first presense of a non blank string and returns it
+// detect detects the first presence of a non blank string and returns it
 func detect(values ...string) string {
 	for _, v := range values {
 		if v != "" {
