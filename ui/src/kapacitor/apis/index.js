90b93ba7a10683ed3fcd90e22d8cc330aa833ef9
Andrew Watkins <watts@influxdb.com>
2016-11-04 09:36:08 -0700

    Change tasks to rules

--- ui/src/kapacitor/apis/index.js
+++ ui/src/kapacitor/apis/index.js
@@ -3,7 +3,7 @@ import AJAX from 'utils/ajax';
 export function createRule(kapacitor, rule) {
   return AJAX({
     method: 'POST',
-    url: kapacitor.links.tasks,
+    url: kapacitor.links.rules,
     data: rule,
   });
 }
