122bd75b6485218799923b25700a985fe6d78dba
Andrew Watkins <watts@influxdb.com>
2016-11-07 13:08:04 -0800

    Change all 1d to 24h

--- ui/src/kapacitor/constants/index.js
+++ ui/src/kapacitor/constants/index.js
@@ -20,9 +20,9 @@ export const defaultRuleConfigs = {
 
 export const OPERATORS = ['greater than', 'equal to or greater', 'equal to or less than', 'less than', 'equal to', 'not equal to'];
 // export const RELATIONS = ['once', 'more than ', 'less than'];
-export const PERIODS = ['1m', '5m', '10m', '30m', '1h', '2h', '1d'];
+export const PERIODS = ['1m', '5m', '10m', '30m', '1h', '2h', '24h'];
 export const CHANGES = ['change', '% change'];
-export const SHIFTS = ['1m', '5m', '10m', '30m', '1h', '2h', '1d'];
+export const SHIFTS = ['1m', '5m', '10m', '30m', '1h', '2h', '24h'];
 export const ALERTS = ['hipchat', 'opsgenie', 'pagerduty', 'sensu', 'slack', 'smtp', 'talk', 'telegram', 'victorops'];
 
 export const DEFAULT_RULE_ID = 'DEFAULT_RULE_ID';
