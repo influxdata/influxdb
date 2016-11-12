534456f557b6d259578aea93eb995b1e22f49d03
Alex P <thealexpaxton@gmail.com>
2016-11-11 16:54:05 -0800

    Removing debugger background color on graphs

--- ui/src/shared/components/LineGraph.js
+++ ui/src/shared/components/LineGraph.js
@@ -67,7 +67,7 @@ export default React.createClass({
     return (
       <div>
         {this.props.isRefreshing ? <h3 className="graph-panel__spinner--small" /> : null}
-        <Dygraph containerStyle={{width: '100%', height: '300px', backgroundColor: '#f00'}} overrideLineColors={this.props.overrideLineColors} isGraphFilled={this.props.isGraphFilled} timeSeries={timeSeries} fields={fields} options={options} />
+        <Dygraph containerStyle={{width: '100%', height: '300px'}} overrideLineColors={this.props.overrideLineColors} isGraphFilled={this.props.isGraphFilled} timeSeries={timeSeries} fields={fields} options={options} />
       </div>
     );
   },
