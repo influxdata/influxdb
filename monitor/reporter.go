0ea807dce1a250470c6aa4432575c50031318be8
Abdisamad Hashi <shaybix@tuta.io>
2016-11-28 10:47:02 +0000

    Make monitor golintable (#7652)
    * added comment on top of the Reporter interface
    * fixed a capitalisation typo which threw a linting error
    * added comment on top of type Statistics
    * Added a proper comment to Reporter interface
    * Added a proper comment to type Statistics
    * Update service.go
    Changed wording 'list' to 'slice'.

--- monitor/reporter.go
+++ monitor/reporter.go
@@ -2,6 +2,7 @@ package monitor
 
 import "github.com/influxdata/influxdb/models"
 
+// Reporter is an interface for gathering internal statistics
 type Reporter interface {
 	Statistics(tags map[string]string) []models.Statistic
 }
