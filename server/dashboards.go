c85b0e4e2ab486fcad4f534013fe31e90f8bbece
Jade McGough <jade@influxdb.com>
2016-12-07 15:18:04 -0800

    add routes and stubs for dashboard API endpoints

---
+++ server/dashboards.go
@@ -0,0 +1,28 @@
+package server
+
+import "net/http"
+
+// Dashboards returns all dashboards within the store
+func (s *Service) Dashboards(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DashboardID returns a single specified dashboard
+func (s *Service) DashboardID(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// NewDashboard creates and returns a new dashboard object
+func (s *Service) NewDashboard(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// RemoveDashboard deletes a dashboard
+func (s *Service) RemoveDashboard(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// UpdateDashboard updates a dashboard
+func (s *Service) UpdateDashboard(w http.ResponseWriter, r *http.Request) {
+
+}
