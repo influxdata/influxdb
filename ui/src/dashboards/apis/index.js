a3cb25a500de88eb9db1faccc78320257a20cd49
Tim Raymond <tim@timraymond.com>
2016-12-13 17:04:42 -0500

    Make dashboard list fetch from API
    This removes some mocked-out data to make the Dashboard list actually
    hit the API.

--- ui/src/dashboards/apis/index.js
+++ ui/src/dashboards/apis/index.js
@@ -1,17 +1,8 @@
-// import AJAX from 'utils/ajax';
+import AJAX from 'utils/ajax';
 
 export function getDashboards() {
-  // return AJAX({
-    // method: 'GET',
-    // url: `/chronograf/v1/dashboards`,
-  // });
-  return {
-    then(cb) {
-      cb([
-        {name: "Goldeneye System Status"},
-        {name: "Carver Media Group Broadcast System Status"},
-        {name: "Icarus Space Program Status"},
-      ]);
-    },
-  };
+  return AJAX({
+    method: 'GET',
+    url: `/chronograf/v1/dashboards`,
+  });
 }
