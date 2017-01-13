4f60cb50ea2533f6f6d2238390c6202483cb6c9d
Joe LeGasse <joelegasse@gmail.com>
2017-01-13 13:11:09 -0500

    Adjust Tags cloning
    This change delays Tag cloning until a new series is found, and will
    only clone Tags acquired from `ParsePoints...` and not those referencing
    the mmap-ed files (TSM) that are created on startup.

--- models/points.go
+++ models/points.go
@@ -1292,6 +1292,11 @@ func (p *point) Tags() Tags {
 		return p.cachedTags
 	}
 	p.cachedTags = parseTags(p.key)
+
+	for i := range p.cachedTags {
+		p.cachedTags[i].shouldCopy = true
+	}
+
 	return p.cachedTags
 }
 
@@ -1555,6 +1560,9 @@ func (p *point) Split(size int) []Point {
 type Tag struct {
 	Key   []byte
 	Value []byte
+
+	// shouldCopy returns whether or not a tag should be copied when Clone-ing
+	shouldCopy bool
 }
 
 // Clone returns a shallow copy of Tag.
@@ -1562,6 +1570,10 @@ type Tag struct {
 // Tags associated with a Point created by ParsePointsWithPrecision will hold references to the byte slice that was parsed.
 // Use Clone to create a Tag with new byte slices that do not refer to the argument to ParsePointsWithPrecision.
 func (t Tag) Clone() Tag {
+	if !t.shouldCopy {
+		return t
+	}
+
 	other := Tag{
 		Key:   make([]byte, len(t.Key)),
 		Value: make([]byte, len(t.Value)),
@@ -1589,11 +1601,24 @@ func NewTags(m map[string]string) Tags {
 	return a
 }
 
-// Clone returns a shallow copy of Tags.
+// Clone returns a copy of the slice where the elements are a result of calling `Clone` on the original elements
 //
 // Tags associated with a Point created by ParsePointsWithPrecision will hold references to the byte slice that was parsed.
 // Use Clone to create Tags with new byte slices that do not refer to the argument to ParsePointsWithPrecision.
 func (a Tags) Clone() Tags {
+	if len(a) == 0 {
+		return nil
+	}
+
+	needsClone := false
+	for i := 0; i < len(a) && !needsClone; i++ {
+		needsClone = a[i].shouldCopy
+	}
+
+	if !needsClone {
+		return a
+	}
+
 	others := make(Tags, len(a))
 	for i := range a {
 		others[i] = a[i].Clone()
