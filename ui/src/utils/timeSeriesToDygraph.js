8ac86ab88ac4e057a016e3aea0a745f2c8e77863
Hunter Trujillo <cryptoquick@gmail.com>
2017-01-30 08:05:57 -0800

    Reuse shared sparse arrays for GC efficiency. Use fast.js methods to further optimize speed.

--- ui/src/utils/timeSeriesToDygraph.js
+++ ui/src/utils/timeSeriesToDygraph.js
@@ -1,27 +1,47 @@
 import _ from 'lodash';
 import {STROKE_WIDTH} from 'src/shared/constants';
+import {map, reduce, forEach} from 'fast.js';
+
 /**
  * Accepts an array of raw influxdb responses and returns a format
  * that Dygraph understands.
  */
 
+const DEFAULT_SIZE = 0;
+const cells = {
+  label: new Array(DEFAULT_SIZE),
+  value: new Array(DEFAULT_SIZE),
+  time: new Array(DEFAULT_SIZE),
+  seriesIndex: new Array(DEFAULT_SIZE),
+  responseIndex: new Array(DEFAULT_SIZE),
+};
+
 // activeQueryIndex is an optional argument that indicated which query's series we want highlighted.
 export default function timeSeriesToDygraph(raw = [], activeQueryIndex, isInDataExplorer) {
   // collect results from each influx response
-  const results = raw.reduce((acc, rawResponse, responseIndex) => {
+  const results = reduce(raw, (acc, rawResponse, responseIndex) => {
     const responses = _.get(rawResponse, 'response.results', []);
-    const indexedResponses = responses.map((response) => ({...response, responseIndex}));
+    const indexedResponses = map(responses, (response) => ({...response, responseIndex}));
     return [...acc, ...indexedResponses];
   }, []);
 
   // collect each series
-  const serieses = results.reduce((acc, {series = [], responseIndex}, index) => {
-    return [...acc, ...series.map((item) => ({...item, responseIndex, index}))];
+  const serieses = reduce(results, (acc, {series = [], responseIndex}, index) => {
+    return [...acc, ...map(series, (item) => ({...item, responseIndex, index}))];
   }, []);
 
+  const size = reduce(serieses, (acc, {columns, values}) => {
+    if (columns.length && values.length) {
+      return acc + (columns.length - 1) * values.length;
+    }
+    return acc;
+  }, 0);
+
   // convert series into cells with rows and columns
-  const cells = serieses.reduce((acc, {name, columns, values, index, responseIndex, tags = {}}) => {
-    const rows = values.map((vals) => ({
+  let cellIndex = 0;
+
+  forEach(serieses, ({name, columns, values, index, responseIndex, tags = {}}) => {
+    const rows = map(values, (vals) => ({
       name,
       vals,
       index,
@@ -30,73 +50,77 @@ export default function timeSeriesToDygraph(raw = [], activeQueryIndex, isInData
     columns.shift();
 
     // tagSet is each tag key and value for a series
-    const tagSet = Object.keys(tags).map((tag) => `[${tag}=${tags[tag]}]`).sort().join('');
+    const tagSet = map(Object.keys(tags), (tag) => `[${tag}=${tags[tag]}]`).sort().join('');
 
-    rows.forEach(({vals, name: measurement, index: seriesIndex}) => {
+    forEach(rows, ({vals, name: measurement, index: seriesIndex}) => {
       const [time, ...rowValues] = vals;
 
-      rowValues.forEach((value, i) => {
+      forEach(rowValues, (value, i) => {
         const field = columns[i];
-        acc.push({
-          label: `${measurement}.${field}${tagSet}`,
-          value,
-          time,
-          seriesIndex,
-          responseIndex,
-        });
+        cells.label[cellIndex] = `${measurement}.${field}${tagSet}`;
+        cells.value[cellIndex] = value;
+        cells.time[cellIndex] = time;
+        cells.seriesIndex[cellIndex] = seriesIndex;
+        cells.responseIndex[cellIndex] = responseIndex;
+        cellIndex++; // eslint-disable-line no-plusplus
       });
     });
-
-    return acc;
-  }, []);
+  });
 
   // labels are a unique combination of measurement, fields, and tags that indicate a specific series on the graph legend
   const labelMemo = {};
-  const labels = cells.reduce((acc, {label, seriesIndex, responseIndex}) => {
+  const labels = [];
+  for (let i = 0; i < size; i++) {
+    const label = cells.label[i];
+    const seriesIndex = cells.seriesIndex[i];
+    const responseIndex = cells.responseIndex[i];
     const memoKey = `${label}-${seriesIndex}`;
     const existingLabel = labelMemo[memoKey];
 
     if (!existingLabel) {
-      acc.push({
+      labels.push({
         label,
         seriesIndex,
         responseIndex,
       });
       labelMemo[memoKey] = true;
     }
-
-    return acc;
-  }, []);
+  }
 
   const sortedLabels = _.sortBy(labels, 'label');
   const tsMemo = {};
 
-  const timeSeries = cells.reduce((acc, cell) => {
-    let existingRowIndex = tsMemo[cell.time];
+  const timeSeries = [];
+
+  for (let i = 0; i < size; i++) {
+    const time = cells.time[i];
+    const value = cells.value[i];
+    const label = cells.label[i];
+    const seriesIndex = cells.seriesIndex[i];
+
+    let existingRowIndex = tsMemo[time];
 
     if (existingRowIndex === undefined) {
-      acc.push({
-        time: cell.time,
-        values: Array(sortedLabels.length).fill(null),
+      timeSeries.push({
+        time,
+        values: Array(sortedLabels.length).fill(null), // TODO clone
       });
 
-      existingRowIndex = acc.length - 1;
-      tsMemo[cell.time] = existingRowIndex;
+      existingRowIndex = timeSeries.length - 1;
+      tsMemo[time] = existingRowIndex;
     }
 
-    const values = acc[existingRowIndex].values;
-    const labelIndex = sortedLabels.findIndex(({label, seriesIndex}) => label === cell.label && cell.seriesIndex === seriesIndex);;
-    values[labelIndex] = cell.value;
-    acc[existingRowIndex].values = values;
-
-    return acc;
-  }, []);
+    const values = timeSeries[existingRowIndex].values;
+    const labelIndex = sortedLabels.findIndex((find) => find.label === label && find.seriesIndex === seriesIndex);
+    values[labelIndex] = value;
+    timeSeries[existingRowIndex].values = values;
+  }
 
   const sortedTimeSeries = _.sortBy(timeSeries, 'time');
 
   const {light, heavy} = STROKE_WIDTH;
 
-  const dygraphSeries = sortedLabels.reduce((acc, {label, responseIndex}) => {
+  const dygraphSeries = reduce(sortedLabels, (acc, {label, responseIndex}) => {
     acc[label] = {
       strokeWidth: responseIndex === activeQueryIndex ? heavy : light,
     };
@@ -109,8 +133,8 @@ export default function timeSeriesToDygraph(raw = [], activeQueryIndex, isInData
   }, {});
 
   return {
-    labels: ["time", ...sortedLabels.map(({label}) => label)],
-    timeSeries: sortedTimeSeries.map(({time, values}) => ([new Date(time), ...values])),
+    labels: ["time", ...map(sortedLabels, ({label}) => label)],
+    timeSeries: map(sortedTimeSeries, ({time, values}) => ([new Date(time), ...values])),
     dygraphSeries,
   };
 }
