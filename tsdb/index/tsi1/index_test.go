d8a73a1954ae7b75b0cc82fa4e12da14dba10bc0
Edd Robinson <me@edd.io>
2017-02-01 13:43:29 +0000

    Go vet

--- tsdb/index/tsi1/index_test.go
+++ tsdb/index/tsi1/index_test.go
@@ -319,7 +319,7 @@ func (idx *Index) Run(t *testing.T, fn func(t *testing.T)) {
 func (idx *Index) CreateSeriesSliceIfNotExists(a []Series) error {
 	for i, s := range a {
 		if err := idx.CreateSeriesIfNotExists(nil, s.Name, s.Tags); err != nil {
-			return fmt.Errorf("i=%d, name=%s, tags=%s, err=%s", i, s.Name, s.Tags, err)
+			return fmt.Errorf("i=%d, name=%s, tags=%v, err=%s", i, s.Name, s.Tags, err)
 		}
 	}
 	return nil
