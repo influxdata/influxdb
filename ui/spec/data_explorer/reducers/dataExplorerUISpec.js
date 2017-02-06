783e25d5f147e3e28f1b091536202b539d782e99
Andrew Watkins <watts@influxdb.com>
2017-02-06 12:53:36 -0800

    Fix typo

--- ui/spec/data_explorer/reducers/dataExplorerUISpec.js
+++ ui/spec/data_explorer/reducers/dataExplorerUISpec.js
@@ -1,7 +1,7 @@
 import reducer from 'src/data_explorer/reducers/dataExplorerUI';
 import {activatePanel} from 'src/data_explorer/actions/view';
 
-describe('DataExploerer.Reducers.UI', () => {
+describe('DataExplorer.Reducers.UI', () => {
   it('can set the active panel', () => {
     const activePanel = 123;
     const actual = reducer({}, activatePanel(activePanel));
