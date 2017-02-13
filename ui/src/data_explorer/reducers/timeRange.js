f1f7bb372052c1431ba04cab4d3293274326b5ec
Andrew Watkins <watts@influxdb.com>
2017-02-13 16:33:59 -0600

    Use es6 flavorz in timeRange reducer

--- ui/src/data_explorer/reducers/timeRange.js
+++ ui/src/data_explorer/reducers/timeRange.js
@@ -1,5 +1,3 @@
-import update from 'react-addons-update';
-
 const initialState = {
   upper: null,
   lower: 'now() - 15m',
@@ -9,11 +7,12 @@ export default function timeRange(state = initialState, action) {
   switch (action.type) {
     case 'SET_TIME_RANGE': {
       const {upper, lower} = action.payload;
+      const newState = {
+        upper,
+        lower,
+      };
 
-      return update(state, {
-        ['lower']: {$set: lower},
-        ['upper']: {$set: upper},
-      });
+      return {...state, ...newState};
     }
   }
   return state;
