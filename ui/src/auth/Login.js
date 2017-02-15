7f0e61e4841cd45d3d7f26ec9b9f4ba2d3bf7907
Chris Goller <goller@gmail.com>
2017-02-14 23:05:03 -0600

    Add Google login button
    Need to update this eventually to use the dynamic routes

--- ui/src/auth/Login.js
+++ ui/src/auth/Login.js
@@ -10,6 +10,7 @@ const Login = React.createClass({
           <h1 className="auth-text-logo">Chronograf</h1>
           <p><strong>v1.2-beta6</strong> / Time-Series Data Visualization</p>
           <a className="btn btn-primary" href="/oauth/github/login"><span className="icon github"></span> Login with GitHub</a>
+          <a className="btn btn-primary" href="/oauth/google/login"><span className="icon google"></span> Login with Google</a>
         </div>
         <p className="auth-credits">Made by <span className="icon cubo-uniform"></span>InfluxData</p>
         <div className="auth-image"></div>
