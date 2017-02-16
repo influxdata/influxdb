cde17a383799aabbce8538e6e324c88498c184b9
Tim Raymond <tim@timraymond.com>
2017-02-16 17:28:14 -0500

    Add test for (*JWTMux).Login()
    Ensures that state is send properly to Provider.

--- oauth2/mux_test.go
+++ oauth2/mux_test.go
@@ -12,13 +12,13 @@ import (
 	"github.com/influxdata/chronograf/oauth2"
 )
 
+var testTime time.Time = time.Date(1985, time.October, 25, 18, 0, 0, 0, time.UTC)
+
 func Test_JWTMux_Logout_DeletesSessionCookie(t *testing.T) {
 	t.Parallel()
 
 	mp := &MockProvider{"biff@example.com"}
 	jm := oauth2.NewJWTMux(mp, &YesManAuthenticator{}, clog.New(clog.ParseLevel("debug")))
-
-	testTime := time.Date(1985, time.October, 25, 18, 0, 0, 0, time.UTC)
 
 	jm.Now = func() time.Time {
 		return testTime
@@ -60,5 +60,44 @@ func Test_JWTMux_Logout_DeletesSessionCookie(t *testing.T) {
 	c := cookies[0]
 	if c.Name != oauth2.DefaultCookieName || c.Expires != testTime.Add(-1*time.Hour) {
 		t.Fatal("Expected cookie to be expired but wasn't")
+	}
+}
+
+func Test_JWTMux_Login_RedirectsToCorrectURL(t *testing.T) {
+	t.Parallel()
+
+	mp := &MockProvider{"biff@example.com"}
+	jm := oauth2.NewJWTMux(mp, &YesManAuthenticator{}, clog.New(clog.ParseLevel("debug")))
+
+	jm.Now = func() time.Time {
+		return testTime
+	}
+
+	ts := httptest.NewServer(jm.Login())
+	defer ts.Close()
+
+	hc := http.Client{
+		CheckRedirect: func(r *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	resp, err := hc.Get(ts.URL)
+	if err != nil {
+		t.Fatal("Error communicating with Login() handler: err:", err)
+	}
+
+	// Ensure we were redirected
+	if resp.StatusCode < 300 || resp.StatusCode >= 400 {
+		t.Fatal("Expected to be redirected, but received status code", resp.StatusCode)
+	}
+
+	loc, err := resp.Location()
+	if err != nil {
+		t.Fatal("Expected a location to be redirected to, but wasn't present")
+	}
+
+	if state := loc.Query().Get("state"); state != "HELLO?!MCFLY?!ANYONEINTHERE?!" {
+		t.Fatal("Expected state to be set but was", state)
 	}
 }
