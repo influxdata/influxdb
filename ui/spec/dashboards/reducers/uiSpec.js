12b533686449082b68f0fa523a328e8779f310fe
Andrew Watkins <watts@influxdb.com>
2017-02-17 12:17:44 -0600

    Add dashboard timeRange to reducer

--- ui/spec/dashboards/reducers/uiSpec.js
+++ ui/spec/dashboards/reducers/uiSpec.js
@@ -3,46 +3,49 @@ import reducer from 'src/dashboards/reducers/ui'
 import {
   loadDashboards,
   setDashboard,
-} from 'src/dashboards/actions';
+  setTimeRange,
+} from 'src/dashboards/actions'
 
 const noopAction = () => {
-  return {type: 'NOOP'};
+  return {type: 'NOOP'}
 }
 
-let state = undefined;
+let state = undefined
+const timeRange = {upper: null, lower: 'now() - 15m'}
+const d1 = {id: 1, cells: [], name: "d1"}
+const d2 = {id: 2, cells: [], name: "d2"}
+const dashboards = [d1, d2]
 
 describe('DataExplorer.Reducers.UI', () => {
   it('can load the dashboards', () => {
-    const dashboard = {id: 2, cells: [], name: "d2"}
-    const dashboards = [
-      {id: 1, cells: [], name: "d1"},
-      dashboard,
-    ]
-
-    const actual = reducer(state, loadDashboards(dashboards, dashboard.id))
+    const actual = reducer(state, loadDashboards(dashboards, d1.id))
     const expected = {
       dashboards,
-      dashboard,
+      dashboard: d1,
+      timeRange,
     }
 
     expect(actual).to.deep.equal(expected)
-  });
+  })
 
   it('can set a dashboard', () => {
-    const d1 = {id: 2, cells: [], name: "d2"}
-    const d2 = {id: 2, cells: [], name: "d2"}
-    const dashboards = [
-      d1,
-      d2,
-    ]
-
     const loadedState = reducer(state, loadDashboards(dashboards, d1.id))
     const actual = reducer(loadedState, setDashboard(d2.id))
     const expected = {
       dashboards,
       dashboard: d2,
+      timeRange,
     }
 
     expect(actual).to.deep.equal(expected)
-  });
-});
+  })
+
+  it('can set the time range', () => {
+    const expected = {upper: null, lower: 'now() - 1h'}
+    const actual = reducer(state, setTimeRange(expected))
+
+    console.log('actual: ', actual)
+
+    expect(actual.timeRange).to.deep.equal(expected)
+  })
+})
