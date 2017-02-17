717fd1ed3ffadd737409ece5ec40217c1f90ac13
Andrew Watkins <watts@influxdb.com>
2017-02-17 09:02:18 -0600

    Add rudimentary style

--- ui/src/dashboards/components/VisualizationSelector.js
+++ ui/src/dashboards/components/VisualizationSelector.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
 const VisualizationSelector = () => (
-  <div className="">
+  <div className="" style={{
+    display: 'flex',
+    width: '100%',
+    background: '#676978',
+    padding: '10px',
+    borderRadius: '3px',
+    marginBottom: '10px',
+  }}>
     <div className="">
-      Visualizations
-      <div className="btn btn-sm btn-info">
+      VISUALIZATIONS
+      <div className="btn btn-info" style={{margin: "0 5px 0 5px"}}>
         Line Graph
       </div>
-      <div className="btn btn-sm btn-info">
+      <div className="btn btn-info" style={{margin: "0 5px 0 5px"}}>
         SingleStat
       </div>
     </div>
