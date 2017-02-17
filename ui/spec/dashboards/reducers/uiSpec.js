9497456db0718ed96956f673471b68bb5a831164
Andrew Watkins <watts@influxdb.com>
2017-02-17 12:34:22 -0600

    Use redux for dashboard time range

--- ui/spec/dashboards/reducers/uiSpec.js
+++ ui/spec/dashboards/reducers/uiSpec.js
@@ -1,4 +1,5 @@
 import reducer from 'src/dashboards/reducers/ui'
+import timeRanges from 'hson!src/shared/data/timeRanges.hson';
 
 import {
   loadDashboards,
@@ -11,7 +12,7 @@ const noopAction = () => {
 }
 
 let state = undefined
-const timeRange = {upper: null, lower: 'now() - 15m'}
+const timeRange = timeRanges[1];
 const d1 = {id: 1, cells: [], name: "d1"}
 const d2 = {id: 2, cells: [], name: "d2"}
 const dashboards = [d1, d2]
@@ -43,8 +44,6 @@ describe('DataExplorer.Reducers.UI', () => {
   it('can set the time range', () => {
     const expected = {upper: null, lower: 'now() - 1h'}
     const actual = reducer(state, setTimeRange(expected))
-
-    console.log('actual: ', actual)
 
     expect(actual.timeRange).to.deep.equal(expected)
   })
