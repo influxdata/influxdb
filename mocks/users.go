9a80d5bdce6aacabdef35d85abb926946c5e18fe
Chris Goller <goller@gmail.com>
2017-02-21 21:36:04 -0600

    Add mock usersstore.
    Mocks are back!

---
+++ mocks/users.go
@@ -0,0 +1,41 @@
+package mocks
+
+import (
+	"context"
+
+	"github.com/influxdata/chronograf"
+)
+
+// UsersStore mock allows all functions to be set for testing
+type UsersStore struct {
+	AllF    func(context.Context) ([]chronograf.User, error)
+	AddF    func(context.Context, *chronograf.User) (*chronograf.User, error)
+	DeleteF func(context.Context, *chronograf.User) error
+	GetF    func(ctx context.Context, name string) (*chronograf.User, error)
+	UpdateF func(context.Context, *chronograf.User) error
+}
+
+// All lists all users from the UsersStore
+func (s *UsersStore) All(ctx context.Context) ([]chronograf.User, error) {
+	return s.AllF(ctx)
+}
+
+// Add a new User in the UsersStore
+func (s *UsersStore) Add(ctx context.Context, u *chronograf.User) (*chronograf.User, error) {
+	return s.AddF(ctx, u)
+}
+
+// Delete the User from the UsersStore
+func (s *UsersStore) Delete(ctx context.Context, u *chronograf.User) error {
+	return s.DeleteF(ctx, u)
+}
+
+// Get retrieves a user if name exists.
+func (s *UsersStore) Get(ctx context.Context, name string) (*chronograf.User, error) {
+	return s.GetF(ctx, name)
+}
+
+// Update the user's permissions or roles
+func (s *UsersStore) Update(ctx context.Context, u *chronograf.User) error {
+	return s.UpdateF(ctx, u)
+}
