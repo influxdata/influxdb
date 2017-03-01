4be9b7520a5a3fb1f99d35346da80137e5f5d972
Andrew Watkins <watts@influxdb.com>
2017-03-01 10:04:31 -0800

    Remove dead function

--- ui/src/utils/queryUrlGenerator.js
+++ ui/src/utils/queryUrlGenerator.js
@@ -1,11 +1,5 @@
 import AJAX from 'utils/ajax';
 
-// TODO: delete this once all references
-// to it have been removed
-export function buildInfluxUrl() {
-  return "You dont need me anymore";
-}
-
 export function proxy({source, query, db, rp}) {
   return AJAX({
     method: 'POST',
