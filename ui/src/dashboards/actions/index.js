9eeffb7b18e741e58a73c2abd011fd4bfa846029
Tim Raymond <tim@timraymond.com>
2017-03-14 10:39:43 -0400

    Use implicit returns for dashboard action creators
    Implicit returns are more concise here, and considered more idiomatic.
    Also, async action creators have been collected together and moved
    towards the end of the file for ease of navigation.

--- ui/src/dashboards/actions/index.js
+++ ui/src/dashboards/actions/index.js
@@ -3,64 +3,53 @@ import {
   updateDashboard as updateDashboardAJAX,
 } from 'src/dashboards/apis'
 
-export function loadDashboards(dashboards, dashboardID) {
-  return {
-    type: 'LOAD_DASHBOARDS',
-    payload: {
-      dashboards,
-      dashboardID,
-    },
-  }
+export const loadDashboards = (dashboards, dashboardID) => ({
+  type: 'LOAD_DASHBOARDS',
+  payload: {
+    dashboards,
+    dashboardID,
+  },
+})
+
+export const setDashboard = (dashboardID) => ({
+  type: 'SET_DASHBOARD',
+  payload: {
+    dashboardID,
+  },
+})
+
+export const setTimeRange = (timeRange) => ({
+  type: 'SET_DASHBOARD_TIME_RANGE',
+  payload: {
+    timeRange,
+  },
+})
+
+export const setEditMode = (isEditMode) => ({
+  type: 'SET_EDIT_MODE',
+  payload: {
+    isEditMode,
+  },
+})
+
+export const updateDashboard = (dashboard) => ({
+  type: 'UPDATE_DASHBOARD',
+  payload: {
+    dashboard,
+  },
+})
+
+
+// Async Action Creators
+
+export const getDashboards = (dashboardID) => (dispatch) => {
+  getDashboardsAJAX().then(({data: {dashboards}}) => {
+    dispatch(loadDashboards(dashboards, dashboardID))
+  })
 }
 
-export function setDashboard(dashboardID) {
-  return {
-    type: 'SET_DASHBOARD',
-    payload: {
-      dashboardID,
-    },
-  }
-}
-
-export function setTimeRange(timeRange) {
-  return {
-    type: 'SET_DASHBOARD_TIME_RANGE',
-    payload: {
-      timeRange,
-    },
-  }
-}
-
-export function setEditMode(isEditMode) {
-  return {
-    type: 'SET_EDIT_MODE',
-    payload: {
-      isEditMode,
-    },
-  }
-}
-
-export function getDashboards(dashboardID) {
-  return (dispatch) => {
-    getDashboardsAJAX().then(({data: {dashboards}}) => {
-      dispatch(loadDashboards(dashboards, dashboardID))
-    });
-  }
-}
-
-export function putDashboard(dashboard) {
-  return (dispatch) => {
-    updateDashboardAJAX(dashboard).then(({data}) => {
-      dispatch(updateDashboard(data))
-    })
-  }
-}
-
-export function updateDashboard(dashboard) {
-  return {
-    type: 'UPDATE_DASHBOARD',
-    payload: {
-      dashboard,
-    },
-  }
+export const putDashboard = (dashboard) => (dispatch) => {
+  updateDashboardAJAX(dashboard).then(({data}) => {
+    dispatch(updateDashboard(data))
+  })
 }
