3df565f37e46f998ba937de77878979163b3c23c
Andrew Watkins <watts@influxdb.com>
2017-03-21 10:56:51 -0700

    Make inputs uncontrolled

--- ui/src/admin/components/DatabaseRow.js
+++ ui/src/admin/components/DatabaseRow.js
@@ -1,84 +1,112 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 import {formatRPDuration} from 'utils/formatting'
 import ConfirmButtons from 'src/admin/components/ConfirmButtons'
 import onClickOutside from 'react-onclickoutside'
 
-const DatabaseRow = ({
-  retentionPolicy,
-  retentionPolicy: {name, duration, replication, isEditing, isDefault, isNew},
-  database,
-  onEdit,
-  onStopEdit,
-  onKeyDown,
-  onCancel,
-  onConfirm,
-}) => {
-  if (isEditing) {
+class DatabaseRow extends Component {
+  constructor(props) {
+    super(props)
+    this.handleKeyDown = ::this.handleKeyDown
+  }
+
+  handleKeyDown(e, db) {
+    const {key} = e
+
+    if (rp.isNew) {
+      if (key === 'Escape') {
+        // return actions.removeRetentionPolicy(db, rp)
+      }
+
+      if (key === 'Enter') {
+        // return actions.createRetentionPolicyAsync(db, rp)
+      }
+    }
+
+    if (key === 'Escape') {
+      // actions.stopEditRetentionPolicy(db, rp)
+    }
+
+    if (key === 'Enter') {
+      // actions.updateRetentionPolicy(db, rp)
+    }
+  }
+
+  render() {
+    const {
+      retentionPolicy,
+      retentionPolicy: {name, duration, replication, isEditing, isDefault, isNew},
+      database,
+      onEdit,
+      onStopEdit,
+      onKeyDown,
+      onCancel,
+      onConfirm,
+    } = this.props
+
+    if (isEditing) {
+      return (
+        <tr>
+          <td>
+            <div className="admin-table--edit-cell">
+              <input
+                className="form-control"
+                name="name"
+                type="text"
+                defaultValue={name}
+                placeholder="give it a name"
+                onKeyDown={(e) => onKeyDown(e, database)}
+                autoFocus={true}
+              />
+            </div>
+          </td>
+          <td>
+            <div className="admin-table--edit-cell">
+              <input
+                className="form-control"
+                name="name"
+                type="text"
+                value={duration}
+                placeholder="how long should data last"
+                onKeyDown={(e) => onKeyDown(e, database)}
+              />
+            </div>
+          </td>
+          <td>
+            <div className="admin-table--edit-cell">
+              <input
+                className="form-control"
+                name="name"
+                type="number"
+                min="1"
+                value={replication || 1}
+                placeholder="how many nodes do you have"
+                onKeyDown={(e) => onKeyDown(e, database)}
+              />
+            </div>
+          </td>
+          <td className="text-right">
+            <ConfirmButtons item={{database, retentionPolicy}} onConfirm={isNew ? onConfirm : () => {}} onCancel={isNew ? onCancel : onStopEdit} />
+          </td>
+        </tr>
+      )
+    }
+
     return (
       <tr>
-        <td>
-          <div className="admin-table--edit-cell">
-            <input
-              className="form-control"
-              name="name"
-              type="text"
-              value={name}
-              placeholder="give it a name"
-              onChange={(e) => onEdit(database, {...retentionPolicy, name: e.target.value})}
-              onKeyDown={(e) => onKeyDown(e, database, retentionPolicy)}
-              autoFocus={true}
-            />
-          </div>
+        <td onClick={() => onEdit(database, retentionPolicy)}>
+          {name}
+          {isDefault ? <span className="default-source-label">default</span> : null}
         </td>
-        <td>
-          <div className="admin-table--edit-cell">
-            <input
-              className="form-control"
-              name="name"
-              type="text"
-              value={duration}
-              placeholder="how long should data last"
-              onChange={(e) => onEdit(database, {...retentionPolicy, duration: e.target.value})}
-              onKeyDown={(e) => onKeyDown(e, database, retentionPolicy)}
-            />
-          </div>
-        </td>
-        <td>
-          <div className="admin-table--edit-cell">
-            <input
-              className="form-control"
-              name="name"
-              type="number"
-              min="1"
-              value={replication || 1}
-              placeholder="how many nodes do you have"
-              onChange={(e) => onEdit(database, {...retentionPolicy, replication: +e.target.value})}
-              onKeyDown={(e) => onKeyDown(e, database, retentionPolicy)}
-            />
-          </div>
-        </td>
+        <td onClick={() => onEdit(database, retentionPolicy)}>{formatRPDuration(duration)}</td>
+        <td onClick={() => onEdit(database, retentionPolicy)}>{replication}</td>
         <td className="text-right">
-          <ConfirmButtons item={{database, retentionPolicy}} onConfirm={isNew ? onConfirm : () => {}} onCancel={isNew ? onCancel : onStopEdit} />
+          <button className="btn btn-xs btn-danger admin-table--delete">
+            {`Delete ${name}`}
+          </button>
         </td>
       </tr>
     )
   }
-
-  return (
-    <tr>
-      <td onClick={() => onEdit(database, retentionPolicy)}>
-        {name}
-        {isDefault ? <span className="default-source-label">default</span> : null}
-      </td>
-      <td onClick={() => onEdit(database, retentionPolicy)}>{formatRPDuration(duration)}</td>
-      <td onClick={() => onEdit(database, retentionPolicy)}>{replication}</td>
-      <td className="text-right">
-        <button className="btn btn-xs btn-danger admin-table--delete">
-          {`Delete ${name}`}
-        </button>
-      </td>
-    </tr>
-  )
 }
 
 const {
