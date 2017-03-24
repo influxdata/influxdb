9470bd1a4103b02d71b24c28ef97d9e3325ed2e3
Luke Morris <lukevmorris@gmail.com>
2017-03-24 13:29:10 -0700

    Remove VisualizationSelector from Dashboard Edit Mode

--- ui/src/dashboards/components/Dashboard.js
+++ ui/src/dashboards/components/Dashboard.js
@@ -2,7 +2,6 @@ import React, {PropTypes} from 'react'
 import classnames from 'classnames'
 
 import LayoutRenderer from 'shared/components/LayoutRenderer'
-import Visualizations from 'src/dashboards/components/VisualizationSelector'
 
 const Dashboard = ({
   dashboard,
@@ -24,7 +23,6 @@ const Dashboard = ({
   return (
     <div className={classnames({'page-contents': true, 'presentation-mode': inPresentationMode})}>
       <div className={classnames('container-fluid full-width dashboard', {'dashboard-edit': isEditMode})}>
-        {isEditMode ? <Visualizations/> : null}
         {Dashboard.renderDashboard(dashboard, autoRefresh, timeRange, source, onPositionChange, onEditCell, onRenameCell, onUpdateCell, onSummonOverlayTechnologies)}
       </div>
     </div>
