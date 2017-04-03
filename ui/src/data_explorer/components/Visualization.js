duanshuaimin/chronograf

Add vis header and console tab

ui/src/data_explorer/components/Visualization.js
@@ -1,20 +1,22 @@
-import React, {PropTypes} from 'react';
-import selectStatement from '../utils/influxql/select';
-import classNames from 'classnames';
-import AutoRefresh from 'shared/components/AutoRefresh';
-import LineGraph from 'shared/components/LineGraph';
-import SingleStat from 'shared/components/SingleStat';
-import MultiTable from './MultiTable';
+import React, {PropTypes} from 'react'
+import selectStatement from '../utils/influxql/select'
+import classNames from 'classnames'
+import AutoRefresh from 'shared/components/AutoRefresh'
+import LineGraph from 'shared/components/LineGraph'
+import SingleStat from 'shared/components/SingleStat'
+import MultiTable from './MultiTable'
+import VisHeader from './VisHeader'
 
-const RefreshingLineGraph = AutoRefresh(LineGraph);
-const RefreshingSingleStat = AutoRefresh(SingleStat);
+const RefreshingLineGraph = AutoRefresh(LineGraph)
+const RefreshingSingleStat = AutoRefresh(SingleStat)
+const VIEWS = ['graph', 'table', 'console']
 
 const {
   arrayOf,
   number,
   shape,
   string,
-} = PropTypes;
+} = PropTypes
 
 const Visualization = React.createClass({
   propTypes: {
@@ -41,17 +43,54 @@ const Visualization = React.createClass({
 
   getInitialState() {
     return {
-      isGraphInView: true,
-    };
+      view: 'graph',
+    }
   },
 
-  handleToggleView() {
-    this.setState({isGraphInView: !this.state.isGraphInView});
+  handleToggleView(view) {
+    this.setState({view})
+  },
+
+  render() {
+    const {queryConfigs, timeRange, height, heightPixels} = this.props;
+    const {source} = this.context;
+    const proxyLink = source.links.proxy;
+    const {view} = this.state;
+
+    const statements = queryConfigs.map((query) => {
+      const text = query.rawText || selectStatement(timeRange, query);
+      return {text, id: query.id};
+    });
+    const queries = statements.filter((s) => s.text !== null).map((s) => {
+      return {host: [proxyLink], text: s.text, id: s.id};
+    });
+
+    return (
+      <div className={classNames("graph", {active: true})} style={{height}}>
+        <VisHeader views={VIEWS} view={view} onToggleView={this.handleToggleView} name={name || 'Graph'}/>
+        <div className={classNames({"graph-container": view === 'graph', "table-container": view === 'table'})}>
+          {this.renderVisualization(view, queries, heightPixels)}
+        </div>
+      </div>
+    );
+  },
+
+  renderVisualization(view, queries, heightPixels) {
+    switch (view) {
+      case 'graph':
+        return this.renderGraph(queries)
+      case 'table':
+        return <MultiTable queries={queries} height={heightPixels} />
+      case 'console':
+        return <div>I'm a console</div>
+      default:
+        this.renderGraph(queries)
+    }
   },
 
   renderGraph(queries) {
     const {cellType, autoRefresh, activeQueryIndex} = this.props
-    const isInDataExplorer = true;
+    const isInDataExplorer = true
 
     if (cellType === 'single-stat') {
       return <RefreshingSingleStat queries={[queries[0]]} autoRefresh={autoRefresh} />
@@ -73,41 +112,6 @@ const Visualization = React.createClass({
     )
   },
 
-  render() {
-    const {queryConfigs, timeRange, height, heightPixels} = this.props;
-    const {source} = this.context;
-    const proxyLink = source.links.proxy;
+})
 
-    const {isGraphInView} = this.state;
-    const statements = queryConfigs.map((query) => {
-      const text = query.rawText || selectStatement(timeRange, query);
-      return {text, id: query.id};
-    });
-    const queries = statements.filter((s) => s.text !== null).map((s) => {
-      return {host: [proxyLink], text: s.text, id: s.id};
-    });
-
-    return (
-      <div className={classNames("graph", {active: true})} style={{height}}>
-        <div className="graph-heading">
-          <div className="graph-title">
-            {name || "Graph"}
-          </div>
-          <div className="graph-actions">
-            <ul className="toggle toggle-sm">
-              <li onClick={this.handleToggleView} className={classNames("toggle-btn ", {active: isGraphInView})}>Graph</li>
-              <li onClick={this.handleToggleView} className={classNames("toggle-btn ", {active: !isGraphInView})}>Table</li>
-            </ul>
-          </div>
-        </div>
-        <div className={classNames({"graph-container": isGraphInView, "table-container": !isGraphInView})}>
-          {isGraphInView ?
-            this.renderGraph(queries) :
-            <MultiTable queries={queries} height={heightPixels} />}
-        </div>
-      </div>
-    );
-  },
-});
-
-export default Visualization;
+export default Visualization

influxdata/influxdb

ui/src/data_explorer/components/Visualization.js
@@ -8,6 +8,7 @@ import MultiTable from './MultiTable'
 
 const RefreshingLineGraph = AutoRefresh(LineGraph)
 const RefreshingSingleStat = AutoRefresh(SingleStat)
+const VIEWS = ['graph', 'table', 'console']
 
 const {
   arrayOf,
@@ -41,12 +42,49 @@ const Visualization = React.createClass({
 
   getInitialState() {
     return {
-      isGraphInView: true,
+      view: 'graph',
     }
   },
 
-  handleToggleView() {
-    this.setState({isGraphInView: !this.state.isGraphInView})
+  handleToggleView(view) {
+    this.setState({view})
+  },
+
+  render() {
+    const {queryConfigs, timeRange, height, heightPixels} = this.props;
+    const {source} = this.context;
+    const proxyLink = source.links.proxy;
+    const {view} = this.state;
+
+    const statements = queryConfigs.map((query) => {
+      const text = query.rawText || buildInfluxQLQuery(timeRange, query);
+      return {text, id: query.id};
+    });
+    const queries = statements.filter((s) => s.text !== null).map((s) => {
+      return {host: [proxyLink], text: s.text, id: s.id};
+    });
+
+    return (
+      <div className={classNames("graph", {active: true})} style={{height}}>
+        <VisHeader views={VIEWS} view={view} onToggleView={this.handleToggleView} name={name || 'Graph'}/>
+        <div className={classNames({"graph-container": view === 'graph', "table-container": view === 'table'})}>
+          {this.renderVisualization(view, queries, heightPixels)}
+        </div>
+      </div>
+    );
+  },
+
+  renderVisualization(view, queries, heightPixels) {
+    switch (view) {
+      case 'graph':
+        return this.renderGraph(queries)
+      case 'table':
+        return <MultiTable queries={queries} height={heightPixels} />
+      case 'console':
+        return <div>I'm a console</div>
+      default:
+        this.renderGraph(queries)
+    }
   },
 
   renderGraph(queries) {
@@ -70,49 +108,6 @@ const Visualization = React.createClass({
         showSingleStat={cellType === "line-plus-single-stat"}
         displayOptions={displayOptions}
       />
-    )
-  },
-
-  render() {
-    const {
-      queryConfigs,
-      timeRange,
-      height,
-      heightPixels,
-      cellName,
-    } = this.props
-
-    const {source} = this.context
-    const proxyLink = source.links.proxy
-
-    const {isGraphInView} = this.state
-    const statements = queryConfigs.map((query) => {
-      const text = query.rawText || buildInfluxQLQuery(timeRange, query)
-      return {text, id: query.id}
-    })
-    const queries = statements.filter((s) => s.text !== null).map((s) => {
-      return {host: [proxyLink], text: s.text, id: s.id}
-    })
-
-    return (
-      <div className={classNames("graph", {active: true})} style={{height}}>
-        <div className="graph-heading">
-          <div className="graph-title">
-            {cellName || "Graph"}
-          </div>
-          <div className="graph-actions">
-            <ul className="toggle toggle-sm">
-              <li onClick={this.handleToggleView} className={classNames("toggle-btn ", {active: isGraphInView})}>Graph</li>
-              <li onClick={this.handleToggleView} className={classNames("toggle-btn ", {active: !isGraphInView})}>Table</li>
-            </ul>
-          </div>
-        </div>
-        <div className={classNames({"graph-container": isGraphInView, "table-container": !isGraphInView})}>
-          {isGraphInView ?
-            this.renderGraph(queries) :
-            <MultiTable queries={queries} height={heightPixels} />}
-        </div>
-      </div>
     )
   },
 })