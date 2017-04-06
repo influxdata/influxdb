ac4423a6fd5d4d64216f1619797dbd9a202588f5
Jared Scheib <jaredscheib@users.noreply.github.com>
2017-04-06 11:40:57 -0700

    Add new auth duration CLI option; add client heartbeat; fix logout (#1119)
    * User can now set oauth cookie session duration via the CLI to any duration or to expire on browser close
    
    * Refactor GET 'me' into heartbeat at constant interval
    
    * Add ping route to all routes
    
    * Add /chronograf/v1/ping endpoint for server status
    
    * Refactor cookie generation to use an interface
    
    * WIP adding refreshable tokens
    
    * Add reminder to review index.js Login error handling
    
    * Refactor Authenticator interface to accommodate cookie duration and logout delay
    
    * Update make run-dev to be more TICKStack compliant
    
    * Remove heartbeat/logout duration from authentication
    
    * WIP Refactor tests to accommodate cookie and auth refactor
    
    * Update oauth2 tests to newly refactored design
    
    * Update oauth provider tests
    
    * Remove unused oauth2/consts.go
    
    * Move authentication middleware to server package
    
    * Fix authentication comment
    
    * Update authenication documentation to mention AUTH_DURATION
    
    * Update /chronograf/v1/ping to simply return 204
    
    * Fix Makefile run-dev target
    
    * Remove spurious ping route
    
    * Update auth docs to clarify authentication duration
    
    * Revert "Refactor GET 'me' into heartbeat at constant interval"
    
    This reverts commit 298a8c47e1431720d9bd97a9cb853744f04501a3.
    
    Conflicts:
    ui/src/index.js
    
    * Add auth test for JWT signing method
    
    * Add comments for why coverage isn't written for some areas of jwt code
    
    * Update auth docs to explicitly mention how to require re-auth for all users on server restart
    
    * Add Duration to Validation interface for Tokens
    
    * Make auth duration of zero yield a everlasting token
    
    * Revert "Revert "Refactor GET 'me' into heartbeat at constant interval""
    
    This reverts commit b4773c15afe4fcd227ad88aa9d5686beb6b0a6cd.
    
    * Rename http status constants and add FORBIDDEN
    
    * Heartbeat only when logged in, notify user if heartbeat fails
    
    * Update changelog
    
    * Fix minor word semantics
    
    * Update oauth2 tests to be in the oauth2_test package
    
    * Add check at compile time that JWT implements Tokenizer
    
    * Rename CookieMux to AuthMux for consistency with earlier refactor
    
    * Fix logout middleware
    
    * Fix logout button not showing due to obsolete data shape expectations
    
    * Update changelog
    
    * Fix proptypes for logout button data shape in SideNav

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,12 +17,14 @@
   1. [#1179](https://github.com/influxdata/chronograf/pull/1179): Admin Databases Page will render a database without retention policies
   1. [#1128](https://github.com/influxdata/chronograf/pull/1128): No more ghost dashboards 👻
   1. [#1189](https://github.com/influxdata/chronograf/pull/1189): Clicking inside the graph header edit box will no longer blur the field. Use the Escape key for that behavior instead.
+  1. [#1095](https://github.com/influxdata/chronograf/pull/1095): Make logout button display again
 
 ### Features
   1. [#1112](https://github.com/influxdata/chronograf/pull/1112): Add ability to delete a dashboard
   1. [#1120](https://github.com/influxdata/chronograf/pull/1120): Allow users to update user passwords.
   1. [#1129](https://github.com/influxdata/chronograf/pull/1129): Allow InfluxDB and Kapacitor configuration via ENV vars or CLI options
   1. [#1130](https://github.com/influxdata/chronograf/pull/1130): Add loading spinner to Alert History page.
+  1. [#1095](https://github.com/influxdata/chronograf/pull/1095): Add new auth duration CLI option; add client heartbeat
   1. [#1168](https://github.com/influxdata/chronograf/issue/1168): Expand support for --basepath on some load balancers
 
 ### UI Improvements
--- oauth2/jwt_test.go
+++ oauth2/jwt_test.go
@@ -11,73 +11,96 @@ import (
 
 func TestAuthenticate(t *testing.T) {
 	var tests = []struct {
-		Desc   string
-		Secret string
-		Token  string
-		User   oauth2.Principal
-		Err    error
+		Desc      string
+		Secret    string
+		Token     oauth2.Token
+		Duration  time.Duration
+		Principal oauth2.Principal
+		Err       error
 	}{
 		{
 			Desc:   "Test bad jwt token",
 			Secret: "secret",
 			Token:  "badtoken",
-			User: oauth2.Principal{
+			Principal: oauth2.Principal{
 				Subject: "",
 			},
 			Err: errors.New("token contains an invalid number of segments"),
 		},
 		{
-			Desc:   "Test valid jwt token",
-			Secret: "secret",
-			Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0NDAwLCJuYmYiOi00NDY3NzQ0MDB9._rZ4gOIei9PizHOABH6kLcJTA3jm8ls0YnDxtz1qeUI",
-			User: oauth2.Principal{
+			Desc:     "Test valid jwt token",
+			Secret:   "secret",
+			Token:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0Mzk5LCJuYmYiOi00NDY3NzQ0MDB9.Ga0zGXWTT2CBVnnIhIO5tUAuBEVk4bKPaT4t4MU1ngo",
+			Duration: time.Second,
+			Principal: oauth2.Principal{
 				Subject: "/chronograf/v1/users/1",
 			},
 		},
 		{
-			Desc:   "Test expired jwt token",
-			Secret: "secret",
-			Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0NDAxLCJuYmYiOi00NDY3NzQ0MDB9.vWXdm0-XQ_pW62yBpSISFFJN_yz0vqT9_INcUKTp5Q8",
-			User: oauth2.Principal{
+			Desc:     "Test expired jwt token",
+			Secret:   "secret",
+			Token:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0NDAxLCJuYmYiOi00NDY3NzQ0MDB9.vWXdm0-XQ_pW62yBpSISFFJN_yz0vqT9_INcUKTp5Q8",
+			Duration: time.Second,
+			Principal: oauth2.Principal{
 				Subject: "",
 			},
 			Err: errors.New("token is expired by 1s"),
 		},
 		{
-			Desc:   "Test jwt token not before time",
-			Secret: "secret",
-			Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0NDAwLCJuYmYiOi00NDY3NzQzOTl9.TMGAhv57u1aosjc4ywKC7cElP1tKyQH7GmRF2ToAxlE",
-			User: oauth2.Principal{
+			Desc:     "Test jwt token not before time",
+			Secret:   "secret",
+			Token:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0NDAwLCJuYmYiOi00NDY3NzQzOTl9.TMGAhv57u1aosjc4ywKC7cElP1tKyQH7GmRF2ToAxlE",
+			Duration: time.Second,
+			Principal: oauth2.Principal{
 				Subject: "",
 			},
 			Err: errors.New("token is not valid yet"),
 		},
 		{
-			Desc:   "Test jwt with empty subject is invalid",
-			Secret: "secret",
-			Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi00NDY3NzQ0MDAsImV4cCI6LTQ0Njc3NDQwMCwibmJmIjotNDQ2Nzc0NDAwfQ.gxsA6_Ei3s0f2I1TAtrrb8FmGiO25OqVlktlF_ylhX4",
-			User: oauth2.Principal{
+			Desc:     "Test jwt with empty subject is invalid",
+			Secret:   "secret",
+			Token:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOi00NDY3NzQ0MDAsImV4cCI6LTQ0Njc3NDQwMCwibmJmIjotNDQ2Nzc0NDAwfQ.gxsA6_Ei3s0f2I1TAtrrb8FmGiO25OqVlktlF_ylhX4",
+			Duration: time.Second,
+			Principal: oauth2.Principal{
 				Subject: "",
 			},
 			Err: errors.New("claim has no subject"),
 		},
+		{
+			Desc:     "Test jwt duration matches auth duration",
+			Secret:   "secret",
+			Token:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0NDAwLCJuYmYiOi00NDY3NzQ0MDB9._rZ4gOIei9PizHOABH6kLcJTA3jm8ls0YnDxtz1qeUI",
+			Duration: 500 * time.Hour,
+			Principal: oauth2.Principal{
+				Subject: "/chronograf/v1/users/1",
+			},
+			Err: errors.New("claims duration is different from auth duration"),
+		},
+		{
+			Desc:   "Test valid EverlastingClaim",
+			Secret: "secret",
+			Token:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIiwibmFtZSI6IkRvYyBCcm93biIsImlhdCI6LTQ0Njc3NDQwMCwiZXhwIjotNDQ2Nzc0Mzk5LCJuYmYiOi00NDY3NzQ0MDB9.Ga0zGXWTT2CBVnnIhIO5tUAuBEVk4bKPaT4t4MU1ngo",
+			Principal: oauth2.Principal{
+				Subject: "/chronograf/v1/users/1",
+			},
+		},
 	}
-	for i, test := range tests {
+	for _, test := range tests {
 		j := oauth2.JWT{
 			Secret: test.Secret,
 			Now: func() time.Time {
 				return time.Unix(-446774400, 0)
 			},
 		}
-		user, err := j.Authenticate(context.Background(), test.Token)
+		principal, err := j.ValidPrincipal(context.Background(), test.Token, test.Duration)
 		if err != nil {
 			if test.Err == nil {
-				t.Errorf("Error in test %d authenticating with bad token: %v", i, err)
+				t.Errorf("Error in test %s authenticating with bad token: %v", test.Desc, err)
 			} else if err.Error() != test.Err.Error() {
-				t.Errorf("Error in test %d expected error: %v actual: %v", i, err, test.Err)
+				t.Errorf("Error in test %s expected error: %v actual: %v", test.Desc, test.Err, err)
 			}
-		} else if test.User != user {
-			t.Errorf("Error in test %d; users different; expected: %v  actual: %v", i, test.User, user)
+		} else if test.Principal != principal {
+			t.Errorf("Error in test %s; principals different; expected: %v  actual: %v", test.Desc, test.Principal, principal)
 		}
 	}
 
@@ -85,7 +108,7 @@ func TestAuthenticate(t *testing.T) {
 
 func TestToken(t *testing.T) {
 	duration := time.Second
-	expected := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOi00NDY3NzQzOTksImlhdCI6LTQ0Njc3NDQwMCwibmJmIjotNDQ2Nzc0NDAwLCJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIn0.ofQM6yTmrmve5JeEE0RcK4_euLXuZ_rdh6bLAbtbC9M"
+	expected := oauth2.Token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOi00NDY3NzQzOTksImlhdCI6LTQ0Njc3NDQwMCwibmJmIjotNDQ2Nzc0NDAwLCJzdWIiOiIvY2hyb25vZ3JhZi92MS91c2Vycy8xIn0.ofQM6yTmrmve5JeEE0RcK4_euLXuZ_rdh6bLAbtbC9M")
 	j := oauth2.JWT{
 		Secret: "secret",
 		Now: func() time.Time {
@@ -95,9 +118,19 @@ func TestToken(t *testing.T) {
 	p := oauth2.Principal{
 		Subject: "/chronograf/v1/users/1",
 	}
-	if token, err := j.Token(context.Background(), p, duration); err != nil {
-		t.Errorf("Error creating token for user: %v", err)
+	if token, err := j.Create(context.Background(), p, duration); err != nil {
+		t.Errorf("Error creating token for principal: %v", err)
 	} else if token != expected {
-		t.Errorf("Error creating token; expected: %s  actual: %s", "", token)
+		t.Errorf("Error creating token; expected: %s  actual: %s", expected, token)
+	}
+}
+
+func TestSigningMethod(t *testing.T) {
+	token := oauth2.Token("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.EkN-DOsnsuRjRO6BxXemmJDm3HbxrbRzXglbN2S4sOkopdU4IsDxTI8jO19W_A4K8ZPJijNLis4EZsHeY559a4DFOd50_OqgHGuERTqYZyuhtF39yxJPAjUESwxk2J5k_4zM3O-vtd1Ghyo4IbqKKSy6J9mTniYJPenn5-HIirE")
+	j := oauth2.JWT{}
+	if _, err := j.ValidPrincipal(context.Background(), token, 0); err == nil {
+		t.Error("Error was expected while validating incorrectly signed token")
+	} else if err.Error() != "unexpected signing method: RS256" {
+		t.Errorf("Error wanted 'unexpected signing method', got %s", err.Error())
 	}
 }
