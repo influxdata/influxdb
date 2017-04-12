0ee672710c0f7ea030eb62fb80c8e0d644923c60
Jared Scheib <jared.scheib@gmail.com>
2017-04-12 12:23:01 -0700

    Add Notifications to Login page

--- ui/src/auth/Login.js
+++ ui/src/auth/Login.js
@@ -2,26 +2,31 @@
 import React, {PropTypes} from 'react'
 import {connect} from 'react-redux'
 
-const Login = ({auth}) => {
+import Notifications from 'shared/components/Notifications'
+
+const Login = ({auth, location}) => {
   if (auth.isAuthLoading) {
     return <div className="page-spinner"></div>
   }
 
   return (
-    <div className="auth-page">
-      <div className="auth-box">
-        <div className="auth-logo"></div>
-        <h1 className="auth-text-logo">Chronograf</h1>
-        <p><strong>{VERSION}</strong> / Time-Series Data Visualization</p>
-        {auth.links.map(({name, login, label}) => (
-          <a key={name} className="btn btn-primary" href={login}>
-            <span className={`icon ${name}`}></span>
-            Login with {label}
-          </a>
-        ))}
+    <div>
+      <Notifications location={location} />
+      <div className="auth-page">
+        <div className="auth-box">
+          <div className="auth-logo"></div>
+          <h1 className="auth-text-logo">Chronograf</h1>
+          <p><strong>{VERSION}</strong> / Time-Series Data Visualization</p>
+          {auth.links.map(({name, login, label}) => (
+            <a key={name} className="btn btn-primary" href={login}>
+              <span className={`icon ${name}`}></span>
+              Login with {label}
+            </a>
+          ))}
+        </div>
+        <p className="auth-credits">Made by <span className="icon cubo-uniform"></span>InfluxData</p>
+        <div className="auth-image"></div>
       </div>
-      <p className="auth-credits">Made by <span className="icon cubo-uniform"></span>InfluxData</p>
-      <div className="auth-image"></div>
     </div>
   )
 }
@@ -30,6 +35,7 @@ const {
   array,
   bool,
   shape,
+  string,
 } = PropTypes
 
 Login.propTypes = {
@@ -37,6 +43,9 @@ Login.propTypes = {
     me: shape(),
     links: array,
     isLoading: bool,
+  }),
+  location: shape({
+    pathname: string,
   }),
 }
 
