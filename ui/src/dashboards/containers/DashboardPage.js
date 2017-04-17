72486c4e3c1542542558e9104d3c97bcbbf1f113
Jared Scheib <jared.scheib@gmail.com>
2017-04-17 11:33:03 -0700

    Remove isRequired from editQueryCell propType in DashboardPage

--- ui/src/dashboards/containers/DashboardPage.js
+++ ui/src/dashboards/containers/DashboardPage.js
@@ -44,7 +44,7 @@ const DashboardPage = React.createClass({
       addDashboardCellAsync: func.isRequired,
       editDashboardCell: func.isRequired,
       renameDashboardCell: func.isRequired,
-      editQueryStatus: func.isRequired,
+      editQueryStatus: func,
     }).isRequired,
     dashboards: arrayOf(shape({
       id: number.isRequired,
