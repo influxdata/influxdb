56c2e6b543e51ec44a5bcb133ef9511c50d0f4ff
Jared Scheib <jared.scheib@gmail.com>
2017-04-17 10:59:02 -0700

    Refactor error middleware to suppress redux actions after auth expires and always show correct logout reason error notification

--- ui/src/shared/middleware/errors.js
+++ ui/src/shared/middleware/errors.js
@@ -1,23 +1,22 @@
-// import {replace} from 'react-router-redux'
 import {authExpired} from 'shared/actions/auth'
 import {publishNotification as notify} from 'shared/actions/notifications'
 
 import {HTTP_FORBIDDEN} from 'shared/constants'
 
+const actionsAllowedDuringBlackout = ['@@', 'AUTH_', 'ME_', 'NOTIFICATION_', 'ERROR_']
 const notificationsBlackoutDuration = 5000
 let allowNotifications = true // eslint-disable-line
 
 const errorsMiddleware = store => next => action => {
-  if (action.type === 'ERROR_THROWN') {
-    const {error, error: {status, auth}, altText} = action
+  const {auth: {me}} = store.getState()
 
-    console.error(error)
+  if (action.type === 'ERROR_THROWN') {
+    const {error: {status, auth}, altText} = action
 
     if (status === HTTP_FORBIDDEN) {
-      const {auth: {me}} = store.getState()
       const wasSessionTimeout = me !== null
 
-      next(authExpired(auth))
+      store.dispatch(authExpired(auth))
 
       if (wasSessionTimeout) {
         store.dispatch(notify('error', 'Session timed out. Please login again.'))
@@ -36,6 +35,14 @@ const errorsMiddleware = store => next => action => {
     }
   }
 
+  // if auth has expired, do not execute any further actions or redux state
+  // changes in order to prevent changing notification that indiciates why
+  // logout occurred and any attempts to change redux state by actions that may
+  // have triggered AJAX requests prior to auth expiration and whose response
+  // returns after logout
+  if (me === null && !(actionsAllowedDuringBlackout.some((allowedAction) => action.type.includes(allowedAction)))) {
+    return
+  }
   next(action)
 }
 
