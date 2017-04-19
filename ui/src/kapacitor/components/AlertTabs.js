3c04ac71bbbfb3ab6d570ec8e664ebb3f3e2213a
Jade McGough <jade@influxdb.com>
2017-04-18 23:11:19 -0700

    update kapacitor config page to new redesign

--- ui/src/kapacitor/components/AlertTabs.js
+++ ui/src/kapacitor/components/AlertTabs.js
@@ -1,60 +1,58 @@
-import React, {PropTypes} from 'react'
+import React, {Component, PropTypes} from 'react'
 import _ from 'lodash'
+
+import {Tab, Tabs, TabPanel, TabPanels, TabList} from 'shared/components/Tabs'
 import {getKapacitorConfig, updateKapacitorConfigSection, testAlertOutput} from 'shared/apis'
-import AlertaConfig from './AlertaConfig'
-import HipChatConfig from './HipChatConfig'
-import OpsGenieConfig from './OpsGenieConfig'
-import PagerDutyConfig from './PagerDutyConfig'
-import SensuConfig from './SensuConfig'
-import SlackConfig from './SlackConfig'
-import SMTPConfig from './SMTPConfig'
-import TalkConfig from './TalkConfig'
-import TelegramConfig from './TelegramConfig'
-import VictorOpsConfig from './VictorOpsConfig'
 
-const AlertTabs = React.createClass({
-  propTypes: {
-    source: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }).isRequired,
-    kapacitor: PropTypes.shape({
-      url: PropTypes.string.isRequired,
-      links: PropTypes.shape({
-        proxy: PropTypes.string.isRequired,
-      }).isRequired,
-    }),
-    addFlashMessage: PropTypes.func.isRequired,
-  },
+import {
+  AlertaConfig,
+  HipChatConfig,
+  OpsGenieConfig,
+  PagerDutyConfig,
+  SensuConfig,
+  SlackConfig,
+  SMTPConfig,
+  TalkConfig,
+  TelegramConfig,
+  VictorOpsConfig,
+} from './config'
 
-  getInitialState() {
-    return {
+class AlertTabs extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
       selectedEndpoint: 'smtp',
       configSections: null,
     }
-  },
+    this.refreshKapacitorConfig = ::this.refreshKapacitorConfig
+    this.getSection = ::this.getSection
+    this.handleSaveConfig = ::this.handleSaveConfig
+    this.handleTest = ::this.handleTest
+    this.sanitizeProperties = ::this.sanitizeProperties
+  }
 
   componentDidMount() {
     this.refreshKapacitorConfig(this.props.kapacitor)
-  },
+  }
 
   componentWillReceiveProps(nextProps) {
     if (this.props.kapacitor.url !== nextProps.kapacitor.url) {
       this.refreshKapacitorConfig(nextProps.kapacitor)
     }
-  },
+  }
 
   refreshKapacitorConfig(kapacitor) {
     getKapacitorConfig(kapacitor).then(({data: {sections}}) => {
       this.setState({configSections: sections})
     }).catch(() => {
       this.setState({configSections: null})
-      this.props.addFlashMessage({type: 'error', text: `There was an error getting the Kapacitor config`})
+      this.props.addFlashMessage({type: 'error', text: 'There was an error getting the Kapacitor config'})
     })
-  },
+  }
 
   getSection(sections, section) {
     return _.get(sections, [section, 'elements', '0'], null)
-  },
+  }
 
   handleSaveConfig(section, properties) {
     if (section !== '') {
@@ -63,25 +61,19 @@ const AlertTabs = React.createClass({
         this.refreshKapacitorConfig(this.props.kapacitor)
         this.props.addFlashMessage({type: 'success', text: `Alert for ${section} successfully saved`})
       }).catch(() => {
-        this.props.addFlashMessage({type: 'error', text: `There was an error saving the kapacitor config`})
+        this.props.addFlashMessage({type: 'error', text: 'There was an error saving the kapacitor config'})
       })
     }
-  },
-
-  changeSelectedEndpoint(e) {
-    this.setState({
-      selectedEndpoint: e.target.value,
-    })
-  },
+  }
 
   handleTest(section, properties) {
     const propsToSend = this.sanitizeProperties(section, properties)
     testAlertOutput(this.props.kapacitor, section, propsToSend).then(() => {
       this.props.addFlashMessage({type: 'success', text: 'Slack test message sent'})
     }).catch(() => {
-      this.props.addFlashMessage({type: 'error', text: `There was an error testing the slack alert`})
+      this.props.addFlashMessage({type: 'error', text: 'There was an error testing the slack alert'})
     })
-  },
+  }
 
   sanitizeProperties(section, properties) {
     const cleanProps = Object.assign({}, properties, {enabled: true})
@@ -94,13 +86,61 @@ const AlertTabs = React.createClass({
       })
     }
     return cleanProps
-  },
+  }
 
   render() {
-    const {configSections, selectedEndpoint} = this.state
-    if (!configSections) { // could use this state to conditionally render spinner or error message
+    const {configSections} = this.state
+
+    if (!configSections) {
       return null
     }
+
+    const test = (properties) => {
+      this.handleTest('slack', properties)
+    }
+
+    const tabs = [
+      {
+        type: 'Alerta',
+        component: (<AlertaConfig onSave={(p) => this.handleSaveConfig('alerta', p)} config={this.getSection(configSections, 'alerta')} />),
+      },
+      {
+        type: 'SMTP',
+        component: (<SMTPConfig onSave={(p) => this.handleSaveConfig('smtp', p)} config={this.getSection(configSections, 'smtp')} />),
+      },
+      {
+        type: 'Slack',
+        component: (<SlackConfig onSave={(p) => this.handleSaveConfig('slack', p)} onTest={test} config={this.getSection(configSections, 'slack')} />),
+      },
+      {
+        type: 'VictorOps',
+        component: (<VictorOpsConfig onSave={(p) => this.handleSaveConfig('victorops', p)} config={this.getSection(configSections, 'victorops')} />),
+      },
+      {
+        type: 'Telegram',
+        component: (<TelegramConfig onSave={(p) => this.handleSaveConfig('telegram', p)} config={this.getSection(configSections, 'telegram')} />),
+      },
+      {
+        type: 'OpsGenie',
+        component: (<OpsGenieConfig onSave={(p) => this.handleSaveConfig('opsgenie', p)} config={this.getSection(configSections, 'opsgenie')} />),
+      },
+      {
+        type: 'PagerDuty',
+        component: (<PagerDutyConfig onSave={(p) => this.handleSaveConfig('pagerduty', p)} config={this.getSection(configSections, 'pagerduty')} />),
+      },
+      {
+        type: 'HipChat',
+        component: (<HipChatConfig onSave={(p) => this.handleSaveConfig('hipchat', p)} config={this.getSection(configSections, 'hipchat')} />),
+      },
+      {
+        type: 'Sensu',
+        component: (<SensuConfig onSave={(p) => this.handleSaveConfig('sensu', p)} config={this.getSection(configSections, 'sensu')} />),
+      },
+      {
+        type: 'Talk',
+        component: (<TalkConfig onSave={(p) => this.handleSaveConfig('talk', p)} config={this.getSection(configSections, 'talk')} />),
+      },
+    ]
 
     return (
       <div className="panel panel-minimal">
@@ -108,78 +148,41 @@ const AlertTabs = React.createClass({
           <h2 className="panel-title">Configure Alert Endpoints</h2>
         </div>
         <div className="panel-body">
-          <div className="row">
-            <div className="form-group col-xs-12 col-sm-8 col-sm-offset-2">
-              <label htmlFor="alert-endpoint" className="sr-only">Alert Enpoint</label>
-              <select value={this.state.selectedEndpoint} className="form-control" id="source" onChange={this.changeSelectedEndpoint}>
-                <option value="alerta">Alerta</option>
-                <option value="hipchat">HipChat</option>
-                <option value="opsgenie">OpsGenie</option>
-                <option value="pagerduty">PagerDuty</option>
-                <option value="sensu">Sensu</option>
-                <option value="slack">Slack</option>
-                <option value="smtp">SMTP</option>
-                <option value="talk">Talk</option>
-                <option value="telegram">Telegram</option>
-                <option value="victorops">VictorOps</option>
-              </select>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-xs-12 col-sm-8 col-sm-offset-2">
-              <hr/>
-            </div>
-            <div className="col-xs-12 col-sm-8 col-sm-offset-2">
-              {this.renderAlertConfig(selectedEndpoint)}
-            </div>
-          </div>
+          <Tabs className="row">
+            <TabList customClass="col-md-2 admin-tabs">
+              {
+                tabs.map((t, i) => (<Tab key={tabs[i].type}>{tabs[i].type}</Tab>))
+              }
+            </TabList>
+            <TabPanels customClass="col-md-10">
+              {
+                tabs.map((t, i) => (<TabPanel key={tabs[i].type}>{t.component}</TabPanel>))
+              }
+            </TabPanels>
+          </Tabs>
         </div>
       </div>
     )
-  },
+  }
+}
 
-  renderAlertConfig(endpoint) {
-    const {configSections} = this.state
-    const save = (properties) => {
-      this.handleSaveConfig(endpoint, properties)
-    }
-    const test = (properties) => {
-      this.handleTest(endpoint, properties)
-    }
+const {
+  func,
+  shape,
+  string,
+} = PropTypes
 
-    switch (endpoint) {
-      case 'alerta': {
-        return <AlertaConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'smtp': {
-        return <SMTPConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'slack': {
-        return <SlackConfig onSave={save} onTest={test} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'victorops': {
-        return <VictorOpsConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'telegram': {
-        return <TelegramConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'opsgenie': {
-        return <OpsGenieConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'pagerduty': {
-        return <PagerDutyConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'hipchat': {
-        return <HipChatConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'sensu': {
-        return <SensuConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-      case 'talk': {
-        return <TalkConfig onSave={save} config={this.getSection(configSections, endpoint)} />
-      }
-    }
-  },
-})
+AlertTabs.propTypes = {
+  source: shape({
+    id: string.isRequired,
+  }).isRequired,
+  kapacitor: shape({
+    url: string.isRequired,
+    links: shape({
+      proxy: string.isRequired,
+    }).isRequired,
+  }),
+  addFlashMessage: func.isRequired,
+}
 
 export default AlertTabs
---
+++ ui/src/kapacitor/components/config/index.js
@@ -0,0 +1,23 @@
+import AlertaConfig from './AlertaConfig'
+import HipChatConfig from './HipChatConfig'
+import OpsGenieConfig from './OpsGenieConfig'
+import PagerDutyConfig from './PagerDutyConfig'
+import SensuConfig from './SensuConfig'
+import SlackConfig from './SlackConfig'
+import SMTPConfig from './SMTPConfig'
+import TalkConfig from './TalkConfig'
+import TelegramConfig from './TelegramConfig'
+import VictorOpsConfig from './VictorOpsConfig'
+
+export {
+  AlertaConfig,
+  HipChatConfig,
+  OpsGenieConfig,
+  PagerDutyConfig,
+  SensuConfig,
+  SlackConfig,
+  SMTPConfig,
+  TalkConfig,
+  TelegramConfig,
+  VictorOpsConfig,
+}
