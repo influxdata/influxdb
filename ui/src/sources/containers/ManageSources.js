103ddce41e2adc06a63b44c757b21a759363c676
Alex P <thealexpaxton@gmail.com>
2017-04-27 14:33:40 -0700

    Use specific class instead of relying on H1 being inside page-header

--- ui/src/sources/containers/ManageSources.js
+++ ui/src/sources/containers/ManageSources.js
@@ -49,7 +49,7 @@ class ManageSources extends Component {
         <div className="page-header">
           <div className="page-header__container">
             <div className="page-header__left">
-              <h1>Configuration</h1>
+              <h1 className="page-header__title">Configuration</h1>
             </div>
           </div>
         </div>
