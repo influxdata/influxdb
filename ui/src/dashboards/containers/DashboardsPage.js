8304c178e1871c21186a44dc08b152e36e500af7
Alex P <thealexpaxton@gmail.com>
2017-05-08 15:54:58 -0700

    Clean up layout styles
    Moving misc styles to unsorted, removing dupes

--- ui/src/dashboards/containers/DashboardsPage.js
+++ ui/src/dashboards/containers/DashboardsPage.js
@@ -63,7 +63,7 @@ const DashboardsPage = React.createClass({
         <div className="page-header">
           <div className="page-header__container">
             <div className="page-header__left">
-              <h1>
+              <h1 className="page-header__title">
                 Dashboards
               </h1>
             </div>
