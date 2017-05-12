f21b196e2d94c397dd12092001e389af7b5a5053
Jade McGough <jade@influxdb.com>
2017-05-12 16:52:48 -0700

    add some stuff to modal

--- ui/src/data_explorer/components/WriteDataForm.js
+++ ui/src/data_explorer/components/WriteDataForm.js
@@ -1,8 +1,28 @@
 import React, {PropTypes} from 'react'
+import classnames from 'classnames'
 
 const WriteDataForm = ({onClose}) => (
-  <div>
-    <a onClick={onClose}>Click Me</a>
+  <div className="template-variable-manager">
+    <div className="template-variable-manager--header">
+      <div className="page-header__left">
+        <h1 className="page-header__title">Write Data To</h1>
+      </div>
+      <div className="page-header__right">
+        <button className="btn btn-primary btn-sm" type="button">
+          Add Variable
+        </button>
+        <button
+          className={classnames('btn btn-success btn-sm', {
+            disabled: !true,
+          })}
+          type="button"
+        >
+          Save Changes
+        </button>
+        <span className="page-header__dismiss" onClick={onClose} />
+      </div>
+    </div>
+    <div className="template-variable-manager--body" />
   </div>
 )
 
