dd3c18ed503975045046f5aa82bba4dea91fe1e5
Jade McGough <jade@influxdb.com>
2017-05-15 17:49:15 -0700

    WIP

--- ui/src/data_explorer/apis/index.js
+++ ui/src/data_explorer/apis/index.js
@@ -1,1 +1,9 @@
-export const writePoints = source => {}
+import AJAX from 'src/utils/ajax'
+
+export const writePoints = (source, db) => {
+  return AJAX({
+    url: source.links.write,
+    method: 'POST',
+    data: {},
+  })
+}
