7f56dd9f536842d445fe80f5dddf337d63d92d89
Jade McGough <jade@influxdb.com>
2017-05-17 00:52:05 -0700

    write POST must include db in the path

--- ui/src/data_explorer/apis/index.js
+++ ui/src/data_explorer/apis/index.js
@@ -2,7 +2,7 @@ import AJAX from 'src/utils/ajax'
 
 export const writePoints = (source, db) => {
   return AJAX({
-    url: source.links.write,
+    url: `${source.links.write$}?db=${db}`,
     method: 'POST',
     data: {},
   })
