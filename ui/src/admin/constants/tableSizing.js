d4bc2939c913e8cbf2059a46662f9ae18902ec81
Alex P <thealexpaxton@gmail.com>
2017-05-24 14:38:43 -0700

    Use constants to control admin table sizing
    Putting it all in one place

---
+++ ui/src/admin/constants/tableSizing.js
@@ -0,0 +1,21 @@
+export const USERS_TABLE = {
+  colUsername: 240,
+  colPassword: 186,
+  colRoles: 190,
+  colPermissions: 190,
+  colDelete: 70,
+}
+export const ROLES_TABLE = {
+  colName: 280,
+  colUsers: 200,
+  colPermissions: 200,
+  colDelete: 70,
+}
+export const QUERIES_TABLE = {
+  colDatabase: 160,
+  colRunning: 80,
+  colKillQuery: 70,
+}
+export const ADMIN_TABLE = {
+  colDelete: 70,
+}
