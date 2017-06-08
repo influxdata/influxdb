061933613787aa0babd561a744f804abc8265964
Jared Scheib <jared.scheib@gmail.com>
2017-06-08 00:03:54 -0500

    Use LayoutRenderer instead of building GridLayout in StatusPage
    Add isNotGraph and real influxQL query to fixtures
    Add autoRefresh, timeRange, and templates to StatusPage

--- ui/src/status/containers/StatusPage.js
+++ ui/src/status/containers/StatusPage.js
@@ -1,52 +1,79 @@
 import React, {Component, PropTypes} from 'react'
 import {connect} from 'react-redux'
-import ReactGridLayout, {WidthProvider} from 'react-grid-layout'
 
 import SourceIndicator from 'shared/components/SourceIndicator'
 import FancyScrollbar from 'shared/components/FancyScrollbar'
-import RefreshingGraph from 'shared/components/RefreshingGraph'
-import NameableGraph from 'shared/components/NameableGraph'
+import LayoutRenderer from 'shared/components/LayoutRenderer'
 
-const GridLayout = WidthProvider(ReactGridLayout)
-
-const fixtureGraphCells = [
+const fixtureStatusPageCells = [
   {
-    name: 'Alerts',
-    type: 'bar',
+    i: 'c-bar-graphs-fly',
+    isNonGraph: false,
     x: 0,
     y: 0,
     w: 12,
     h: 4,
-    i: 'bar',
+    name: 'Alerts – Aspiring Bar Graph',
+    queries: [
+      {
+        query:
+          'SELECT "usage_user" FROM "telegraf"."autogen"."cpu" WHERE time > :dashboardTime:',
+        label: 'cpu.usage_user',
+        queryConfig: {
+          database: 'telegraf',
+          measurement: 'cpu',
+          retentionPolicy: 'autogen',
+          fields: [
+            {
+              field: 'usage_user',
+              funcs: [],
+            },
+          ],
+          tags: {},
+          groupBy: {
+            time: '',
+            tags: [],
+          },
+          areTagsAccepted: false,
+          rawText: null,
+          range: null,
+        },
+      },
+    ],
+    type: 'line',
+    links: {
+      self: '/chronograf/v1/status/23/cells/c-bar-graphs-fly',
+    },
   },
-]
-const fixtureNonGraphCells = [
   {
+    i: 'recent-alerts',
+    isNonGraph: true,
     name: 'Recent Alerts',
     type: 'alerts',
     x: 0,
     y: 5,
     w: 4,
     h: 5,
-    i: 'recent-alerts',
   },
   {
+    i: 'news-feed',
+    isNonGraph: true,
     name: 'News Feed',
     type: 'news',
     x: 4,
     y: 5,
     w: 4,
     h: 5,
-    i: 'news-feed',
   },
   {
+    i: 'getting-started',
+    isNonGraph: true,
     name: 'Getting Started',
     type: 'guide',
     x: 8,
     y: 5,
     w: 4,
     h: 5,
-    i: 'getting-started',
   },
 ]
 
@@ -55,94 +82,27 @@ class StatusPage extends Component {
     super(props)
 
     this.state = {
-      graphCells: fixtureGraphCells,
-      nonGraphCells: fixtureNonGraphCells,
+      cells: fixtureStatusPageCells,
     }
-
-    this.generateLayoutCells = ::this.generateLayoutCells
-    this.triggerWindowResize = ::this.triggerWindowResize
-  }
-
-  generateNonGraphCell(cell) {
-    switch (cell.type) {
-      case 'alerts': {
-        return (
-          <div className="graph-empty">
-            <p>Coming soon: Alerts list</p>
-          </div>
-        )
-      }
-      case 'news': {
-        return (
-          <div className="graph-empty">
-            <p>Coming soon: Newsfeed</p>
-          </div>
-        )
-      }
-      case 'guide': {
-        return (
-          <div className="graph-empty">
-            <p>Coming soon: Markdown-based guide</p>
-          </div>
-        )
-      }
-    }
-    return (
-      <div className="graph-empty">
-        <p>No Results</p>
-      </div>
-    )
-  }
-
-  generateLayoutCells(graphCells, nonGraphCells) {
-    return [
-      ...graphCells.map(cell => {
-        return (
-          <div key={cell.i}>
-            <NameableGraph
-              cell={{
-                name: cell.name,
-                x: cell.x,
-                y: cell.y,
-              }}
-              shouldNotBeEditable={true}
-            >
-              <RefreshingGraph />
-            </NameableGraph>
-          </div>
-        )
-      }),
-      ...nonGraphCells.map(cell => {
-        return (
-          <div key={cell.i}>
-            <NameableGraph
-              cell={{
-                name: cell.name,
-                x: cell.x,
-                y: cell.y,
-              }}
-              shouldNotBeEditable={true}
-            >
-              {this.generateNonGraphCell(cell)}
-            </NameableGraph>
-          </div>
-        )
-      }),
-    ]
-  }
-
-  triggerWindowResize() {
-    // Hack to get dygraphs to fit properly during and after resize (dispatchEvent is a global method on window).
-    const evt = document.createEvent('CustomEvent') // MUST be 'CustomEvent'
-    evt.initCustomEvent('resize', false, false, null)
-    dispatchEvent(evt)
   }
 
   render() {
-    const {source} = this.props
-    const {graphCells, nonGraphCells} = this.state
+    const {source, autoRefresh, timeRange} = this.props
+    const {cells} = this.state
 
-    const layoutMargin = 4
+    const dashboardTime = {
+      id: 'dashtime',
+      tempVar: ':dashboardTime:',
+      type: 'constant',
+      values: [
+        {
+          value: timeRange.lower,
+          type: 'constant',
+          selected: true,
+        },
+      ],
+    }
+    const templates = [dashboardTime]
 
     return (
       <div className="page">
@@ -160,23 +120,16 @@ class StatusPage extends Component {
         </div>
         <FancyScrollbar className={'page-contents'}>
           <div className="dashboard container-fluid full-width">
-            {graphCells.length && nonGraphCells.length
-              ? <GridLayout
-                  layout={[...graphCells, ...nonGraphCells]}
-                  cols={12}
-                  rowHeight={83.5}
-                  margin={[layoutMargin, layoutMargin]}
-                  containerPadding={[0, 0]}
-                  useCSSTransforms={false}
-                  onResize={this.triggerWindowResize}
-                  onLayoutChange={this.handleLayoutChange}
-                  draggableHandle={'.dash-graph--name'}
-                  isDraggable={false}
-                  isResizable={false}
-                >
-                  {this.generateLayoutCells(graphCells, nonGraphCells)}
-                </GridLayout>
-              : <span>Loading status...</span>}
+            {cells.length
+              ? <LayoutRenderer
+                  autoRefresh={autoRefresh}
+                  timeRange={timeRange}
+                  cells={cells}
+                  templates={templates}
+                  source={source.links.proxy}
+                  shouldNotBeEditable={true}
+                />
+              : <span>Loading Status Page...</span>}
           </div>
         </FancyScrollbar>
       </div>
@@ -184,12 +137,21 @@ class StatusPage extends Component {
   }
 }
 
-const {shape, string} = PropTypes
+const {number, shape, string} = PropTypes
 
 StatusPage.propTypes = {
   source: shape({
     name: string.isRequired,
   }).isRequired,
+  autoRefresh: number.isRequired,
+  timeRange: shape({
+    lower: string.isRequired,
+  }).isRequired,
 }
 
-export default connect(null)(StatusPage)
+const mapStateToProps = ({status: {autoRefresh, timeRange}}) => ({
+  autoRefresh,
+  timeRange,
+})
+
+export default connect(mapStateToProps, null)(StatusPage)
