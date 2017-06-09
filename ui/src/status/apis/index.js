35fa6da2923128d818d56ccf74050f2617d72fbb
Jared Scheib <jared.scheib@gmail.com>
2017-06-09 01:03:46 -0500

    Comment out linter errors temporarily

--- ui/src/status/apis/index.js
+++ ui/src/status/apis/index.js
@@ -1,8 +1,9 @@
-import {get} from 'utils/ajax'
+// import {get} from 'utils/ajax'
 
 import {fixtureJSONFeed} from 'src/status/fixtures'
 // TODO: remove async/await & object return, uncomment get(url) when proxy route implemented
-export const fetchJSONFeed = async url => {
+// export const fetchJSONFeed = async url => {
+export const fetchJSONFeed = async () => {
   return await {data: fixtureJSONFeed}
   // get(url)
 }
