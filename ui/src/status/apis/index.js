e8bebebe19e64d664069f23e534b173ca50e4d2f
Jared Scheib <jared.scheib@gmail.com>
2017-06-08 23:27:31 -0500

    Return fixture data until json feed proxy implemented

--- ui/src/status/apis/index.js
+++ ui/src/status/apis/index.js
@@ -1,3 +1,8 @@
 import {get} from 'utils/ajax'
 
-export const getJSONFeed = url => get(url)
+import {fixtureJSONFeed} from 'src/status/fixtures'
+// TODO: remove async/await & object return, uncomment get(url) when proxy route implemented
+export const getJSONFeed = async url => {
+  return await {data: fixtureJSONFeed}
+  // get(url)
+}
