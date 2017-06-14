3a37bfc40976afee8b42f6ac51dd39ed4e877a09
Jared Scheib <jared.scheib@gmail.com>
2017-06-13 17:57:25 -0700

    Retrieve live JSON feed; delete fixture data

--- ui/src/status/apis/index.js
+++ ui/src/status/apis/index.js
@@ -1,9 +1,10 @@
-// import {get} from 'utils/ajax'
+import AJAX from 'utils/ajax'
 
-import {fixtureJSONFeed} from 'src/status/fixtures'
-// TODO: remove async/await & object return, uncomment get(url) when proxy route implemented
-// export const fetchJSONFeed = async url => {
-export const fetchJSONFeed = async () => {
-  return await {data: fixtureJSONFeed}
-  // get(url)
-}
+export const fetchJSONFeed = url =>
+  AJAX({
+    method: 'GET',
+    url,
+    // For explanation of why this header makes this work:
+    // https://stackoverflow.com/questions/22968406/how-to-skip-the-options-preflight-request-in-angularjs
+    headers: {'Content-Type': 'text/plain'},
+  })
