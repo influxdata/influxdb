20fe6b358a82025af407f41f4c95f325b3ff8ce8
Andrew Watkins <watts@influxdb.com>
2017-06-21 10:06:07 -0700

    WIP style file input

--- ui/src/data_explorer/components/WriteDataBody.js
+++ ui/src/data_explorer/components/WriteDataBody.js
@@ -7,39 +7,56 @@ const WriteDataBody = ({
   handleSubmit,
   inputContent,
   isManual,
-}) => (
-  <div className="write-data-form--body">
-    {isManual
-      ? <textarea
-          className="form-control write-data-form--input"
-          autoComplete="off"
-          spellCheck="false"
-          placeholder="<measurement>,<tag_key>=<tag_value> <field_key>=<field_value>"
-          onKeyUp={handleKeyUp}
-          onChange={handleEdit}
-          autoFocus={true}
-        />
-      : <input type="file" onChange={handleFile} />}
-    <div className="write-data-form--footer">
-      <span className="write-data-form--helper">
-        Need help writing InfluxDB Line Protocol? -&nbsp;
-        <a
-          href="https://docs.influxdata.com/influxdb/latest/write_protocols/line_protocol_tutorial/"
-          target="_blank"
+}) => {
+  let inputRef
+
+  return (
+    <div className="write-data-form--body">
+      {isManual
+        ? <textarea
+            className="form-control write-data-form--input"
+            autoComplete="off"
+            spellCheck="false"
+            placeholder="<measurement>,<tag_key>=<tag_value> <field_key>=<field_value>"
+            onKeyUp={handleKeyUp}
+            onChange={handleEdit}
+            autoFocus={true}
+          />
+        : <div className="write-data-form--file">
+            <input
+              type="file"
+              onChange={handleFile}
+              className="write-data-form--upload"
+              ref={r => inputRef = r}
+            />
+            <button
+              className="btn btn-sm btn-primary"
+              onClick={() => inputRef.click()}
+            >
+              Upload File
+            </button>
+          </div>}
+      <div className="write-data-form--footer">
+        <span className="write-data-form--helper">
+          Need help writing InfluxDB Line Protocol? -&nbsp;
+          <a
+            href="https://docs.influxdata.com/influxdb/latest/write_protocols/line_protocol_tutorial/"
+            target="_blank"
+          >
+            See Documentation
+          </a>
+        </span>
+        <button
+          className="btn btn-sm btn-primary write-data-form--submit"
+          onClick={handleSubmit}
+          disabled={!inputContent}
         >
-          See Documentation
-        </a>
-      </span>
-      <button
-        className="btn btn-sm btn-primary write-data-form--submit"
-        onClick={handleSubmit}
-        disabled={!inputContent}
-      >
-        Write
-      </button>
+          Write
+        </button>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 const {func, string, bool} = PropTypes
 
