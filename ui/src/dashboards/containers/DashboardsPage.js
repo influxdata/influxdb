bf0dbcf5c3203c1e2d71b8b8eb5bca8771f31025
Alex P <thealexpaxton@gmail.com>
2017-06-22 14:24:45 -0700

    Make dashboards page contents into its own stateless component

--- ui/src/dashboards/containers/DashboardsPage.js
+++ ui/src/dashboards/containers/DashboardsPage.js
@@ -4,8 +4,7 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
 import DashboardsHeader from 'src/dashboards/components/DashboardsHeader'
-import DashboardsTable from 'src/dashboards/components/DashboardsTable'
-import FancyScrollbar from 'shared/components/FancyScrollbar'
+import DashboardsContents from 'src/dashboards/components/DashboardsPageContents'
 
 import {createDashboard} from 'src/dashboards/apis'
 import {getDashboardsAsync, deleteDashboardAsync} from 'src/dashboards/actions'
@@ -50,45 +49,16 @@ const DashboardsPage = React.createClass({
   render() {
     const {dashboards} = this.props
     const dashboardLink = `/sources/${this.props.source.id}`
-    let tableHeader
-    if (dashboards === null) {
-      tableHeader = 'Loading Dashboards...'
-    } else if (dashboards.length === 1) {
-      tableHeader = '1 Dashboard'
-    } else {
-      tableHeader = `${dashboards.length} Dashboards`
-    }
 
     return (
       <div className="page">
         <DashboardsHeader sourceName={this.props.source.name} />
-        <FancyScrollbar className="page-contents">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="panel panel-minimal">
-                  <div className="panel-heading u-flex u-ai-center u-jc-space-between">
-                    <h2 className="panel-title">{tableHeader}</h2>
-                    <button
-                      className="btn btn-sm btn-primary"
-                      onClick={this.handleCreateDashbord}
-                    >
-                      Create Dashboard
-                    </button>
-                  </div>
-                  <div className="panel-body">
-                    <DashboardsTable
-                      dashboards={dashboards}
-                      onDeleteDashboard={this.handleDeleteDashboard}
-                      onCreateDashboard={this.handleCreateDashbord}
-                      dashboardLink={dashboardLink}
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </FancyScrollbar>
+        <DashboardsContents
+          dashboardLink={dashboardLink}
+          dashboards={dashboards}
+          onDeleteDashboard={this.handleDeleteDashboard}
+          onCreateDashboard={this.handleCreateDashbord}
+        />
       </div>
     )
   },
