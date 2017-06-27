32ecf1b79605ba4db0c83d02ffec0730dbda7600
Andrew Watkins <watts@influxdb.com>
2017-06-27 15:12:16 -0700

    Add ability to sort numerically and alphabetically

--- ui/src/shared/components/DygraphLegend.js
+++ ui/src/shared/components/DygraphLegend.js
@@ -1,18 +1,24 @@
 import React, {PropTypes} from 'react'
 
-const DygraphLegend = ({series}) => (
+const DygraphLegend = ({series, onSort}) => (
   <div style={{userSelect: 'text'}} className="container--dygraph-legend">
     <div className="dygraph-legend--header">
       <input className="form-control input-xs" type="text" />
-      <button className="btn btn-primary btn-xs">
+      <button
+        className="btn btn-primary btn-xs"
+        onClick={() => onSort('alphabetic')}
+      >
         A-Z
       </button>
-      <button className="btn btn-primary btn-xs">
+      <button
+        className="btn btn-primary btn-xs"
+        onClick={() => onSort('numeric')}
+      >
         0-9
       </button>
     </div>
     <div className="dygraph-legend--contents">
-      {series.map(({label, color, yHTML}) => {
+      {series.map(({label, color, yHTML, isHighlighted}) => {
         return (
           <span key={label + color}>
             <b>
@@ -27,7 +33,7 @@ const DygraphLegend = ({series}) => (
   </div>
 )
 
-const {arrayOf, bool, number, shape, string} = PropTypes
+const {arrayOf, bool, func, number, shape, string} = PropTypes
 
 DygraphLegend.propTypes = {
   x: number,
@@ -43,6 +49,7 @@ DygraphLegend.propTypes = {
     })
   ),
   dygraph: shape(),
+  onSort: func.isRequired,
 }
 
 export default DygraphLegend
