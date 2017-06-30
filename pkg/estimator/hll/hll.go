4cb01c17689ef8727b0cdb21bca073758ef3a7fe
Seif Lotfy <seif.lotfy@gmail.com>
2017-06-30 07:47:16 -0700

    change beta constants for the hll cardinality bias estimator

--- pkg/estimator/hll/hll.go
+++ pkg/estimator/hll/hll.go
@@ -33,14 +33,14 @@ const DefaultPrecision = 16
 
 func beta(ez float64) float64 {
 	zl := math.Log(ez + 1)
-	return -0.370393911*ez +
-		0.070471823*zl +
-		0.17393686*math.Pow(zl, 2) +
-		0.16339839*math.Pow(zl, 3) +
-		-0.09237745*math.Pow(zl, 4) +
-		0.03738027*math.Pow(zl, 5) +
-		-0.005384159*math.Pow(zl, 6) +
-		0.00042419*math.Pow(zl, 7)
+	return -0.37331876643753059*ez +
+		-1.41704077448122989*zl +
+		0.40729184796612533*math.Pow(zl, 2) +
+		1.56152033906584164*math.Pow(zl, 3) +
+		-0.99242233534286128*math.Pow(zl, 4) +
+		0.26064681399483092*math.Pow(zl, 5) +
+		-0.03053811369682807*math.Pow(zl, 6) +
+		0.00155770210179105*math.Pow(zl, 7)
 }
 
 // Plus implements the Hyperloglog++ algorithm, described in the following
