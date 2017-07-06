0ba94497e03dca3562cdd093628b4ed1e4dc4ea7
Hunter Trujillo <cryptoquick@gmail.com>
2017-07-06 14:10:26 -0600

    Ranger improvements.

--- ui/src/dashboards/components/Ranger.js
+++ ui/src/dashboards/components/Ranger.js
@@ -2,17 +2,19 @@ import React, {PropTypes} from 'react'
 
 const Ranger = ({onSetRange, yRange}) =>
   <div className="yRanger">
-    <form onSubmit={onSetRange}>
-      <label htmlFor="min">Min</label>
+    <form onSubmit={onSetRange} autoComplete="off">
+      <label htmlFor="min">Minimum</label>
       <input
+        className="form-control input-sm"
         type="text"
         name="min"
         id="min"
         value={yRange.min}
         onChange={onSetRange}
       />
-      <label htmlFor="max">Max</label>
+      <label htmlFor="max">Maximum</label>
       <input
+        className="form-control input-sm"
         type="text"
         name="max"
         id="max"
