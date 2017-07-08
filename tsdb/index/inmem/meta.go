6a488dfac12b24a91c74ec1fed99078b8fa4a256
Jason Wilder <mail@jasonwilder.com>
2017-07-07 20:46:00 -0600

    Fix duplicate points returned after delete
    The sortedSeriesIds slice was not getting reset to 0 which caused
    the same series ids to exist in the slice more than once.  Since
    the size of the slice never matched the size of the seriesID map,
    it kept appendending to the slice and sorting it which cause multiple
    cursor to get created for the same series.
    
    Fixes #8531

--- tsdb/index/inmem/meta.go
+++ tsdb/index/inmem/meta.go
@@ -131,9 +131,11 @@ func (m *Measurement) SeriesIDs() SeriesIDs {
 		return s
 	}
 
+	m.sortedSeriesIDs = m.sortedSeriesIDs[:0]
 	if cap(m.sortedSeriesIDs) < len(m.seriesByID) {
 		m.sortedSeriesIDs = make(SeriesIDs, 0, len(m.seriesByID))
 	}
+
 	for k := range m.seriesByID {
 		m.sortedSeriesIDs = append(m.sortedSeriesIDs, k)
 	}
@@ -210,7 +212,7 @@ func (m *Measurement) AddSeries(s *Series) bool {
 
 	m.seriesByID[s.ID] = s
 
-	if len(m.sortedSeriesIDs) == 0 || s.ID > m.sortedSeriesIDs[len(m.sortedSeriesIDs)-1] {
+	if len(m.seriesByID) == 1 || (len(m.sortedSeriesIDs) == len(m.seriesByID)-1 && s.ID > m.sortedSeriesIDs[len(m.sortedSeriesIDs)-1]) {
 		m.sortedSeriesIDs = append(m.sortedSeriesIDs, s.ID)
 	}
 
