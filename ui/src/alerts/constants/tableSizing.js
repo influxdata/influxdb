b6fa438fef5eff18fe096ac85ae9900a5ac9ff97
Alex P <thealexpaxton@gmail.com>
2017-07-07 17:20:33 -0700

    Make NAME and HOST columns less likely to wrap
    - LEVEL, TIME and and VALUE have fixed pixels widths as the maximum
    content size is predictable

--- ui/src/alerts/constants/tableSizing.js
+++ ui/src/alerts/constants/tableSizing.js
@@ -1,7 +1,7 @@
 export const ALERTS_TABLE = {
-  colName: '15%',
-  colLevel: '10%',
-  colTime: '25%',
-  colHost: '25%',
-  colValue: '25%',
+  colName: 'calc(45% - 216px)',
+  colLevel: '82px',
+  colTime: '190px',
+  colHost: 'calc(55% - 216px)',
+  colValue: '160px',
 }
