95b1fedf23a5408437bfd355da708230a19a3f2f
Alex P <thealexpaxton@gmail.com>
2017-07-10 12:22:40 -0700

    Prioritize alert name over host

--- ui/src/alerts/constants/tableSizing.js
+++ ui/src/alerts/constants/tableSizing.js
@@ -1,7 +1,7 @@
 export const ALERTS_TABLE = {
-  colName: 'calc(45% - 216px)',
+  colName: 'calc(55% - 216px)',
   colLevel: '82px',
   colTime: '190px',
-  colHost: 'calc(55% - 216px)',
+  colHost: 'calc(45% - 216px)',
   colValue: '160px',
 }
