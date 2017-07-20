8c3a9db9192477d7da847db89e636916a56f1b75
Jared Scheib <jared.scheib@gmail.com>
2017-07-20 16:11:17 -0700

    Make CodeData a presentational component

--- ui/src/kapacitor/components/CodeData.js
+++ ui/src/kapacitor/components/CodeData.js
@@ -1,25 +1,13 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 
-// needs to be React Component for click handler to work
-class CodeData extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    const {onClickTemplate, template} = this.props
-
-    return (
-      <code
-        className="rule-builder--message-template"
-        data-tip={template.text}
-        onClick={onClickTemplate}
-      >
-        {template.label}
-      </code>
-    )
-  }
-}
+const CodeData = ({onClickTemplate, template}) =>
+  <code
+    className="rule-builder--message-template"
+    data-tip={template.text}
+    onClick={onClickTemplate}
+  >
+    {template.label}
+  </code>
 
 const {func, shape, string} = PropTypes
 
