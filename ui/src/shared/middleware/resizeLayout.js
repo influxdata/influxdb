badc0a73749060194d5e5664bdc53b495931b615
Hunter Trujillo <cryptoquick@gmail.com>
2017-07-27 17:43:45 -0600

    Remember, kids... Always leave a note.

--- ui/src/shared/middleware/resizeLayout.js
+++ ui/src/shared/middleware/resizeLayout.js
@@ -7,6 +7,7 @@ export default function resizeLayout() {
       action.type === 'ENABLE_PRESENTATION_MODE' ||
       action.type === 'DISABLE_PRESENTATION_MODE'
     ) {
+      // Uses longer event object creation method due to IE compatibility.
       const evt = document.createEvent('HTMLEvents')
       evt.initEvent('resize', false, true)
       window.dispatchEvent(evt)
