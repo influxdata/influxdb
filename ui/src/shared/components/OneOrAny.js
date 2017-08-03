81a85b13ba97f6719a650d1e907ca842a82439e3
Alex Paxton <thealexpaxton@gmail.com>
2017-08-03 16:13:37 -0700

    Create OneOrAny component
    Signed-off-by: Jared Scheib <jared.scheib@gmail.com>

---
+++ ui/src/shared/components/OneOrAny.js
@@ -0,0 +1,85 @@
+import React, {Component, PropTypes} from 'react'
+import classnames from 'classnames'
+
+class OneOrAny extends Component {
+  constructor(props) {
+    super(props)
+
+    const {leftValue, rightValue} = props
+
+    this.state = {
+      leftValue,
+      rightValue,
+    }
+
+    this.handleChangeRightValue = ::this.handleChangeRightValue
+    this.handleToggleLeftValue = ::this.handleToggleLeftValue
+    this.handleSetValues = ::this.handleSetValues
+  }
+
+  handleChangeRightValue(newValue) {
+    this.setState({rightValue: newValue}, this.handleSetValues)
+  }
+
+  handleToggleLeftValue() {
+    const {leftValue} = this.state
+
+    this.setState({leftValue: !leftValue}, this.handleSetValues)
+  }
+
+  handleSetValues() {
+    const {onSetValues} = this.props
+    const {leftValue, rightValue} = this.state
+
+    onSetValues({leftValue, rightValue})
+  }
+
+  render() {
+    const {leftLabel, rightLabel} = this.props
+    const {leftValue: useLeftValue, rightValue} = this.state
+
+    return (
+      <div
+        className={classnames('one-or-any', {'use-right-value': !useLeftValue})}
+      >
+        <div className="one-or-any--auto">
+          {leftLabel}
+        </div>
+        <div
+          className="one-or-any--groove"
+          onClick={this.handleToggleLeftValue}
+        >
+          <div className="one-or-any--knob" />
+        </div>
+        <input
+          className="form-control input-sm"
+          type="number"
+          name="rightValue"
+          id="rightValue"
+          value={rightValue === null ? '' : rightValue}
+          onChange={e => this.handleChangeRightValue(e.target.value)}
+          placeholder={rightLabel}
+          disabled={useLeftValue}
+        />
+      </div>
+    )
+  }
+}
+
+OneOrAny.defaultProps = {
+  leftLabel: 'auto',
+  leftValue: true,
+  rightLabel: 'Custom Value',
+  rightValue: null,
+}
+const {bool, func, number, string} = PropTypes
+
+OneOrAny.propTypes = {
+  leftLabel: string,
+  leftValue: bool,
+  rightLabel: string,
+  rightValue: number,
+  onSetValues: func.isRequired,
+}
+
+export default OneOrAny
