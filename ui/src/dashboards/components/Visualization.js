538dda35410c4c0d3fd294f718a8eaf180a01506
Andrew Watkins <watts@influxdb.com>
2017-08-09 16:48:50 -0700

    Move logic from render and remove VisView

--- ui/src/dashboards/components/Visualization.js
+++ ui/src/dashboards/components/Visualization.js
@@ -1,35 +1,29 @@
 import React, {PropTypes} from 'react'
+import RefreshingGraph from 'shared/components/RefreshingGraph'
 import buildInfluxQLQuery from 'utils/influxql'
-import VisHeader from 'src/data_explorer/components/VisHeader'
-import VisView from 'src/data_explorer/components/VisView'
-import {GRAPH} from 'shared/constants'
 import _ from 'lodash'
 
-const {arrayOf, bool, func, number, shape, string} = PropTypes
+const {arrayOf, func, number, shape, string} = PropTypes
 
-const Visualization = React.createClass({
+const DashVisualization = React.createClass({
   propTypes: {
     cellName: string,
     cellType: string,
     autoRefresh: number.isRequired,
     templates: arrayOf(shape()),
-    isInDataExplorer: bool,
     timeRange: shape({
       upper: string,
       lower: string,
     }).isRequired,
     queryConfigs: arrayOf(shape({})).isRequired,
-    activeQueryIndex: number,
     height: string,
     heightPixels: number,
     editQueryStatus: func.isRequired,
-    views: arrayOf(string).isRequired,
     axes: shape({
       y: shape({
         bounds: arrayOf(string),
       }),
     }),
-    resizerBottomHeight: number,
   },
 
   contextTypes: {
@@ -50,22 +44,41 @@ const Visualization = React.createClass({
   render() {
     const {
       axes,
-      views,
       height,
       cellType,
       cellName,
-      timeRange,
       templates,
       autoRefresh,
       heightPixels,
-      queryConfigs,
       editQueryStatus,
-      activeQueryIndex,
-      isInDataExplorer,
-      resizerBottomHeight,
     } = this.props
 
+    return (
+      <div className="graph" style={{height}}>
+        <div className="graph-heading">
+          <div className="graph-title">
+            {cellName}
+          </div>
+        </div>
+        <div className="graph-container">
+          <RefreshingGraph
+            axes={axes}
+            type={cellType}
+            queries={::this.buildQueries()}
+            templates={templates}
+            cellHeight={heightPixels}
+            autoRefresh={autoRefresh}
+            editQueryStatus={editQueryStatus}
+          />
+        </div>
+      </div>
+    )
+  },
+
+  buildQueries() {
     const {source: {links: {proxy}}} = this.context
+    const {queryConfigs, timeRange} = this.props
+
     const statements = queryConfigs.map(query => {
       const text =
         query.rawText || buildInfluxQLQuery(query.range || timeRange, query)
@@ -76,31 +89,7 @@ const Visualization = React.createClass({
       return {host: [proxy], text: s.text, id: s.id, queryConfig: s.queryConfig}
     })
 
-    return (
-      <div className="graph" style={{height}}>
-        <VisHeader
-          views={views}
-          view={GRAPH}
-          onToggleView={this.handleToggleView}
-          name={cellName}
-        />
-        <div className="graph-container">
-          <VisView
-            view={GRAPH}
-            axes={axes}
-            queries={queries}
-            templates={templates}
-            cellType={cellType}
-            autoRefresh={autoRefresh}
-            heightPixels={heightPixels}
-            editQueryStatus={editQueryStatus}
-            activeQueryIndex={activeQueryIndex}
-            isInDataExplorer={isInDataExplorer}
-            resizerBottomHeight={resizerBottomHeight}
-          />
-        </div>
-      </div>
-    )
+    return queries
   },
 
   getQueryText(queryConfigs, index) {
@@ -109,4 +98,4 @@ const Visualization = React.createClass({
   },
 })
 
-export default Visualization
+export default DashVisualization
