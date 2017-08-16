9803a0d8baa0c588493c31754f5cc6dad3280051
Andrew Watkins <watts@influxdb.com>
2017-08-16 11:33:08 -0700

    Convert RuleMessageText to SFC

--- ui/src/kapacitor/components/RuleMessageText.js
+++ ui/src/kapacitor/components/RuleMessageText.js
@@ -1,29 +1,13 @@
-import React, {Component, PropTypes} from 'react'
+import React, {PropTypes} from 'react'
 
-class RuleMessageText extends Component {
-  constructor(props) {
-    super(props)
-  }
-
-  handleChange = e => {
-    const {rule, updateMessage} = this.props
-    updateMessage(rule, e.targe.value)
-  }
-
-  render() {
-    const {rule} = this.props
-
-    return (
-      <textarea
-        className="form-control form-malachite monotype rule-builder--message"
-        onChange={this.handleChange}
-        placeholder="Example: {{ .ID }} is {{ .Level }} value: {{ index .Fields &quot;value&quot; }}"
-        value={rule.message}
-        spellCheck={false}
-      />
-    )
-  }
-}
+const RuleMessageText = ({rule, updateMessage}) =>
+  <textarea
+    className="form-control form-malachite monotype rule-builder--message"
+    onChange={updateMessage}
+    placeholder="Example: {{ .ID }} is {{ .Level }} value: {{ index .Fields &quot;value&quot; }}"
+    value={rule.message}
+    spellCheck={false}
+  />
 
 const {func, shape} = PropTypes
 
