a04bc139f18494fbff7b85bebf37f1403521774a
Alex P <thealexpaxton@gmail.com>
2017-08-17 20:35:24 -0700

    Refactor data section to use fat arrow pattern

--- ui/src/kapacitor/components/DataSection.js
+++ ui/src/kapacitor/components/DataSection.js
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 import buildInfluxQLQuery from 'utils/influxql'
 import classnames from 'classnames'
 
@@ -9,83 +9,56 @@ import FillQuery from 'shared/components/FillQuery'
 
 import {defaultEveryFrequency} from 'src/kapacitor/constants'
 
-export const DataSection = React.createClass({
-  propTypes: {
-    source: PropTypes.shape({
-      links: PropTypes.shape({
-        kapacitors: PropTypes.string.isRequired,
-      }).isRequired,
-    }),
-    query: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }).isRequired,
-    addFlashMessage: PropTypes.func,
-    actions: PropTypes.shape({
-      chooseNamespace: PropTypes.func.isRequired,
-      chooseMeasurement: PropTypes.func.isRequired,
-      applyFuncsToField: PropTypes.func.isRequired,
-      chooseTag: PropTypes.func.isRequired,
-      groupByTag: PropTypes.func.isRequired,
-      toggleField: PropTypes.func.isRequired,
-      groupByTime: PropTypes.func.isRequired,
-      toggleTagAcceptance: PropTypes.func.isRequired,
-    }).isRequired,
-    onAddEvery: PropTypes.func.isRequired,
-    onRemoveEvery: PropTypes.func.isRequired,
-    timeRange: PropTypes.shape({}).isRequired,
-    isKapacitorRule: PropTypes.bool,
-  },
+class DataSection extends Component {
+  constructor(props) {
+    super(props)
+  }
 
-  childContextTypes: {
-    source: PropTypes.shape({
-      links: PropTypes.shape({
-        proxy: PropTypes.string.isRequired,
-        self: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  },
-
-  getChildContext() {
+  getChildContext = () => {
     return {source: this.props.source}
-  },
+  }
 
-  handleChooseNamespace(namespace) {
+  handleChooseNamespace = namespace => {
     this.props.actions.chooseNamespace(this.props.query.id, namespace)
-  },
+  }
 
-  handleChooseMeasurement(measurement) {
+  handleChooseMeasurement = measurement => {
     this.props.actions.chooseMeasurement(this.props.query.id, measurement)
-  },
+  }
 
-  handleToggleField(field) {
+  handleToggleField = field => {
     this.props.actions.toggleField(this.props.query.id, field)
     // Every is only added when a function has been added to a field.
     // Here, the field is selected without a function.
     this.props.onRemoveEvery()
     // Because there are no functions there is no group by time.
     this.props.actions.groupByTime(this.props.query.id, null)
-  },
+  }
 
-  handleGroupByTime(time) {
+  handleFillQuery = (val, isNum) => {
+    console.log(val, isNum)
+  }
+
+  handleGroupByTime = time => {
     this.props.actions.groupByTime(this.props.query.id, time)
-  },
+  }
 
-  handleApplyFuncsToField(fieldFunc) {
+  handleApplyFuncsToField = fieldFunc => {
     this.props.actions.applyFuncsToField(this.props.query.id, fieldFunc)
     this.props.onAddEvery(defaultEveryFrequency)
-  },
+  }
 
-  handleChooseTag(tag) {
+  handleChooseTag = tag => {
     this.props.actions.chooseTag(this.props.query.id, tag)
-  },
+  }
 
-  handleToggleTagAcceptance() {
+  handleToggleTagAcceptance = () => {
     this.props.actions.toggleTagAcceptance(this.props.query.id)
-  },
+  }
 
-  handleGroupByTag(tagKey) {
+  handleGroupByTag = tagKey => {
     this.props.actions.groupByTag(this.props.query.id, tagKey)
-  },
+  }
 
   render() {
     const {query, timeRange: {lower}} = this.props
@@ -96,10 +69,8 @@ export const DataSection = React.createClass({
         <h3 className="rule-section--heading">
           Select a Time Series
           <FillQuery
-            onSelection={e => {
-              console.log(e)
-            }}
-            theme="green"
+            onSelection={this.handleFillQuery}
+            theme="GREEN"
             size="sm"
           />
         </h3>
@@ -117,9 +88,9 @@ export const DataSection = React.createClass({
         </div>
       </div>
     )
-  },
+  }
 
-  renderQueryBuilder() {
+  renderQueryBuilder = () => {
     const {query, isKapacitorRule} = this.props
 
     return (
@@ -144,7 +115,44 @@ export const DataSection = React.createClass({
         />
       </div>
     )
-  },
-})
+  }
+}
+
+const {bool, func, shape, string} = PropTypes
+
+DataSection.propTypes = {
+  source: shape({
+    links: shape({
+      kapacitors: string.isRequired,
+    }).isRequired,
+  }),
+  query: shape({
+    id: string.isRequired,
+  }).isRequired,
+  addFlashMessage: func,
+  actions: shape({
+    chooseNamespace: func.isRequired,
+    chooseMeasurement: func.isRequired,
+    applyFuncsToField: func.isRequired,
+    chooseTag: func.isRequired,
+    groupByTag: func.isRequired,
+    toggleField: func.isRequired,
+    groupByTime: func.isRequired,
+    toggleTagAcceptance: func.isRequired,
+  }).isRequired,
+  onAddEvery: func.isRequired,
+  onRemoveEvery: func.isRequired,
+  timeRange: shape({}).isRequired,
+  isKapacitorRule: bool,
+}
+
+DataSection.childContextTypes = {
+  source: PropTypes.shape({
+    links: PropTypes.shape({
+      proxy: PropTypes.string.isRequired,
+      self: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+}
 
 export default DataSection
