noorall/tsdb

inputs are closed if Merge returns error and use <type>FinalizerIterator
* <type>FinalizerIterator sets a runtime finalizer and calls Close
  when garbage collected. This will ensure any associated cursors
  are closed and the associated TSM files released
* `query.Iterators#Merge` call could return an error and the inputs
  would not be closed, causing a cursor leak

engine/tsm1/engine.go
@@ -1563,7 +1563,7 @@ func (e *Engine) CreateIterator(measurement string, opt query.IteratorOptions) (
 				if err != nil {
 					return nil, err
 				}
-				return query.Iterators(itrs).Merge(opt)
+				return newMergeGuardIterator(itrs, opt)
 			}
 		}
 
@@ -1573,14 +1573,14 @@ func (e *Engine) CreateIterator(measurement string, opt query.IteratorOptions) (
 		} else if len(inputs) == 0 {
 			return nil, nil
 		}
-		return query.Iterators(inputs).Merge(opt)
+		return newMergeGuardIterator(inputs, opt)
 	}
 
 	itrs, err := e.createVarRefIterator(measurement, opt)
 	if err != nil {
 		return nil, err
 	}
-	return query.Iterators(itrs).Merge(opt)
+	return newMergeGuardIterator(itrs, opt)
 }
 
 func (e *Engine) createCallIterator(measurement string, call *influxql.Call, opt query.IteratorOptions) ([]query.Iterator, error) {
@@ -1634,6 +1634,7 @@ func (e *Engine) createCallIterator(measurement string, call *influxql.Call, opt
 
 				itr, err := query.NewCallIterator(input, opt)
 				if err != nil {
+					query.Iterators(inputs).Close()
 					return err
 				}
 				inputs[i] = itr

engine/tsm1/iterator.go
@@ -148,3 +148,39 @@ func (c cursorsAt) close() {
 		cur.close()
 	}
 }
+
+// newMergeGuardIterator creates a new Merge iterator from the inputs. If Merge returns an error,
+// the inputs will be closed
+func newMergeGuardIterator(inputs []query.Iterator, opt query.IteratorOptions) (query.Iterator, error) {
+	itr, err := query.Iterators(inputs).Merge(opt)
+	if err != nil {
+		query.Iterators(inputs).Close()
+		return nil, err
+	}
+	return newFinalizerIterator(itr), nil
+}
+
+// newFinalizerIterator creates a new iterator that installs a runtime finalizer
+// to ensure close is eventually called if the iterator is garbage collected.
+// This additional guard attempts to protect against clients of CreateIterator not
+// correctly closing them and leaking cursors.
+func newFinalizerIterator(itr query.Iterator) query.Iterator {
+	if itr == nil {
+		return nil
+	}
+
+	switch inner := itr.(type) {
+	case query.FloatIterator:
+		return newFloatFinalizerIterator(inner)
+	case query.IntegerIterator:
+		return newIntegerFinalizerIterator(inner)
+	case query.UnsignedIterator:
+		return newUnsignedFinalizerIterator(inner)
+	case query.StringIterator:
+		return newStringFinalizerIterator(inner)
+	case query.BooleanIterator:
+		return newBooleanFinalizerIterator(inner)
+	default:
+		panic(fmt.Sprintf("unsupported finalizer iterator type: %T", itr))
+	}
+}

influxdata/influxdb

(cherry picked from commit 823f903)

tsdb/engine/tsm1/engine.go
@@ -1560,7 +1560,7 @@ func (e *Engine) CreateIterator(measurement string, opt influxql.IteratorOptions
 				if err != nil {
 					return nil, err
 				}
-				return influxql.Iterators(itrs).Merge(opt)
+				return newMergeGuardIterator(itrs, opt)
 			}
 		}
 
@@ -1570,14 +1570,14 @@ func (e *Engine) CreateIterator(measurement string, opt influxql.IteratorOptions
 		} else if len(inputs) == 0 {
 			return nil, nil
 		}
-		return influxql.Iterators(inputs).Merge(opt)
+		return newMergeGuardIterator(inputs, opt)
 	}
 
 	itrs, err := e.createVarRefIterator(measurement, opt)
 	if err != nil {
 		return nil, err
 	}
-	return influxql.Iterators(itrs).Merge(opt)
+	return newMergeGuardIterator(itrs, opt)
 }
 
 func (e *Engine) createCallIterator(measurement string, call *influxql.Call, opt influxql.IteratorOptions) ([]influxql.Iterator, error) {
@@ -1631,6 +1631,7 @@ func (e *Engine) createCallIterator(measurement string, call *influxql.Call, opt
 
 				itr, err := influxql.NewCallIterator(input, opt)
 				if err != nil {
+					influxql.Iterators(inputs).Close()
 					return err
 				}
 				inputs[i] = itr

tsdb/engine/tsm1/iterator.go
@@ -54,3 +54,37 @@ func (c cursorsAt) close() {
 		cur.close()
 	}
 }
+
+// newMergeGuardIterator creates a new Merge iterator from the inputs. If Merge returns an error,
+// the inputs will be closed
+func newMergeGuardIterator(inputs []influxql.Iterator, opt influxql.IteratorOptions) (influxql.Iterator, error) {
+	itr, err := influxql.Iterators(inputs).Merge(opt)
+	if err != nil {
+		influxql.Iterators(inputs).Close()
+		return nil, err
+	}
+	return newFinalizerIterator(itr), nil
+}
+
+// newFinalizerIterator creates a new iterator that installs a runtime finalizer
+// to ensure close is eventually called if the iterator is garbage collected.
+// This additional guard attempts to protect against clients of CreateIterator not
+// correctly closing them and leaking cursors.
+func newFinalizerIterator(itr influxql.Iterator) influxql.Iterator {
+	if itr == nil {
+		return nil
+	}
+
+	switch inner := itr.(type) {
+	case influxql.FloatIterator:
+		return newFloatFinalizerIterator(inner)
+	case influxql.IntegerIterator:
+		return newIntegerFinalizerIterator(inner)
+	case influxql.StringIterator:
+		return newStringFinalizerIterator(inner)
+	case influxql.BooleanIterator:
+		return newBooleanFinalizerIterator(inner)
+	default:
+		panic(fmt.Sprintf("unsupported finalizer iterator type: %T", itr))
+	}
+}