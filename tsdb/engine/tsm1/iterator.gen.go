noorall/tsdb

log message when iterator closed by finalizer

engine/tsm1/iterator.gen.go
@@ -15,6 +15,7 @@ import (
 	"github.com/influxdata/influxdb/influxql"
 	"github.com/influxdata/influxdb/query"
 	"github.com/influxdata/influxdb/tsdb"
+	"github.com/uber-go/zap"
 )
 
 type cursor interface {
@@ -121,12 +122,19 @@ const statsBufferCopyIntervalN = 100
 
 type floatFinalizerIterator struct {
 	query.FloatIterator
+	logger zap.Logger
 }
 
-func newFloatFinalizerIterator(inner query.FloatIterator) *floatFinalizerIterator {
-	itr := &floatFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*floatFinalizerIterator).Close)
+func newFloatFinalizerIterator(inner query.FloatIterator, logger zap.Logger) *floatFinalizerIterator {
+	itr := &floatFinalizerIterator{FloatIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*floatFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *floatFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("FloatIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *floatFinalizerIterator) Close() error {
@@ -547,12 +555,19 @@ func (c *floatDescendingCursor) nextTSM() {
 
 type integerFinalizerIterator struct {
 	query.IntegerIterator
+	logger zap.Logger
 }
 
-func newIntegerFinalizerIterator(inner query.IntegerIterator) *integerFinalizerIterator {
-	itr := &integerFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*integerFinalizerIterator).Close)
+func newIntegerFinalizerIterator(inner query.IntegerIterator, logger zap.Logger) *integerFinalizerIterator {
+	itr := &integerFinalizerIterator{IntegerIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*integerFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *integerFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("IntegerIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *integerFinalizerIterator) Close() error {
@@ -973,12 +988,19 @@ func (c *integerDescendingCursor) nextTSM() {
 
 type unsignedFinalizerIterator struct {
 	query.UnsignedIterator
+	logger zap.Logger
 }
 
-func newUnsignedFinalizerIterator(inner query.UnsignedIterator) *unsignedFinalizerIterator {
-	itr := &unsignedFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*unsignedFinalizerIterator).Close)
+func newUnsignedFinalizerIterator(inner query.UnsignedIterator, logger zap.Logger) *unsignedFinalizerIterator {
+	itr := &unsignedFinalizerIterator{UnsignedIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*unsignedFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *unsignedFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("UnsignedIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *unsignedFinalizerIterator) Close() error {
@@ -1399,12 +1421,19 @@ func (c *unsignedDescendingCursor) nextTSM() {
 
 type stringFinalizerIterator struct {
 	query.StringIterator
+	logger zap.Logger
 }
 
-func newStringFinalizerIterator(inner query.StringIterator) *stringFinalizerIterator {
-	itr := &stringFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*stringFinalizerIterator).Close)
+func newStringFinalizerIterator(inner query.StringIterator, logger zap.Logger) *stringFinalizerIterator {
+	itr := &stringFinalizerIterator{StringIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*stringFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *stringFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("StringIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *stringFinalizerIterator) Close() error {
@@ -1825,12 +1854,19 @@ func (c *stringDescendingCursor) nextTSM() {
 
 type booleanFinalizerIterator struct {
 	query.BooleanIterator
+	logger zap.Logger
 }
 
-func newBooleanFinalizerIterator(inner query.BooleanIterator) *booleanFinalizerIterator {
-	itr := &booleanFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*booleanFinalizerIterator).Close)
+func newBooleanFinalizerIterator(inner query.BooleanIterator, logger zap.Logger) *booleanFinalizerIterator {
+	itr := &booleanFinalizerIterator{BooleanIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*booleanFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *booleanFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("BooleanIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *booleanFinalizerIterator) Close() error {

engine/tsm1/iterator.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/influxdata/influxdb/query"
 	"github.com/influxdata/influxdb/tsdb"
+	"github.com/uber-go/zap"
 )
 
 func newLimitIterator(input query.Iterator, opt query.IteratorOptions) query.Iterator {
@@ -152,35 +153,35 @@ func (c cursorsAt) close() {
 // newMergeFinalizerIterator creates a new Merge iterator from the inputs. If the call to Merge succeeds,
 // the resulting Iterator will be wrapped in a finalizer iterator.
 // If Merge returns an error, the inputs will be closed.
-func newMergeFinalizerIterator(inputs []query.Iterator, opt query.IteratorOptions) (query.Iterator, error) {
+func newMergeFinalizerIterator(inputs []query.Iterator, opt query.IteratorOptions, log zap.Logger) (query.Iterator, error) {
 	itr, err := query.Iterators(inputs).Merge(opt)
 	if err != nil {
 		query.Iterators(inputs).Close()
 		return nil, err
 	}
-	return newFinalizerIterator(itr), nil
+	return newFinalizerIterator(itr, log), nil
 }
 
 // newFinalizerIterator creates a new iterator that installs a runtime finalizer
 // to ensure close is eventually called if the iterator is garbage collected.
 // This additional guard attempts to protect against clients of CreateIterator not
 // correctly closing them and leaking cursors.
-func newFinalizerIterator(itr query.Iterator) query.Iterator {
+func newFinalizerIterator(itr query.Iterator, log zap.Logger) query.Iterator {
 	if itr == nil {
 		return nil
 	}
 
 	switch inner := itr.(type) {
 	case query.FloatIterator:
-		return newFloatFinalizerIterator(inner)
+		return newFloatFinalizerIterator(inner, log)
 	case query.IntegerIterator:
-		return newIntegerFinalizerIterator(inner)
+		return newIntegerFinalizerIterator(inner, log)
 	case query.UnsignedIterator:
-		return newUnsignedFinalizerIterator(inner)
+		return newUnsignedFinalizerIterator(inner, log)
 	case query.StringIterator:
-		return newStringFinalizerIterator(inner)
+		return newStringFinalizerIterator(inner, log)
 	case query.BooleanIterator:
-		return newBooleanFinalizerIterator(inner)
+		return newBooleanFinalizerIterator(inner, log)
 	default:
 		panic(fmt.Sprintf("unsupported finalizer iterator type: %T", itr))
 	}

influxdata/influxdb

(cherry picked from commit d189621)

tsdb/engine/tsm1/iterator.gen.go
@@ -14,6 +14,7 @@ import (
 
 	"github.com/influxdata/influxdb/influxql"
 	"github.com/influxdata/influxdb/tsdb"
+	"github.com/uber-go/zap"
 )
 
 type cursor interface {
@@ -118,12 +119,19 @@ const statsBufferCopyIntervalN = 100
 
 type floatFinalizerIterator struct {
 	influxql.FloatIterator
+	logger zap.Logger
 }
 
-func newFloatFinalizerIterator(inner influxql.FloatIterator) *floatFinalizerIterator {
-	itr := &floatFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*floatFinalizerIterator).Close)
+func newFloatFinalizerIterator(inner influxql.FloatIterator, logger zap.Logger) *floatFinalizerIterator {
+	itr := &floatFinalizerIterator{FloatIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*floatFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *floatFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("FloatIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *floatFinalizerIterator) Close() error {
@@ -570,12 +578,19 @@ func (c *floatNilLiteralCursor) nextAt(seek int64) interface{}  { return (*float
 
 type integerFinalizerIterator struct {
 	influxql.IntegerIterator
+	logger zap.Logger
 }
 
-func newIntegerFinalizerIterator(inner influxql.IntegerIterator) *integerFinalizerIterator {
-	itr := &integerFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*integerFinalizerIterator).Close)
+func newIntegerFinalizerIterator(inner influxql.IntegerIterator, logger zap.Logger) *integerFinalizerIterator {
+	itr := &integerFinalizerIterator{IntegerIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*integerFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *integerFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("IntegerIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *integerFinalizerIterator) Close() error {
@@ -1022,12 +1037,19 @@ func (c *integerNilLiteralCursor) nextAt(seek int64) interface{}  { return (*int
 
 type stringFinalizerIterator struct {
 	influxql.StringIterator
+	logger zap.Logger
 }
 
-func newStringFinalizerIterator(inner influxql.StringIterator) *stringFinalizerIterator {
-	itr := &stringFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*stringFinalizerIterator).Close)
+func newStringFinalizerIterator(inner influxql.StringIterator, logger zap.Logger) *stringFinalizerIterator {
+	itr := &stringFinalizerIterator{StringIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*stringFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *stringFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("StringIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *stringFinalizerIterator) Close() error {
@@ -1474,12 +1496,19 @@ func (c *stringNilLiteralCursor) nextAt(seek int64) interface{}  { return (*stri
 
 type booleanFinalizerIterator struct {
 	influxql.BooleanIterator
+	logger zap.Logger
 }
 
-func newBooleanFinalizerIterator(inner influxql.BooleanIterator) *booleanFinalizerIterator {
-	itr := &booleanFinalizerIterator{inner}
-	runtime.SetFinalizer(itr, (*booleanFinalizerIterator).Close)
+func newBooleanFinalizerIterator(inner influxql.BooleanIterator, logger zap.Logger) *booleanFinalizerIterator {
+	itr := &booleanFinalizerIterator{BooleanIterator: inner, logger: logger}
+	runtime.SetFinalizer(itr, (*booleanFinalizerIterator).closeGC)
 	return itr
+}
+
+func (itr *booleanFinalizerIterator) closeGC() {
+	runtime.SetFinalizer(itr, nil)
+	itr.logger.Error("BooleanIterator finalized by GC")
+	itr.Close()
 }
 
 func (itr *booleanFinalizerIterator) Close() error {

tsdb/engine/tsm1/iterator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/influxdata/influxdb/influxql"
+	"github.com/uber-go/zap"
 )
 
 func newLimitIterator(input influxql.Iterator, opt influxql.IteratorOptions) influxql.Iterator {
@@ -58,33 +59,33 @@ func (c cursorsAt) close() {
 // newMergeFinalizerIterator creates a new Merge iterator from the inputs. If the call to Merge succeeds,
 // the resulting Iterator will be wrapped in a finalizer iterator.
 // If Merge returns an error, the inputs will be closed.
-func newMergeFinalizerIterator(inputs []influxql.Iterator, opt influxql.IteratorOptions) (influxql.Iterator, error) {
+func newMergeFinalizerIterator(inputs []influxql.Iterator, opt influxql.IteratorOptions, log zap.Logger) (influxql.Iterator, error) {
 	itr, err := influxql.Iterators(inputs).Merge(opt)
 	if err != nil {
 		influxql.Iterators(inputs).Close()
 		return nil, err
 	}
-	return newFinalizerIterator(itr), nil
+	return newFinalizerIterator(itr, log), nil
 }
 
 // newFinalizerIterator creates a new iterator that installs a runtime finalizer
 // to ensure close is eventually called if the iterator is garbage collected.
 // This additional guard attempts to protect against clients of CreateIterator not
 // correctly closing them and leaking cursors.
-func newFinalizerIterator(itr influxql.Iterator) influxql.Iterator {
+func newFinalizerIterator(itr influxql.Iterator, log zap.Logger) influxql.Iterator {
 	if itr == nil {
 		return nil
 	}
 
 	switch inner := itr.(type) {
 	case influxql.FloatIterator:
-		return newFloatFinalizerIterator(inner)
+		return newFloatFinalizerIterator(inner, log)
 	case influxql.IntegerIterator:
-		return newIntegerFinalizerIterator(inner)
+		return newIntegerFinalizerIterator(inner, log)
 	case influxql.StringIterator:
-		return newStringFinalizerIterator(inner)
+		return newStringFinalizerIterator(inner, log)
 	case influxql.BooleanIterator:
-		return newBooleanFinalizerIterator(inner)
+		return newBooleanFinalizerIterator(inner, log)
 	default:
 		panic(fmt.Sprintf("unsupported finalizer iterator type: %T", itr))
 	}