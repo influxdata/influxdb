4b3dd53010a4f13647bc1b7af977081d2b439a24
Jared Scheib <jared.scheib@gmail.com>
2017-08-24 15:10:48 -0700

    Remove parens from queryFill options

--- ui/src/shared/constants/queryFillOptions.js
+++ ui/src/shared/constants/queryFillOptions.js
@@ -1,7 +1,7 @@
-export const LINEAR = '(linear)'
-export const NONE = '(none)'
-export const NULL = '(null)'
-export const NUMBER = '(number)'
-export const PREVIOUS = '(previous)'
+export const LINEAR = 'linear'
+export const NONE = 'none'
+export const NULL = 'null'
+export const NUMBER = 'number'
+export const PREVIOUS = 'previous'
 
 export const QUERY_FILL_OPTIONS = [NULL, PREVIOUS, NUMBER, NONE, LINEAR]
