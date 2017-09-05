d79bf2976f6bb8f3012b4d6547508a0e903591fa
Andrew Watkins <watts@influxdb.com>
2017-09-05 15:01:13 -0700

    Convert to modern syntax

--- ui/src/kapacitor/components/DataSection.js
+++ ui/src/kapacitor/components/DataSection.js
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 import buildInfluxQLQuery from 'utils/influxql'
 import classnames from 'classnames'
 
@@ -8,83 +8,52 @@ import FieldList from 'src/shared/components/FieldList'
 
 import {defaultEveryFrequency} from 'src/kapacitor/constants'
 
-export const DataSection = React.createClass({
-  propTypes: {
-    source: PropTypes.shape({
-      links: PropTypes.shape({
-        kapacitors: PropTypes.string.isRequired,
-      }).isRequired,
-    }),
-    query: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    }).isRequired,
-    addFlashMessage: PropTypes.func,
-    actions: PropTypes.shape({
-      chooseNamespace: PropTypes.func.isRequired,
-      chooseMeasurement: PropTypes.func.isRequired,
-      applyFuncsToField: PropTypes.func.isRequired,
-      chooseTag: PropTypes.func.isRequired,
-      groupByTag: PropTypes.func.isRequired,
-      toggleField: PropTypes.func.isRequired,
-      groupByTime: PropTypes.func.isRequired,
-      toggleTagAcceptance: PropTypes.func.isRequired,
-    }).isRequired,
-    onAddEvery: PropTypes.func.isRequired,
-    onRemoveEvery: PropTypes.func.isRequired,
-    timeRange: PropTypes.shape({}).isRequired,
-    isKapacitorRule: PropTypes.bool,
-  },
-
-  childContextTypes: {
-    source: PropTypes.shape({
-      links: PropTypes.shape({
-        proxy: PropTypes.string.isRequired,
-        self: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  },
+class DataSection extends Component {
+  constructor(props) {
+    super(props)
+  }
 
   getChildContext() {
     return {source: this.props.source}
-  },
+  }
 
-  handleChooseNamespace(namespace) {
+  handleChooseNamespace = namespace => {
     this.props.actions.chooseNamespace(this.props.query.id, namespace)
-  },
+  }
 
-  handleChooseMeasurement(measurement) {
+  handleChooseMeasurement = measurement => {
     this.props.actions.chooseMeasurement(this.props.query.id, measurement)
-  },
+  }
 
-  handleToggleField(field) {
+  handleToggleField = field => {
     this.props.actions.toggleField(this.props.query.id, field)
     // Every is only added when a function has been added to a field.
     // Here, the field is selected without a function.
     this.props.onRemoveEvery()
     // Because there are no functions there is no group by time.
     this.props.actions.groupByTime(this.props.query.id, null)
-  },
+  }
 
-  handleGroupByTime(time) {
+  handleGroupByTime = time => {
     this.props.actions.groupByTime(this.props.query.id, time)
-  },
+  }
 
-  handleApplyFuncsToField(fieldFunc) {
+  handleApplyFuncsToField = fieldFunc => {
     this.props.actions.applyFuncsToField(this.props.query.id, fieldFunc)
     this.props.onAddEvery(defaultEveryFrequency)
-  },
+  }
 
-  handleChooseTag(tag) {
+  handleChooseTag = tag => {
     this.props.actions.chooseTag(this.props.query.id, tag)
-  },
+  }
 
-  handleToggleTagAcceptance() {
+  handleToggleTagAcceptance = () => {
     this.props.actions.toggleTagAcceptance(this.props.query.id)
-  },
+  }
 
-  handleGroupByTag(tagKey) {
+  handleGroupByTag = tagKey => {
     this.props.actions.groupByTag(this.props.query.id, tagKey)
-  },
+  }
 
   render() {
     const {query, timeRange: {lower}} = this.props
@@ -107,7 +76,7 @@ export const DataSection = React.createClass({
         </div>
       </div>
     )
-  },
+  }
 
   renderQueryBuilder() {
     const {query, isKapacitorRule} = this.props
@@ -134,7 +103,42 @@ export const DataSection = React.createClass({
         />
       </div>
     )
-  },
-})
+  }
+}
+
+DataSection.propTypes = {
+  source: PropTypes.shape({
+    links: PropTypes.shape({
+      kapacitors: PropTypes.string.isRequired,
+    }).isRequired,
+  }),
+  query: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+  addFlashMessage: PropTypes.func,
+  actions: PropTypes.shape({
+    chooseNamespace: PropTypes.func.isRequired,
+    chooseMeasurement: PropTypes.func.isRequired,
+    applyFuncsToField: PropTypes.func.isRequired,
+    chooseTag: PropTypes.func.isRequired,
+    groupByTag: PropTypes.func.isRequired,
+    toggleField: PropTypes.func.isRequired,
+    groupByTime: PropTypes.func.isRequired,
+    toggleTagAcceptance: PropTypes.func.isRequired,
+  }).isRequired,
+  onAddEvery: PropTypes.func.isRequired,
+  onRemoveEvery: PropTypes.func.isRequired,
+  timeRange: PropTypes.shape({}).isRequired,
+  isKapacitorRule: PropTypes.bool,
+}
+
+DataSection.childContextTypes = {
+  source: PropTypes.shape({
+    links: PropTypes.shape({
+      proxy: PropTypes.string.isRequired,
+      self: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+}
 
 export default DataSection
