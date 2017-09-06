708d96539e20ad7bdb580f9887950a844ec8317e
Hunter Trujillo <cryptoquick@gmail.com>
2017-09-06 11:56:04 -0600

    whoops! forgot my hat.

--- ui/src/shared/middleware/queryStringConfig.js
+++ ui/src/shared/middleware/queryStringConfig.js
@@ -1,12 +1,10 @@
 // Middleware generally used for actions needing parsed queryStrings
 import queryString from 'query-string'
-import uuid from 'node-uuid'
 
 import {enablePresentationMode} from 'src/shared/actions/app'
-import {editRawTextAsync} from 'src/data_explorer/actions/view'
 import {templateVariablesSelectedByName} from 'src/dashboards/actions'
 
-export const queryStringConfig = store => next => action => {
+export const queryStringConfig = () => next => action => {
   next(action)
   const qs = queryString.parse(window.location.search)
 
