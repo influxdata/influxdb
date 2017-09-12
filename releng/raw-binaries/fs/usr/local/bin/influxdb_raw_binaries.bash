e50df1ed6c23ea017ca71ff3c5c29e3d8303480a
Adam <adam@influxdb.com>
2017-09-12 16:27:15 -0400

    fixed arm arch building

--- releng/raw-binaries/fs/usr/local/bin/influxdb_raw_binaries.bash
+++ releng/raw-binaries/fs/usr/local/bin/influxdb_raw_binaries.bash
@@ -5,10 +5,36 @@ if [ -z "$GOOS" ] || [ -z "$GOARCH" ]; then
   exit 1
 fi
 
+
 # Extract tarball into GOPATH.
 tar xz -C "$GOPATH" -f /influxdb-src.tar.gz
 
 SHA=$(jq -r .sha < "$GOPATH/src/github.com/influxdata/influxdb/.metadata.json")
+
+
+SUFFIX=
+if [ "$CGO_ENABLED" == "0" ]; then
+  # Only add the static suffix to the filename when explicitly requested.
+  SUFFIX=_static
+fi
+
+TARBALL_NAME="influxdb_bin_${GOOS}_${GOARCH}${SUFFIX}-${SHA}.tar.gz"
+
+# note: according to https://github.com/golang/go/wiki/GoArm
+# we want to support armel using GOARM=5
+# and we want to support armhf using GOARM=6
+# no GOARM setting is necessary for arm64
+if [ $GOARCH == "armel"]; then
+  GOARCH=arm
+  GOARM=5
+fi
+
+if [ $GOARCH == "armhf" ]; then
+  GOARCH=arm
+  GOARM=6
+fi
+
+
 
 OUTDIR=$(mktemp -d)
 for cmd in \
@@ -21,13 +47,7 @@ for cmd in \
     go build -i -o "$OUTDIR/$(basename $cmd)" "github.com/influxdata/$cmd"
 done
 
-SUFFIX=
-if [ "$CGO_ENABLED" == "0" ]; then
-  # Only add the static suffix to the filename when explicitly requested.
-  SUFFIX=_static
-fi
 
-TARBALL_NAME="influxdb_bin_${GOOS}_${GOARCH}${SUFFIX}-${SHA}.tar.gz"
 (cd "$OUTDIR" && tar czf "/out/$TARBALL_NAME" ./*)
 (cd /out && md5sum "$TARBALL_NAME" > "$TARBALL_NAME.md5")
 (cd /out && sha256sum "$TARBALL_NAME" > "$TARBALL_NAME.sha256")
