bbe1572b263281509073631aa8abb6fc2f063e48
Ben Johnson <benbjohnson@yahoo.com>
2017-09-19 08:41:47 -0600

    intermediate

--- tsdb/index/tsi1/file_set.go
+++ tsdb/index/tsi1/file_set.go
@@ -167,21 +167,6 @@ func (fs *FileSet) LastContiguousIndexFilesByLevel(level int) []*IndexFile {
 	return a
 }
 
-/*
-// SeriesIDIterator returns an iterator over all series in the index.
-func (fs *FileSet) SeriesIDIterator() SeriesIDIterator {
-	a := make([]SeriesIDIterator, 0, len(fs.files))
-	for _, f := range fs.files {
-		itr := f.SeriesIDIterator()
-		if itr == nil {
-			continue
-		}
-		a = append(a, itr)
-	}
-	return FilterUndeletedSeriesIDIterator(MergeSeriesIDIterators(a...))
-}
-*/
-
 // Measurement returns a measurement by name.
 func (fs *FileSet) Measurement(name []byte) MeasurementElem {
 	for _, f := range fs.files {
