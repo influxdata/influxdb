834f9935c67ff5fe3d1a1f20205d3149632cc06c
Andrew Watkins <watts@influxdb.com>
2017-10-04 15:52:03 -0700

    Update copy

--- ui/src/shared/copy/cell.js
+++ ui/src/shared/copy/cell.js
@@ -4,7 +4,7 @@ const emptyFunny = [
   'Looks like you don’t have any queries. Be a lot cooler if you did.',
   'Create a query. Go on, I dare ya!',
   'Create a query. Have fun!',
-  '1) Create a query \n2) Profit',
+  '1) Create a query \n2) Call your mom',
 ]
 
 export const emptyGraphCopy = _.sample(emptyFunny)
