921bf66dc8257d95704e08f4290dc0549d076841
deniz kusefoglu <denizk@gmail.com>
2017-10-13 15:20:23 -0700

    add test to dashboardtoCSV

--- ui/spec/shared/parsing/resultsToCSVSpec.js
+++ ui/spec/shared/parsing/resultsToCSVSpec.js
@@ -1,4 +1,8 @@
-import {resultsToCSV, formatDate} from 'shared/parsing/resultsToCSV'
+import {
+  resultsToCSV,
+  formatDate,
+  dashboardtoCSV,
+} from 'shared/parsing/resultsToCSV'
 
 describe('formatDate', () => {
   it('converts timestamp to an excel compatible date string', () => {
@@ -44,5 +48,58 @@ describe('resultsToCSV', () => {
     expect(response.flag).to.equal(expected.flag)
     expect(response.name).to.equal(expected.name)
     expect(response.CSVString).to.equal(expected.CSVString)
+  })
+})
+
+describe('dashboardtoCSV', () => {
+  it('parses the array of timeseries data displayed by the dashboard cell to a CSVstring for download', () => {
+    const data = [
+      {
+        results: [
+          {
+            statement_id: 0,
+            series: [
+              {
+                name: 'procstat',
+                columns: ['time', 'mean_cpu_usage'],
+                values: [
+                  [1505262600000, 0.06163066773148772],
+                  [1505264400000, 2.616484718180463],
+                  [1505266200000, 1.6174323943535571],
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        results: [
+          {
+            statement_id: 0,
+            series: [
+              {
+                name: 'procstat',
+                columns: ['not-time', 'mean_cpu_usage'],
+                values: [
+                  [1505262600000, 0.06163066773148772],
+                  [1505264400000, 2.616484718180463],
+                  [1505266200000, 1.6174323943535571],
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ]
+    const result = dashboardtoCSV(data)
+    const expected = `time,mean_cpu_usage,not-time,mean_cpu_usage\n${formatDate(
+      1505262600000
+    )},0.06163066773148772,1505262600000,0.06163066773148772\n${formatDate(
+      1505264400000
+    )},2.616484718180463,1505264400000,2.616484718180463\n${formatDate(
+      1505266200000
+    )},1.6174323943535571,1505266200000,1.6174323943535571`
+    expect(result).to.be.a('string')
+    expect(result).to.equal(expected)
   })
 })
