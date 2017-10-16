af6e71f2d4231d0f7228d2b5dbf79968f3a8b48a
Alex P <thealexpaxton@gmail.com>
2017-10-16 11:38:41 -0700

    Use resizer component to simultaneously display logs and the editor

--- ui/src/kapacitor/components/Tickscript.js
+++ ui/src/kapacitor/components/Tickscript.js
@@ -1,6 +1,8 @@
 import React, {PropTypes} from 'react'
 import TickscriptHeader from 'src/kapacitor/components/TickscriptHeader'
 import TickscriptEditor from 'src/kapacitor/components/TickscriptEditor'
+import ResizeContainer from 'shared/components/ResizeContainer'
+import LogsTable from 'src/kapacitor/components/LogsTable'
 
 const Tickscript = ({
   source,
@@ -12,6 +14,8 @@ const Tickscript = ({
   onChangeType,
   onChangeID,
   isNewTickscript,
+  areLogsVisible,
+  onToggleLogsVisbility,
 }) =>
   <div className="page">
     <TickscriptHeader
@@ -22,32 +26,59 @@ const Tickscript = ({
       onChangeType={onChangeType}
       onSelectDbrps={onSelectDbrps}
       isNewTickscript={isNewTickscript}
+      areLogsVisible={areLogsVisible}
+      onToggleLogsVisbility={onToggleLogsVisbility}
     />
-    <div className="page-contents">
-      <div className="tickscript-console">
-        <div className="tickscript-console--output">
-          {validation
-            ? <p>
-                {validation}
-              </p>
-            : <p className="tickscript-console--default">
-                Save your TICKscript to validate it
-              </p>}
-        </div>
-      </div>
-      <div className="tickscript-editor">
-        <TickscriptEditor
-          script={task.tickscript}
-          onChangeScript={onChangeScript}
-        />
-      </div>
-    </div>
+    {areLogsVisible
+      ? <ResizeContainer containerClass="page-contents">
+          <div>
+            <div className="tickscript-console">
+              <div className="tickscript-console--output">
+                {validation
+                  ? <p>
+                      {validation}
+                    </p>
+                  : <p className="tickscript-console--default">
+                      Save your TICKscript to validate it
+                    </p>}
+              </div>
+            </div>
+            <div className="tickscript-editor">
+              <TickscriptEditor
+                script={task.tickscript}
+                onChangeScript={onChangeScript}
+              />
+            </div>
+          </div>
+          <LogsTable isWidget={false} />
+        </ResizeContainer>
+      : <div className="page-contents">
+          <div className="tickscript-console">
+            <div className="tickscript-console--output">
+              {validation
+                ? <p>
+                    {validation}
+                  </p>
+                : <p className="tickscript-console--default">
+                    Save your TICKscript to validate it
+                  </p>}
+            </div>
+          </div>
+          <div className="tickscript-editor">
+            <TickscriptEditor
+              script={task.tickscript}
+              onChangeScript={onChangeScript}
+            />
+          </div>
+        </div>}
   </div>
 
 const {arrayOf, bool, func, shape, string} = PropTypes
 
 Tickscript.propTypes = {
   onSave: func.isRequired,
+  areLogsVisible: bool,
+  onToggleLogsVisbility: func.isRequired,
   source: shape(),
   task: shape({
     id: string,
