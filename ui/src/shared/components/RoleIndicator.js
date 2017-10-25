c8163b68aa02e31267af7ebaaccff160221aa04b
Alex P <thealexpaxton@gmail.com>
2017-10-25 14:14:35 -0700

    Create component to indicate current role to the user

---
+++ ui/src/shared/components/RoleIndicator.js
@@ -0,0 +1,55 @@
+import React, {PropTypes} from 'react'
+import uuid from 'node-uuid'
+
+import {connect} from 'react-redux'
+
+import ReactTooltip from 'react-tooltip'
+
+const getRoleName = ({roles: [{name}, ..._]}) => name
+
+const RoleIndicator = ({me, isUsingAuth}) => {
+  if (!isUsingAuth) {
+    return null
+  }
+
+  const roleName = getRoleName(me)
+  const RoleTooltip = `<h1>Role: <code>${roleName}</code></h1>`
+  const uuidTooltip = uuid.v4()
+
+  return (
+    <div
+      className="role-indicator"
+      data-for={uuidTooltip}
+      data-tip={RoleTooltip}
+    >
+      <span className="icon user" />
+      <ReactTooltip
+        id={uuidTooltip}
+        effect="solid"
+        html={true}
+        place="bottom"
+        class="influx-tooltip"
+      />
+    </div>
+  )
+}
+
+const {arrayOf, bool, shape, string} = PropTypes
+
+RoleIndicator.propTypes = {
+  isUsingAuth: bool.isRequired,
+  me: shape({
+    roles: arrayOf(
+      shape({
+        name: string.isRequired,
+      })
+    ),
+  }),
+}
+
+const mapStateToProps = ({auth: {me, isUsingAuth}}) => ({
+  me,
+  isUsingAuth,
+})
+
+export default connect(mapStateToProps)(RoleIndicator)
