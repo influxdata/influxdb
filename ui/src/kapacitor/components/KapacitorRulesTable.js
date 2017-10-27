b67373b9feb8b1481e1f276e71b642cb83bfe88e
Jared Scheib <jared.scheib@gmail.com>
2017-10-26 17:58:58 -0700

    Revert "Render Delete & Edit TICKscript buttons in Rules Table based on user authorization"
    This reverts commit f1d385a97eda4978268502233902c94a9ab5f965.

--- ui/src/kapacitor/components/KapacitorRulesTable.js
+++ ui/src/kapacitor/components/KapacitorRulesTable.js
@@ -2,8 +2,6 @@ import React, {PropTypes} from 'react'
 import {Link} from 'react-router'
 import _ from 'lodash'
 
-import Authorized, {EDITOR_ROLE} from 'src/auth/Authorized'
-
 import {KAPACITOR_RULES_TABLE} from 'src/kapacitor/constants/tableSizing'
 const {
   colName,
@@ -79,22 +77,18 @@ const RuleRow = ({rule, source, onDelete, onChangeRuleStatus}) =>
       </div>
     </td>
     <td style={{width: colActions}} className="text-right table-cell-nowrap">
-      <Authorized requiredRole={EDITOR_ROLE}>
-        <Link
-          className="btn btn-info btn-xs"
-          to={`/sources/${source.id}/tickscript/${rule.id}`}
-        >
-          Edit TICKscript
-        </Link>
-      </Authorized>
-      <Authorized requiredRole={EDITOR_ROLE}>
-        <button
-          className="btn btn-danger btn-xs"
-          onClick={handleDelete(rule, onDelete)}
-        >
-          Delete
-        </button>
-      </Authorized>
+      <Link
+        className="btn btn-info btn-xs"
+        to={`/sources/${source.id}/tickscript/${rule.id}`}
+      >
+        Edit TICKscript
+      </Link>
+      <button
+        className="btn btn-danger btn-xs"
+        onClick={handleDelete(rule, onDelete)}
+      >
+        Delete
+      </button>
     </td>
   </tr>
 
