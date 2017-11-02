c61e215ded39ae031ce0528dbac0edf9ed43600f
Edd Robinson <me@edd.io>
2017-11-02 08:42:42 +0000

    Add time-based rewriter tests

--- query/statement_rewriter_test.go
+++ query/statement_rewriter_test.go
@@ -88,6 +88,42 @@ func TestRewriteStatement(t *testing.T) {
 			s:    `SELECT "key" FROM mydb.myrp1._series WHERE _name =~ /c.*/`,
 		},
 		{
+			stmt: `SHOW SERIES WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM /.+/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES ON db0 WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM db0../.+/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES FROM cpu WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES ON db0 FROM cpu WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM db0..cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES FROM mydb.myrp1.cpu WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM mydb.myrp1.cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES ON db0 FROM mydb.myrp1.cpu WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM mydb.myrp1.cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES FROM mydb.myrp1./c.*/ WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM mydb.myrp1./c.*/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES FROM mydb.myrp1./c.*/ WHERE region = 'uswest' AND time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM mydb.myrp1./c.*/ WHERE region = 'uswest' AND time > 0`,
+		},
+		{
+			stmt: `SHOW SERIES ON db0 FROM mydb.myrp1./c.*/ WHERE time > 0`,
+			s:    `SELECT _seriesKey AS "key" FROM mydb.myrp1./c.*/ WHERE time > 0`,
+		},
+		{
 			stmt: `SHOW TAG KEYS`,
 			s:    `SELECT tagKey FROM _tagKeys`,
 		},
@@ -142,6 +178,62 @@ func TestRewriteStatement(t *testing.T) {
 		{
 			stmt: `SHOW TAG KEYS ON db0 FROM mydb.myrp1.cpu WHERE region = 'uswest'`,
 			s:    `SELECT tagKey FROM mydb.myrp1._tagKeys WHERE (_name = 'cpu') AND (region = 'uswest')`,
+		},
+		{
+			stmt: `SHOW TAG KEYS WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM /.+/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM db0../.+/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS FROM cpu WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 FROM cpu WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM db0..cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS FROM /c.*/ WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM /c.*/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 FROM /c.*/ WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM db0../c.*/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS FROM cpu WHERE region = 'uswest' AND time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM cpu WHERE region = 'uswest' AND time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 FROM cpu WHERE region = 'uswest' AND time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM db0..cpu WHERE region = 'uswest' AND time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS FROM mydb.myrp1.cpu WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM mydb.myrp1.cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 FROM mydb.myrp1.cpu WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM mydb.myrp1.cpu WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS FROM mydb.myrp1./c.*/ WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM mydb.myrp1./c.*/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 FROM mydb.myrp1./c.*/ WHERE time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM mydb.myrp1./c.*/ WHERE time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS FROM mydb.myrp1.cpu WHERE region = 'uswest' AND time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM mydb.myrp1.cpu WHERE region = 'uswest' AND time > 0`,
+		},
+		{
+			stmt: `SHOW TAG KEYS ON db0 FROM mydb.myrp1.cpu WHERE region = 'uswest' AND time > 0`,
+			s:    `SELECT distinct(_tagKey) AS tagKey FROM mydb.myrp1.cpu WHERE region = 'uswest' AND time > 0`,
 		},
 		{
 			stmt: `SELECT value FROM cpu`,
