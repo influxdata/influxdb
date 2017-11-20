e22e6f6ccf471a89a152a9ac7795210f4ba6cf0f
Alex P <thealexpaxton@gmail.com>
2017-11-20 14:31:53 -0800

    Introduce array of available gauge colors

---
+++ ui/src/dashboards/constants/gaugeColors.js
@@ -0,0 +1,66 @@
+export const GAUGE_COLORS = [
+  {
+    hex: '#BF3D5E',
+    text: 'Ruby',
+  },
+  {
+    hex: '#DC4E58',
+    text: 'Fire',
+  },
+  {
+    hex: '#DC4E58',
+    text: 'Fire',
+  },
+  {
+    hex: '#F95F53',
+    text: 'Curacao',
+  },
+  {
+    hex: '#F48D38',
+    text: 'Tiger',
+  },
+  {
+    hex: '#FFB94A',
+    text: 'Pineapple',
+  },
+  {
+    hex: '#FFD255',
+    text: 'Thunder',
+  },
+  {
+    hex: '#7CE490',
+    text: 'Honeydew',
+  },
+  {
+    hex: '#4ED8A0',
+    text: 'Rainforest',
+  },
+  {
+    hex: '#32B08C',
+    text: 'Viridian',
+  },
+  {
+    hex: '#4591ED',
+    text: 'Ocean',
+  },
+  {
+    hex: '#22ADF6',
+    text: 'Pool',
+  },
+  {
+    hex: '#00C9FF',
+    text: 'Laser',
+  },
+  {
+    hex: '#513CC6',
+    text: 'Planet',
+  },
+  {
+    hex: '#7A65F2',
+    text: 'Star',
+  },
+  {
+    hex: '#9394FF',
+    text: 'Comet',
+  },
+]
