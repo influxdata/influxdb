63d4f70d1134d9ef08f48bd773cdc94a261e6a7a
Jared Scheib <jared.scheib@gmail.com>
2017-11-30 14:45:27 -0800

    Remove obsolete Authenticated component import/export

--- ui/src/auth/index.js
+++ ui/src/auth/index.js
@@ -1,15 +1,6 @@
 import Login from './Login'
 import Purgatory from './Purgatory'
 
-import {
-  UserIsAuthenticated,
-  Authenticated,
-  UserIsNotAuthenticated,
-} from './Authenticated'
-export {
-  Login,
-  Purgatory,
-  UserIsAuthenticated,
-  Authenticated,
-  UserIsNotAuthenticated,
-}
+import {UserIsAuthenticated, UserIsNotAuthenticated} from './Authenticated'
+
+export {Login, Purgatory, UserIsAuthenticated, UserIsNotAuthenticated}
