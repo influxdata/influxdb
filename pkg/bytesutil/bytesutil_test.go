d7a8368d2d0ca7307550fcb98b47abcae5f86043
Stuart Carnie <stuart.carnie@gmail.com>
2017-12-15 17:58:54 -0700

    Add bytesutil.Contains; improve performance of SearchBytes
    ```
    benchmark                           old ns/op     new ns/op     delta
    BenchmarkContains_True-8            67.0          41.8          -37.61%
    BenchmarkContains_False-8           73.2          47.6          -34.97%
    BenchmarkSearchBytes_Exists-8       51.5          34.3          -33.40%
    BenchmarkSearchBytes_NotExits-8     57.7          39.8          -31.02%
    ```

--- pkg/bytesutil/bytesutil_test.go
+++ pkg/bytesutil/bytesutil_test.go
@@ -34,6 +34,54 @@ func TestSearchBytesFixed(t *testing.T) {
 	}
 }
 
+func TestSearchBytes(t *testing.T) {
+	in := toByteSlices("bbb", "ccc", "eee", "fff", "ggg", "hhh")
+	tests := []struct {
+		name string
+		x    string
+		exp  int
+	}{
+		{"exists first", "bbb", 0},
+		{"exists middle", "eee", 2},
+		{"exists last", "hhh", 5},
+		{"not exists last", "zzz", 6},
+		{"not exists first", "aaa", 0},
+		{"not exists mid", "ddd", 2},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := bytesutil.SearchBytes(in, []byte(test.x))
+			if got != test.exp {
+				t.Errorf("got %t, expected %t", got, test.exp)
+			}
+		})
+	}
+}
+
+func TestContains(t *testing.T) {
+	in := toByteSlices("bbb", "ccc", "eee", "fff", "ggg", "hhh")
+	tests := []struct {
+		name string
+		x    string
+		exp  bool
+	}{
+		{"exists first", "bbb", true},
+		{"exists middle", "eee", true},
+		{"exists last", "hhh", true},
+		{"not exists last", "zzz", false},
+		{"not exists first", "aaa", false},
+		{"not exists mid", "ddd", false},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got := bytesutil.Contains(in, []byte(test.x))
+			if got != test.exp {
+				t.Errorf("got %t, expected %t", got, test.exp)
+			}
+		})
+	}
+}
+
 func TestPack_WidthOne_One(t *testing.T) {
 	a := make([]byte, 8)
 
@@ -128,5 +176,45 @@ func TestPack_WidthOne_LastFill(t *testing.T) {
 		if got, exp := a[i], v; got != exp {
 			t.Fatalf("value mismatch: a[%d] = %v, exp %v", i, got, exp)
 		}
+	}
+}
+
+func BenchmarkContains_True(b *testing.B) {
+	var in [][]byte
+	for i := 'a'; i <= 'z'; i++ {
+		in = append(in, []byte(strings.Repeat(string(i), 3)))
+	}
+	for i := 0; i < b.N; i++ {
+		bytesutil.Contains(in, []byte("xxx"))
+	}
+}
+
+func BenchmarkContains_False(b *testing.B) {
+	var in [][]byte
+	for i := 'a'; i <= 'z'; i++ {
+		in = append(in, []byte(strings.Repeat(string(i), 3)))
+	}
+	for i := 0; i < b.N; i++ {
+		bytesutil.Contains(in, []byte("a"))
+	}
+}
+
+func BenchmarkSearchBytes_Exists(b *testing.B) {
+	var in [][]byte
+	for i := 'a'; i <= 'z'; i++ {
+		in = append(in, []byte(strings.Repeat(string(i), 3)))
+	}
+	for i := 0; i < b.N; i++ {
+		bytesutil.SearchBytes(in, []byte("xxx"))
+	}
+}
+
+func BenchmarkSearchBytes_NotExits(b *testing.B) {
+	var in [][]byte
+	for i := 'a'; i <= 'z'; i++ {
+		in = append(in, []byte(strings.Repeat(string(i), 3)))
+	}
+	for i := 0; i < b.N; i++ {
+		bytesutil.SearchBytes(in, []byte("a"))
 	}
 }
