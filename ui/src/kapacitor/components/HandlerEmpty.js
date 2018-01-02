b1f16adc7b1409858719d3fd439cf367acd86b43
deniz kusefoglu <denizk@gmail.com>
2018-01-02 11:19:08 -0800

    Improve copy around un-configured handlers

--- ui/src/kapacitor/components/HandlerEmpty.js
+++ ui/src/kapacitor/components/HandlerEmpty.js
@@ -4,7 +4,7 @@ const HandlerEmpty = ({onGoToConfig, validationError}) =>
   <div className="endpoint-tab-contents">
     <div className="endpoint-tab--parameters">
       <div className="endpoint-tab--parameters--empty">
-        <p>This handler does not seem to be configured.</p>
+        <p>This handler has not been configured</p>
         <div className="form-group-submit col-xs-12 text-center">
           <button
             className="btn btn-primary"
@@ -12,8 +12,8 @@ const HandlerEmpty = ({onGoToConfig, validationError}) =>
             onClick={onGoToConfig}
           >
             {validationError
-              ? 'Exit Rule and Configure Alert Handlers'
-              : 'Save Rule and Configure Alert Handlers'}
+              ? 'Exit Rule and Configure this Alert Handler'
+              : 'Save Rule and Configure this Alert Handler'}
           </button>
         </div>
       </div>
