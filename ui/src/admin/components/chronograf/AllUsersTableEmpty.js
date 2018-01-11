679d2374e28f8c2a78b68ae0d1c352f8f6526f4c
Jared Scheib <jared.scheib@gmail.com>
2018-01-10 16:09:49 -0800

    Make AllUsersTableEmpty an SFC

--- ui/src/admin/components/chronograf/AllUsersTableEmpty.js
+++ ui/src/admin/components/chronograf/AllUsersTableEmpty.js
@@ -3,40 +3,30 @@ import React from 'react'
 import UsersTableHeader from 'src/admin/components/chronograf/UsersTableHeader'
 
 import {USERS_TABLE} from 'src/admin/constants/chronografTableSizing'
+const {colRole, colSuperAdmin, colProvider, colScheme, colActions} = USERS_TABLE
 
-const AllUsersTableEmpty = () => {
-  const {
-    colRole,
-    colSuperAdmin,
-    colProvider,
-    colScheme,
-    colActions,
-  } = USERS_TABLE
-
-  return (
-    <div className="panel panel-default">
-      <UsersTableHeader />
-      <div className="panel-body">
-        <table className="table table-highlight v-center chronograf-admin-table">
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th style={{width: colRole}} className="align-with-col-text">
-                Role
-              </th>
-              <th style={{width: colSuperAdmin}} className="text-center">
-                SuperAdmin
-              </th>
-              <th style={{width: colProvider}}>Provider</th>
-              <th style={{width: colScheme}}>Scheme</th>
-              <th className="text-right" style={{width: colActions}} />
-            </tr>
-          </thead>
-          <tbody />
-        </table>
-      </div>
+const AllUsersTableEmpty = () =>
+  <div className="panel panel-default">
+    <UsersTableHeader />
+    <div className="panel-body">
+      <table className="table table-highlight v-center chronograf-admin-table">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th style={{width: colRole}} className="align-with-col-text">
+              Role
+            </th>
+            <th style={{width: colSuperAdmin}} className="text-center">
+              SuperAdmin
+            </th>
+            <th style={{width: colProvider}}>Provider</th>
+            <th style={{width: colScheme}}>Scheme</th>
+            <th className="text-right" style={{width: colActions}} />
+          </tr>
+        </thead>
+        <tbody />
+      </table>
     </div>
-  )
-}
+  </div>
 
 export default AllUsersTableEmpty
