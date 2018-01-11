6e5b5c27904d133b69518e078190fa9f373b77f6
Jared Scheib <jared.scheib@gmail.com>
2018-01-10 16:06:48 -0800

    Make UsersTableEmpty an SFC

--- ui/src/admin/components/chronograf/UsersTableEmpty.js
+++ ui/src/admin/components/chronograf/UsersTableEmpty.js
@@ -3,31 +3,27 @@ import React from 'react'
 import UsersTableHeader from 'src/admin/components/chronograf/UsersTableHeader'
 
 import {USERS_TABLE} from 'src/admin/constants/chronografTableSizing'
+const {colRole, colProvider, colScheme, colActions} = USERS_TABLE
 
-const UsersTableEmpty = () => {
-  const {colRole, colProvider, colScheme, colActions} = USERS_TABLE
-
-  return (
-    <div className="panel panel-default">
-      <UsersTableHeader />
-      <div className="panel-body">
-        <table className="table table-highlight v-center chronograf-admin-table">
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th style={{width: colRole}} className="align-with-col-text">
-                Role
-              </th>
-              <th style={{width: colProvider}}>Provider</th>
-              <th style={{width: colScheme}}>Scheme</th>
-              <th className="text-right" style={{width: colActions}} />
-            </tr>
-          </thead>
-          <tbody />
-        </table>
-      </div>
+const UsersTableEmpty = () =>
+  <div className="panel panel-default">
+    <UsersTableHeader />
+    <div className="panel-body">
+      <table className="table table-highlight v-center chronograf-admin-table">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th style={{width: colRole}} className="align-with-col-text">
+              Role
+            </th>
+            <th style={{width: colProvider}}>Provider</th>
+            <th style={{width: colScheme}}>Scheme</th>
+            <th className="text-right" style={{width: colActions}} />
+          </tr>
+        </thead>
+        <tbody />
+      </table>
     </div>
-  )
-}
+  </div>
 
 export default UsersTableEmpty
