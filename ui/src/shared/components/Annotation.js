fd9dd2d7a478b087c28d4c2996adbbdc5c81f7ca
Andrew Watkins <watts@influxdb.com>
2018-01-12 12:59:46 -0800

    Store left in var

--- ui/src/shared/components/Annotation.js
+++ ui/src/shared/components/Annotation.js
@@ -1,10 +1,12 @@
 import React, {PropTypes} from 'react'
 
 const calcStyle = (annotation, dygraph) => {
+  const left = `${dygraph.toDomXCoord(annotation.time)}px`
+
   return {
+    left,
     position: 'absolute',
     top: '0px',
-    left: `${dygraph.toDomXCoord(annotation.time)}px`,
     backgroundColor: '#f00',
     height: '100%',
     width: '2px',
