2e93c5d2221978b09c2182a025285569fd87b613
Andrew Watkins <watts@influxdb.com>
2018-01-16 09:41:36 -0800

    Remove storybook

--- ui/stories/shared.js
+++
@@ -1,8 +0,0 @@
-export const spyActions = (actions) => Object.keys(actions)
-  .reduce((acc, a) => {
-    acc[a] = (...evt) => {
-      action(a)(...evt);
-      return actions[a](...evt);
-    };
-    return acc;
-  }, {});
