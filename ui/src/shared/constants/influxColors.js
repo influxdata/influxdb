b39461c4b0b922ab0c5eb668ec54684a55101c76
Alex P <thealexpaxton@gmail.com>
2018-01-18 12:22:15 -0800

    Introduce JS version of color palette

---
+++ ui/src/shared/constants/influxColors.js
@@ -0,0 +1,95 @@
+// InfluxData Color Palette
+// -----------------------------------------------------------------------------
+
+// Nuetrals (Dark to Light)
+export const NEUTRALS = [
+  '#0f0e15', //obsidian
+  '#1c1c21', //raven
+  '#202028', //kevlar
+  '#292933', //castle
+  '#31313d', //onyx
+  '#383846', //pepper
+  '#434453', //smoke
+  '#545667', //graphite
+  '#676978', //storm
+  '#757888', //mountain
+  '#8e91a1', //wolf
+  '#999dab', //sidewalk
+  '#a4a8b6', //forge
+  '#bec2cc', //mist
+  '#c6cad3', //chromium
+  '#d4d7dd', //platinum
+  '#e7e8eb', //pearl
+  '#eeeff2', //whisper
+  '#f6f6f8', //cloud
+  '#fafafc', //ghost
+  '#ffffff', //white
+]
+
+// Telegraf (Dark to Light)
+export const REDS = {
+  basalt: '#2F1F29',
+  ruby: '#BF3D5E',
+  fire: '#DC4E58',
+  fireDisabled: '#6F3943',
+  curacao: '#F95F53',
+  curacaoDisabled: '#EADAD8',
+  dreamsicle: '#FF8564',
+  tungsten: '#FFB6A0',
+  marmelade: '#FFDCCF',
+  flan: '#FFF7F4',
+}
+
+// InfluxDB (Dark to Light)
+export const BLUES = {
+  abyss: '#182838',
+  sapphire: '#326BBA',
+  ocean: '#4591ED',
+  oceanDisabled: '#32547F',
+  pool: '#22ADF6',
+  poolDisabled: '#C9E0ED',
+  laser: '#00C9FF',
+  hydrogen: '#6BDFFF',
+  neutrino: '#BEF0FF',
+  yeti: '#F0FCFF',
+}
+
+// Chronograf (Dark to Light)
+export const PURPLES = {
+  shadow: '#1F2039',
+  void: '#311F94',
+  amethyst: '#513CC6',
+  amethystDisabled: '#484281',
+  star: '#7A65F2',
+  starDisabled: '#D6D5ED',
+  comet: '#9394FF',
+  potassium: '#B1B6FF',
+  moonstone: '#C9D0FF',
+  twilight: '#F2F4FF',
+}
+
+// Kapacitor (Dark to Light)
+export const GREENS = {
+  ypsy: '#152B2D',
+  emerald: '#108174',
+  viridian: '#32B08C',
+  viridianDisabled: '#2B6058',
+  rainforest: '#4ED8A0',
+  rainforestDisabled: '#CFE6E1',
+  honeydew: '#7CE490',
+  krypton: '#A5F3B4',
+  wasabi: '#C6FFD0',
+  mint: '#F2FFF4',
+}
+
+// Warnings (Dark to Light)
+export const YELLOWS = {
+  oak: '#3F241F',
+  topaz: '#E85B1C',
+  tiger: '#F48D38',
+  pineapple: '#FFB94A',
+  thunder: '#FFD255',
+  sulfur: '#FFE480',
+  daisy: '#FFF6B8',
+  banana: '#FFFDDE',
+}
