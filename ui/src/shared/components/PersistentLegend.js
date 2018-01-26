13c83f8985c57e3662ff1e3350bb251141212f67
Alex P <thealexpaxton@gmail.com>
2018-01-26 14:41:23 -0800

    WIP polish appearance of toggleable persistent legends

--- ui/src/shared/components/PersistentLegend.js
+++ ui/src/shared/components/PersistentLegend.js
@@ -10,24 +10,41 @@ const style = {
   height: '30px',
 }
 
+const persistentLegendItemClassname = (visibilities, i) => {
+  if (visibilities.length) {
+    return `persistent-legend--item${visibilities[i] ? '' : ' disabled'}`
+  }
+
+  return 'persistent-legend--item'
+}
+
 class PersistentLegend extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      visibilities: [],
+    }
   }
+
   handleClick = i => () => {
     const visibilities = this.props.dygraph.visibility()
-    this.props.dygraph.setVisibility(i, !visibilities[i])
+    visibilities[i] = !visibilities[i]
+
+    this.props.dygraph.setVisibility(visibilities)
+    this.setState({visibilities})
   }
 
   render() {
     const {dygraph} = this.props
+    const {visibilities} = this.state
     const labels = dygraph ? _.drop(dygraph.getLabels()) : []
 
     return (
       <div className="persistent-legend" style={style}>
         {_.map(labels, (v, i) =>
           <div
-            className="persistent-legend--item"
+            className={persistentLegendItemClassname(visibilities, i)}
             key={uuid.v4()}
             onClick={this.handleClick(i)}
           >
