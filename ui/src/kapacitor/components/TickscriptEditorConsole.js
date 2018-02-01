b814f0d5a0ca269d36b7057069f99b921d97f904
Alex P <thealexpaxton@gmail.com>
2018-02-01 15:34:59 -0800

    Redesign tick script editor console
    Moved to bottom, color coded, is larger

--- ui/src/kapacitor/components/TickscriptEditorConsole.js
+++ ui/src/kapacitor/components/TickscriptEditorConsole.js
@@ -2,10 +2,32 @@ import React, {PropTypes} from 'react'
 
 const TickscriptEditorConsole = ({consoleMessage, unsavedChanges}) => {
   let consoleOutput
-    </div>
+  let consoleClass = 'tickscript-console--default'
 
+  if (!unsavedChanges) {
+    consoleOutput = 'TICKscript is valid'
+    consoleClass = 'tickscript-console--valid'
+  } else if (unsavedChanges && !consoleMessage) {
+    consoleOutput = 'You have unsaved changes, save to validate TICKscript'
+    consoleClass = 'tickscript-console--default'
+  } else if (unsavedChanges && consoleMessage) {
+    consoleOutput = consoleMessage
+    consoleClass = 'tickscript-console--error'
+  }
+  return (
+    <div className="tickscript-console">
+      <p className={consoleClass}>
+        {consoleOutput}
+      </p>
+    </div>
+  )
+}
+
+const {bool, string} = PropTypes
 
 TickscriptEditorConsole.propTypes = {
+  consoleMessage: string,
+  unsavedChanges: bool,
 }
 
 export default TickscriptEditorConsole
