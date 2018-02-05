b615be2f238a488b17a394d0b2d0572541164c95
Alex P <thealexpaxton@gmail.com>
2018-02-05 13:45:50 -0800

    Remove infinite scroll from logs table and render only 200 most recent logs

--- ui/src/kapacitor/components/LogsTable.js
+++ ui/src/kapacitor/components/LogsTable.js
@@ -1,29 +1,30 @@
 import React, {PropTypes} from 'react'
 
-import InfiniteScroll from 'shared/components/InfiniteScroll'
 import LogsTableRow from 'src/kapacitor/components/LogsTableRow'
+import FancyScrollbar from 'src/shared/components/FancyScrollbar'
+
+const numLogsToRender = 200
 
 const LogsTable = ({logs, onToggleExpandLog}) =>
   <div className="logs-table--container">
     <div className="logs-table--header">
-      <h2 className="panel-title">Logs</h2>
+      <h2 className="panel-title">{`${numLogsToRender} Most Recent Logs`}</h2>
     </div>
-    <div className="logs-table--panel fancy-scroll--kapacitor">
-      {logs.length
-        ? <InfiniteScroll
-            className="logs-table"
-            itemHeight={87}
-            items={logs.map((log, i) =>
-              <LogsTableRow
-                key={log.key}
-                logItem={log}
-                index={i}
-                onToggleExpandLog={onToggleExpandLog}
-              />
-            )}
+    <FancyScrollbar
+      autoHide={false}
+      className="logs-table--panel fancy-scroll--kapacitor"
+    >
+      {logs
+        .slice(0, numLogsToRender)
+        .map((log, i) =>
+          <LogsTableRow
+            key={log.key}
+            logItem={log}
+            index={i}
+            onToggleExpandLog={onToggleExpandLog}
           />
-        : <div className="page-spinner" />}
-    </div>
+        )}
+    </FancyScrollbar>
   </div>
 
 const {arrayOf, func, shape, string} = PropTypes
