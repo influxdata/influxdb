043d134a8198bbf5fec3ab406a93bb6bec3182b8
Luke Morris <lukevmorris@gmail.com>
2018-02-09 14:29:43 -0800

    add user to Default org in add-superadmin cmd

--- cmd/chronoctl/add.go
+++ cmd/chronoctl/add.go
@@ -41,9 +41,15 @@ func (l *AddCommand) Execute(args []string) error {
 		return err
 	} else if err == chronograf.ErrUserNotFound {
 		user = &chronograf.User{
-			Name:       l.Username,
-			Provider:   l.Provider,
-			Scheme:     l.Scheme,
+			Name:     l.Username,
+			Provider: l.Provider,
+			Scheme:   l.Scheme,
+			Roles: []chronograf.Role{
+				{
+					Name:         "member",
+					Organization: "default",
+				},
+			},
 			SuperAdmin: true,
 		}
 
@@ -53,6 +59,14 @@ func (l *AddCommand) Execute(args []string) error {
 		}
 	} else {
 		user.SuperAdmin = true
+		if len(user.Roles) == 0 {
+			user.Roles = []chronograf.Role{
+				{
+					Name:         "member",
+					Organization: "default",
+				},
+			}
+		}
 		if err = c.UsersStore.Update(ctx, user); err != nil {
 			return err
 		}
