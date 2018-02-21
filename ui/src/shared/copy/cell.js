a4d4cca52b221ec28efd2f69faaf6f2be3f89626
Deniz Kusefoglu <deniz@influxdata.com>
2018-02-21 11:44:19 -0800

    Change emptyfunnies

--- ui/src/shared/copy/cell.js
+++ ui/src/shared/copy/cell.js
@@ -1,8 +1,8 @@
 import _ from 'lodash'
 
 const emptyFunny = [
-  'Looks like you don’t have any queries. Be a lot cooler if you did.',
-  'Create a query. Go on, I dare ya!',
+  'Looks like you don’t have any queries. Be a lot cooler if you did!',
+  'Create a query. Go on!',
   'Create a query. Have fun!',
 ]
 
