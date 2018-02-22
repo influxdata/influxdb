5da992ae2750315f90db3c5ad71d590820b1cd44
Michael Desa <mjdesa@gmail.com>
2018-02-22 10:40:22 -0500

    Use Generic APIKey for Oauth2 group lookup

--- oauth2/generic_test.go
+++ oauth2/generic_test.go
@@ -10,6 +10,98 @@ import (
 	"github.com/influxdata/chronograf/oauth2"
 )
 
+func TestGenericGroup_withNotEmail(t *testing.T) {
+	t.Parallel()
+
+	response := struct {
+		Email string `json:"not-email"`
+	}{
+		"martymcfly@pinheads.rok",
+	}
+	mockAPI := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+		enc := json.NewEncoder(rw)
+
+		rw.WriteHeader(http.StatusOK)
+		_ = enc.Encode(response)
+	}))
+	defer mockAPI.Close()
+
+	logger := clog.New(clog.ParseLevel("debug"))
+	prov := oauth2.Generic{
+		Logger: logger,
+		APIURL: mockAPI.URL,
+		APIKey: "not-email",
+	}
+	tt, err := oauth2.NewTestTripper(logger, mockAPI, http.DefaultTransport)
+	if err != nil {
+		t.Fatal("Error initializing TestTripper: err:", err)
+	}
+
+	tc := &http.Client{
+		Transport: tt,
+	}
+
+	got, err := prov.Group(tc)
+	if err != nil {
+		t.Fatal("Unexpected error while retrieiving PrincipalID: err:", err)
+	}
+
+	want := "pinheads.rok"
+	if got != want {
+		t.Fatal("Retrieved group was not as expected. Want:", want, "Got:", got)
+	}
+}
+
+func TestGenericGroup_withEmail(t *testing.T) {
+	t.Parallel()
+
+	response := struct {
+		Email string `json:"email"`
+	}{
+		"martymcfly@pinheads.rok",
+	}
+	mockAPI := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+		enc := json.NewEncoder(rw)
+
+		rw.WriteHeader(http.StatusOK)
+		_ = enc.Encode(response)
+	}))
+	defer mockAPI.Close()
+
+	logger := clog.New(clog.ParseLevel("debug"))
+	prov := oauth2.Generic{
+		Logger: logger,
+		APIURL: mockAPI.URL,
+		APIKey: "email",
+	}
+	tt, err := oauth2.NewTestTripper(logger, mockAPI, http.DefaultTransport)
+	if err != nil {
+		t.Fatal("Error initializing TestTripper: err:", err)
+	}
+
+	tc := &http.Client{
+		Transport: tt,
+	}
+
+	got, err := prov.Group(tc)
+	if err != nil {
+		t.Fatal("Unexpected error while retrieiving PrincipalID: err:", err)
+	}
+
+	want := "pinheads.rok"
+	if got != want {
+		t.Fatal("Retrieved group was not as expected. Want:", want, "Got:", got)
+	}
+}
+
 func TestGenericPrincipalID(t *testing.T) {
 	t.Parallel()
 
