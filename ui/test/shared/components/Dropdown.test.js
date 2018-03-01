6712a9af4be4a060ac719bc71f0633db60cc2eb7
Andrew Watkins <watts@influxdb.com>
2018-02-28 17:37:07 -0700

    Add test for handleSelection

--- ui/test/shared/components/Dropdown.test.js
+++ ui/test/shared/components/Dropdown.test.js
@@ -4,8 +4,13 @@ import DropdownMenu from 'shared/components/DropdownMenu'
 import DropdownInput from 'shared/components/DropdownInput'
 
 import {shallow} from 'enzyme'
-const items = [{text: 'foo'}, {text: 'bar'}]
 
+// mock props
+const itemOne = {text: 'foo'}
+const itemTwo = {text: 'bar'}
+const items = [itemOne, itemTwo]
+
+// automate shallow render and providing new props
 const setup = (override = {}) => {
   const props = {
     items: [],
@@ -14,13 +19,21 @@ const setup = (override = {}) => {
     ...override,
   }
 
+  const defaultState = {
+    isOpen: false,
+    searchTerm: '',
+    filteredItems: items,
+    highlightedItemIndex: null,
+  }
+
   const dropdown = shallow(<Dropdown {...props} />).dive({
     'data-test': 'dropdown-button',
   })
 
   return {
-    dropdown,
     props,
+    dropdown,
+    defaultState,
   }
 }
 
@@ -83,72 +96,112 @@ describe('Components.Shared.Dropdown', () => {
         })
       })
     })
+  })
 
-    describe('instance methods', () => {
-      describe('applyFilter', () => {
-        it('filters the list by the searchTerm', () => {
-          const {dropdown} = setup({items, useAutoComplete: true})
+  describe('instance methods', () => {
+    describe('applyFilter', () => {
+      it('filters the list by the searchTerm', () => {
+        const {dropdown} = setup({items, useAutoComplete: true})
 
-          dropdown.instance().applyFilter('fo')
-          expect(dropdown.state().filteredItems).toEqual([{text: 'foo'}])
-        })
+        dropdown.instance().applyFilter('fo')
+        expect(dropdown.state().filteredItems).toEqual([{text: 'foo'}])
+      })
+    })
+
+    describe('handleFilterChange', () => {
+      it('resets filteredList and searchTerm if the filter is empty', () => {
+        const {dropdown} = setup({items, useAutoComplete: true})
+        const event = {target: {value: ''}}
+        dropdown.instance().applyFilter('fo')
+
+        // assert that the list is filtered
+        expect(dropdown.state().filteredItems).toEqual([{text: 'foo'}])
+
+        dropdown.instance().handleFilterChange(event)
+        const {filteredItems, searchTerm} = dropdown.state()
+
+        expect(filteredItems).toEqual(items)
+        expect(searchTerm).toEqual('')
+      })
+    })
+
+    describe('handleClickOutside', () => {
+      it('sets isOpen to false', () => {
+        const {dropdown} = setup()
+        dropdown.simulate('click')
+        dropdown.instance().handleClickOutside()
+
+        expect(dropdown.state().isOpen).toBe(false)
+      })
+    })
+
+    describe('handleClick', () => {
+      it('fires the onClick prop', () => {
+        const onClick = jest.fn()
+        const {dropdown} = setup({onClick})
+        dropdown.instance().handleClick()
+
+        expect(onClick).toHaveBeenCalled()
       })
 
-      describe('handleFilterChange', () => {
-        it('resets filteredList and searchTerm if the filter is empty', () => {
-          const {dropdown} = setup({items, useAutoComplete: true})
-          const event = {target: {value: ''}}
-          dropdown.instance().applyFilter('fo')
+      it('toggles the isOpen', () => {
+        const {dropdown} = setup()
 
-          // assert that the list is filtered
-          expect(dropdown.state().filteredItems).toEqual([{text: 'foo'}])
+        dropdown.instance().handleClick()
+        expect(dropdown.state().isOpen).toBe(true)
 
-          dropdown.instance().handleFilterChange(event)
-          const {filteredItems, searchTerm} = dropdown.state()
-
-          expect(filteredItems).toEqual(items)
-          expect(searchTerm).toEqual('')
-        })
+        dropdown.instance().handleClick()
+        expect(dropdown.state().isOpen).toBe(false)
       })
 
-      describe('handleClickOutside', () => {
-        it('sets isOpen to false', () => {
-          const {dropdown} = setup()
-          dropdown.simulate('click')
-          dropdown.instance().handleClickOutside()
+      it('does nothing if disabled', () => {
+        const onClick = jest.fn()
+        const {dropdown} = setup({disabled: true, onClick})
 
-          expect(dropdown.state().isOpen).toBe(false)
-        })
+        dropdown.instance().handleClick()
+
+        expect(dropdown.state().isOpen).toBe(false)
+        expect(onClick).not.toHaveBeenCalled()
+      })
+    })
+
+    describe('handleSelection', () => {
+      it('it calls onChoose with the item provided', () => {
+        const onChoose = jest.fn(item => item)
+        const {dropdown} = setup({onChoose})
+
+        dropdown.simulate('click')
+        dropdown.instance().handleSelection(itemOne)()
+        expect(onChoose).toHaveBeenCalledTimes(1)
+        expect(onChoose.mock.calls[0][0]).toEqual(itemOne)
       })
 
-      describe('handleClick', () => {
-        it('fires the onClick prop', () => {
-          const onClick = jest.fn()
-          const {dropdown} = setup({onClick})
-          dropdown.instance().handleClick()
+      it('closes the menu', () => {
+        const {dropdown} = setup()
+        dropdown.simulate('click')
+        expect(dropdown.state().isOpen).toBe(true)
 
-          expect(onClick).toHaveBeenCalled()
+        dropdown.instance().handleSelection(itemOne)()
+        expect(dropdown.state().isOpen).toBe(false)
+      })
+
+      it('resets state to defaults when opening', () => {
+        const {
+          dropdown,
+          defaultState: {searchTerm, filteredItems, highlightedItemIndex},
+        } = setup({items})
+
+        dropdown.setState({
+          searchTerm: 'foo',
+          filteredItems: [itemOne],
+          highlightedItemIndex: 100,
         })
 
-        it('toggles the isOpen', () => {
-          const {dropdown} = setup()
-
-          dropdown.instance().handleClick()
-          expect(dropdown.state().isOpen).toBe(true)
-
-          dropdown.instance().handleClick()
-          expect(dropdown.state().isOpen).toBe(false)
-        })
-
-        it('does nothing if disabled', () => {
-          const onClick = jest.fn()
-          const {dropdown} = setup({disabled: true, onClick})
-
-          dropdown.instance().handleClick()
-
-          expect(dropdown.state().isOpen).toBe(false)
-          expect(onClick).not.toHaveBeenCalled()
-        })
+        dropdown.instance().handleSelection(itemOne)()
+        expect(dropdown.state().isOpen).toBe(true)
+        expect(dropdown.state().searchTerm).toBe(searchTerm)
+        expect(dropdown.state().filteredItems).toEqual(filteredItems)
+        expect(dropdown.state().highlightedItemIndex).toBe(highlightedItemIndex)
       })
     })
   })
