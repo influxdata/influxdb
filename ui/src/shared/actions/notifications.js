489f62b7cb58c7fabf12929664f2600e9ad7fb18
Alex P <thealexpaxton@gmail.com>
2018-03-02 14:45:53 -0800

    Transform old notification payload shape into new shape
    Will eventually have to refactor, but this makes it backwards
    compatible for now

--- ui/src/shared/actions/notifications.js
+++ ui/src/shared/actions/notifications.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export const publishNotification = notification => ({
   type: 'PUBLISH_NOTIFICATION',
   payload: {notification},
@@ -8,20 +9,12 @@ export const dismissNotification = id => ({
   type: 'DISMISS_NOTIFICATION',
   payload: {id},
 })
-=======
-export function publishNotification(notification) {
-  return {
-    type: 'PUBLISH_NOTIFICATION',
-    payload: {
-      notification,
-    },
-  }
-}
+export const publishNotification = notification => ({
+  type: 'PUBLISH_NOTIFICATION',
+  payload: {notification},
+})
 
-export function dismissNotification(id) {
-  return {
-    type: 'DISMISS_NOTIFICATION',
-    payload: {id},
-  }
-}
->>>>>>> WIP Refactor Notifications
+export const dismissNotification = id => ({
+  type: 'DISMISS_NOTIFICATION',
+  payload: {id},
+})
