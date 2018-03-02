882fb2492eb183d12aa5e989e00ae798e81a2794
Alex P <thealexpaxton@gmail.com>
2018-03-01 16:37:05 -0800

    Add info type to valid notification types

--- ui/src/shared/actions/notifications.js
+++ ui/src/shared/actions/notifications.js
@@ -1,6 +1,6 @@
 export function publishNotification(type, message, options = {once: false}) {
   // this validator is purely for development purposes. It might make sense to move this to a middleware.
-  const validTypes = ['error', 'success', 'warning']
+  const validTypes = ['error', 'success', 'warning', 'info']
   if (!validTypes.includes(type) || message === undefined) {
     console.error('handleNotification must have a valid type and text') // eslint-disable-line no-console
   }
