59fb31a1bab91deef421410a934701766dc43539
Alex P <thealexpaxton@gmail.com>
2018-03-03 18:51:42 -0800

    Remove notification shape transformer

--- ui/src/shared/actions/notifications.js
+++ ui/src/shared/actions/notifications.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-export const publishNotification = notification => ({
-  type: 'PUBLISH_NOTIFICATION',
-  payload: {notification},
-})
-
-export const dismissNotification = id => ({
-  type: 'DISMISS_NOTIFICATION',
-  payload: {id},
-})
 export const publishNotification = notification => ({
   type: 'PUBLISH_NOTIFICATION',
   payload: {notification},
