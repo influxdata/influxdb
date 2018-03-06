aaaeeeb04549e25870f755d1dbc32d65b29c568f
Andrew Watkins <watts@influxdb.com>
2018-03-06 09:56:53 -0800

    Remove gzip compression (already done on server)

--- ui/webpack/prod.config.js
+++ ui/webpack/prod.config.js
@@ -5,7 +5,6 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-const CompressionPlugin = require('compression-webpack-plugin')
 
 const package = require('../package.json')
 const dependencies = package.dependencies
@@ -148,13 +147,6 @@ const config = {
     }),
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
-    }),
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.js$|\.css$/,
-      threshold: 0,
-      minRatio: 0.8,
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', 'src', 'index.template.html'),
