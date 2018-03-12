515c452903258fd6b2ba32d350edc5b31f4d9fbd
Alex P <thealexpaxton@gmail.com>
2018-03-12 12:31:47 -0700

    Polish crosshair styles
    Also renamed some things

--- ui/src/shared/components/Crosshair.js
+++ ui/src/shared/components/Crosshair.js
@@ -1,24 +1,28 @@
 import React, {PropTypes, Component} from 'react'
 import {DYGRAPH_CONTAINER_XLABEL_MARGIN} from 'shared/constants'
+import {NULL_HOVER_TIME} from 'shared/constants/tableGraph'
+
 import classnames from 'classnames'
 
 class Crosshair extends Component {
   render() {
     const {dygraph, staticLegendHeight, hoverTime} = this.props
-    const crosshairleft = Math.round(
+    const crosshairLeft = Math.round(
       Math.max(-1000, dygraph.toDomXCoord(hoverTime)) || -1000 + 1
     )
     const crosshairHeight = `calc(100% - ${staticLegendHeight +
       DYGRAPH_CONTAINER_XLABEL_MARGIN}px)`
 
+    const crosshairHidden = hoverTime === NULL_HOVER_TIME
+
     return (
-      <div className="crosshair">
+      <div className="crosshair-container">
         <div
-          className={classnames('crosshair--crosshair', {
-            hidden: crosshairleft < 0,
+          className={classnames('crosshair', {
+            hidden: crosshairHidden,
           })}
           style={{
-            left: crosshairleft,
+            left: crosshairLeft,
             height: crosshairHeight,
             zIndex: 1999,
           }}
