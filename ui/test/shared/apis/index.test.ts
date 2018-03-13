f404eb572c2d31d75fefa12d0a055d7d0a13f07c
Brandon Farmer <bthesorceror@gmail.com>
2018-03-13 15:34:54 -0700

    WIP

--- ui/test/shared/apis/index.test.ts
+++ ui/test/shared/apis/index.test.ts
@@ -1,5 +1,5 @@
-import {createKapacitor} from 'src/shared/apis'
-import {source, createKapacitorBody} from 'mocks/dummy'
+import {createKapacitor, updateKapacitor} from 'src/shared/apis'
+import {source, kapacitor, createKapacitorBody} from 'mocks/dummy'
 import AJAX from 'src/utils/ajax'
 
 jest.mock('src/utils/ajax', () => require('mocks/utils/ajax'))
@@ -13,6 +13,22 @@ describe('Shared.Apis', () => {
         url: source.links.kapacitors,
         method: 'POST',
         data: createKapacitorBody
+      })
+    })
+  })
+
+  describe('updateKapacitor', () => {
+    it('is called with the expected body', () => {
+      const username = 'user'
+      const password = 'password'
+      const data = {username, password, ...kapacitor}
+
+      updateKapacitor(data)
+
+      expect(AJAX).toHaveBeenCalledWith({
+        url: kapacitor.links.self,
+        method: 'PATCH',
+        data,
       })
     })
   })
