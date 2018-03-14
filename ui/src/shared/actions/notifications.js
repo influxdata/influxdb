09be4453539b97948dc4dd4ff4d9f45f7380b8b2
Alex P <thealexpaxton@gmail.com>
2018-03-13 19:06:22 -0700

    Cleanup syntax in action creators

--- ui/src/shared/actions/notifications.js
+++ ui/src/shared/actions/notifications.js
@@ -1,13 +1,9 @@
-export function publishNotification(notification) {
-  return {
-    type: 'PUBLISH_NOTIFICATION',
-    payload: {notification},
-  }
-}
+export const publishNotification = notification => ({
+  type: 'PUBLISH_NOTIFICATION',
+  payload: {notification},
+})
 
-export function dismissNotification(id) {
-  return {
-    type: 'DISMISS_NOTIFICATION',
-    payload: {id},
-  }
-}
+export const dismissNotification = id => ({
+  type: 'DISMISS_NOTIFICATION',
+  payload: {id},
+})
