afa0acb63ec9009b1d9d1b91b362ec23aba3d74c
Jared Scheib <jared.scheib@gmail.com>
2018-03-14 16:06:34 -0700

    Add initial passing enzyme test for KapacitorRules render

---
+++ ui/test/kapacitor/components/KapacitorRules.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import KapacitorRules from 'src/kapacitor/components/KapacitorRules'
+
+import {source, kapacitorRules} from 'test/resources'
+
+jest.mock('src/shared/apis', () => require('mocks/shared/apis'))
+
+const setup = (override = {}) => {
+  const props = {
+    source,
+    rules: kapacitorRules,
+    hasKapacitor: true,
+    loading: false,
+    onDelete: () => {},
+    onChangeRuleStatus: () => {},
+  }
+
+  const wrapper = shallow(<KapacitorRules {...props} />)
+
+  return {
+    wrapper,
+    props,
+  }
+}
+
+describe('Kapacitor.Containers.KapacitorRules', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('rendering', () => {
+    it('renders the KapacitorRules', () => {
+      const {wrapper} = setup()
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
+})
