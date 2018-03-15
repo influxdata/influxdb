bd6674859cb23500fd24d4cfbdf5503aeef0c831
Jared Scheib <jared.scheib@gmail.com>
2018-03-14 18:44:51 -0700

    Test KapacitorRulesTable tr keys to be UUIDv4

--- ui/test/kapacitor/components/KapacitorRulesTable.test.tsx
+++ ui/test/kapacitor/components/KapacitorRulesTable.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 
+import {isUUIDv4} from 'src/utils/stringValidators'
+
 import KapacitorRulesTable from 'src/kapacitor/components/KapacitorRulesTable'
 
 import {source, kapacitorRules} from 'test/resources'
@@ -10,14 +12,14 @@ const setup = () => {
     source,
     rules: kapacitorRules,
     onDelete: () => {},
-    onChangeRuleStatus: () => {}
+    onChangeRuleStatus: () => {},
   }
 
   const wrapper = shallow(<KapacitorRulesTable {...props} />)
 
   return {
     wrapper,
-    props
+    props,
   }
 }
 
@@ -26,6 +28,14 @@ describe('Kapacitor.Components.KapacitorRulesTable', () => {
     it('renders KapacitorRulesTable', () => {
       const {wrapper} = setup()
       expect(wrapper.exists()).toBe(true)
+    })
+
+    it('renders each row with key that is a UUIDv4', () => {
+      const {wrapper} = setup()
+      wrapper
+        .find('tbody')
+        .children()
+        .forEach(child => expect(isUUIDv4(child.key())).toEqual(true))
     })
   })
 })
