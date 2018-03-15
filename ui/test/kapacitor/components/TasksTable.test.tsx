13da9bec6fa81b8497f6f07ac9880d28ac39308d
Jared Scheib <jared.scheib@gmail.com>
2018-03-14 17:06:27 -0700

    Add initial enzyme test for component TasksTable render

---
+++ ui/test/kapacitor/components/TasksTable.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import TasksTable from 'src/kapacitor/components/TasksTable'
+
+import {source, kapacitorRules} from 'test/resources'
+
+const setup = () => {
+  const props = {
+    source,
+    tasks: kapacitorRules,
+    onDelete: () => {},
+    onChangeRuleStatus: () => {}
+  }
+
+  const wrapper = shallow(<TasksTable {...props} />)
+
+  return {
+    wrapper,
+    props
+  }
+}
+
+describe('Kapacitor.Components.TasksTable', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('rendering', () => {
+    it('renders the TasksTable', () => {
+      const {wrapper} = setup()
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
+})
