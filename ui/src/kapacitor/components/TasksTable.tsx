a70e3edc3ab7f82ef59a606f1d9ff7601ac689b9
Jared Scheib <jared.scheib@gmail.com>
2018-03-16 20:55:02 -0700

    Convert TasksTable to working TypeScript

--- ui/src/kapacitor/components/TasksTable.tsx
+++ ui/src/kapacitor/components/TasksTable.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, {PureComponent, SFC, MouseEvent} from 'react'
 import {Link} from 'react-router'
 import _ from 'lodash'
 
+import {AlertRule, Source} from 'src/types'
+
 import ConfirmButton from 'src/shared/components/ConfirmButton'
 import {TASKS_TABLE} from 'src/kapacitor/constants/tableSizing'
-
 const {colName, colType, colEnabled, colActions} = TASKS_TABLE
 
-const TasksTable = ({tasks, source, onDelete, onChangeRuleStatus}) =>
+interface TasksTableProps {
+  tasks: AlertRule[]
+  source: Source
+  onChangeRuleStatus: (rule: AlertRule) => void
+  onDelete: (rule: AlertRule) => void
+}
+
+interface TaskRowProps {
+  task: AlertRule
+  source: Source
+  onChangeRuleStatus: (rule: AlertRule) => void
+  onDelete: (rule: AlertRule) => void
+}
+
+const TasksTable: SFC<TasksTableProps> = ({
+  tasks,
+  source,
+  onDelete,
+  onChangeRuleStatus,
+}) => (
   <table className="table v-center table-highlight">
     <thead>
       <tr>
@@ -34,61 +53,55 @@ const TasksTable = ({tasks, source, onDelete, onChangeRuleStatus}) =>
       })}
     </tbody>
   </table>
+)
 
 const handleDelete = (task, onDelete) => onDelete(task)
 
-const TaskRow = ({task, source, onDelete, onChangeRuleStatus}) =>
-  <tr key={task.id}>
-    <td style={{minWidth: colName}}>
-      <Link
-        className="link-success"
-        to={`/sources/${source.id}/tickscript/${task.id}`}
-      >
-        {task.name}
-      </Link>
-    </td>
-    <td style={{width: colType, textTransform: 'capitalize'}}>
-      {task.type}
-    </td>
-    <td style={{width: colEnabled}} className="text-center">
-      <div className="dark-checkbox">
-        <input
-          id={`kapacitor-task-row-task-enabled ${task.id}`}
-          className="form-control-static"
-          type="checkbox"
-          defaultChecked={task.status === 'enabled'}
-          onClick={onChangeRuleStatus(task)}
-        />
-        <label htmlFor={`kapacitor-task-row-task-enabled ${task.id}`} />
-      </div>
-    </td>
-    <td style={{width: colActions}} className="text-right">
-      <ConfirmButton
-        text="Delete"
-        type="btn-danger"
-        size="btn-xs"
-        customClass="table--show-on-row-hover"
-        confirmAction={handleDelete(task, onDelete)}
-      />
-    </td>
-  </tr>
+class TaskRow extends PureComponent<TaskRowProps> {
+  handleClickRuleStatusEnabled(_: MouseEvent<HTMLInputElement>) {
+    return (rule: AlertRule) => this.props.onChangeRuleStatus(rule)
+  }
 
-const {arrayOf, func, shape, string} = PropTypes
+  render() {
+    const {task, source, onDelete} = this.props
 
-TasksTable.propTypes = {
-  tasks: arrayOf(shape()),
-  onChangeRuleStatus: func,
-  onDelete: func,
-  source: shape({
-    id: string.isRequired,
-  }).isRequired,
-}
-
-TaskRow.propTypes = {
-  task: shape(),
-  source: shape(),
-  onChangeRuleStatus: func,
-  onDelete: func,
+    return (
+      <tr key={task.id}>
+        <td style={{minWidth: colName}}>
+          <Link
+            className="link-success"
+            to={`/sources/${source.id}/tickscript/${task.id}`}
+          >
+            {task.name}
+          </Link>
+        </td>
+        <td style={{width: colType, textTransform: 'capitalize'}}>
+          {task.type}
+        </td>
+        <td style={{width: colEnabled}} className="text-center">
+          <div className="dark-checkbox">
+            <input
+              id={`kapacitor-task-row-task-enabled ${task.id}`}
+              className="form-control-static"
+              type="checkbox"
+              defaultChecked={task.status === 'enabled'}
+              onClick={this.handleClickRuleStatusEnabled}
+            />
+            <label htmlFor={`kapacitor-task-row-task-enabled ${task.id}`} />
+          </div>
+        </td>
+        <td style={{width: colActions}} className="text-right">
+          <ConfirmButton
+            text="Delete"
+            type="btn-danger"
+            size="btn-xs"
+            customClass="table--show-on-row-hover"
+            confirmAction={handleDelete(task, onDelete)}
+          />
+        </td>
+      </tr>
+    )
+  }
 }
 
 export default TasksTable
