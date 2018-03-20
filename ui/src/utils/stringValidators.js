6002526fd6c94f9fbe2324194d32baf93abe8fbe
Jared Scheib <jared.scheib@gmail.com>
2018-03-19 17:04:43 -0700

    Remove unused UUID string validator & test

--- ui/src/utils/stringValidators.js
+++
@@ -1,6 +0,0 @@
-// uses Gajus' regex matcher from https://stackoverflow.com/questions/136505/searching-for-uuids-in-text-with-regex
-export const isUUIDv4 = str =>
-  str.length === 36 &&
-  !!str.match(
-    /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/
-  )
