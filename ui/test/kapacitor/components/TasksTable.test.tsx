6c76e2b4f4541dde2821bc31ec6f78dcce7d4f28
Jared Scheib <jared.scheib@gmail.com>
2018-03-21 16:43:14 -0700

    Refactor TasksTable test to per-test shallow
    Make dedicated describe block props

--- ui/test/kapacitor/components/TasksTable.test.tsx
+++ ui/test/kapacitor/components/TasksTable.test.tsx
@@ -5,26 +5,18 @@ import TasksTable from 'src/kapacitor/components/TasksTable'
 
 import {source, kapacitorRules} from 'test/resources'
 
-const setup = () => {
-  const props = {
-    source,
-    tasks: kapacitorRules,
-    onDelete: () => {},
-    onChangeRuleStatus: () => {}
-  }
-
-  const wrapper = shallow(<TasksTable {...props} />)
-
-  return {
-    wrapper,
-    props
-  }
-}
-
 describe('Kapacitor.Components.TasksTable', () => {
   describe('rendering', () => {
+    const props = {
+      source,
+      tasks: kapacitorRules,
+      onDelete: () => {},
+      onChangeRuleStatus: () => {}
+    }
+
     it('renders TasksTable', () => {
-      const {wrapper} = setup()
+      const wrapper = shallow(<TasksTable {...props} />)
+
       expect(wrapper.exists()).toBe(true)
     })
   })
