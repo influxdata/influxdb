a55d50fd4b09c87f40b0d272d685e0f885e81eb5
Andrew Watkins <watts@influxdb.com>
2018-03-26 16:48:14 -0700

    Wrap ifql navigation with feature flag

--- ui/src/side_nav/containers/SideNav.tsx
+++ ui/src/side_nav/containers/SideNav.tsx
@@ -139,13 +139,6 @@ class SideNav extends PureComponent<Props> {
             sourcePrefix={sourcePrefix}
           />
         ) : null}
-        <NavBlock
-          icon="cog-thick"
-          link={`${sourcePrefix}/ifql`}
-          location={location}
-        >
-          <NavHeader link={`${sourcePrefix}/ifql`} title="IFQL Builder" />
-        </NavBlock>
       </nav>
     )
   }
