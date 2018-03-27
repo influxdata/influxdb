4bd5f71cd2f848a3925fc3a8eeef42cedf3474a6
Alex P <thealexpaxton@gmail.com>
2018-03-27 15:06:58 -0700

    Remove lodash

--- ui/src/admin/components/chronograf/AllUsersTableRow.js
+++ ui/src/admin/components/chronograf/AllUsersTableRow.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import _ from 'lodash'
 
 import Tags from 'shared/components/Tags'
 import SlideToggle from 'shared/components/SlideToggle'
