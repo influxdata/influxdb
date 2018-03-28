d59f85125618bb5585a6c34ad8a3e5b842e83b37
Alex P <thealexpaxton@gmail.com>
2018-03-28 12:36:57 -0700

    Exit annotation editing mode when user leaves dashboard

--- ui/src/dashboards/containers/DashboardPage.js
+++ ui/src/dashboards/containers/DashboardPage.js
@@ -28,6 +28,8 @@ import {
   hideCellEditorOverlay,
 } from 'src/dashboards/actions/cellEditorOverlay'
 
+import {dismissEditingAnnotation} from 'src/shared/actions/annotations'
+
 import {
   setAutoRefresh,
   templateControlBarVisibilityToggled as templateControlBarVisibilityToggledAction,
@@ -109,6 +111,7 @@ class DashboardPage extends Component {
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleWindowResize, true)
+    this.props.handleDismissEditingAnnotation()
   }
 
   inView = cell => {
@@ -511,6 +514,7 @@ DashboardPage.propTypes = {
   getAnnotationsAsync: func.isRequired,
   handleShowCellEditorOverlay: func.isRequired,
   handleHideCellEditorOverlay: func.isRequired,
+  handleDismissEditingAnnotation: func.isRequired,
   selectedCell: shape({}),
   thresholdsListType: string.isRequired,
   thresholdsListColors: arrayOf(shape({}).isRequired).isRequired,
@@ -585,6 +589,10 @@ const mapDispatchToProps = dispatch => ({
   ),
   handleHideCellEditorOverlay: bindActionCreators(
     hideCellEditorOverlay,
+    dispatch
+  ),
+  handleDismissEditingAnnotation: bindActionCreators(
+    dismissEditingAnnotation,
     dispatch
   ),
 })
