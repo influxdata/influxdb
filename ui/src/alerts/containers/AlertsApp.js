c6c59bdc4ff871fb39c13f696eed988f6c6afa6f
ebb-tide <denizk@gmail.com>
2018-04-02 16:00:37 -0700

    Reverse unnecessary change

--- ui/src/alerts/containers/AlertsApp.js
+++ ui/src/alerts/containers/AlertsApp.js
@@ -118,9 +118,9 @@ class AlertsApp extends Component {
 
   renderSubComponents = () => {
     const {source, isWidget, limit} = this.props
-    const {isAlertsMaxedOut, alerts, hasKapacitor} = this.state
+    const {isAlertsMaxedOut, alerts} = this.state
 
-    return hasKapacitor ? (
+    return this.state.hasKapacitor ? (
       <AlertsTable
         source={source}
         alerts={this.state.alerts}
