7c425f328a3e94b8b7cba01c6d25495771320711
Adam <adam@influxdata.com>
2018-04-12 14:41:12 -0400

    Fixes from PR review

--- cmd/influxd/backup/backup.go
+++ cmd/influxd/backup/backup.go
@@ -11,6 +11,7 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -19,7 +20,6 @@ import (
 	"github.com/influxdata/influxdb/cmd/influxd/backup_util"
 	"github.com/influxdata/influxdb/services/snapshotter"
 	"github.com/influxdata/influxdb/tcp"
-	"math"
 )
 
 const (
@@ -515,6 +515,7 @@ func (cmd *Command) download(req *snapshotter.Request, path string) error {
 	}
 	defer f.Close()
 
+	min := 2 * time.Second
 	for i := 0; i < 10; i++ {
 		if err = func() error {
 			// Connect to snapshotter service.
@@ -542,7 +543,6 @@ func (cmd *Command) download(req *snapshotter.Request, path string) error {
 		}(); err == nil {
 			break
 		} else if err != nil {
-			min := 2 * time.Second
 			backoff := time.Duration(math.Pow(3.8, float64(i))) * time.Millisecond
 			if backoff < min {
 				backoff = min
