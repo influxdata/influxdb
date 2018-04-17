0171aa36b54bba6106f0322796d3d121a586be29
Andrew Watkins <watts@influxdb.com>
2018-04-17 09:30:50 -0700

    Introduce FuncArg test

---
+++ ui/test/ifql/components/FuncArg.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import FuncArg from 'src/ifql/components/FuncArg'
+
+const setup = () => {
+  const props = {
+    funcID: '',
+    argKey: '',
+    value: '',
+    type: '',
+    onChangeArg: () => {},
+    onGenerateScript: () => {},
+  }
+
+  const wrapper = shallow(<FuncArg {...props} />)
+
+  return {
+    wrapper,
+  }
+}
+
+describe('IFQL.Components.FuncArg', () => {
+  describe('rendering', () => {
+    it('renders without errors', () => {
+      const {wrapper} = setup()
+
+      expect(wrapper.exists()).toBe(true)
+    })
+  })
+})
