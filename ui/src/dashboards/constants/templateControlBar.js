3475373e6b8e327ea991a0e73bd6630e38709fed
Brandon Farmer <bthesorceror@gmail.com>
2018-04-18 14:05:22 -0700

    Use lodash to determine value length

--- ui/src/dashboards/constants/templateControlBar.js
+++ ui/src/dashboards/constants/templateControlBar.js
@@ -5,13 +5,7 @@ export const minDropdownWidth = 146
 export const maxDropdownWidth = 300
 export const dropdownPadding = 30
 
-const valueLength = a => {
-  if (a.value) {
-    return a.value.length
-  }
-
-  return -1
-}
+const valueLength = a => _.size(a.value)
 
 export const calculateDropdownWidth = (values = []) => {
   const longestValue = _.maxBy(values, valueLength)
