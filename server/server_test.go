9710854b2587b8061895567311698b9134a2c8f8
Jared Scheib <jared.scheib@gmail.com>
2018-04-23 16:46:16 -0700

    Comment WithContext fn for context

--- server/server_test.go
+++ server/server_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/bouk/httprouter"
 )
 
+// WithContext is a helper function to cut down on boilerplate in server test files
 func WithContext(ctx context.Context, r *http.Request, kv map[string]string) *http.Request {
 	params := make(httprouter.Params, 0, len(kv))
 	for k, v := range kv {
