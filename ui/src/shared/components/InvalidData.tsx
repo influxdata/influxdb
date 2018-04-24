3e5a27ce6f86dda28adb3b9903c096eb6dd6ebf7
Alex P <thealexpaxton@gmail.com>
2018-04-24 16:49:38 -0700

    Polish error state in line graph

--- ui/src/shared/components/InvalidData.tsx
+++ ui/src/shared/components/InvalidData.tsx
@@ -3,13 +3,12 @@ import React, {PureComponent} from 'react'
 class InvalidData extends PureComponent<{}> {
   public render() {
     return (
-      <p
-        className="data-error"
-        style={{textAlign: 'center', paddingTop: '10px'}}
-      >
-        The data returned from the query can't be visualized with this graph
-        type. Try updating the query or selecting a different graph type.
-      </p>
+      <div className="graph-empty">
+        <p>
+          The data returned from the query can't be visualized with this graph
+          type.<br />Try updating the query or selecting a different graph type.
+        </p>
+      </div>
     )
   }
 }
