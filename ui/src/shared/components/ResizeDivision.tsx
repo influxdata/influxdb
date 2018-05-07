5f8564ffa2f13e2519a541c9748bf12e40a69e11
Alex P <thealexpaxton@gmail.com>
2018-05-07 11:26:43 -0700

    Add threesizer division header with collapsible title

--- ui/src/shared/components/ResizeDivision.tsx
+++ ui/src/shared/components/ResizeDivision.tsx
@@ -1,5 +1,6 @@
 import React, {PureComponent, ReactElement, MouseEvent} from 'react'
 import classnames from 'classnames'
+import calculateSize from 'calculate-size'
 
 import {HANDLE_VERTICAL, HANDLE_HORIZONTAL} from 'src/shared/constants/index'
 
@@ -26,26 +27,49 @@ class Division extends PureComponent<Props> {
     handleDisplay: 'visible',
   }
 
+  private collapseThreshold: number = 0
+  private containerRef: HTMLElement
+
+  public componentDidMount() {
+    const {name} = this.props
+
+    if (!name) {
+      return 0
+    }
+
+    const {width} = calculateSize(name, {
+      font: '"Roboto", Helvetica, Arial, Tahoma, Verdana, sans-serif',
+      fontSize: '16px',
+      fontWeight: '500',
+    })
+    const NAME_OFFSET = 66
+
+    this.collapseThreshold = width + NAME_OFFSET
+  }
+
   public render() {
     const {name, render, draggable} = this.props
     return (
-      <>
-        <div className={this.containerClass} style={this.containerStyle}>
-          <div
-            style={this.handleStyle}
-            title={this.title}
-            draggable={draggable}
-            onDragStart={this.drag}
-            className={this.handleClass}
-            onDoubleClick={this.handleDoubleClick}
-          >
-            <div className="threesizer--title">{name}</div>
-          </div>
-          <div className={this.contentsClass} style={this.contentStyle}>
-            {render()}
-          </div>
+      <div
+        className={this.containerClass}
+        style={this.containerStyle}
+        ref={r => (this.containerRef = r)}
+      >
+        <div
+          style={this.handleStyle}
+          title={this.title}
+          draggable={draggable}
+          onDragStart={this.drag}
+          className={this.handleClass}
+          onDoubleClick={this.handleDoubleClick}
+        >
+          <div className={this.titleClass}>{name}</div>
         </div>
-      </>
+        <div className={this.contentsClass} style={this.contentStyle}>
+          {name && <div className="threesizer--header" />}
+          <div className="threesizer--body">{render()}</div>
+        </div>
+      </div>
     )
   }
 
@@ -132,6 +156,32 @@ class Division extends PureComponent<Props> {
     return classnames(`threesizer--contents ${orientation}`, {
       'no-shadows': !size,
     })
+  }
+
+  private get titleClass(): string {
+    const {orientation} = this.props
+
+    const collapsed = orientation === HANDLE_VERTICAL && this.isTitleObscured
+
+    return classnames('threesizer--title', {
+      'threesizer--collapsed': collapsed,
+      vertical: orientation === HANDLE_VERTICAL,
+      horizontal: orientation === HANDLE_HORIZONTAL,
+    })
+  }
+
+  private get isTitleObscured(): boolean {
+    if (this.props.size === 0) {
+      return true
+    }
+
+    if (!this.containerRef || this.props.size === 1) {
+      return false
+    }
+
+    const {width} = this.containerRef.getBoundingClientRect()
+
+    return width <= this.collapseThreshold
   }
 
   private get isDragging(): boolean {
