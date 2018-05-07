b8959ae7692492e00fc61a2e8c7e4a60dee3ef51
Alex P <thealexpaxton@gmail.com>
2018-05-07 16:43:50 -0700

    Move variable names into own component

--- ui/src/ifql/components/BodyBuilder.tsx
+++ ui/src/ifql/components/BodyBuilder.tsx
@@ -2,6 +2,7 @@ import React, {PureComponent} from 'react'
 import _ from 'lodash'
 
 import ExpressionNode from 'src/ifql/components/ExpressionNode'
+import VariableName from 'src/ifql/components/VariableName'
 
 import {FlatBody, Suggestion} from 'src/types/ifql'
 
@@ -22,9 +23,7 @@ class BodyBuilder extends PureComponent<Props> {
           if (d.funcs) {
             return (
               <div className="declaration" key={b.id}>
-                <div className="variable-string">
-                  <span className="variable-name">{d.name}</span>
-                </div>
+                <VariableName name={d.name} />
                 <ExpressionNode
                   key={b.id}
                   bodyID={b.id}
@@ -38,47 +37,25 @@ class BodyBuilder extends PureComponent<Props> {
 
           return (
             <div className="declaration" key={b.id}>
-              <div className="variable-string">
-                {this.colorVariableSyntax(b.source)}
-              </div>
+              <VariableName name={b.source} />
             </div>
           )
         })
       }
 
       return (
-        <ExpressionNode
-          key={b.id}
-          bodyID={b.id}
-          funcs={b.funcs}
-          funcNames={this.funcNames}
-        />
+        <div className="declaration" key={b.id}>
+          <VariableName />
+          <ExpressionNode
+            bodyID={b.id}
+            funcs={b.funcs}
+            funcNames={this.funcNames}
+          />
+        </div>
       )
     })
 
     return <div className="body-builder">{_.flatten(bodybuilder)}</div>
-  }
-
-  private colorVariableSyntax = (varString: string) => {
-    const split = varString.split('=')
-    const varName = split[0].substring(0, split[0].length - 1)
-    const varValue = split[1].substring(1)
-
-    const valueIsString = varValue.endsWith('"')
-
-    return (
-      <>
-        <span className="variable-name">{varName}</span>
-        {' = '}
-        <span
-          className={
-            valueIsString ? 'variable-value--string' : 'variable-value--number'
-          }
-        >
-          {varValue}
-        </span>
-      </>
-    )
   }
 
   private get funcNames() {
