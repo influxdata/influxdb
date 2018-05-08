5d7257494efa63bdb542e3ade4b60d210b5707ca
Andrew Watkins <watts@influxdb.com>
2018-05-08 10:08:02 -0700

    Fix copy

--- ui/src/shared/components/Tags.tsx
+++ ui/src/shared/components/Tags.tsx
@@ -11,6 +11,7 @@ interface Item {
 
 interface TagsProps {
   tags: Item[]
+  confirmText?: string
   onDeleteTag?: (item: Item) => void
   addMenuItems?: Item[]
   addMenuChoose?: (item: Item) => void
@@ -21,11 +22,19 @@ const Tags: SFC<TagsProps> = ({
   onDeleteTag,
   addMenuItems,
   addMenuChoose,
+  confirmText,
 }) => {
   return (
     <div className="input-tag-list">
       {tags.map(item => {
-        return <Tag key={uuid.v4()} item={item} onDelete={onDeleteTag} />
+        return (
+          <Tag
+            key={uuid.v4()}
+            item={item}
+            onDelete={onDeleteTag}
+            confirmText={confirmText}
+          />
+        )
       })}
       {addMenuItems && addMenuItems.length && addMenuChoose ? (
         <TagsAddButton items={addMenuItems} onChoose={addMenuChoose} />
@@ -35,36 +44,32 @@ const Tags: SFC<TagsProps> = ({
 }
 
 interface TagProps {
+  confirmText?: string
   item: Item
   onDelete: (item: Item) => void
 }
 
 @ErrorHandling
 class Tag extends PureComponent<TagProps> {
+  public static defaultProps: Partial<TagProps> = {
+    confirmText: 'Delete',
+  }
+
   public render() {
-    const {item} = this.props
+    const {item, confirmText} = this.props
     return (
       <span key={uuid.v4()} className="input-tag--item">
         <span>{item.text || item.name || item}</span>
         <ConfirmButton
           icon="remove"
           size="btn-xs"
-          customClass="input-tag--remove"
           square={true}
-          confirmText={this.confirmText}
+          confirmText={confirmText}
+          customClass="input-tag--remove"
           confirmAction={this.handleClickDelete(item)}
         />
       </span>
     )
-  }
-
-  private get confirmText(): string {
-    const {item} = this.props
-    if (item.name || item.text) {
-      return `Delete ${item.name || item.text}?`
-    }
-
-    return 'Delete?'
   }
 
   private handleClickDelete = item => () => {
