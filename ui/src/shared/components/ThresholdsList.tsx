7e7b163b037fa0828ff848f1c89cc1f14c3cbeea
ebb-tide <denizk@gmail.com>
2018-05-09 14:47:18 -0700

    Convert ThresholdsList to TS

--- ui/src/shared/components/ThresholdsList.tsx
+++ ui/src/shared/components/ThresholdsList.tsx
@@ -1,5 +1,4 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import React, {PureComponent} from 'react'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
@@ -7,10 +6,10 @@ import _ from 'lodash'
 import uuid from 'uuid'
 
 import Threshold from 'src/dashboards/components/Threshold'
-import ColorDropdown from 'shared/components/ColorDropdown'
+import ColorDropdown from 'src/shared/components/ColorDropdown'
 
 import {updateThresholdsListColors} from 'src/dashboards/actions/cellEditorOverlay'
-import {colorsNumberSchema} from 'shared/schemas'
+import {ColorNumber} from 'src/types/colors'
 
 import {
   THRESHOLD_COLORS,
@@ -18,7 +17,7 @@ import {
   DEFAULT_VALUE_MAX,
   MAX_THRESHOLDS,
   THRESHOLD_TYPE_BASE,
-} from 'shared/constants/thresholds'
+} from 'src/shared/constants/thresholds'
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
 const formatColor = color => {
@@ -26,93 +25,18 @@ const formatColor = color => {
   return {hex, name}
 }
 
+interface Props {
+  onResetFocus: () => void
+  showListHeading: boolean
+  thresholdsListType: string
+  thresholdsListColors: ColorNumber[]
+  handleUpdateThresholdsListColors: (c: ColorNumber[]) => void
+}
+
 @ErrorHandling
-class ThresholdsList extends Component {
-  handleAddThreshold = () => {
-    const {
-      thresholdsListColors,
-      thresholdsListType,
-      handleUpdateThresholdsListColors,
-      onResetFocus,
-    } = this.props
-
-    const randomColor = _.random(0, THRESHOLD_COLORS.length - 1)
-
-    const maxValue = DEFAULT_VALUE_MIN
-    const minValue = DEFAULT_VALUE_MAX
-
-    let randomValue = _.round(_.random(minValue, maxValue, true), 2)
-
-    if (thresholdsListColors.length > 0) {
-      const colorsValues = _.mapValues(thresholdsListColors, 'value')
-      do {
-        randomValue = _.round(_.random(minValue, maxValue, true), 2)
-      } while (_.includes(colorsValues, randomValue))
-    }
-
-    const newThreshold = {
-      type: thresholdsListType,
-      id: uuid.v4(),
-      value: randomValue,
-      hex: THRESHOLD_COLORS[randomColor].hex,
-      name: THRESHOLD_COLORS[randomColor].name,
-    }
-
-    const updatedColors = _.sortBy(
-      [...thresholdsListColors, newThreshold],
-      color => color.value
-    )
-
-    handleUpdateThresholdsListColors(updatedColors)
-    onResetFocus()
-  }
-
-  handleDeleteThreshold = threshold => {
-    const {
-      handleUpdateThresholdsListColors,
-      onResetFocus,
-      thresholdsListColors,
-    } = this.props
-    const updatedThresholdsListColors = thresholdsListColors.filter(
-      color => color.id !== threshold.id
-    )
-    const sortedColors = _.sortBy(
-      updatedThresholdsListColors,
-      color => color.value
-    )
-
-    handleUpdateThresholdsListColors(sortedColors)
-    onResetFocus()
-  }
-
-  handleChooseColor = threshold => chosenColor => {
-    const {handleUpdateThresholdsListColors} = this.props
-
-    const thresholdsListColors = this.props.thresholdsListColors.map(
-      color =>
-        color.id === threshold.id
-          ? {...color, hex: chosenColor.hex, name: chosenColor.name}
-          : color
-    )
-
-    handleUpdateThresholdsListColors(thresholdsListColors)
-  }
-
-  handleUpdateColorValue = (threshold, value) => {
-    const {handleUpdateThresholdsListColors} = this.props
-
-    const thresholdsListColors = this.props.thresholdsListColors.map(
-      color => (color.id === threshold.id ? {...color, value} : color)
-    )
-
-    handleUpdateThresholdsListColors(thresholdsListColors)
-  }
-
-  handleValidateColorValue = (threshold, targetValue) => {
-    const {thresholdsListColors} = this.props
-    const sortedColors = _.sortBy(thresholdsListColors, color => color.value)
-
-    return !sortedColors.some(color => color.value === targetValue)
+class ThresholdsList extends PureComponent<Props> {
+  public static defaultProps: Partial<Props> = {
+    showListHeading: false,
   }
 
   get sortedColors() {
@@ -122,7 +46,7 @@ class ThresholdsList extends Component {
     return sortedColors
   }
 
-  render() {
+  public render() {
     const {thresholdsListColors, showListHeading} = this.props
     const disableAddThreshold = thresholdsListColors.length > MAX_THRESHOLDS
 
@@ -161,24 +85,102 @@ class ThresholdsList extends Component {
                 onValidateColorValue={this.handleValidateColorValue}
                 onUpdateColorValue={this.handleUpdateColorValue}
                 onDeleteThreshold={this.handleDeleteThreshold}
+                disableMaxColor={false}
+                isMin={false}
+                isMax={false}
               />
             )
         )}
       </div>
     )
   }
-}
-const {bool, func, string} = PropTypes
 
-ThresholdsList.defaultProps = {
-  showListHeading: false,
-}
-ThresholdsList.propTypes = {
-  thresholdsListType: string.isRequired,
-  thresholdsListColors: colorsNumberSchema.isRequired,
-  handleUpdateThresholdsListColors: func.isRequired,
-  onResetFocus: func.isRequired,
-  showListHeading: bool,
+  private handleAddThreshold = () => {
+    const {
+      thresholdsListColors,
+      thresholdsListType,
+      handleUpdateThresholdsListColors,
+      onResetFocus,
+    } = this.props
+
+    const randomColor = _.random(0, THRESHOLD_COLORS.length - 1)
+
+    const maxValue = DEFAULT_VALUE_MIN
+    const minValue = DEFAULT_VALUE_MAX
+
+    let randomValue = _.round(_.random(minValue, maxValue, true), 2)
+
+    if (thresholdsListColors.length > 0) {
+      const colorsValues = _.mapValues(thresholdsListColors, 'value')
+      do {
+        randomValue = _.round(_.random(minValue, maxValue, true), 2)
+      } while (_.includes(colorsValues, randomValue))
+    }
+
+    const newThreshold = {
+      type: thresholdsListType,
+      id: uuid.v4(),
+      value: randomValue,
+      hex: THRESHOLD_COLORS[randomColor].hex,
+      name: THRESHOLD_COLORS[randomColor].name,
+    }
+
+    const updatedColors = _.sortBy(
+      [...thresholdsListColors, newThreshold],
+      color => color.value
+    )
+
+    handleUpdateThresholdsListColors(updatedColors)
+    onResetFocus()
+  }
+
+  private handleDeleteThreshold = threshold => {
+    const {
+      handleUpdateThresholdsListColors,
+      onResetFocus,
+      thresholdsListColors,
+    } = this.props
+    const updatedThresholdsListColors = thresholdsListColors.filter(
+      color => color.id !== threshold.id
+    )
+    const sortedColors = _.sortBy(
+      updatedThresholdsListColors,
+      color => color.value
+    )
+
+    handleUpdateThresholdsListColors(sortedColors)
+    onResetFocus()
+  }
+
+  private handleChooseColor = threshold => chosenColor => {
+    const {handleUpdateThresholdsListColors} = this.props
+
+    const thresholdsListColors = this.props.thresholdsListColors.map(
+      color =>
+        color.id === threshold.id
+          ? {...color, hex: chosenColor.hex, name: chosenColor.name}
+          : color
+    )
+
+    handleUpdateThresholdsListColors(thresholdsListColors)
+  }
+
+  private handleUpdateColorValue = (threshold, value) => {
+    const {handleUpdateThresholdsListColors} = this.props
+
+    const thresholdsListColors = this.props.thresholdsListColors.map(
+      color => (color.id === threshold.id ? {...color, value} : color)
+    )
+
+    handleUpdateThresholdsListColors(thresholdsListColors)
+  }
+
+  private handleValidateColorValue = (__, targetValue) => {
+    const {thresholdsListColors} = this.props
+    const sortedColors = _.sortBy(thresholdsListColors, color => color.value)
+
+    return !sortedColors.some(color => color.value === targetValue)
+  }
 }
 
 const mapStateToProps = ({
