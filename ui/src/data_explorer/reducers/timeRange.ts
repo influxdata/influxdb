2e9689b1706eabaf1b26cf09cdaf52d0f68db89d
Andrew Watkins <watts@influxdb.com>
2018-05-11 11:30:26 -0700

    Convert time range reducer

--- ui/src/data_explorer/reducers/timeRange.ts
+++ ui/src/data_explorer/reducers/timeRange.ts
@@ -1,4 +1,5 @@
 import {timeRanges} from 'src/shared/data/timeRanges'
+import {TimeRange} from 'src/types'
 
 const {lower, upper} = timeRanges.find(tr => tr.lower === 'now() - 1h')
 
@@ -7,7 +8,18 @@ const initialState = {
   lower,
 }
 
-export default function timeRange(state = initialState, action) {
+type State = Readonly<TimeRange>
+
+interface ActionSetTimeRange {
+  type: 'DE_SET_TIME_RANGE'
+  payload: {
+    bounds: TimeRange
+  }
+}
+
+type Action = ActionSetTimeRange
+
+const timeRange = (state: State = initialState, action: Action): State => {
   switch (action.type) {
     case 'DE_SET_TIME_RANGE': {
       const {bounds} = action.payload
@@ -17,3 +29,5 @@ export default function timeRange(state = initialState, action) {
   }
   return state
 }
+
+export default timeRange
