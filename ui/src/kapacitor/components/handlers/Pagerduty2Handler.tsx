8f5ebbcbe18e4761f1848b3901154349ce4aa44c
Jared Scheib <jared.scheib@gmail.com>
2018-05-14 16:48:52 -0700

    Remove ternaries from PagerDuty2Handler for ease of debugging

--- ui/src/kapacitor/components/handlers/Pagerduty2Handler.tsx
+++ ui/src/kapacitor/components/handlers/Pagerduty2Handler.tsx
@@ -16,35 +16,44 @@ const Pagerduty2Handler: SFC<Props> = ({
   handleModifyHandler,
   onGoToConfig,
   validationError,
-}) =>
-  selectedHandler.enabled ? (
-    <div className="endpoint-tab-contents">
-      <div className="endpoint-tab--parameters">
-        <h4 className="u-flex u-jc-space-between">
-          Parameters from Kapacitor Configuration
-          <div className="btn btn-default btn-sm" onClick={onGoToConfig}>
-            <span className="icon cog-thick" />
-            {validationError
-              ? 'Exit this Rule and Edit Configuration'
-              : 'Save this Rule and Edit Configuration'}
-          </div>
-        </h4>
-        <HandlerInput
-          selectedHandler={selectedHandler}
-          handleModifyHandler={handleModifyHandler}
-          fieldName="routingKey"
-          fieldDisplay="Routing Key:"
-          placeholder="ex: routing_key"
-          redacted={true}
-          fieldColumns="col-md-12"
-        />
+}) => {
+  if (selectedHandler.enabled) {
+    let goToConfigText
+    if (validationError) {
+      goToConfigText = 'Exit this Rule and Edit Configuration'
+    } else {
+      goToConfigText = 'Save this Rule and Edit Configuration'
+    }
+    return (
+      <div className="endpoint-tab-contents">
+        <div className="endpoint-tab--parameters">
+          <h4 className="u-flex u-jc-space-between">
+            Parameters from Kapacitor Configuration
+            <div className="btn btn-default btn-sm" onClick={onGoToConfig}>
+              <span className="icon cog-thick" />
+              {goToConfigText}
+            </div>
+          </h4>
+          <HandlerInput
+            selectedHandler={selectedHandler}
+            handleModifyHandler={handleModifyHandler}
+            fieldName="routingKey"
+            fieldDisplay="Routing Key:"
+            placeholder="ex: routing_key"
+            redacted={true}
+            fieldColumns="col-md-12"
+          />
+        </div>
       </div>
-    </div>
-  ) : (
+    )
+  }
+
+  return (
     <HandlerEmpty
       onGoToConfig={onGoToConfig}
       validationError={validationError}
     />
   )
+}
 
 export default Pagerduty2Handler
