bce90535e7ba25eba2703518dbe452d300b42be9
Alex P <thealexpaxton@gmail.com>
2018-05-14 16:40:24 -0700

    Convert ThresholdsListTypeToggle to TypeScript

--- ui/src/shared/components/ThresholdsListTypeToggle.tsx
+++ ui/src/shared/components/ThresholdsListTypeToggle.tsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
@@ -8,36 +7,32 @@ import {updateThresholdsListType} from 'src/dashboards/actions/cellEditorOverlay
 import {
   THRESHOLD_TYPE_TEXT,
   THRESHOLD_TYPE_BG,
-} from 'shared/constants/thresholds'
+} from 'src/shared/constants/thresholds'
 import {ErrorHandling} from 'src/shared/decorators/errors'
 
+interface Props {
+  containerClass: string
+  thresholdsListType: string
+  handleUpdateThresholdsListType: (newType: string) => void
+}
+
 @ErrorHandling
-class ThresholdsListTypeToggle extends Component {
-  handleToggleThresholdsListType = newType => () => {
-    const {handleUpdateThresholdsListType} = this.props
-
-    handleUpdateThresholdsListType(newType)
-  }
-
-  render() {
-    const {thresholdsListType, containerClass} = this.props
+class ThresholdsListTypeToggle extends Component<Props> {
+  public render() {
+    const {containerClass} = this.props
 
     return (
       <div className={containerClass}>
         <label>Threshold Coloring</label>
         <ul className="nav nav-tablist nav-tablist-sm">
           <li
-            className={`${
-              thresholdsListType === THRESHOLD_TYPE_BG ? 'active' : ''
-            }`}
+            className={this.bgTabClassName}
             onClick={this.handleToggleThresholdsListType(THRESHOLD_TYPE_BG)}
           >
             Background
           </li>
           <li
-            className={`${
-              thresholdsListType === THRESHOLD_TYPE_TEXT ? 'active' : ''
-            }`}
+            className={this.textTabClassName}
             onClick={this.handleToggleThresholdsListType(THRESHOLD_TYPE_TEXT)}
           >
             Text
@@ -46,13 +41,32 @@ class ThresholdsListTypeToggle extends Component {
       </div>
     )
   }
-}
-const {func, string} = PropTypes
 
-ThresholdsListTypeToggle.propTypes = {
-  thresholdsListType: string.isRequired,
-  handleUpdateThresholdsListType: func.isRequired,
-  containerClass: string.isRequired,
+  private get bgTabClassName(): string {
+    const {thresholdsListType} = this.props
+
+    if (thresholdsListType === THRESHOLD_TYPE_BG) {
+      return 'active'
+    }
+
+    return ''
+  }
+
+  private get textTabClassName(): string {
+    const {thresholdsListType} = this.props
+
+    if (thresholdsListType === THRESHOLD_TYPE_TEXT) {
+      return 'active'
+    }
+
+    return ''
+  }
+
+  private handleToggleThresholdsListType = (newType: string) => (): void => {
+    const {handleUpdateThresholdsListType} = this.props
+
+    handleUpdateThresholdsListType(newType)
+  }
 }
 
 const mapStateToProps = ({cellEditorOverlay: {thresholdsListType}}) => ({
