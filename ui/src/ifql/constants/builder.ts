4538e05710e65e67eb9a369d867974caa900fb7c
ebb-tide <denizk@gmail.com>
2018-05-17 14:07:22 -0700

    Improve default join query

--- ui/src/ifql/constants/builder.ts
+++ ui/src/ifql/constants/builder.ts
@@ -1,2 +1,2 @@
 export const NEW_FROM = `from(db: "pick a db")\n\t|> filter(fn: (r) => r.tag == "value")\n\t|> range(start: -1m)`
-export const NEW_JOIN = `join(tables: {table1:table1, table2:table2}, on:["host"], fn: (tables) => tables.table1["_value"] + tables.table2["_value"])`
+export const NEW_JOIN = `join(tables: {fil:fil, tele:tele}, on:["host"], fn: (tables) => tables.fil["_value"] + tables.tele["_value"])`
