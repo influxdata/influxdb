357cfe2085a9adb38b6916a6816fc70a088da0ba
Brandon Farmer <bthesorceror@gmail.com>
2018-05-18 15:45:34 -0700

    Update changelog

--- ui/src/kapacitor/constants/index.ts
+++ ui/src/kapacitor/constants/index.ts
@@ -242,7 +242,7 @@ export const HANDLERS_TO_RULE: FieldsFromAllAlerts = {
     'service',
   ],
   hipChat: ['room'],
-  kafka: ['cluster', 'topic', 'template'],
+  kafka: ['id', 'cluster', 'topic', 'template'],
   opsGenie: ['teams', 'recipients'],
   opsGenie2: ['teams', 'recipients'],
   pagerDuty: [],
