559d42c26a7ff92f53dbf17b78f1d21c7070212b
Jared Scheib <jared.scheib@gmail.com>
2018-05-21 16:22:53 -0700

    Correct spelling of 'fallack' to 'fallback' in getNested

--- ui/src/utils/wrappers.ts
+++ ui/src/utils/wrappers.ts
@@ -1,5 +1,5 @@
 import _ from 'lodash'
 
-export function getNested<T = any>(obj: any, path: string, fallack: T): T {
-  return _.get<T>(obj, path, fallack)
+export function getNested<T = any>(obj: any, path: string, fallback: T): T {
+  return _.get<T>(obj, path, fallback)
 }
