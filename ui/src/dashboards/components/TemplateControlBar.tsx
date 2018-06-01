0a2d817586fa6011702275b47f900995a6584531
Jared Scheib <jared.scheib@gmail.com>
2018-05-31 18:00:18 -0700

    Prevent unnecessary TemplateControlBar re-render so TemplateControlDropdowns stay open
    Convert TemplateControlBar to ES6 class to use shouldComponentUpdate

--- ui/src/dashboards/components/TemplateControlBar.tsx
+++ ui/src/dashboards/components/TemplateControlBar.tsx
@@ -1,4 +1,5 @@
-import React, {SFC} from 'react'
+import React, {Component} from 'react'
+import _ from 'lodash'
 
 import classnames from 'classnames'
 import uuid from 'uuid'
@@ -16,44 +17,55 @@ interface Props {
   onSelectTemplate: (id: string) => void
 }
 
-const TemplateControlBar: SFC<Props> = ({
-  isOpen,
-  templates,
-  onSelectTemplate,
-  onOpenTemplateManager,
-  meRole,
-  isUsingAuth,
-}) => (
-  <div className={classnames('template-control-bar', {show: isOpen})}>
-    <div className="template-control--container">
-      <div className="template-control--controls">
-        {templates && templates.length ? (
-          templates.map(template => (
-            <TemplateControlDropdown
-              key={uuid.v4()}
-              meRole={meRole}
-              isUsingAuth={isUsingAuth}
-              template={template}
-              onSelectTemplate={onSelectTemplate}
-            />
-          ))
-        ) : (
-          <div className="template-control--empty" data-test="empty-state">
-            This dashboard does not have any <strong>Template Variables</strong>
+class TemplateControlBar extends Component<Props> {
+  public shouldComponentUpdate(nextProps) {
+    return !_.isEqual(this.props, nextProps)
+  }
+
+  public render() {
+    const {
+      isOpen,
+      templates,
+      onSelectTemplate,
+      onOpenTemplateManager,
+      meRole,
+      isUsingAuth,
+    } = this.props
+
+    return (
+      <div className={classnames('template-control-bar', {show: isOpen})}>
+        <div className="template-control--container">
+          <div className="template-control--controls">
+            {templates && templates.length ? (
+              templates.map(template => (
+                <TemplateControlDropdown
+                  key={uuid.v4()}
+                  meRole={meRole}
+                  isUsingAuth={isUsingAuth}
+                  template={template}
+                  onSelectTemplate={onSelectTemplate}
+                />
+              ))
+            ) : (
+              <div className="template-control--empty" data-test="empty-state">
+                This dashboard does not have any{' '}
+                <strong>Template Variables</strong>
+              </div>
+            )}
           </div>
-        )}
+          <Authorized requiredRole={EDITOR_ROLE}>
+            <button
+              className="btn btn-primary btn-sm template-control--manage"
+              onClick={onOpenTemplateManager}
+            >
+              <span className="icon cog-thick" />
+              Manage
+            </button>
+          </Authorized>
+        </div>
       </div>
-      <Authorized requiredRole={EDITOR_ROLE}>
-        <button
-          className="btn btn-primary btn-sm template-control--manage"
-          onClick={onOpenTemplateManager}
-        >
-          <span className="icon cog-thick" />
-          Manage
-        </button>
-      </Authorized>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default TemplateControlBar
