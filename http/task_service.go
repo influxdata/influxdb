e68b635402f4071200d20d0512a3b0d06fdf1f0e
Jade McGough <jade@thezets.com>
2018-06-07 11:45:43 -0700

    feat(http): add single task http handlers

--- http/task_service.go
+++ http/task_service.go
@@ -22,43 +22,12 @@ func NewTaskHandler() *TaskHandler {
 		Router: httprouter.New(),
 	}
 
-	h.HandlerFunc("POST", "/v1/tasks", h.handlePostTask)
 	h.HandlerFunc("GET", "/v1/tasks", h.handleGetTasks)
+	h.HandlerFunc("POST", "/v1/tasks", h.handlePostTask)
+	h.HandlerFunc("GET", "/v1/tasks/:id", h.handleGetTask)
+	h.HandlerFunc("PATCH", "/v1/tasks/:id", h.handleUpdateTask)
+	h.HandlerFunc("DELETE", "/v1/tasks/:id", h.handleDeleteTask)
 	return h
-}
-
-func (h *TaskHandler) handlePostTask(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-
-	req, err := decodePostTaskRequest(ctx, r)
-	if err != nil {
-		kerrors.EncodeHTTP(ctx, err, w)
-		return
-	}
-
-	if err := h.TaskService.CreateTask(ctx, req.Task); err != nil {
-		kerrors.EncodeHTTP(ctx, err, w)
-		return
-	}
-
-	if err := encodeResponse(ctx, w, http.StatusCreated, req.Task); err != nil {
-		kerrors.EncodeHTTP(ctx, err, w)
-		return
-	}
-}
-
-type postTaskRequest struct {
-	Task *platform.Task
-}
-
-func decodePostTaskRequest(ctx context.Context, r *http.Request) (*postTaskRequest, error) {
-	task := &platform.Task{}
-	if err := json.NewDecoder(r.Body).Decode(task); err != nil {
-		return nil, err
-	}
-	return &postTaskRequest{
-		Task: task,
-	}, nil
 }
 
 func (h *TaskHandler) handleGetTasks(w http.ResponseWriter, r *http.Request) {
@@ -112,4 +81,169 @@ func decodeGetTasksRequest(ctx context.Context, r *http.Request) (*getTasksReque
 	}
 
 	return req, nil
+}
+
+func (h *TaskHandler) handlePostTask(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req, err := decodePostTaskRequest(ctx, r)
+	if err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	if err := h.TaskService.CreateTask(ctx, req.Task); err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	if err := encodeResponse(ctx, w, http.StatusCreated, req.Task); err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+}
+
+type postTaskRequest struct {
+	Task *platform.Task
+}
+
+func decodePostTaskRequest(ctx context.Context, r *http.Request) (*postTaskRequest, error) {
+	task := &platform.Task{}
+	if err := json.NewDecoder(r.Body).Decode(task); err != nil {
+		return nil, err
+	}
+	return &postTaskRequest{
+		Task: task,
+	}, nil
+}
+
+func (h *TaskHandler) handleGetTask(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req, err := decodeGetTaskRequest(ctx, r)
+	if err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	task, err := h.TaskService.FindTaskByID(ctx, req.TaskID)
+	if err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	if err := encodeResponse(ctx, w, http.StatusOK, task); err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+}
+
+type getTaskRequest struct {
+	TaskID platform.ID
+}
+
+func decodeGetTaskRequest(ctx context.Context, r *http.Request) (*getTaskRequest, error) {
+	params := httprouter.ParamsFromContext(ctx)
+	id := params.ByName("id")
+	if id == "" {
+		return nil, kerrors.InvalidDataf("url missing id")
+	}
+
+	var i platform.ID
+	if err := i.DecodeFromString(id); err != nil {
+		return nil, err
+	}
+
+	req := &getTaskRequest{
+		TaskID: i,
+	}
+
+	return req, nil
+}
+
+func (h *TaskHandler) handleUpdateTask(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req, err := decodeUpdateTaskRequest(ctx, r)
+	if err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	task, err := h.TaskService.UpdateTask(ctx, req.TaskID, req.Update)
+	if err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	if err := encodeResponse(ctx, w, http.StatusOK, task); err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+}
+
+type updateTaskRequest struct {
+	Update platform.TaskUpdate
+	TaskID platform.ID
+}
+
+func decodeUpdateTaskRequest(ctx context.Context, r *http.Request) (*updateTaskRequest, error) {
+	params := httprouter.ParamsFromContext(ctx)
+	id := params.ByName("id")
+	if id == "" {
+		return nil, kerrors.InvalidDataf("url missing id")
+	}
+
+	var i platform.ID
+	if err := i.DecodeFromString(id); err != nil {
+		return nil, err
+	}
+
+	var upd platform.TaskUpdate
+	if err := json.NewDecoder(r.Body).Decode(&upd); err != nil {
+		return nil, err
+	}
+
+	return &updateTaskRequest{
+		Update: upd,
+		TaskID: i,
+	}, nil
+}
+
+func (h *TaskHandler) handleDeleteTask(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	req, err := decodeDeleteTaskRequest(ctx, r)
+	if err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	if err := h.TaskService.DeleteTask(ctx, req.TaskID); err != nil {
+		kerrors.EncodeHTTP(ctx, err, w)
+		return
+	}
+
+	w.WriteHeader(http.StatusAccepted)
+}
+
+type deleteTaskRequest struct {
+	TaskID platform.ID
+}
+
+func decodeDeleteTaskRequest(ctx context.Context, r *http.Request) (*deleteTaskRequest, error) {
+	params := httprouter.ParamsFromContext(ctx)
+	id := params.ByName("id")
+	if id == "" {
+		return nil, kerrors.InvalidDataf("url missing id")
+	}
+
+	var i platform.ID
+	if err := i.DecodeFromString(id); err != nil {
+		return nil, err
+	}
+
+	return &deleteTaskRequest{
+		TaskID: i,
+	}, nil
 }
--- task.go
+++ task.go
@@ -15,12 +15,28 @@ type Task struct {
 
 // TaskService represents a service for managing one-off and recurring tasks.
 type TaskService interface {
-	// Creates a new task
-	CreateTask(ctx context.Context, t *Task) error
+	FindTaskByID(ctx context.Context, id ID) (*Task, error)
+
+	// Returns a task that matches filter.
+	FindTask(ctx context.Context, filter TaskFilter) (*Task, error)
 
 	// Returns a list of tasks that match a filter (limit 100) and the total count
 	// of matching tasks.
 	FindTasks(ctx context.Context, filter TaskFilter) ([]*Task, int, error)
+
+	// Creates a new task
+	CreateTask(ctx context.Context, t *Task) error
+
+	// Updates a single task with changeset
+	UpdateTask(ctx context.Context, id ID, upd TaskUpdate) (*Task, error)
+
+	// Removes a task by ID and purges all associated data and scheduled runs
+	DeleteTask(ctx context.Context, id ID) error
+}
+
+// TaskUpdate represents updates to a task
+type TaskUpdate struct {
+	Name *string `json:"name"`
 }
 
 // TaskFilter represents a set of filters that restrict the returned results
