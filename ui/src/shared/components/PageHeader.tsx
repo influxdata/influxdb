b160ec6ed509ba726331b1f9b9e13dfbc5897d59
Alex P <thealexpaxton@gmail.com>
2018-06-13 13:28:48 -0700

    Shorten name of prop
    Also fixing a css classname typo

--- ui/src/shared/components/PageHeader.tsx
+++ ui/src/shared/components/PageHeader.tsx
@@ -3,27 +3,27 @@ import classnames from 'classnames'
 import Title from 'src/shared/components/PageHeaderTitle'
 
 interface Props {
-  pageTitle?: string
+  title?: string
   renderTitle?: () => ReactElement<any>
   renderOptions?: () => ReactElement<any>
   fullWidth?: boolean
 }
 
 const PageHeader: SFC<Props> = ({
-  pageTitle,
+  title,
   renderTitle,
   renderOptions,
   fullWidth,
 }) => {
-  if (!pageTitle && !renderTitle) {
-    console.error('PageHeader requires either PageTitle or RenderTitle props')
+  if (!title && !renderTitle) {
+    console.error('PageHeader requires either title or RenderTitle props')
   }
 
   const className = classnames('page-header', {'full-width': fullWidth})
   let renderLeft = renderTitle
 
   if (!renderTitle) {
-    renderLeft = () => <Title title={pageTitle} />
+    renderLeft = () => <Title title={title} />
   }
 
   return (
@@ -31,7 +31,7 @@ const PageHeader: SFC<Props> = ({
       <div className="page-header--container">
         <div className="page-header--left">{renderLeft()}</div>
         {renderOptions && (
-          <div className="page-header--Options">{renderOptions()}</div>
+          <div className="page-header--right">{renderOptions()}</div>
         )}
       </div>
     </div>
