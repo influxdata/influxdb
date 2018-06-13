ed2db34454f33c3c14304a5e24c023b1b0e421a4
Alex P <thealexpaxton@gmail.com>
2018-06-13 11:56:29 -0700

    Make page header accept either a string title or a render prop

--- ui/src/shared/components/PageHeader.tsx
+++ ui/src/shared/components/PageHeader.tsx
@@ -1,21 +1,37 @@
 import React, {SFC, ReactElement} from 'react'
 import classnames from 'classnames'
+import Title from 'src/shared/components/PageHeaderTitle'
 
 interface Props {
-  renderLeft: () => ReactElement<any>
-  renderRight?: () => ReactElement<any>
+  pageTitle?: string
+  renderTitle?: () => ReactElement<any>
+  renderOptions?: () => ReactElement<any>
   fullWidth: boolean
 }
 
-const PageHeader: SFC<Props> = ({renderLeft, renderRight, fullWidth}) => {
+const PageHeader: SFC<Props> = ({
+  pageTitle,
+  renderTitle,
+  renderOptions,
+  fullWidth,
+}) => {
+  if (!pageTitle && !renderTitle) {
+    console.error('PageHeader requires either PageTitle or RenderTitle props')
+  }
+
   const className = classnames('page-header', {'full-width': fullWidth})
+  let renderLeft = renderTitle
+
+  if (!renderTitle) {
+    renderLeft = () => <Title title={pageTitle} />
+  }
 
   return (
     <div className={className}>
       <div className="page-header--container">
         <div className="page-header--left">{renderLeft()}</div>
-        {renderRight && (
-          <div className="page-header--right">{renderRight()}</div>
+        {renderOptions && (
+          <div className="page-header--Options">{renderOptions()}</div>
         )}
       </div>
     </div>
