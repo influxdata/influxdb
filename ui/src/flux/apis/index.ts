590e8aa690f472a06d9be00eeb0cc0a4298089eb
Andrew Watkins <watts@influxdb.com>
2018-06-14 11:50:38 -0700

    Remove dead code

--- ui/src/flux/apis/index.ts
+++ ui/src/flux/apis/index.ts
@@ -78,41 +78,6 @@ export const getTimeSeries = async (
   }
 }
 
-// TODO: replace with actual requests to Flux daemon
-export const getDatabases = async () => {
-  try {
-    const response = {data: {dbs: ['telegraf', 'chronograf', '_internal']}}
-    const {data} = await Promise.resolve(response)
-
-    return data.dbs
-  } catch (error) {
-    console.error('Could not get databases', error)
-    throw error
-  }
-}
-
-export const getTags = async () => {
-  try {
-    const response = {data: {tags: ['tk1', 'tk2', 'tk3']}}
-    const {data} = await Promise.resolve(response)
-    return data.tags
-  } catch (error) {
-    console.error('Could not get tags', error)
-    throw error
-  }
-}
-
-export const getTagValues = async () => {
-  try {
-    const response = {data: {values: ['tv1', 'tv2', 'tv3']}}
-    const {data} = await Promise.resolve(response)
-    return data.values
-  } catch (error) {
-    console.error('Could not get tag values', error)
-    throw error
-  }
-}
-
 export const updateScript = async (service: Service, script: string) => {
   const updates = {...service, metadata: {script}}
 
