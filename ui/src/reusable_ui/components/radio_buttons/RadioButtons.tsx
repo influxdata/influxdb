f4f6a6b033728fcee4897e1fce88f114a4fd8c22
Alex P <thealexpaxton@gmail.com>
2018-06-29 16:53:17 -0700

    Introduce Radio Buttons component
    No styles yet

---
+++ ui/src/reusable_ui/components/radio_buttons/RadioButtons.tsx
@@ -0,0 +1,83 @@
+import React, {Component} from 'react'
+import classnames from 'classnames'
+import _ from 'lodash'
+
+import {ComponentColor, ComponentSize, ButtonShape} from 'src/reusable_ui/types'
+import {
+  COMPONENT_COLORS,
+  COMPONENT_SIZES,
+  BUTTON_SHAPES,
+} from 'src/reusable_ui/constants'
+
+interface RadioButton {
+  text: string
+  disabled?: boolean
+}
+
+interface Props {
+  color?: ComponentColor
+  size?: ComponentSize
+  shape?: ButtonShape
+  buttons: RadioButton[]
+  activeButton: RadioButton
+  onChange: (RadioButton) => void
+}
+
+class RadioButtons extends Component<Props> {
+  public static defaultProps = {
+    color: COMPONENT_COLORS.default,
+    size: COMPONENT_SIZES.small,
+    shape: BUTTON_SHAPES.none,
+  }
+
+  public render() {
+    return <div className={this.containerClassName}>{this.buttons}</div>
+  }
+
+  private get buttons(): JSX.Element[] {
+    const {buttons} = this.props
+
+    return buttons.map(button => (
+      <button
+        key={button.text}
+        className={this.buttonClassName(button)}
+        onClick={this.handleButtonClick(button)}
+        disabled={button.disabled}
+      >
+        {button.text}
+      </button>
+    ))
+  }
+
+  private get containerClassName(): string {
+    const {color, size, shape} = this.props
+
+    return classnames(
+      `radio-buttons radio-buttons-${color} radio-buttons-${size}`,
+      {
+        'radio-buttons-square': shape === BUTTON_SHAPES.square,
+        'radio-buttons-stretch': shape === BUTTON_SHAPES.stretchToFit,
+      }
+    )
+  }
+
+  private buttonClassName = (button: RadioButton): string => {
+    const {activeButton} = this.props
+
+    return classnames('radio-button', {
+      disabled: button.disabled,
+      active: _.isEqual(button, activeButton),
+    })
+  }
+
+  private handleButtonClick = (button: RadioButton) => () => {
+    const {onChange} = this.props
+    if (button.disabled) {
+      return
+    }
+
+    onChange(button)
+  }
+}
+
+export default RadioButtons
