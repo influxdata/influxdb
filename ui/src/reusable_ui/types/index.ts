5c170ae2d5142524672623186ee91a5a30568309
Alex P <thealexpaxton@gmail.com>
2018-06-29 16:52:56 -0700

    Introduce shared types and constants intended for use across multiple UI components

---
+++ ui/src/reusable_ui/types/index.ts
@@ -0,0 +1,11 @@
+export type ComponentColor =
+  | 'default'
+  | 'primary'
+  | 'success'
+  | 'warning'
+  | 'danger'
+  | 'alert'
+
+export type ComponentSize = 'xs' | 'sm' | 'md' | 'lg'
+
+export type ButtonShape = 'square' | 'stretch' | 'none'
