07e5465cb884a158246e71206d48bd940d616542
Jeff Wendling <jeff@influxdata.com>
2018-07-05 12:09:07 -0600

    httpd: fix flaky test in timeout handler
    there were two problems with this code:
    
    1. the send on pending did not imply that the handler was running
    2. there was a race starting the handler with timing out
    
    1 is fixed by sending to a begin channel inside the handler. it is
    then guaranteed that the timeout handler code has been entered.
    
    2 is fixed by attempting to acquire the semaphore channel once before
    checking the timeout channel. in this way, if there is capacity, which
    in this test there is known to be, it is guaranteed to be taken. if
    we check with the timer at the same time and the timer has already
    fired, there is a pseudorandom chance the timer will be taken even
    if there is capacity.

--- services/httpd/handler.go
+++ services/httpd/handler.go
@@ -1703,13 +1703,19 @@ func (t *Throttler) Handler(h http.Handler) http.Handler {
 			}
 		}
 
-		// Wait for a spot in the list of concurrent requests.
+		// First check if we can immediately send in to current because there is
+		// available capacity. This helps reduce racyness in tests.
 		select {
 		case t.current <- struct{}{}:
-		case <-timerCh:
-			t.Logger.Warn("request throttled, exceeds timeout", zap.Duration("d", timeout))
-			http.Error(w, "request throttled, exceeds timeout", http.StatusServiceUnavailable)
-			return
+		default:
+			// Wait for a spot in the list of concurrent requests, but allow checking the timeout.
+			select {
+			case t.current <- struct{}{}:
+			case <-timerCh:
+				t.Logger.Warn("request throttled, exceeds timeout", zap.Duration("d", timeout))
+				http.Error(w, "request throttled, exceeds timeout", http.StatusServiceUnavailable)
+				return
+			}
 		}
 		defer func() { <-t.current }()
 
