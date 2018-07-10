72b767566f42a93a2025d0cec1239089382d0cbe
Delmer Reed <delmer814+1@gmail.com>
2018-07-10 18:59:07 -0400

    Update loadDashboardLinks util to live in apis

--- ui/src/dashboards/apis/index.ts
+++ ui/src/dashboards/apis/index.ts
@@ -1,13 +1,38 @@
 import AJAX from 'src/utils/ajax'
 
+import {
+  linksFromDashboards,
+  updateActiveDashboardLink,
+} from 'src/dashboards/utils/dashboardSwitcherLinks'
+
 import {AxiosResponse} from 'axios'
-import {DashboardsResponse, GetDashboards} from 'src/types/apis/dashboards'
+import {
+  DashboardsResponse,
+  GetDashboards,
+  LoadLinksOptions,
+} from 'src/types/apis/dashboards'
+import {DashboardSwitcherLinks} from 'src/types/dashboards'
+import {Source} from 'src/types/sources'
 
 export const getDashboards: GetDashboards = () => {
   return AJAX<DashboardsResponse>({
     method: 'GET',
     resource: 'dashboards',
   }) as Promise<AxiosResponse<DashboardsResponse>>
+}
+
+export const loadDashboardLinks = async (
+  source: Source,
+  {activeDashboard, dashboardsAJAX = getDashboards}: LoadLinksOptions
+): Promise<DashboardSwitcherLinks> => {
+  const {
+    data: {dashboards},
+  } = await dashboardsAJAX()
+
+  const links = linksFromDashboards(dashboards, source)
+  const dashboardLinks = updateActiveDashboardLink(links, activeDashboard)
+
+  return dashboardLinks
 }
 
 export const getDashboard = async dashboardID => {
