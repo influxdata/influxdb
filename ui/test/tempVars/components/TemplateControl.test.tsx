6b92c0b5f1309ca5ad1c26454088c60bf2aef35f
Christopher Henn <chris@chrishenn.net>
2018-07-11 14:18:58 -0700

    Add tests

--- ui/test/tempVars/components/TemplateControl.test.tsx
+++ ui/test/tempVars/components/TemplateControl.test.tsx
@@ -4,11 +4,13 @@ import {shallow} from 'enzyme'
 import OverlayTechnology from 'src/reusable_ui/components/overlays/OverlayTechnology'
 import TemplateVariableEditor from 'src/tempVars/components/TemplateVariableEditor'
 import TemplateControl from 'src/tempVars/components/TemplateControl'
+import TextTemplateSelector from 'src/tempVars/components/TextTemplateSelector'
+import TemplateDropdown from 'src/tempVars/components/TemplateDropdown'
 import {source} from 'test/resources'
 
 import {TemplateType, TemplateValueType} from 'src/types'
 
-const template = {
+const defaultTemplate = () => ({
   id: '0',
   tempVar: ':my-var:',
   label: '',
@@ -21,23 +23,23 @@ const template = {
       localSelected: true,
     },
   ],
-}
+})
 
-const defaultProps = {
+const defaultProps = ({template = defaultTemplate()} = {}) => ({
   template,
   templates: [template],
   meRole: 'EDITOR',
   isUsingAuth: true,
   source,
-  onPickTemplate: () => Promise.resolve(),
+  onPickValue: () => Promise.resolve(),
   onCreateTemplate: () => Promise.resolve(),
   onUpdateTemplate: () => Promise.resolve(),
   onDeleteTemplate: () => Promise.resolve(),
-}
+})
 
 describe('TemplateControl', () => {
   it('should show a TemplateVariableEditor overlay when the settings icon is clicked', () => {
-    const wrapper = shallow(<TemplateControl {...defaultProps} />, {
+    const wrapper = shallow(<TemplateControl {...defaultProps()} />, {
       context: {
         store: {},
       },
@@ -59,5 +61,46 @@ describe('TemplateControl', () => {
       .find(TemplateVariableEditor)
 
     expect(elements).toHaveLength(1)
+  })
+
+  it('displays a TextTemplateSelector for text templates', () => {
+    const props = defaultProps({
+      template: {
+        id: '0',
+        tempVar: ':my-var:',
+        label: '',
+        type: TemplateType.Text,
+        values: [
+          {
+            value: 'initial value',
+            type: TemplateValueType.Constant,
+            selected: true,
+            localSelected: true,
+          },
+        ],
+      },
+    })
+
+    const wrapper = shallow(<TemplateControl {...props} />)
+
+    expect(wrapper.find(TemplateDropdown).length).toEqual(0)
+    expect(wrapper.find(TextTemplateSelector).length).toEqual(1)
+  })
+
+  it('displays a TemplateDropdown for non-text templates', () => {
+    const props = defaultProps({
+      template: {
+        id: '0',
+        tempVar: ':my-var:',
+        label: '',
+        type: TemplateType.CSV,
+        values: [],
+      },
+    })
+
+    const wrapper = shallow(<TemplateControl {...props} />)
+
+    expect(wrapper.find(TemplateDropdown).length).toEqual(1)
+    expect(wrapper.find(TextTemplateSelector).length).toEqual(0)
   })
 })
