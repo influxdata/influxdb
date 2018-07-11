391d65efb084a4df44463aca51f4bc3d75ef3c8b
Andrew Watkins <watts@influxdb.com>
2018-07-11 08:47:04 -0700

    Move pluralizer to getter

--- ui/src/tempVars/components/MapTemplateBuilder.tsx
+++ ui/src/tempVars/components/MapTemplateBuilder.tsx
@@ -37,7 +37,6 @@ class MapTemplateBuilder extends PureComponent<TemplateBuilderProps, State> {
   public render() {
     const {onUpdateDefaultTemplateValue, template} = this.props
     const {templateValuesString} = this.state
-    const pluralizer = template.values.length === 1 ? '' : 's'
 
     return (
       <>
@@ -65,7 +64,7 @@ class MapTemplateBuilder extends PureComponent<TemplateBuilderProps, State> {
         <div className="form-group col-xs-12 temp-builder--results">
           <p className="temp-builder--validation">
             Mapping contains <strong>{template.values.length}</strong> key-value
-            pair{pluralizer}
+            pair{this.pluralizer}
           </p>
           {template.values.length > 0 && (
             <TemplatePreviewList
@@ -76,6 +75,10 @@ class MapTemplateBuilder extends PureComponent<TemplateBuilderProps, State> {
         </div>
       </>
     )
+  }
+
+  private get pluralizer(): string {
+    return this.props.template.values.length === 1 ? '' : 's'
   }
 
   private handleUploadFile = (
