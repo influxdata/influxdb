0a4196b504cb68370c7ad968903f01f78e9039b5
Michael Desa <mjdesa@gmail.com>
2018-07-30 15:39:39 -0400

    feat(http/influxdb): add http bucket service for oss influxdb
    Co-authored-by: Andrew Watkins <andrew.watkinz@gmail.com>
    Co-authored-by: Michael Desa <mjdesa@gmail.com>

--- http/influxdb/bucket.go
+++ http/influxdb/bucket.go
@@ -2,38 +2,69 @@ package influxdb
 
 import (
 	"context"
+	"fmt"
+	"path"
+	"time"
 
 	"github.com/influxdata/platform"
 )
 
 // BucketService connects to Influx via HTTP using tokens to manage buckets
 type BucketService struct {
-	Addr               string
-	Username           string
-	Password           string
-	InsecureSkipVerify bool
+	Source *platform.Source
 }
 
 func (s *BucketService) FindBucketByID(ctx context.Context, id platform.ID) (*platform.Bucket, error) {
-	panic("not implemented")
+	return nil, fmt.Errorf("not supported")
 }
 
 func (s *BucketService) FindBucket(ctx context.Context, filter platform.BucketFilter) (*platform.Bucket, error) {
-	panic("not implemented")
+	return nil, fmt.Errorf("not supported")
 }
 
 func (s *BucketService) FindBuckets(ctx context.Context, filter platform.BucketFilter, opt ...platform.FindOptions) ([]*platform.Bucket, int, error) {
-	panic("not implemented")
+	c, err := newClient(s.Source)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	dbs, err := c.AllDB(ctx)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	bs := []*platform.Bucket{}
+	for _, db := range dbs {
+		rps, err := c.AllRP(ctx, db.Name)
+		if err != nil {
+			return nil, 0, err
+		}
+		for _, rp := range rps {
+			d, err := time.ParseDuration(rp.Duration)
+			if err != nil {
+				return nil, 0, err
+			}
+			b := &platform.Bucket{
+				// TODO(desa): what to do about IDs?
+				RetentionPeriod: d,
+				Name:            path.Join(db.Name, rp.Name),
+			}
+
+			bs = append(bs, b)
+		}
+	}
+
+	return bs, len(bs), nil
 }
 
 func (s *BucketService) CreateBucket(ctx context.Context, b *platform.Bucket) error {
-	panic("not implemented")
+	return fmt.Errorf("not supported")
 }
 
 func (s *BucketService) UpdateBucket(ctx context.Context, id platform.ID, upd platform.BucketUpdate) (*platform.Bucket, error) {
-	panic("not implemented")
+	return nil, fmt.Errorf("not supported")
 }
 
 func (s *BucketService) DeleteBucket(ctx context.Context, id platform.ID) error {
-	panic("not implemented")
+	return fmt.Errorf("not supported")
 }
