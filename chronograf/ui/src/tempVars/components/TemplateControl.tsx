1a9e1f26f04d56d11cd25aa895dc3d93e710f9b0
Andrew Watkins <watts@influxdb.com>
2018-07-31 14:35:49 -0700

    Fix overzealous deletion

--- chronograf/ui/src/tempVars/components/TemplateControl.tsx
+++ chronograf/ui/src/tempVars/components/TemplateControl.tsx
@@ -57,7 +57,14 @@ class TemplateControl extends PureComponent<Props, State> {
           <TemplateDropdown template={template} onPickValue={onPickValue} />
         )}
 
-        <label className="template-control--label">{template.tempVar}</label>
+        <label className="template-control--label">
+          {template.tempVar}
+          <span
+            className="icon cog-thick"
+            onClick={this.handleShowSettings}
+            data-test="edit"
+          />
+        </label>
 
         <OverlayTechnology visible={isEditing}>
           <TemplateVariableEditor
@@ -72,6 +79,10 @@ class TemplateControl extends PureComponent<Props, State> {
         </OverlayTechnology>
       </div>
     )
+  }
+
+  private handleShowSettings = (): void => {
+    this.setState({isEditing: true})
   }
 
   private handleHideSettings = (): void => {
