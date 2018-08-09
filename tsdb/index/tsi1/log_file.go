dd37e0dc62ef4d67459ff07fd2cd92371bf78e06
Edd Robinson <me@edd.io>
2018-08-09 23:07:02 +0100

    Address PR feedback

--- tsdb/index/tsi1/log_file.go
+++ tsdb/index/tsi1/log_file.go
@@ -536,7 +536,7 @@ func (f *LogFile) AddSeriesList(seriesSet *tsdb.SeriesIDSet, names [][]byte, tag
 			continue
 		}
 		writeRequired = true
-		entries = append(entries, LogEntry{SeriesID: seriesIDs[i], name: names[i], tags: tagsSlice[i], cached: true})
+		entries = append(entries, LogEntry{SeriesID: seriesIDs[i], name: names[i], tags: tagsSlice[i], cached: true, batchidx: i})
 	}
 	seriesSet.RUnlock()
 
@@ -555,7 +555,7 @@ func (f *LogFile) AddSeriesList(seriesSet *tsdb.SeriesIDSet, names [][]byte, tag
 		entry := &entries[i]
 		if seriesSet.ContainsNoLock(entry.SeriesID) {
 			// We don't need to allocate anything for this series.
-			seriesIDs[i] = 0
+			seriesIDs[entry.batchidx] = 0
 			continue
 		}
 		if err := f.appendEntry(entry); err != nil {
@@ -1078,9 +1078,10 @@ type LogEntry struct {
 	Checksum uint32 // checksum of flag/name/tags.
 	Size     int    // total size of record, in bytes.
 
-	cached bool        // Hint to LogFile that series data is already parsed
-	name   []byte      // series naem, this is a cached copy of the parsed measurement name
-	tags   models.Tags // series tags, this is a cached copied of the parsed tags
+	cached   bool        // Hint to LogFile that series data is already parsed
+	name     []byte      // series naem, this is a cached copy of the parsed measurement name
+	tags     models.Tags // series tags, this is a cached copied of the parsed tags
+	batchidx int         // position of entry in batch.
 }
 
 // UnmarshalBinary unmarshals data into e.
