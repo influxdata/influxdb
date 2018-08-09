308b55df9710bfc15c9074b69031646be7b5ecc0
Stuart Carnie <stuart.carnie@gmail.com>
2018-08-09 15:54:05 -0700

    feat(tsdb): Add CursorType to enable selection of batch cursors

--- tsdb/engine/tsm1/engine_cursor.go
+++ tsdb/engine/tsm1/engine_cursor.go
@@ -2,11 +2,18 @@ package tsm1
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/influxdata/influxdb/tsdb"
 )
 
 func (e *Engine) CreateCursorIterator(ctx context.Context) (tsdb.CursorIterator, error) {
-	return &arrayCursorIterator{e: e}, nil
-	//return &cursorIterator{e: e}, nil
+	switch ct := tsdb.CursorTypeFromContext(ctx); ct {
+	case tsdb.BatchCursorType:
+		return &cursorIterator{e: e}, nil
+	case tsdb.ArrayCursorType, tsdb.DefaultCursorType:
+		return &arrayCursorIterator{e: e}, nil
+	default:
+		panic(fmt.Sprintf("unexpected cursor type %d", ct))
+	}
 }
