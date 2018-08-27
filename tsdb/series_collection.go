5d0cda875ddc4cefcc1b482f532506fdd8418e3a
Jeff Wendling <jeff@influxdata.com>
2018-08-27 09:48:49 -0600

    respond to review comments
    just some documentation and whitespace issues.

--- tsdb/series_collection.go
+++ tsdb/series_collection.go
@@ -5,9 +5,8 @@ import (
 	"sync/atomic"
 	"unsafe"
 
-	"github.com/influxdata/influxdb/pkg/bytesutil"
-
 	"github.com/influxdata/influxdb/models"
+	"github.com/influxdata/influxdb/pkg/bytesutil"
 )
 
 // SeriesCollection is a struct of arrays representation of a collection of series that allows
