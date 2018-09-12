88d006a18c473fb2611c07f4e61d257689cd545a
Ben Johnson <benbjohnson@yahoo.com>
2018-09-12 08:48:40 -0600

    Remove TSI1 HLL sketches from heap.
    This commit removes the HLL sketches on each `tsi1.LogFile` and
    `tsi1.IndexFile` and instead caches the data at the `tsi1.Index`
    level. This reduces the heap size significantly for servers with
    many TSI-enabled shards.

--- tsdb/index/tsi1/partition.go
+++ tsdb/index/tsi1/partition.go
@@ -655,7 +655,6 @@ func (p *Partition) createSeriesListIfNotExists(names [][]byte, tagsSlice []mode
 
 	// Ensure fileset cannot change during insert.
 	p.mu.RLock()
-	// Insert series into log file.
 	if err := p.activeLogFile.AddSeriesList(p.seriesIDSet, names, tagsSlice); err != nil {
 		p.mu.RUnlock()
 		return err
