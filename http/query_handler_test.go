02ac64fc35bc3d0606831e9324828d97cbde1de4
Chris Goller <goller@gmail.com>
2018-09-13 11:56:49 -0500

    test(http): add test for querying flux service

---
+++ http/query_handler_test.go
@@ -0,0 +1,82 @@
+package http
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/influxdata/flux/lang"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/influxdata/platform/query"
+)
+
+func TestFluxService_Query(t *testing.T) {
+	tests := []struct {
+		name    string
+		token   string
+		ctx     context.Context
+		r       *query.ProxyRequest
+		status  int
+		want    int64
+		wantW   string
+		wantErr bool
+	}{
+		{
+			name:  "query",
+			ctx:   context.Background(),
+			token: "mytoken",
+			r: &query.ProxyRequest{
+				Request: query.Request{
+					Compiler: lang.FluxCompiler{
+						Query: "from()",
+					},
+				},
+			},
+			status: http.StatusOK,
+			want:   6,
+			wantW:  "howdy\n",
+		},
+		{
+			name:  "error status",
+			token: "mytoken",
+			ctx:   context.Background(),
+			r: &query.ProxyRequest{
+				Request: query.Request{
+					Compiler: lang.FluxCompiler{
+						Query: "from()",
+					},
+				},
+			},
+			status:  http.StatusUnauthorized,
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(tt.status)
+				fmt.Fprintln(w, "howdy")
+			}))
+			defer ts.Close()
+			s := &FluxService{
+				URL:   ts.URL,
+				Token: tt.token,
+			}
+
+			w := &bytes.Buffer{}
+			got, err := s.Query(tt.ctx, w, tt.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FluxService.Query() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FluxService.Query() = %v, want %v", got, tt.want)
+			}
+			if gotW := w.String(); gotW != tt.wantW {
+				t.Errorf("FluxService.Query() = %v, want %v", gotW, tt.wantW)
+			}
+		})
+	}
+}
