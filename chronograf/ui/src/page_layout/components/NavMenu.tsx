9817d76f9a1332c428c1e4549230eb5c6d6d0684
Alex P <thealexpaxton@gmail.com>
2018-09-25 15:06:58 -0700

    Introduce Avatar component for displaying profile pictures

--- chronograf/ui/src/page_layout/components/NavMenu.tsx
+++ chronograf/ui/src/page_layout/components/NavMenu.tsx
@@ -4,19 +4,15 @@ import _ from 'lodash'
 
 // Components
 import NavMenuItem from 'src/page_layout/components/NavMenuItem'
+import Avatar from 'src/shared/components/avatar/Avatar'
 
+// Types
+import {NavItem, NavItemType} from 'src/page_layout/containers/Nav'
 import {ErrorHandling} from 'src/shared/decorators/errors'
+import {IconFont} from 'src/clockface'
 
 interface Props {
   navItems: NavItem[]
-}
-
-interface NavItem {
-  title: string
-  link: string
-  icon: string
-  location: string
-  highlightWhen: string[]
 }
 
 @ErrorHandling
@@ -30,19 +26,39 @@ class NavMenu extends PureComponent<Props> {
 
     return (
       <nav className="nav">
-        {navItems.map(({title, highlightWhen, icon, link, location}) => (
-          <NavMenuItem
-            key={`navigation--${title}`}
-            title={title}
-            highlightWhen={highlightWhen}
-            link={link}
-            location={location}
-          >
-            <span className={`icon sidebar--icon ${icon}`} />
-          </NavMenuItem>
-        ))}
+        {navItems.map(
+          ({title, highlightWhen, icon, link, location, type, image}) => (
+            <NavMenuItem
+              key={`navigation--${title}`}
+              title={title}
+              highlightWhen={highlightWhen}
+              link={link}
+              location={location}
+            >
+              {this.renderIconOrGraphic(type, icon, image)}
+            </NavMenuItem>
+          )
+        )}
       </nav>
     )
+  }
+
+  private renderIconOrGraphic = (
+    type: NavItemType,
+    icon?: IconFont,
+    image?: string
+  ): JSX.Element => {
+    if (type === NavItemType.Avatar) {
+      return (
+        <Avatar
+          imageURI={image}
+          diameterPixels={30}
+          customClass="nav--avatar"
+        />
+      )
+    } else if (type === NavItemType.Icon) {
+      return <span className={`icon sidebar--icon ${icon}`} />
+    }
   }
 }
 
