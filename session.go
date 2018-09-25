d29d1aa698b31d4be874c56d6ec9386d97020d54
Michael Desa <mjdesa@gmail.com>
2018-09-25 15:13:38 -0400

    feat(platform): add session service

---
+++ session.go
@@ -0,0 +1,23 @@
+package platform
+
+import (
+	"context"
+	"time"
+)
+
+// Session is a user session.
+type Session struct {
+	ID          ID           `json:"id"`
+	Key         string       `json:"key"`
+	CreatedAt   time.Time    `json:"createdAt"`
+	ExpiresAt   time.Time    `json:"expiresAt"`
+	UserID      ID           `json:"userID,omitempty"`
+	Permissions []Permission `json:"permissions,omitempty"`
+}
+
+// SessionService represents a service for managing user sessions.
+type SessionService interface {
+	FindSession(ctx context.Context, key string) (*Session, error)
+	ExpireSession(ctx context.Context, key string) error
+	CreateSession(ctx context.Context, user string) (*Session, error)
+}
