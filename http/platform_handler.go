0cf834e5329da10010c76e37ef82b3dafa8b43e3
Michael Desa <mjdesa@gmail.com>
2018-10-02 12:00:29 -0400

    chore(platform): refactor platform http handler to allow for auth

--- http/platform_handler.go
+++ http/platform_handler.go
@@ -1,35 +1,19 @@
 package http
 
 import (
-	"context"
-	nethttp "net/http"
+	"net/http"
 	"strings"
 
-	idpctx "github.com/influxdata/platform/context"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
 // PlatformHandler is a collection of all the service handlers.
 type PlatformHandler struct {
-	BucketHandler        *BucketHandler
-	UserHandler          *UserHandler
-	OrgHandler           *OrgHandler
-	AuthorizationHandler *AuthorizationHandler
-	DashboardHandler     *DashboardHandler
-	AssetHandler         *AssetHandler
-	ChronografHandler    *ChronografHandler
-	ViewHandler          *ViewHandler
-	SourceHandler        *SourceHandler
-	MacroHandler         *MacroHandler
-	TaskHandler          *TaskHandler
-	FluxLangHandler      *FluxLangHandler
-	QueryHandler         *FluxHandler
-	WriteHandler         *WriteHandler
-	SetupHandler         *SetupHandler
-	SessionHandler       *SessionHandler
+	AssetHandler *AssetHandler
+	APIHandler   http.Handler
 }
 
-func setCORSResponseHeaders(w nethttp.ResponseWriter, r *nethttp.Request) {
+func setCORSResponseHeaders(w http.ResponseWriter, r *http.Request) {
 	if origin := r.Header.Get("Origin"); origin != "" {
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
@@ -37,44 +21,20 @@ func setCORSResponseHeaders(w nethttp.ResponseWriter, r *nethttp.Request) {
 	}
 }
 
-var platformLinks = map[string]interface{}{
-	"signin":     "/api/v2/signin",
-	"signout":    "/api/v2/singout",
-	"setup":      "/api/v2/setup",
-	"sources":    "/api/v2/sources",
-	"dashboards": "/api/v2/dashboards",
-	"query":      "/api/v2/query",
-	"write":      "/api/v2/write",
-	"orgs":       "/api/v2/orgs",
-	"auths":      "/api/v2/authorizations",
-	"buckets":    "/api/v2/buckets",
-	"users":      "/api/v2/users",
-	"tasks":      "/api/v2/tasks",
-	"flux": map[string]string{
-		"self":        "/api/v2/flux",
-		"ast":         "/api/v2/flux/ast",
-		"suggestions": "/api/v2/flux/suggestions",
-	},
-	"external": map[string]string{
-		"statusFeed": "https://www.influxdata.com/feed/json",
-	},
-	"system": map[string]string{
-		"metrics": "/metrics",
-		"debug":   "/debug/pprof",
-		"health":  "/healthz",
-	},
-}
+// NewPlatformHandler returns a platform handler that serves the API and associated assets.
+func NewPlatformHandler(b *APIBackend) *PlatformHandler {
+	h := NewAuthenticationHandler()
+	h.Handler = NewAPIHandler(b)
+	h.RegisterNoAuthRoute("GET", "/api/v2")
 
-func (h *PlatformHandler) serveLinks(w nethttp.ResponseWriter, r *nethttp.Request) {
-	ctx := r.Context()
-	if err := encodeResponse(ctx, w, nethttp.StatusOK, platformLinks); err != nil {
-		EncodeError(ctx, err, w)
-		return
+	return &PlatformHandler{
+		AssetHandler: NewAssetHandler(),
+		APIHandler:   h,
 	}
 }
 
 // ServeHTTP delegates a request to the appropriate subhandler.
-func (h *PlatformHandler) ServeHTTP(w nethttp.ResponseWriter, r *nethttp.Request) {
+func (h *PlatformHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	setCORSResponseHeaders(w, r)
 	if r.Method == "OPTIONS" {
 		return
@@ -89,107 +49,11 @@ func (h *PlatformHandler) ServeHTTP(w nethttp.ResponseWriter, r *nethttp.Request
 		return
 	}
 
-	// Serve the links base links for the API.
-	if r.URL.Path == "/api/v2/" || r.URL.Path == "/api/v2" {
-		h.serveLinks(w, r)
-		return
-	}
-
-	if r.URL.Path == "/api/v2/signin" || r.URL.Path == "/api/v2/signout" {
-		h.SessionHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/setup") {
-		h.SetupHandler.ServeHTTP(w, r)
-		return
-	}
-
-	ctx := r.Context()
-	var err error
-	if ctx, err = extractAuthorization(ctx, r); err != nil {
-		// TODO(desa): remove this when the authentication middleware is added.
-	}
-	r = r.WithContext(ctx)
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/write") {
-		h.WriteHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/query") {
-		h.QueryHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/buckets") {
-		h.BucketHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/users") {
-		h.UserHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/orgs") {
-		h.OrgHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/authorizations") {
-		h.AuthorizationHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/dashboards") {
-		h.DashboardHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/sources") {
-		h.SourceHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/tasks") {
-		h.TaskHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/views") {
-		h.ViewHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/macros") {
-		h.MacroHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/api/v2/flux") {
-		h.FluxLangHandler.ServeHTTP(w, r)
-		return
-	}
-
-	if strings.HasPrefix(r.URL.Path, "/chronograf/") {
-		h.ChronografHandler.ServeHTTP(w, r)
-		return
-	}
-
-	nethttp.NotFound(w, r)
+	h.APIHandler.ServeHTTP(w, r)
 }
 
 // PrometheusCollectors satisfies the prom.PrometheusCollector interface.
 func (h *PlatformHandler) PrometheusCollectors() []prometheus.Collector {
 	// TODO: collect and return relevant metrics.
 	return nil
-}
-
-func extractAuthorization(ctx context.Context, r *nethttp.Request) (context.Context, error) {
-	t, err := GetToken(r)
-	if err != nil {
-		return ctx, err
-	}
-	return idpctx.SetToken(ctx, t), nil
 }
