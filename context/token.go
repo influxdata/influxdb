c54df3d052d106df38e500227b3bb89f9daca8f1
Kelvin Wang <sherkrainwang@gmail.com>
2018-10-19 12:39:54 -0400

    refactor(token): remove context token

--- context/token.go
+++ context/token.go
@@ -29,33 +29,3 @@ func GetAuthorizer(ctx context.Context) (platform.Authorizer, error) {
 
 	return a, nil
 }
-
-// SetToken sets an token on context.
-func SetToken(ctx context.Context, t string) context.Context {
-	return context.WithValue(ctx, tokenCtxKey, t)
-}
-
-// GeToken retrieves an token from context.
-func GetToken(ctx context.Context) (string, error) {
-	t, ok := ctx.Value(tokenCtxKey).(string)
-	if !ok {
-		return "", errors.InternalErrorf("token not found on context")
-	}
-
-	return t, nil
-}
-
-// SetAuthorization sets an authorization on context.
-func SetAuthorization(ctx context.Context, a *platform.Authorization) context.Context {
-	return context.WithValue(ctx, authorizationCtxKey, a)
-}
-
-// GetAuthorization retrieves an authorization from context.
-func GetAuthorization(ctx context.Context) (*platform.Authorization, error) {
-	a, ok := ctx.Value(authorizationCtxKey).(*platform.Authorization)
-	if !ok {
-		return nil, errors.InternalErrorf("authorization not found on context")
-	}
-
-	return a, nil
-}
