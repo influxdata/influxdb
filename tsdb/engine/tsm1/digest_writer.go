3d01051dfcb3aa1262c6dc4c239f2a7dace24a14
David Norton <dgnorton@gmail.com>
2018-10-22 13:07:53 -0400

    make digest reader skip manifest if needed
    This change makes the digest reader read and discard the manifest if
    needed. Not all readers of a digest are interested in the manifest.
    
    This change also makes it a requirement for the writer to write a
    manifest because it is a non-optional part of a digest file.

--- tsdb/engine/tsm1/digest_writer.go
+++ tsdb/engine/tsm1/digest_writer.go
@@ -3,17 +3,25 @@ package tsm1
 import (
 	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"io"
 
 	"github.com/golang/snappy"
+)
+
+var (
+	// ErrNoDigestManifest is returned if an attempt is made to write other parts of a
+	// digest before writing the manifest.
+	ErrNoDigestManifest = errors.New("no digest manifest")
 )
 
 // DigestWriter allows for writing a digest of a shard.  A digest is a condensed
 // representation of the contents of a shard.  It can be scoped to one or more series
 // keys, ranges of times or sets of files.
 type DigestWriter struct {
-	w  io.WriteCloser
-	sw *snappy.Writer
+	w               io.WriteCloser
+	sw              *snappy.Writer
+	manifestWritten bool
 }
 
 func NewDigestWriter(w io.WriteCloser) (*DigestWriter, error) {
@@ -32,11 +40,20 @@ func (w *DigestWriter) WriteManifest(m *DigestManifest) error {
 	}
 
 	// Write manifest.
-	_, err = w.sw.Write(b)
+	if _, err = w.sw.Write(b); err != nil {
+		return err
+	}
+
+	w.manifestWritten = true
+
 	return err
 }
 
 func (w *DigestWriter) WriteTimeSpan(key string, t *DigestTimeSpan) error {
+	if !w.manifestWritten {
+		return ErrNoDigestManifest
+	}
+
 	if err := binary.Write(w.sw, binary.BigEndian, uint16(len(key))); err != nil {
 		return err
 	}
