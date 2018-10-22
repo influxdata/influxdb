51fa92ee3d8979caf37872ee4965f5f1b2cc54eb
Brandon Farmer <bthesorceror@gmail.com>
2018-10-22 15:21:25 -0700

    Add Task row component to avoid higher order functions in component

---
+++ chronograf/ui/src/tasks/components/TaskRow.tsx
@@ -0,0 +1,49 @@
+import React, {PureComponent} from 'react'
+
+import {
+  ComponentSpacer,
+  Alignment,
+  Button,
+  ComponentColor,
+  ComponentSize,
+} from 'src/clockface'
+import IndexList from 'src/shared/components/index_views/IndexList'
+
+import {Task} from 'src/types/v2/tasks'
+
+interface Props {
+  task: Task
+  onDelete: (task: Task) => void
+}
+
+export default class TaskRow extends PureComponent<Props> {
+  public render() {
+    const {task} = this.props
+
+    return (
+      <IndexList.Row>
+        <IndexList.Cell>
+          <a href="#">{task.name}</a>
+        </IndexList.Cell>
+        <IndexList.Cell>
+          <a href="#">{task.organization.name}</a>
+        </IndexList.Cell>
+        <IndexList.Cell>Enabled</IndexList.Cell>
+        <IndexList.Cell alignment={Alignment.Right} revealOnHover={true}>
+          <ComponentSpacer align={Alignment.Right}>
+            <Button
+              size={ComponentSize.ExtraSmall}
+              color={ComponentColor.Danger}
+              text="Delete"
+              onClick={this.handleDelete}
+            />
+          </ComponentSpacer>
+        </IndexList.Cell>
+      </IndexList.Row>
+    )
+  }
+
+  private handleDelete = () => {
+    this.props.onDelete(this.props.task)
+  }
+}
