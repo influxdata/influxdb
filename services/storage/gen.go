c7db2114dae25d56a84b647e84d0d9b8e1b94a2a
Stuart Carnie <stuart.carnie@gmail.com>
2018-11-12 10:22:21 -0700

    fix(storage): Fix go:generate line to refer to correct proto file
    Closes #10473
    
    (cherry picked from commit b075de6c966b97df348b1264ef88ef9ddc2f8d5d)

--- services/storage/gen.go
+++ services/storage/gen.go
@@ -1,3 +1,3 @@
 package storage
 
-//go:generate protoc -I$GOPATH/src/github.com/influxdata/influxdb/vendor -I. --gogofaster_out=. storage.proto
+//go:generate protoc -I$GOPATH/src/github.com/influxdata/influxdb/vendor -I. --gogofaster_out=. source.proto
