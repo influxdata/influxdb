e5c63d23d9b6e96ec0b8a2dfce171744779cd22b
Brandon Farmer <bthesorceror@gmail.com>
2018-11-20 16:30:37 -0800

    Move api initialization to util

--- ui/src/tasks/api/v2/index.ts
+++ ui/src/tasks/api/v2/index.ts
@@ -1,30 +1,17 @@
-import {Task, TasksApi} from 'src/api'
-
-const getBasePath = () => {
-  const host = window.location.hostname
-  const port = window.location.port
-  const protocol = window.location.protocol
-
-  return `${protocol}//${host}:${port}/api/v2`
-}
-
-const createTaskAPI = () => {
-  return new TasksApi({basePath: getBasePath()})
-}
+import {Task} from 'src/api'
+import {taskAPI} from 'src/utils/api'
 
 export const submitNewTask = async (
   organizationId: string,
   flux: string
 ): Promise<Task> => {
-  const api = createTaskAPI()
-  const {data} = await api.tasksPost({organizationId, flux})
+  const {data} = await taskAPI.tasksPost({organizationId, flux})
 
   return data
 }
 
 export const updateTaskFlux = async (id, flux: string): Promise<Task> => {
-  const api = createTaskAPI()
-  const {data} = await api.tasksTaskIDPatch(id, {flux})
+  const {data} = await taskAPI.tasksTaskIDPatch(id, {flux})
 
   return data
 }
@@ -33,29 +20,24 @@ export const updateTaskStatus = async (
   id: string,
   status: Task.StatusEnum
 ): Promise<Task> => {
-  const api = createTaskAPI()
-  const {data} = await api.tasksTaskIDPatch(id, {status})
+  const {data} = await taskAPI.tasksTaskIDPatch(id, {status})
 
   return data
 }
 
 export const getUserTasks = async (user): Promise<Task[]> => {
-  const api = createTaskAPI()
   const after = ''
-  const {data} = await api.tasksGet(after, user.id)
+  const {data} = await taskAPI.tasksGet(after, user.id)
 
   return data.tasks
 }
 
 export const getTask = async (id): Promise<Task> => {
-  const api = createTaskAPI()
-  const {data} = await api.tasksTaskIDGet(id)
+  const {data} = await taskAPI.tasksTaskIDGet(id)
 
   return data.task
 }
 
 export const deleteTask = (taskID: string) => {
-  const api = createTaskAPI()
-
-  return api.tasksTaskIDDelete(taskID)
+  return taskAPI.tasksTaskIDDelete(taskID)
 }
