b3ccd3064dc3ccf853d122aa8e18af660f3b671f
Deniz Kusefoglu <deniz@influxdata.com>
2018-11-27 12:08:43 -0800

    Add option to download telegraf config to onboarding

--- ui/src/onboarding/apis/index.ts
+++ ui/src/onboarding/apis/index.ts
@@ -2,6 +2,9 @@ import _ from 'lodash'
 
 import AJAX from 'src/utils/ajax'
 
+import {Telegraf, TelegrafRequest, TelegrafRequestPlugins} from 'src/api'
+import {telegrafsApi} from 'src/utils/api'
+
 export const getSetupStatus = async (url: string): Promise<boolean> => {
   try {
     const {data} = await AJAX({
@@ -14,6 +17,40 @@ export const getSetupStatus = async (url: string): Promise<boolean> => {
     console.error("Can't get setup status", error)
     throw error
   }
+}
+
+export const getTelegrafConfigTOML = async (
+  telegrafID: string
+): Promise<string> => {
+  const options = {
+    headers: {
+      Accept: 'application/toml',
+    },
+  }
+
+  const response = await telegrafsApi.telegrafsTelegrafIDGet(
+    telegrafID,
+    options
+  )
+
+  return response.data as string // response.data is string with 'application/toml' header
+}
+
+export const createTelegrafConfig = async (): Promise<Telegraf> => {
+  const telegrafRequest: TelegrafRequest = {
+    name: 'testName',
+    agent: {interval: 1},
+    plugins: [
+      {
+        name: TelegrafRequestPlugins.NameEnum.Cpu,
+        type: TelegrafRequestPlugins.TypeEnum.Input,
+        comment: 'this is a test',
+        config: {},
+      },
+    ],
+  }
+  const {data} = await telegrafsApi.telegrafsPost('123', telegrafRequest)
+  return data
 }
 
 export interface SetupParams {
--- ui/src/onboarding/components/ConfigureDataSourceSwitcher.tsx
+++ ui/src/onboarding/components/ConfigureDataSourceSwitcher.tsx
@@ -1,10 +1,23 @@
 // Libraries
 import React, {PureComponent} from 'react'
 
+// Utils
+import {downloadTextFile} from 'src/shared/utils/download'
+
+// APIs
+import {getTelegrafConfigTOML, createTelegrafConfig} from 'src/onboarding/apis'
+
 // Components
 import {ErrorHandling} from 'src/shared/decorators/errors'
+import {Button} from 'src/clockface'
 
-export interface Props {
+// Constants
+import {getTelegrafConfigFailed} from 'src/shared/copy/v2/notifications'
+
+// Types
+import {OnboardingStepProps} from 'src/onboarding/containers/OnboardingWizard'
+
+export interface Props extends OnboardingStepProps {
   dataSource: string
 }
 
@@ -13,7 +26,23 @@ class ConfigureDataSourceSwitcher extends PureComponent<Props> {
   public render() {
     const {dataSource} = this.props
 
-    return <div>{dataSource}</div>
+    return (
+      <div>
+        {dataSource}
+        <Button text="Click to Download Config" onClick={this.handleDownload} />
+      </div>
+    )
+  }
+
+  private handleDownload = async () => {
+    const {notify} = this.props
+    try {
+      const telegraf = await createTelegrafConfig()
+      const config = await getTelegrafConfigTOML(telegraf.id)
+      downloadTextFile(config, 'config.toml')
+    } catch (error) {
+      notify(getTelegrafConfigFailed())
+    }
   }
 }
 
