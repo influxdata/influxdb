f6e209cb3aa7c88decced0a08eede53463a7d8ed
Andrew Watkins <watts@influxdb.com>
2018-12-04 11:15:19 -0800

    feat(account-settings): settings and tokens tabs

--- ui/src/me/components/Account.tsx
+++ ui/src/me/components/Account.tsx
@@ -1,7 +1,38 @@
+// Libraries
 import React, {PureComponent} from 'react'
+import {WithRouterProps} from 'react-router'
 
-export default class Account extends PureComponent {
+// Components
+import {Page} from 'src/pageLayout'
+import ProfilePage from 'src/shared/components/profile_page/ProfilePage'
+import ProfilePageSection from 'src/shared/components/profile_page/ProfilePageSection'
+
+export default class Account extends PureComponent<WithRouterProps> {
   public render() {
-    return <div>Hai, I'm an account page</div>
+    const {params} = this.props
+    return (
+      <Page>
+        <Page.Header fullWidth={false}>
+          <Page.Header.Left>
+            <Page.Title title="Your name here" />
+          </Page.Header.Left>
+          <Page.Header.Right />
+        </Page.Header>
+        <Page.Contents fullWidth={false} scrollable={true}>
+          <ProfilePage
+            name="Account"
+            activeTabUrl={params.tab}
+            parentUrl="/account"
+          >
+            <ProfilePageSection id="settings" title="Settings" url="settings">
+              <div>Settings go here</div>
+            </ProfilePageSection>
+            <ProfilePageSection id="tokens" title="Tokens" url="tokens">
+              <div>Tokens go here</div>
+            </ProfilePageSection>
+          </ProfilePage>
+        </Page.Contents>
+      </Page>
+    )
   }
 }
