f3b1677fdf38efec89258cdaa40a5b40f91a571a
Stuart Carnie <stuart.carnie@gmail.com>
2018-12-05 16:03:39 -0700

    feat(influx_tools): Add gen-init and gen-exec commands
    * gen-init initializes a database based on the provided CLI spec
    * gen-exec generates the data for the target database based on the same
      CLI spec as gen-init
    
    (cherry picked from commit 23d1281b2a01b2bbf9b334d8ebb266b75f21be69)

--- cmd/influx_tools/server/server.go
+++ cmd/influx_tools/server/server.go
@@ -18,6 +18,8 @@ type Interface interface {
 
 type MetaClient interface {
 	Database(name string) *meta.DatabaseInfo
+	NodeID() uint64
+	DropDatabase(name string) error
 	RetentionPolicy(database, name string) (rpi *meta.RetentionPolicyInfo, err error)
 	ShardGroupsByTimeRange(database, policy string, min, max time.Time) (a []meta.ShardGroupInfo, err error)
 	CreateRetentionPolicy(database string, spec *meta.RetentionPolicySpec, makeDefault bool) (*meta.RetentionPolicyInfo, error)
@@ -26,4 +28,8 @@ type MetaClient interface {
 	CreateDatabaseWithRetentionPolicy(name string, spec *meta.RetentionPolicySpec) (*meta.DatabaseInfo, error)
 	DeleteShardGroup(database, policy string, id uint64) error
 	CreateShardGroup(database, policy string, timestamp time.Time) (*meta.ShardGroupInfo, error)
+	// NodeShardGroupsByTimeRange returns a list of all shard groups on a database and policy
+	// that may contain data for the specified time range and limits the Shards to the current node only.
+	// Shard groups are sorted by start time.
+	NodeShardGroupsByTimeRange(database, policy string, min, max time.Time) (a []meta.ShardGroupInfo, err error)
 }
