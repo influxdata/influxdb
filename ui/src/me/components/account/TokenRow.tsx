59eae630a14446714d40da2d62231c1c2ac98b91
Alirie Gray <alirie.gray@gmail.com>
2018-12-14 14:13:14 -0800

    Add delete button to token index row

--- ui/src/me/components/account/TokenRow.tsx
+++ ui/src/me/components/account/TokenRow.tsx
@@ -2,13 +2,25 @@
 import React, {PureComponent} from 'react'
 
 // Components
-import {IndexList} from 'src/clockface'
+import {
+  IndexList,
+  ComponentSpacer,
+  Alignment,
+  Button,
+  ComponentSize,
+  ComponentColor,
+} from 'src/clockface'
 
 // Types
 import {Authorization} from 'src/api'
 
+// Actions
+import {notify} from 'src/shared/actions/notifications'
+
 interface Props {
   auth: Authorization
+  onNotify: typeof notify
+  onDelete: (authID: string) => void
 }
 
 export default class TokenRow extends PureComponent<Props> {
@@ -19,7 +31,25 @@ export default class TokenRow extends PureComponent<Props> {
       <IndexList.Row>
         <IndexList.Cell>{description}</IndexList.Cell>
         <IndexList.Cell>{status}</IndexList.Cell>
+        <IndexList.Cell alignment={Alignment.Right} revealOnHover={true}>
+          <ComponentSpacer align={Alignment.Right}>
+            <Button
+              size={ComponentSize.ExtraSmall}
+              color={ComponentColor.Danger}
+              text="Delete"
+              onClick={this.handleDelete}
+            />
+          </ComponentSpacer>
+        </IndexList.Cell>
       </IndexList.Row>
     )
+  }
+
+  private handleDelete = () => {
+    const {
+      auth: {id},
+      onDelete,
+    } = this.props
+    onDelete(id)
   }
 }
