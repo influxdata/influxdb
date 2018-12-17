4e964ecb634a62a41bb1502e2e281b634f0bac5b
zhulongcheng <zhulongcheng.me@gmail.com>
2018-12-17 23:30:50 +0800

    add tests for bucket member

--- http/bucket_test.go
+++ http/bucket_test.go
@@ -763,6 +763,182 @@ func TestService_handlePatchBucket(t *testing.T) {
 	}
 }
 
+func TestService_handlePostBucketMember(t *testing.T) {
+	type fields struct {
+		UserService platform.UserService
+	}
+	type args struct {
+		bucketID string
+		user     *platform.User
+	}
+	type wants struct {
+		statusCode  int
+		contentType string
+		body        string
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		wants  wants
+	}{
+		{
+			name: "add a bucket member",
+			fields: fields{
+				UserService: &mock.UserService{
+					FindUserByIDFn: func(ctx context.Context, id platform.ID) (*platform.User, error) {
+						return &platform.User{
+							ID:   id,
+							Name: "name",
+						}, nil
+					},
+				},
+			},
+			args: args{
+				bucketID: "020f755c3c082000",
+				user: &platform.User{
+					ID: platformtesting.MustIDBase16("6f626f7274697320"),
+				},
+			},
+			wants: wants{
+				statusCode:  http.StatusCreated,
+				contentType: "application/json; charset=utf-8",
+				body: `
+{
+  "links": {
+    "log": "/api/v2/users/6f626f7274697320/log",
+    "self": "/api/v2/users/6f626f7274697320"
+  },
+  "role": "member",
+  "id": "6f626f7274697320",
+  "name": "name"
+}
+`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewBucketHandler(mock.NewUserResourceMappingService(), mock.NewLabelService(), tt.fields.UserService)
+
+			b, err := json.Marshal(tt.args.user)
+			if err != nil {
+				t.Fatalf("failed to marshal user: %v", err)
+			}
+
+			path := fmt.Sprintf("/api/v2/buckets/%s/members", tt.args.bucketID)
+			r := httptest.NewRequest("POST", path, bytes.NewReader(b))
+			w := httptest.NewRecorder()
+
+			h.ServeHTTP(w, r)
+
+			res := w.Result()
+			content := res.Header.Get("Content-Type")
+			body, _ := ioutil.ReadAll(res.Body)
+
+			if res.StatusCode != tt.wants.statusCode {
+				t.Errorf("%q. handlePostBucketMember() = %v, want %v", tt.name, res.StatusCode, tt.wants.statusCode)
+			}
+			if tt.wants.contentType != "" && content != tt.wants.contentType {
+				t.Errorf("%q. handlePostBucketMember() = %v, want %v", tt.name, content, tt.wants.contentType)
+			}
+			if eq, _ := jsonEqual(string(body), tt.wants.body); tt.wants.body != "" && !eq {
+				t.Errorf("%q. handlePostBucketMember() = \n***%v***\n,\nwant\n***%v***", tt.name, string(body), tt.wants.body)
+			}
+		})
+	}
+}
+
+func TestService_handlePostBucketOwner(t *testing.T) {
+	type fields struct {
+		UserService platform.UserService
+	}
+	type args struct {
+		bucketID string
+		user     *platform.User
+	}
+	type wants struct {
+		statusCode  int
+		contentType string
+		body        string
+	}
+
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		wants  wants
+	}{
+		{
+			name: "add a bucket owner",
+			fields: fields{
+				UserService: &mock.UserService{
+					FindUserByIDFn: func(ctx context.Context, id platform.ID) (*platform.User, error) {
+						return &platform.User{
+							ID:   id,
+							Name: "name",
+						}, nil
+					},
+				},
+			},
+			args: args{
+				bucketID: "020f755c3c082000",
+				user: &platform.User{
+					ID: platformtesting.MustIDBase16("6f626f7274697320"),
+				},
+			},
+			wants: wants{
+				statusCode:  http.StatusCreated,
+				contentType: "application/json; charset=utf-8",
+				body: `
+{
+  "links": {
+    "log": "/api/v2/users/6f626f7274697320/log",
+    "self": "/api/v2/users/6f626f7274697320"
+  },
+  "role": "owner",
+  "id": "6f626f7274697320",
+  "name": "name"
+}
+`,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			h := NewBucketHandler(mock.NewUserResourceMappingService(), mock.NewLabelService(), tt.fields.UserService)
+
+			b, err := json.Marshal(tt.args.user)
+			if err != nil {
+				t.Fatalf("failed to marshal user: %v", err)
+			}
+
+			path := fmt.Sprintf("/api/v2/buckets/%s/owners", tt.args.bucketID)
+			r := httptest.NewRequest("POST", path, bytes.NewReader(b))
+			w := httptest.NewRecorder()
+
+			h.ServeHTTP(w, r)
+
+			res := w.Result()
+			content := res.Header.Get("Content-Type")
+			body, _ := ioutil.ReadAll(res.Body)
+
+			if res.StatusCode != tt.wants.statusCode {
+				t.Errorf("%q. handlePostBucketOwner() = %v, want %v", tt.name, res.StatusCode, tt.wants.statusCode)
+			}
+			if tt.wants.contentType != "" && content != tt.wants.contentType {
+				t.Errorf("%q. handlePostBucketOwner() = %v, want %v", tt.name, content, tt.wants.contentType)
+			}
+			if eq, _ := jsonEqual(string(body), tt.wants.body); tt.wants.body != "" && !eq {
+				t.Errorf("%q. handlePostBucketOwner() = \n***%v***\n,\nwant\n***%v***", tt.name, string(body), tt.wants.body)
+			}
+		})
+	}
+}
+
 func initBucketService(f platformtesting.BucketFields, t *testing.T) (platform.BucketService, string, func()) {
 	svc := inmem.NewService()
 	svc.IDGenerator = f.IDGenerator
