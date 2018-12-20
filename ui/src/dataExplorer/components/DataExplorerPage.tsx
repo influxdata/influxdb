27b3c6e87b461e17d1a41cf509a8229b00ec0491
Christopher Henn <chris@chrishenn.net>
2018-12-20 12:22:14 -0800

    Add “Save As” button and modal to DE
    Closes #1867
    Closes #1868

--- ui/src/dataExplorer/components/DataExplorerPage.tsx
+++ ui/src/dataExplorer/components/DataExplorerPage.tsx
@@ -4,6 +4,7 @@ import React, {SFC} from 'react'
 // Components
 import DataExplorer from 'src/dataExplorer/components/DataExplorer'
 import {Page} from 'src/pageLayout'
+import SaveAsButton from 'src/dataExplorer/components/SaveAsButton'
 
 const DataExplorerPage: SFC<{}> = () => {
   return (
@@ -12,7 +13,9 @@ const DataExplorerPage: SFC<{}> = () => {
         <Page.Header.Left>
           <Page.Title title="Data Explorer" />
         </Page.Header.Left>
-        <Page.Header.Right />
+        <Page.Header.Right>
+          <SaveAsButton />
+        </Page.Header.Right>
       </Page.Header>
       <Page.Contents fullWidth={true} scrollable={false}>
         <DataExplorer />
