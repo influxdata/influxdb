30b2d9ca00775bff0adcd23a2ad028ae4f161e69
Adam <aanthony1243@users.noreply.github.com>
2018-12-20 17:05:36 -0500

    refactor(cmd/influxd): move driver code for influxd main package to sub-package so it can be reused. (#2089)

--- cmd/influxd/launcher/launcher_test.go
+++ cmd/influxd/launcher/launcher_test.go
@@ -1,4 +1,4 @@
-package main_test
+package launcher_test
 
 import (
 	"bytes"
@@ -12,23 +12,24 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/influxdata/platform/cmd/influxd/launcher"
+
 	"github.com/google/go-cmp/cmp"
 	"github.com/influxdata/platform"
-	"github.com/influxdata/platform/cmd/influxd"
 	"github.com/influxdata/platform/http"
 )
 
 // Default context.
 var ctx = context.Background()
 
-func TestMain_Setup(t *testing.T) {
-	m := NewMain()
-	if err := m.Run(ctx); err != nil {
+func TestLauncher_Setup(t *testing.T) {
+	l := NewLauncher()
+	if err := l.Run(ctx); err != nil {
 		t.Fatal(err)
 	}
-	defer m.Shutdown(ctx)
+	defer l.Shutdown(ctx)
 
-	svc := &http.SetupService{Addr: m.URL()}
+	svc := &http.SetupService{Addr: l.URL()}
 	if results, err := svc.Generate(ctx, &platform.OnboardingRequest{
 		User:     "USER",
 		Password: "PASSWORD",
@@ -47,13 +48,13 @@ func TestMain_Setup(t *testing.T) {
 	}
 }
 
-func TestMain_WriteAndQuery(t *testing.T) {
-	m := RunMainOrFail(t, ctx)
-	m.SetupOrFail(t)
-	defer m.ShutdownOrFail(t, ctx)
+func TestLauncher_WriteAndQuery(t *testing.T) {
+	l := RunLauncherOrFail(t, ctx)
+	l.SetupOrFail(t)
+	defer l.ShutdownOrFail(t, ctx)
 
 	// Execute single write against the server.
-	if resp, err := nethttp.DefaultClient.Do(m.MustNewHTTPRequest("POST", fmt.Sprintf("/api/v2/write?org=%s&bucket=%s", m.Org.ID, m.Bucket.ID), `m,k=v f=100i 946684800000000000`)); err != nil {
+	if resp, err := nethttp.DefaultClient.Do(l.MustNewHTTPRequest("POST", fmt.Sprintf("/api/v2/write?org=%s&bucket=%s", l.Org.ID, l.Bucket.ID), `m,k=v f=100i 946684800000000000`)); err != nil {
 		t.Fatal(err)
 	} else if err := resp.Body.Close(); err != nil {
 		t.Fatal(err)
@@ -67,19 +68,19 @@ func TestMain_WriteAndQuery(t *testing.T) {
 		`,result,table,2000-01-01T00:00:00Z,2000-01-02T00:00:00Z,2000-01-01T00:00:00Z,100,f,m,v` + "\r\n\r\n"
 
 	var buf bytes.Buffer
-	req := (http.QueryRequest{Query: qs, Org: m.Org}).WithDefaults()
+	req := (http.QueryRequest{Query: qs, Org: l.Org}).WithDefaults()
 	if preq, err := req.ProxyRequest(); err != nil {
 		t.Fatal(err)
-	} else if _, err := m.FluxService().Query(ctx, &buf, preq); err != nil {
+	} else if _, err := l.FluxService().Query(ctx, &buf, preq); err != nil {
 		t.Fatal(err)
 	} else if diff := cmp.Diff(buf.String(), exp); diff != "" {
 		t.Fatal(diff)
 	}
 }
 
-// Main is a test wrapper for main.Main.
-type Main struct {
-	*main.Main
+// Launcher is a test wrapper for launcher.Launcher.
+type Launcher struct {
+	*launcher.Launcher
 
 	// Root temporary directory for all data.
 	Path string
@@ -96,63 +97,63 @@ type Main struct {
 	Stderr bytes.Buffer
 }
 
-// NewMain returns a new instance of Main.
-func NewMain() *Main {
-	m := &Main{Main: main.NewMain()}
-	m.Main.Stdin = &m.Stdin
-	m.Main.Stdout = &m.Stdout
-	m.Main.Stderr = &m.Stderr
+// NewLauncher returns a new instance of Launcher.
+func NewLauncher() *Launcher {
+	l := &Launcher{Launcher: launcher.NewLauncher()}
+	l.Launcher.Stdin = &l.Stdin
+	l.Launcher.Stdout = &l.Stdout
+	l.Launcher.Stderr = &l.Stderr
 	if testing.Verbose() {
-		m.Main.Stdout = io.MultiWriter(m.Main.Stdout, os.Stdout)
-		m.Main.Stderr = io.MultiWriter(m.Main.Stderr, os.Stderr)
+		l.Launcher.Stdout = io.MultiWriter(l.Launcher.Stdout, os.Stdout)
+		l.Launcher.Stderr = io.MultiWriter(l.Launcher.Stderr, os.Stderr)
 	}
 
 	path, err := ioutil.TempDir("", "")
 	if err != nil {
 		panic(err)
 	}
-	m.Path = path
-	return m
+	l.Path = path
+	return l
 }
 
-// RunMainOrFail initializes and starts the server.
-func RunMainOrFail(tb testing.TB, ctx context.Context, args ...string) *Main {
+// RunLauncherOrFail initializes and starts the server.
+func RunLauncherOrFail(tb testing.TB, ctx context.Context, args ...string) *Launcher {
 	tb.Helper()
-	m := NewMain()
-	if err := m.Run(ctx, args...); err != nil {
+	l := NewLauncher()
+	if err := l.Run(ctx, args...); err != nil {
 		tb.Fatal(err)
 	}
-	return m
+	return l
 }
 
 // Run executes the program with additional arguments to set paths and ports.
-func (m *Main) Run(ctx context.Context, args ...string) error {
-	args = append(args, "--bolt-path", filepath.Join(m.Path, "influxd.bolt"))
-	args = append(args, "--engine-path", filepath.Join(m.Path, "engine"))
-	args = append(args, "--nats-path", filepath.Join(m.Path, "nats"))
+func (l *Launcher) Run(ctx context.Context, args ...string) error {
+	args = append(args, "--bolt-path", filepath.Join(l.Path, "influxd.bolt"))
+	args = append(args, "--engine-path", filepath.Join(l.Path, "engine"))
+	args = append(args, "--nats-path", filepath.Join(l.Path, "nats"))
 	args = append(args, "--http-bind-address", "127.0.0.1:0")
 	args = append(args, "--log-level", "debug")
-	return m.Main.Run(ctx, args...)
+	return l.Launcher.Run(ctx, args...)
 }
 
 // Shutdown stops the program and cleans up temporary paths.
-func (m *Main) Shutdown(ctx context.Context) error {
-	m.Cancel()
-	m.Main.Shutdown(ctx)
-	return os.RemoveAll(m.Path)
+func (l *Launcher) Shutdown(ctx context.Context) error {
+	l.Cancel()
+	l.Launcher.Shutdown(ctx)
+	return os.RemoveAll(l.Path)
 }
 
 // ShutdownOrFail stops the program and cleans up temporary paths. Fail on error.
-func (m *Main) ShutdownOrFail(tb testing.TB, ctx context.Context) {
+func (l *Launcher) ShutdownOrFail(tb testing.TB, ctx context.Context) {
 	tb.Helper()
-	if err := m.Shutdown(ctx); err != nil {
+	if err := l.Shutdown(ctx); err != nil {
 		tb.Fatal(err)
 	}
 }
 
 // SetupOrFail creates a new user, bucket, org, and auth token. Fail on error.
-func (m *Main) SetupOrFail(tb testing.TB) {
-	svc := &http.SetupService{Addr: m.URL()}
+func (l *Launcher) SetupOrFail(tb testing.TB) {
+	svc := &http.SetupService{Addr: l.URL()}
 	results, err := svc.Generate(ctx, &platform.OnboardingRequest{
 		User:     "USER",
 		Password: "PASSWORD",
@@ -163,23 +164,23 @@ func (m *Main) SetupOrFail(tb testing.TB) {
 		tb.Fatal(err)
 	}
 
-	m.User = results.User
-	m.Org = results.Org
-	m.Bucket = results.Bucket
-	m.Auth = results.Auth
+	l.User = results.User
+	l.Org = results.Org
+	l.Bucket = results.Bucket
+	l.Auth = results.Auth
 }
 
-func (m *Main) FluxService() *http.FluxService {
-	return &http.FluxService{Addr: m.URL(), Token: m.Auth.Token}
+func (l *Launcher) FluxService() *http.FluxService {
+	return &http.FluxService{Addr: l.URL(), Token: l.Auth.Token}
 }
 
 // MustNewHTTPRequest returns a new nethttp.Request with base URL and auth attached. Fail on error.
-func (m *Main) MustNewHTTPRequest(method, rawurl, body string) *nethttp.Request {
-	req, err := nethttp.NewRequest(method, m.URL()+rawurl, strings.NewReader(body))
+func (l *Launcher) MustNewHTTPRequest(method, rawurl, body string) *nethttp.Request {
+	req, err := nethttp.NewRequest(method, l.URL()+rawurl, strings.NewReader(body))
 	if err != nil {
 		panic(err)
 	}
 
-	req.Header.Set("Authorization", "Token "+m.Auth.Token)
+	req.Header.Set("Authorization", "Token "+l.Auth.Token)
 	return req
 }
