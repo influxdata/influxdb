af0cad76ba0ceef7a2abad966176b760f196b974
Adam <aanthony1243@users.noreply.github.com>
2018-12-20 11:48:33 -0500

    chore(query/influxql): moved query_test.go and requisite files to the influxql dir to be closer to the code it tests. (#2013)

--- query/influxql/end_to_end_test.go
+++ query/influxql/end_to_end_test.go
@@ -1,4 +1,4 @@
-package functions_test
+package influxql_test
 
 import (
 	"bufio"
@@ -8,15 +8,12 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/csv"
 	"github.com/influxdata/flux/execute"
 	"github.com/influxdata/flux/execute/executetest"
 	ifql "github.com/influxdata/flux/influxql"
-	"github.com/influxdata/flux/lang"
 	"github.com/influxdata/flux/memory"
 	"github.com/influxdata/flux/querytest"
 	"github.com/influxdata/platform"
@@ -25,13 +22,11 @@ import (
 	_ "github.com/influxdata/platform/query/builtin"
 	"github.com/influxdata/platform/query/influxql"
 	platformtesting "github.com/influxdata/platform/testing"
-
-	"github.com/andreyvit/diff"
 )
 
 const generatedInfluxQLDataDir = "testdata"
 
-var dbrpMappingSvc = mock.NewDBRPMappingService()
+var dbrpMappingSvcE2E = mock.NewDBRPMappingService()
 
 func init() {
 	mapping := platform.DBRPMapping{
@@ -42,13 +37,13 @@ func init() {
 		OrganizationID:  platformtesting.MustIDBase16("cadecadecadecade"),
 		BucketID:        platformtesting.MustIDBase16("da7aba5e5eedca5e"),
 	}
-	dbrpMappingSvc.FindByFn = func(ctx context.Context, cluster string, db string, rp string) (*platform.DBRPMapping, error) {
+	dbrpMappingSvcE2E.FindByFn = func(ctx context.Context, cluster string, db string, rp string) (*platform.DBRPMapping, error) {
 		return &mapping, nil
 	}
-	dbrpMappingSvc.FindFn = func(ctx context.Context, filter platform.DBRPMappingFilter) (*platform.DBRPMapping, error) {
+	dbrpMappingSvcE2E.FindFn = func(ctx context.Context, filter platform.DBRPMappingFilter) (*platform.DBRPMapping, error) {
 		return &mapping, nil
 	}
-	dbrpMappingSvc.FindManyFn = func(ctx context.Context, filter platform.DBRPMappingFilter, opt ...platform.FindOptions) ([]*platform.DBRPMapping, int, error) {
+	dbrpMappingSvcE2E.FindManyFn = func(ctx context.Context, filter platform.DBRPMappingFilter, opt ...platform.FindOptions) ([]*platform.DBRPMapping, int, error) {
 		return []*platform.DBRPMapping{&mapping}, 1, nil
 	}
 }
@@ -151,26 +146,6 @@ var skipTests = map[string]string{
 
 var querier = querytest.NewQuerier()
 
-func withEachFluxFile(t testing.TB, fn func(prefix, caseName string)) {
-	dir, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	path := filepath.Join(dir, "testdata")
-
-	fluxFiles, err := filepath.Glob(filepath.Join(path, "*.flux"))
-	if err != nil {
-		t.Fatalf("error searching for Flux files: %s", err)
-	}
-
-	for _, fluxFile := range fluxFiles {
-		ext := filepath.Ext(fluxFile)
-		prefix := fluxFile[0 : len(fluxFile)-len(ext)]
-		_, caseName := filepath.Split(prefix)
-		fn(prefix, caseName)
-	}
-}
-
 func withEachInfluxQLFile(t testing.TB, fn func(prefix, caseName string)) {
 	dir, err := os.Getwd()
 	if err != nil {
@@ -191,27 +166,6 @@ func withEachInfluxQLFile(t testing.TB, fn func(prefix, caseName string)) {
 	}
 }
 
-func Test_QueryEndToEnd(t *testing.T) {
-	withEachFluxFile(t, func(prefix, caseName string) {
-		reason, skip := skipTests[caseName]
-
-		fluxName := caseName + ".flux"
-		influxqlName := caseName + ".influxql"
-		t.Run(fluxName, func(t *testing.T) {
-			if skip {
-				t.Skip(reason)
-			}
-			testFlux(t, querier, prefix, ".flux")
-		})
-		t.Run(influxqlName, func(t *testing.T) {
-			if skip {
-				t.Skip(reason)
-			}
-			testInfluxQL(t, querier, prefix, ".influxql")
-		})
-	})
-}
-
 func Test_GeneratedInfluxQLQueries(t *testing.T) {
 	withEachInfluxQLFile(t, func(prefix, caseName string) {
 		reason, skip := skipTests[caseName]
@@ -223,109 +177,6 @@ func Test_GeneratedInfluxQLQueries(t *testing.T) {
 			testGeneratedInfluxQL(t, prefix, ".influxql")
 		})
 	})
-}
-
-func Benchmark_QueryEndToEnd(b *testing.B) {
-	withEachFluxFile(b, func(prefix, caseName string) {
-		reason, skip := skipTests[caseName]
-		if skip {
-			b.Skip(reason)
-		}
-
-		fluxName := caseName + ".flux"
-		influxqlName := caseName + ".influxql"
-		b.Run(fluxName, func(b *testing.B) {
-			if skip {
-				b.Skip(reason)
-			}
-			b.ResetTimer()
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				testFlux(b, querier, prefix, ".flux")
-			}
-		})
-		b.Run(influxqlName, func(b *testing.B) {
-			if skip {
-				b.Skip(reason)
-			}
-			b.ResetTimer()
-			b.ReportAllocs()
-			for i := 0; i < b.N; i++ {
-				testInfluxQL(b, querier, prefix, ".influxql")
-			}
-		})
-	})
-}
-
-func testFlux(t testing.TB, querier *querytest.Querier, prefix, queryExt string) {
-	q, err := ioutil.ReadFile(prefix + queryExt)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	csvInFilename := prefix + ".in.csv"
-	csvOut, err := ioutil.ReadFile(prefix + ".out.csv")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	compiler := lang.FluxCompiler{
-		Query: string(q),
-	}
-	req := &query.ProxyRequest{
-		Request: query.Request{
-			Compiler: querytest.FromCSVCompiler{
-				Compiler:  compiler,
-				InputFile: csvInFilename,
-			},
-		},
-		Dialect: csv.DefaultDialect(),
-	}
-
-	QueryTestCheckSpec(t, querier, req, string(csvOut))
-}
-
-func testInfluxQL(t testing.TB, querier *querytest.Querier, prefix, queryExt string) {
-	q, err := ioutil.ReadFile(prefix + queryExt)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			t.Fatal(err)
-		}
-		t.Skip("influxql query is missing")
-	}
-
-	csvInFilename := prefix + ".in.csv"
-	csvOut, err := ioutil.ReadFile(prefix + ".out.csv")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	compiler := influxql.NewCompiler(dbrpMappingSvc)
-	compiler.Cluster = "cluster"
-	compiler.DB = "db0"
-	compiler.Query = string(q)
-	req := &query.ProxyRequest{
-		Request: query.Request{
-			Compiler: querytest.FromCSVCompiler{
-				Compiler:  compiler,
-				InputFile: csvInFilename,
-			},
-		},
-		Dialect: csv.DefaultDialect(),
-	}
-	QueryTestCheckSpec(t, querier, req, string(csvOut))
-
-	// Rerun test for InfluxQL JSON dialect
-	req.Dialect = new(influxql.Dialect)
-
-	jsonOut, err := ioutil.ReadFile(prefix + ".out.json")
-	if err != nil {
-		if !os.IsNotExist(err) {
-			t.Fatal(err)
-		}
-		t.Skip("influxql expected json is missing")
-	}
-	QueryTestCheckSpec(t, querier, req, string(jsonOut))
 }
 
 func testGeneratedInfluxQL(t testing.TB, prefix, queryExt string) {
@@ -400,7 +251,7 @@ func resultsFromQuerier(querier *querytest.Querier, compiler flux.Compiler) (flu
 }
 
 func influxQLCompiler(query, filename string) querytest.FromInfluxJSONCompiler {
-	compiler := influxql.NewCompiler(dbrpMappingSvc)
+	compiler := influxql.NewCompiler(dbrpMappingSvcE2E)
 	compiler.Cluster = "cluster"
 	compiler.DB = "db0"
 	compiler.Query = query
@@ -437,21 +288,4 @@ func jsonToResultIterator(file string) (flux.ResultIterator, error) {
 		return nil, err
 	}
 	return results, nil
-}
-
-func QueryTestCheckSpec(t testing.TB, querier *querytest.Querier, req *query.ProxyRequest, want string) {
-	t.Helper()
-
-	var buf bytes.Buffer
-	_, err := querier.Query(context.Background(), &buf, req.Request.Compiler, req.Dialect)
-	if err != nil {
-		t.Errorf("failed to run query: %v", err)
-		return
-	}
-
-	got := buf.String()
-
-	if g, w := strings.TrimSpace(got), strings.TrimSpace(want); g != w {
-		t.Errorf("result not as expected want(-) got (+):\n%v", diff.LineDiff(w, g))
-	}
 }
