8061968b1698421a208e0db0c7ba7da395593209
Skeletrox <android.svr@gmail.com>
2019-01-12 14:31:43 +0530

    remove old comment

--- client/v2/client.go
+++ client/v2/client.go
@@ -1,5 +1,5 @@
 // Package client (v2) is the current official Go client for InfluxDB.
-package client // import "github.com/influxdata/influxdb/client/v2"
+package client // import "github.com/Skeletrox/influxdb/client/v2"
 
 import (
 	"bytes"
