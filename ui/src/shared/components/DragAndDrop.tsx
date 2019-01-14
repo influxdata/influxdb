6fa4c3bda18e70637779614b91b7b12857393ec1
Daniel Campbell <metalwhirlwind@gmail.com>
2019-01-14 15:58:54 -0800

    use Button component

--- ui/src/shared/components/DragAndDrop.tsx
+++ ui/src/shared/components/DragAndDrop.tsx
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react'
 import classnames from 'classnames'
+import {Button, ComponentColor, ComponentSize, ButtonType} from 'src/clockface'
 
 import './DragAndDrop.scss'
 
@@ -133,32 +134,33 @@ class DragAndDrop extends PureComponent<Props, State> {
     if (submitOnDrop) {
       return (
         <span className="drag-and-drop--buttons">
-          <button
-            className="button button-sm button-default"
+          <Button
+            color={ComponentColor.Default}
+            text="Cancel"
+            size={ComponentSize.Medium}
+            type={ButtonType.Button}
             onClick={this.handleCancelFile}
-            type="button"
-          >
-            Cancel
-          </button>
+          />
         </span>
       )
     }
 
     return (
       <span className="drag-and-drop--buttons">
-        <button
-          className="button button-sm button-success"
+        <Button
+          color={ComponentColor.Primary}
+          text={submitText}
+          size={ComponentSize.Medium}
+          type={ButtonType.Submit}
           onClick={this.handleSubmit}
-        >
-          {submitText}
-        </button>
-        <button
-          className="button button-sm button-default"
+        />
+        <Button
+          color={ComponentColor.Default}
+          text="Cancel"
+          size={ComponentSize.Medium}
+          type={ButtonType.Submit}
           onClick={this.handleCancelFile}
-          type="button"
-        >
-          Cancel
-        </button>
+        />
       </span>
     )
   }
