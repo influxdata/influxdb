aa0053c4bb5620e8a4e9618c7ad6cc24dd352d57
Mark Rushakoff <mark@influxdata.com>
2019-01-18 09:18:54 -0800

    fix(http): display platform error from task client

--- http/task_service.go
+++ http/task_service.go
@@ -993,7 +993,7 @@ func (t TaskService) CreateTask(ctx context.Context, tsk *platform.Task) error {
 	}
 	defer resp.Body.Close()
 
-	if err := CheckError(resp); err != nil {
+	if err := CheckError(resp, true); err != nil {
 		return err
 	}
 
