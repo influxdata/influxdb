29e3942756d8e4100a79e4e8729b68cab900a91c
Chris Goller <goller@gmail.com>
2019-01-18 19:07:10 -0600

    feat(telemetry): add boltdb resource counts to telemetry collection

--- telemetry/usage.go
+++ telemetry/usage.go
@@ -13,6 +13,13 @@ var _ platform.UsageService = (*UsageService)(nil)
 var defaultMatcher = pr.NewMatcher().
 	Family("influxdb_info").
 	Family("influxdb_uptime_seconds").
+	Family("influxdb_organizations_total").
+	Family("influxdb_buckets_total").
+	Family("influxdb_users_total").
+	Family("influxdb_tokens_total").
+	Family("influxdb_dashboards_total").
+	Family("influxdb_scrapers_total").
+	Family("influxdb_telegrafs_total").
 	Family("http_api_requests_total",
 		pr.L("handler", "platform"),
 		pr.L("method", "GET"),
