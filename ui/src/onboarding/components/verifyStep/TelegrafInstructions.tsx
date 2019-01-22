63855236ed6795a480dbc577b084a4ff0ca299aa
Andrew Watkins <watts@influxdb.com>
2019-01-22 13:46:06 -0800

    chore: change streaming copy to telegraf branding (#11414)

--- ui/src/onboarding/components/verifyStep/TelegrafInstructions.tsx
+++ ui/src/onboarding/components/verifyStep/TelegrafInstructions.tsx
@@ -26,24 +26,37 @@ class TelegrafInstructions extends PureComponent<Props> {
       ''}`
     return (
       <>
-        <h3 className="wizard-step--title">Listen for Streaming Data</h3>
+        <h3 className="wizard-step--title">Listen for Telegraf Data</h3>
         <h5 className="wizard-step--sub-title">
-          You have selected streaming data sources. Follow the instructions
-          below to begin listening for incoming data.
+          Get started pushing data into InfluxDB using our open source Telegraf
+          agent.
         </h5>
         <div className="wizard-step--body">
-          <h6>Install</h6>
+          <h6>1. Install the Latest Telegraf</h6>
           <p>
-            You can download the binaries directly from the downloads page or
-            from the releases section.{' '}
+            You can install the latest Telegraf by visiting the{' '}
+            <a
+              href="https://portal.influxdata.com/downloads/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              InfluxData Downloads&nbsp;
+            </a>
+            page . If you already have Telegraf installed on your system, make
+            sure it's up to date. You will need version 1.9.2 or higher.
           </p>
-          <h6>Start Data Stream</h6>
+          <h6>2. Configure your API Token</h6>
           <p>
-            After installing the telegraf client, save this environment
-            variable. run the following command.
+            Your API token is required for pushing data into InfluxDB. You can
+            copy the following command to your terminal window to set an
+            environment variable with your token.
           </p>
           <CopyText copyText={exportToken} notify={notify} />
-          <p>Run the following command.</p>
+          <h6>3. Start Telegraf</h6>
+          <p>
+            Finally, you can run the following command the start Telegraf agent
+            running on your machine.
+          </p>
           <CopyText copyText={configScript} notify={notify} />
         </div>
       </>
