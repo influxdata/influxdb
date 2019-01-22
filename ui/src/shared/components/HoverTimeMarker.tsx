5f0db16b7f52dc299225a69bceda559046ae7a9c
Alex P <thealexpaxton@gmail.com>
2019-01-22 15:40:52 -0800

    Ensure Y axis label does not get cut off

--- ui/src/shared/components/HoverTimeMarker.tsx
+++ ui/src/shared/components/HoverTimeMarker.tsx
@@ -2,14 +2,12 @@ import React, {SFC} from 'react'
 
 import 'src/shared/components/HoverTimeMarker.scss'
 
-const MARGIN_LEFT = 10
-
 interface Props {
   x: number
 }
 
 const HoverTimeMarker: SFC<Props> = props => {
-  const style = {left: `${props.x + MARGIN_LEFT}px`}
+  const style = {left: `${props.x}px`}
 
   return <div className="hover-time-marker" style={style} />
 }
