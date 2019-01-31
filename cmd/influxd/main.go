88333abb0d7bee3a30b1103d62896ea367fc19d6
Chris Goller <goller@gmail.com>
2019-01-31 11:23:44 -0800

    hack

--- cmd/influxd/main.go
+++ cmd/influxd/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/influxdata/influxdb/cmd/influxd/launcher"
+	"github.com/influxdata/influxdb/cmd/influxd/mem"
 	"github.com/influxdata/influxdb/kit/signals"
 	_ "github.com/influxdata/influxdb/query/builtin"
 	"github.com/influxdata/influxdb/telemetry"
@@ -22,10 +23,36 @@ var (
 	date    = "unknown"
 )
 
+func hasMemTestHack() bool {
+	for _, arg := range os.Args {
+		if arg == "--inmem" {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	// exit with SIGINT and SIGTERM
 	ctx := context.Background()
 	ctx = signals.WithStandardSignals(ctx)
+
+	if hasMemTestHack() {
+		m := mem.NewLauncher()
+		if err := m.Run(ctx, os.Args[1:]...); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		} else if !m.Running() {
+			os.Exit(1)
+		}
+
+		<-ctx.Done()
+
+		// Attempt clean shutdown.
+		ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
+		defer cancel()
+		m.Shutdown(ctx)
+	}
 
 	m := launcher.NewLauncher()
 	m.SetBuild(version, commit, date)
