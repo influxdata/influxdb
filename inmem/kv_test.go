05bf5fab5b7e84afef5c211d319690e06a7e0d53
Michael Desa <mjdesa@gmail.com>
2019-02-05 16:28:28 -0500

    feat(kv:inmem:bolt): implement user service in a kv

--- inmem/kv_test.go
+++ inmem/kv_test.go
@@ -1,40 +1,12 @@
 package inmem_test
 
 import (
-	"context"
 	"testing"
 
-	platform "github.com/influxdata/influxdb"
 	"github.com/influxdata/influxdb/inmem"
 	"github.com/influxdata/influxdb/kv"
 	platformtesting "github.com/influxdata/influxdb/testing"
 )
-
-func initExampleService(f platformtesting.UserFields, t *testing.T) (platform.UserService, string, func()) {
-	s := inmem.NewKVStore()
-	svc := kv.NewExampleService(s, f.IDGenerator)
-	if err := svc.Initialize(); err != nil {
-		t.Fatalf("error initializing user service: %v", err)
-	}
-
-	ctx := context.Background()
-	for _, u := range f.Users {
-		if err := svc.PutUser(ctx, u); err != nil {
-			t.Fatalf("failed to populate users")
-		}
-	}
-	return svc, "kv/", func() {
-		for _, u := range f.Users {
-			if err := svc.DeleteUser(ctx, u.ID); err != nil {
-				t.Logf("failed to remove users: %v", err)
-			}
-		}
-	}
-}
-
-func TestExampleService(t *testing.T) {
-	platformtesting.UserService(initExampleService, t)
-}
 
 func initKVStore(f platformtesting.KVStoreFields, t *testing.T) (kv.Store, func()) {
 	s := inmem.NewKVStore()
