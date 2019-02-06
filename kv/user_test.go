65459c596ed0474d3e9d00b612a21f80e2b43c4f
Chris Goller <goller@gmail.com>
2019-02-06 10:03:51 -0600

    refactor(kv): copy bolt user test into kv
    Co-authored-by: Michael Desa <mjdesa@gmail.com>

--- kv/user_test.go
+++ kv/user_test.go
@@ -1,33 +1,58 @@
-package bolt_test
+package kv_test
 
 import (
 	"context"
+	"errors"
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/influxdata/influxdb"
+	"github.com/influxdata/influxdb/bolt"
 	"github.com/influxdata/influxdb/kv"
 	influxdbtesting "github.com/influxdata/influxdb/testing"
 )
 
+func NewTestKVStore() (*bolt.KVStore, func(), error) {
+	f, err := ioutil.TempFile("", "influxdata-bolt-")
+	if err != nil {
+		return nil, nil, errors.New("unable to open temporary boltdb file")
+	}
+	f.Close()
+
+	path := f.Name()
+	s := bolt.NewKVStore(path)
+	if err := s.Open(context.Background()); err != nil {
+		return nil, nil, err
+	}
+
+	close := func() {
+		s.Close()
+		os.Remove(path)
+	}
+
+	return s, close, nil
+}
+
 func initUserService(f influxdbtesting.UserFields, t *testing.T) (influxdb.UserService, string, func()) {
-	svc, closeFn, err := NewTestClient()
+	s, closeFn, err := NewTestKVStore()
 	if err != nil {
 		t.Fatalf("failed to create new kv store: %v", err)
 	}
+	svc := kv.NewService(s)
 	svc.IDGenerator = f.IDGenerator
 
 	ctx := context.Background()
-	/*
-		if err := svc.Initialize(ctx); err != nil {
-			t.Fatalf("error initializing user service: %v", err)
-		}
-	*/
+	if err := svc.Initialize(ctx); err != nil {
+		t.Fatalf("error initializing user service: %v", err)
+	}
 
 	for _, u := range f.Users {
 		if err := svc.PutUser(ctx, u); err != nil {
 			t.Fatalf("failed to populate users")
 		}
 	}
+
 	return svc, kv.OpPrefix, func() {
 		defer closeFn()
 		for _, u := range f.Users {
