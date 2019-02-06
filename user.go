9d86b3bd3cce8da46b49e7aa5cee596a6c0d2829
Chris Goller <goller@gmail.com>
2019-02-06 08:20:44 -0600

    refactor(passwords): move auth interface into own file

--- user.go
+++ user.go
@@ -44,18 +44,6 @@ type UserService interface {
 	DeleteUser(ctx context.Context, id ID) error
 }
 
-// BasicAuthService is the service for managing basic auth.
-type BasicAuthService interface {
-	// SetPassword overrides the password of a known user.
-	SetPassword(ctx context.Context, name string, password string) error
-	// ComparePassword checks if the password matches the password recorded.
-	// Passwords that do not match return errors.
-	ComparePassword(ctx context.Context, name string, password string) error
-	// CompareAndSetPassword checks the password and if they match
-	// updates to the new password.
-	CompareAndSetPassword(ctx context.Context, name string, old string, new string) error
-}
-
 // UserUpdate represents updates to a user.
 // Only fields which are set are updated.
 type UserUpdate struct {
