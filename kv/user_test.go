a8e7bea6429969421308472c897337a26d1d7524
Chris Goller <goller@gmail.com>
2019-02-06 12:02:13 -0600

    feat(kv): add inmem testing to kv store

--- kv/user_test.go
+++ kv/user_test.go
@@ -2,43 +2,48 @@ package kv_test
 
 import (
 	"context"
-	"errors"
-	"io/ioutil"
-	"os"
 	"testing"
 
 	"github.com/influxdata/influxdb"
-	"github.com/influxdata/influxdb/bolt"
 	"github.com/influxdata/influxdb/kv"
 	influxdbtesting "github.com/influxdata/influxdb/testing"
 )
 
-func NewTestKVStore() (*bolt.KVStore, func(), error) {
-	f, err := ioutil.TempFile("", "influxdata-bolt-")
-	if err != nil {
-		return nil, nil, errors.New("unable to open temporary boltdb file")
-	}
-	f.Close()
-
-	path := f.Name()
-	s := bolt.NewKVStore(path)
-	if err := s.Open(context.Background()); err != nil {
-		return nil, nil, err
-	}
-
-	close := func() {
-		s.Close()
-		os.Remove(path)
-	}
-
-	return s, close, nil
+func TestBoltUserService(t *testing.T) {
+	influxdbtesting.UserService(initBoltUserService, t)
 }
 
-func initUserService(f influxdbtesting.UserFields, t *testing.T) (influxdb.UserService, string, func()) {
-	s, closeFn, err := NewTestKVStore()
+func TestInmemUserService(t *testing.T) {
+	influxdbtesting.UserService(initInmemUserService, t)
+}
+
+func initBoltUserService(f influxdbtesting.UserFields, t *testing.T) (influxdb.UserService, string, func()) {
+	s, closeBolt, err := NewTestBoltStore()
 	if err != nil {
 		t.Fatalf("failed to create new kv store: %v", err)
 	}
+
+	svc, op, closeSvc := initUserService(s, f, t)
+	return svc, op, func() {
+		closeSvc()
+		closeBolt()
+	}
+}
+
+func initInmemUserService(f influxdbtesting.UserFields, t *testing.T) (influxdb.UserService, string, func()) {
+	s, closeBolt, err := NewTestInmemStore()
+	if err != nil {
+		t.Fatalf("failed to create new kv store: %v", err)
+	}
+
+	svc, op, closeSvc := initUserService(s, f, t)
+	return svc, op, func() {
+		closeSvc()
+		closeBolt()
+	}
+}
+
+func initUserService(s kv.Store, f influxdbtesting.UserFields, t *testing.T) (influxdb.UserService, string, func()) {
 	svc := kv.NewService(s)
 	svc.IDGenerator = f.IDGenerator
 
@@ -54,15 +59,10 @@ func initUserService(f influxdbtesting.UserFields, t *testing.T) (influxdb.UserS
 	}
 
 	return svc, kv.OpPrefix, func() {
-		defer closeFn()
 		for _, u := range f.Users {
 			if err := svc.DeleteUser(ctx, u.ID); err != nil {
 				t.Logf("failed to remove users: %v", err)
 			}
 		}
 	}
-}
-
-func TestUserService(t *testing.T) {
-	influxdbtesting.UserService(initUserService, t)
 }
