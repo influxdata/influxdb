2eee8d6ceb33f5cdae08a8976b5179d30ae03877
Alex P <thealexpaxton@gmail.com>
2019-02-07 11:48:00 -0800

    Ensure type safety in default props

--- ui/src/clockface/components/grid_sizer/ResponsiveGridSizer.tsx
+++ ui/src/clockface/components/grid_sizer/ResponsiveGridSizer.tsx
@@ -8,15 +8,20 @@ import {ErrorHandling} from 'src/shared/decorators/errors'
 // Styles
 import 'src/clockface/components/grid_sizer/ResponsiveGridSizer.scss'
 
-interface Props {
+interface PassedProps {
   children: JSX.Element[]
   columns: number
+}
+
+interface DefaultProps {
   gutter?: number
 }
 
+type Props = PassedProps & DefaultProps
+
 @ErrorHandling
 class ResponsiveGridSizer extends PureComponent<Props> {
-  public static defaultProps: Partial<Props> = {
+  public static defaultProps: DefaultProps = {
     gutter: 4,
   }
 
