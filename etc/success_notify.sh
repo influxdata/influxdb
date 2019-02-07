66b0200cec5b9839c0fd42eb229a891ff91379ff
Gershon Shif <gershon@influxdata.com>
2019-02-07 14:07:32 -0800

    added an extra space between double quates and bach-slash

--- etc/success_notify.sh
+++ etc/success_notify.sh
@@ -2,9 +2,10 @@
 
 # SLACK_TOKEN is an env var defined in CircleCI
 # CIRCLE_BRANCH, CIRCLE_JOB, CIRLCE_BUILD_NUM and CIRCLE_BUILD_URL are CircleCI's vars.
+# "text":"SUCCESSFUL: Branch: $CIRCLE_BRANCH, Job: $CIRCLE_JOB, Build: $CIRCLE_BUILD_NUM, Build URL: $CIRCLE_BUILD_URL",
 
 curl -X POST https://slack.com/api/chat.postMessage \
--H "Authorization: Bearer $SLACK_TOKEN"\
+-H "Authorization: Bearer $SLACK_TOKEN" \
 -H "Content-type: application/json; charset=utf-8" \
 --data @<(cat <<EOF
 {
