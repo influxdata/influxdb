a601fb43d5a5418a7f9a6e9628b3c5ead7563b5e
Christopher Henn <chris@chrishenn.net>
2019-02-14 11:12:39 -0800

    Display more helpful errors when fetching AST fails

--- ui/src/shared/utils/renderQuery.ts
+++ ui/src/shared/utils/renderQuery.ts
@@ -1,3 +1,6 @@
+// Libraries
+import {get} from 'lodash'
+
 // APIs
 import {client} from 'src/utils/api'
 
@@ -28,7 +31,7 @@ export async function renderQuery(
   let variableDeclarations = formatVariables(variables, query)
 
   if (query.includes(WINDOW_PERIOD)) {
-    const ast = await client.queries.ast(`${variableDeclarations}\n\n${query}`)
+    const ast = await getAST(`${variableDeclarations}\n\n${query}`)
 
     let windowPeriod: number
 
@@ -44,10 +47,25 @@ export async function renderQuery(
   return `${imports}\n\n${variableDeclarations}\n\n${body}`
 }
 
+async function getAST(query: string) {
+  try {
+    const resp = await client.queries.ast(query)
+    return resp
+  } catch (e) {
+    const message = get(e, 'response.data.error')
+
+    if (message) {
+      throw new Error(message)
+    } else {
+      throw e
+    }
+  }
+}
+
 async function extractImports(
   query: string
 ): Promise<{imports: string; body: string}> {
-  const ast = await client.queries.ast(query)
+  const ast = await getAST(query)
   const {imports, body} = ast.files[0]
   const importStatements = (imports || [])
     .map(i => i.location.source)
