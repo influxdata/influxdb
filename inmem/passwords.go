6ff7f0bc633c9405bd46d9c7298ab54480d99d54
Chris Goller <goller@gmail.com>
2019-02-14 16:15:50 -0600

    fix(testing): update error messages for incorrect passwords

--- inmem/passwords.go
+++ inmem/passwords.go
@@ -15,13 +15,13 @@ var (
 	// EIncorrectPassword is returned when any password operation fails in which
 	// we do not want to leak information.
 	EIncorrectPassword = &platform.Error{
-		Msg: "your username or password is incorrect",
+		Msg: "<forbidden> your username or password is incorrect",
 	}
 
 	// EShortPassword is used when a password is less than the minimum
 	// acceptable password length.
 	EShortPassword = &platform.Error{
-		Msg: "passwords are required to be longer than 8 characters",
+		Msg: "<invalid> passwords are required to be longer than 8 characters",
 	}
 )
 
@@ -62,7 +62,7 @@ func (s *Service) ComparePassword(ctx context.Context, name string, password str
 	}
 
 	if err := bcrypt.CompareHashAndPassword(hash.([]byte), []byte(password)); err != nil {
-		return fmt.Errorf("your username or password is incorrect")
+		return fmt.Errorf("<forbidden> your username or password is incorrect")
 	}
 	return nil
 }
