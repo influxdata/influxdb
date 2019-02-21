7fc8ca928db15977b7a9837cf4aa71b6041123d3
Alirie Gray <alirie.gray@gmail.com>
2019-02-21 14:52:19 -0800

    test(tasks): add test for task without valid script

--- ui/cypress/e2e/tasks.ts
+++ ui/cypress/e2e/tasks.ts
@@ -1,5 +1,4 @@
-// currently getting unauthorized errors for task creation
-describe.skip('Tasks', () => {
+describe('Tasks', () => {
   beforeEach(() => {
     cy.flush()
 
@@ -11,11 +10,36 @@ describe.skip('Tasks', () => {
   })
 
   it('can create a task', () => {
+    const taskName = '🦄ask'
     cy.get('.empty-state').within(() => {
       cy.contains('Create').click()
     })
 
-    cy.getByInputName('name').type('🅱️ask')
+    cy.getByInputName('name').type(taskName)
+    cy.getByInputName('interval').type('1d')
+    cy.getByInputName('offset').type('20m')
+
+    cy.getByDataTest('flux-editor').within(() => {
+      cy.get('textarea').type(
+        `from(bucket: "default")
+      |> range(start: -2m)`,
+        {force: true}
+      )
+    })
+
+    cy.contains('Save').click()
+
+    cy.getByDataTest('task-row')
+      .and('have.length', 1)
+      .and('contain', taskName)
+  })
+
+  it('fails to create a task without a valid script', () => {
+    cy.get('.empty-state').within(() => {
+      cy.contains('Create').click()
+    })
+
+    cy.getByInputName('name').type('🦄ask')
     cy.getByInputName('interval').type('1d')
     cy.getByInputName('offset').type('20m')
 
@@ -24,5 +48,7 @@ describe.skip('Tasks', () => {
     })
 
     cy.contains('Save').click()
+
+    cy.getByDataTest('notification-error').should('exist')
   })
 })
