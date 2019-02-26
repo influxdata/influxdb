0de2e37b8e39b8b93f5f9bcbb2023456548241a5
Christopher Henn <chris@chrishenn.net>
2019-02-26 10:21:11 -0800

    Add e2e test for dis/enabled DE submit button
    Closes #12191

--- ui/cypress/e2e/explorer.test.ts
+++ ui/cypress/e2e/explorer.test.ts
@@ -11,7 +11,14 @@ describe('DataExplorer', () => {
     })
   })
 
-  it('renders the page', () => {
-    cy.get('.data-explorer').should('exist')
+  it('typing a custom query enables the submit button', () => {
+    cy.getByTestID('switch-to-script-editor').click()
+
+    cy.getByTestID('time-machine-submit-button').should('be.disabled')
+
+    cy.getByTestID('flux-editor').within(() => {
+      cy.get('textarea').type('yo', {force: true})
+      cy.getByTestID('time-machine-submit-button').should('not.be.disabled')
+    })
   })
 })
