6ba2b74adabbc372fe1d75649135c19f5052dbb3
Delmer Reed <delmer814+1@gmail.com>
2019-02-27 12:08:46 -0500

    test(ui/de): add test for de deleting query text
    Adds test for adding then removing a query. However, an empty query
    with only spaces will not currently disable the de submit

--- ui/cypress/e2e/explorer.test.ts
+++ ui/cypress/e2e/explorer.test.ts
@@ -19,4 +19,17 @@ describe('DataExplorer', () => {
       cy.getByTestID('time-machine-submit-button').should('not.be.disabled')
     })
   })
+
+  it('deleting query from script editor disables submit', () => {
+    cy.getByTestID('switch-to-script-editor').click()
+
+    cy.getByTestID('flux-editor').within(() => {
+      cy.get('textarea').type('from(bucket: "foo")', {force: true})
+      cy.getByTestID('time-machine-submit-button').should('not.be.disabled')
+
+      cy.get('textarea').type('{selectall} {backspace}', {force: true})
+    })
+
+    cy.getByTestID('time-machine-submit-button').should('be.disabled')
+  })
 })
