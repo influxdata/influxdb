f40634a2f8deaeeadc8981d7ff4faedfd84f28b9
Alex P <thealexpaxton@gmail.com>
2019-02-27 16:27:51 -0800

    First pass at component family for handling inline label editing

---
+++ ui/src/shared/components/inlineLabels/InlineLabels.tsx
@@ -0,0 +1,84 @@
+// Libraries
+import React, {Component} from 'react'
+import _ from 'lodash'
+
+// Components
+import {Label} from 'src/clockface'
+import InlineLabelsEditor from 'src/shared/components/inlineLabels/InlineLabelsEditor'
+
+// Types
+import {Label as LabelType} from 'src/types/v2/labels'
+
+// Styles
+import 'src/shared/components/inlineLabels/InlineLabels.scss'
+
+// Decorators
+import {ErrorHandling} from 'src/shared/decorators/errors'
+
+interface Props {
+  selectedLabels: LabelType[]
+  labels: LabelType[]
+  onRemoveLabel: (label: LabelType) => void
+  onAddLabel: (label: LabelType) => void
+  onFilterChange: (searchTerm: string) => void
+}
+
+@ErrorHandling
+export default class InlineLabels extends Component<Props> {
+  public render() {
+    return <div className="inline-labels">{this.selectedLabels}</div>
+  }
+
+  private get selectedLabels(): JSX.Element {
+    const {selectedLabels, labels, onAddLabel} = this.props
+
+    return (
+      <div className="inline-labels--container">
+        <InlineLabelsEditor
+          labels={labels}
+          selectedLabels={selectedLabels}
+          onAddLabel={onAddLabel}
+          onCreateLabel={this.handleCreateLabel}
+        />
+        {this.currentLabels}
+      </div>
+    )
+  }
+
+  private get currentLabels(): JSX.Element[] {
+    const {selectedLabels} = this.props
+
+    if (selectedLabels.length) {
+      return selectedLabels.map(label => (
+        <Label
+          id={label.id}
+          key={label.id}
+          name={label.name}
+          colorHex={label.properties.color}
+          description={label.properties.description}
+          onDelete={this.handleDeleteLabel}
+          onClick={this.handleLabelClick}
+        />
+      ))
+    }
+  }
+
+  private handleLabelClick = (labelID: string): void => {
+    const {onFilterChange, labels} = this.props
+
+    const labelName = labels.find(l => l.id === labelID).name
+
+    onFilterChange(labelName)
+  }
+
+  private handleDeleteLabel = (labelID: string): void => {
+    const {onRemoveLabel, selectedLabels} = this.props
+    const label = selectedLabels.find(label => label.id === labelID)
+
+    onRemoveLabel(label)
+  }
+
+  private handleCreateLabel = (name: string): void => {
+    console.log(name)
+  }
+}
---
+++ ui/src/shared/components/inlineLabels/InlineLabelsListItem.tsx
@@ -0,0 +1,61 @@
+// Libraries
+import React, {Component} from 'react'
+import classnames from 'classnames'
+import _ from 'lodash'
+
+// Components
+import Label from 'src/clockface/components/label/Label'
+
+import {ErrorHandling} from 'src/shared/decorators/errors'
+
+interface Props {
+  active: boolean
+  id: string
+  name: string
+  colorHex: string
+  description: string
+  onClick: (labelID: string) => void
+  onMouseOver: (labelID: string) => void
+}
+
+@ErrorHandling
+class InlineLabelsListItem extends Component<Props> {
+  public render() {
+    const {name, colorHex, description, id} = this.props
+
+    return (
+      <div
+        className={this.className}
+        onMouseOver={this.handleMouseOver}
+        onClick={this.handleClick}
+      >
+        <Label
+          name={name}
+          description={description}
+          id={id}
+          colorHex={colorHex}
+        />
+      </div>
+    )
+  }
+
+  private handleMouseOver = (): void => {
+    const {onMouseOver, id} = this.props
+
+    onMouseOver(id)
+  }
+
+  private handleClick = (): void => {
+    const {onClick, id} = this.props
+
+    onClick(id)
+  }
+
+  private get className(): string {
+    const {active} = this.props
+
+    return classnames('inline-labels--list-item', {active})
+  }
+}
+
+export default InlineLabelsListItem
