28976e9b92bbdca6f88d2bdfea3685c549521cc3
Stuart Carnie <stuart.carnie@gmail.com>
2019-02-28 21:18:00 -0700

    feat: Allow StreamReader to provide cursors.CursorStats
    StorageReadClient adapts a gRPC Storage_ReadClient to provide
    cursors.CursorStats by reading the trailer after receiving the final
    message from the stream.

--- storage/reads/stream_reader_test.go
+++ storage/reads/stream_reader_test.go
@@ -230,6 +230,73 @@ series: _m=cpu,tag0=unsigned
 `,
 			expErr: reads.ErrSeriesKeyOrder,
 		},
+
+		{
+			name: "some empty frames",
+			stream: newStreamReader(
+				response(
+					seriesF(Float, "cpu,tag0=val0"),
+				),
+				response(
+					floatF(floatS{
+						0: 1.0,
+						1: 2.0,
+						2: 3.0,
+					}),
+				),
+				response(),
+				response(
+					seriesF(Float, "cpu,tag0=val1"),
+				),
+				response(),
+				response(
+					floatF(floatS{
+						10: 11.0,
+						11: 12.0,
+						12: 13.0,
+					}),
+				),
+				response(),
+			),
+			exp: `series: _m=cpu,tag0=val0
+  cursor:Float
+                     0 |               1.00
+                     1 |               2.00
+                     2 |               3.00
+series: _m=cpu,tag0=val1
+  cursor:Float
+                    10 |              11.00
+                    11 |              12.00
+                    12 |              13.00
+`,
+		},
+
+		{
+			name: "ErrUnexpectedEOF",
+			stream: newStreamReader(
+				response(
+					seriesF(Float, "cpu,tag0=val0"),
+				),
+				response(
+					floatF(floatS{
+						0: 1.0,
+						1: 2.0,
+						2: 3.0,
+					}),
+				),
+				response(),
+				response(),
+				response(),
+			),
+			exp: `series: _m=cpu,tag0=val0
+  cursor:Float
+                     0 |               1.00
+                     1 |               2.00
+                     2 |               3.00
+  cursor err: peekFrame: no data
+`,
+			expErr: reads.ErrStreamNoData,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
