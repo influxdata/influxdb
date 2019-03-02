c7e22f070117ea51ba9c57714ff5864de41959bc
Alex P <thealexpaxton@gmail.com>
2019-03-01 18:44:29 -0800

    Add missing blank header cell to scrapers table

--- ui/src/organizations/components/ScraperList.tsx
+++ ui/src/organizations/components/ScraperList.tsx
@@ -23,9 +23,10 @@ export default class ScraperList extends PureComponent<Props> {
       <>
         <IndexList>
           <IndexList.Header>
-            <IndexList.HeaderCell columnName="Name" width="33%" />
-            <IndexList.HeaderCell columnName="URL" width="34%" />
-            <IndexList.HeaderCell columnName="Bucket" width="33%" />
+            <IndexList.HeaderCell columnName="Name" width="50%" />
+            <IndexList.HeaderCell columnName="Target URL" width="20%" />
+            <IndexList.HeaderCell columnName="Bucket" width="15%" />
+            <IndexList.HeaderCell columnName="" width="15%" />
           </IndexList.Header>
           <IndexList.Body columnCount={3} emptyState={emptyState}>
             {this.scrapersList}
