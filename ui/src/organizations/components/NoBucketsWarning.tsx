1aad9cdc9b9a01f8b0ee9ff6535254c542f35e41
Alex P <thealexpaxton@gmail.com>
2019-03-04 14:58:29 -0800

    Use alert component in no buckets warning
    Co-Authored-By: Iris Scholten <ischolten.is@gmail.com>

--- ui/src/organizations/components/NoBucketsWarning.tsx
+++ ui/src/organizations/components/NoBucketsWarning.tsx
@@ -1,6 +1,10 @@
 // Libraries
 import React, {SFC} from 'react'
 
+// Components
+import {ComponentColor, IconFont} from '@influxdata/clockface'
+import {Alert} from 'src/clockface'
+
 interface Props {
   visible: boolean
   resourceName: string
@@ -9,11 +13,13 @@ interface Props {
 const NoBucketsWarning: SFC<Props> = ({visible, resourceName}) => {
   return (
     visible && (
-      <div>
-        You don't currently have any buckets. Any{' '}
-        <strong>{resourceName}</strong> you have will not be able to write data
-        until a bucket is created and they are directed to that bucket.
-      </div>
+      <Alert color={ComponentColor.Primary} icon={IconFont.AlertTriangle}>
+        <p>
+          You don't currently have any buckets. Any{' '}
+          <strong>{resourceName}</strong> you have will not be able to write
+          data until a bucket is created and they are directed to that bucket.
+        </p>
+      </Alert>
     )
   )
 }
