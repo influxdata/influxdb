e4b3023748f6268e96dea6e24e04324150867ceb
Alirie Gray <alirie.gray@gmail.com>
2019-03-04 15:21:07 -0800

    fix(tasks): replace organization on tasks with org name string

--- ui/src/types/v2/tasks.ts
+++
@@ -1,9 +0,0 @@
-import {Task as TaskAPI, Organization, User, Label} from '@influxdata/influx'
-
-export interface Task extends Exclude<TaskAPI, 'labels'> {
-  labels: Label[]
-  organization: Organization
-  owner?: User
-}
-
-export const TaskStatus = TaskAPI.StatusEnum
