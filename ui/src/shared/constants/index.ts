aa4062da1b7549bdfec2863ddb0022d0f391a7c6
Chris Henn <chris@chrishenn.net>
2019-03-04 17:03:06 -0800

    fix(ui): Update constant to be more readable
    Co-Authored-By: ischolten <ischolten.is@gmail.com>

--- ui/src/shared/constants/index.ts
+++ ui/src/shared/constants/index.ts
@@ -435,4 +435,4 @@ export const MIN_SIZE = 0
 export const VERSION = process.env.npm_package_version
 export const GIT_SHA = process.env.GIT_SHA
 
-export const CLOUD = !!process.env.CLOUD ? process.env.CLOUD === 'true' : false
+export const CLOUD = process.env.CLOUD && process.env.CLOUD === 'true'
