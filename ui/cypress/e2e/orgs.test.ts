72834c74671e68482abec3c03ed5aca85013ede0
Andrew Watkins <watts@influxdb.com>
2019-03-05 16:45:20 -0800

    unskipping update org

--- ui/cypress/e2e/orgs.test.ts
+++ ui/cypress/e2e/orgs.test.ts
@@ -9,7 +9,7 @@ describe('Orgs', () => {
     cy.visit(orgRoute)
   })
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 75; i++) {
     it('can create an org', () => {
       cy.get('.index-list--row').should('have.length', 1)
 
@@ -46,25 +46,25 @@ describe('Orgs', () => {
 
       cy.getByTestID('table-row').should('have.length', 1)
     })
-  }
 
-  //TODO: skipping update an org name because it is flaky but needs fixing: https://github.com/influxdata/influxdb/issues/12311
-  it.skip('can update an org name', () => {
-    cy.createOrg().then(({body}) => {
-      const newName = 'new 🅱️organization'
-      cy.visit(`${orgRoute}/${body.id}/members`)
+    //TODO: skipping update an org name because it is flaky but needs fixing: https://github.com/influxdata/influxdb/issues/12311
+    it('can update an org name', () => {
+      cy.createOrg().then(({body}) => {
+        const newName = 'new 🅱️organization'
+        cy.visit(`${orgRoute}/${body.id}/members`)
 
-      cy.get('.renamable-page-title--title').click()
+        cy.get('.renamable-page-title--title').click()
 
-      cy.getByTestID('page-header').within(() => {
-        cy.getByTestID('input-field')
-          .type(newName)
-          .type('{enter}')
+        cy.getByTestID('page-header').within(() => {
+          cy.getByTestID('input-field')
+            .type(newName)
+            .type('{enter}')
+        })
+
+        cy.visit('/organizations')
+
+        cy.get('.index-list--row').should('contain', newName)
       })
-
-      cy.visit('/organizations')
-
-      cy.get('.index-list--row').should('contain', newName)
     })
-  })
+  }
 })
