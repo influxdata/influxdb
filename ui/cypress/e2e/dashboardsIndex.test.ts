acc7697e5776fd80ba85d187afe44c0022b3433a
Alex P <thealexpaxton@gmail.com>
2019-03-06 14:51:19 -0800

    Remove for loop and fix search name assertion
    Co-Authored-By: Delmer <ofthedelmer@users.noreply.github.com>
    Co-Authored-By: Andrew Watkins <121watts@users.noreply.github.com>

--- ui/cypress/e2e/dashboardsIndex.test.ts
+++ ui/cypress/e2e/dashboardsIndex.test.ts
@@ -17,186 +17,181 @@ describe('Dashboards', () => {
     })
   })
 
-  for (let i = 0; i < 20; i++) {
-    it('can create a dashboard from empty state', () => {
-      cy.getByTestID('empty-state')
-        .contains('Create')
-        .click()
+  it('can create a dashboard from empty state', () => {
+    cy.getByTestID('empty-state')
+      .contains('Create')
+      .click()
 
-      cy.visit('/dashboards')
+    cy.visit('/dashboards')
 
-      cy.getByTestID('dashboard-card').should('have.length', 1)
-    })
+    cy.getByTestID('dashboard-card').should('have.length', 1)
+  })
 
-    it('can create a dashboard from the header', () => {
-      cy.get('.page-header--container')
-        .contains('Create')
-        .click()
+  it('can create a dashboard from the header', () => {
+    cy.get('.page-header--container')
+      .contains('Create')
+      .click()
 
-      cy.getByTestID('dropdown--item New Dashboard').click()
+    cy.getByTestID('dropdown--item New Dashboard').click()
 
-      cy.visit('/dashboards')
+    cy.visit('/dashboards')
 
-      cy.getByTestID('dashboard-card').should('have.length', 1)
-    })
+    cy.getByTestID('dashboard-card').should('have.length', 1)
+  })
 
-    describe('Dashboard List', () => {
-      beforeEach(() => {
-        cy.get<Organization>('@org').then(({id}) => {
-          cy.createDashboard(id, dashboardName).then(({body}) => {
-            cy.createAndAddLabel('dashboards', body.id, newLabelName)
-          })
-
-          cy.createDashboard(id).then(({body}) => {
-            cy.createAndAddLabel('dashboards', body.id, 'bar')
-          })
+  describe('Dashboard List', () => {
+    beforeEach(() => {
+      cy.get<Organization>('@org').then(({id}) => {
+        cy.createDashboard(id, dashboardName).then(({body}) => {
+          cy.createAndAddLabel('dashboards', body.id, newLabelName)
         })
 
-        cy.visit('/dashboards')
+        cy.createDashboard(id).then(({body}) => {
+          cy.createAndAddLabel('dashboards', body.id, 'bar')
+        })
       })
 
-      it('can delete a dashboard', () => {
-        cy.getByTestID('dashboard-card').should('have.length', 2)
+      cy.visit('/dashboards')
+    })
 
-        cy.getByTestID('dashboard-card')
+    it('can delete a dashboard', () => {
+      cy.getByTestID('dashboard-card').should('have.length', 2)
+
+      cy.getByTestID('dashboard-card')
+        .first()
+        .trigger('mouseover')
+        .within(() => {
+          cy.getByTestID('context-delete-menu').click()
+          cy.getByTestID('context-delete-dashboard').click()
+        })
+
+      cy.getByTestID('dashboard-card').should('have.length', 1)
+    })
+
+    it('can edit a dashboards name', () => {
+      const newName = 'new 🅱️ashboard'
+
+      cy.getByTestID('dashboard-card').within(() => {
+        cy.getByTestID('dashboard-card--name')
           .first()
           .trigger('mouseover')
+
+        cy.getByTestID('dashboard-card--name-button')
+          .first()
+          .click()
+
+        cy.get('.input-field')
+          .type(newName)
+          .type('{enter}')
+      })
+
+      cy.getByTestID('dashboard-card').should('contain', newName)
+    })
+
+    describe('Labeling', () => {
+      it('can click to filter dashboard labels', () => {
+        cy.getByTestID('dashboard-card').should('have.length', 2)
+
+        cy.getByTestID(`label--pill ${newLabelName}`).click()
+
+        cy.getByTestID('dashboard-card')
+          .should('have.length', 1)
+          .and('contain', newLabelName)
+      })
+
+      it('can delete a label from a dashboard', () => {
+        cy.getByTestID('dashboard-card')
+          .first()
           .within(() => {
-            cy.getByTestID('context-delete-menu').click()
-            cy.getByTestID('context-delete-dashboard').click()
-          })
+            const pillID = `label--pill ${newLabelName}`
 
-        cy.getByTestID('dashboard-card').should('have.length', 1)
-      })
+            cy.getByTestID(pillID).should('have.length', 1)
 
-      it('can edit a dashboards name', () => {
-        const newName = 'new 🅱️ashboard'
-
-        cy.getByTestID('dashboard-card').within(() => {
-          cy.getByTestID('dashboard-card--name')
-            .first()
-            .trigger('mouseover')
-
-          cy.getByTestID('dashboard-card--name-button')
-            .first()
-            .click()
-
-          cy.get('.input-field')
-            .type(newName)
-            .type('{enter}')
-        })
-
-        cy.getByTestID('dashboard-card').should('contain', newName)
-      })
-
-      describe('Labeling', () => {
-        it('can click to filter dashboard labels', () => {
-          cy.getByTestID('dashboard-card').should('have.length', 2)
-
-          cy.getByTestID(`label--pill ${newLabelName}`).click()
-
-          cy.getByTestID('dashboard-card')
-            .should('have.length', 1)
-            .and('contain', newLabelName)
-        })
-
-        it('can delete a label from a dashboard', () => {
-          cy.getByTestID('dashboard-card')
-            .first()
-            .within(() => {
-              const pillID = `label--pill ${newLabelName}`
-
-              cy.getByTestID(pillID).should('have.length', 1)
-
-              cy.getByTestID(`label--pill--delete ${newLabelName}`).click({
-                force: true,
-              })
-
-              cy.getByTestID(pillID).should('have.length', 0)
-              cy.getByTestID(`inline-labels--empty`).should('have.length', 1)
-            })
-        })
-
-        it('can add an existing label to a dashboard', () => {
-          const labelName = 'swogglez'
-
-          cy.createLabel(labelName).then(() => {
-            cy.getByTestID(`inline-labels--add`)
-              .first()
-              .click()
-
-            cy.getByTestID('inline-labels--popover').within(() => {
-              cy.getByTestID(`label--pill ${labelName}`).click()
+            cy.getByTestID(`label--pill--delete ${newLabelName}`).click({
+              force: true,
             })
 
-            cy.getByTestID('dashboard-card')
-              .first()
-              .within(() => {
-                cy.getByTestID(`label--pill ${labelName}`).should('be.visible')
-              })
+            cy.getByTestID(pillID).should('have.length', 0)
+            cy.getByTestID(`inline-labels--empty`).should('have.length', 1)
           })
-        })
+      })
 
-        it('can create a label and add to a dashboard', () => {
-          const label = 'plerps'
+      it('can add an existing label to a dashboard', () => {
+        const labelName = 'swogglez'
+
+        cy.createLabel(labelName).then(() => {
           cy.getByTestID(`inline-labels--add`)
             .first()
             .click()
 
           cy.getByTestID('inline-labels--popover').within(() => {
-            cy.getByTestID('input-field').type(label)
-            cy.getByTestID('inline-labels--create-new').click()
-          })
-
-          cy.getByTestID('overlay--container').within(() => {
-            cy.getByInputName('name').should('have.value', label)
-            cy.getByTestID('create-label--button').click()
+            cy.getByTestID(`label--pill ${labelName}`).click()
           })
 
           cy.getByTestID('dashboard-card')
             .first()
             .within(() => {
-              cy.getByTestID(`label--pill ${label}`).should('be.visible')
+              cy.getByTestID(`label--pill ${labelName}`).should('be.visible')
             })
         })
       })
 
-      describe('Searching', () => {
-        it('can search dashboards by labels', () => {
-          cy.getByTestID('dashboard-card').should('have.length', 2)
+      it('can create a label and add to a dashboard', () => {
+        const label = 'plerps'
+        cy.getByTestID(`inline-labels--add`)
+          .first()
+          .click()
 
-          cy.getByTestID('search-widget').type(newLabelName)
-
-          cy.getByTestID('dashboard-card').should('have.length', 1)
-
-          cy.getByTestID('dashboard-card')
-            .first()
-            .get('.label')
-            .should('contain', newLabelName)
+        cy.getByTestID('inline-labels--popover').within(() => {
+          cy.getByTestID('input-field').type(label)
+          cy.getByTestID('inline-labels--create-new').click()
         })
 
-        it('can search by clicking label', () => {
-          const clicked = 'click-me'
-
-          cy.getByTestID('dashboard-card').should('have.length', 2)
-
-          cy.getByTestID(`label--pill ${clicked}`).click()
-
-          cy.getByTestID('search-widget').should('have.value', clicked)
-
-          cy.getByTestID('dashboard-card').should('have.length', 1)
+        cy.getByTestID('overlay--container').within(() => {
+          cy.getByInputName('name').should('have.value', label)
+          cy.getByTestID('create-label--button').click()
         })
 
-        it('can search by dashboard name', () => {
-          cy.getByTestID('search-widget').type(dashSearchName)
-
-          cy.getByTestID('dashboard-card').should('have.length', 1)
-          cy.getByTestID('dashboard-card--name').contains(
-            'span',
-            dashSearchName
-          )
-        })
+        cy.getByTestID('dashboard-card')
+          .first()
+          .within(() => {
+            cy.getByTestID(`label--pill ${label}`).should('be.visible')
+          })
       })
     })
-  }
+
+    describe('Searching', () => {
+      it('can search dashboards by labels', () => {
+        cy.getByTestID('dashboard-card').should('have.length', 2)
+
+        cy.getByTestID('search-widget').type(newLabelName)
+
+        cy.getByTestID('dashboard-card').should('have.length', 1)
+
+        cy.getByTestID('dashboard-card')
+          .first()
+          .get('.label')
+          .should('contain', newLabelName)
+      })
+
+      it('can search by clicking label', () => {
+        const clicked = 'click-me'
+
+        cy.getByTestID('dashboard-card').should('have.length', 2)
+
+        cy.getByTestID(`label--pill ${clicked}`).click()
+
+        cy.getByTestID('search-widget').should('have.value', clicked)
+
+        cy.getByTestID('dashboard-card').should('have.length', 1)
+      })
+
+      it.only('can search by dashboard name', () => {
+        cy.getByTestID('search-widget').type(dashSearchName)
+
+        cy.getByTestID('dashboard-card').should('have.length', 1)
+        cy.getByTestID('dashboard-card--name').contains('span', dashboardName)
+      })
+    })
+  })
 })
