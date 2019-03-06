e304dd7d7d4482bd3140577621fc956f1ba6c273
Deniz Kusefoglu <deniz@influxdata.com>
2019-03-05 23:02:24 -0800

    Configure importOverlay to work in tasks

--- ui/src/organizations/components/OrgTaskImportOverlay.tsx
+++ ui/src/organizations/components/OrgTaskImportOverlay.tsx
@@ -9,17 +9,26 @@ import ImportOverlay from 'src/shared/components/ImportOverlay'
 // Actions
 import {notify as notifyAction} from 'src/shared/actions/notifications'
 import {createTaskFromTemplate as createTaskFromTemplateAction} from 'src/organizations/actions/orgView'
+import {AppState, Organization} from 'src/types/v2'
+import {getTasks as getTasksAction} from 'src/organizations/actions/orgView'
+import {populateTasks as populateTasksAction} from 'src/tasks/actions/v2'
 
 interface DispatchProps {
   notify: typeof notifyAction
   createTaskFromTemplate: typeof createTaskFromTemplateAction
+  getTasks: typeof getTasksAction
+  populateTasks: typeof populateTasksAction
+}
+
+interface StateProps {
+  orgs: Organization[]
 }
 
 interface OwnProps extends WithRouterProps {
   params: {orgID: string}
 }
 
-type Props = DispatchProps & OwnProps
+type Props = DispatchProps & StateProps & OwnProps
 
 class OrgTaskImportOverlay extends PureComponent<Props> {
   public render() {
@@ -41,8 +50,11 @@ class OrgTaskImportOverlay extends PureComponent<Props> {
   private handleImportTask = async (importString: string): Promise<void> => {
     const {
       params: {orgID},
+      orgs,
+      createTaskFromTemplate,
+      getTasks,
+      populateTasks,
     } = this.props
-    const {createTaskFromTemplate} = this.props
 
     try {
       const template = JSON.parse(importString)
@@ -51,7 +63,15 @@ class OrgTaskImportOverlay extends PureComponent<Props> {
         this.onDismiss()
       }
 
-      createTaskFromTemplate(template, orgID)
+      await createTaskFromTemplate(template, orgID || orgs[0].id)
+
+      if (orgID) {
+        // import overlay is in org view
+        getTasks(orgID)
+      } else {
+        // import overlay is in tasks view
+        populateTasks()
+      }
 
       this.onDismiss()
     } catch (error) {}
@@ -61,9 +81,16 @@ class OrgTaskImportOverlay extends PureComponent<Props> {
 const mdtp: DispatchProps = {
   notify: notifyAction,
   createTaskFromTemplate: createTaskFromTemplateAction,
+  getTasks: getTasksAction,
+  populateTasks: populateTasksAction,
 }
 
-export default connect<null, DispatchProps, Props>(
-  null,
+const mstp = (state: AppState): StateProps => {
+  const {orgs} = state
+  return {orgs}
+}
+
+export default connect<StateProps, DispatchProps, Props>(
+  mstp,
   mdtp
 )(withRouter(OrgTaskImportOverlay))
