0ae84c7ea84ae76264f1e2aa2d33f4277337d9f5
Andrew Watkins <watts@influxdb.com>
2019-03-07 09:53:23 -0800

    test: only run each test once

--- ui/cypress/e2e/orgs.test.ts
+++ ui/cypress/e2e/orgs.test.ts
@@ -7,8 +7,6 @@ describe('Orgs', () => {
     cy.flush()
 
     cy.signin()
-
-    cy.visit(orgRoute)
   })
 
   describe('Org List', () => {
@@ -20,59 +18,57 @@ describe('Orgs', () => {
       cy.visit(orgRoute)
     })
 
-    for (let i = 0; i < 75; i++) {
-      it('can update an org name', () => {
-        cy.get<Organization>('@org').then(org => {
-          const newName = 'new 🅱️organization'
+    it('can update an org name', () => {
+      cy.get<Organization>('@org').then(org => {
+        const newName = 'new 🅱️organization'
 
-          cy.visit(`${orgRoute}/${org.id}/members`)
+        cy.visit(`${orgRoute}/${org.id}/members`)
 
-          cy.get('.renamable-page-title--title').click()
+        cy.get('.renamable-page-title--title').click()
 
-          cy.getByTestID('page-header').within(() => {
-            cy.getByTestID('input-field')
-              .type(newName)
-              .type('{enter}')
-          })
-
-          cy.visit('/organizations')
-
-          cy.get('.index-list--row').should('contain', newName)
+        cy.getByTestID('page-header').within(() => {
+          cy.getByTestID('input-field')
+            .type(newName)
+            .type('{enter}')
         })
+
+        cy.visit('/organizations')
+
+        cy.get('.index-list--row').should('contain', newName)
       })
+    })
 
-      it('can create an org', () => {
-        cy.get('.index-list--row').should('have.length', 2)
+    it('can create an org', () => {
+      cy.get('.index-list--row').should('have.length', 2)
 
-        cy.getByTestID('create-org-button').click()
+      cy.getByTestID('create-org-button').click()
 
-        const orgName = '🅱️organization'
+      const orgName = '🅱️organization'
 
-        cy.getByTestID('create-org-name-input').type(orgName)
+      cy.getByTestID('create-org-name-input').type(orgName)
 
-        cy.getByTestID('create-org-submit-button').click()
+      cy.getByTestID('create-org-submit-button').click()
 
-        cy.get('.index-list--row')
-          .should('contain', orgName)
-          .and('have.length', 3)
-      })
+      cy.get('.index-list--row')
+        .should('contain', orgName)
+        .and('have.length', 3)
+    })
 
-      it('can delete an org', () => {
-        cy.getByTestID('table-row').should('have.length', 2)
+    it('can delete an org', () => {
+      cy.getByTestID('table-row').should('have.length', 2)
 
-        cy.getByTestID('table-row')
-          .last()
-          .trigger('mouseover')
-          .within(() => {
-            cy.getByTestID('delete-button')
-              .trigger('mouseover')
-              .click()
+      cy.getByTestID('table-row')
+        .last()
+        .trigger('mouseover')
+        .within(() => {
+          cy.getByTestID('delete-button')
+            .trigger('mouseover')
+            .click()
 
-            cy.getByTestID('confirmation-button').click()
-          })
+          cy.getByTestID('confirmation-button').click()
+        })
 
-        cy.getByTestID('table-row').should('have.length', 1)
-      })
-    }
+      cy.getByTestID('table-row').should('have.length', 1)
+    })
   })
 })
