6dba91de77c7645db3206b496273c2a7b91b908a
Iris Scholten <ischolten.is@gmail.com>
2019-03-12 16:48:09 -0700

    fix(ui): Update keys for legend items to avoid duplicate key warnings

--- ui/src/shared/components/Legend.tsx
+++ ui/src/shared/components/Legend.tsx
@@ -47,18 +47,18 @@ class Legend extends PureComponent<Props> {
         <div className="legend--time">{this.time}</div>
         <FancyScrollbar autoHeight={true} maxHeight={120}>
           <div className="legend--columns">
-            {this.columns.map(({name, isNumeric, rows}) => (
+            {this.columns.map(({name, isNumeric, rows}, i) => (
               <div
-                key={name}
+                key={`${name}-${i}`}
                 className={`legend--column ${isNumeric ? 'numeric' : ''}`}
               >
                 <div className="legend--column-header">{name}</div>
-                {rows.map(({color, value}) => {
+                {rows.map(({color, value}, j) => {
                   const emptyClass = !value && value !== 0 ? 'empty' : ''
 
                   return (
                     <div
-                      key={color}
+                      key={`${color}-${j}`}
                       className={`legend--column-row ${emptyClass}`}
                       style={{color}}
                     >
