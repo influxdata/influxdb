75184ad7dc6e872d2f9e0661a89f4f39e7e571f5
Lyon Hill <lyondhill@gmail.com>
2019-03-18 10:49:35 -0600

    update now to schedulerFor

--- task/backend/task.go
+++ task/backend/task.go
@@ -11,16 +11,16 @@ import (
 // task executors and schedulers, which allows creation, completion, and status updates of runs.
 type TaskControlService interface {
 	// CreateNextRun attempts to create a new run.
-	// The new run's now is assigned the earliest possible time according to task's cron,
+	// The new run's ScheduledFor is assigned the earliest possible time according to task's cron,
 	// that is later than any in-progress run and LatestCompleted run.
-	// If the run's now would be later than the passed-in now, CreateNextRun returns a RunNotYetDueError.
+	// If the run's ScheduledFor would be later than the passed-in now, CreateNextRun returns a RunNotYetDueError.
 	CreateNextRun(ctx context.Context, taskID influxdb.ID, now int64) (RunCreation, error)
 
 	// FinishRun removes runID from the list of running tasks and if its `now` is later then last completed update it.
 	FinishRun(ctx context.Context, taskID, runID influxdb.ID) (*influxdb.Run, error)
 
 	// NextDueRun returns the Unix timestamp of when the next call to CreateNextRun will be ready.
-	// The returned timestamp reflects the task's delay, so it does not necessarily exactly match the schedule time.
+	// The returned timestamp reflects the task's offset, so it does not necessarily exactly match the schedule time.
 	NextDueRun() (int64, error)
 
 	// UpdateRunState sets the run state and the respective time.
