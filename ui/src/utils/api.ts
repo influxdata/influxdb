77cf726a52c29c19986c4fc40b19fdd4fd045c02
Andrew Watkins <watts@influxdb.com>
2019-03-18 15:44:41 -0700

    chore: use error message from server (#12704)

--- ui/src/utils/api.ts
+++ ui/src/utils/api.ts
@@ -1,5 +1,25 @@
+// Libraries
 import Client from '@influxdata/influx'
+import {get} from 'lodash'
 
 const basePath = '/api/v2'
+
+export const getErrorMessage = (e: any) => {
+  let message = get(e, 'response.data.error.message', '')
+
+  if (message === '') {
+    message = get(e, 'response.data.error', '')
+  }
+
+  if (message === '') {
+    message = get(e, 'response.headers.x-influx-error', '')
+  }
+
+  if (message === '') {
+    message = 'unknown error'
+  }
+
+  return message
+}
 
 export const client = new Client(basePath)
