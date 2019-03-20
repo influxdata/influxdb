8c80064375c1eba2e1f2ae6151fe685fefec437e
Delmer Reed <delmer814+1@gmail.com>
2019-03-20 14:47:20 -0400

    fix(ui/labels): select labels without prefix
    Co-authored-by: Andrew Watkins <watts@influxdb.com>

--- ui/src/labels/selectors/index.ts
+++ ui/src/labels/selectors/index.ts
@@ -1,5 +1,5 @@
 import {ILabel} from '@influxdata/influx'
-import {TOKEN_LABEL} from 'src/labels/constants'
+import {INFLUX_LABEL_PREFIX} from 'src/labels/constants'
 
 export const viewableLabels = (labels: ILabel[]) =>
-  labels.filter(l => l.name !== TOKEN_LABEL)
+  labels.filter(l => !l.name.startsWith(INFLUX_LABEL_PREFIX))
