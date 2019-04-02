d378e9e212d324a7a470de8cb8bc40100adb15f5
Christopher Wolff <chris.wolff@influxdata.com>
2019-04-02 14:30:10 -0700

    refactor(query): update testing InfluxQL testing compiler to use new interface

--- query/querytest/compiler.go
+++ query/querytest/compiler.go
@@ -1,20 +1,34 @@
 package querytest
 
 import (
-	"github.com/influxdata/flux"
-	"github.com/influxdata/flux/querytest"
+	"github.com/influxdata/flux/plan"
 	"github.com/influxdata/flux/stdlib/influxdata/influxdb/v1"
+	"github.com/influxdata/influxdb/query/influxql"
 	"github.com/influxdata/influxdb/query/stdlib/influxdata/influxdb"
 )
 
-// FromInfluxJSONCompiler returns a compiler that replaces all From operations with FromJSON.
-func FromInfluxJSONCompiler(c flux.Compiler, jsonFile string) *querytest.ReplaceSpecCompiler {
-	return querytest.NewReplaceSpecCompiler(c, func(op *flux.Operation) flux.OperationSpec {
-		if op.Spec.Kind() == influxdb.FromKind {
-			return &v1.FromInfluxJSONOpSpec{
-				File: jsonFile,
-			}
-		}
-		return nil
-	})
+// MakeFromInfluxJSONCompiler returns a compiler that replaces all From operations with FromJSON.
+func MakeFromInfluxJSONCompiler(c *influxql.Compiler, jsonFile string) {
+	c.WithLogicalPlannerOptions(plan.AddLogicalRules(ReplaceFromRule{Filename: jsonFile}))
+}
+
+type ReplaceFromRule struct {
+	Filename string
+}
+
+func (ReplaceFromRule) Name() string {
+	return "ReplaceFromRule"
+}
+
+func (ReplaceFromRule) Pattern() plan.Pattern {
+	return plan.Pat(influxdb.FromKind)
+}
+
+func (r ReplaceFromRule) Rewrite(n plan.Node) (plan.Node, bool, error) {
+	if err := n.ReplaceSpec(&v1.FromInfluxJSONProcedureSpec{
+		File: r.Filename,
+	}); err != nil {
+		return nil, false, err
+	}
+	return n, true, nil
 }
