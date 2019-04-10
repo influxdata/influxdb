7c0d4f85c3340371471a3297e73fccd495c3c5cd
Iris Scholten <ischolten.is@gmail.com>
2019-04-10 16:19:51 -0700

    feat(ui): sync note editor text and preview scrolling

--- ui/src/dashboards/components/NoteEditorPreview.tsx
+++ ui/src/dashboards/components/NoteEditorPreview.tsx
@@ -1,16 +1,22 @@
-import React, {SFC} from 'react'
+import React, {SFC, MouseEvent} from 'react'
 import ReactMarkdown from 'react-markdown'
 
 import FancyScrollbar from 'src/shared/components/fancy_scrollbar/FancyScrollbar'
 
 interface Props {
   note: string
+  scrollTop: number
+  onScroll: (e: MouseEvent) => void
 }
 
 const NoteEditorPreview: SFC<Props> = props => {
   return (
     <div className="note-editor--preview">
-      <FancyScrollbar className="note-editor--preview-scroll">
+      <FancyScrollbar
+        className="note-editor--preview-scroll"
+        scrollTop={props.scrollTop}
+        setScrollTop={props.onScroll}
+      >
         <div className="note-editor--markdown-container">
           <ReactMarkdown
             source={props.note}
