7fcaa3ba7f2f7616afd174b59464d9f9124dbfcf
Kelvin Wang <sherkrainwang@gmail.com>
2019-04-22 22:37:52 -0400

    fix(ui): remove getAllByOrg

--- ui/src/organizations/apis/index.ts
+++ ui/src/organizations/apis/index.ts
@@ -4,22 +4,15 @@ import _ from 'lodash'
 import {client} from 'src/utils/api'
 
 // Types
-import {Organization} from '@influxdata/influx'
 import {Dashboard} from 'src/types'
 
 // CRUD APIs for Organizations and Organization resources
 // i.e. Organization Members, Buckets, Dashboards etc
 
-export const getDashboards = async (
-  org?: Organization
-): Promise<Dashboard[]> => {
+export const getDashboards = async (): Promise<Dashboard[]> => {
   try {
     let result
-    if (org) {
-      result = await client.dashboards.getAllByOrg(org.name)
-    } else {
-      result = await client.dashboards.getAll()
-    }
+    result = await client.dashboards.getAll()
 
     return result
   } catch (error) {
