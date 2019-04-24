ba3c8721e6a964e35d7b2693b54289d56177c72a
Alex P <thealexpaxton@gmail.com>
2019-04-24 14:32:04 -0700

    Remove unused imports

--- ui/src/onboarding/containers/SigninPage.tsx
+++ ui/src/onboarding/containers/SigninPage.tsx
@@ -14,14 +14,7 @@ import {dismissAllNotifications} from 'src/shared/actions/notifications'
 import {ErrorHandling} from 'src/shared/decorators/errors'
 import SplashPage from 'src/shared/components/splash_page/SplashPage'
 import SigninForm from 'src/onboarding/components/SigninForm'
-import {
-  SpinnerContainer,
-  TechnoSpinner,
-  Panel,
-  ComponentSpacer,
-  FlexDirection,
-  AlignItems,
-} from '@influxdata/clockface'
+import {SpinnerContainer, TechnoSpinner, Panel} from '@influxdata/clockface'
 import {RemoteDataState} from 'src/types'
 import VersionInfo from 'src/shared/components/VersionInfo'
 
