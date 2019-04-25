27b00b293d329f15e08f74deb8419c647bd6a9ba
Deniz Kusefoglu <deniz@influxdata.com>
2019-04-25 15:00:28 -0700

    Remove ActionType on notifications

--- ui/src/shared/actions/notifications.ts
+++ ui/src/shared/actions/notifications.ts
@@ -5,14 +5,8 @@ export type Action =
   | DismissNotificationAction
   | DismissAllNotificationsAction
 
-export enum ActionTypes {
-  PUBLISH_NOTIFICATION = 'PUBLISH_NOTIFICATION',
-  DISMISS_NOTIFICATION = 'DISMISS_NOTIFICATION',
-  DISMISS_ALL_NOTIFICATIONS = 'DISMISS_ALL_NOTIFICATIONS',
-}
-
 export interface PublishNotificationAction {
-  type: ActionTypes.PUBLISH_NOTIFICATION
+  type: 'PUBLISH_NOTIFICATION'
   payload: {
     notification: Notification
   }
@@ -20,24 +14,24 @@ export interface PublishNotificationAction {
 export const notify = (
   notification: Notification
 ): PublishNotificationAction => ({
-  type: ActionTypes.PUBLISH_NOTIFICATION,
+  type: 'PUBLISH_NOTIFICATION',
   payload: {notification},
 })
 
 export interface DismissNotificationAction {
-  type: ActionTypes.DISMISS_NOTIFICATION
+  type: 'DISMISS_NOTIFICATION'
   payload: {
     id: string
   }
 }
 export const dismissNotification = (id: string): DismissNotificationAction => ({
-  type: ActionTypes.DISMISS_NOTIFICATION,
+  type: 'DISMISS_NOTIFICATION',
   payload: {id},
 })
 
 export interface DismissAllNotificationsAction {
-  type: ActionTypes.DISMISS_ALL_NOTIFICATIONS
+  type: 'DISMISS_ALL_NOTIFICATIONS'
 }
 export const dismissAllNotifications = (): DismissAllNotificationsAction => ({
-  type: ActionTypes.DISMISS_ALL_NOTIFICATIONS,
+  type: 'DISMISS_ALL_NOTIFICATIONS',
 })
--- ui/src/shared/reducers/notifications.ts
+++ ui/src/shared/reducers/notifications.ts
@@ -1,6 +1,6 @@
 import {produce} from 'immer'
 import uuid from 'uuid'
-import {Action, ActionTypes} from 'src/shared/actions/notifications'
+import {Action} from 'src/shared/actions/notifications'
 import {Notification} from 'src/types'
 
 export const initialState: Notification[] = []
@@ -11,7 +11,7 @@ export const notificationsReducer = (
 ): Notification[] =>
   produce(state, draftState => {
     switch (action.type) {
-      case ActionTypes.PUBLISH_NOTIFICATION: {
+      case 'PUBLISH_NOTIFICATION': {
         const {notification} = action.payload
         const publishedNotification = {
           ...notification,
@@ -22,13 +22,13 @@ export const notificationsReducer = (
         return
       }
 
-      case ActionTypes.DISMISS_NOTIFICATION: {
+      case 'DISMISS_NOTIFICATION': {
         const {id} = action.payload
         draftState = state.filter(n => n.id !== id)
         return
       }
 
-      case ActionTypes.DISMISS_ALL_NOTIFICATIONS: {
+      case 'DISMISS_ALL_NOTIFICATIONS': {
         draftState = []
         return
       }
