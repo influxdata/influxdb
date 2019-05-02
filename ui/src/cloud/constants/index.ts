dbd36b7a2e692ffb7e7135a5fc65dffe6e90d60f
Deniz Kusefoglu <deniz@influxdata.com>
2019-05-02 14:56:36 -0700

    Differentiate between asset limit errors and rate limit errors

--- ui/src/cloud/constants/index.ts
+++ ui/src/cloud/constants/index.ts
@@ -2,3 +2,8 @@ export const RATE_LIMIT_ERROR_STATUS = 429
 
 export const RATE_LIMIT_ERROR_TEXT =
   'Oops. It looks like you have exceeded the query limits allowed as part of your plan. If you would like to increase your query limits, reach out to cloudbeta@influxdata.com.'
+
+export const ASSET_LIMIT_ERROR_STATUS = 403
+
+export const ASSET_LIMIT_ERROR_TEXT =
+  'Oops. It looks like you have exceeded the asset limits allowed as part of your plan. If you would like to increase your limits, reach out to cloudbeta@influxdata.com.'
