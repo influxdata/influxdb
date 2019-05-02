bf774b66ceda9b808bb483c67a59cc1f4679e83b
Stuart Carnie <stuart.carnie@gmail.com>
2019-05-02 09:45:38 -0700

    fix(storage): Ensure Tag(Keys|Values) APIs never return (nil, nil)
    Formalized this post condition in the documentation and added additional
    unit tests.
    
    Added a nil guard and unit test to WriteStringIterator.

--- tsdb/tsm1/engine_schema.go
+++ tsdb/tsm1/engine_schema.go
@@ -17,6 +17,8 @@ import (
 // TagValues returns an iterator which enumerates the values for the specific
 // tagKey in the given bucket matching the predicate within the
 // time range (start, end].
+//
+// TagValues will always return a StringIterator if there is no error.
 func (e *Engine) TagValues(ctx context.Context, orgID, bucketID influxdb.ID, tagKey string, start, end int64, predicate influxql.Expr) (cursors.StringIterator, error) {
 	encoded := tsdb.EncodeName(orgID, bucketID)
 
@@ -115,7 +117,7 @@ func (e *Engine) tagValuesPredicate(ctx context.Context, orgBucket, tagKeyBytes 
 	}
 
 	if len(keys) == 0 {
-		return nil, nil
+		return cursors.EmptyStringIterator, nil
 	}
 
 	var files []TSMFile
@@ -226,6 +228,10 @@ func (e *Engine) findCandidateKeys(ctx context.Context, orgBucket []byte, predic
 	return keys, nil
 }
 
+// TagKeys returns an iterator which enumerates the tag keys for the given
+// bucket matching the predicate within the time range (start, end].
+//
+// TagKeys will always return a StringIterator if there is no error.
 func (e *Engine) TagKeys(ctx context.Context, orgID, bucketID influxdb.ID, start, end int64, predicate influxql.Expr) (cursors.StringIterator, error) {
 	encoded := tsdb.EncodeName(orgID, bucketID)
 
@@ -309,7 +315,7 @@ func (e *Engine) tagKeysPredicate(ctx context.Context, orgBucket []byte, start, 
 	}
 
 	if len(keys) == 0 {
-		return nil, nil
+		return cursors.EmptyStringIterator, nil
 	}
 
 	var files []TSMFile
