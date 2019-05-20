814600b1389115073e4b9c1646cb7ce92d5ea6c9
Deniz Kusefoglu <deniz@influxdata.com>
2019-05-20 11:05:37 -0700

    Add scatter visualization to view switcher

--- ui/src/shared/components/VisTableTransform.tsx
+++ ui/src/shared/components/VisTableTransform.tsx
@@ -1,16 +1,30 @@
 // Libraries
 import {useMemo, FunctionComponent} from 'react'
 import {fluxToTable, Table} from '@influxdata/vis'
+import {isSet} from 'lodash'
+
+interface VisTableTransformResult {
+  table: Table
+  groupKeyUnion: Array<string>
+}
 
 interface Props {
   files: string[]
-  children: (table: Table) => JSX.Element
+  children: (result: VisTableTransformResult) => JSX.Element
 }
 
 const VisTableTransform: FunctionComponent<Props> = ({files, children}) => {
-  const {table} = useMemo(() => fluxToTable(files.join('\n\n')), [files])
+  const {table, fluxGroupKeyUnion} = useMemo(
+    () => fluxToTable(files.join('\n\n')),
+    [files]
+  )
 
-  return children(table)
+  let groupKeyUnion = []
+  if (isSet(fluxGroupKeyUnion)) {
+    groupKeyUnion = Array.from(fluxGroupKeyUnion)
+  }
+
+  return children({table, groupKeyUnion})
 }
 
 export default VisTableTransform
