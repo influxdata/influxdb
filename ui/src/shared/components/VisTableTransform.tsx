bd6f635820d5e0b80b881b9ea7bd8705e7e28946
Deniz Kusefoglu <deniz@influxdata.com>
2019-05-28 12:05:33 -0700

    Assume fluxgroupkey is set

--- ui/src/shared/components/VisTableTransform.tsx
+++ ui/src/shared/components/VisTableTransform.tsx
@@ -1,7 +1,6 @@
 // Libraries
 import {useMemo, FunctionComponent} from 'react'
 import {fluxToTable, Table} from '@influxdata/vis'
-import {isSet} from 'lodash'
 
 interface VisTableTransformResult {
   table: Table
@@ -19,10 +18,7 @@ const VisTableTransform: FunctionComponent<Props> = ({files, children}) => {
     [files]
   )
 
-  let groupKeyUnion = []
-  if (isSet(fluxGroupKeyUnion)) {
-    groupKeyUnion = Array.from(fluxGroupKeyUnion)
-  }
+  const groupKeyUnion = Array.from(fluxGroupKeyUnion)
 
   return children({table, groupKeyUnion})
 }
