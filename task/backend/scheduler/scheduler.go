8584e8f093b45dc9e20ef3d4952bc9395bca9f86
Lyon Hill <lyondhill@gmail.com>
2019-06-19 16:35:22 -0600

    Make a few minor improvements to the functions and comments for scheduler

--- task/backend/scheduler/scheduler.go
+++ task/backend/scheduler/scheduler.go
@@ -7,15 +7,16 @@ import (
 	"github.com/influxdata/influxdb"
 )
 
-type Storage interface {
-	// UpdateLatestCompleted will allow us to set a latest completed time for an id.
-	UpdateLatestCompleted(ctx context.Context, id influxdb.ID, lastRun time.Time) error
+type CompletionStore interface {
+	// SetTime will allow us to set a latest completed time for an id.
+	SetTime(ctx context.Context, id influxdb.ID, lastRun time.Time) error
 
-	// retrieve the latest completed for this schedulable id.
+	// Time retrieves the latest completed for this schedulable id.
 	// If GetLatestCompleted is empty scheduler will use "now" as its latest completed
-	GetLatestCompleted(ctx context.Context, id influxdb.ID) (time.Time, error)
+	Time(ctx context.Context, id influxdb.ID) (time.Time, error)
 }
 
+// Executor is a system used by the scheduler to actually execute the scheduleable item.
 type Executor interface {
 	// Execute is used to execute run's for any schedulable object.
 	// the executor can go through manual runs, clean currently running, and then create a new run based on `now`.
@@ -25,9 +26,11 @@ type Executor interface {
 	// so the time can be configurable.
 	Execute(ctx context.Context, id influxdb.ID, meta interface{}, now time.Time) (Execution, error)
 
-	CancelRun(ctx context.Context, taskID, runID influxdb.ID) error
+	// CancelExecution is used to cancel a running execution
+	CancelExecution(ctx context.Context, id, executionID influxdb.ID) error
 }
 
+// Execution is the currently executing element from the a call to Execute.
 type Execution interface {
 	// ID is a unique ID usable to look up or cancel a run
 	ID() influxdb.ID
@@ -43,17 +46,20 @@ type Execution interface {
 	Error() error
 }
 
+// Schedulable is a object that can be scheduled by the scheduler.
 type Schedulable interface {
 	// ID a unique ID we can to lookup a scheduler
 	ID() influxdb.ID
 
-	// Schedule is the schedule you want execution
+	// Schedule is the schedule you want execution.
 	Schedule() string // cron or every dx
 
 	// Extra meta data to give to the executor on execution
 	Meta() interface{}
 }
 
+// Scheduler is a example interface of a Scheduler.
+// todo(lh): this probably wont be defined here but defined in the system that requires it.
 type Scheduler interface {
 	// Start allows the scheduler to Tick. A scheduler without start will do nothing
 	Start(ctx context.Context)
