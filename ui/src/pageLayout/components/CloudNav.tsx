03a1c86e550eb66d1799e6e7299c15521e823bc3
Delmer <delmer814+1@gmail.com>
2019-06-20 19:12:05 -0400

    feat(ui/cloud_nav): adds and feature flags billing/usage sub navs (#14170)
    Adds a feature flag for the billing and usage sub navs in cloud.
    
    Co-authored-by: Iris Scholten <ischolten.is@gmail.com>

--- ui/src/pageLayout/components/CloudNav.tsx
+++ ui/src/pageLayout/components/CloudNav.tsx
@@ -1,39 +1,58 @@
 import React, {PureComponent} from 'react'
 
 // Components
+import {FeatureFlag} from 'src/shared/utils/featureFlag'
 import {NavMenu, Icon} from '@influxdata/clockface'
+import CloudOnly from 'src/shared/components/cloud/CloudOnly'
 
 // Types
 import {IconFont} from '@influxdata/clockface'
 
 export default class CloudNav extends PureComponent {
   render() {
-    if (!this.shouldRender) {
-      return null
-    }
-
     return (
-      <NavMenu.Item
-        active={false}
-        titleLink={className => (
-          <a className={className} href={this.usageURL}>
-            Usage
-          </a>
-        )}
-        iconLink={className => (
-          <a className={className} href={this.usageURL}>
-            <Icon glyph={IconFont.Cloud} />
-          </a>
-        )}
-      />
+      <CloudOnly>
+        <NavMenu.Item
+          active={false}
+          titleLink={className => (
+            <a className={className} href={this.usageURL}>
+              Usage
+            </a>
+          )}
+          iconLink={className => (
+            <a className={className} href={this.usageURL}>
+              <Icon glyph={IconFont.Cloud} />
+            </a>
+          )}
+        >
+          <FeatureFlag name="cloudSubNav">
+            <NavMenu.SubItem
+              active={false}
+              titleLink={className => (
+                <a className={className} href={this.usageURL}>
+                  Usage
+                </a>
+              )}
+            />
+            <NavMenu.SubItem
+              active={false}
+              titleLink={className => (
+                <a className={className} href={this.billingURL}>
+                  Billing
+                </a>
+              )}
+            />
+          </FeatureFlag>
+        </NavMenu.Item>
+      </CloudOnly>
     )
-  }
-
-  private get shouldRender(): boolean {
-    return process.env.CLOUD === 'true'
   }
 
   private get usageURL(): string {
     return `${process.env.CLOUD_URL}${process.env.CLOUD_USAGE_PATH}`
+  }
+
+  private get billingURL(): string {
+    return `${process.env.CLOUD_URL}${process.env.CLOUD_BILLING_PATH}`
   }
 }
