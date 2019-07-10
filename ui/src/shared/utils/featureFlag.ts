a0db5af9fc1b7caa964fab072f0372a041f04448
Deniz Kusefoglu <deniz@influxdata.com>
2019-07-10 16:31:24 -0700

    Remove vis type specific parameters from cloud and oss flags

--- ui/src/shared/utils/featureFlag.ts
+++ ui/src/shared/utils/featureFlag.ts
@@ -1,16 +1,9 @@
 import {FunctionComponent} from 'react'
 import {CLOUD, CLOUD_BILLING_VISIBLE} from 'src/shared/constants'
 
-const OSS_FLAGS = {
-  heatmap: true,
-  scatter: true,
-  lineGraphShading: true,
-}
+const OSS_FLAGS = {}
 
 const CLOUD_FLAGS = {
-  heatmap: false, // We need to ensure the API updates have been deployed before enabling
-  scatter: false, // ditto ^^
-  lineGraphShading: false, // ditto! ^^
   cloudBilling: CLOUD_BILLING_VISIBLE, // should be visible in dev and acceptance, but not in cloud
 }
 
