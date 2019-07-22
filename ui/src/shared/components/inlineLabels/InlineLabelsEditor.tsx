ecf18f61532c7a3817fcdc33deb00d91270a42c9
Alex P <thealexpaxton@gmail.com>
2019-07-22 07:35:13 -1000

    Fix appearance of inline label editor

--- ui/src/shared/components/inlineLabels/InlineLabelsEditor.tsx
+++ ui/src/shared/components/inlineLabels/InlineLabelsEditor.tsx
@@ -116,11 +116,11 @@ class InlineLabelsEditor extends Component<Props, State> {
 
     return (
       <div
-        className="label label--xs label--colorless"
+        className="cf-label cf-label--xs cf-label--colorless"
         onClick={this.handleShowPopover}
         data-testid="inline-labels--empty"
       >
-        <span className="label--name">Add a label</span>
+        <span className="cf-label--name">Add a label</span>
       </div>
     )
   }
