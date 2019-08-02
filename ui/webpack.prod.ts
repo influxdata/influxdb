7f9101ca612929efca19302e74c82e5f6fe88e31
Andrew Watkins <watts@influxdb.com>
2019-08-02 16:31:25 -0700

    build(webpack): fix hashing and css hmr

--- ui/webpack.prod.ts
+++ ui/webpack.prod.ts
@@ -1,18 +1,55 @@
+export {}
 const merge = require('webpack-merge')
 const common = require('./webpack.common.ts')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  output: {
+    filename: '[name].[hash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              hmr: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
   optimization: {
     minimizer: [
-      new TerserJSPlugin({sourceMap: true}),
+      new TerserJSPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
       new OptimizeCSSAssetsPlugin({}),
     ],
     splitChunks: {
       chunks: 'all',
     },
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: false,
+    }),
+  ],
 })
