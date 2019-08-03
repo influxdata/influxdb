841f22cbee8ed81f3388fde45de70fbfb90b526f
Andrew Watkins <watts@influxdb.com>
2019-08-03 11:36:58 -0700

    build(webpack): fix hashing and css hmr (#14552)

--- ui/webpack.common.ts
+++ ui/webpack.common.ts
@@ -1,19 +1,14 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const webpack = require('webpack')
 const GIT_SHA = require('child_process')
   .execSync('git rev-parse HEAD')
   .toString()
 
-const devMode = process.env.NODE_ENV !== 'production'
-
 module.exports = {
   context: __dirname,
   output: {
-    filename: devMode ? '[name].bundle.js' : '[name].[hash].bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
   entry: {
@@ -43,20 +38,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.s?css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              implementation: require('sass'),
-              hmr: process.env.NODE_ENV === 'development',
-            },
-          },
-        ],
-      },
-      {
         test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader'],
       },
@@ -73,14 +54,6 @@ module.exports = {
       template: './assets/index.html',
       favicon: './assets/images/favicon.ico',
       inject: 'body',
-    }),
-    new MiniCssExtractPlugin({
-      filename: devMode ? '[name].css' : '[name].[hash].css',
-      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    }),
-    new ForkTsCheckerWebpackPlugin({
-      eslint: !devMode,
-      warningsFilter: /export * was not found in/,
     }),
     new webpack.ProgressPlugin(),
     new webpack.EnvironmentPlugin({...process.env, GIT_SHA}),
--- ui/webpack.dev.ts
+++ ui/webpack.dev.ts
@@ -1,4 +1,5 @@
 export {}
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 const common = require('./webpack.common.ts')
@@ -7,6 +8,9 @@ const path = require('path')
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'cheap-inline-source-map',
+  output: {
+    filename: '[name].js',
+  },
   devServer: {
     hot: true,
     historyApiFallback: true,
@@ -16,10 +20,31 @@ module.exports = merge(common, {
       '/debug/flush': 'http://localhost:9999',
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              hmr: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new webpack.DllReferencePlugin({
       context: path.join(__dirname, 'build'),
       manifest: require('./build/vendor-manifest.json'),
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      eslint: true,
     }),
   ],
 })
