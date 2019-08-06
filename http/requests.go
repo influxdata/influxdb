b6b5befad7c43247eb4f6bbbef944351cf3ceb4c
Jakub Bednář <jakub.bednar@gmail.com>
2019-08-06 06:20:26 +0200

    fix(http): The org parameter takes either the ID or Name interchangeably (#14540)

--- http/requests.go
+++ http/requests.go
@@ -8,25 +8,30 @@ import (
 )
 
 const (
-	// OrgName is the http query parameter to specify an organization by name.
-	OrgName = "org"
 	// OrgID is the http query parameter to specify an organization by ID.
 	OrgID = "orgID"
+	// Org is the http query parameter that take either the ID or Name interchangeably
+	Org = "org"
 )
 
 // queryOrganization returns the organization for any http request.
 func queryOrganization(ctx context.Context, r *http.Request, svc platform.OrganizationService) (o *platform.Organization, err error) {
+
 	filter := platform.OrganizationFilter{}
+
+	if organization := r.URL.Query().Get(Org); organization != "" {
+		if id, err := platform.IDFromString(organization); err == nil {
+			filter.ID = id
+		} else {
+			filter.Name = &organization
+		}
+	}
+
 	if reqID := r.URL.Query().Get(OrgID); reqID != "" {
 		filter.ID, err = platform.IDFromString(reqID)
 		if err != nil {
 			return nil, err
 		}
 	}
-
-	if name := r.URL.Query().Get(OrgName); name != "" {
-		filter.Name = &name
-	}
-
 	return svc.FindOrganization(ctx, filter)
 }
