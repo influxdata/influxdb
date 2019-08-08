d64a180d5a0842d84ed25237ec8bdb28ab83f994
Alex P <thealexpaxton@gmail.com>
2019-08-08 15:11:48 -0700

    refactor(ui): add placeholder buttons in to thresholds card

--- ui/src/alerting/components/builder/CheckThresholdsCard.tsx
+++ ui/src/alerting/components/builder/CheckThresholdsCard.tsx
@@ -1,8 +1,43 @@
 // Libraries
 import React, {FunctionComponent} from 'react'
 
+// Components
+import {
+  ComponentSpacer,
+  FlexDirection,
+  AlignItems,
+  ComponentColor,
+  ComponentSize,
+} from '@influxdata/clockface'
+import DashedButton from 'src/shared/components/dashed_button/DashedButton'
+
 const CheckThresholdsCard: FunctionComponent = () => {
-  return <div>Thresholds go here</div>
+  return (
+    <ComponentSpacer
+      direction={FlexDirection.Column}
+      alignItems={AlignItems.Stretch}
+      margin={ComponentSize.Medium}
+    >
+      <DashedButton
+        text="+ INFO"
+        color={ComponentColor.Success}
+        size={ComponentSize.Large}
+        onClick={() => {}}
+      />
+      <DashedButton
+        text="+ WARN"
+        color={ComponentColor.Warning}
+        size={ComponentSize.Large}
+        onClick={() => {}}
+      />
+      <DashedButton
+        text="+ CRIT"
+        color={ComponentColor.Danger}
+        size={ComponentSize.Large}
+        onClick={() => {}}
+      />
+    </ComponentSpacer>
+  )
 }
 
 export default CheckThresholdsCard
