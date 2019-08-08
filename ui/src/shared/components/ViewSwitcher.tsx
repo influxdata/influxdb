3a0d7258bff9a63c15ea48def492f4a8ee966702
Deniz Kusefoglu <deniz@influxdata.com>
2019-08-07 17:34:16 -0700

    Use CheckPlot to visualize check view

--- ui/src/shared/components/ViewSwitcher.tsx
+++ ui/src/shared/components/ViewSwitcher.tsx
@@ -12,6 +12,7 @@ import FluxTablesTransform from 'src/shared/components/FluxTablesTransform'
 import XYPlot from 'src/shared/components/XYPlot'
 import ScatterPlot from 'src/shared/components/ScatterPlot'
 import LatestValueTransform from 'src/shared/components/LatestValueTransform'
+import CheckPlot from 'src/shared/components/CheckPlot'
 
 // Types
 import {
@@ -153,6 +154,19 @@ const ViewSwitcher: FunctionComponent<Props> = ({
         >
           {config => <Plot config={config} />}
         </ScatterPlot>
+      )
+
+    case 'check':
+      return (
+        <CheckPlot
+          table={table}
+          fluxGroupKeyUnion={fluxGroupKeyUnion}
+          loading={loading}
+          timeZone={timeZone}
+          viewProperties={properties}
+        >
+          {config => <Plot config={config} />}
+        </CheckPlot>
       )
 
     default:
