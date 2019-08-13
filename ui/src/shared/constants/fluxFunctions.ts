1da2e5087afc3cc0a04b5ae93f5e6e375754e94f
Kelly <kelly@influxdata.com>
2019-08-13 14:53:37 -0700

    add date.week

--- ui/src/shared/constants/fluxFunctions.ts
+++ ui/src/shared/constants/fluxFunctions.ts
@@ -583,6 +583,23 @@ export const FLUX_FUNCTIONS: FluxToolbarFunction[] = [
       'https://v2.docs.influxdata.com/v2.0/reference/flux/functions/date/truncate/',
   },
   {
+    name: 'date.week',
+    args: [
+      {
+        name: 't',
+        desc: 'The time to operate on.',
+        type: 'Time',
+      },
+    ],
+    package: 'date',
+    desc: 
+      'Returns the ISO week of the year for a specified time. Results range from `[1-53]`.',
+    example: 'date.week(t: 2019-07-17T12:05:21.012Z)',
+    category: 'Transformations',
+    link:
+      'https://v2.docs.influxdata.com/v2.0/reference/flux/functions/date/week/',
+  },
+  {
     name: 'date.weekDay',
     args: [
       {
