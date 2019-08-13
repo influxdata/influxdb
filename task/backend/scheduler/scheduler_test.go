3fd94cbb6978f6de390f4300d1bf4a0acfe74a9d
j. Emrys Landivar (docmerlin) <landivar@gmail.com>
2019-08-12 22:19:39 -0500

    feat(task): new scheduler now with more tests

--- task/backend/scheduler/scheduler_test.go
+++ task/backend/scheduler/scheduler_test.go
@@ -6,27 +6,348 @@ import (
 	"testing"
 	"time"
 
-	"github.com/influxdata/influxdb/task/backend"
-
 	"github.com/influxdata/influxdb/task/backend/scheduler"
 )
 
 type mockExecutor struct {
 	sync.Mutex
+	fn  func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time)
+	Err error
 }
 
-func (e *mockExecutor) Execute(ctx context.Context, id scheduler.ID, scheduledAt time.Time) (backend.RunPromise, error) {
+type mockSchedulable struct {
+	id            scheduler.ID
+	schedule      scheduler.Schedule
+	offset        time.Duration
+	lastScheduled time.Time
+}
+
+func (s mockSchedulable) ID() scheduler.ID {
+	return s.id
+}
+
+func (s mockSchedulable) Schedule() scheduler.Schedule {
+	return s.schedule
+}
+func (s mockSchedulable) Offset() time.Duration {
+	return s.offset
+}
+func (s mockSchedulable) LastScheduled() time.Time {
+	return s.lastScheduled
+}
+
+func (e *mockExecutor) Execute(ctx context.Context, id scheduler.ID, scheduledAt time.Time) error {
+	done := make(chan struct{}, 1)
 	select {
 	case <-ctx.Done():
+	default:
+		e.fn(&sync.Mutex{}, ctx, id, scheduledAt)
+		done <- struct{}{}
 	}
+	return nil
+}
+
+type mockSchedulableService struct {
+	fn func(ctx context.Context, id scheduler.ID, t time.Time) error
+}
+
+func (m *mockSchedulableService) UpdateLastScheduled(ctx context.Context, id scheduler.ID, t time.Time) error {
+
+	return nil
 }
 
 func TestSchedule_Next(t *testing.T) {
+	t.Run("fires properly", func(t *testing.T) {
+		now := time.Now()
+		c := make(chan time.Time, 100)
+		exe := &mockExecutor{fn: func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time) {
+			select {
+			case <-ctx.Done():
+				t.Log("ctx done")
+			case c <- scheduledAt:
+			default:
+				t.Errorf("called the executor too many times")
+			}
+		}}
+		sch, _, err := scheduler.NewScheduler(
+			exe,
+			&mockSchedulableService{fn: func(ctx context.Context, id scheduler.ID, t time.Time) error {
+				return nil
+			}},
+			scheduler.WithMaxConcurrentWorkers(2))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer sch.Stop()
+		schedule, err := scheduler.NewSchedule("* * * * * * *")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = sch.Schedule(mockSchedulable{id: 1, schedule: schedule, offset: time.Second, lastScheduled: now.Add(-20 * time.Second)})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		select {
+		case <-c:
+		case <-time.After(10 * time.Second):
+			t.Fatal("test timed out", sch.Now().Unix(), sch.When().Unix())
+		}
+	})
+	t.Run("doesn't fire when the task isn't ready", func(t *testing.T) {
+		now := time.Now()
+		c := make(chan time.Time, 100)
+		exe := &mockExecutor{fn: func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time) {
+			select {
+			case <-ctx.Done():
+				t.Log("ctx done")
+			case c <- scheduledAt:
+			default:
+				t.Errorf("called the executor too many times")
+			}
+		}}
+		mockTime := scheduler.NewMockTime(now)
+		sch, _, err := scheduler.NewScheduler(
+			exe,
+			&mockSchedulableService{fn: func(ctx context.Context, id scheduler.ID, t time.Time) error {
+				return nil
+			}},
+			scheduler.WithTime(mockTime),
+			scheduler.WithMaxConcurrentWorkers(2))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer sch.Stop()
+		schedule, err := scheduler.NewSchedule("* * * * * * *")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = sch.Schedule(mockSchedulable{id: 1, schedule: schedule, offset: time.Second, lastScheduled: now.Add(time.Second)})
+		if err != nil {
+			t.Fatal(err)
+		}
+		go func() {
+			mockTime.Set(mockTime.T.Add(2 * time.Second))
+		}()
+
+		select {
+		case <-c:
+			t.Fatal("test timed out", sch.Now().Unix(), sch.When().Unix())
+		case <-time.After(2 * time.Second):
+		}
+
+	})
+
+	t.Run("fires the correct number of times for the interval with a single schedulable", func(t *testing.T) {
+		now := time.Now().UTC()
+		c := make(chan time.Time, 100)
+		exe := &mockExecutor{fn: func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time) {
+			select {
+			case <-ctx.Done():
+				t.Log("ctx done")
+			case c <- scheduledAt:
+			}
+		}}
+		mockTime := scheduler.NewMockTime(now)
+		sch, _, err := scheduler.NewScheduler(
+			exe,
+			&mockSchedulableService{fn: func(ctx context.Context, id scheduler.ID, t time.Time) error {
+				return nil
+			}},
+			scheduler.WithTime(mockTime),
+			scheduler.WithMaxConcurrentWorkers(20))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer sch.Stop()
+		schedule, err := scheduler.NewSchedule("* * * * * * *")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = sch.Schedule(mockSchedulable{id: 1, schedule: schedule, offset: time.Second, lastScheduled: now})
+		if err != nil {
+			t.Fatal(err)
+		}
+		go func() {
+			mockTime.Set(mockTime.T.Add(17 * time.Second))
+		}()
+
+		after := time.After(6 * time.Second)
+		for i := 0; i < 16; i++ {
+			select {
+			case <-c:
+			case <-after:
+				t.Fatalf("test timed out, only fired %d times but should have fired 16 times", i)
+			}
+		}
+
+		go func() {
+			mockTime.Set(mockTime.T.Add(2 * time.Second))
+		}()
+
+		after = time.After(6 * time.Second)
+
+		for i := 0; i < 2; i++ {
+			select {
+			case <-c:
+			case <-after:
+				t.Fatalf("test timed out, only fired %d times but should have fired 2 times", i)
+			}
+		}
+
+		select {
+		case <-c:
+			t.Fatalf("test scheduler fired too many times")
+		case <-time.After(2 * time.Second):
+		}
+	})
+
+	t.Run("fires the correct number of times for the interval with multiple schedulables", func(t *testing.T) {
+		now := time.Date(2016, 0, 0, 0, 1, 1, 0, time.UTC)
+		c := make(chan struct {
+			ts time.Time
+			id scheduler.ID
+		}, 100)
+		exe := &mockExecutor{fn: func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time) {
+			select {
+			case <-ctx.Done():
+				t.Log("ctx done")
+			case c <- struct {
+				ts time.Time
+				id scheduler.ID
+			}{
+				ts: scheduledAt,
+				id: id,
+			}:
+			}
+		}}
+		mockTime := scheduler.NewMockTime(now)
+		sch, _, err := scheduler.NewScheduler(
+			exe,
+			&mockSchedulableService{fn: func(ctx context.Context, id scheduler.ID, t time.Time) error {
+				return nil
+			}},
+			scheduler.WithTime(mockTime),
+			scheduler.WithMaxConcurrentWorkers(20))
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer sch.Stop()
+		schedule, err := scheduler.NewSchedule("* * * * * * *")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = sch.Schedule(mockSchedulable{id: 1, schedule: schedule, offset: time.Second, lastScheduled: now})
+		if err != nil {
+			t.Fatal(err)
+		}
+		schedule2, err := scheduler.NewSchedule("*/2 * * * * * *")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = sch.Schedule(mockSchedulable{id: 2, schedule: schedule2, offset: time.Second, lastScheduled: now})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		go func() {
+			mockTime.Set(mockTime.T.Add(17 * time.Second))
+		}()
+
+		after := time.After(6 * time.Second)
+		for i := 0; i < 24; i++ {
+			select {
+			case <-c:
+			case <-after:
+				t.Fatalf("test timed out, only fired %d times but should have fired 24 times", i)
+			}
+		}
+
+		go func() {
+			mockTime.Set(mockTime.T.Add(2 * time.Second))
+		}()
+
+		after = time.After(6 * time.Second)
+
+		for i := 0; i < 3; i++ {
+			select {
+			case <-c:
+			case <-after:
+				t.Fatalf("test timed out, only fired %d times but should have fired 3 times", i)
+			}
+		}
+
+		select {
+		case <-c:
+			t.Fatalf("test scheduler fired too many times")
+		case <-time.After(2 * time.Second):
+		}
+	})
+}
+
+func TestTreeScheduler_Stop(t *testing.T) {
 	now := time.Now().Add(-20 * time.Second)
-	exe := mockExecutor{}
-	sch, err := scheduler.NewScheduler(exe.Execute)
+	mockTime := scheduler.NewMockTime(now)
+	exe := &mockExecutor{fn: func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time) {}}
+	sch, _, err := scheduler.NewScheduler(exe, &mockSchedulableService{fn: func(ctx context.Context, id scheduler.ID, t time.Time) error {
+		return nil
+	}},
+		scheduler.WithTime(mockTime))
 	if err != nil {
 		t.Fatal(err)
 	}
-	sch.Schedule(1, "* * * * * * *", 10*time.Second, now.Add(20*time.Second))
+	sch.Stop()
+}
+
+func TestSchedule_panic(t *testing.T) {
+	// panics in the executor should be treated as errors
+	now := time.Now().UTC()
+	c := make(chan struct {
+		ts  time.Time
+		err error
+	}, 1)
+
+	exe := &mockExecutor{fn: func(l *sync.Mutex, ctx context.Context, id scheduler.ID, scheduledAt time.Time) {
+		panic("yikes oh no!")
+	}}
+
+	sch, _, err := scheduler.NewScheduler(
+		exe,
+		&mockSchedulableService{fn: func(ctx context.Context, id scheduler.ID, t time.Time) error {
+			return nil
+		}},
+		scheduler.WithMaxConcurrentWorkers(1), // to make debugging easier
+		scheduler.WithOnErrorFn(func(_ context.Context, _ scheduler.ID, ts time.Time, err error) {
+			c <- struct {
+				ts  time.Time
+				err error
+			}{
+				ts:  ts,
+				err: err,
+			}
+		}))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	schedule, err := scheduler.NewSchedule("* * * * * * *")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = sch.Schedule(mockSchedulable{id: 1, schedule: schedule, offset: time.Second, lastScheduled: now.Add(-20 * time.Second)})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	select {
+	case <-c: // panic was caught and error handler used
+	case <-time.After(10 * time.Second):
+		t.Fatal("test timed out", now.UTC().Unix(), sch.Now().Unix(), sch.When().Unix())
+	}
 }
--- task/backend/scheduler/treescheduler.go
+++ task/backend/scheduler/treescheduler.go
@@ -2,210 +2,256 @@ package scheduler
 
 import (
 	"context"
+	"encoding/binary"
 	"errors"
+	"math"
 	"sync"
 	"time"
 
+	"github.com/cespare/xxhash"
 	"github.com/google/btree"
-	"github.com/influxdata/cron"
-	"github.com/influxdata/influxdb/task/backend"
 )
 
-const cancelTimeOut = 30 * time.Second
+const (
+	maxWaitTime          = time.Hour
+	degreeBtreeScheduled = 3
+	defaultMaxWorkers    = 32
+)
 
-const defaultMaxRunsOutstanding = 1 << 16
-
-type runningItem struct {
-	cancel func()
-	runID  ID
-	taskID ID
-}
-
-func (it runningItem) Less(bItem btree.Item) bool {
-	it2 := bItem.(runningItem)
-	return it.taskID < it2.taskID || (it.taskID == it2.taskID && it.runID < it2.runID)
-}
-
-// TreeScheduler is a Scheduler based on a btree
+// TreeScheduler is a Scheduler based on a btree.
+// It calls Executor in-order per ID.  That means you are guaranteed that for a specific ID,
+//
+// If a call to an Executorfunc returns an *ErrRetry then all calls to Executor of the entire task will be delayed
+// temporarily by the amount specified in *ErrRetry, but future calls to Executor for that task will proceed normally.
+//
+// - The scheduler should, after creation, automatically call ExecutorFunc, when a task should run as defined by its Schedulable.
+//
+// - the scheduler's should not be able to get into a state where blocks Release and Schedule indefinitely.
+//
+// - Schedule should add a Schedulable to being scheduled, and Release should remove a task from being scheduled.
+//
+// - Calling of ExecutorFunc should be serial in time on a per taskID basis. I.E.: the run at 12:00 will go before the run at 12:01.
+//
+// Design:
+//
+// The core of the scheduler is a btree keyed by time, a nonce, and a task ID, and a map keyed by task ID and containing a
+// nonce and a time (called a uniqueness index from now on).
+// The map is to ensure task uniqueness in the tree, so we can replace or delete tasks in the tree.
+//
+// Scheduling in the tree consists of a main loop that feeds a fixed set of workers, each with their own communication channel.
+// Distribution is handled by hashing the TaskID (to ensure uniform distribution) and then distributing over those channels
+// evenly based on the hashed ID.  This is to ensure that all tasks of the same ID go to the same worker.
+//
+//The workers call ExecutorFunc handle any errors and update the LastScheduled time internally and also via the Checkpointer.
+//
+// The main loop:
+//
+// The main loop waits on a time.Timer to grab the task with the minimum time.  Once it successfully grabs a task ready
+// to trigger, it will start walking the btree from the item nearest
+//
+// Putting a task into the scheduler:
+//
+// Adding a task to the scheduler acquires a write lock, grabs the task from the uniqueness map, and replaces the item
+// in the uniqueness index and btree.  If new task would trigger sooner than the current soonest triggering task, it
+// replaces the Timer when added to the scheduler.  Finally it releases the write lock.
+//
+// Removing a task from the scheduler:
+//
+// Removing a task from the scheduler acquires a write lock, deletes the task from the uniqueness index and from the
+// btree, then releases the lock.  We do not have to readjust the time on delete, because, if the minimum task isn't
+// ready yet, the main loop just resets the timer and keeps going.
 type TreeScheduler struct {
 	sync.RWMutex
-	scheduled btree.BTree
-	running   btree.BTree
-	nextTime  map[ID]int64 // we need this index so we can delete items from the scheduled
-	when      time.Time
-	executor  func(ctx context.Context, id ID, scheduledAt time.Time) (backend.RunPromise, error)
-	onErr     func(ctx context.Context, id ID, scheduledAt time.Time, err error) bool
-	timer     *time.Timer
-	done      chan struct{}
-	sema      chan struct{}
-	wg        sync.WaitGroup
+	scheduled    *btree.BTree
+	nextTime     map[ID]ordering // we need this index so we can delete items from the scheduled
+	when         time.Time
+	executor     Executor
+	onErr        ErrorFunc
+	time         Time
+	timer        Timer
+	done         chan struct{}
+	workchans    []chan Item
+	wg           sync.WaitGroup
+	checkpointer SchedulableService
 
 	sm *SchedulerMetrics
 }
 
-// clearTask is a method for deleting a range of tasks.
-// TODO(docmerlin): add an actual ranged delete to github.com/google/btree
-func (s *TreeScheduler) clearTask(taskID ID) btree.ItemIterator {
-	return func(i btree.Item) bool {
-		del := i.(runningItem).taskID == taskID
-		if !del {
-			return false
-		}
-		s.running.Delete(runningItem{taskID: taskID})
-		return true
-	}
-}
+type ExecutorFunc func(ctx context.Context, id ID, scheduledAt time.Time) error
 
-// clearTask is a method for deleting a range of tasks.
-func (s *TreeScheduler) runs(taskID ID, limit int) btree.ItemIterator {
-
-	acc := make([]ID, 0, limit)
-
-	return func(i btree.Item) bool {
-		match := i.(runningItem).taskID == taskID
-		if !match {
-			return false
-		}
-
-		return true
-	}
-}
-
-const maxWaitTime = 1000000 * time.Hour
-
-type ExecutorFunc func(ctx context.Context, id ID, scheduledAt time.Time) (backend.RunPromise, error)
-
-type ErrorFunc func(ctx context.Context, id ID, scheduledAt time.Time, err error) bool
+type ErrorFunc func(ctx context.Context, taskID ID, scheduledAt time.Time, err error)
 
 type treeSchedulerOptFunc func(t *TreeScheduler) error
 
-func WithOnErrorFn(fn func(ctx context.Context, id ID, scheduledAt time.Time, err error) bool) treeSchedulerOptFunc {
+func WithOnErrorFn(fn ErrorFunc) treeSchedulerOptFunc {
 	return func(t *TreeScheduler) error {
 		t.onErr = fn
 		return nil
 	}
 }
 
-func WithMaxRunsOutsanding(n int) treeSchedulerOptFunc {
+func WithMaxConcurrentWorkers(n int) treeSchedulerOptFunc {
 	return func(t *TreeScheduler) error {
-		t.sema = make(chan struct{}, n)
+		t.workchans = make([]chan Item, n)
+		return nil
+	}
+}
+
+func WithTime(t Time) treeSchedulerOptFunc {
+	return func(sch *TreeScheduler) error {
+		sch.time = t
 		return nil
 	}
 }
 
 // Executor is any function that accepts an ID, a time, and a duration.
-// OnErr is a function that takes am error, it is called when we cannot find a viable time before jan 1, 2100.  The default behavior is to drop the task on error.
-func NewScheduler(Executor ExecutorFunc, opts ...treeSchedulerOptFunc) (*TreeScheduler, *SchedulerMetrics, error) {
+func NewScheduler(executor Executor, checkpointer SchedulableService, opts ...treeSchedulerOptFunc) (*TreeScheduler, *SchedulerMetrics, error) {
 	s := &TreeScheduler{
-		executor: Executor,
-		onErr:    func(_ context.Context, _ ID, _ time.Time, _ error) bool { return true },
-		sema:     make(chan struct{}, defaultMaxRunsOutstanding),
+		executor:     executor,
+		scheduled:    btree.New(degreeBtreeScheduled),
+		nextTime:     map[ID]ordering{},
+		onErr:        func(_ context.Context, _ ID, _ time.Time, _ error) {},
+		time:         stdTime{},
+		done:         make(chan struct{}, 1),
+		checkpointer: checkpointer,
 	}
 
 	// apply options
 	for i := range opts {
 		if err := opts[i](s); err != nil {
-			return nil, err
+			return nil, nil, err
 		}
+	}
+	if s.workchans == nil {
+		s.workchans = make([]chan Item, defaultMaxWorkers)
+
+	}
+
+	s.wg.Add(len(s.workchans))
+	for i := 0; i < len(s.workchans); i++ {
+		s.workchans[i] = make(chan Item)
+		go s.work(i)
 	}
 
 	s.sm = NewSchedulerMetrics(s)
-	s.when = time.Now().Add(maxWaitTime)
-	s.timer = time.NewTimer(time.Until(s.when)) //time.Until(s.when))
-	if Executor == nil {
-		return nil, errors.New("Executor must be a non-nil function")
+	s.when = s.time.Now().Add(maxWaitTime)
+	s.timer = s.time.NewTimer(maxWaitTime)
+	if executor == nil {
+		return nil, nil, errors.New("Executor must be a non-nil function")
 	}
+	s.wg.Add(1)
 	go func() {
+		defer s.wg.Done()
+	schedulerLoop:
 		for {
 			select {
 			case <-s.done:
 				s.Lock()
 				s.timer.Stop()
+				// close workchans
+				for i := range s.workchans {
+					close(s.workchans[i])
+				}
 				s.Unlock()
-				close(s.sema)
 				return
-			case <-s.timer.C:
-				iti := s.scheduled.DeleteMin()
-				if iti == nil {
-					s.Lock()
-					s.timer.Reset(maxWaitTime)
+			case <-s.timer.C():
+				s.Lock()
+				min := s.scheduled.Min()
+				if min == nil { // grab a new item, because there could be a different item at the top of the queue
+					s.when = s.time.Now().Add(maxWaitTime)
+					s.timer.Reset(maxWaitTime) // we can reset without stop, because its fired.
 					s.Unlock()
-					continue
+					continue schedulerLoop
 				}
-				if iti == nil {
-					s.Lock()
-					s.timer.Reset(maxWaitTime)
+				it := min.(Item)
+				if it.when > s.when.UTC().Unix() {
 					s.Unlock()
-					continue
+					continue schedulerLoop
 				}
-				it := iti.(item)
-				s.sm.startExecution(it.id, time.Since(time.Unix(it.next, 0)))
-				if prom, err := s.executor(context.Background(), it.id, time.Unix(it.next, 0)); err == nil {
-					t, err := it.cron.Next(time.Unix(it.next, 0))
-					it.next = t.Unix()
-					// we need to return the item to the scheduled before calling s.onErr
-					if err != nil {
-						it.nonce++
-						s.onErr(context.TODO(), it.id, time.Unix(it.next, 0), err)
-					}
-					s.scheduled.ReplaceOrInsert(it)
-					if prom == nil {
-						break
-					}
-					run := prom.Run()
-					s.Lock()
-					s.running.ReplaceOrInsert(runningItem{cancel: prom.Cancel, runID: ID(run.RunID), taskID: ID(run.TaskID)})
+				s.process()
+				min = s.scheduled.Min()
+				if min == nil { // grab a new item, because there could be a different item at the top of the queue after processing
+					s.when = s.time.Now().Add(maxWaitTime)
+					s.timer.Reset(maxWaitTime) // we can reset without stop, because its fired.
 					s.Unlock()
-
-					s.wg.Add(1)
-
-					s.sema <- struct{}{}
-					go func() {
-						defer func() {
-							s.wg.Done()
-							<-s.sema
-						}()
-						res, err := prom.Wait()
-						if err != nil {
-							s.onErr(context.TODO(), it.id, time.Unix(it.next, 0), err)
-						}
-						// belt and suspenders
-						if res == nil {
-							return
-						}
-						run := prom.Run()
-						s.Lock()
-						s.running.Delete(runningItem{cancel: prom.Cancel, runID: ID(run.RunID), taskID: ID(run.TaskID)})
-						s.Unlock()
-
-						s.sm.finishExecution(it.id, res.Err() == nil, time.Since(time.Unix(it.next, 0)))
-
-						if err = res.Err(); err != nil {
-							s.onErr(context.TODO(), it.id, time.Unix(it.next, 0), err)
-							return
-						}
-						// TODO(docmerlin); handle statistics on the run
-					}()
-				} else if err != nil {
-					s.onErr(context.Background(), it.id, time.Unix(it.next, 0), err)
+					continue schedulerLoop
 				}
+				it = min.(Item)
+				s.when = time.Unix(it.when, 0)
+				until := s.time.Until(s.when)
+				s.timer.Reset(until) // we can reset without stop, because its fired.
+				s.Unlock()
 			}
 		}
 	}()
-	return s, nil
+	return s, s.sm, nil
 }
 
 func (s *TreeScheduler) Stop() {
-	s.RLock()
-	semaCap := cap(s.sema)
-	s.RUnlock()
-	s.done <- struct{}{}
-
-	// this is to make sure the semaphore is closed.  It tries to pull cap+1 empty structs from the semaphore, only possible when closed
-	for i := 0; i <= semaCap; i++ {
-		<-s.sema
-	}
+	s.Lock()
+	close(s.done)
+	s.Unlock()
 	s.wg.Wait()
+}
+
+type unsent struct {
+	items []Item
+}
+
+func (u *unsent) append(i Item) {
+	u.items = append(u.items, i)
+}
+
+func (s *TreeScheduler) process() {
+	iter, toReAdd := s.iterator(s.time.Now())
+	s.scheduled.Ascend(iter)
+	for i := range toReAdd.items {
+		s.nextTime[toReAdd.items[i].id] = toReAdd.items[i].ordering
+		s.scheduled.ReplaceOrInsert(toReAdd.items[i])
+	}
+}
+
+func (s *TreeScheduler) iterator(ts time.Time) (btree.ItemIterator, *unsent) {
+	itemsToPlace := &unsent{}
+	return func(i btree.Item) bool {
+		if i == nil {
+			return false
+		}
+		it := i.(Item) // we want it to panic if things other than Items are populating the scheduler, as it is something we can't recover from.
+		if time.Unix(it.next+it.Offset, 0).After(ts) {
+			return false
+		}
+		// distribute to the right worker.
+		{
+			buf := [8]byte{}
+			binary.LittleEndian.PutUint64(buf[:], uint64(it.id))
+			wc := xxhash.Sum64(buf[:]) % uint64(len(s.workchans)) // we just hash so that the number is uniformly distributed
+			select {
+			case s.workchans[wc] <- it:
+				s.scheduled.Delete(it)
+				if err := it.updateNext(); err != nil {
+					s.onErr(context.Background(), it.id, it.Next(), err)
+				}
+				itemsToPlace.append(it)
+
+			case <-s.done:
+				return false
+			default:
+				s.scheduled.Delete(it)
+				it.incrementNonce()
+				itemsToPlace.append(it)
+				return true
+			}
+		}
+		return true
+	}, itemsToPlace
+}
+
+func (s *TreeScheduler) Now() time.Time {
+	s.RLock()
+	now := s.time.Now().UTC()
+	s.RUnlock()
+	return now
 }
 
 // When gives us the next time the scheduler will run a task.
@@ -216,86 +262,142 @@ func (s *TreeScheduler) When() time.Time {
 	return w
 }
 
+func (s *TreeScheduler) release(taskID ID) {
+	ordering, ok := s.nextTime[taskID]
+	if !ok {
+		return
+	}
+
+	// delete the old task run time
+	s.scheduled.Delete(Item{id: taskID, ordering: ordering})
+	delete(s.nextTime, taskID)
+}
+
 // Release releases a task, if it doesn't own the task it just returns.
 // Release also cancels the running task.
 // Task deletion would be faster if the tree supported deleting ranges.
-func (s *TreeScheduler) Release(taskID ID) error {
+func (s *TreeScheduler) Release(taskID ID) {
 	s.sm.release(taskID)
 	s.Lock()
-	defer s.Unlock()
-	nextTime, ok := s.nextTime[taskID]
-	if !ok {
-		return nil
-	}
-
-	// delete the old task run time
-	s.scheduled.Delete(item{
-		next: nextTime,
-		id:   taskID,
-	})
-
-	s.running.AscendGreaterOrEqual(runningItem{taskID: taskID}, s.clearTask(taskID))
-	return nil
+	s.release(taskID)
+	s.Unlock()
 }
 
-// put puts an Item on the TreeScheduler.
-func (s *TreeScheduler) Schedule(id ID, cronString string, offset time.Duration, since time.Time) error {
-	s.sm.schedule(taskID)
-	crSch, err := cron.ParseUTC(cronString)
+// work does work and reschedules the work as necessary.
+// it handles the resceduling, because we need to be able to reschedule based on executor error
+func (s *TreeScheduler) work(i int) {
+	var it Item
+	defer func() {
+		s.wg.Done()
+	}()
+	for it = range s.workchans[i] {
+		t := time.Unix(it.next, 0)
+		err := func() (err error) {
+			defer func() {
+				if r := recover(); r != nil {
+					err = &ErrUnrecoverable{errors.New("Executor panicked")}
+				}
+			}()
+			return s.executor.Execute(context.Background(), it.id, t)
+		}()
+		if err != nil {
+			s.onErr(context.Background(), it.id, it.Next(), err)
+		}
+		if err := s.checkpointer.UpdateLastScheduled(context.TODO(), it.id, t); err != nil {
+			s.onErr(context.Background(), it.id, it.Next(), err)
+		}
+	}
+}
+
+// Schedule put puts a Schedulable on the TreeScheduler.
+func (s *TreeScheduler) Schedule(sch Schedulable) error {
+	s.sm.schedule(sch.ID())
+	it := Item{
+		cron:   sch.Schedule(),
+		id:     sch.ID(),
+		Offset: int64(sch.Offset().Seconds()),
+		//last:   sch.LastScheduled().Unix(),
+	}
+	nt, err := it.cron.Next(sch.LastScheduled())
 	if err != nil {
 		return err
 	}
-	nt, err := crSch.Next(since)
-	if err != nil {
-		return err
-	}
-	it := item{
-		cron: crSch,
-		next: nt.Add(offset).Unix(),
-		id:   id,
-	}
+	it.next = nt.UTC().Unix()
+	it.ordering.when = it.next + it.Offset
+
 	s.Lock()
 	defer s.Unlock()
-	nextTime, ok := s.nextTime[id]
-	if !ok {
-		s.scheduled.ReplaceOrInsert(it)
-		return nil
-	}
 
-	if s.when.Before(nt) {
+	nt = nt.Add(sch.Offset())
+	if s.when.After(nt) {
 		s.when = nt
-		s.timer.Reset(time.Until(s.when))
+		s.timer.Stop()
+		until := s.time.Until(s.when)
+		if until <= 0 {
+			s.timer.Reset(0)
+		} else {
+			s.timer.Reset(s.time.Until(s.when))
+		}
 	}
+	nextTime, ok := s.nextTime[it.id]
 
-	// delete the old task run time
-	s.scheduled.Delete(item{
-		next: nextTime,
-		id:   id,
-	})
+	if ok {
+		// delete the old task run time
+		s.scheduled.Delete(Item{
+			ordering: nextTime,
+			id:       it.id,
+		})
+	}
+	s.nextTime[it.id] = ordering{when: it.next + it.Offset + it.wait}
 
 	// insert the new task run time
 	s.scheduled.ReplaceOrInsert(it)
 	return nil
 }
 
-func (s *TreeScheduler) Runs(taskID ID) {
-	s.RLock()
-	defer s.RUnlock()
-	s.running.AscendGreaterOrEqual(runningItem{taskID: 0})
+var maxItem = Item{
+	ordering: ordering{
+		when:  math.MaxInt64,
+		nonce: int(^uint(0) >> 1),
+	},
+	id: maxID,
+}
 
+type ordering struct {
+	when  int64
+	nonce int // for retries
+}
+
+func (k *ordering) incrementNonce() {
+	k.nonce++
 }
 
 // Item is a task in the scheduler.
-type item struct {
-	cron   cron.Parsed
-	next   int64
-	nonce  int // for retries
-	offset int
+type Item struct {
+	ordering
 	id     ID
+	cron   Schedule
+	next   int64
+	wait   int64
+	Offset int64
+}
+
+func (it Item) Next() time.Time {
+	return time.Unix(it.next, 0)
 }
 
 // Less tells us if one Item is less than another
-func (it item) Less(bItem btree.Item) bool {
-	it2 := bItem.(item)
-	return it.next < it2.next || (it.next == it2.next && (it.nonce < it2.nonce || it.nonce == it2.nonce && it.id < it2.id))
+func (it Item) Less(bItem btree.Item) bool {
+	it2 := bItem.(Item)
+	return it.when < it2.when || (it.when == it2.when && (it.nonce < it2.nonce || it.nonce == it2.nonce && it.id < it2.id))
+}
+
+func (it *Item) updateNext() error {
+	newNext, err := it.cron.Next(time.Unix(it.next, 0))
+	if err != nil {
+		return err
+	}
+	it.next = newNext.UTC().Unix()
+	it.when = it.next + it.Offset
+	return nil
 }
