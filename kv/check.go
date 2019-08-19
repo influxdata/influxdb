fdcce1eb81eda94028e8c728a7723e64c1b6f1a6
Jade McGough <jade@thezets.com>
2019-08-19 01:10:31 -0700

    fix(kv): check createdat and updatedat should be identical

--- kv/check.go
+++ kv/check.go
@@ -320,8 +320,9 @@ func (s *Service) createCheck(ctx context.Context, tx Tx, c influxdb.Check, user
 
 	c.SetID(s.IDGenerator.ID())
 	c.SetOwnerID(userID)
-	c.SetCreatedAt(s.Now())
-	c.SetUpdatedAt(s.Now())
+	now := s.Now()
+	c.SetCreatedAt(now)
+	c.SetUpdatedAt(now)
 
 	t, err := s.createCheckTask(ctx, tx, c)
 	if err != nil {
