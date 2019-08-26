2768aa3712ccfa40c95a917d350eeea47d4883e6
Andrew Watkins <watts@influxdb.com>
2019-08-26 14:12:44 -0700

    test(cypress): downgrade cypress

--- ui/cypress/e2e/notificationRules.test.ts
+++ ui/cypress/e2e/notificationRules.test.ts
@@ -1,7 +1,6 @@
 import {SlackNotificationEndpoint} from '../../src/types'
 
-// TODO(watts): remove skip once the ownerID stuff has been sorted out
-describe.skip('NotificationRules', () => {
+describe('NotificationRules', () => {
   const name1 = 'Slack 1'
   const name2 = 'Slack 2'
   const name3 = 'Slack 3'
