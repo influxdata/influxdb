8bd18ffe8859658cdb4ed0718ebb9c0a935a6531
Alex P <thealexpaxton@gmail.com>
2019-08-30 16:47:03 -0700

    refactor(ui): wrap entire UI in Clockface AppWrapper

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,23 +1,25 @@
 import React, {SFC, ReactChildren} from 'react'
 
+import {AppWrapper} from '@influxdata/clockface'
 import RightClickLayer from 'src/clockface/components/right_click_menu/RightClickLayer'
 import Nav from 'src/pageLayout'
 import TooltipPortal from 'src/portals/TooltipPortal'
 import NotesPortal from 'src/portals/NotesPortal'
-import Notifications from 'src/shared/containers/Notifications'
+import Notifications from 'src/shared/components/notifications/Notifications'
 
 interface Props {
   children: ReactChildren
 }
 
 const App: SFC<Props> = ({children}) => (
-  <Notifications>
+  <AppWrapper>
+    <Notifications />
     <RightClickLayer />
-    <Nav />
     <TooltipPortal />
     <NotesPortal />
+    <Nav />
     {children}
-  </Notifications>
+  </AppWrapper>
 )
 
 export default App
