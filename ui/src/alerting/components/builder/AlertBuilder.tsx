cdf4dd6ac9d5cacf6610064fabf0d3fecf7f7d93
Alex P <thealexpaxton@gmail.com>
2019-09-06 15:44:27 -0700

    refactor(ui): remove matching rules card

--- ui/src/alerting/components/builder/AlertBuilder.tsx
+++ ui/src/alerting/components/builder/AlertBuilder.tsx
@@ -3,11 +3,9 @@ import React, {FC} from 'react'
 
 // Components
 import FancyScrollbar from 'src/shared/components/fancy_scrollbar/FancyScrollbar'
-import BuilderCard from 'src/timeMachine/components/builderCard/BuilderCard'
 import CheckMetaCard from 'src/alerting/components/builder/CheckMetaCard'
 import CheckMessageCard from 'src/alerting/components/builder/CheckMessageCard'
 import CheckConditionsCard from 'src/alerting/components/builder/CheckConditionsCard'
-import CheckMatchingRulesCard from 'src/alerting/components/builder/CheckMatchingRulesCard'
 
 const AlertBuilder: FC = () => {
   return (
@@ -18,16 +16,6 @@ const AlertBuilder: FC = () => {
             <CheckMetaCard />
             <CheckMessageCard />
             <CheckConditionsCard />
-            <BuilderCard
-              testID="builder-matching-rules"
-              widthPixels={420}
-              className="alert-builder--card"
-            >
-              <BuilderCard.Header title="Matching Notification Rules" />
-              <BuilderCard.Body addPadding={true} autoHideScrollbars={true}>
-                <CheckMatchingRulesCard />
-              </BuilderCard.Body>
-            </BuilderCard>
           </div>
         </FancyScrollbar>
       </div>
