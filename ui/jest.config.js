2aad40a2439861485ca84836a07e74202f76f406
Alex Boatwright <aboatwright@influxdata.com>
2019-10-08 15:04:06 -0700

    chore(ui): relocate testing configuration
    while exploring containerization of the frontend, i decided to remove the
    existing environment configuration from package.json and push them into
    external files in an attempt to improve the caching strategies available
    for local development. the CI pipeline should be watching the lock file,
    but changes to the testing environment locally shouldn't cause a rebuild
    of node_modules.

---
+++ ui/jest.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    './jestSetup.ts'
+  ],
+  displayName: 'test',
+  testURL: 'http://localhost',
+  testPathIgnorePatterns: [
+    'build',
+    '<rootDir>/node_modules/(?!(jest-test))',
+    'cypress'
+  ],
+  setupFiles: [
+    '<rootDir>/testSetup.ts'
+  ],
+  modulePaths: [
+    '<rootDir>',
+    '<rootDir>/node_modules'
+  ],
+  moduleDirectories: [
+    'src'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test))\\.(ts?|tsx?)$',
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer'
+  ],
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json'
+    }
+  }
+}
