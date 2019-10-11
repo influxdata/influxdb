c8185993b19533518d77fe676465a49da440d280
Alex Boatwright <aboatwright@influxdata.com>
2019-10-11 09:58:55 -0700

    chore(ui): get non ts script through the tests AND the build

--- ui/src/utils/env.ts
+++ ui/src/utils/env.ts
@@ -1,55 +1,57 @@
-const GIT_SHA =
-  process.env.INFLUXDB_SHA ||
-  require('child_process')
-    .execSync('git rev-parse HEAD')
-    .toString()
+module.exports = (() => {
+  const GIT_SHA =
+    process.env.INFLUXDB_SHA ||
+    require('child_process')
+      .execSync('git rev-parse HEAD')
+      .toString()
 
-// Webpack has some specific rules about formatting
-// lets protect our developers from that!
-function formatStatic(dir) {
-  if (!dir.length) {
-    return dir
+  // Webpack has some specific rules about formatting
+  // lets protect our developers from that!
+  function formatStatic(dir) {
+    if (!dir.length) {
+      return dir
+    }
+
+    // NOTE: slices for sideeffect free editing (alex)
+    let _dir = dir.slice(0)
+
+    if (_dir[0] === '/') {
+      _dir = _dir.slice(1)
+    }
+
+    if (_dir[_dir.length - 1] === '/') {
+      _dir = _dir.slice(0, -1)
+    }
+
+    return _dir
   }
 
-  // NOTE: slices for sideeffect free editing (alex)
-  let _dir = dir.slice(0)
+  // Webpack has some specific rules about formatting
+  // lets protect our developers from that!
+  function formatBase(prefix) {
+    if (prefix === '/') {
+      return prefix
+    }
 
-  if (_dir[0] === '/') {
-    _dir = _dir.slice(1)
+    // NOTE: slices for sideeffect free editing (alex)
+    let _prefix = prefix.slice(0)
+
+    if (_prefix[0] !== '/') {
+      _prefix = '/' + _prefix
+    }
+
+    if (_prefix[_prefix.length - 1] !== '/') {
+      _prefix = _prefix + '/'
+    }
+
+    return _prefix
   }
 
-  if (_dir[_dir.length - 1] === '/') {
-    _dir = _dir.slice(0, -1)
-  }
+  const STATIC_DIRECTORY = formatStatic(process.env.STATIC_DIRECTORY || '')
+  const BASE_PATH = '/'
+  // TODO: adding a basePath feature in the @influxdata/oats
+  // project is currently required before turning this on (alex)
+  //const BASE_PATH = formatBase(process.env.BASE_PATH || '/')
 
-  return _dir
-}
-
-// Webpack has some specific rules about formatting
-// lets protect our developers from that!
-function formatBase(prefix) {
-  if (prefix === '/') {
-    return prefix
-  }
-
-  // NOTE: slices for sideeffect free editing (alex)
-  let _prefix = prefix.slice(0)
-
-  if (_prefix[0] !== '/') {
-    _prefix = '/' + _prefix
-  }
-
-  if (_prefix[_prefix.length - 1] !== '/') {
-    _prefix = _prefix + '/'
-  }
-
-  return _prefix
-}
-
-const STATIC_DIRECTORY = formatStatic(process.env.STATIC_DIRECTORY || '')
-const BASE_PATH = '/'
-// TODO: adding a basePath feature in the @influxdata/oats
-// project is currently required before turning this on (alex)
-//const BASE_PATH = formatBase(process.env.BASE_PATH || '/')
-
-export {formatStatic, formatBase, GIT_SHA, STATIC_DIRECTORY, BASE_PATH}
+  return {formatStatic, formatBase, GIT_SHA, STATIC_DIRECTORY, BASE_PATH}
+})()
