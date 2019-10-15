a947a4eeb003bc13a2072908afbec9ea80520da3
Russ Savage <russorat@users.noreply.github.com>
2019-10-15 16:26:14 -0700

    feat(clientLibs): Add Client Libs page (#15413)
    * feat(clientLibs): fix icons, add go and c#
    
    * feat(clientLibs): fix go client overlay
    
    * feat(clientLibs): update remaining client libs
    
    * feat(clientLibs): remove feature flag
    
    * fix(telegraf): fix broken test
    
    * fix(clientLibs): removing unused featureflag class
    
    * feat(clientLibs): addressing pr feedback
    
    * feat(clientLibs): fixing wording and formatting
    
    * feat(clientLibs): update the text on JS page

--- ui/src/clientLibraries/components/ClientJSOverlay.tsx
+++ ui/src/clientLibraries/components/ClientJSOverlay.tsx
@@ -3,12 +3,19 @@ import React, {FunctionComponent} from 'react'
 
 // Components
 import ClientLibraryOverlay from 'src/clientLibraries/components/ClientLibraryOverlay'
+import CodeSnippet from 'src/shared/components/CodeSnippet'
 
 // Constants
 import {clientJSLibrary} from 'src/clientLibraries/constants'
 
 const ClientJSOverlay: FunctionComponent<{}> = () => {
-  const {name, url} = clientJSLibrary
+  const {
+    name,
+    url,
+    initializeClientCodeSnippet,
+    executeQueryCodeSnippet,
+    writingDataLineProtocolCodeSnippet,
+  } = clientJSLibrary
 
   return (
     <ClientLibraryOverlay title={`${name} Client Library`}>
@@ -19,35 +26,18 @@ const ClientJSOverlay: FunctionComponent<{}> = () => {
         </a>
       </p>
       <br />
-      <h5>Initializing the Client</h5>
-      <pre>
-        <code>import Client from '@influxdata/influx'</code>
-        <br />
-        <code>const client = new Client('basepath', 'token')</code>
-      </pre>
-      <h5>Using the client to execute a query</h5>
-      <pre>
-        <code>
-          const query = 'from(bucket: "my_bucket") |> range(start: -1h)'
-        </code>
-        <br />
-        <code>
-          const &#123;promise, cancel&#125; =
-          client.queries.execute('someorgid', query)
-        </code>
-        <br />
-        <code>const csv = await promise</code>
-        <br />
-        <code>cancel() // Cancels request</code>
-      </pre>
-      <h5>Writing Data</h5>
-      <p>Data should be written using InfluxDB Line Protocol</p>
-      <pre>
-        <code>const data = '' // Line protocal string</code>
-        <code>
-          const response = await client.write.create('orgID', 'bucketID', data)
-        </code>
-      </pre>
+      <h5>Initialize the Client</h5>
+      <CodeSnippet
+        copyText={initializeClientCodeSnippet}
+        label="JavaScript Code"
+      />
+      <h5>Write Data</h5>
+      <CodeSnippet
+        copyText={writingDataLineProtocolCodeSnippet}
+        label="JavaScript Code"
+      />
+      <h5>Execute a Flux query</h5>
+      <CodeSnippet copyText={executeQueryCodeSnippet} label="JavaScript Code" />
     </ClientLibraryOverlay>
   )
 }
