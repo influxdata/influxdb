b828d6452a4892622f1c1d0d0ed71f6d5d0812db
Alex P <thealexpaxton@gmail.com>
2019-10-15 16:03:05 -0700

    refactor: update instance of question mark tooltip

--- ui/src/timeMachine/components/EditorShortcutsTooltip.tsx
+++ ui/src/timeMachine/components/EditorShortcutsTooltip.tsx
@@ -1,12 +1,14 @@
+// Libraries
 import React, {FC} from 'react'
 
-import QuestionMarkTooltip from 'src/shared/components/question_mark_tooltip/QuestionMarkTooltip'
+// Components
+import {QuestionMarkTooltip} from '@influxdata/clockface'
 
 const EditorShortcutsTooltip: FC = () => {
   return (
     <QuestionMarkTooltip
       testID="editor-shortcuts"
-      tipContent={
+      tooltipContents={
         <div className="editor-shortcuts">
           <h5>Shortcuts</h5>
           <dl className="editor-shortcuts--body">
