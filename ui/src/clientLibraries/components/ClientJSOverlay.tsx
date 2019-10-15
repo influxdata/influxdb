f2a3e6e805bf2311661363f0bb330637f7b7f775
Russ Savage <russ@influxdata.com>
2019-10-15 15:58:44 -0700

    feat(clientLibs): update the text on JS page

--- ui/src/clientLibraries/components/ClientJSOverlay.tsx
+++ ui/src/clientLibraries/components/ClientJSOverlay.tsx
@@ -26,17 +26,17 @@ const ClientJSOverlay: FunctionComponent<{}> = () => {
         </a>
       </p>
       <br />
-      <h5>Initializing the Client</h5>
+      <h5>Initialize the Client</h5>
       <CodeSnippet
         copyText={initializeClientCodeSnippet}
         label="JavaScript Code"
       />
-      <h5>Writing Data</h5>
+      <h5>Write Data</h5>
       <CodeSnippet
         copyText={writingDataLineProtocolCodeSnippet}
         label="JavaScript Code"
       />
-      <h5>Example for executing a Flux query</h5>
+      <h5>Execute a Flux query</h5>
       <CodeSnippet copyText={executeQueryCodeSnippet} label="JavaScript Code" />
     </ClientLibraryOverlay>
   )
