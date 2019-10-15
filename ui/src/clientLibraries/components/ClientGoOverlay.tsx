1b567448c6337a9aa3a7ffd94d19203c6d1a3d89
Russ Savage <russ@influxdata.com>
2019-10-15 09:31:57 -0700

    feat(clientLibs): fix go client overlay

--- ui/src/clientLibraries/components/ClientGoOverlay.tsx
+++ ui/src/clientLibraries/components/ClientGoOverlay.tsx
@@ -9,37 +9,12 @@ import {clientGoLibrary} from 'src/clientLibraries/constants'
 import CodeSnippet from 'src/shared/components/CodeSnippet'
 
 const ClientGoOverlay: FunctionComponent<{}> = () => {
-  const {name, url} = clientGoLibrary
-  const initializeClientCodeSnippet = `
-    influx, err := influxdb.New(myHTTPInfluxAddress, myToken, influxdb.WithHTTPClient(myHTTPClient))
-	  if err != nil {
-		  panic(err) // error handling here; normally we wouldn't use fmt but it works for the example
-    }
-    // Add your app code here
-    influx.Close() // closes the client.  After this the client is useless.
-  `
-  const writeDataCodeSnippet = `
-    // we use client.NewRowMetric for the example because it's easy, but if you need extra performance
-    // it is fine to manually build the []client.Metric{}.
-    myMetrics := []influxdb.Metric{
-      influxdb.NewRowMetric(
-        map[string]interface{}{"memory": 1000, "cpu": 0.93},
-        "system-metrics",
-        map[string]string{"hostname": "hal9000"},
-        time.Date(2018, 3, 4, 5, 6, 7, 8, time.UTC)),
-      influxdb.NewRowMetric(
-        map[string]interface{}{"memory": 1000, "cpu": 0.93},
-        "system-metrics",
-        map[string]string{"hostname": "hal9000"},
-        time.Date(2018, 3, 4, 5, 6, 7, 9, time.UTC)),
-    }
-
-    // The actual write..., this method can be called concurrently.
-    if _, err := influx.Write(context.Background(), "my-awesome-bucket", "my-very-awesome-org", myMetrics...)
-    if err != nil {
-      log.Fatal(err) // as above use your own error handling here.
-    }
-  `
+  const {
+    name,
+    url, 
+    initializeClientCodeSnippet, 
+    writeDataCodeSnippet
+  } = clientGoLibrary
   return (
     <ClientLibraryOverlay title={`${name} Client Library`}>
       <p>
--- ui/src/clientLibraries/constants/index.ts
+++ ui/src/clientLibraries/constants/index.ts
@@ -62,6 +62,32 @@ export const clientGoLibrary = {
   name: 'GO',
   url: 'https://github.com/influxdata/influxdb-client-go',
   logoUrl: 'assets/images/client-lib-go.svg',
+  initializeClientCodeSnippet: `  influx, err := influxdb.New(myHTTPInfluxAddress, myToken, influxdb.WithHTTPClient(myHTTPClient))
+	  if err != nil {
+		  panic(err) // error handling here; normally we wouldn't use fmt but it works for the example
+    }
+    // Add your app code here
+    influx.Close() // closes the client.  After this the client is useless.`,
+  writeDataCodeSnippet: `  // we use client.NewRowMetric for the example because it's easy, but if you need extra performance
+    // it is fine to manually build the []client.Metric{}.
+    myMetrics := []influxdb.Metric{
+      influxdb.NewRowMetric(
+        map[string]interface{}{"memory": 1000, "cpu": 0.93},
+        "system-metrics",
+        map[string]string{"hostname": "hal9000"},
+        time.Date(2018, 3, 4, 5, 6, 7, 8, time.UTC)),
+      influxdb.NewRowMetric(
+        map[string]interface{}{"memory": 1000, "cpu": 0.93},
+        "system-metrics",
+        map[string]string{"hostname": "hal9000"},
+        time.Date(2018, 3, 4, 5, 6, 7, 9, time.UTC)),
+    }
+
+    // The actual write..., this method can be called concurrently.
+    if _, err := influx.Write(context.Background(), "my-awesome-bucket", "my-very-awesome-org", myMetrics...)
+    if err != nil {
+      log.Fatal(err) // as above use your own error handling here.
+    }`,
 }
 
 export const clientJavaLibrary = {
