dc23a0afe23efcb8670876024574d76e3d6a0072
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2019-10-15 23:03:54 -0600

    (Fix) Create dashboard from empty state test

--- ui/src/dashboards/components/dashboard_index/DashboardsIndex.tsx
+++ ui/src/dashboards/components/dashboard_index/DashboardsIndex.tsx
@@ -76,7 +76,7 @@ class DashboardIndex extends PureComponent<Props, State> {
     const {searchTerm} = this.state
     return (
       <>
-        <Page titleTag={pageTitleSuffixer(['Dashboards'])}>
+        <Page testID="empty-dashboards-list" titleTag={pageTitleSuffixer(['Dashboards'])}>
           <Page.Header fullWidth={false}>
             <Page.Header.Left>
               <PageTitleWithOrg title="Dashboards" />
