f0fdaf497856bc0bb059c0eb583dbaa8e2daa590
Alex P <thealexpaxton@gmail.com>
2019-10-18 12:41:17 -0700

    refactor(ui): add explainer panel to buckets index view

---
+++ ui/src/buckets/components/BucketExplainer.tsx
@@ -0,0 +1,29 @@
+// Libraries
+import React, {SFC} from 'react'
+
+// Components
+import {Panel, InfluxColors} from '@influxdata/clockface'
+
+const TelegrafExplainer: SFC = () => (
+  <Panel backgroundColor={InfluxColors.Onyx} style={{marginTop: '32px'}}>
+    <Panel.Header>
+      <Panel.Title>What is a Bucket?</Panel.Title>
+    </Panel.Header>
+    <Panel.Body>
+      <p>
+        A bucket is a named location where time series data is stored. All buckets have a <b>Retention Policy</b>, a duration of time that each data point persists.
+        <br />
+        <br />
+        Here's{' '}
+        <a
+          href="https://v2.docs.influxdata.com/v2.0/write-data/"
+          target="_blank"
+        >
+          how to write data
+        </a> into your bucket.
+      </p>
+    </Panel.Body>
+  </Panel>
+)
+
+export default TelegrafExplainer
