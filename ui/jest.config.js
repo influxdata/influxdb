910cdde871701deb745de8a00b78b9d612f2aeef
Alex Boatwright <aboatwright@influxdata.com>
2019-10-21 10:52:06 -0700

    test(ui): gotta do this in git

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   // https://github.com/facebook/jest/issues/7842
-  testMatch: '<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)',
+  testMatch: ['<rootDir>/**/?(*.)+(spec|test).[tj]s?(x)'],
   //testRegex: '<rootDir>/**/*\.test\.{tsx,ts}$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   snapshotSerializers: ['enzyme-to-json/serializer'],
