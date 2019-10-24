722e41d9dbefa886991467b3352c51d4e7b4ecf2
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2019-10-24 13:33:48 -0600

    improvement (ui):  popover change for grpah tips
    Adding popover to the page

--- ui/src/shared/components/graph_tips/GraphTips.tsx
+++ ui/src/shared/components/graph_tips/GraphTips.tsx
@@ -1,28 +1,33 @@
-// Libraries
-import React, {SFC} from 'react'
-import ReactTooltip from 'react-tooltip'
+// // Libraries
+// import React, {SFC, useRef, RefObject} from 'react'
+// import ReactTooltip from 'react-tooltip'
 
-// Components
-import {Popover, PopoverInteraction} from '@influxdata/clockface'
+// // Components
+// import {Popover, PopoverPosition, PopoverType,ButtonRef} from '@influxdata/clockface'
 
-const graphTipsText =
-  '<h1>Graph Tips:</h1><p><code>Click + Drag</code> Zoom in (X or Y)<br/><code>Shift + Click</code> Pan Graph Window<br/><code>Double Click</code> Reset Graph Window</p><h1>Static Legend Tips:</h1><p><code>Click</code>Focus on single Series<br/><code>Shift + Click</code> Show/Hide single Series</p>'
+// const graphTipsText =
+//   '<h1>Graph Tips:</h1><p><code>Click + Drag</code> Zoom in (X or Y)<br/><code>Shift + Click</code> Pan Graph Window<br/><code>Double Click</code> Reset Graph Window</p><h1>Static Legend Tips:</h1><p><code>Click</code>Focus on single Series<br/><code>Shift + Click</code> Show/Hide single Series</p>'
+// const triggerRef: RefObject<ButtonRef> = useRef(null)
 
-const GraphTips: SFC = () => (
-  <div
-    className="graph-tips"
-    data-for="graph-tips-tooltip"
-    data-tip={graphTipsText}
-  >
-    <span>?</span>
-    <ReactTooltip
-      id="graph-tips-tooltip"
-      effect="solid"
-      html={true}
-      place="bottom"
-      class="influx-tooltip"
-    />
-  </div>
-)
+// const GraphTips: SFC = () => (
+//   <div
+//   className="graph-tips"
+//   data-for="graph-tips-tooltip"
+//   data-tip={graphTipsText}
+// >
+//   <span>?</span>
+//   <Popover
+//   type={PopoverType.Outline}
+//   position={PopoverPosition.ToTheRight}
+//   triggerRef={triggerRef}
+//   distanceFromTrigger={8}
+//   contents={()=> (
+//     graphTipsText
+//   )}
+// />
+// </div>
+  
+  
+// )
 
-export default GraphTips
+// export default GraphTips
