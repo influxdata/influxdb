8b4b54fffec185d26afe914a96eb134e68509882
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2019-10-25 10:04:45 -0600

    improvement (ui): popover change for grpah tips
    The final fix in a seperate file

--- ui/src/shared/components/graph_tips/GraphTips.tsx
+++ ui/src/shared/components/graph_tips/GraphTips.tsx
@@ -1,33 +1,50 @@
-// // Libraries
-// import React, {SFC, useRef, RefObject} from 'react'
-// import ReactTooltip from 'react-tooltip'
+// Libraries
+import React, {Component, createRef, RefObject} from 'react'
 
-// // Components
-// import {Popover, PopoverPosition, PopoverType,ButtonRef} from '@influxdata/clockface'
+// Components
+import {
+  Popover,
+  PopoverPosition,
+  PopoverInteraction,
+  PopoverType,
+} from '@influxdata/clockface'
 
-// const graphTipsText =
-//   '<h1>Graph Tips:</h1><p><code>Click + Drag</code> Zoom in (X or Y)<br/><code>Shift + Click</code> Pan Graph Window<br/><code>Double Click</code> Reset Graph Window</p><h1>Static Legend Tips:</h1><p><code>Click</code>Focus on single Series<br/><code>Shift + Click</code> Show/Hide single Series</p>'
-// const triggerRef: RefObject<ButtonRef> = useRef(null)
+interface Props {}
 
-// const GraphTips: SFC = () => (
-//   <div
-//   className="graph-tips"
-//   data-for="graph-tips-tooltip"
-//   data-tip={graphTipsText}
-// >
-//   <span>?</span>
-//   <Popover
-//   type={PopoverType.Outline}
-//   position={PopoverPosition.ToTheRight}
-//   triggerRef={triggerRef}
-//   distanceFromTrigger={8}
-//   contents={()=> (
-//     graphTipsText
-//   )}
-// />
-// </div>
-  
-  
-// )
+export default class GraphTips extends Component<Props> {
+  private triggerRef: RefObject<HTMLSpanElement> = createRef()
 
-// export default GraphTips
+  public render() {
+    return (
+      <>
+        <span ref={this.triggerRef}>?</span>
+        <Popover
+          type={PopoverType.Outline}
+          position={PopoverPosition.Below}
+          triggerRef={this.triggerRef}
+          distanceFromTrigger={8}
+          showEvent={PopoverInteraction.Hover}
+          hideEvent={PopoverInteraction.Hover}
+          contents={() => (
+            <span>
+              <h1>Graph Tips:</h1>
+              <p>
+                <code>Click + Drag</code> Zoom in (X or Y)
+                <br />
+                <code>Shift + Click</code> Pan Graph Window
+                <br />
+                <code>Double Click</code> Reset Graph Window
+              </p>
+              <h1>Static Legend Tips:</h1>
+              <p>
+                <code>Click</code>Focus on single Series
+                <br />
+                <code>Shift + Click</code> Show/Hide single Series
+              </p>
+            </span>
+          )}
+        />
+      </>
+    )
+  }
+}
