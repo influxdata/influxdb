dea9ad81c1708043af4c07a6f692812584bf42c3
Alex Boatwright <aboatwright@influxdata.com>
2019-10-31 11:59:35 -0700

    fix: LINTTTTTTTING

--- ui/src/shared/components/TemplatedCodeSnippet.test.tsx
+++ ui/src/shared/components/TemplatedCodeSnippet.test.tsx
@@ -11,88 +11,79 @@ describe('TeplatedCodeSnippet', () => {
   })
 
   it('should allow normal strings', () => {
-    let props:any = {};
-    jest.mock(
-      'src/shared/components/CodeSnippet',
-      () => ({ children, ...rest }) => {
-        props = rest
-
-        return false
-      }
-    );
+    let props: any = {}
+    jest.mock('src/shared/components/CodeSnippet', () => _props => {
+      props = _props
+      return false
+    })
 
     const template = 'this is only a test'
-    const Component = require('src/shared/components/TemplatedCodeSnippet').default
-    const wrapper = render(<Component template={ template } />)
+    const Component = require('src/shared/components/TemplatedCodeSnippet')
+      .default
+    render(<Component template={template} />)
 
     expect(props.hasOwnProperty('copyText')).toBe(true)
     expect(props.copyText).toEqual(template)
   })
 
   it('should show undefined', () => {
-    let props:any = {};
-    jest.mock(
-      'src/shared/components/CodeSnippet',
-      () => ({ children, ...rest }) => {
-        props = rest;
-
-        return false
-      }
-    );
+    let props: any = {}
+    jest.mock('src/shared/components/CodeSnippet', () => _props => {
+      props = _props
+      return false
+    })
 
     const template = 'this is only a <%= word %>'
     const response = 'this is only a undefined'
-    const Component = require('src/shared/components/TemplatedCodeSnippet').default
-    const wrapper = render(<Component template={ template } />)
+    const Component = require('src/shared/components/TemplatedCodeSnippet')
+      .default
+    render(<Component template={template} />)
 
     expect(props.hasOwnProperty('copyText')).toBe(true)
     expect(props.copyText).toEqual(response)
   })
 
   it('should respect defaults', () => {
-    let props:any = {};
-    jest.mock(
-      'src/shared/components/CodeSnippet',
-      () => ({ children, ...rest }) => {
-        props = rest;
-
-        return false
-      }
-    );
+    let props: any = {}
+    jest.mock('src/shared/components/CodeSnippet', () => _props => {
+      props = _props
+      return false
+    })
 
     const template = 'this is only a <%= word %>'
     const response = 'this is only a sentance'
     const defaults = {
-      word: 'sentance'
+      word: 'sentance',
     }
-    const Component = require('src/shared/components/TemplatedCodeSnippet').default
-    const wrapper = render(<Component template={ template } defaults={ defaults } />)
+    const Component = require('src/shared/components/TemplatedCodeSnippet')
+      .default
+    render(<Component template={template} defaults={defaults} />)
 
     expect(props.hasOwnProperty('copyText')).toBe(true)
     expect(props.copyText).toEqual(response)
   })
 
   it('should overwrite defaults with values', () => {
-    let props:any = {};
-    jest.mock(
-      'src/shared/components/CodeSnippet',
-      () => ({ children, ...rest }) => {
-        props = rest;
+    let props: any = {}
+    jest.mock('src/shared/components/CodeSnippet', () => _props => {
+      props = _props
 
-        return false
-      }
-    );
+      return false
+    })
 
     const template = 'this is only a <%= word %>'
     const response = 'this is only a test'
     const defaults = {
-      word: 'sentance'
+      word: 'sentance',
     }
     const values = {
-      word: 'test'
+      word: 'test',
     }
-    const Component = require('src/shared/components/TemplatedCodeSnippet').default
-    const wrapper = render(<Component template={ template } defaults={ defaults } values={ values } />)
+    const Component = require('src/shared/components/TemplatedCodeSnippet')
+      .default
+    render(
+      <Component template={template} defaults={defaults} values={values} />
+    )
 
     expect(props.hasOwnProperty('copyText')).toBe(true)
     expect(props.copyText).toEqual(response)
