4e2549199a1d515d2536d318523c17ab59eba871
alexpaxton <thealexpaxton@gmail.com>
2019-11-04 12:58:45 -0500

    refactor: simplify component to use style prop

--- ui/src/shared/components/ColorDropdown.tsx
+++ ui/src/shared/components/ColorDropdown.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {FC} from 'react'
+import React, {FC, CSSProperties} from 'react'
 import {capitalize} from 'lodash'
 
 // Components
@@ -20,8 +20,7 @@ interface PassedProps {
 
 interface DefaultProps {
   disabled?: boolean
-  stretchToFit?: boolean
-  widthPixels?: number
+  style?: CSSProperties
 }
 
 type Props = PassedProps & DefaultProps
@@ -32,12 +31,10 @@ const ColorDropdown: FC<Props> = props => {
     colors,
     onChoose,
     disabled,
-    stretchToFit,
-    widthPixels,
+    style,
   } = props
 
   const status = disabled ? ComponentStatus.Disabled : ComponentStatus.Default
-  const style = stretchToFit ? null : {width: `${widthPixels}px`}
 
   return (
     <Dropdown
@@ -83,9 +80,8 @@ const ColorDropdown: FC<Props> = props => {
 }
 
 ColorDropdown.defaultProps = {
-  stretchToFit: false,
   disabled: false,
-  widthPixels: 100,
+  style: {flex: '0 0 120px'},
 }
 
 export default ColorDropdown
