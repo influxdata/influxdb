9d9562533ea749da740f00f6467b03461c2c6946
alexpaxton <thealexpaxton@gmail.com>
2019-11-04 11:22:11 -0500

    refactor: replace hamburger icon with dor grid

--- ui/src/shared/components/cells/CellHeader.tsx
+++ ui/src/shared/components/cells/CellHeader.tsx
@@ -13,7 +13,9 @@ const CellHeader: FunctionComponent<Props> = ({name, note, children}) => {
   return (
     <div className="cell--header">
       <div className="cell--draggable">
-        <div className="cell--hamburger" />
+        <div className="cell--dot-grid" />
+        <div className="cell--dot-grid" />
+        <div className="cell--dot-grid" />
       </div>
       <div className="cell--name">{name}</div>
       {note && <CellHeaderNote note={note} />}
