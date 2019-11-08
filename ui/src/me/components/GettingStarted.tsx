4f03541a916f1a5297bfa1268434073c603df34d
alexpaxton <thealexpaxton@gmail.com>
2019-11-08 11:14:35 -0500

    refactor: replace home page cards with Panels

--- ui/src/me/components/GettingStarted.tsx
+++ ui/src/me/components/GettingStarted.tsx
@@ -1,53 +1,55 @@
 // Libraries
-import React, {PureComponent} from 'react'
-import {withRouter, Link, WithRouterProps} from 'react-router'
+import React, {FunctionComponent} from 'react'
+import {withRouter, WithRouterProps} from 'react-router'
 
 // Components
-import GradientBorder from 'src/shared/components/cells/GradientBorder'
+import {Panel, Button, Gradients, InfluxColors, ComponentColor, ComponentSize} from '@influxdata/clockface'
 import DashboardingGraphic from 'src/me/graphics/DashboardingGraphic'
 import ExploreGraphic from 'src/me/graphics/ExploreGraphic'
 import CollectorGraphic from 'src/me/graphics/CollectorGraphic'
 
-class GettingStarted extends PureComponent<WithRouterProps> {
-  public render() {
-    const {
-      params: {orgID},
-    } = this.props
-    return (
-      <div className="getting-started">
-        <div className="getting-started--container">
-          <Link
-            to={`/orgs/${orgID}/load-data/telegrafs`}
-            className="getting-started--card"
-          >
-            <GradientBorder />
-            <CollectorGraphic />
-            <h3 className="getting-started--title">Load your data</h3>
-          </Link>
-        </div>
-        <div className="getting-started--container">
-          <Link
-            to={`/orgs/${orgID}/dashboards`}
-            className="getting-started--card"
-          >
-            <GradientBorder />
-            <DashboardingGraphic />
-            <h3 className="getting-started--title">Build a dashboard</h3>
-          </Link>
-        </div>
-        <div className="getting-started--container">
-          <Link
-            to={`/orgs/${orgID}/alerting`}
-            className="getting-started--card"
-          >
-            <GradientBorder />
-            <ExploreGraphic />
-            <h3 className="getting-started--title">Set up alerting</h3>
-          </Link>
-        </div>
-      </div>
-    )
+const GettingStarted: FunctionComponent<WithRouterProps>  = ({params: {orgID}, router}) => {
+  const handleLoadDataClick = (): void => {
+    router.push(`/orgs/${orgID}/load-data/telegrafs`)
   }
+  const handleDashboardsClick = (): void => {
+    router.push(`/orgs/${orgID}/dashboards`)
+  }
+  const handleAlertsClick = (): void => {
+    router.push(`/orgs/${orgID}/alerting`)
+  }
+
+  return (
+    <div className="getting-started">
+      <Panel className="getting-started--card highlighted" gradient={Gradients.PolarExpress}>
+        <div className="getting-started--card-digit">1</div>
+        <Panel.Body>
+          <CollectorGraphic />
+        </Panel.Body>
+        <Panel.Footer>
+          <Button text="Load your data" color={ComponentColor.Primary} size={ComponentSize.Large} onClick={handleLoadDataClick} />
+        </Panel.Footer>
+      </Panel>
+      <Panel className="getting-started--card" backgroundColor={InfluxColors.Onyx}>
+        <div className="getting-started--card-digit">2</div>
+        <Panel.Body>
+          <DashboardingGraphic />
+        </Panel.Body>
+        <Panel.Footer>
+          <Button text="Build a dashboard" color={ComponentColor.Primary} size={ComponentSize.Large} onClick={handleDashboardsClick} />
+        </Panel.Footer>
+      </Panel>
+      <Panel className="getting-started--card" backgroundColor={InfluxColors.Onyx}>
+        <div className="getting-started--card-digit">3</div>
+        <Panel.Body>
+          <ExploreGraphic />
+        </Panel.Body>
+        <Panel.Footer>
+          <Button text="Set up alerting" color={ComponentColor.Primary} size={ComponentSize.Large} onClick={handleAlertsClick} />
+        </Panel.Footer>
+      </Panel>
+    </div>
+  )
 }
 
 export default withRouter<{}>(GettingStarted)
