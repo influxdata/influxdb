cacb9632e5066bfc6cf80788e1f23529ca9351da
alexpaxton <thealexpaxton@gmail.com>
2019-11-08 11:36:01 -0500

    fix: appease linter

--- ui/src/me/components/GettingStarted.tsx
+++ ui/src/me/components/GettingStarted.tsx
@@ -3,12 +3,22 @@ import React, {FunctionComponent} from 'react'
 import {withRouter, WithRouterProps} from 'react-router'
 
 // Components
-import {Panel, Button, Gradients, InfluxColors, ComponentColor, ComponentSize} from '@influxdata/clockface'
+import {
+  Panel,
+  Button,
+  Gradients,
+  InfluxColors,
+  ComponentColor,
+  ComponentSize,
+} from '@influxdata/clockface'
 import DashboardingGraphic from 'src/me/graphics/DashboardingGraphic'
 import ExploreGraphic from 'src/me/graphics/ExploreGraphic'
 import CollectorGraphic from 'src/me/graphics/CollectorGraphic'
 
-const GettingStarted: FunctionComponent<WithRouterProps>  = ({params: {orgID}, router}) => {
+const GettingStarted: FunctionComponent<WithRouterProps> = ({
+  params: {orgID},
+  router,
+}) => {
   const handleLoadDataClick = (): void => {
     router.push(`/orgs/${orgID}/load-data/telegrafs`)
   }
@@ -21,31 +31,55 @@ const GettingStarted: FunctionComponent<WithRouterProps>  = ({params: {orgID}, r
 
   return (
     <div className="getting-started">
-      <Panel className="getting-started--card highlighted" gradient={Gradients.PolarExpress}>
+      <Panel
+        className="getting-started--card highlighted"
+        gradient={Gradients.PolarExpress}
+      >
         <div className="getting-started--card-digit">1</div>
         <Panel.Body>
           <CollectorGraphic />
         </Panel.Body>
         <Panel.Footer>
-          <Button text="Load your data" color={ComponentColor.Primary} size={ComponentSize.Large} onClick={handleLoadDataClick} />
+          <Button
+            text="Load your data"
+            color={ComponentColor.Primary}
+            size={ComponentSize.Large}
+            onClick={handleLoadDataClick}
+          />
         </Panel.Footer>
       </Panel>
-      <Panel className="getting-started--card" backgroundColor={InfluxColors.Onyx}>
+      <Panel
+        className="getting-started--card"
+        backgroundColor={InfluxColors.Onyx}
+      >
         <div className="getting-started--card-digit">2</div>
         <Panel.Body>
           <DashboardingGraphic />
         </Panel.Body>
         <Panel.Footer>
-          <Button text="Build a dashboard" color={ComponentColor.Primary} size={ComponentSize.Large} onClick={handleDashboardsClick} />
+          <Button
+            text="Build a dashboard"
+            color={ComponentColor.Primary}
+            size={ComponentSize.Large}
+            onClick={handleDashboardsClick}
+          />
         </Panel.Footer>
       </Panel>
-      <Panel className="getting-started--card" backgroundColor={InfluxColors.Onyx}>
+      <Panel
+        className="getting-started--card"
+        backgroundColor={InfluxColors.Onyx}
+      >
         <div className="getting-started--card-digit">3</div>
         <Panel.Body>
           <ExploreGraphic />
         </Panel.Body>
         <Panel.Footer>
-          <Button text="Set up alerting" color={ComponentColor.Primary} size={ComponentSize.Large} onClick={handleAlertsClick} />
+          <Button
+            text="Set up alerting"
+            color={ComponentColor.Primary}
+            size={ComponentSize.Large}
+            onClick={handleAlertsClick}
+          />
         </Panel.Footer>
       </Panel>
     </div>
