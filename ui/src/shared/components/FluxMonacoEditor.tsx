8f08503d70b36e636975d6a71d7a794f16f02003
Alex Boatwright <aboatwright@influxdata.com>
2019-11-11 16:07:54 -0800

    feat: adding in some editor events for monaco

--- ui/src/shared/components/FluxMonacoEditor.tsx
+++ ui/src/shared/components/FluxMonacoEditor.tsx
@@ -6,17 +6,51 @@ import MonacoEditor from 'react-monaco-editor'
 import {tokenizeFlux} from 'src/external/monaco.fluxLang'
 import {addFluxTheme} from 'src/external/monaco.fluxTheme'
 import {addSnippets} from 'src/external/monaco.fluxCompletions'
+import {OnChangeScript} from 'src/types/flux'
+
+interface Position {
+  line: number
+  ch: number
+}
 
 interface Props {
   script: string
+  onChangeScript: OnChangeScript
+  onSubmitScript?: () => void
+  onCursorChange?: (position: Position) => void
 }
 
-const FluxEditorMonaco: FC<Props> = ({script}) => {
+const FluxEditorMonaco: FC<Props> = props => {
   const editorWillMount = monaco => {
     tokenizeFlux(monaco)
     addFluxTheme(monaco)
     addSnippets(monaco)
   }
+  const editorDidMount = editor => {
+    editor.onDidChangeCursorPosition(evt => {
+      const {position} = evt
+      const {onCursorChange} = props
+      const pos = {
+        line: position.lineNumber,
+        ch: position.column,
+      }
+
+      if (onCursorChange) {
+        onCursorChange(pos)
+      }
+    })
+
+    editor.onKeyUp(evt => {
+      const {ctrlKey, code} = evt
+      const {onSubmitScript} = props
+      if (ctrlKey && code === 'Enter') {
+        if (onSubmitScript) {
+          onSubmitScript()
+        }
+      }
+    })
+  }
+  const {script, onChangeScript} = props
 
   return (
     <div className="time-machine-editor" data-testid="flux-editor">
@@ -26,7 +60,9 @@ const FluxEditorMonaco: FC<Props> = ({script}) => {
         language="flux"
         theme="vs-light"
         value={script}
+        onChange={onChangeScript}
         editorWillMount={editorWillMount}
+        editorDidMount={editorDidMount}
       />
     </div>
   )
