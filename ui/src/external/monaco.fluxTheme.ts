624a92a0a67bc6c654ab5f51698667cbd8a379e0
Alex Boatwright <drdelambre@gmail.com>
2019-11-15 15:13:45 -0800

    feat(ui): add some color to monaco (#15882)

--- ui/src/external/monaco.fluxTheme.ts
+++ ui/src/external/monaco.fluxTheme.ts
@@ -1,7 +1,24 @@
-export const addFluxTheme = monaco => {
-  monaco.editor.defineTheme('fluxTheme', {
-    base: 'vs',
+export const THEME_NAME = 'baseTheme'
+export default function(monaco) {
+  monaco.editor.defineTheme(THEME_NAME, {
+    base: 'vs-dark',
     inherit: false,
-    rules: [{token: 'keyword', foreground: 'ff00ff', fontStyle: 'bold'}],
+    rules: [
+      {
+        token: '',
+        foreground: '#f8f8f8',
+        background: '#202028',
+      },
+    ],
+    colors: {
+      'editor.foreground': '#F8F8F8',
+      'editor.background': '#202028',
+      'editorGutter.background': '#25252e',
+      'editor.selectionBackground': '#353640',
+      'editorLineNumber.foreground': '#666978',
+      'editor.lineHighlightBackground': '#353640',
+      'editorCursor.foreground': '#ffffff',
+      'editorActiveLineNumber.foreground': '#bec2cc',
+    },
   })
 }
--- ui/src/shared/components/FluxMonacoEditor.tsx
+++ ui/src/shared/components/FluxMonacoEditor.tsx
@@ -4,9 +4,10 @@ import React, {FC} from 'react'
 // Components
 import MonacoEditor from 'react-monaco-editor'
 import {tokenizeFlux} from 'src/external/monaco.fluxLang'
-import {addFluxTheme} from 'src/external/monaco.fluxTheme'
+import addFluxTheme, {THEME_NAME} from 'src/external/monaco.fluxTheme'
 import {addSnippets} from 'src/external/monaco.fluxCompletions'
 import {OnChangeScript} from 'src/types/flux'
+import './FluxMonacoEditor.scss'
 
 interface Position {
   line: number
@@ -22,8 +23,8 @@ interface Props {
 
 const FluxEditorMonaco: FC<Props> = props => {
   const editorWillMount = monaco => {
-    tokenizeFlux(monaco)
     addFluxTheme(monaco)
+    tokenizeFlux(monaco)
     addSnippets(monaco)
   }
   const editorDidMount = editor => {
@@ -55,12 +56,22 @@ const FluxEditorMonaco: FC<Props> = props => {
   return (
     <div className="time-machine-editor" data-testid="flux-editor">
       <MonacoEditor
-        width="800"
-        height="600"
         language="flux"
-        theme="vs-light"
+        theme={THEME_NAME}
         value={script}
         onChange={onChangeScript}
+        options={{
+          fontSize: 13,
+          fontFamily: '"RobotoMono", monospace',
+          cursorWidth: 2,
+          lineNumbersMinChars: 4,
+          lineDecorationsWidth: 0,
+          minimap: {
+            renderCharacters: false,
+          },
+          overviewRulerBorder: false,
+          automaticLayout: true,
+        }}
         editorWillMount={editorWillMount}
         editorDidMount={editorDidMount}
       />
