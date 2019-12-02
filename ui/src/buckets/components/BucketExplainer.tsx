5ef4c1137904cb9b543076e04e6c56961f37dd62
Ariel Salem <Asalem1@users.noreply.github.com>
2019-12-02 10:03:40 -0800

    fix(ui): updated telegraf description to be more explicit (#16061)
    * fix(ui): updated telegraf description to be more explicit
    
    * fix(ui): updated title to display same styling as empty text
    
    * fix(ui): added defaultProps and checks for collectors

--- ui/src/buckets/components/BucketExplainer.tsx
+++ ui/src/buckets/components/BucketExplainer.tsx
@@ -4,8 +4,8 @@ import React, {FunctionComponent} from 'react'
 // Components
 import {Panel, InfluxColors} from '@influxdata/clockface'
 
-const TelegrafExplainer: FunctionComponent = () => (
-  <Panel backgroundColor={InfluxColors.Onyx} style={{marginTop: '32px'}}>
+const BucketExplainer: FunctionComponent = () => (
+  <Panel backgroundColor={InfluxColors.Smoke} style={{marginTop: '32px'}}>
     <Panel.Header>
       <Panel.Title>What is a Bucket?</Panel.Title>
     </Panel.Header>
@@ -29,4 +29,4 @@ const TelegrafExplainer: FunctionComponent = () => (
   </Panel>
 )
 
-export default TelegrafExplainer
+export default BucketExplainer
--- ui/src/telegrafs/components/TelegrafExplainer.tsx
+++ ui/src/telegrafs/components/TelegrafExplainer.tsx
@@ -2,27 +2,51 @@
 import React, {FunctionComponent} from 'react'
 
 // Components
-import {Panel, InfluxColors} from '@influxdata/clockface'
+import {
+  Panel,
+  EmptyState,
+  InfluxColors,
+  ComponentSize,
+} from '@influxdata/clockface'
+import {TextAlignProperty} from 'csstype'
 
-const TelegrafExplainer: FunctionComponent = () => (
-  <Panel backgroundColor={InfluxColors.Onyx} style={{marginTop: '32px'}}>
-    <Panel.Header>
-      <Panel.Title>What is Telegraf?</Panel.Title>
-    </Panel.Header>
-    <Panel.Body>
-      <p>
-        Telegraf is an agent written in Go for collecting metrics and writing
-        them into <strong>InfluxDB</strong> or other possible outputs.
-        <br />
-        <br />
-        Here's a handy guide for{' '}
-        <a
-          href="https://v2.docs.influxdata.com/v2.0/write-data/use-telegraf/"
-          target="_blank"
-        >
-          Getting Started with Telegraf
-        </a>
-      </p>
+interface Props {
+  hasNoTelegrafs?: boolean
+  textAlign?: TextAlignProperty
+  bodySize?: ComponentSize
+}
+
+const TelegrafExplainer: FunctionComponent<Props> = ({
+  hasNoTelegrafs = false,
+  textAlign = 'inherit',
+  bodySize,
+}) => (
+  <Panel
+    backgroundColor={InfluxColors.Smoke}
+    style={{textAlign, marginTop: 32}}
+  >
+    {hasNoTelegrafs && (
+      <EmptyState.Text style={{color: InfluxColors.Platinum, marginTop: 16}}>
+        What is Telegraf?
+      </EmptyState.Text>
+    )}
+    {!hasNoTelegrafs && (
+      <Panel.Header>
+        <Panel.Title>What is Telegraf?</Panel.Title>
+      </Panel.Header>
+    )}
+    <Panel.Body size={bodySize}>
+      Telegraf is an agent written in Go for collecting metrics and writing them
+      into <strong>InfluxDB</strong> or other possible outputs.
+      <br />
+      <br />
+      Here's a handy guide for{' '}
+      <a
+        href="https://v2.docs.influxdata.com/v2.0/write-data/use-telegraf/"
+        target="_blank"
+      >
+        Getting Started with Telegraf
+      </a>
     </Panel.Body>
   </Panel>
 )
