02ead488abc435ad4ecaaae904df1b7cf274029e
Deniz Kusefoglu <deniz@influxdata.com>
2019-12-02 13:05:36 -0800

    chore(ui): Add init pass at reasonable timeRange constants

--- ui/src/shared/constants/timeRanges.ts
+++ ui/src/shared/constants/timeRanges.ts
@@ -1,24 +1,54 @@
-import {TimeRange} from 'src/types'
+import {
+  TimeRange,
+  CUSTOM_TIME_RANGE_LABEL,
+  TimeRangeLower,
+  SelectedTimeRange,
+  CustomTimeRange,
+  CheckTimeRange,
+} from 'src/types'
 
-export const TIME_RANGE_LABEL = 'Time Range'
-export const CUSTOM_TIME_RANGE_LABEL = 'Custom Time Range'
 export const TIME_RANGE_FORMAT = 'YYYY-MM-DD HH:mm'
 
-export const TIME_RANGES: TimeRange[] = [
-  {
-    lower: '',
-    label: TIME_RANGE_LABEL,
-  },
-  {
-    lower: '',
-    label: CUSTOM_TIME_RANGE_LABEL,
-  },
+export const pastHourTimeRange: SelectedTimeRange = {
+  seconds: 3600,
+  lower: 'now() - 1h' as TimeRangeLower,
+  upper: null,
+  label: 'Past 1h',
+  duration: '1h',
+  type: 'selected',
+}
+
+export const pastThirtyDaysTimeRange: SelectedTimeRange = {
+  seconds: 2592000,
+  lower: 'now() - 30d',
+  upper: null,
+  label: 'Past 30d',
+  duration: '30d',
+  type: 'selected',
+}
+
+export const CUSTOM_TIME_RANGE: CustomTimeRange = {
+  lower: '',
+  upper: null,
+  label: 'Custom Time Range' as CUSTOM_TIME_RANGE_LABEL,
+  type: 'custom',
+}
+
+export const CHECK_TIME_RANGE: CheckTimeRange = {
+  lower: '', // format: `now() - ${durationMultiple}`
+  upper: null,
+  label: 'Auto-generated time range',
+  type: 'check',
+}
+
+export const SELECTED_TIME_RANGES: SelectedTimeRange[] = [
   {
     seconds: 300,
     lower: 'now() - 5m',
     upper: null,
     label: 'Past 5m',
     duration: '5m',
+    type: 'selected',
   },
   {
     seconds: 900,
@@ -26,20 +56,16 @@ export const TIME_RANGES: TimeRange[] = [
     upper: null,
     label: 'Past 15m',
     duration: '15m',
+    type: 'selected',
   },
-  {
-    seconds: 3600,
-    lower: 'now() - 1h',
-    upper: null,
-    label: 'Past 1h',
-    duration: '1h',
-  },
+  pastHourTimeRange,
   {
     seconds: 21600,
     lower: 'now() - 6h',
     upper: null,
     label: 'Past 6h',
     duration: '6h',
+    type: 'selected',
   },
   {
     seconds: 43200,
@@ -47,6 +73,7 @@ export const TIME_RANGES: TimeRange[] = [
     upper: null,
     label: 'Past 12h',
     duration: '12h',
+    type: 'selected',
   },
   {
     seconds: 86400,
@@ -54,6 +81,7 @@ export const TIME_RANGES: TimeRange[] = [
     upper: null,
     label: 'Past 24h',
     duration: '24h',
+    type: 'selected',
   },
   {
     seconds: 172800,
@@ -61,6 +89,7 @@ export const TIME_RANGES: TimeRange[] = [
     upper: null,
     label: 'Past 2d',
     duration: '2d',
+    type: 'selected',
   },
   {
     seconds: 604800,
@@ -68,20 +97,14 @@ export const TIME_RANGES: TimeRange[] = [
     upper: null,
     label: 'Past 7d',
     duration: '7d',
+    type: 'selected',
   },
-  {
-    seconds: 2592000,
-    lower: 'now() - 30d',
-    upper: null,
-    label: 'Past 30d',
-    duration: '30d',
-  },
+  pastThirtyDaysTimeRange,
 ]
 
-export const DEFAULT_TIME_RANGE: TimeRange = TIME_RANGES[2]
+export const TIME_RANGES: TimeRange[] = [
+  CUSTOM_TIME_RANGE,
+  ...SELECTED_TIME_RANGES,
+]
 
-export const ABSOLUTE = 'absolute'
-export const INVALID = 'invalid'
-export const RELATIVE_LOWER = 'relative lower'
-export const RELATIVE_UPPER = 'relative upper'
-export const INFLUXQL = 'influxql'
+export const DEFAULT_TIME_RANGE: TimeRange = pastHourTimeRange
