e692840c431a70cdf13186bdabd07ae62845f12a
Deniz Kusefoglu <deniz@influxdata.com>
2019-12-04 15:59:11 -0800

    fix(ui): Fix tests

--- ui/src/alerting/utils/vis.test.ts
+++ ui/src/alerting/utils/vis.test.ts
@@ -1,9 +1,17 @@
 import {getCheckVisTimeRange} from 'src/alerting/utils/vis'
+import {CHECK_TIME_RANGE} from 'src/shared/constants/timeRanges'
 
 const TESTS = [
-  ['5s', {lower: 'now() - 1500s'}],
-  ['1m', {lower: 'now() - 300m'}],
-  ['1m5s', {lower: 'now() - 300m1500s'}],
+  ['5s', {...CHECK_TIME_RANGE, lower: 'now() - 1500s', label: 'now() - 1500s'}],
+  ['1m', {...CHECK_TIME_RANGE, lower: 'now() - 300m', label: 'now() - 300m'}],
+  [
+    '1m5s',
+    {
+      ...CHECK_TIME_RANGE,
+      lower: 'now() - 300m1500s',
+      label: 'now() - 300m1500s',
+    },
+  ],
 ]
 
 test.each(TESTS)('getCheckVisTimeRange(%s)', (input, expected) => {
