c887468d9efc0f7752ba1164e194fe93a2bf1d21
alexpaxton <thealexpaxton@gmail.com>
2019-12-11 10:43:24 -0800

    refactor: add destination for Upgrade button

--- ui/src/pageLayout/components/CloudNav.tsx
+++ ui/src/pageLayout/components/CloudNav.tsx
@@ -5,7 +5,15 @@ import {connect} from 'react-redux'
 
 // Components
 import {FeatureFlag} from 'src/shared/utils/featureFlag'
-import {AppHeader, PopNav, Button, ComponentColor, FlexBox, FlexDirection, ComponentSize} from '@influxdata/clockface'
+import {
+  AppHeader,
+  PopNav,
+  Button,
+  ComponentColor,
+  FlexBox,
+  FlexDirection,
+  ComponentSize,
+} from '@influxdata/clockface'
 import CloudOnly from 'src/shared/components/cloud/CloudOnly'
 
 // Constants
@@ -30,35 +38,54 @@ interface StateProps {
 const CloudNav: FunctionComponent<StateProps> = ({orgName, orgID}) => {
   const usageURL = `${CLOUD_URL}${CLOUD_USAGE_PATH}`
   const billingURL = `${CLOUD_URL}${CLOUD_BILLING_PATH}`
-  const handleUpgradeClick = () => {
-    console.log('boop')
+  const handleUpgradeClick = (): void => {
+    window.location.assign(billingURL)
   }
+
   return (
     <CloudOnly>
       <AppHeader className="cloud-nav">
-      <AppHeader.Logo>
-        <Link to={`/orgs/${orgID}`} className="cloud-nav--logo-link"><img className="cloud-nav--logo" alt="InfluxData Logo" src={Logo} /></Link></AppHeader.Logo>
+        <AppHeader.Logo>
+          <Link to={`/orgs/${orgID}`} className="cloud-nav--logo-link">
+            <img className="cloud-nav--logo" alt="InfluxData Logo" src={Logo} />
+          </Link>
+        </AppHeader.Logo>
         <FlexBox direction={FlexDirection.Row} margin={ComponentSize.Medium}>
-          <Button color={ComponentColor.Success} text="Upgrade" onClick={handleUpgradeClick} />
+          <Button
+            color={ComponentColor.Success}
+            text="Upgrade"
+            onClick={handleUpgradeClick}
+          />
           <PopNav>
-            <p className="cloud-nav--account">Logged in as <strong>{orgName}</strong></p>
-            <PopNav.Item active={false} titleLink={className => (
-              <a className={className} href={usageURL}>
-                Usage
-              </a>
-            )} />
-            <FeatureFlag name="cloudBilling">
-              <PopNav.Item active={false} titleLink={className => (
-                <a className={className} href={billingURL}>
-                  Billing
+            <p className="cloud-nav--account">
+              Logged in as <strong>{orgName}</strong>
+            </p>
+            <PopNav.Item
+              active={false}
+              titleLink={className => (
+                <a className={className} href={usageURL}>
+                  Usage
                 </a>
-              )} />
+              )}
+            />
+            <FeatureFlag name="cloudBilling">
+              <PopNav.Item
+                active={false}
+                titleLink={className => (
+                  <a className={className} href={billingURL}>
+                    Billing
+                  </a>
+                )}
+              />
             </FeatureFlag>
-            <PopNav.Item active={false} titleLink={className => (
-              <a className={className} href={CLOUD_SIGNOUT_URL}>
-                Logout
-              </a>
-            )} />
+            <PopNav.Item
+              active={false}
+              titleLink={className => (
+                <a className={className} href={CLOUD_SIGNOUT_URL}>
+                  Logout
+                </a>
+              )}
+            />
           </PopNav>
         </FlexBox>
       </AppHeader>
@@ -66,7 +93,7 @@ const CloudNav: FunctionComponent<StateProps> = ({orgName, orgID}) => {
   )
 }
 
-const mstp = ({ orgs: { org } }: AppState) => {
+const mstp = ({orgs: {org}}: AppState) => {
   return {orgName: org.name, orgID: org.id}
 }
 
