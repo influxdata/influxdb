5896a06d583f9b6eeca1eeeaa9b0afc3274aa170
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2019-12-12 10:33:03 -0800

    feat (ui): Add the view check page
    Stable for now, its all fixed just needs to be cleaned up and tested

--- ui/src/alerting/components/CheckHistory.tsx
+++ ui/src/alerting/components/CheckHistory.tsx
@@ -1,41 +1,32 @@
 // Libraries
-import React, {useMemo, FC, createContext, useState, useEffect} from 'react'
+import React, {useMemo, FC} from 'react'
 import {Page} from '@influxdata/clockface'
-import {Plot} from '@influxdata/giraffe'
 import {connect} from 'react-redux'
-import {get} from 'lodash'
 
 // Components
+import {ResourceIDsContext} from 'src/alerting/components/AlertHistoryIndex'
 import EventViewer from 'src/eventViewer/components/EventViewer'
-import EventTable from 'src/eventViewer/components/EventTable'
 import CheckHistoryControls from 'src/alerting/components/CheckHistoryControls'
 import CheckHistoryVisualization from 'src/alerting/components/CheckHistoryVisualization'
 import AlertHistoryQueryParams from 'src/alerting/components/AlertHistoryQueryParams'
-import CheckPlot from 'src/shared/components/CheckPlot'
-import EmptyQueryView, {ErrorFormat} from 'src/shared/components/EmptyQueryView'
+import EventTable from 'src/eventViewer/components/EventTable'
 
 // Constants
 import {STATUS_FIELDS} from 'src/alerting/constants/history'
 
 // Utils
 import {loadStatuses, getInitialState} from 'src/alerting/utils/history'
+import {getCheckIDs} from 'src/alerting/selectors'
 
 // Types
 import GetResources, {ResourceType} from 'src/shared/components/GetResources'
-import {AppState, Check, TimeZone, CheckViewProperties} from 'src/types'
-import TimeSeries from 'src/shared/components/TimeSeries'
-import {createView} from 'src/shared/utils/view'
-import {checkResultsLength} from 'src/shared/utils/vis'
-import {getTimeRangeVars} from 'src/variables/utils/getTimeRangeVars'
-import { queries } from 'react-testing-library'
+import {AppState, Check, TimeZone} from 'src/types'
 
 interface ResourceIDs {
   checkIDs: {[x: string]: boolean}
   endpointIDs: {[x: string]: boolean}
   ruleIDs: {[x: string]: boolean}
 }
-
-export const ResourceIDsContext = createContext<ResourceIDs>(null)
 
 interface OwnProps {
   params: {orgID: string; checkID: string}
@@ -44,25 +35,19 @@ interface OwnProps {
 interface StateProps {
   check: Check
   timeZone: TimeZone
+  resourceIDs: ResourceIDs
 }
 
 type Props = OwnProps & StateProps
 //TODO maybe update submitToken when we know how
 
 const CheckHistory: FC<Props> = ({
-  params: {orgID, checkID},
+  params: {orgID},
   check,
   timeZone,
+  resourceIDs,
 }) => {
   const historyType = 'statuses'
-  let properties: CheckViewProperties
-
-  useEffect(() => {
-    const view = createView<CheckViewProperties>(
-      get(check, 'type', 'threshold')
-    )
-    properties = view.properties
-  }, [check])
 
   const loadRows = useMemo(() => options => loadStatuses(orgID, options), [
     orgID,
@@ -71,38 +56,53 @@ const CheckHistory: FC<Props> = ({
   const fields = STATUS_FIELDS
   return (
     <GetResources resources={[ResourceType.Checks]}>
-      <EventViewer loadRows={loadRows} initialState={getInitialState()}>
-        {props => (
-          <Page
-            titleTag="Check Statuses | InfluxDB 2.0"
-            className="alert-history-page"
-          >
-            <Page.Header fullWidth={true}>
-              <div className="alert-history-page--header">
-                <Page.Title
-                  title="Check Statuses"
-                  testID="alert-history-title"
-                />
-                <AlertHistoryQueryParams
-                  searchInput={props.state.searchInput}
-                  historyType={historyType}
-                />
-                <CheckHistoryControls eventViewerProps={props} />
-              </div>
-            </Page.Header>
-            <Page.Contents
-              fullWidth={true}
-              scrollable={false}
-              className="alert-history-page--contents"
+      <ResourceIDsContext.Provider value={resourceIDs}>
+        <EventViewer loadRows={loadRows} initialState={getInitialState()}>
+          {props => (
+            <Page
+              titleTag="Check Statuses | InfluxDB 2.0"
+              className="alert-history-page"
             >
-            <CheckHistoryVisualization check={check} timeZone={timeZone}} />
-              <div className="alert-history">
-                {/* <EventTable {...props} fields={fields} /> */}
-              </div>
-            </Page.Contents>
-          </Page>
-        )}
-      </EventViewer>
+              <Page.Header fullWidth={true}>
+                <div className="alert-history-page--header">
+                  <Page.Title
+                    title="Check Statuses"
+                    testID="alert-history-title"
+                  />
+                  <AlertHistoryQueryParams
+                    searchInput={props.state.searchInput}
+                    historyType={historyType}
+                  />
+                  <CheckHistoryControls eventViewerProps={props} />
+                </div>
+              </Page.Header>
+              <Page.Contents
+                fullWidth={true}
+                scrollable={false}
+                className="alert-history-page--contents"
+              >
+                <div
+                  className="zoescool"
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%',
+                    width: '100%',
+                  }}
+                >
+                  <CheckHistoryVisualization
+                    check={check}
+                    timeZone={timeZone}
+                  />
+                  <div className="alert-history">
+                    <EventTable {...props} fields={fields} />
+                  </div>
+                </div>
+              </Page.Contents>
+            </Page>
+          )}
+        </EventViewer>
+      </ResourceIDsContext.Provider>
     </GetResources>
   )
 }
@@ -110,8 +110,15 @@ const CheckHistory: FC<Props> = ({
 const mstp = (state: AppState, props: OwnProps) => {
   const check = state.checks.list.find(({id}) => id === props.params.checkID)
   const timeZone = state.app.persisted.timeZone
+  const checkIDs = getCheckIDs(state)
 
-  return {check, timeZone}
+  const resourceIDs = {
+    checkIDs,
+    endpointIDs: null,
+    ruleIDs: null,
+  }
+
+  return {check, timeZone, resourceIDs}
 }
 
 export default connect<StateProps>(mstp)(CheckHistory)
--- ui/src/alerting/components/CheckHistoryControls.tsx
+++ ui/src/alerting/components/CheckHistoryControls.tsx
@@ -9,27 +9,21 @@ import SearchBar from 'src/alerting/components/SearchBar'
 import {EventViewerChildProps} from 'src/eventViewer/types'
 
 // Constants
-import {
-  EXAMPLE_STATUS_SEARCHES,
-} from 'src/alerting/constants/history'
+import {EXAMPLE_STATUS_SEARCHES} from 'src/alerting/constants/history'
 
 interface Props {
   eventViewerProps: EventViewerChildProps
 }
 
-const CheckHistoryControls: FC<Props> = ({
-  eventViewerProps,
-}) => {
+const CheckHistoryControls: FC<Props> = ({eventViewerProps}) => {
   return (
     <div className="alert-history-controls">
       <div className="alert-history-controls--right">
         <BackToTopButton {...eventViewerProps} />
         <SearchBar
           {...eventViewerProps}
-          placeholder={`Search statuses...`}
-          exampleSearches={
-            EXAMPLE_STATUS_SEARCHES
-          }
+          placeholder="Search statuses..."
+          exampleSearches={EXAMPLE_STATUS_SEARCHES}
         />
       </div>
     </div>
