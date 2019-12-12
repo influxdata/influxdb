907a4b643172d122a8e21d7c24e02214066ea5f9
Deniz Kusefoglu <deniz@influxdata.com>
2019-12-12 11:53:42 -0800

    chore(ui): Remove eventMarkers feature flag

--- ui/src/shared/components/TimeSeries.tsx
+++ ui/src/shared/components/TimeSeries.tsx
@@ -17,7 +17,6 @@ import {runStatusesQuery} from 'src/alerting/utils/statusEvents'
 import {checkQueryResult} from 'src/shared/utils/checkQueryResult'
 import {getWindowVars} from 'src/variables/utils/getWindowVars'
 import {buildVarsOption} from 'src/variables/utils/buildVarsOption'
-import {isFlagEnabled} from 'src/shared/utils/featureFlag'
 import 'intersection-observer'
 
 // Constants
@@ -188,7 +187,7 @@ class TimeSeries extends Component<Props & WithRouterProps, State> {
       const results = await Promise.all(this.pendingResults.map(r => r.promise))
 
       let statuses = [] as StatusRow[][]
-      if (check && isFlagEnabled('eventMarkers')) {
+      if (check) {
         const extern = buildVarsOption(variables)
         this.pendingCheckStatuses = runStatusesQuery(orgID, check.id, extern)
         statuses = await this.pendingCheckStatuses.promise // TODO handle errors
