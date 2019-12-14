e9dff7b06c0eea2e68d7665eb80ed7f7630c2489
tmgordeeva <tanya@influxdata.com>
2019-12-13 16:09:02 -0800

    fix(storage): skip TSM files with block read errors (#16221)
    * fix(storage): skip TSM files with block read errors
    
    When we find a bad TSM file during compaction, propagate the error up and move
    the bad file aside. The engine will disregard the file so the next compaction
    will not hit the same error.

--- tsdb/engine/tsm1/compact_test.go
+++ tsdb/engine/tsm1/compact_test.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 	"testing"
 	"time"
 
@@ -296,7 +297,8 @@ func TestCompactor_DecodeError(t *testing.T) {
 	compactor.Open()
 
 	files, err = compactor.CompactFull([]string{f1, f2, f3})
-	if err == nil || err.Error() != "decode error: unable to decompress block type float for key 'cpu,host=A#!~#value': unpackBlock: not enough data for timestamp" {
+	if err == nil ||
+		!strings.Contains(err.Error(), "decode error: unable to decompress block type float for key 'cpu,host=A#!~#value': unpackBlock: not enough data for timestamp") {
 		t.Fatalf("expected error writing snapshot: %v", err)
 	}
 }
