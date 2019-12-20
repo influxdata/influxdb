5281af357b830538d42839cbc26e9f7e1b73c856
Alex Boatwright <drdelambre@gmail.com>
2019-12-20 14:15:38 -0800

    fix: speeding them tests right up (#16311)

--- ui/jestSetup.ts
+++ ui/jestSetup.ts
@@ -1,5 +1,7 @@
 import {cleanup} from 'react-testing-library'
 
+jest.mock('honeybadger-js', () => () => null)
+
 process.env.API_PREFIX = '/'
 // cleans up state between react-testing-library tests
 afterEach(() => {
