b984dcc887ff8ddb7ae3b9d85ba3acb0206864f6
Johnny Steenbergen <jonathansteenbergen@gmail.com>
2019-12-31 10:28:26 -0800

    fix(kv): add legacy variable orgs index back
    issue here is that the unique by name index for variables was implemented
    and has the same functionality about it that this orgs index has. The duplicative
    orgs index was nuked. The migration to hydrate the org/name index never
    happened. This is a stop gap until that migration is in place.

--- kv/service.go
+++ kv/service.go
@@ -147,6 +147,10 @@ func (s *Service) Initialize(ctx context.Context) error {
 			return err
 		}
 
+		if err := s.initializeVariablesOrgIndex(tx); err != nil {
+			return err
+		}
+
 		if err := s.initializeChecks(ctx, tx); err != nil {
 			return err
 		}
