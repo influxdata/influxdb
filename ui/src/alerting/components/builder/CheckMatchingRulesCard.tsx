acf73dfdd4c56522db14dba61e0f6432adcb853f
Deniz Kusefoglu <deniz@influxdata.com>
2020-01-02 12:05:14 +0000

    feat(ui): Add matching rules card to alert builder (#16014)
    * feat(ui): Add matching rules card to alert builder
    
    * feat(checks): Add tags from query results to NR matching query parameters
    
    * feat(ui): Update changelog
    
    * feat(nr): Change api function to match import convention
    
    * fix(ui): Fix changelog update mistake
    
    * Click submit button after typing in time machine
    
    * feat(notification rules): Add feature flag to matching notification rules

--- ui/src/alerting/components/builder/CheckMatchingRulesCard.tsx
+++ ui/src/alerting/components/builder/CheckMatchingRulesCard.tsx
@@ -1,33 +1,67 @@
 // Libraries
 import React, {FunctionComponent, useState, useEffect} from 'react'
 import {connect} from 'react-redux'
+import {uniq} from 'lodash'
+import {fromFlux} from '@influxdata/giraffe'
 
 // Components
 import MatchingRuleCard from 'src/alerting/components/builder/MatchingRuleCard'
-import {SpinnerContainer, TechnoSpinner} from '@influxdata/clockface'
+import {
+  SpinnerContainer,
+  TechnoSpinner,
+  FlexBox,
+  FlexDirection,
+  AlignItems,
+} from '@influxdata/clockface'
 
 // API
-import * as api from 'src/client'
+import {getNotificationRules as apiGetNotificationRules} from 'src/client'
 
 //Types
 import {NotificationRule, AppState, CheckTagSet} from 'src/types'
 import {EmptyState, ComponentSize, RemoteDataState} from '@influxdata/clockface'
+import BuilderCard from 'src/timeMachine/components/builderCard/BuilderCard'
+import {getActiveTimeMachine} from 'src/timeMachine/selectors'
 
 interface StateProps {
   tags: CheckTagSet[]
   orgID: string
+  queryResults: string[] | null
 }
 
 const CheckMatchingRulesCard: FunctionComponent<StateProps> = ({
   orgID,
   tags,
+  queryResults,
 }) => {
   const getMatchingRules = async (): Promise<NotificationRule[]> => {
-    const tagsList = tags.map(t => ['tag', `${t.key}:${t.value}`])
+    const checkTags = tags
+      .filter(t => t.key && t.value)
+      .map(t => [t.key, t.value])
 
-    // todo also: get tags from query results
+    const queryTags = []
 
-    const resp = await api.getNotificationRules({
+    if (queryResults) {
+      const joined = queryResults.join('\n\n')
+      const table = fromFlux(joined).table
+      const fluxGroupKeyUnion = fromFlux(joined).fluxGroupKeyUnion.filter(
+        v => v !== '_start' && v !== '_stop'
+      )
+
+      fluxGroupKeyUnion.forEach(gk => {
+        const values = uniq(table.getColumn(gk, 'string'))
+        values.forEach(v => {
+          queryTags.push([gk, v])
+        })
+      })
+    }
+
+    const tagsList = [...checkTags, ...queryTags].map(t => [
+      'tag',
+      `${t[0].trim()}:${t[1].trim()}`,
+    ])
+
+    const resp = await apiGetNotificationRules({
       query: [['orgID', orgID], ...tagsList] as any,
     })
 
@@ -52,43 +86,61 @@ const CheckMatchingRulesCard: FunctionComponent<StateProps> = ({
       matchingRules,
       status: RemoteDataState.Loading,
     })
-    getMatchingRules()
-  }, [tags])
 
-  const emptyState = (
-    <EmptyState
-      size={ComponentSize.Small}
-      className="alert-builder--card__empty"
-    >
-      <EmptyState.Text>
-        Notification Rules configured to act on tag sets matching this Alert
-        Check will automatically show up here
-      </EmptyState.Text>
-      <EmptyState.Text>
-        Looks like no notification rules match the tag set defined in this Alert
-        Check
-      </EmptyState.Text>
-    </EmptyState>
-  )
+    getMatchingRules()
+  }, [tags, queryResults])
+
+  let contents: JSX.Element
 
   if (
     status === RemoteDataState.NotStarted ||
     status === RemoteDataState.Loading
   ) {
-    return (
+    contents = (
       <SpinnerContainer spinnerComponent={<TechnoSpinner />} loading={status} />
+    )
+  } else if (matchingRules.length === 0) {
+    contents = (
+      <EmptyState
+        size={ComponentSize.Small}
+        className="alert-builder--card__empty"
+      >
+        <EmptyState.Text>
+          Notification Rules configured to act on tag sets matching this Alert
+          Check will show up here
+        </EmptyState.Text>
+        <EmptyState.Text>
+          Looks like no notification rules match the tag set defined in this
+          Alert Check
+        </EmptyState.Text>
+      </EmptyState>
+    )
+  } else {
+    contents = (
+      <>
+        {matchingRules.map(r => (
+          <MatchingRuleCard key={r.id} rule={r} />
+        ))}
+      </>
     )
   }
 
-  if (matchingRules.length === 0) {
-    return emptyState
-  }
   return (
-    <>
-      {matchingRules.map(r => (
-        <MatchingRuleCard key={r.id} rule={r} />
-      ))}
-    </>
+    <BuilderCard
+      testID="builder-conditions"
+      className="alert-builder--card alert-builder--conditions-card"
+    >
+      <BuilderCard.Header title="Matching Notification Rules" />
+      <BuilderCard.Body addPadding={true} autoHideScrollbars={true}>
+        <FlexBox
+          direction={FlexDirection.Column}
+          alignItems={AlignItems.Stretch}
+          margin={ComponentSize.Medium}
+        >
+          {contents}
+        </FlexBox>
+      </BuilderCard.Body>
+    </BuilderCard>
   )
 }
 
@@ -100,7 +152,11 @@ const mstp = (state: AppState): StateProps => {
     alertBuilder: {tags},
   } = state
 
-  return {tags, orgID}
+  const {
+    queryResults: {files},
+  } = getActiveTimeMachine(state)
+
+  return {tags, orgID, queryResults: files}
 }
 
 export default connect<StateProps, {}, {}>(
