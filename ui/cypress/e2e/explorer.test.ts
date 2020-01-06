6d2de273c901b16a2d23022ca87d201cd59f9199
Alex Boatwright <aboatwright@influxdata.com>
2020-01-06 12:45:15 -0800

    chore: lint

--- ui/cypress/e2e/explorer.test.ts
+++ ui/cypress/e2e/explorer.test.ts
@@ -390,7 +390,7 @@ describe('DataExplorer', () => {
       cy.getByTestID('time-machine-submit-button').should('be.disabled')
     })
 
-    it.only('imports the appropriate packages to build a query', () => {
+    it('imports the appropriate packages to build a query', () => {
       cy.getByTestID('functions-toolbar-tab').click()
 
       cy.getByTestID('flux-function from').click()
