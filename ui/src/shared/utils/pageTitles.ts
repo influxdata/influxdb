f300db2e7e102cff792a39dfbdb07f4886b0f702
alexpaxton <thealexpaxton@gmail.com>
2020-01-09 11:22:30 -0800

    fix(ui): update get path of orgname in page title suffixer

--- ui/src/shared/utils/pageTitles.ts
+++ ui/src/shared/utils/pageTitles.ts
@@ -3,7 +3,7 @@ import {store} from 'src/index'
 
 export const pageTitleSuffixer = (pageTitles: string[]): string => {
   const state = store.getState()
-  const currentOrg = get(state, 'orgs.org.name', '')
+  const currentOrg = get(state, 'resources.orgs.org.name', null)
   const titles = [...pageTitles, currentOrg, 'InfluxDB 2.0']
 
   return titles.join(' | ')
