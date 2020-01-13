a95ca316134f8534416073386718ed608801bb76
Deniz Kusefoglu <deniz@influxdata.com>
2020-01-13 15:53:40 -0500

    feat(duration-inputs) : use window isNan instead of lodash

--- ui/src/alerting/utils/checkValidate.ts
+++ ui/src/alerting/utils/checkValidate.ts
@@ -1,5 +1,4 @@
 import {Threshold} from 'src/types'
-import {isNaN} from 'lodash'
 
 export const checkThresholdsValid = (thresholds: Threshold[]) => {
   thresholds.forEach(t => {
@@ -10,7 +9,7 @@ export const checkThresholdsValid = (thresholds: Threshold[]) => {
       throw new Error('Threshold must have defined value')
     }
     if (t.type === 'range' && (isNaN(t.min) || isNaN(t.min))) {
-      throw new Error('Threshold must have defined values')
+      throw new Error('Threshold must have defined min and max values')
     }
   })
 }
