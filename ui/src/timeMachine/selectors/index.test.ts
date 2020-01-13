6415a6e389f163d0b3ac56a80410eea62c634713
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-01-13 12:45:27 -0800

    feat: query builder - hide the keys search dropdown when grouping

--- ui/src/timeMachine/selectors/index.test.ts
+++ ui/src/timeMachine/selectors/index.test.ts
@@ -168,6 +168,7 @@ describe('getting active tag values', () => {
     expect(actualTags).toEqual([
       ...activeQueryTags[0].values,
       ...activeQueryTags[1].values,
+      ...activeQueryTags[2].values,
     ])
   })
 })
