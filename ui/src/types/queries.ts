f496f840173f2f5d1d77a222d8bb7bf9a3a07ee6
Deniz Kusefoglu <deniz@influxdata.com>
2020-01-14 14:24:29 -0500

    feat(agg-window): Add window periods for common time ranges

--- ui/src/types/queries.ts
+++ ui/src/types/queries.ts
@@ -24,6 +24,7 @@ export interface SelectableDurationTimeRange {
   label: string
   duration: string
   type: 'selectable-duration'
+  windowPeriod: number
 }
 
 export interface DurationTimeRange {
