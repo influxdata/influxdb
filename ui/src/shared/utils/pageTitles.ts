10b033cd9ffd933917d03e0c4e31b4b4f00888e1
alexpaxton <thealexpaxton@gmail.com>
2020-01-15 01:21:30 +0700

    fix(ui): fix appearance of org names in page titles (#16471)

--- ui/src/shared/utils/pageTitles.ts
+++ ui/src/shared/utils/pageTitles.ts
@@ -1,10 +1,10 @@
-import {get} from 'lodash'
+import {getOrg} from 'src/organizations/selectors'
 import {store} from 'src/index'
 
 export const pageTitleSuffixer = (pageTitles: string[]): string => {
   const state = store.getState()
-  const currentOrg = get(state, 'orgs.org.name', '')
-  const titles = [...pageTitles, currentOrg, 'InfluxDB 2.0']
+  const {name} = getOrg(state) || null
+  const titles = [...pageTitles, name, 'InfluxDB 2.0']
 
   return titles.join(' | ')
 }
