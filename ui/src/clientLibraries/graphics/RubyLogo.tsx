135bf9e32473ac54df2cf8a4d7e186507805edb5
Jakub Bednar <jakub.bednar@gmail.com>
2020-01-22 11:40:34 +0100

    feat(clientLibs): add Ruby client library

---
+++ ui/src/clientLibraries/graphics/RubyLogo.tsx
@@ -0,0 +1,18 @@
+// Libraries
+import React, {SFC} from 'react'
+
+/* eslint-disable no-mixed-spaces-and-tabs */
+const RubyLogo: SFC = () => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 300 300"
+    >
+    </svg>
+  )
+}
+
+/* eslint-enable */
+export default RubyLogo
