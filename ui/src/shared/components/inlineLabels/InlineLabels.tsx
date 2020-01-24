3b7da5df1da0cbe9b18684ade54b8ed94b3c02f1
Alex Boatwright <drdelambre@gmail.com>
2020-01-24 09:14:53 -0800

    fix: default label properties (#16665)

--- ui/src/shared/components/inlineLabels/InlineLabels.tsx
+++ ui/src/shared/components/inlineLabels/InlineLabels.tsx
@@ -57,17 +57,30 @@ export default class InlineLabels extends Component<Props> {
     const onDelete = this.isEditable ? this.handleDeleteLabel : null
 
     if (selectedLabels.length) {
-      return selectedLabels.map(label => (
-        <LabelComponent
-          id={label.id}
-          key={label.id}
-          name={label.name}
-          color={label.properties.color}
-          description={label.properties.description}
-          onDelete={onDelete}
-          onClick={this.handleLabelClick.bind(this, label.name)}
-        />
-      ))
+      return selectedLabels.map(_label => {
+        const label = {..._label}
+
+        // TODO: clean this up during normalization effort at
+        // the service layer and not in the view (alex)
+        if (!label.hasOwnProperty('properties')) {
+          label.properties = {
+            color: '#FF0000',
+            description: '',
+          }
+        }
+
+        return (
+          <LabelComponent
+            id={label.id}
+            key={label.id}
+            name={label.name}
+            color={label.properties.color}
+            description={label.properties.description}
+            onDelete={onDelete}
+            onClick={this.handleLabelClick.bind(this, label.name)}
+          />
+        )
+      })
     }
   }
 
