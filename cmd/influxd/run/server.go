638f720e2f0cbf4bf99673229f3582965d91820a
Ayan George <ayan@ayan.net>
2020-01-29 16:09:30 -0500

    Revert "go fmt"
    This reverts commit f9dec7a4b6a9f695059d788bc68d4a8f3a5d8e4c.

--- cmd/influxd/run/server.go
+++ cmd/influxd/run/server.go
@@ -1,7 +1,6 @@
 package run
 
 import (
-	"context"
 	"crypto/tls"
 	"fmt"
 	"io"
@@ -12,6 +11,7 @@ import (
 	"runtime"
 	"runtime/pprof"
 	"time"
+	"context"
 
 	"github.com/influxdata/influxdb"
 	"github.com/influxdata/influxdb/coordinator"
