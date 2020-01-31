081d38fd6686b1d7c4a92be922c84c69ff58f4a1
Andrew Watkins <watts@influxdb.com>
2020-01-31 13:05:53 -0800

    chore: remove test loop

--- ui/cypress/e2e/notificationRules.test.ts
+++ ui/cypress/e2e/notificationRules.test.ts
@@ -151,144 +151,142 @@ describe('NotificationRules', () => {
     })
   })
 
-  for (let i = 0; i < 25; i++) {
-    it('can create a notification rule', () => {
-      const ruleName = 'my-new-rule'
-      cy.getByTestID('create-rule').click()
+  it('can create a notification rule', () => {
+    const ruleName = 'my-new-rule'
+    cy.getByTestID('create-rule').click()
 
-      cy.getByTestID('rule-name--input').type(ruleName)
+    cy.getByTestID('rule-name--input').type(ruleName)
 
-      cy.getByTestID('rule-schedule-every--input')
-        .clear()
-        .type('20m')
-        .should('have.value', '20m')
+    cy.getByTestID('rule-schedule-every--input')
+      .clear()
+      .type('20m')
+      .should('have.value', '20m')
 
-      cy.getByTestID('rule-schedule-offset--input')
-        .clear()
-        .type('1m')
-        .should('have.value', '1m')
+    cy.getByTestID('rule-schedule-offset--input')
+      .clear()
+      .type('1m')
+      .should('have.value', '1m')
 
-      // Editing a Status Rule
-      cy.getByTestID('status-rule').within(() => {
-        cy.getByTestID('status-change--dropdown')
-          .click()
-          .within(() => {
-            cy.getByTestID('status-change--dropdown-item is equal to').click()
-            cy.getByTestID('status-change--dropdown--button').within(() => {
-              cy.contains('equal')
-            })
+    // Editing a Status Rule
+    cy.getByTestID('status-rule').within(() => {
+      cy.getByTestID('status-change--dropdown')
+        .click()
+        .within(() => {
+          cy.getByTestID('status-change--dropdown-item is equal to').click()
+          cy.getByTestID('status-change--dropdown--button').within(() => {
+            cy.contains('equal')
           })
-
-        cy.getByTestID('levels--dropdown previousLevel').should('not.exist')
-        cy.getByTestID('levels--dropdown currentLevel').should('exist')
-
-        cy.getByTestID('status-change--dropdown')
-          .click()
-          .within(() => {
-            cy.getByTestID('status-change--dropdown-item changes from').click()
-            cy.getByTestID('status-change--dropdown--button').within(() => {
-              cy.contains('changes from')
-            })
-          })
-
-        cy.getByTestID('levels--dropdown previousLevel').click()
-        cy.getByTestID('levels--dropdown-item INFO').click()
-        cy.getByTestID('levels--dropdown--button previousLevel').within(() => {
-          cy.contains('INFO')
         })
 
-        cy.getByTestID('levels--dropdown currentLevel').click()
-        cy.getByTestID('levels--dropdown-item CRIT').click()
-        cy.getByTestID('levels--dropdown--button currentLevel').within(() => {
-          cy.contains('CRIT')
+      cy.getByTestID('levels--dropdown previousLevel').should('not.exist')
+      cy.getByTestID('levels--dropdown currentLevel').should('exist')
+
+      cy.getByTestID('status-change--dropdown')
+        .click()
+        .within(() => {
+          cy.getByTestID('status-change--dropdown-item changes from').click()
+          cy.getByTestID('status-change--dropdown--button').within(() => {
+            cy.contains('changes from')
+          })
         })
+
+      cy.getByTestID('levels--dropdown previousLevel').click()
+      cy.getByTestID('levels--dropdown-item INFO').click()
+      cy.getByTestID('levels--dropdown--button previousLevel').within(() => {
+        cy.contains('INFO')
       })
 
+      cy.getByTestID('levels--dropdown currentLevel').click()
+      cy.getByTestID('levels--dropdown-item CRIT').click()
+      cy.getByTestID('levels--dropdown--button currentLevel').within(() => {
+        cy.contains('CRIT')
+      })
+    })
+
+    cy.getByTestID('endpoint--dropdown--button')
+      .within(() => {
+        cy.contains(name1)
+      })
+      .click()
+
+    cy.get<SlackNotificationEndpoint>('@selectedEndpoint').then(({id}) => {
+      cy.getByTestID(`endpoint--dropdown-item ${id}`).click()
       cy.getByTestID('endpoint--dropdown--button')
         .within(() => {
-          cy.contains(name1)
+          cy.contains(name2)
         })
         .click()
+    })
 
-      cy.get<SlackNotificationEndpoint>('@selectedEndpoint').then(({id}) => {
-        cy.getByTestID(`endpoint--dropdown-item ${id}`).click()
-        cy.getByTestID('endpoint--dropdown--button')
-          .within(() => {
-            cy.contains(name2)
-          })
-          .click()
-      })
-
-      const message = `
+    const message = `
       Have you ever wanted to interrupt all your co-workers, but don't
       want to struggle with the hassle of typing @here in #general? Well,
       do we have the notification for you!
     `
 
-      cy.getByTestID('slack-message-template--textarea')
-        .clear()
-        .type(message)
-        .should('contain', message)
+    cy.getByTestID('slack-message-template--textarea')
+      .clear()
+      .type(message)
+      .should('contain', message)
 
-      cy.getByTestID('rule-overlay-save--button').click()
+    cy.getByTestID('rule-overlay-save--button').click()
 
-      // Add a label
-      cy.getByTestID(`rule-card ${ruleName}`).within(() => {
-        cy.getByTestID('inline-labels--add').click()
-      })
-
-      const labelName = 'l1'
-      cy.getByTestID('inline-labels--popover--contents').type(labelName)
-      cy.getByTestID('inline-labels--create-new').click()
-      cy.getByTestID('create-label-form--submit').click()
-
-      cy.getByTestID(`label--pill ${labelName}`).should('exist')
-
-      // Filter for the new rule
-      cy.getByTestID('filter--input rules').type(ruleName)
-
-      cy.getByTestID('rule-card--name')
-        .contains(ruleName)
-        .click()
-
-      const editedName = ruleName + '!'
-
-      // Edit the rule
-      cy.getByTestID('rule-name--input')
-        .clear()
-        .type(editedName)
-
-      cy.getByTestID('rule-schedule-every--input')
-        .clear()
-        .type('21m')
-        .should('have.value', '21m')
-
-      cy.getByTestID('rule-schedule-offset--input')
-        .clear()
-        .type('2m')
-        .should('have.value', '2m')
-
-      cy.getByTestID('rule-overlay-save--button').click()
-
-      // Open overlay
-      cy.getByTestID('rule-card--name')
-        .contains(editedName)
-        .click()
-
-      // Close overlay
-      cy.getByTestID('dismiss-overlay')
-        .find('button')
-        .click()
-
-      // Delete the rule
-      cy.getByTestID('rules--column').within(() => {
-        cy.getByTestID(`context-delete-menu`).click()
-        cy.getByTestID(`context-delete-task`).click()
-      })
-
-      // Remove the filter
-      cy.getByTestID('filter--input rules').clear()
-      cy.getByTestID('rule-card--name').should('have.length', 0)
+    // Add a label
+    cy.getByTestID(`rule-card ${ruleName}`).within(() => {
+      cy.getByTestID('inline-labels--add').click()
     })
-  }
+
+    const labelName = 'l1'
+    cy.getByTestID('inline-labels--popover--contents').type(labelName)
+    cy.getByTestID('inline-labels--create-new').click()
+    cy.getByTestID('create-label-form--submit').click()
+
+    cy.getByTestID(`label--pill ${labelName}`).should('exist')
+
+    // Filter for the new rule
+    cy.getByTestID('filter--input rules').type(ruleName)
+
+    cy.getByTestID('rule-card--name')
+      .contains(ruleName)
+      .click()
+
+    const editedName = ruleName + '!'
+
+    // Edit the rule
+    cy.getByTestID('rule-name--input')
+      .clear()
+      .type(editedName)
+
+    cy.getByTestID('rule-schedule-every--input')
+      .clear()
+      .type('21m')
+      .should('have.value', '21m')
+
+    cy.getByTestID('rule-schedule-offset--input')
+      .clear()
+      .type('2m')
+      .should('have.value', '2m')
+
+    cy.getByTestID('rule-overlay-save--button').click()
+
+    // Open overlay
+    cy.getByTestID('rule-card--name')
+      .contains(editedName)
+      .click()
+
+    // Close overlay
+    cy.getByTestID('dismiss-overlay')
+      .find('button')
+      .click()
+
+    // Delete the rule
+    cy.getByTestID('rules--column').within(() => {
+      cy.getByTestID(`context-delete-menu`).click()
+      cy.getByTestID(`context-delete-task`).click()
+    })
+
+    // Remove the filter
+    cy.getByTestID('filter--input rules').clear()
+    cy.getByTestID('rule-card--name').should('have.length', 0)
+  })
 })
