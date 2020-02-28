9eb9cafe6afaf4fbd09ef762d74a0260f3f4a591
asalem <ariel.salem1989@gmail.com>
2020-02-28 05:46:50 -0800

    feat(ui): added logout functionality to epic

--- ui/src/Logout.tsx
+++ ui/src/Logout.tsx
@@ -1,6 +1,7 @@
 // Libraries
 import {PureComponent} from 'react'
 import {withRouter, WithRouterProps} from 'react-router'
+import auth0js from 'auth0-js'
 
 // APIs
 import {postSignout} from 'src/client'
@@ -10,6 +11,15 @@ import {CLOUD, CLOUD_URL, CLOUD_LOGOUT_PATH} from 'src/shared/constants'
 
 // Components
 import {ErrorHandling} from 'src/shared/decorators/errors'
+
+// Utils
+import {isFlagEnabled} from 'src/shared/utils/featureFlag'
+
+// TODO: these are filler properties that will be populated on IDPE in a later iteration
+const auth0 = new auth0js.WebAuth({
+  domain: 'www.influxdata.com',
+  clientID: 'abc123',
+})
 
 type Props = WithRouterProps
 
@@ -24,6 +34,10 @@ export class Logout extends PureComponent<Props> {
   }
 
   private handleSignOut = async () => {
+    if (CLOUD && isFlagEnabled('IDPELoginPage')) {
+      auth0.logout({})
+      return
+    }
     if (CLOUD) {
       window.location.href = `${CLOUD_URL}${CLOUD_LOGOUT_PATH}`
       return
