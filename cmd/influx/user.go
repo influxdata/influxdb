fec0a021fd5116501f39828eeed57e5728ea8190
Jade McGough <jade@thezets.com>
2020-03-04 15:39:04 -0800

    fix(cmd): use list instead of find in cli (#16805)

--- cmd/influx/user.go
+++ cmd/influx/user.go
@@ -280,8 +280,9 @@ func (b *cmdUserBuilder) cmdCreateRunEFn(*cobra.Command, []string) error {
 }
 
 func (b *cmdUserBuilder) cmdFind() *cobra.Command {
-	cmd := b.newCmd("find", b.cmdFindRunEFn)
-	cmd.Short = "Find user"
+	cmd := b.newCmd("list", b.cmdFindRunEFn)
+	cmd.Short = "List users"
+	cmd.Aliases = []string{"find", "ls"}
 
 	cmd.Flags().StringVarP(&b.id, "id", "i", "", "The user ID")
 	cmd.Flags().StringVarP(&b.name, "name", "n", "", "The user name")
