95893147c0ad87590fa1034e95f0d63649319a12
Brandon Farmer <bthesorceror@gmail.com>
2020-03-04 15:20:23 -0800

    chore: Refactor LSP server integration for future work

--- ui/src/external/monaco.flux.server.ts
+++ ui/src/external/monaco.flux.server.ts
@@ -1,44 +1,118 @@
-import {sendMessage, initialize} from 'src/external/monaco.flux.messages'
+import Deferred from '../utils/Deferred'
+import {
+  LSPResponse,
+  parseResponse,
+  initialize,
+  LSPMessage,
+  completion,
+  didOpen,
+  didChange,
+} from 'src/external/monaco.flux.messages'
+import {registerCompletion} from './monaco.flux.completions'
 
-const queue = []
-let _module = null,
-  loading = false
+import {
+  CompletionItem,
+  CompletionContext,
+  Position,
+} from 'monaco-languageclient/lib/services'
+import {Server} from '@influxdata/flux-lsp-browser'
 
 type BucketCallback = () => Promise<string[]>
 
-export interface LSPServer {
-  send: (string) => Promise<object>
+export interface WASMServer extends Server {
   register_buckets_callback: (BucketCallback) => void
 }
 
-export default function loader(): Promise<LSPServer> {
-  return new Promise(resolve => {
-    if (_module) {
-      resolve(_module as LSPServer)
-      return
+const queue = []
+let server = null,
+  loading = false
+
+export class LSPServer {
+  private server: WASMServer
+  private messageID: number = 0
+  private buckets: string[] = []
+  private documentVersions: {[key: string]: number} = {}
+
+  constructor(server: WASMServer) {
+    this.server = server
+    this.server.register_buckets_callback(this.getBuckets)
+  }
+
+  getBuckets = () => {
+    return Promise.resolve(this.buckets)
+  }
+
+  updateBuckets(buckets: string[]) {
+    this.buckets = buckets
+  }
+
+  async send(message: LSPMessage): Promise<LSPResponse> {
+    const body = JSON.stringify(message)
+    const fullMessage = `Content-Length: ${body.length}\r\n\r\n${body}`
+    const response = await this.server.process(fullMessage)
+
+    return parseResponse(response)
+  }
+
+  initialize() {
+    return this.send(initialize(this.currentMessageID))
+  }
+
+  async completionItems(
+    uri: string,
+    position: Position,
+    context: CompletionContext
+  ): Promise<CompletionItem[]> {
+    try {
+      const response = (await this.send(
+        completion(this.currentMessageID, uri, position, context)
+      )) as {result?: {items?: []}}
+
+      return (response.result || {}).items || []
+    } catch (e) {
+      return []
     }
+  }
 
-    queue.push(resolve)
+  async didOpen(uri: string, script: string) {
+    await this.send(didOpen(this.currentMessageID, uri, script, 1))
+    this.documentVersions[uri] = 1
+  }
 
-    if (loading) {
-      return
-    }
+  async didChange(uri: string, script: string) {
+    const version = this.documentVersions[uri] || 1
+    await this.send(didChange(this.currentMessageID, uri, script, version + 1))
+    this.documentVersions[uri] = version + 1
+  }
 
-    loading = true
+  private get currentMessageID(): number {
+    const result = this.messageID
+    this.messageID++
+    return result
+  }
+}
 
-    import('@influxdata/flux-lsp-browser').then(({Server}) => {
-      _module = new Server(false)
-      _module.send = function(message) {
-        return sendMessage(message, _module)
-      }
+export default async function loader(): Promise<LSPServer> {
+  if (server) {
+    return server
+  }
 
-      sendMessage(initialize, _module)
+  const deferred = new Deferred<LSPServer>()
 
-      queue.reduce((prev, curr) => {
-        return prev.then(() => {
-          return curr(_module as LSPServer)
-        })
-      }, Promise.resolve([]))
-    })
-  })
+  if (loading) {
+    queue.push(deferred)
+    return await deferred.promise
+  }
+
+  loading = true
+
+  const {Server} = await import('@influxdata/flux-lsp-browser')
+  server = new LSPServer(new Server(false) as WASMServer)
+  registerCompletion(window.monaco, server)
+
+  await server.initialize()
+
+  queue.forEach(d => d.resolve(server))
+
+  return server
 }
