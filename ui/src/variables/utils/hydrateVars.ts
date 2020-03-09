6572faf837bb1925358955c33edfa9b23fc9d3b7
drdelambre <drdelambre>
2020-03-09 11:32:49 -0700

    fix: whoops

--- ui/src/variables/utils/hydrateVars.ts
+++ ui/src/variables/utils/hydrateVars.ts
@@ -323,7 +323,6 @@ const invalidateCycles = (graph: VariableNode[]): void => {
 
     if (cyclicPath) {
       for (const invalidNode of cyclicPath) {
-        console.log('marking this one')
         invalidNode.status = RemoteDataState.Error
       }
     }
