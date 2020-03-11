334faa033b4aeababebe93272729097fca669b55
alexpaxton <thealexpaxton@gmail.com>
2020-03-11 15:34:13 -0700

    fix(ui): make detection of dashboard viewing more resilient and precise

--- ui/src/dashboards/utils/dashboardLightMode.ts
+++ ui/src/dashboards/utils/dashboardLightMode.ts
@@ -3,9 +3,10 @@ export const isLightMode = (
   pathname: string,
   search: string
 ): boolean => {
-  return (
-    pathname.includes('dashboards') &&
-    search.includes('?lower=') &&
-    dashboardLightMode
-  )
+  const fullURL = pathname + search
+  const regex = /.+orgs[/][\w\d]+[/]dashboards[/][\w\d]+[?/].+/g
+  const keyword = 'isADashboard'
+  const urlIsADashboard = fullURL.replace(regex, keyword) === keyword
+
+  return urlIsADashboard && dashboardLightMode
 }
