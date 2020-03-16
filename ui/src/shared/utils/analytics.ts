500ac5a2cd62287afbe1e6bbe943e4b73ce6620c
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-03-16 15:28:30 -0700

    feat(cloud): add shallow wrapper around eventing in gtm

---
+++ ui/src/shared/utils/analytics.ts
@@ -0,0 +1,25 @@
+export const fireOrgIdReady = function fireOrgIdReady(
+  organizationIds: string[]
+) {
+  window.dataLayer = window.dataLayer || []
+  window.dataLayer.push({
+    event: 'cloudAppOrgIdReady',
+    identity: {
+      organizationIds,
+    },
+  })
+}
+
+export const fireUserDataReady = function fireUserDataReady(
+  id: string,
+  email: string
+) {
+  window.dataLayer = window.dataLayer || []
+  window.dataLayer.push({
+    event: 'cloudAppUserDataReady',
+    identity: {
+      email,
+      id,
+    },
+  })
+}
