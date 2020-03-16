7588e126c9ab83b586a9cf11c0f990a5ef97d2f4
Scott Anderson <sanderson@users.noreply.github.com>
2020-03-16 17:10:08 -0600

    chore(flux): add experimental csv.from function (#17298)
    * chore(flux): add experimental csv.from function
    
    * fix(fluxFunctions): lowercasing URL param
    
    Co-authored-by: Russ Savage <russorat@users.noreply.github.com>

--- ui/src/shared/constants/fluxFunctions.ts
+++ ui/src/shared/constants/fluxFunctions.ts
@@ -482,9 +482,25 @@ export const FLUX_FUNCTIONS: FluxToolbarFunction[] = [
     ],
     package: 'csv',
     desc: 'Retrieves data from a comma-separated value (CSV) data source.',
-    example: 'csv.from(file: "/path/to/data-file.csv")',
+    example: 'csv.from(csv: csvData)',
     category: 'Inputs',
     link: 'https://v2.docs.influxdata.com/v2.0/reference/flux/stdlib/csv/from/',
+  },
+  {
+    name: 'csv.from',
+    args: [
+      {
+        name: 'url',
+        desc: 'The URL to retrieve annotated CSV from.',
+        type: 'String',
+      },
+    ],
+    package: 'experimental/csv',
+    desc: 'Retrieves annotated CSV data from a URL.',
+    example: 'csv.from(url: "http://example.com/data.csv")',
+    category: 'Inputs',
+    link:
+      'https://v2.docs.influxdata.com/v2.0/reference/flux/stdlib/experimental/csv/from/',
   },
   {
     name: 'cumulativeSum',
