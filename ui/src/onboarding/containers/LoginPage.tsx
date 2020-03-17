e81d764fe6fef00c5e5dbfec7eef911f11054a55
Ariel Salem <Asalem1@users.noreply.github.com>
2020-03-17 13:44:08 -0700

    fix(ui): fixed styling for login page (#17315)

--- ui/src/onboarding/containers/LoginPage.tsx
+++ ui/src/onboarding/containers/LoginPage.tsx
@@ -1,20 +1,6 @@
 // Libraries
 import React, {FC} from 'react'
-import {
-  AlignItems,
-  AppWrapper,
-  Columns,
-  FlexBox,
-  FlexDirection,
-  FontWeight,
-  Grid,
-  Heading,
-  HeadingElement,
-  JustifyContent,
-  Page,
-  Panel,
-  Typeface,
-} from '@influxdata/clockface'
+import {AppWrapper, Columns, Grid, Page} from '@influxdata/clockface'
 
 // Components
 import ErrorBoundary from 'src/shared/components/ErrorBoundary'
@@ -29,33 +15,22 @@ export const LoginPage: FC = () => (
           fullWidth={true}
           className="sign-up--page-contents"
         >
-          <Panel className="sign-up--panel">
-            <Heading
-              element={HeadingElement.H2}
-              type={Typeface.Rubik}
-              weight={FontWeight.Regular}
-            >
-              Create your Free InfluxDB Cloud Account
-            </Heading>
-            <FlexBox
-              direction={FlexDirection.Column}
-              stretchToFitHeight={true}
-              justifyContent={JustifyContent.Center}
-              alignItems={AlignItems.Center}
-            >
-              <Grid.Row className="sign-up--full-height">
-                <Grid.Column
-                  widthXS={Columns.Twelve}
-                  widthMD={Columns.Five}
-                  offsetMD={Columns.Four}
-                  widthLG={Columns.Four}
-                  className="sign-up--full-height"
-                >
-                  <LoginPageContents />
-                </Grid.Column>
-              </Grid.Row>
-            </FlexBox>
-          </Panel>
+          <h2 className="cf-funnel-page--title">
+            Create your Free InfluxDB Cloud Account
+          </h2>
+          <h5 className="cf-funnel-page--subtitle">No credit card required</h5>
+          <LoginPageContents />
+          <Grid>
+            <Grid.Row className="sign-up--full-height">
+              <Grid.Column
+                widthXS={Columns.Twelve}
+                widthMD={Columns.Five}
+                offsetMD={Columns.Four}
+                widthLG={Columns.Four}
+                className="sign-up--full-height"
+              />
+            </Grid.Row>
+          </Grid>
         </Page.Contents>
       </Page>
     </AppWrapper>
