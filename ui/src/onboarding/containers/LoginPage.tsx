4453b600529d8d293973e1105bf43c4ec1499efa
asalem <ariel.salem1989@gmail.com>
2020-03-24 08:28:09 -0700

    chore(ui): updated the login page to new design

--- ui/src/onboarding/containers/LoginPage.tsx
+++ ui/src/onboarding/containers/LoginPage.tsx
@@ -1,6 +1,6 @@
 // Libraries
 import React, {FC} from 'react'
-import {AppWrapper, Page} from '@influxdata/clockface'
+import {AppWrapper, FunnelPage, InfluxDBCloudLogo} from '@influxdata/clockface'
 
 // Components
 import ErrorBoundary from 'src/shared/components/ErrorBoundary'
@@ -9,19 +9,15 @@ import LoginPageContents from 'src/onboarding/containers/LoginPageContents'
 export const LoginPage: FC = () => (
   <ErrorBoundary>
     <AppWrapper className="sign-up--page">
-      <Page titleTag="Sign Up for InfluxDB Cloud">
-        <Page.Contents
-          scrollable={true}
-          fullWidth={true}
-          className="sign-up--page-contents"
-        >
-          <h2 className="cf-funnel-page--title">
-            Create your Free InfluxDB Cloud Account
-          </h2>
-          <h5 className="cf-funnel-page--subtitle">No credit card required</h5>
-          <LoginPageContents />
-        </Page.Contents>
-      </Page>
+      <FunnelPage
+        logo={<InfluxDBCloudLogo cloud={true} className="login-page--logo" />}
+      >
+        <h2 className="cf-funnel-page--title">
+          Create your Free InfluxDB Cloud Account
+        </h2>
+        <h5 className="cf-funnel-page--subtitle">No credit card required</h5>
+        <LoginPageContents />
+      </FunnelPage>
     </AppWrapper>
   </ErrorBoundary>
 )
