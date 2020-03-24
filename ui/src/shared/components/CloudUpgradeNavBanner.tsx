602a370876cafa18fd47a6d39a4c6181bad46b17
alexpaxton <thealexpaxton@gmail.com>
2020-03-24 09:46:46 -0700

    refactor(ui): only show tiny banner ad if screen is large enough and menu is collapsed

--- ui/src/shared/components/CloudUpgradeNavBanner.tsx
+++ ui/src/shared/components/CloudUpgradeNavBanner.tsx
@@ -1,7 +1,6 @@
 // Libraries
 import React, {FC} from 'react'
 import {Link} from 'react-router'
-import {connect} from 'react-redux'
 
 // Components
 import {
@@ -19,16 +18,9 @@ import CloudOnly from 'src/shared/components/cloud/CloudOnly'
 // Constants
 import {CLOUD_URL, CLOUD_CHECKOUT_PATH} from 'src/shared/constants'
 
-// Types
-import {AppState, NavBarState} from 'src/types'
-
-interface StateProps {
-  navBarState: NavBarState
-}
-
-const CloudUpgradeNavBanner: FC<StateProps> = ({navBarState}) => {
-  if (navBarState == 'expanded') {
-    return (
+const CloudUpgradeNavBanner: FC = () => {
+  return (
+    <>
       <CloudOnly>
         <Panel
           gradient={Gradients.HotelBreakfast}
@@ -51,34 +43,16 @@ const CloudUpgradeNavBanner: FC<StateProps> = ({navBarState}) => {
             </Link>
           </Panel.Footer>
         </Panel>
+        <Link
+          className="cloud-upgrade-banner__collapsed"
+          to={`${CLOUD_URL}${CLOUD_CHECKOUT_PATH}`}
+        >
+          <Icon glyph={IconFont.Star} />
+          <Heading element={HeadingElement.H5}>Upgrade Now</Heading>
+        </Link>
       </CloudOnly>
-    )
-  }
-
-  return (
-    <CloudOnly>
-      <Link
-        className="cloud-upgrade-banner__collapsed"
-        to={`${CLOUD_URL}${CLOUD_CHECKOUT_PATH}`}
-      >
-        <Icon glyph={IconFont.Star} />
-        <Heading element={HeadingElement.H5}>Upgrade Now</Heading>
-      </Link>
-    </CloudOnly>
+    </>
   )
 }
 
-const mstp = (state: AppState): StateProps => {
-  const {
-    app: {
-      persisted: {navBarState},
-    },
-  } = state
-
-  return {navBarState}
-}
-
-export default connect<StateProps, {}>(
-  mstp,
-  null
-)(CloudUpgradeNavBanner)
+export default CloudUpgradeNavBanner
