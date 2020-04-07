dd50c5366f7782bb3d6e093e4e0c0fe1eaf14fd1
alexpaxton <thealexpaxton@gmail.com>
2020-04-07 11:22:22 -0700

    feat(ui): sort getting started dashboards widget by modified

--- ui/src/me/components/DashboardsList.tsx
+++ ui/src/me/components/DashboardsList.tsx
@@ -1,10 +1,11 @@
 // Libraries
-import React, {PureComponent} from 'react'
+import React, {FC} from 'react'
 import {Link} from 'react-router'
 import {connect} from 'react-redux'
+import {sortBy} from 'lodash'
 
 // Components
-import {EmptyState} from '@influxdata/clockface'
+import {EmptyState, DapperScrollbars} from '@influxdata/clockface'
 
 // Types
 import {Dashboard, Organization, AppState, ResourceType} from 'src/types'
@@ -22,34 +23,32 @@ interface StateProps {
 
 type Props = StateProps
 
-class DashboardList extends PureComponent<Props> {
-  public render() {
-    const {dashboards, org} = this.props
-
-    if (this.isEmpty) {
-      return (
-        <EmptyState size={ComponentSize.ExtraSmall}>
-          <EmptyState.Text>You don't have any Dashboards</EmptyState.Text>
-        </EmptyState>
-      )
-    }
+const DashboardList: FC<Props> = ({dashboards, org}) => {
+  if (dashboards && dashboards.length) {
+    const sortedDashboards = sortBy(dashboards, ['meta.updatedAt']).reverse()
 
     return (
-      <>
-        <ul className="link-list">
-          {dashboards.map(({id, name}) => (
-            <li key={id}>
-              <Link to={`/orgs/${org.id}/dashboards/${id}`}>{name}</Link>
-            </li>
+      <DapperScrollbars autoSizeHeight={true} style={{maxHeight: '400px'}}>
+        <div className="recent-dashboards">
+          {sortedDashboards.map(({id, name}) => (
+            <Link
+              key={id}
+              to={`/orgs/${org.id}/dashboards/${id}`}
+              className="recent-dashboards--item"
+            >
+              {name}
+            </Link>
           ))}
-        </ul>
-      </>
+        </div>
+      </DapperScrollbars>
     )
   }
 
-  private get isEmpty(): boolean {
-    return !this.props.dashboards.length
-  }
+  return (
+    <EmptyState size={ComponentSize.ExtraSmall}>
+      <EmptyState.Text>You don't have any Dashboards</EmptyState.Text>
+    </EmptyState>
+  )
 }
 
 const mstp = (state: AppState): StateProps => {
