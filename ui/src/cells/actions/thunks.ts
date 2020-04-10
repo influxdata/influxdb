b987f1da66e9143d42c60116e043feac0ad3b2e1
alexpaxton <thealexpaxton@gmail.com>
2020-04-10 14:23:41 -0700

    fix(ui): make cell cloning more predictable (#17700)
    * fix: simplify cloned cell positioning
    
    * refactor: append "(Clone)" to the name of cloned cells

--- ui/src/cells/actions/thunks.ts
+++ ui/src/cells/actions/thunks.ts
@@ -25,9 +25,6 @@ import {notify} from 'src/shared/actions/notifications'
 import {setDashboard} from 'src/dashboards/actions/creators'
 import {setCells, setCell, removeCell} from 'src/cells/actions/creators'
 
-// Utils
-import {getClonedDashboardCell} from 'src/dashboards/utils/cellGetters'
-
 // Constants
 import * as copy from 'src/shared/copy/notifications'
 
@@ -71,6 +68,7 @@ export const createCellWithView = (
   clonedCell?: Cell
 ) => async (dispatch, getState: GetState): Promise<void> => {
   const state = getState()
+  let workingView = view
 
   let dashboard = getByID<Dashboard>(
     state,
@@ -106,8 +104,12 @@ export const createCellWithView = (
 
     const cellID = cellResp.data.id
 
+    if (clonedCell) {
+      workingView = {...workingView, name: `${view.name} (Clone)`}
+    }
+
     // Create the view and associate it with the cell
-    const newView = await updateView(dashboardID, cellID, view)
+    const newView = await updateView(dashboardID, cellID, workingView)
 
     const normCell = normalize<Cell, CellEntities, string>(
       {...cellResp.data, dashboardID},
@@ -182,23 +184,6 @@ export const updateCells = (dashboardID: string, cells: Cell[]) => async (
     dispatch(setCells(dashboardID, RemoteDataState.Done, normCells))
   } catch (error) {
     dispatch(notify(copy.cellUpdateFailed()))
-    console.error(error)
-  }
-}
-
-export const copyCell = (dashboard: Dashboard, cell: Cell) => dispatch => {
-  try {
-    const clonedCell = getClonedDashboardCell(dashboard, cell)
-
-    const normCell = normalize<Dashboard, DashboardEntities, string>(
-      clonedCell,
-      cellSchema
-    )
-
-    dispatch(setCell(cell.id, RemoteDataState.Done, normCell))
-    dispatch(notify(copy.cellAdded()))
-  } catch (error) {
-    dispatch(notify(copy.cellCopyFailed()))
     console.error(error)
   }
 }
