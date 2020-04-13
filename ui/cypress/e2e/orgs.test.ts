1da976fe8656cb836a2a84039fd2894b0c4a8465
Timmy Luong <TCL735@users.noreply.github.com>
2020-04-13 12:11:03 -0700

    fix(ui): add component to route hierarchy to handle org renaming

--- ui/cypress/e2e/orgs.test.ts
+++ ui/cypress/e2e/orgs.test.ts
@@ -18,4 +18,28 @@ describe('Orgs', () => {
       cy.url().should('contain', 'signin')
     })
   })
+
+  describe('when user wants to rename an org', () => {
+    beforeEach(() => {
+      cy.signin().then(({body}) => {
+        console.log('signed in, body is', body)
+      })
+
+      cy.visit('/')
+    })
+    it('should be able to rename the org', () => {
+      const extraText = '_renamed'
+      cy.getByTestID('nav-item-org').click()
+      cy.get('span:contains("About")').click()
+      cy.get('span:contains("Rename")').click()
+      cy.get('button.cf-button.cf-button-danger').click()
+      cy.getByTestID('create-org-name-input')
+        .click()
+        .type(extraText)
+      cy.get('button.cf-button.cf-button-success').click()
+      cy.get('.cf-tree-nav--team')
+        .contains(extraText)
+        .should('have.length', 1)
+    })
+  })
 })
