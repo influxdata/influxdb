d32709ed1678151f0f0006d80b41440d897411a5
Stuart Carnie <stuart.carnie@gmail.com>
2020-04-24 12:49:31 -0700

    chore: More cleanup

--- v1/models/points.go
+++ v1/models/points.go
@@ -2438,23 +2438,26 @@ func (p *point) Reset() {
 // again later to an int64
 // NOTE2: uint is accepted, and may be 64 bits, and is for some reason accepted...
 func (p Fields) MarshalBinary() []byte {
-	var b []byte
+	sz := len(p) - 1 // separators
 	keys := make([]string, 0, len(p))
-
 	for k := range p {
 		keys = append(keys, k)
+		sz += len(k)
 	}
 
-	// Not really necessary, can probably be removed.
-	sort.Strings(keys)
+	// Only sort if we have multiple fields to sort.
+	// This length check removes an allocation incurred by the sort.
+	if len(keys) > 1 {
+		sort.Strings(keys)
+	}
 
+	b := make([]byte, 0, sz)
 	for i, k := range keys {
 		if i > 0 {
 			b = append(b, ',')
 		}
 		b = appendField(b, k, p[k])
 	}
-
 	return b
 }
 
