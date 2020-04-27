d56966faceaaa360523afafb5fd0b35cda84f92b
alexpaxton <thealexpaxton@gmail.com>
2020-04-27 14:19:12 -0700

    feat(ui): ability to create a bucket from time machine (#17860)
    * feat: WIP allow on the fly bucket creation
    
    * refactor: fully implement create bucket in both places
    
    * refactor: use separate popover based component for selector list bucket creation
    
    * chore: prettier
    
    * chore: cleanup buckets tab
    
    Doesn't need org, overlay state, or any overlay components
    
    * chore: prettier
    
    * refactor: convert CreateBucketOverlay to function component
    
    * chore: changelog
    
    * chore: cleanup
    
    * feat: add integration test for creating a bucket from the query builder
    
    * refactor: rebuild selector list bucket creator with useReducer
    
    * refactor: keeping it DRY - both bucket creation components use the same state management

--- ui/src/overlays/components/OverlayController.tsx
+++ ui/src/overlays/components/OverlayController.tsx
@@ -14,6 +14,7 @@ import BucketsTokenOverlay from 'src/authorizations/components/BucketsTokenOverl
 import TelegrafConfigOverlay from 'src/telegrafs/components/TelegrafConfigOverlay'
 import TelegrafOutputOverlay from 'src/telegrafs/components/TelegrafOutputOverlay'
 import OrgSwitcherOverlay from 'src/pageLayout/components/OrgSwitcherOverlay'
+import CreateBucketOverlay from 'src/buckets/components/CreateBucketOverlay'
 
 // Actions
 import {dismissOverlay} from 'src/overlays/actions/overlays'
@@ -61,6 +62,9 @@ const OverlayController: FunctionComponent<OverlayControllerProps> = props => {
       break
     case 'switch-organizations':
       activeOverlay = <OrgSwitcherOverlay onClose={closer} />
+      break
+    case 'create-bucket':
+      activeOverlay = <CreateBucketOverlay onClose={closer} />
       break
     default:
       visibility = false
