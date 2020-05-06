25d710ea4756e47187ebd84965eed0b24c50776d
Timmy Luong <TCL735@users.noreply.github.com>
2020-05-05 19:54:57 -0700

    refactor(ui): use fetch instead of axios

--- ui/src/cloud/apis/orgsettings.ts
+++ ui/src/cloud/apis/orgsettings.ts
@@ -1,13 +1,12 @@
-import AJAX from 'src/utils/ajax'
 import {OrgSettings} from 'src/types'
+import {getAPIBasepath} from 'src/utils/basepath'
 
 export const getOrgSettings = async (orgID: string): Promise<OrgSettings> => {
   try {
-    const {data} = await AJAX({
-      method: 'GET',
-      url: `/api/v2private/orgs/${orgID}/settings`,
-    })
-
+    const response = await fetch(
+      `${getAPIBasepath()}/api/v2private/orgs/${orgID}/settings`
+    )
+    const data = await response.json()
     return data
   } catch (error) {
     console.error(error)
