ed6d6ccbadfc4bf1ff9abb813667b479053c6c33
asalem <ariel.salem1989@gmail.com>
2020-05-07 11:56:45 -0700

    feat(getTimeRangeWithTimeZone): updated to account for PR suggestions.
    Update the .circleci config file to set the timezone

--- ui/src/dashboards/selectors/index.ts
+++ ui/src/dashboards/selectors/index.ts
@@ -18,10 +18,10 @@ export const getTimeRange = (state: AppState): TimeRange => {
 export const getTimeRangeWithTimezone = (state: AppState): TimeRange => {
   const timeRange = getTimeRange(state)
   const timeZone = getTimeZone(state)
-  // create a copy of the timeRange so as not to mutate the original timeRange
-  const newTimeRange = Object.assign({}, timeRange)
+
+  const newTimeRange = {...timeRange}
   if (timeRange.type === 'custom' && timeZone === 'UTC') {
-    // check to see if the timeRange has an offset
+    // conforms dates to account to UTC with proper offset if needed
     newTimeRange.lower = setTimeToUTC(newTimeRange.lower)
     newTimeRange.upper = setTimeToUTC(newTimeRange.upper)
   }
@@ -29,27 +29,29 @@ export const getTimeRangeWithTimezone = (state: AppState): TimeRange => {
 }
 
 export const setTimeToUTC = (date: string): string => {
+  // The purpose of this function is to set a user's custom time range selection
+  // from the local time to the same time in UTC if UTC is selected from the
+  // timezone dropdown. This is feature was original requested here:
+  // https://github.com/influxdata/influxdb/issues/17877
+  // Example: user selected 10-11:00am and sets the dropdown to UTC
+  // Query should run against 10-11:00am UTC rather than querying
+  // 10-11:00am local time (offset depending on timezone)
   const offset = new Date(date).getTimezoneOffset()
-  // check if date has offset
-  if (offset === 0) {
-    return date
-  }
-  let offsetDate = date
   if (offset > 0) {
     // subtract tz minute difference
-    offsetDate = moment
+    return moment
       .utc(date)
       .subtract(offset, 'minutes')
       .format()
   }
   if (offset < 0) {
     // add tz minute difference
-    offsetDate = moment
+    return moment
       .utc(date)
       .add(offset, 'minutes')
       .format()
   }
-  return offsetDate
+  return date
 }
 
 export const getTimeZone = (state: AppState): TimeZone => {
