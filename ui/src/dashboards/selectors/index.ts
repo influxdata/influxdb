7b3b52cc6a5498111004ce9ee78d206abe6049f1
asalem <ariel.salem1989@gmail.com>
2020-05-07 06:14:10 -0700

    feat(selector_tests): refactored the previous implementation of setting custom timeRanges to UTC by creating a selector to handle that logic
    wrote tests to test the new selector

--- ui/src/dashboards/selectors/index.ts
+++ ui/src/dashboards/selectors/index.ts
@@ -1,5 +1,5 @@
 import {get} from 'lodash'
-
+import moment from 'moment'
 import {AppState, View, Check, ViewType, TimeRange, TimeZone} from 'src/types'
 import {currentContext} from 'src/shared/selectors/currentContext'
 
@@ -13,6 +13,43 @@ export const getTimeRange = (state: AppState): TimeRange => {
   }
 
   return state.ranges[contextID] || DEFAULT_TIME_RANGE
+}
+
+export const getTimeRangeWithTimezone = (state: AppState): TimeRange => {
+  const timeRange = getTimeRange(state)
+  const timeZone = getTimeZone(state)
+  // create a copy of the timeRange so as not to mutate the original timeRange
+  const newTimeRange = Object.assign({}, timeRange)
+  if (timeRange.type === 'custom' && timeZone === 'UTC') {
+    // check to see if the timeRange has an offset
+    newTimeRange.lower = setTimeToUTC(newTimeRange.lower)
+    newTimeRange.upper = setTimeToUTC(newTimeRange.upper)
+  }
+  return newTimeRange
+}
+
+export const setTimeToUTC = (date: string): string => {
+  const offset = new Date(date).getTimezoneOffset()
+  // check if date has offset
+  if (offset === 0) {
+    return date
+  }
+  let offsetDate = date
+  if (offset > 0) {
+    // subtract tz minute difference
+    offsetDate = moment
+      .utc(date)
+      .subtract(offset, 'minutes')
+      .format()
+  }
+  if (offset < 0) {
+    // add tz minute difference
+    offsetDate = moment
+      .utc(date)
+      .add(offset, 'minutes')
+      .format()
+  }
+  return offsetDate
 }
 
 export const getTimeZone = (state: AppState): TimeZone => {
