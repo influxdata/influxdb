4c2347e7ca4add2c14b74238f476308043cab847
asalem <ariel.salem1989@gmail.com>
2020-05-12 13:46:42 -0700

    feat(getRemainingRange): updated the way we were defaulting extent

--- ui/src/shared/utils/useVisDomainSettings.ts
+++ ui/src/shared/utils/useVisDomainSettings.ts
@@ -60,7 +60,7 @@ export const getRemainingRange = (
   timeRange: TimeRange | null,
   storedDomain: number[]
 ) => {
-  const range = extent((data as number[]) || [])
+  const range = extent(data as number[]) || []
   if (Array.isArray(range) && range.length >= 2) {
     const startTime = getStartTime(timeRange)
     const endTime = getEndTime(timeRange)
