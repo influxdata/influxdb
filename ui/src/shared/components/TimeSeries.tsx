5b9bf62561302a27415c1e1cb962c47a4530a130
Deniz Kusefoglu <deniz@influxdata.com>
2020-05-14 13:41:13 -0700

    feat(demodata): notify user of demodata availability error in dashboards

--- ui/src/shared/components/TimeSeries.tsx
+++ ui/src/shared/components/TimeSeries.tsx
@@ -28,9 +28,14 @@ import {buildVarsOption} from 'src/variables/utils/buildVarsOption'
 import 'intersection-observer'
 import {getAll} from 'src/resources/selectors'
 import {getOrgIDFromBuckets} from 'src/timeMachine/actions/queries'
+import {isDemoDataAvailabilityError} from 'src/cloud/utils/demoDataErrors'
 
 // Constants
-import {rateLimitReached, resultTooLarge} from 'src/shared/copy/notifications'
+import {
+  rateLimitReached,
+  resultTooLarge,
+  demoDataSwitchedOff,
+} from 'src/shared/copy/notifications'
 import {TIME_RANGE_START, TIME_RANGE_STOP} from 'src/variables/constants'
 
 // Actions
@@ -233,6 +238,9 @@ class TimeSeries extends Component<Props & WithRouterProps, State> {
 
       for (const result of results) {
         if (result.type === 'UNKNOWN_ERROR') {
+          if (isDemoDataAvailabilityError(result.code, result.message)) {
+            notify(demoDataSwitchedOff())
+          }
           errorMessage = result.message
           throw new Error(result.message)
         }
