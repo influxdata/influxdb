a280a50f9959dd131683cebd576df05dce28d71c
Alex Boatwright <drdelambre@gmail.com>
2020-05-14 13:17:19 -0700

    feat: adding notebook context (#18089)

--- ui/src/notebooks/components/Notebook.tsx
+++ ui/src/notebooks/components/Notebook.tsx
@@ -1,13 +1,16 @@
 import React, {FC} from 'react'
 
 import {Page} from '@influxdata/clockface'
+import {NotebookProvider} from 'src/notebooks/context/notebook'
 
 const NotebookPage: FC = () => {
   return (
-    <Page titleTag="Notebook">
-      <Page.Header fullWidth={false} />
-      <Page.Contents fullWidth={false} scrollable={true} />
-    </Page>
+    <NotebookProvider>
+      <Page titleTag="Notebook">
+        <Page.Header fullWidth={false} />
+        <Page.Contents fullWidth={false} scrollable={true} />
+      </Page>
+    </NotebookProvider>
   )
 }
 
