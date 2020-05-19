9653c12cd7c170fb62a96f576d072e0b7a976eec
Ayan George <ageorge@influxdata.com>
2020-05-18 21:58:50 -0400

    fix: Update CI configuration files
    This patch updates the Circle CI and Jenkins related configruation files
    to allow automated building and testing with Go 1.13.
    
    This also removes any remaning mentions of Go 1.12.

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@
 #      1: race enabled 64bit tests
 #      2: normal 32bit tests
 #      3: tsi build
-#      4: go 1.12
+#      4: go 1.13
 #      count: print the number of test environments
 #      *: to run all tests in parallel containers
 #
@@ -32,7 +32,7 @@ TIMEOUT=${TIMEOUT-1500s}
 DOCKER_RM=${DOCKER_RM-true}
 
 # Update this value if you add a new test environment.
-ENV_COUNT=5
+ENV_COUNT=4
 
 # Default return code 0
 rc=0
@@ -121,11 +121,6 @@ case $ENVIRONMENT_INDEX in
         # tsi
         INFLUXDB_DATA_INDEX_VERSION="tsi1"
         run_test_docker Dockerfile_build_ubuntu64 test_64bit --test --junit-report
-        rc=$?
-        ;;
-    4)
-        # go1.12
-        run_test_docker Dockerfile_build_ubuntu64_go1.12 test_64bit --test --junit-report
         rc=$?
         ;;
     "count")
