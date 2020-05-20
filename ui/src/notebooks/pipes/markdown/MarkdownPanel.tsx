7e574f7ef0e5b2c1a60afc4283a4d37a430f00e9
alexpaxton <thealexpaxton@gmail.com>
2020-05-20 15:31:34 -0700

    feat: WIP enable edting / preview modes of markdown panel

--- ui/src/notebooks/pipes/markdown/MarkdownPanel.tsx
+++ ui/src/notebooks/pipes/markdown/MarkdownPanel.tsx
@@ -7,11 +7,13 @@ import {MarkdownMode} from './'
 
 // Components
 import MarkdownModeToggle from './MarkdownModeToggle'
+import MarkdownPanelEditor from './MarkdownPanelEditor'
+import {MarkdownRenderer} from 'src/shared/components/views/MarkdownRenderer'
 
 const MarkdownPanel: FC<PipeProp> = ({data, Context, onUpdate}) => {
   const handleToggleMode = (mode: MarkdownMode): void => {
     const updatedData = {...data, mode}
-
+    
     onUpdate(updatedData)
   }
 
@@ -19,10 +21,26 @@ const MarkdownPanel: FC<PipeProp> = ({data, Context, onUpdate}) => {
     <MarkdownModeToggle mode={data.mode} onToggleMode={handleToggleMode} />
   )
 
+  const handleChange = (text: string): void => {
+    const updatedData = {...data, text}
+
+    onUpdate(updatedData)
+  }
+
+  let panelContents = (
+    <MarkdownPanelEditor text={data.text} onChange={handleChange} />
+  )
+
+  if (data.mode === 'preview') {
+    panelContents = (
+      <div className="notebook-panel--markdown markdown-format">
+        <MarkdownRenderer text={data.text} />
+      </div>)
+  }
+
   return (
     <Context controls={controls}>
-      {data.mode}
-      <h1>{data.text}</h1>
+      {panelContents}
     </Context>
   )
 }
