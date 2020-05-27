8b3edc369fd7556b7e699641005b8976d9dd7ea4
alexpaxton <thealexpaxton@gmail.com>
2020-05-27 14:20:33 -0700

    feat: display empty state when no pipes exist

--- ui/src/notebooks/components/PipeList.tsx
+++ ui/src/notebooks/components/PipeList.tsx
@@ -1,23 +1,40 @@
+// Libraries
 import React, {FC, useContext, useCallback} from 'react'
+
+// Contexts
 import {NotebookContext} from 'src/notebooks/context/notebook'
+
+// Components
 import NotebookPipe from 'src/notebooks/components/NotebookPipe'
+import EmptyPipeList from 'src/notebooks/components/EmptyPipeList'
+import {Page} from '@influxdata/clockface'
 
 const PipeList: FC = () => {
   const {id, pipes, updatePipe} = useContext(NotebookContext)
   const update = useCallback(updatePipe, [id])
 
-  const _pipes = pipes.map((_, index) => {
-    return (
-      <NotebookPipe
-        key={`pipe-${id}-${index}`}
-        index={index}
-        data={pipes[index]}
-        onUpdate={update}
-      />
-    )
-  })
+  const scrollable = !!pipes.length
 
-  return <>{_pipes}</>
+  let _pipes: JSX.Element | JSX.Element[] = <EmptyPipeList />
+
+  if (pipes.length) {
+    _pipes = pipes.map((_, index) => {
+      return (
+        <NotebookPipe
+          key={`pipe-${id}-${index}`}
+          index={index}
+          data={pipes[index]}
+          onUpdate={update}
+        />
+      )
+    })
+  }
+
+  return (
+    <Page.Contents fullWidth={true} scrollable={scrollable}>
+      {_pipes}
+    </Page.Contents>
+  )
 }
 
 export default PipeList
