49b0fc947372dbba2961296b8a2260fb0be79385
Gershon Shif <gershon@influxdata.com>
2020-05-27 19:37:21 -0700

    chore: update go version to 1.13.8 (#18258)

--- releng/_go_versions.sh
+++ releng/_go_versions.sh
@@ -1,5 +1,5 @@
 # These are the current and "next" Go versions used to build influxdb.
 # This file is meant to be sourced from other scripts.
 
-export GO_CURRENT_VERSION=1.12
-export GO_NEXT_VERSION=1.12
+export GO_CURRENT_VERSION=1.13.8
+export GO_NEXT_VERSION=1.13.8
