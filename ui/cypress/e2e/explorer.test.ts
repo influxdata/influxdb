96144637d74e73d19acf882e56eab424b5e9d330
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2020-06-02 15:08:36 -0600

    fix: Flakey explorer test wrong test id
    specifcally the 'can filter aggregation functions by name from script editor mode'

--- ui/cypress/e2e/explorer.test.ts
+++ ui/cypress/e2e/explorer.test.ts
@@ -516,7 +516,7 @@ describe('DataExplorer', () => {
     })
 
     it('can filter aggregation functions by name from script editor mode', () => {
-      cy.get('.cf-input-field').type('covariance')
+      cy.getByTestID('input-field').type('covariance')
       cy.get('.flux-toolbar--list-item').should('have.length', 1)
     })
 
