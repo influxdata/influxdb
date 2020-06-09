0e458e7175545a9ffbcf127466910673502f325f
Pavel Zavora <pavel.zavora@bonitoo.io>
2020-06-09 07:09:27 +0200

    fix(handler): add User-Agent to allowed CORS headers

--- services/httpd/handler.go
+++ services/httpd/handler.go
@@ -218,8 +218,8 @@ func NewHandler(c Config) *Handler {
 			"status-head",
 			"HEAD", "/status", false, true, authWrapper(h.serveStatus),
 		},
-		Route{ // Ping
-			"ping",
+		Route{ // Health
+			"health",
 			"GET", "/health", false, true, authWrapper(h.serveHealth),
 		},
 		Route{
@@ -1845,6 +1845,7 @@ func cors(inner http.Handler) http.Handler {
 				`Authorization`,
 				`Content-Length`,
 				`Content-Type`,
+				`User-Agent`,
 				`X-CSRF-Token`,
 				`X-HTTP-Method-Override`,
 			}, ", "))
