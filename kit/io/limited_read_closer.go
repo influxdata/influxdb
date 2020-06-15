8f3b887721f267d492a4b61d6bd54f76aac94257
Brett Buddin <brett@buddin.org>
2020-06-15 10:03:19 -0400

    chore: Tidy up documentation

--- kit/io/limited_read_closer.go
+++ kit/io/limited_read_closer.go
@@ -7,12 +7,16 @@ import (
 
 var ErrReadLimitExceeded = errors.New("read limit exceeded")
 
+// LimitedReadCloser wraps an io.ReadCloser in limiting behavior using
+// io.LimitedReader. It allows us to obtain the limit error at the time of close
+// instead of just when writing.
 type LimitedReadCloser struct {
 	*io.LimitedReader
 	err   error
 	close func() error
 }
 
+// NewLimitedReadCloser returns a new LimitedReadCloser.
 func NewLimitedReadCloser(r io.ReadCloser, n int64) *LimitedReadCloser {
 	// read up to max + 1 as limited reader just returns EOF when the limit is reached
 	// or when there is nothing left to read. If we exceed the max batch size by one
@@ -23,9 +27,9 @@ func NewLimitedReadCloser(r io.ReadCloser, n int64) *LimitedReadCloser {
 	}
 }
 
-// Close returns an ErrMaxBatchSizeExceeded when the wrapped reader
-// exceeds the set limit for number of bytes.
-// This is safe to call more than once but not concurrently.
+// Close returns an ErrReadLimitExceeded when the wrapped reader exceeds the set
+// limit for number of bytes.  This is safe to call more than once but not
+// concurrently.
 func (l *LimitedReadCloser) Close() (err error) {
 	defer func() {
 		if cerr := l.close(); cerr != nil && err == nil {
