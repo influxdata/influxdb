85c0d900174f17a58c1290ec538bfdb0d433f6af
Alex Boatwright <drdelambre@gmail.com>
2020-06-16 16:24:57 -0700

    feat: adding pagination ability to raw data (#18474)

--- ui/src/notebooks/pipes/Query/Results.tsx
+++ ui/src/notebooks/pipes/Query/Results.tsx
@@ -1,11 +1,13 @@
 // Libraries
-import React, {FC} from 'react'
+import React, {FC, useEffect, useState, useMemo} from 'react'
 import {BothResults} from 'src/notebooks/context/query'
 import {AutoSizer} from 'react-virtualized'
 
 // Components
 import RawFluxDataTable from 'src/timeMachine/components/RawFluxDataTable'
+import {ROW_HEIGHT} from 'src/timeMachine/components/RawFluxDataGrid'
 import Resizer from 'src/notebooks/shared/Resizer'
+import ResultsPagination from 'src/notebooks/pipes/Query/ResultsPagination'
 
 // Types
 import {PipeData} from 'src/notebooks/index'
@@ -19,6 +21,36 @@ interface Props {
 const Results: FC<Props> = ({results, onUpdate, data}) => {
   const resultsExist = !!results.raw && !!results.parsed.table.length
 
+  const rows = useMemo(() => (results.raw || '').split('\n'), [results.raw])
+  const [startRow, setStartRow] = useState<number>(0)
+  const [pageSize, setPageSize] = useState<number>(0)
+
+  useEffect(() => {
+    setStartRow(0)
+  }, [results.raw])
+
+  const prevDisabled = startRow <= 0
+  const nextDisabled = startRow + pageSize >= rows.length
+
+  const prev = () => {
+    const index = startRow - pageSize
+    if (index <= 0) {
+      setStartRow(0)
+      return
+    }
+    setStartRow(index)
+  }
+
+  const next = () => {
+    const index = startRow + pageSize
+    const max = rows.length - pageSize
+    if (index >= max) {
+      setStartRow(max)
+      return
+    }
+    setStartRow(index)
+  }
+
   return (
     <Resizer
       data={data}
@@ -28,18 +60,38 @@ const Results: FC<Props> = ({results, onUpdate, data}) => {
       hiddenText="Results hidden"
       toggleVisibilityEnabled={true}
     >
-      <AutoSizer>
-        {({width, height}) =>
-          width &&
-          height && (
-            <RawFluxDataTable
-              files={[results.raw]}
-              width={width}
-              height={height}
-            />
-          )
-        }
-      </AutoSizer>
+      <div className="query-results">
+        <ResultsPagination
+          onClickPrev={prev}
+          onClickNext={next}
+          disablePrev={prevDisabled}
+          disableNext={nextDisabled}
+          visible={resultsExist && data.panelVisibility === 'visible'}
+          pageSize={pageSize}
+          startRow={startRow}
+        />
+        <div className="query-results--container">
+          <AutoSizer>
+            {({width, height}) => {
+              if (!width || !height) {
+                return false
+              }
+
+              const page = Math.floor(height / ROW_HEIGHT)
+              setPageSize(page)
+
+              return (
+                <RawFluxDataTable
+                  files={[rows.slice(startRow, startRow + page).join('\n')]}
+                  width={width}
+                  height={page * ROW_HEIGHT}
+                  disableVerticalScrolling={true}
+                />
+              )
+            }}
+          </AutoSizer>
+        </div>
+      </div>
     </Resizer>
   )
 }
