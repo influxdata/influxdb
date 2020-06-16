4e535f36a384c9a514c5cf3ac4f59934cd1c801a
alexpaxton <thealexpaxton@gmail.com>
2020-06-16 16:01:24 -0700

    feat: style results pagination

--- ui/src/notebooks/pipes/Query/Results.tsx
+++ ui/src/notebooks/pipes/Query/Results.tsx
@@ -2,12 +2,12 @@
 import React, {FC, useEffect, useState, useMemo} from 'react'
 import {BothResults} from 'src/notebooks/context/query'
 import {AutoSizer} from 'react-virtualized'
-import classnames from 'classnames'
 
 // Components
 import RawFluxDataTable from 'src/timeMachine/components/RawFluxDataTable'
 import {ROW_HEIGHT} from 'src/timeMachine/components/RawFluxDataGrid'
 import Resizer from 'src/notebooks/shared/Resizer'
+import ResultsPagination from 'src/notebooks/pipes/Query/ResultsPagination'
 
 // Types
 import {PipeData} from 'src/notebooks/index'
@@ -22,19 +22,16 @@ const Results: FC<Props> = ({results, onUpdate, data}) => {
   const resultsExist = !!results.raw && !!results.parsed.table.length
 
   const rows = useMemo(() => (results.raw || '').split('\n'), [results.raw])
-  const [startRow, setStartRow] = useState(0)
-  const [pageSize, setPageSize] = useState(0)
+  const [startRow, setStartRow] = useState<number>(0)
+  const [pageSize, setPageSize] = useState<number>(0)
 
   useEffect(() => {
     setStartRow(0)
   }, [results.raw])
 
-  const prevClass = classnames('notebook-raw-data--prev', {
-    'notebook-raw-data--button__disabled': startRow <= 0,
-  })
-  const nextClass = classnames('notebook-raw-data--next', {
-    'notebook-raw-data--button__disabled': startRow + pageSize >= rows.length,
-  })
+  const prevDisabled = startRow <= 0
+  const nextDisabled = startRow + pageSize >= rows.length
+
   const prev = () => {
     const index = startRow - pageSize
     if (index <= 0) {
@@ -55,45 +52,47 @@ const Results: FC<Props> = ({results, onUpdate, data}) => {
   }
 
   return (
-    <>
-      <Resizer
-        data={data}
-        onUpdate={onUpdate}
-        resizingEnabled={resultsExist}
-        emptyText="Run the Flow to see Results"
-        hiddenText="Results hidden"
-        toggleVisibilityEnabled={true}
-      >
-        <AutoSizer>
-          {({width, height}) => {
-            if (!width || !height) {
-              return false
-            }
+    <Resizer
+      data={data}
+      onUpdate={onUpdate}
+      resizingEnabled={resultsExist}
+      emptyText="Run the Flow to see Results"
+      hiddenText="Results hidden"
+      toggleVisibilityEnabled={true}
+    >
+      <div className="query-results">
+        <ResultsPagination
+          onClickPrev={prev}
+          onClickNext={next}
+          disablePrev={prevDisabled}
+          disableNext={nextDisabled}
+          visible={resultsExist && data.panelVisibility === 'visible'}
+          pageSize={pageSize}
+          startRow={startRow}
+        />
+        <div className="query-results--container">
+          <AutoSizer>
+            {({width, height}) => {
+              if (!width || !height) {
+                return false
+              }
 
-            const page = Math.floor(height / ROW_HEIGHT)
-            setPageSize(page)
+              const page = Math.floor(height / ROW_HEIGHT)
+              setPageSize(page)
 
-            return (
-              <RawFluxDataTable
-                files={[rows.slice(startRow, startRow + page).join('\n')]}
-                width={width}
-                height={page * ROW_HEIGHT}
-              />
-            )
-          }}
-        </AutoSizer>
-      </Resizer>
-      {resultsExist && data.resultsVisibility === 'visible' && (
-        <div>
-          <div className={prevClass} onClick={prev}>
-            prev
-          </div>
-          <div className={nextClass} onClick={next}>
-            next
-          </div>
+              return (
+                <RawFluxDataTable
+                  files={[rows.slice(startRow, startRow + page).join('\n')]}
+                  width={width}
+                  height={page * ROW_HEIGHT}
+                  disableVerticalScrolling={true}
+                />
+              )
+            }}
+          </AutoSizer>
         </div>
-      )}
-    </>
+      </div>
+    </Resizer>
   )
 }
 
