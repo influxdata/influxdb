73a3e826972526c3558e138ca7bde4f989e172df
alexpaxton <thealexpaxton@gmail.com>
2020-06-23 13:08:25 -0700

    feat(templates-ui): display template resources in accordion list (#18669)
    * feat: display template resources list in accordion fashion
    
    * refactor: polish template name widget and add graphic
    
    * fix: cleanup

---
+++ ui/src/templates/components/CommunityTemplateListGroup.tsx
@@ -0,0 +1,69 @@
+// Libraries
+import React, {FC, ReactNode, useState} from 'react'
+import classnames from 'classnames'
+
+// Components
+import {
+  Heading,
+  HeadingElement,
+  Icon,
+  IconFont,
+  FlexBox,
+  ComponentSize,
+  AlignItems,
+  FlexDirection,
+} from '@influxdata/clockface'
+
+interface Props {
+  title: string
+  count?: string
+  children?: ReactNode
+}
+
+const CommunityTemplateListGroup: FC<Props> = ({title, count, children}) => {
+  const [mode, setMode] = useState<'expanded' | 'collapsed'>('collapsed')
+  const groupClassName = classnames('community-templates--list-group', {
+    [`community-templates--list-group__${mode}`]: mode,
+  })
+
+  const handleToggleMode = () => {
+    if (mode === 'expanded') {
+      setMode('collapsed')
+    } else {
+      setMode('expanded')
+    }
+  }
+
+  return (
+    <div className={groupClassName}>
+      <div
+        className="community-templates--list-header"
+        onClick={handleToggleMode}
+      >
+        <div className="community-templates--list-toggle">
+          <Icon glyph={IconFont.CaretRight} />
+        </div>
+        <Heading element={HeadingElement.H5}>{title}</Heading>
+        {count && (
+          <Heading
+            element={HeadingElement.Div}
+            appearance={HeadingElement.H6}
+            className="community-templates--list-counter"
+          >{`(${count})`}</Heading>
+        )}
+      </div>
+      {mode === 'expanded' && (
+        <FlexBox
+          margin={ComponentSize.Small}
+          direction={FlexDirection.Column}
+          alignItems={AlignItems.Stretch}
+          className="community-templates--list-group-items"
+        >
+          {children}
+        </FlexBox>
+      )}
+    </div>
+  )
+}
+
+export default CommunityTemplateListGroup
