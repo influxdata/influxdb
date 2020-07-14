d0ea4c237438a67247e923b9bb2e8176b4c40709
Michael Desa <mjdesa@gmail.com>
2020-07-14 13:39:33 -0700

    fix: remove extra catious empty me state

--- ui/src/me/selectors/index.ts
+++ ui/src/me/selectors/index.ts
@@ -1,20 +1,7 @@
-// Libraries
-import {get} from 'lodash'
-
 // Types
 import {AppState} from 'src/types'
 import {MeState} from 'src/shared/reducers/me'
 
-// TODO(desa): is this reasonable or can I trust app state?
-const emptyMeState: MeState = {
-  id: '',
-  name: '',
-  links: {
-    self: '',
-    log: '',
-  },
-}
-
 export const getMe = (state: AppState): MeState => {
-  return get(state, 'me', emptyMeState)
+  return state.me
 }
