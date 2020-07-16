6f8e22beb9c41cad5537a041087b360a8dac9d20
Gavin Cabbage <dev@gavincabbage.com>
2020-07-16 19:58:36 -0400

    refactor(tsdb): improve error message when missing testdata

--- tsdb/tsi1/gen_test.go
+++ tsdb/tsi1/gen_test.go
@@ -1,2 +1,10 @@
 //go:generate sh -c "curl -L https://github.com/influxdata/testdata/raw/master/tsi1testdata.tar.gz | tar xz"
 package tsi1_test
+
+import "os"
+
+func init() {
+	if _, err := os.Stat("./testdata"); err != nil {
+		panic("Run go generate to download testdata directory.")
+	}
+}
