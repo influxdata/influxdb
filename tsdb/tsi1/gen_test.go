bafcefc94c564f67c9170804f33165c735d82ca4
Gavin Cabbage <dev@gavincabbage.com>
2020-07-17 08:21:14 -0400

    refactor(tsdb): tagged testdata and avoid stacktrace when missing

--- tsdb/tsi1/gen_test.go
+++ tsdb/tsi1/gen_test.go
@@ -1,10 +1,14 @@
-//go:generate sh -c "curl -L https://github.com/influxdata/testdata/raw/master/tsi1testdata.tar.gz | tar xz"
+//go:generate sh -c "curl -L https://github.com/influxdata/testdata/raw/2020.07.17.0/tsi1testdata.tar.gz | tar xz"
 package tsi1_test
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func init() {
 	if _, err := os.Stat("./testdata"); err != nil {
-		panic("Run go generate to download testdata directory.")
+		fmt.Println("Run go generate to download testdata directory.")
+		os.Exit(1)
 	}
 }
