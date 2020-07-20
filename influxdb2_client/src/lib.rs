53ec997d8df88f08ac2605cff7235fd9effcd011
Carol (Nichols || Goulding) <carol.nichols@integer32.com>
2020-07-20 11:22:33 -0400

    docs: Clean up link to API docs

--- influxdb2_client/src/lib.rs
+++ influxdb2_client/src/lib.rs
@@ -9,8 +9,9 @@
 
 //! # influxdb2_client
 //!
-//! This is a Rust client to InfluxDB using the 2.0 API.
-//! that implements the API in https://v2.docs.influxdata.com/v2.0/reference/api/
+//! This is a Rust client to InfluxDB using the [2.0 API][2api].
+//!
+//! [2api]: https://v2.docs.influxdata.com/v2.0/reference/api/
 //!
 //! ## Work Remaining
 //!
