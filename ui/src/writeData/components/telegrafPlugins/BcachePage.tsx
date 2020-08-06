8fda8ac4319f9597ed4faa932c00b8afe8aac2c3
alexpaxton <thealexpaxton@gmail.com>
2020-08-06 14:54:13 -0700

    feat: use markdown source for example telegraf plugin

--- ui/src/writeData/components/telegrafPlugins/BcachePage.tsx
+++ ui/src/writeData/components/telegrafPlugins/BcachePage.tsx
@@ -1,34 +1,14 @@
 // Libraries
-import React, {FunctionComponent} from 'react'
-import {connect} from 'react-redux'
+import React, {FC} from 'react'
 
 // Components
 import TelegrafPluginPage from 'src/writeData/components/telegrafPlugins/TelegrafPluginPage'
 
-// Types
-import {AppState} from 'src/types'
+// Markdown
+import markdown from 'src/writeData/components/telegrafPlugins/Bcache.md'
 
-// Selectors
-import {getOrg} from 'src/organizations/selectors'
-
-interface StateProps {
-  org: string
+const BcachePage: FC = () => {
+  return <TelegrafPluginPage title="Bcache" markdown={markdown} />
 }
 
-type Props = StateProps
-
-const BcachePage: FunctionComponent<Props> = () => {
-  return (
-    <TelegrafPluginPage title="Bcache">
-      <p>Docs go here</p>
-    </TelegrafPluginPage>
-  )
-}
-
-const mstp = (state: AppState) => {
-  return {
-    org: getOrg(state).id,
-  }
-}
-
-export default connect<StateProps, {}, Props>(mstp, null)(BcachePage)
+export default BcachePage
--- ui/src/writeData/components/telegrafPlugins/TelegrafPluginPage.tsx
+++ ui/src/writeData/components/telegrafPlugins/TelegrafPluginPage.tsx
@@ -1,21 +1,29 @@
 // Libraries
 import React, {FC, ReactNode} from 'react'
+import ReactMarkdown, {Renderer} from 'react-markdown'
 
 // Components
 import {Page} from '@influxdata/clockface'
+import CodeSnippet from 'src/shared/components/CodeSnippet'
 
 interface Props {
   title: string
+  markdown: string
   children?: ReactNode
 }
 
-const TelegrafPluginPage: FC<Props> = ({title, children}) => {
+const codeRenderer: Renderer<HTMLPreElement> = (props: HTMLPreElement): any => {
+  return <CodeSnippet copyText={props.value} label={props.language} />
+}
+
+const TelegrafPluginPage: FC<Props> = ({title, children, markdown}) => {
   return (
     <Page>
       <Page.Header fullWidth={false}>
         <Page.Title title={title} />
       </Page.Header>
       <Page.Contents fullWidth={false} scrollable={true}>
+        <ReactMarkdown source={markdown} renderers={{code: codeRenderer}} />
         {children}
       </Page.Contents>
     </Page>
