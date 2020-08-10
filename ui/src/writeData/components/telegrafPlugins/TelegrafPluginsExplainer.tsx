32fbca761fab8d0b0ea18183a4a0937b49d28b8e
alexpaxton <thealexpaxton@gmail.com>
2020-08-10 16:21:06 -0700

    refactor: add real content to telegraf plugin explainer

--- ui/src/writeData/components/telegrafPlugins/TelegrafPluginsExplainer.tsx
+++ ui/src/writeData/components/telegrafPlugins/TelegrafPluginsExplainer.tsx
@@ -2,33 +2,41 @@
 import React, {FC} from 'react'
 
 // Components
-import {Panel, InfluxColors} from '@influxdata/clockface'
+import {
+  Panel,
+  InfluxColors,
+  Heading,
+  FontWeight,
+  HeadingElement,
+} from '@influxdata/clockface'
 
 const TelegrafPluginsExplainer: FC = () => {
   return (
-    <>
-      <h3>Getting Started with Telegraf</h3>
-      <Panel backgroundColor={InfluxColors.Castle}>
-        <Panel.Body>
-          <p>
-            Telegraf is InfluxData’s data collection agent for collecting and
-            reporting metrics. Its vast library of input plugins and
-            “plug-and-play” architecture lets you quickly and easily collect
-            metrics from many different sources.
-          </p>
-          <p>
-            You will need to have Telegraf installed in order to use this
-            plugin. See our handy{' '}
-            <a
-              href="https://docs.influxdata.com/telegraf/v1.15/introduction/installation/"
-              target="_blank"
-            >
-              Installation Guide
-            </a>
-          </p>
-        </Panel.Body>
-      </Panel>
-    </>
+    <Panel backgroundColor={InfluxColors.Castle} style={{marginBottom: '8px'}}>
+      <Panel.Header>
+        <Heading element={HeadingElement.H4} weight={FontWeight.Regular}>
+          Getting Started with Telegraf
+        </Heading>
+      </Panel.Header>
+      <Panel.Body>
+        <p>
+          Telegraf is InfluxData’s data collection agent for collecting and
+          reporting metrics. Its vast library of input plugins and
+          “plug-and-play” architecture lets you quickly and easily collect
+          metrics from many different sources.
+        </p>
+        <p>
+          You will need to have Telegraf installed in order to use this plugin.
+          See our handy{' '}
+          <a
+            href="https://docs.influxdata.com/telegraf/v1.15/introduction/installation/"
+            target="_blank"
+          >
+            Installation Guide
+          </a>
+        </p>
+      </Panel.Body>
+    </Panel>
   )
 }
 
