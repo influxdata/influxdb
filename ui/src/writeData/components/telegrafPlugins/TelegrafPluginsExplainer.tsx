2322b4a41a1813041a3806ed2d12c0f8e829e587
alexpaxton <thealexpaxton@gmail.com>
2020-08-10 10:31:30 -0700

    feat: add telegraf explainer to telegraf plugins pages

---
+++ ui/src/writeData/components/telegrafPlugins/TelegrafPluginsExplainer.tsx
@@ -0,0 +1,35 @@
+// Libraries
+import React, {FC} from 'react'
+
+// Components
+import {Panel, InfluxColors} from '@influxdata/clockface'
+
+const TelegrafPluginsExplainer: FC = () => {
+  return (
+    <>
+      <h3>Getting Started with Telegraf</h3>
+      <Panel backgroundColor={InfluxColors.Castle}>
+        <Panel.Body>
+          <p>
+            Telegraf is InfluxData’s data collection agent for collecting and
+            reporting metrics. Its vast library of input plugins and
+            “plug-and-play” architecture lets you quickly and easily collect
+            metrics from many different sources.
+          </p>
+          <p>
+            You will need to have Telegraf installed in order to use these
+            plugins. See our handy{' '}
+            <a
+              href="https://docs.influxdata.com/telegraf/v1.15/introduction/installation/"
+              target="_blank"
+            >
+              Installation Guide
+            </a>
+          </p>
+        </Panel.Body>
+      </Panel>
+    </>
+  )
+}
+
+export default TelegrafPluginsExplainer
--- ui/src/writeData/containers/TelegrafPluginsPage.tsx
+++ ui/src/writeData/containers/TelegrafPluginsPage.tsx
@@ -5,6 +5,7 @@ import {Switch, Route} from 'react-router-dom'
 // Components
 import {ErrorHandling} from 'src/shared/decorators/errors'
 import TelegrafPluginsIndex from 'src/writeData/components/telegrafPlugins/TelegrafPluginsIndex'
+import TelegrafPluginsExplainer from 'src/writeData/components/telegrafPlugins/TelegrafPluginsExplainer'
 import WriteDataDetailsView from 'src/writeData/components/WriteDataDetailsView'
 
 // Constants
@@ -14,7 +15,11 @@ import WRITE_DATA_TELEGRAF_PLUGINS_SECTION from 'src/writeData/constants/content
 const telegrafPluginPath = `/${ORGS}/${ORG_ID}/load-data/${TELEGRAF_PLUGINS}`
 
 const TelegrafPluginsDetailsPage: FC = () => {
-  return <WriteDataDetailsView section={WRITE_DATA_TELEGRAF_PLUGINS_SECTION} />
+  return (
+    <WriteDataDetailsView section={WRITE_DATA_TELEGRAF_PLUGINS_SECTION}>
+      <TelegrafPluginsExplainer />
+    </WriteDataDetailsView>
+  )
 }
 
 @ErrorHandling
