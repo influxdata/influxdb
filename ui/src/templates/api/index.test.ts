c96059b67566ce91502932aa555cdc2fdc62c098
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-08-10 11:59:21 -0700

    added some mocks

--- ui/src/templates/api/index.test.ts
+++ ui/src/templates/api/index.test.ts
@@ -1,21 +1,76 @@
+import {mocked} from 'ts-jest/utils'
+jest.mock('src/client')
+
+import {postTemplatesApply as postTemplatesApplyMock} from 'src/client'
+
+import {reviewTemplate} from 'src/templates/api/'
+
 describe('templates api calls', () => {
-    it('reviewTemplate', () => {
+  it('reviews a template successfully', async () => {
+    const orgID = '1234'
+    const templateUrl = 'http://example.com'
 
+    mocked(postTemplatesApplyMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        data: {
+          foo: 'hi!'
+        }
+      })
     })
 
-    it('installTemplate', () => {
+    const summary = await reviewTemplate(orgID, templateUrl)
+    const [mockArguments] = mocked(postTemplatesApplyMock).mock.calls[0]
 
+    expect(mockArguments.data.dryRun).toBeTruthy()
+    expect(mockArguments.data.orgID).toBe(orgID)
+    expect(mockArguments.data.remotes[0].url).toBe(templateUrl)
+  })
+
+  it ('reviews a template unsuccessfully', async () => {
+    const orgID = '1234'
+    const templateUrl = 'http://example.com'
+
+    mocked(postTemplatesApplyMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 404,
+        data: {
+          message: 'whoops'
+        }
+      })
     })
 
-    it('fetchStacks', () => {
+    try {
+      await reviewTemplate(orgID, templateUrl)
+    } catch (error) {
+      expect(error.message).toBe('whoops')
+    }
 
-    })
+  })
 
-    it('deleteStack', () => {
+//   it('installTemplate', () => {
+//     mocked(postTemplatesApplyMock).mockImplementation(() => {
+//       return Promise.resolve({
+//         status: 200,
+//         data: {
+//           foo: 'hi!'
+//         }
+//       })
 
-    })
+        // // TODO: test for dry-run being false
+//     })
 
-    it('updateStackName', () => {
+//   })
 
-    })
+//   it('fetchStacks', () => {
+
+//   })
+
+//   it('deleteStack', () => {
+
+//   })
+
+//   it('updateStackName', () => {
+
+//   })
 })
