30ec017e647063b0e2864431d2eeddf13a83c7d1
Bucky Schwarz <d.w.schwarz@gmail.com>
2020-08-11 08:55:26 -0700

    refactor: change communityTemplateToInstall => stagedCommunityTemplate

--- ui/src/templates/components/CommunityTemplateImportOverlay.tsx
+++ ui/src/templates/components/CommunityTemplateImportOverlay.tsx
@@ -6,7 +6,7 @@ import {connect, ConnectedProps} from 'react-redux'
 import {CommunityTemplateOverlay} from 'src/templates/components/CommunityTemplateOverlay'
 
 // Actions
-import {setCommunityTemplateToInstall} from 'src/templates/actions/creators'
+import {setStagedCommunityTemplate} from 'src/templates/actions/creators'
 import {createTemplate, fetchAndSetStacks} from 'src/templates/actions/thunks'
 import {notify} from 'src/shared/actions/notifications'
 
@@ -89,7 +89,7 @@ class UnconnectedTemplateImportOverlay extends PureComponent<Props> {
     try {
       const summary = await reviewTemplate(orgID, yamlLocation)
 
-      this.props.setCommunityTemplateToInstall(summary)
+      this.props.setStagedCommunityTemplate(summary)
       return summary
     } catch (err) {
       this.props.notify(communityTemplateInstallFailed(err.message))
@@ -157,17 +157,17 @@ const mstp = (state: AppState, props: RouterProps) => {
     templateExtension: props.match.params.templateExtension,
     flags: state.flags.original,
     resourceCount: getTotalResourceCount(
-      state.resources.templates.communityTemplateToInstall.summary
+      state.resources.templates.stagedCommunityTemplate.summary
     ),
     resourcesToSkip:
-      state.resources.templates.communityTemplateToInstall.resourcesToSkip,
+      state.resources.templates.stagedCommunityTemplate.resourcesToSkip,
   }
 }
 
 const mdtp = {
   createTemplate,
   notify,
-  setCommunityTemplateToInstall,
+  setStagedCommunityTemplate,
   fetchAndSetStacks,
 }
 
