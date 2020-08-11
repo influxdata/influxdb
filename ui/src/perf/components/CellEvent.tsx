d0a42098a0a251e39d0b4e7ec4702d56c417ac2f
Andrew Watkins <watts@influxdb.com>
2020-08-11 16:36:06 -0700

    perf: calculate and event dashboard visit => dashboard mount times (#19291)
    * perf: calculate and emit dashboard and variable render times
    
    * fix: name of action

--- ui/src/perf/components/CellEvent.tsx
+++ ui/src/perf/components/CellEvent.tsx
@@ -16,11 +16,10 @@ interface Props {
 const getState = (cellID: string) => (state: AppState) => {
   const {perf} = state
   const {dashboard, cells} = perf
-  const {scroll, renderID} = dashboard
+  const {scroll} = dashboard
 
   return {
     scroll,
-    renderID,
     cellMountStartMs: cells.byID[cellID]?.mountStartMs,
   }
 }
@@ -29,31 +28,14 @@ const CellEvent: FC<Props> = ({id, type}) => {
   const params = useParams<{dashboardID?: string}>()
   const dashboardID = params?.dashboardID
 
-  const {renderID, scroll, cellMountStartMs} = useSelector(getState(id))
-
-  useEffect(() => {
-    if (scroll === 'scrolled') {
-      return
-    }
-
-    const hasIDs = dashboardID && id && renderID
-
-    if (!hasIDs) {
-      return
-    }
-
-    const tags = {dashboardID, cellID: id, type}
-    const fields = {renderID}
-
-    event('Cell Visualized', tags, fields)
-  }, [dashboardID, id, renderID, type, scroll])
+  const {cellMountStartMs} = useSelector(getState(id))
 
   useEffect(() => {
     if (!cellMountStartMs) {
       return
     }
 
-    const hasIDs = dashboardID && id && renderID
+    const hasIDs = dashboardID && id
     if (!hasIDs) {
       return
     }
@@ -62,10 +44,10 @@ const CellEvent: FC<Props> = ({id, type}) => {
     const timeToAppearMs = visRenderedMs - cellMountStartMs
 
     const tags = {dashboardID, cellID: id, type}
-    const fields = {timeToAppearMs, renderID}
+    const fields = {timeToAppearMs}
 
     event('Cell Render Cycle', tags, fields)
-  }, [cellMountStartMs, dashboardID, id, renderID, type])
+  }, [cellMountStartMs, dashboardID, id, type])
 
   return null
 }
