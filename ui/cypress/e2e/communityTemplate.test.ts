1e90f5e5cbddc6e4a6f4563c038e6374f15e50f6
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2020-08-12 09:37:10 -0600

    test: e2e test failing

--- ui/cypress/e2e/communityTemplate.test.ts
+++ ui/cypress/e2e/communityTemplate.test.ts
@@ -15,12 +15,8 @@ describe('Community Templates', () => {
   })
 
   it('The browse community template button launches github', () => {
-    cy.getByTestID('browse-template-button')
-      .should('have.prop', 'href')
-      .and(
-        'equal',
-        'https://github.com/influxdata/community-templates#templates'
-      )
+    cy.getByTestID('browse-template-button').invoke('removeAttr', 'target').click()
+    cy.url().should('include', 'https://github.com/influxdata/community-templates#templates')
   })
 
   it('The lookup template errors on invalid data', () => {
