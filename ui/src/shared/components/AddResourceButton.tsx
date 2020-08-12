e84fc20f42e7ce0224f18e33ae16421d1945e9d9
Deniz Kusefoglu <deniz@influxdata.com>
2020-08-12 10:42:16 -0700

    feat(templates): Fix AddResourceButton

--- ui/src/shared/components/AddResourceButton.tsx
+++ ui/src/shared/components/AddResourceButton.tsx
@@ -1,5 +1,5 @@
 // Libraries
-import React, {PureComponent} from 'react'
+import React, {FC} from 'react'
 import {connect, ConnectedProps} from 'react-redux'
 import _ from 'lodash'
 
@@ -8,7 +8,7 @@ import {
   IconFont,
   ComponentColor,
   ComponentSize,
-  Dropdown,
+  Button,
   ComponentStatus,
 } from '@influxdata/clockface'
 
@@ -21,104 +21,43 @@ import {LimitStatus} from 'src/cloud/actions/limits'
 // Constants
 import {CLOUD} from 'src/shared/constants'
 
-interface OwnProps {
+interface Props {
   onSelectNew: () => void
   resourceName: string
+  status?: ComponentStatus
   limitStatus?: LimitStatus
-}
-
-interface DefaultProps {
-  status: ComponentStatus
-  titleText: string
 }
 
 type ReduxProps = ConnectedProps<typeof connector>
 
-type Props = OwnProps & DefaultProps & ReduxProps
-
-class AddResourceButton extends PureComponent<Props> {
-  public static defaultProps: DefaultProps = {
-    status: ComponentStatus.Default,
-    titleText: null,
-  }
-
-  public render() {
-    const {titleText, status} = this.props
-    return (
-      <Dropdown
-        style={{width: '190px'}}
-        testID="add-resource-dropdown"
-        button={(active, onClick) => (
-          <Dropdown.Button
-            testID="add-resource-dropdown--button"
-            active={active}
-            onClick={onClick}
-            color={ComponentColor.Primary}
-            size={ComponentSize.Small}
-            icon={IconFont.Plus}
-            status={status}
-          >
-            {titleText || `Create ${this.props.resourceName}`}
-          </Dropdown.Button>
-        )}
-        menu={onCollapse => (
-          <Dropdown.Menu
-            onCollapse={onCollapse}
-            testID="add-resource-dropdown--menu"
-          >
-            {this.optionItems}
-          </Dropdown.Menu>
-        )}
-      >
-        {this.optionItems}
-      </Dropdown>
-    )
-  }
-
-  private get optionItems(): JSX.Element[] {
-    const importOption = this.importOption
-    const newOption = this.newOption
-
-    const items = [
-      <Dropdown.Item
-        id={importOption}
-        key={importOption}
-        onClick={this.handleSelect}
-        value={importOption}
-        testID="add-resource-dropdown--import"
-      >
-        {importOption}
-      </Dropdown.Item>,
-    ]
-
-    return items
-  }
-
-  private get newOption(): string {
-    return `New ${this.props.resourceName}`
-  }
-
-  private get importOption(): string {
-    return `Import ${this.props.resourceName}`
-  }
-
-  private handleLimit = (): void => {
-    const {resourceName, onShowOverlay, onDismissOverlay} = this.props
+const AddResourceButton: FC<Props & ReduxProps> = ({
+  resourceName,
+  onSelectNew,
+  onShowOverlay,
+  onDismissOverlay,
+  limitStatus = LimitStatus.OK,
+  status = ComponentStatus.Default,
+}) => {
+  const showLimitOverlay = () =>
     onShowOverlay('asset-limit', {asset: `${resourceName}s`}, onDismissOverlay)
-  }
 
-  private handleSelect = (selection: string): void => {
-    const {onSelectNew, limitStatus = LimitStatus.OK} = this.props
+  const onClick =
+    CLOUD && limitStatus === LimitStatus.EXCEEDED
+      ? showLimitOverlay
+      : onSelectNew
 
-    if (CLOUD && limitStatus === LimitStatus.EXCEEDED) {
-      this.handleLimit()
-      return
-    }
-
-    if (selection === this.newOption) {
-      onSelectNew()
-    }
-  }
+  return (
+    <Button
+      style={{width: '190px'}}
+      testID="add-resource-dropdown--button"
+      onClick={onClick}
+      color={ComponentColor.Primary}
+      size={ComponentSize.Small}
+      text={`Create New ${resourceName}`}
+      icon={IconFont.Plus}
+      status={status}
+    />
+  )
 }
 
 const mdtp = {
