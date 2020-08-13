b2edbfc33e3dcca9e8e84d939288595ff39a3d56
Zoe Steinkamp <zoe.steinkamp@gmail.com>
2020-08-13 10:17:51 -0600

    test: all success tests passing

--- ui/src/templates/api/index.test.ts
+++ ui/src/templates/api/index.test.ts
@@ -1,11 +1,26 @@
 import {mocked} from 'ts-jest/utils'
 jest.mock('src/client')
 
-import {postTemplatesApply as postTemplatesApplyMock} from 'src/client'
+import {
+  postTemplatesApply as postTemplatesApplyMock,
+  getStacks as getStacksMock,
+  deleteStack as deleteStackMock,
+  patchStack as patchStackMock,
+} from 'src/client'
 
-import {reviewTemplate} from 'src/templates/api/'
+import {
+  reviewTemplate,
+  installTemplate,
+  fetchStacks,
+  deleteStack,
+  updateStackName,
+} from 'src/templates/api/'
 
 describe('templates api calls', () => {
+  beforeEach(() => {
+    mocked(postTemplatesApplyMock).mockReset()
+  })
+
   it('reviews a template successfully', async () => {
     const orgID = '1234'
     const templateUrl = 'http://example.com'
@@ -13,7 +28,7 @@ describe('templates api calls', () => {
     mocked(postTemplatesApplyMock).mockImplementation(() => {
       return Promise.resolve({
         status: 200,
-        data: {}
+        data: {},
       })
     })
 
@@ -25,7 +40,7 @@ describe('templates api calls', () => {
     expect(mockArguments.data.remotes[0].url).toBe(templateUrl)
   })
 
-  it ('reviews a template unsuccessfully', async () => {
+  it('reviews a template unsuccessfully', async () => {
     const orgID = '1234'
     const templateUrl = 'http://example.com'
 
@@ -33,8 +48,8 @@ describe('templates api calls', () => {
       return Promise.resolve({
         status: 404,
         data: {
-          message: 'whoops'
-        }
+          message: 'whoops',
+        },
       })
     })
 
@@ -43,32 +58,91 @@ describe('templates api calls', () => {
     } catch (error) {
       expect(error.message).toBe('whoops')
     }
-
   })
 
-//   it('installTemplate', () => {
-//     mocked(postTemplatesApplyMock).mockImplementation(() => {
-//       return Promise.resolve({
-//         status: 200,
-//         data: {
-//           foo: 'hi!'
-//         }
-//       })
+  it('can install template succsefully', async () => {
+    const orgID = '1234'
+    const templateUrl = 'http://example.com'
+    mocked(postTemplatesApplyMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        data: {},
+      })
+    })
+    const summary = await installTemplate(orgID, templateUrl, [])
+    const [mockArguments] = mocked(postTemplatesApplyMock).mock.calls[0]
 
-        // // TODO: test for dry-run being false
-//     })
+    expect(mockArguments.data.dryRun).toBeFalsy()
+    expect(mockArguments.data.orgID).toBe(orgID)
+    expect(mockArguments.data.remotes[0].url).toBe(templateUrl)
+  })
 
-//   })
+  it('installs a template unsuccessfully', async () => {
+    const orgID = '1234'
+    const templateUrl = 'http://example.com'
 
-//   it('fetchStacks', () => {
+    mocked(postTemplatesApplyMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 404,
+        data: {
+          message: 'whoops',
+        },
+      })
+    })
 
-//   })
+    try {
+      await installTemplate(orgID, templateUrl, [])
+    } catch (error) {
+      expect(error.message).toBe('whoops')
+    }
+  })
 
-//   it('deleteStack', () => {
+  it('fetchStacks pass', async () => {
+    const orgID = '1234'
+    mocked(getStacksMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        data: {},
+      })
+    })
 
-//   })
+    const summary = await fetchStacks(orgID)
+    const [mockArguments] = mocked(getStacksMock).mock.calls[0]
 
-//   it('updateStackName', () => {
+    expect(mockArguments.query.orgID).toBe(orgID)
+  })
 
-//   })
+  it('deleteStack pass', async () => {
+    const orgID = '1234'
+    const stackID = '63728'
+    mocked(deleteStackMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        data: {},
+      })
+    })
+
+    const summary = await deleteStack(stackID, orgID)
+    const [mockArguments] = mocked(deleteStackMock).mock.calls[0]
+
+    expect(mockArguments.query.orgID).toBe(orgID)
+    expect(mockArguments.stack_id).toBe(stackID)
+  })
+
+  it('updateStackName', async () => {
+    const name = 'test rule'
+    const stackID = '63728'
+    mocked(patchStackMock).mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        data: {},
+      })
+    })
+
+    const summary = await updateStackName(stackID, name)
+    const [mockArguments] = mocked(patchStackMock).mock.calls[0]
+
+    expect(mockArguments.data.name).toBe(name)
+    expect(mockArguments.stack_id).toBe(stackID)
+  })
 })
