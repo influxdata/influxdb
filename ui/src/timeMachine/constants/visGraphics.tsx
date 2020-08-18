7d7be7d2f127f39df51b8b7bf87f2f021e2a5217
alexpaxton <thealexpaxton@gmail.com>
2020-08-18 09:46:07 -0700

    fix: fine tune appearance and line thickness

--- ui/src/timeMachine/constants/visGraphics.tsx
+++ ui/src/timeMachine/constants/visGraphics.tsx
@@ -1148,7 +1148,7 @@ const GRAPHIC_SVGS = {
         viewBox="0 0 150 150"
       >
         <path
-          className="vis-graphic--fill vis-graphic--fill-c"
+          className="vis-graphic--fill vis-graphic--fill-e"
           d="M10,127V52.6v-8c0-1.8,0.8-3.5,2.2-4.6l25-18.3c1.6-1.2,3.7-0.9,5,0.5l24.6,26.9c1.8,1.9,4.5,2.1,6.5,0.5
 	L104,23c1.8-1.5,7.1-1.5,8.3,0.6l16.5,27c0.7,1.2,2,1.9,3.3,1.9h7.9v5.5V127H10z"
         />
@@ -1161,7 +1161,7 @@ const GRAPHIC_SVGS = {
 	c-1.3,0-2.6-0.5-3.5-1.6L42.3,22.3C41.5,21.4,40.5,21,39.5,21L39.5,21z"
         />
         <path
-          className="vis-graphic--line vis-graphic--line-c"
+          className="vis-graphic--line vis-graphic--line-e"
           d="M10,91h0.2c1.2,0,2.4-0.4,3.3-1.2L35.6,71c2-1.7,5-1.6,6.9,0.3L66.6,96c2.1,2.1,5.6,2,7.5-0.3l29.2-34.4
 	c2.2-2.6,6.4-2.3,8.2,0.6l15,24.6c0.9,1.5,2.5,2.4,4.3,2.4h9.3"
         />
