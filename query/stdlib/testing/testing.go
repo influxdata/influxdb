390069cc4c80c986e1ee82a2abfdb679deb754e4
Christopher Wolff <chris.wolff@influxdata.com>
2020-09-15 08:24:37 -0700

    refactor: skip tests that are failing with associated issues

--- query/stdlib/testing/testing.go
+++ query/stdlib/testing/testing.go
@@ -92,6 +92,8 @@ var FluxEndToEndSkipList = map[string]map[string]string{
 
 		"holt_winters_panic": "Expected output is an empty table which breaks the testing framework (https://github.com/influxdata/influxdb/issues/14749)",
 		"map_nulls":          "to cannot write null values",
+
+		"range_stop": "pushed down range stop no longer exclusive https://github.com/influxdata/influxdb/issues/19564",
 	},
 	"experimental": {
 		"set":       "Reason TBD",
