214625e7c0dc1dc36d0debd5e88959b14eef6a36
greg linton <greg@influxdata.com>
2020-09-22 10:22:55 -0600

    chore: remove crufty comments

--- pkger/service_logging.go
+++ pkger/service_logging.go
@@ -148,7 +148,6 @@ func (s *loggingMW) Export(ctx context.Context, opts ...ExportOptFn) (template *
 			s.logger.Error("failed to export template", zap.Error(err), dur)
 			return
 		}
-		// todo: should these be Debug logs?
 		s.logger.Info("exported template", append(s.summaryLogFields(template.Summary()), dur)...)
 	}(time.Now())
 	return s.next.Export(ctx, opts...)
@@ -177,7 +176,6 @@ func (s *loggingMW) DryRun(ctx context.Context, orgID, userID influxdb.ID, opts 
 			fields = append(fields, zap.Stringer("stackID", opt.StackID))
 		}
 		fields = append(fields, dur)
-		// todo: should these be Debug logs?
 		s.logger.Info("template dry run successful", fields...)
 	}(time.Now())
 	return s.next.DryRun(ctx, orgID, userID, opts...)
@@ -203,7 +201,6 @@ func (s *loggingMW) Apply(ctx context.Context, orgID, userID influxdb.ID, opts .
 			fields = append(fields, zap.Stringer("stackID", opt.StackID))
 		}
 		fields = append(fields, dur)
-		// todo: should these be Debug logs?
 		s.logger.Info("template apply successful", fields...)
 	}(time.Now())
 	return s.next.Apply(ctx, orgID, userID, opts...)
