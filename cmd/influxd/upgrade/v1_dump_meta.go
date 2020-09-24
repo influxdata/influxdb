1a6865ad0ca1ca0e6548897edb734d41a3263d31
vlastahajek <vlastimil.hajek@bonitoo.io>
2020-09-24 13:37:15 +0200

    feat(upgrade): extending dump commands for more info

--- cmd/influxd/upgrade/v1_dump_meta.go
+++ cmd/influxd/upgrade/v1_dump_meta.go
@@ -30,9 +30,47 @@ var v1DumpMetaCommand = &cobra.Command{
 
 		fmt.Fprintln(os.Stdout, "Databases")
 		fmt.Fprintln(os.Stdout, "---------")
-		fmt.Fprintf(tw, "%s\t%s\n", "Name", "Default RP")
+		fmt.Fprintf(tw, "%s\t%s\t%s\n", "Name", "Default RP", "Shards")
 		for _, row := range meta.Databases() {
-			fmt.Fprintf(tw, "%s\t%s\n", row.Name, row.DefaultRetentionPolicy)
+			fmt.Fprintf(tw, "%s\t%s\t", row.Name, row.DefaultRetentionPolicy)
+			for i, si := range row.ShardInfos() {
+				if i > 0 {
+					fmt.Fprint(tw, ",")
+				}
+				fmt.Fprintf(tw, "%d", si.ID)
+			}
+			fmt.Fprintln(tw)
+		}
+		_ = tw.Flush()
+		fmt.Fprintln(os.Stdout)
+
+		fmt.Fprintln(os.Stdout, "Retention policies")
+		fmt.Fprintln(os.Stdout, "---------")
+		fmt.Fprintf(tw, "%s\t%s\t%s\t%s\n", "Database", "Name", "Duration", "Shard Group duration")
+		for _, db := range meta.Databases() {
+			for _, rp := range db.RetentionPolicies {
+				fmt.Fprintf(tw, "%s\t%s\t%s\t%s\n", db.Name, rp.Name, rp.Duration.String(), rp.ShardGroupDuration.String())
+			}
+		}
+		_ = tw.Flush()
+		fmt.Fprintln(os.Stdout)
+
+		fmt.Fprintln(os.Stdout, "Shard groups")
+		fmt.Fprintln(os.Stdout, "---------")
+		fmt.Fprintf(tw, "%s\t%s\t%s\t%s\n", "Database/RP", "Start Time", "End Time", "Shards")
+		for _, db := range meta.Databases() {
+			for _, rp := range db.RetentionPolicies {
+				for _, sg := range rp.ShardGroups {
+					fmt.Fprintf(tw, "%s/%s\t%s\t%s\t", db.Name, rp.Name, sg.StartTime.String(), sg.EndTime.String())
+					for i, si := range sg.Shards {
+						if i > 0 {
+							fmt.Fprint(tw, ",")
+						}
+						fmt.Fprintf(tw, "%d", si.ID)
+					}
+					fmt.Fprintln(tw)
+				}
+			}
 		}
 		_ = tw.Flush()
 		fmt.Fprintln(os.Stdout)
@@ -44,7 +82,7 @@ var v1DumpMetaCommand = &cobra.Command{
 			fmt.Fprintf(tw, "%s\t%s\n", row.Name, showBool(row.Admin))
 		}
 		_ = tw.Flush()
-
+		fmt.Fprintln(os.Stdout)
 		return nil
 	},
 }
