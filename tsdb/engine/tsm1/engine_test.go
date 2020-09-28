bde12093495ba166a7a67e2a2d47ddad28c874b0
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-09-28 12:04:30 -0500

    fix(tsdb): exclude the stop time from the array cursor (#19622)
    This is a backport of #14262 to the 1.x storage engine. The 1.x storage
    engine is now the primary engine for open source so when we switched we
    regressed to the old behavior.
    
    This also fixes `go generate` for the tsm1 package by running `tmpl`
    with `go run` instead of assuming the correct one is installed in the
    path.

--- tsdb/engine/tsm1/engine_test.go
+++ tsdb/engine/tsm1/engine_test.go
@@ -1949,7 +1949,7 @@ func TestEngine_CreateCursor_Ascending(t *testing.T) {
 				Field:     "value",
 				Ascending: true,
 				StartTime: 2,
-				EndTime:   11,
+				EndTime:   12,
 			})
 			if err != nil {
 				t.Fatal(err)
@@ -2008,7 +2008,7 @@ func TestEngine_CreateCursor_Descending(t *testing.T) {
 				Tags:      models.ParseTags([]byte("cpu,host=A")),
 				Field:     "value",
 				Ascending: false,
-				StartTime: 2,
+				StartTime: 1,
 				EndTime:   11,
 			})
 			if err != nil {
