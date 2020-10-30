ce8034073e3f76d288ac9864fc24f5d033e04207
Stuart Carnie <stuart.carnie@gmail.com>
2020-10-30 11:12:36 -0700

    fix: Fixed typos in doc comments

--- credentials.go
+++ credentials.go
@@ -18,10 +18,10 @@ const (
 	// SchemeV1Basic indicates the credentials came from an Authorization header using the BASIC scheme
 	SchemeV1Basic SchemeV1 = "basic"
 
-	// SchemeToken indicates the credentials came from an Authorization header using the Token scheme
+	// SchemeV1Token indicates the credentials came from an Authorization header using the Token scheme
 	SchemeV1Token SchemeV1 = "token"
 
-	// SchemeURL indicates the credentials came from the u and p query parameters
+	// SchemeV1URL indicates the credentials came from the u and p query parameters
 	SchemeV1URL SchemeV1 = "url"
 )
 
