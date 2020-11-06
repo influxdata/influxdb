ee390ddfd33a0326eae59641155a4628609bb85a
Daniel Moran <danxmoran@gmail.com>
2020-11-06 17:50:32 -0500

    fix: parsing of retention policy CLI args in `influx setup` and `influxd upgrade` (#19928)

--- cmd/influx/setup.go
+++ cmd/influx/setup.go
@@ -240,7 +240,7 @@ func nonInteractive() (*influxdb.OnboardingRequest, error) {
 		return nil, err
 	}
 	if dur > 0 {
-		req.RetentionPeriod = dur / time.Hour
+		req.RetentionPeriod = dur
 	}
 	return req, nil
 }
@@ -283,7 +283,7 @@ func interactive() (req *influxdb.OnboardingRequest, err error) {
 	}
 
 	if dur > 0 {
-		req.RetentionPeriod = dur / time.Hour
+		req.RetentionPeriod = dur
 	} else {
 		for {
 			rpStr := internal2.GetInput(ui, "Please type your retention period in hours.\r\nOr press ENTER for infinite.", strconv.Itoa(influxdb.InfiniteRetention))
