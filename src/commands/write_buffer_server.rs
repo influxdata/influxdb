33f3ca8b6d43b6be713f259387365aae8b36db34
Andrew Lamb <alamb@influxdata.com>
2020-11-11 06:41:54 -0500

    feat: Print message to stdout when the server is read (#432)

--- src/commands/write_buffer_server.rs
+++ src/commands/write_buffer_server.rs
@@ -84,6 +84,8 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let server = Server::bind(&bind_addr).serve(make_svc);
     info!("Listening on http://{}", bind_addr);
 
+    println!("InfluxDB IOx server ready");
+
     // Wait for both the servers to complete
     let (grpc_server, server) = futures::future::join(grpc_server, server).await;
 
--- src/main.rs
+++ src/main.rs
@@ -183,7 +183,7 @@ async fn dispatch_args(matches: ArgMatches<'_>) {
             }
         }
         ("server", Some(_)) | (_, _) => {
-            println!("Starting InfluxDB IOx server");
+            println!("InfluxDB IOx server starting");
             match commands::write_buffer_server::main().await {
                 Ok(()) => eprintln!("Shutdown OK"),
                 Err(e) => {
