bfb2ca001bda764e800f1924b48f5077f64c6563
Stuart Carnie <stuart.carnie@gmail.com>
2020-11-16 09:20:14 +1100

    chore: Lots of cleanup and response to PR feedback.

---
+++ tests/defaults.go
@@ -0,0 +1,17 @@
+package tests
+
+// Default values created when calling NewPipeline.
+const (
+	DefaultOrgName = "myorg"
+
+	DefaultBucketName = "db/rp" // Since we can only write data via 1.x path we need to have a 1.x bucket name
+
+	DefaultUsername = "admin"
+	DefaultPassword = "password"
+
+	// OperToken has permissions to do anything.
+	OperToken = "opertoken"
+)
+
+// VeryVerbose when set to true, will enable very verbose logging of services.
+var VeryVerbose bool
