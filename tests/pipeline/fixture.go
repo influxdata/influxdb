72cebeabc028cc73346241ba2c046caad7d091ae
Stuart Carnie <stuart.carnie@gmail.com>
2020-11-16 09:20:14 +1100

    chore: Lots of cleanup and response to PR feedback.

--- tests/pipeline/fixture.go
+++ tests/pipeline/fixture.go
@@ -1,7 +1,6 @@
 package pipeline
 
 import (
-	"context"
 	"fmt"
 	"testing"
 
@@ -72,87 +71,5 @@ func (f BaseFixture) GetClient(tag ClientTag) *tests.Client {
 		return f.NoAccess
 	default:
 		panic(fmt.Sprintf("unknown tag %s", tag))
-	}
-}
-
-// NewDefaultBaseFixture creates a BaseFixture with the default ids.
-func NewDefaultBaseFixture(t *testing.T, p *tests.Pipeline) BaseFixture {
-	return NewBaseFixture(t, p, p.DefaultOrgID, p.DefaultBucketID)
-}
-
-// ResourceFixture is a general purpose fixture.
-// It contains a resource, users for every role, and a label.
-// Users of the fixture can use it to create URMs from users to the resource or else.
-// The label is there to create label mappings.
-type ResourceFixture struct {
-	BaseFixture
-	ResourceType influxdb.ResourceType
-	ResourceID   influxdb.ID
-	LabelID      influxdb.ID
-}
-
-// NewResourceFixture creates a new ResourceFixture.
-func NewResourceFixture(t *testing.T, p *tests.Pipeline, typ influxdb.ResourceType) ResourceFixture {
-	bfx := NewDefaultBaseFixture(t, p)
-	l := &influxdb.Label{
-		OrgID:      bfx.Admin.OrgID,
-		Name:       "map_me",
-		Properties: nil,
-	}
-	if err := bfx.Admin.CreateLabel(context.Background(), l); err != nil {
-		t.Fatalf("error in creating label as admin: %v", err)
-	}
-	rid := bfx.Admin.MustCreateResource(t, typ)
-	return ResourceFixture{
-		BaseFixture:  bfx,
-		ResourceType: typ,
-		ResourceID:   rid,
-		LabelID:      l.ID,
-	}
-}
-
-// SubjectResourceFixture is a ResourceFixture associated with a subject.
-// The subject is a client that performs the actions.
-type SubjectResourceFixture struct {
-	ResourceFixture
-	SubjectTag ClientTag
-}
-
-// NewSubjectResourceFixture creates a new SubjectResourceFixture.
-func NewSubjectResourceFixture(t *testing.T, p *tests.Pipeline, subj ClientTag, rt influxdb.ResourceType) SubjectResourceFixture {
-	return SubjectResourceFixture{
-		ResourceFixture: NewResourceFixture(t, p, rt),
-		SubjectTag:      subj,
-	}
-}
-
-func (f SubjectResourceFixture) Subject() *tests.Client {
-	return f.GetClient(f.SubjectTag)
-}
-
-// ResourceSubjectIteration is the iteration on all the resources and subjects given.
-// One can specify what do to for an iteration step by passing a ResourceSubjectIterationStepFn
-// to `ResourceSubjectIteration.Do` or `ResourceSubjectIteration.ParDo`.
-type ResourceSubjectIteration struct {
-	subjects []ClientTag
-}
-
-// Creates a ResourceSubjectIteration for applying a ResourceSubjectIterationStepFn to each
-// resource-subject couple.
-func ForEachResourceSubject(subjects ...ClientTag) ResourceSubjectIteration {
-	return ResourceSubjectIteration{
-		subjects: subjects,
-	}
-}
-
-// ResourceSubjectIterationStepFn is a function applied to each step of iteration on resources
-// and subjects.
-type ResourceSubjectIterationStepFn func(subj ClientTag, rt influxdb.ResourceType)
-
-func (p ResourceSubjectIteration) Do(f ResourceSubjectIterationStepFn) {
-	for _, typ := range influxdb.OrgResourceTypes {
-		for _, subj := range p.subjects {
-			f(subj, typ)
-		}
 	}
 }
