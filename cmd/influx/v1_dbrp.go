b78a2e6fcdfa65d1c918c887cc55704ba839c826
Dan Moran <dmoran@influxdata.com>
2020-11-18 10:36:15 -0500

    chore: adjust help text based on PR feedback

--- cmd/influx/v1_dbrp.go
+++ cmd/influx/v1_dbrp.go
@@ -46,6 +46,7 @@ func v1DBRPFindCmd(f *globalFlags, opt genericCLIOpts) *cobra.Command {
 		Use:   "list",
 		Short: "List database and retention policy mappings",
 		RunE:  checkSetupRunEMiddleware(&flags)(v1DBRPFindF),
+		Args:  cobra.NoArgs,
 	}
 
 	f.registerFlags(opt.viper, cmd)
@@ -142,8 +143,8 @@ func v1DBRPCreateCmd(f *globalFlags, opt genericCLIOpts) *cobra.Command {
 	_ = cmd.MarkFlagRequired("bucket-id")
 	cmd.Flags().StringVar(&v1DBRPCreateFlags.DB, "db", "", "The name of the database")
 	_ = cmd.MarkFlagRequired("db")
-	cmd.Flags().BoolVar(&v1DBRPCreateFlags.Default, "default", false, "Identify this mapping as the default retention policy")
-	cmd.Flags().StringVar(&v1DBRPCreateFlags.RP, "rp", "", "Name of the retention policy")
+	cmd.Flags().BoolVar(&v1DBRPCreateFlags.Default, "default", false, "Identify this retention policy as the default for the database")
+	cmd.Flags().StringVar(&v1DBRPCreateFlags.RP, "rp", "", "The name of the retention policy")
 	_ = cmd.MarkFlagRequired("rp")
 	return cmd
 }
@@ -302,14 +303,16 @@ func v1DBRPUpdateCmd(f *globalFlags, opt genericCLIOpts) *cobra.Command {
 		Use:   "update",
 		Short: "Update a database and retention policy mapping",
 		RunE:  checkSetupRunEMiddleware(&flags)(v1DBRPUpdateF),
+		Args:  cobra.NoArgs,
 	}
 	f.registerFlags(opt.viper, cmd)
 	registerPrintOptions(opt.viper, cmd, &v1DBRPCRUDFlags.hideHeaders, &v1DBRPCRUDFlags.json)
 	v1DBRPUpdateFlags.Org.register(opt.viper, cmd, false)
 	cli.IDVar(cmd.Flags(), &v1DBRPUpdateFlags.ID, "id", 0, "The ID of the mapping to be updated")
 	_ = cmd.MarkFlagRequired("id")
+	// note for update we only care about update flags that the user set
 	cmd.Flags().StringVar(&v1DBRPUpdateFlags.RP, "rp", "", "The updated name of the retention policy")
-	cmd.Flags().Bool("default", false, "Set this as the default mapping for the associated database name") // note for update we only care about update flags that the user set
+	cmd.Flags().Bool("default", false, "Set this mapping's retention policy as the default for the mapping's database")
 	return cmd
 }
 
