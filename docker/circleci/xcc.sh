57b53c8f7661b2f2428e1f951b02e28c0d33c01d
Dan Moran <dmoran@influxdata.com>
2020-12-06 12:03:26 -0500

    build: try fix armv7 build

--- docker/circleci/xcc.sh
+++ docker/circleci/xcc.sh
@@ -12,7 +12,7 @@ GOARM=${GOARM:-$(go env GOARM)}
 case "${GOOS}_${GOARCH}_${GOARM}" in
   linux_amd64_)  CC=musl-gcc ;;
   linux_arm64_)  CC=aarch64-unknown-linux-musl-gcc ;;
-  linux_arm_6)   CC=arm-unknown-linux-musleabihf-gcc ;;
+  linux_arm_6)   CC=armv6-unknown-linux-musleabihf-gcc ;;
   linux_arm_7)   CC=armv7-unknown-linux-musleabihf-gcc ;;
   darwin_amd64_) CC=x86_64-apple-darwin15-clang ;;
   *) die "No cross-compiler set for ${GOOS}_${GOARCH}_${GOARM}" ;;
