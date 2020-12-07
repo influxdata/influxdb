926a6ceef7e3fa09452294e0c96bad9360a0dd0b
Sam Arnold <sarnold@influxdata.com>
2020-12-07 11:24:45 -0400

    chore: fix clustering build
    Clustering requires taking the hash of synthetic points, so
    allow this function to work on anything with a HashID.
    
    (cherry picked from commit d96c8fb125468c11680cec71ef1c65067b5fce5e)

--- services/meta/data.go
+++ services/meta/data.go
@@ -1363,12 +1363,15 @@ func (sgi ShardGroupInfo) clone() ShardGroupInfo {
 	return other
 }
 
-// ShardFor returns the ShardInfo for a Point.
-func (sgi *ShardGroupInfo) ShardFor(p models.Point) ShardInfo {
+type hashIDer interface {
+	HashID() uint64
+}
+
+// ShardFor returns the ShardInfo for a Point or other hashIDer.
+func (sgi *ShardGroupInfo) ShardFor(p hashIDer) ShardInfo {
 	if len(sgi.Shards) == 1 {
 		return sgi.Shards[0]
 	}
-
 	return sgi.Shards[p.HashID()%uint64(len(sgi.Shards))]
 }
 
