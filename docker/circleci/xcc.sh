ccbaf4e8835f5aa465be272385e2d196edbc2332
Daniel Moran <danxmoran@gmail.com>
2020-12-10 13:03:45 -0500

    build: add ARM64 support to release (#20260)
    * Create Dockerfile for running build & release in CI
    * Update CircleCI config to use new Docker image
    * Update goreleaser to add a linux ARM64 release

--- docker/circleci/xcc.sh
+++ docker/circleci/xcc.sh
@@ -9,10 +9,9 @@ GOOS=${GOOS:-$(go env GOOS)}
 GOARCH=${GOARCH:-$(go env GOARCH)}
 
 case "${GOOS}_${GOARCH}" in
-  linux_amd64) CC=musl-gcc ;;
-  linux_arm64) CC=aarch64-linux-gnu-gcc ;;
-  linux_arm)   CC=arm-linux-gnueabihf-gcc ;;
-  darwin_amd64) CC=/opt/osxcross/target/bin/o64-clang ;;
+  linux_amd64)  CC=musl-gcc ;;
+  linux_arm64)  CC=aarch64-unknown-linux-musl-gcc ;;
+  darwin_amd64) CC=x86_64-apple-darwin15-clang ;;
   *) die "No cross-compiler set for ${GOOS}_${GOARCH}" ;;
 esac
 
