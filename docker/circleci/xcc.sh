2e991bdca6da996d39eaa9031f137cf367a02218
Dan Moran <dmoran@influxdata.com>
2020-12-10 10:57:22 -0500

    build: remove ARM32 builds, and update Dockerfile

--- docker/circleci/xcc.sh
+++ docker/circleci/xcc.sh
@@ -7,15 +7,12 @@ function die() {
 
 GOOS=${GOOS:-$(go env GOOS)}
 GOARCH=${GOARCH:-$(go env GOARCH)}
-GOARM=${GOARM:-$(go env GOARM)}
 
-case "${GOOS}_${GOARCH}_${GOARM}" in
-  linux_amd64_)  CC=musl-gcc ;;
-  linux_arm64_)  CC=aarch64-unknown-linux-musl-gcc ;;
-  linux_arm_6)   CC=armv6-unknown-linux-musleabihf-gcc ;;
-  linux_arm_7)   CC=armv7-unknown-linux-musleabihf-gcc ;;
-  darwin_amd64_) CC=x86_64-apple-darwin15-clang ;;
-  *) die "No cross-compiler set for ${GOOS}_${GOARCH}_${GOARM}" ;;
+case "${GOOS}_${GOARCH}" in
+  linux_amd64)  CC=musl-gcc ;;
+  linux_arm64)  CC=aarch64-unknown-linux-musl-gcc ;;
+  darwin_amd64) CC=x86_64-apple-darwin15-clang ;;
+  *) die "No cross-compiler set for ${GOOS}_${GOARCH}" ;;
 esac
 
 exec ${CC} "$@"
