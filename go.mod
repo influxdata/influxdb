9528283a0ba1fc6c49530dfc4bfbaadc06ccfa80
greg linton <greg@influxdata.com>
2020-12-15 12:01:10 -0700

    wip: try getting go swagger parser to follow refs

--- go.mod
+++ go.mod
@@ -24,7 +24,7 @@ require (
 	github.com/elazarl/go-bindata-assetfs v1.0.0
 	github.com/fatih/color v1.9.0
 	github.com/fujiwara/shapeio v0.0.0-20170602072123-c073257dd745
-	github.com/getkin/kin-openapi v0.2.0
+	github.com/getkin/kin-openapi v0.26.0
 	github.com/ghodss/yaml v1.0.0
 	github.com/glycerine/go-unsnap-stream v0.0.0-20181221182339-f9677308dec2 // indirect
 	github.com/glycerine/goconvey v0.0.0-20180728074245-46e3a41ad493 // indirect
--- http/swagger_test.go
+++ http/swagger_test.go
@@ -2,19 +2,13 @@ package http
 
 import (
 	"context"
-	"io/ioutil"
 	"testing"
 
 	"github.com/getkin/kin-openapi/openapi3"
 )
 
 func TestValidSwagger(t *testing.T) {
-	data, err := ioutil.ReadFile("./swagger.yml")
-	if err != nil {
-		t.Fatalf("unable to read swagger specification: %v", err)
-	}
-
-	swagger, err := openapi3.NewSwaggerLoader().LoadSwaggerFromData(data)
+	swagger, err := openapi3.NewSwaggerLoader().LoadSwaggerFromFile("./swagger.yml")
 	if err != nil {
 		t.Fatalf("unable to load swagger specification: %v", err)
 	}
