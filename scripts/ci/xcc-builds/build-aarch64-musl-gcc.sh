69fda2fda585def524a45e1ebe53ac7d0ff9944b
Dan Moran <dmoran@influxdata.com>
2021-01-08 10:20:19 -0800

    build: refactor build to remove custom Docker and use machine for xcc

--- scripts/ci/xcc-builds/build-aarch64-musl-gcc.sh
+++ scripts/ci/xcc-builds/build-aarch64-musl-gcc.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 declare -r SCRIPT_DIR=$(cd $(dirname ${0}) >/dev/null 2>&1 && pwd)
 declare -r OUT_DIR=${SCRIPT_DIR}/out
 
-declare -r BUILD_IMAGE=circleci/golang:1.15-node-browsers
+declare -r BUILD_IMAGE=ubuntu:20.04
 declare -r MUSL_VERSION=1.1.24
 declare -r MUSL_CROSS_MAKE_VERSION=0.9.9
 
@@ -12,15 +12,22 @@ docker run --rm -i -v ${OUT_DIR}:/out -w /tmp ${BUILD_IMAGE} bash <<EOF
   set -euo pipefail
 
   declare -r BUILD_TIME=\$(date -u '+%Y%m%d%H%M%S')
+  export DEBIAN_FRONTEND=noninteractive
 
   # Install dependencies.
-  sudo apt-get update && sudo apt-get install -y --no-install-recommends patch
+  apt-get update && apt-get install -y --no-install-recommends \
+    build-essential \
+    ca-certificates \
+    curl \
+    git \
+    make \
+    patch
 
   # Clone and build musl-cross-make's ARM64 target.
   git clone https://github.com/richfelker/musl-cross-make.git && \
     cd musl-cross-make && \
     git checkout v${MUSL_CROSS_MAKE_VERSION} && \
-    make MUSL_VER=${MUSL_VERSION} TARGET=aarch64-unknown-linux-musl install && \
+    make MUSL_VER=${MUSL_VERSION} TARGET=aarch64-unknown-linux-musl DL_CMD="curl -C - -L -o" install && \
     mv output /tmp/musl-cross && \
     cd /tmp
 
