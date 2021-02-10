b0c7aa7ea18dab7fbe0ac81b7e04c7c7a052688e
Carol (Nichols || Goulding) <carol.nichols@integer32.com>
2021-02-10 16:17:54 -0500

    test: Fix doc test that wasn't getting compiled and was broken

--- influxdb_iox_client/src/client/flight.rs
+++ influxdb_iox_client/src/client/flight.rs
@@ -19,23 +19,25 @@ use crate::errors::{GrpcError, GrpcQueryError};
 
 /// An IOx Arrow Flight gRPC API client.
 ///
-/// ```rust
-/// #[tokio::test]
-/// # async fn test() {
+/// ```rust,no_run
+/// #[tokio::main]
+/// # async fn main() {
 /// use data_types::database_rules::DatabaseRules;
 /// use influxdb_iox_client::FlightClientBuilder;
 ///
-/// let client = FlightClientBuilder::default()
+/// let mut client = FlightClientBuilder::default()
 ///     .build("http://127.0.0.1:8082")
+///     .await
 ///     .expect("client should be valid");
 ///
 /// let mut query_results = client
-///     .perform_query(scenario.database_name(), sql_query)
-///     .await;
+///     .perform_query("my_database", "select * from cpu_load")
+///     .await
+///     .expect("query request should work");
 ///
 /// let mut batches = vec![];
 ///
-/// while let Some(data) = query_results.next().await {
+/// while let Some(data) = query_results.next().await.expect("valid batches") {
 ///     batches.push(data);
 /// }
 /// # }
