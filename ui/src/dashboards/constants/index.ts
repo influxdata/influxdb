b775e1c51f542d68ea5411512007614b80eb394a
Timmy Luong <TCL735@users.noreply.github.com>
2021-03-04 17:11:53 -0800

    fix(ui): respect 24 hour clock format and allow more choices (#20856)

--- ui/src/dashboards/constants/index.ts
+++ ui/src/dashboards/constants/index.ts
@@ -36,17 +36,26 @@ export const DEFAULT_TABLE_OPTIONS = {
 }
 
 export const FORMAT_OPTIONS: Array<{text: string}> = [
-  {text: DEFAULT_TIME_FORMAT},
+  {text: DEFAULT_TIME_FORMAT}, // 'YYYY-MM-DD HH:mm:ss ZZ'
+  {text: 'YYYY-MM-DD hh:mm:ss a ZZ'},
   {text: 'DD/MM/YYYY HH:mm:ss.sss'},
+  {text: 'DD/MM/YYYY hh:mm:ss.sss a'},
   {text: 'MM/DD/YYYY HH:mm:ss.sss'},
+  {text: 'MM/DD/YYYY hh:mm:ss.sss a'},
   {text: 'YYYY/MM/DD HH:mm:ss'},
-  {text: 'hh:mm a'},
+  {text: 'YYYY/MM/DD hh:mm:ss a'},
   {text: 'HH:mm'},
+  {text: 'hh:mm a'},
   {text: 'HH:mm:ss'},
+  {text: 'hh:mm:ss a'},
   {text: 'HH:mm:ss ZZ'},
+  {text: 'hh:mm:ss a ZZ'},
   {text: 'HH:mm:ss.sss'},
+  {text: 'hh:mm:ss.sss a'},
   {text: 'MMMM D, YYYY HH:mm:ss'},
+  {text: 'MMMM D, YYYY hh:mm:ss a'},
   {text: 'dddd, MMMM D, YYYY HH:mm:ss'},
+  {text: 'dddd, MMMM D, YYYY hh:mm:ss a'},
 ]
 
 export type NewDefaultCell = Pick<
