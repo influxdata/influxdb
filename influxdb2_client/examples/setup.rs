f3ce84d67c507565da593cf6c4685a7710e6f90a
Aakash Hemadri <aakashhemadri123@gmail.com>
2021-03-23 21:12:55 +0530

    fix: setup.rs example
    Signed-off-by: Aakash Hemadri <aakashhemadri123@gmail.com>

--- influxdb2_client/examples/setup.rs
+++ influxdb2_client/examples/setup.rs
@@ -5,32 +5,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = influxdb2_client::Client::new(influx_url, token);
 
-    println!("{:?}", client.setup().await?);
+    if client.is_onboarding_allowed().await? {
+        println!(
+            "{:?}",
+            client
+                .onboarding("some-user", "some-org", "some-bucket", None, None, None,)
+                .await?
+        );
+    }
+
     println!(
         "{:?}",
         client
-            .setup_init(
-                "some-user",
-                "some-org",
-                "some-bucket",
-                Some("some-password".to_string()),
-                Some(1),
-                None
-            )
-            .await?
-    );
-    println!(
-        "{:?}",
-        client
-            .setup_new(
+            .post_setup_user(
                 "some-new-user",
                 "some-new-org",
                 "some-new-bucket",
-                Some("some-new-password".to_string()),
-                Some(1),
+                None,
+                None,
                 None,
             )
             .await?
     );
+
     Ok(())
 }
