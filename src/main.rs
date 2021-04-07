df5349406afbf1b1f69d086c95a9fe2cb5dc895d
Marko Mikulicic <mkm@influxdata.com>
2021-04-07 17:36:34 +0200

    feat: Switch to jemalloc

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ use tokio::runtime::Runtime;
 use commands::logging::LoggingLevel;
 use ingest::parquet::writer::CompressionLevel;
 
+use tikv_jemallocator::Jemalloc;
+
 mod commands {
     pub mod convert;
     pub mod database;
@@ -33,6 +35,9 @@ mod commands {
 }
 
 pub mod influxdb_ioxd;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 enum ReturnCode {
     Failure = 1,
