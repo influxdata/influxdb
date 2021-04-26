5dfe312867af99c7014e17a922d1065d42026a15
Edd Robinson <me@edd.io>
2021-04-26 16:31:48 +0100

    test: add benchmark for influxrpc read_filter

--- server/benches/influxrpc.rs
+++ server/benches/influxrpc.rs
@@ -1,8 +1,11 @@
+mod read_filter;
 mod tag_values;
 
 use criterion::{criterion_group, criterion_main};
 
+use read_filter::benchmark_read_filter;
 use tag_values::benchmark_tag_values;
 
-criterion_group!(benches, benchmark_tag_values);
+// criterion_group!(benches, benchmark_tag_values, benchmark_read_filter);
+criterion_group!(benches, benchmark_tag_values, benchmark_read_filter);
 criterion_main!(benches);
