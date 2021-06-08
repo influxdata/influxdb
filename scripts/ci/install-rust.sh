b600dca439e61a87baecb04884051ddacb0780be
pierwill <pierwill@users.noreply.github.com>
2021-06-08 11:04:06 -0500

    fix: install Rust securely in CI

---
+++ scripts/ci/install-rust.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -ex
+
+# Install common packages
+apt-get update
+apt-get install --no-install-recommends -y clang musl-tools
+rm -rf /var/lib/apt/lists/*
+
+
+RUST_LATEST_VERSION=1.52.0
+# For security, we specify a particular rustup version and a SHA256 hash, computed
+# ourselves and hardcoded here. When updating `RUSTUP_LATEST_VERSION`:
+#   1. Download the new rustup script from https://github.com/rust-lang/rustup/releases.
+#   2. Audit the script and changes to it. You might want to grep for strange URLs...
+#   3. Update `OUR_RUSTUP_SHA` with the result of running `sha256sum rustup-init.sh`.
+RUSTUP_LATEST_VERSION=1.24.2
+OUR_RUSTUP_SHA="40229562d4fa60e102646644e473575bae22ff56c3a706898a47d7241c9c031e"
+
+
+# Download rustup script
+curl --proto '=https' --tlsv1.2 -sSf \
+  https://raw.githubusercontent.com/rust-lang/rustup/${RUSTUP_LATEST_VERSION}/rustup-init.sh -O && \
+
+# Verify checksum of rustup script. Exit with error if check fails.
+echo "${OUR_RUSTUP_SHA} rustup-init.sh" | sha256sum --check -- \
+    || { echo "Checksum problem!"; exit 1; } && \
+
+# Run rustup.
+sh rustup-init.sh --default-toolchain "$RUST_LATEST_VERSION" -y
+export PATH="${HOME}/.cargo/bin:${PATH}"
