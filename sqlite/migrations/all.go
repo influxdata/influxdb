6f37e1ae742802067d4d3028db574458051befc1
Will Baker <wbaker@gmail.com>
2021-06-10 09:55:10 -0400

    feat: use go embed for sqlite migrations

--- sqlite/migrations/all.go
+++ sqlite/migrations/all.go
@@ -1,13 +1,6 @@
-//go:generate env GO111MODULE=on go run github.com/kevinburke/go-bindata/go-bindata -prefix "migrations/" -o ./migrations_gen.go -ignore README|go|conf -pkg migrations .
-
 package migrations
 
-type All struct{}
+import "embed"
 
-func (s *All) ListNames() []string {
-	return AssetNames()
-}
-
-func (s *All) MustAssetString(n string) string {
-	return MustAssetString(n)
-}
+//go:embed *.sql
+var All embed.FS
