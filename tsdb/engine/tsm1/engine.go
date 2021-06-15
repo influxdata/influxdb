1661b6f67d72d0be7736053a717a87f62c42ea9c
Sam Arnold <sarnold@influxdata.com>
2021-06-15 12:36:52 -0400

    fix: avoid rewriting fields.idx unnecessarily (#21692)
    * fix: avoid rewriting fields.idx unnecessarily (#21592) [Port to 1.9] (#21609)
    
    Under heavy write load creating new fields and measurements
    the rewrite of the fields.idx file is a bottleneck. This
    enhancement combines multiple writes into a single one and
    shares any error return value with all of the combined
    invocations. MeasurementFieldSet and the new
    MeasurementFieldSetWriter must both now be explicitly
    closed.
    
    Closes #21577
    
    (cherry picked from commit f64be286be178d481c9897b8a388d227ebb62b52)
    
    Closes #21597
    
    * chore: minor refactor suggested by go lint (#21614) (#21616)
    
    (cherry picked from commit 7d10228e19a6032ddd16b560679f00a56d591d05)
    (cherry picked from commit f8202876ad990a503f7a5f355fb5060d3e9552c6)
    
    Co-authored-by: davidby-influx <72418212+davidby-influx@users.noreply.github.com>

--- tsdb/engine/tsm1/engine.go
+++ tsdb/engine/tsm1/engine.go
@@ -778,6 +778,7 @@ func (e *Engine) Close() error {
 	e.mu.Lock()
 	defer e.mu.Unlock()
 	e.done = nil // Ensures that the channel will not be closed again.
+	e.fieldset.Close()
 
 	if err := e.FileStore.Close(); err != nil {
 		return err
