aca69e530f16441e343265d717001c68cd1db115
davidby-influx <72418212+davidby-influx@users.noreply.github.com>
2021-06-15 10:23:38 -0700

    fix: don't access a field in a nil struct (#21693)

--- tsdb/shard.go
+++ tsdb/shard.go
@@ -1762,8 +1762,8 @@ func (fs *MeasurementFieldSet) SetMeasurementFieldSetWriter(queueLength int) {
 func (w *MeasurementFieldSetWriter) Close() {
 	if w != nil {
 		close(w.writeRequests)
+		w.wg.Wait()
 	}
-	w.wg.Wait()
 }
 
 func (fs *MeasurementFieldSet) Save() error {
