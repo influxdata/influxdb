9db4f985d03ce2787404cd3d23e2b0d2f4bbcdf7
Will Baker <wbaker@gmail.com>
2021-06-23 10:20:46 -0400

    build: verify checksum of built ui assets

--- scripts/fetch-ui-assets.sh
+++ scripts/fetch-ui-assets.sh
@@ -12,6 +12,21 @@
 # respective releases in "influxdata/ui" (OSS-2.0, OSS-2.1, etc). Those releases
 # are updated only when a bug fix needs included for the UI of that OSS release.
 
-curl -L https://github.com/influxdata/ui/releases/download/OSS-Master/build.tar.gz --output ui/build.tar.gz
-tar -xzf ui/build.tar.gz -C ui
-rm ui/build.tar.gz
+set -e
+
+# Download the SHA256 checksum attached to the release. To verify the integrity
+# of the download, this checksum will be used to check the download tar file
+# containing the built UI assets.
+curl -Ls https://github.com/influxdata/ui/releases/download/OSS-Master/sha256.txt --output sha256.txt
+
+# Download the tar file containing the built UI assets.
+curl -L https://github.com/influxdata/ui/releases/download/OSS-Master/build.tar.gz --output build.tar.gz
+
+# Verify the checksums match; exit if they don't.
+echo "$(cat sha256.txt)" | sha256sum --check -- \
+    || { echo "Checksums did not match for downloaded UI assets!"; exit 1; }
+
+# Extract the assets and clean up.
+tar -xzf build.tar.gz -C ui
+rm sha256.txt
+rm build.tar.gz
