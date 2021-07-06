8f172509b000f77a7774c1ff5ae085de6caff0ad
Nga Tran <ntran@influxdata.com>
2021-07-06 17:03:44 -0400

    fix: remove redudant quote

--- data_types/src/chunk_metadata.rs
+++ data_types/src/chunk_metadata.rs
@@ -24,7 +24,7 @@ impl std::fmt::Display for ChunkAddr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Chunk('{}':'{}':'{}':{}')",
+            "Chunk('{}':'{}':'{}':{})",
             self.db_name, self.table_name, self.partition_key, self.chunk_id
         )
     }
