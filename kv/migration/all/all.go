e26c32d6e321937bac3e7876a33409567ba7d200
Will Baker <wbaker@gmail.com>
2021-07-13 11:58:10 -0500

    chore: better description comment for migration

--- kv/migration/all/all.go
+++ kv/migration/all/all.go
@@ -37,7 +37,7 @@ var Migrations = [...]migration.Spec{
 	Migration0014_ReindexDBRPs,
 	// record shard group durations in bucket metadata
 	Migration0015_RecordShardGroupDurationsInBucketMetadata,
-	// add annotations notebooks to oper token
+	// add annotations and notebooks resource types to the operator token
 	Migration0016_AddAnnotationsNotebooksToOperToken,
 	// {{ do_not_edit . }}
 }
