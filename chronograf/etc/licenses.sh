50174db1d2cdef9616ef985ed5fbbc2eb5188472
Dan Moran <dmoran@influxdata.com>
2021-07-20 11:25:01 -0400

    chore: delete as many files from chronograf/ as we can to still build

--- chronograf/etc/licenses.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-for a in `gdl -no-vendored -test -repo ./... | awk 'NR>1 {print $5}'`; do echo \[\]\($a/blob/master/\) ; done
-nlf -c |awk -F, '{printf "%s %s \[%s\]\(%s\)\n", $1, $2, $5, $4}'
