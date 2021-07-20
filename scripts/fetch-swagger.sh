858be901394422398ba174f30aaee78c9c920418
Daniel Moran <danxmoran@gmail.com>
2021-07-20 15:11:46 -0400

    fix: regenerate protos using gogo 1.3.2 (#21880)
    * Regenerate protos using gogo 1.3.2
    * Add protos to generate, add checkgenerate to CI
    * Address proto warning
    * Add generator tooling to Makefile
    * Delete recursive Makefiles, simplify generation run by goreleaser
    * Use env bash for fetch-ui-assets
    * Add static-data to clean target

--- scripts/fetch-swagger.sh
+++ scripts/fetch-swagger.sh
@@ -5,11 +5,16 @@
 
 set -e
 
+declare -r SCRIPT_DIR=$(cd $(dirname ${0}) >/dev/null 2>&1 && pwd)
+declare -r ROOT_DIR=$(dirname ${SCRIPT_DIR})
+declare -r STATIC_DIR="$ROOT_DIR/static"
+
 # Pins the swagger that will be downloaded to a specific commit
 declare -r OPENAPI_SHA=c87a08f832e79bc338a37509028641cda9e1875b
 
 # Don't do a shallow clone since the commit we want might be several commits
 # back; but do only clone the main branch.
 git clone https://github.com/influxdata/openapi.git --single-branch
-cd openapi && git checkout ${OPENAPI_SHA} --quiet && cp contracts/oss.json ../data/swagger.json
+mkdir -p "$STATIC_DIR/data"
+cd openapi && git checkout ${OPENAPI_SHA} --quiet && cp contracts/oss.json "$STATIC_DIR/data/swagger.json"
 cd ../ && rm -rf openapi
