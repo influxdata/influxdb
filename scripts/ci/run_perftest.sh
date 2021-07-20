7ea348db35d144706302b2efe7ce8930a6dbf129
Cody Shepherd <cody.shepherd@gmail.com>
2021-07-20 09:05:06 -0700

    chore: enable query performance tests (#21892)

--- scripts/ci/run_perftest.sh
+++ scripts/ci/run_perftest.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/sh -ex
 
+echo "Running as user: $(whoami)"
+
 # Install Telegraf
 wget -qO- https://repos.influxdata.com/influxdb.key | apt-key add -
 echo "deb https://repos.influxdata.com/ubuntu focal stable" | tee /etc/apt/sources.list.d/influxdb.list
 
 DEBIAN_FRONTEND=noninteractive apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -y git jq telegraf awscli
+
+# we need libc6 version 2.32 (released in ubuntu for 20.10 and later) for influx_tools
+cp /etc/apt/sources.list /etc/apt/sources.list.d/groovy.list
+sed -i 's/focal/groovy/g' /etc/apt/sources.list.d/groovy.list
+DEBIAN_FRONTEND=noninteractive apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y libc6 -t groovy
 
 root_branch="$(echo "${INFLUXDB_VERSION}" | rev | cut -d '-' -f1 | rev)"
 trap "aws s3 cp /home/ubuntu/perftest_log.txt s3://perftest-logs-influxdb/oss/$root_branch/${TEST_COMMIT}-$(date +%Y%m%d%H%M%S).log" EXIT KILL
@@ -28,12 +36,14 @@ cat << EOF > /etc/telegraf/telegraf.conf
     "branch",
     "commit",
     "i_type",
+    "query_format",
     "time"
   ]
   json_time_key = "time"
   json_time_format = "unix"
   tag_keys = [
     "i_type"
+    "query_format"
   ]
 EOF
 systemctl restart telegraf
@@ -68,27 +78,68 @@ go get \
   github.com/influxdata/influxdb-comparisons/cmd/bulk_query_gen \
   github.com/influxdata/influxdb-comparisons/cmd/query_benchmarker_influxdb
 
+# hack to get the daemon to start up again until https://github.com/influxdata/influxdb/issues/21757 is resolved
+systemctl stop influxdb
+sed -i 's/User=influxdb/User=root/g' /lib/systemd/system/influxdb.service
+sed -i 's/Group=influxdb/Group=root/g' /lib/systemd/system/influxdb.service
+systemctl daemon-reload
+systemctl unmask influxdb.service
+systemctl start influxdb
+
 # Run and record tests
 datestring=$(date +%s)
 seed=$datestring
+db_name="benchmark_db"
 for scale in 50 100 500; do
   # generate bulk data
   scale_string="scalevar-$scale"
   scale_seed_string="$scale_string-seed-$seed"
   data_fname="influx-bulk-records-usecase-devops-$scale_seed_string.txt"
   $GOPATH/bin/bulk_data_gen --seed=$seed --use-case=devops --scale-var=$scale --format=influx-bulk > ${DATASET_DIR}/$data_fname
+  query_files=""
+  for format in http flux-http; do
+    query_fname="query-devops-$scale_seed_string-$format.txt"
+    $GOPATH/bin/bulk_query_gen --seed=$seed --use-case=devops --scale-var=$scale --format=influx-$format --db $db_name --queries 10000 --query-type 8-host-1-hr > ${DATASET_DIR}/$query_fname
+    query_files="$query_files $query_fname"
+  done
 
-  # run ingest tests
-  test_type=ingest
+  # Test loop
   for parseme in "5000:2" "5000:20" "15000:2" "15000:20"; do
     batch=$(echo $parseme | cut -d: -f1)
     workers=$(echo $parseme | cut -d: -f2)
+
+    # generate load data
     load_opts="-batch-size=$batch -workers=$workers -urls=http://${NGINX_HOST}:8086 -do-abort-on-exist=false -do-db-create=true -backoff=1s -backoff-timeout=300m0s"
     if [ -z $INFLUXDB2 ] || [ $INFLUXDB2 = true ]; then
       load_opts="$load_opts -organization=$TEST_ORG -token=$TEST_TOKEN"
     fi
 
-    cat ${DATASET_DIR}/$data_fname | $GOPATH/bin/bulk_load_influx $load_opts | jq ". += {branch: \"${INFLUXDB_VERSION}\", commit: \"${TEST_COMMIT}\", time: \"$datestring\", i_type: \"${DATA_I_TYPE}\"}" > $working_dir/test-$test_type-$scale_string-batchsize-$batch-workers-$workers.json
+    # run ingest tests
+    cat ${DATASET_DIR}/$data_fname | $GOPATH/bin/bulk_load_influx $load_opts | jq ". += {branch: \"${INFLUXDB_VERSION}\", commit: \"${TEST_COMMIT}\", time: \"$datestring\", i_type: \"${DATA_I_TYPE}\"}" > $working_dir/test-ingest-$scale_string-batchsize-$batch-workers-$workers.json
+
+    bucket_id=$(influx bucket list --host http://${NGINX_HOST}:8086 -n ${db_name} -o $TEST_ORG --token $TEST_TOKEN | head -2 | tail -1 | cut -d '	' -f1)
+    sleep 1
+    # stop daemon and force compaction
+    systemctl stop influxdb
+    set +e
+    shards=$(find /var/lib/influxdb/engine/data/$bucket_id/autogen/ -maxdepth 1 -mindepth 1)
+    set -e
+    for shard in $shards; do
+      /home/ubuntu/influx_tools compact-shard -force -verbose -path $shard
+    done
+
+    # restart daemon
+    systemctl unmask influxdb.service
+    systemctl start influxdb
+
+    # run influxql and flux query tests
+    for query_file in $query_files; do
+      format=$(echo $query_file | cut -d '-' -f7)
+      cat ${DATASET_DIR}/$query_file | ${GOPATH}/bin/query_benchmarker_influxdb --urls=http://localhost:8086 --debug=0 --print-interval=0 --workers=$workers --json=true | jq ". += {branch: \"${INFLUXDB_VERSION}\", commit: \"${TEST_COMMIT}\", time: \"$datestring\", i_type: \"${DATA_I_TYPE}\", query_format: \"$format\"}" > $working_dir/test-query-$scale_string-format-$format-workers-$workers.json
+    done
+
+    # delete db to start anew
+    influx bucket delete -o $TEST_ORG -t $TEST_TOKEN -n $db_name
   done
 done
 
