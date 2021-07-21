ffe6e62aeef5540ccf4cbca15a04cd92008b7635
Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>
2021-07-21 12:43:27 +0100

    feat: add instant to datetime conversion (#2078)
    * feat: add instant to datetime conversion
    
    * chore: review feedback

---
+++ data_types/src/instant.rs
@@ -0,0 +1,53 @@
+use chrono::{DateTime, Utc};
+use once_cell::sync::OnceCell;
+use std::time::Instant;
+
+/// Stores an Instant and DateTime<Utc> captured as close as possible together
+static INSTANCE: OnceCell<(DateTime<Utc>, Instant)> = OnceCell::new();
+
+/// Provides a conversion from Instant to DateTime<Utc> for display purposes
+///
+/// It is an approximation as if the system clock changes, the returned DateTime will not be
+/// the same as the DateTime that would have been recorded at the time the Instant was created.
+///
+/// The conversion does, however, preserve the monotonic property of Instant, i.e. a larger
+/// Instant will have a larger returned DateTime.
+///
+/// This should ONLY be used for display purposes, the results should not be used to
+/// drive logic, nor persisted
+pub fn to_approximate_datetime(instant: Instant) -> DateTime<Utc> {
+    let (ref_date, ref_instant) = *INSTANCE.get_or_init(|| (Utc::now(), Instant::now()));
+
+    if ref_instant > instant {
+        ref_date
+            - chrono::Duration::from_std(ref_instant.duration_since(instant))
+                .expect("date overflow")
+    } else {
+        ref_date
+            + chrono::Duration::from_std(instant.duration_since(ref_instant))
+                .expect("date overflow")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_datetime() {
+        // Seed global state
+        to_approximate_datetime(Instant::now());
+
+        let (ref_date, ref_instant) = *INSTANCE.get().unwrap();
+
+        assert_eq!(
+            to_approximate_datetime(ref_instant + std::time::Duration::from_nanos(78)),
+            ref_date + chrono::Duration::nanoseconds(78)
+        );
+
+        assert_eq!(
+            to_approximate_datetime(ref_instant - std::time::Duration::from_nanos(23)),
+            ref_date - chrono::Duration::nanoseconds(23)
+        );
+    }
+}
