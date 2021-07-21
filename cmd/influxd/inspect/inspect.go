3b86c324687ad5e53380e247e639fd158f6d8d7b
mcfarlm3 <58636946+mcfarlm3@users.noreply.github.com>
2021-07-21 09:15:37 -0700

    feat: port `influxd inspect dump-wal` (#21888)
    * feat: porting influxd inspect dump-wal from 1.x
    
    * refactor: adapted 1.x logic to cobra command syntax
    
    * refactor: improved clarity of printed output and help documentation
    
    * chore: ran make fmt
    
    * chore: updated CHANGELOG.md
    
    * refactor: made small changes suggested in code review

--- cmd/influxd/inspect/inspect.go
+++ cmd/influxd/inspect/inspect.go
@@ -4,6 +4,7 @@ import (
 	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/delete_tsm"
 	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/dump_tsi"
 	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/dump_tsm"
+	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/dump_wal"
 	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/export_index"
 	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/export_lp"
 	"github.com/influxdata/influxdb/v2/cmd/influxd/inspect/report_tsi"
@@ -40,6 +41,7 @@ func NewCommand(v *viper.Viper) (*cobra.Command, error) {
 	base.AddCommand(dump_tsm.NewDumpTSMCommand())
 	base.AddCommand(dump_tsi.NewDumpTSICommand())
 	base.AddCommand(delete_tsm.NewDeleteTSMCommand())
+	base.AddCommand(dump_wal.NewDumpWALCommand())
 	base.AddCommand(verify_wal.NewVerifyWALCommand())
 	base.AddCommand(report_tsm.NewReportTSMCommand())
 
