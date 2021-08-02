54af62b8b6eec243499c7e0000b5c6393cc900d3
Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>
2021-08-02 17:10:36 +0100

    fix: server shutdown (#2165)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/influxdb_ioxd.rs
+++ src/influxdb_ioxd.rs
@@ -285,7 +285,10 @@ pub async fn main(config: Config) -> Result<()> {
 
     info!("frontend shutdown completed");
     internal_shutdown.cancel();
-    server_worker.await;
+
+    if !server_worker.is_terminated() {
+        server_worker.await;
+    }
 
     info!("server completed shutting down");
 
