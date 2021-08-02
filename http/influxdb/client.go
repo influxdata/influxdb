3f7b996a10760151bf3604d1a753e16d5ab2124c
William Baker <wbaker@gmail.com>
2021-08-02 09:02:54 -0600

    chore: delete the rest of chronograf (#21998)
    * chore: deleted the rest of chronograf
    
    * chore: tidy

--- http/influxdb/client.go
+++ http/influxdb/client.go
@@ -5,9 +5,6 @@ import (
 	"net/http"
 	"net/url"
 
-	platform "github.com/influxdata/influxdb/v2"
-	"github.com/influxdata/influxdb/v2/chronograf"
-	"github.com/influxdata/influxdb/v2/chronograf/influx"
 	"github.com/influxdata/influxdb/v2/kit/tracing"
 )
 
@@ -18,37 +15,6 @@ var (
 	}
 	defaultTransport = &http.Transport{}
 )
-
-func newClient(s *platform.Source) (*influx.Client, error) {
-	c := &influx.Client{}
-	url, err := url.Parse(s.URL)
-	if err != nil {
-		return nil, err
-	}
-	c.URL = url
-	c.Authorizer = DefaultAuthorization(s)
-	c.InsecureSkipVerify = s.InsecureSkipVerify
-	c.Logger = &chronograf.NoopLogger{}
-	return c, nil
-}
-
-// DefaultAuthorization creates either a shared JWT builder, basic auth or Noop
-// This is copy of the method from chronograf/influx adapted for platform sources.
-func DefaultAuthorization(src *platform.Source) influx.Authorizer {
-	// Optionally, add the shared secret JWT token creation
-	if src.Username != "" && src.SharedSecret != "" {
-		return &influx.BearerJWT{
-			Username:     src.Username,
-			SharedSecret: src.SharedSecret,
-		}
-	} else if src.Username != "" && src.Password != "" {
-		return &influx.BasicAuth{
-			Username: src.Username,
-			Password: src.Password,
-		}
-	}
-	return &influx.NoAuthorization{}
-}
 
 func newURL(addr, path string) (*url.URL, error) {
 	u, err := url.Parse(addr)
