b0b7af7f3fc2f3f576a5d1e33895b76c30ec9462
Cody Shepherd <cody.shepherd@gmail.com>
2021-08-03 11:52:14 -0700

    fix: handle https in systemd wrapper, and prevent it from looping forever (#22026)
    * fix: handle https in systemd wrapper, and prevent it from looping forever
    
    * chore: update pkg install test to check https
    
    * chore: update changelog

--- scripts/influxd-systemd-start.sh
+++ scripts/influxd-systemd-start.sh
@@ -1,17 +1,38 @@
 #!/bin/bash -e
 
 /usr/bin/influxd -config /etc/influxdb/influxdb.conf $INFLUXD_OPTS &
-echo $! > /var/lib/influxdb/influxd.pid
+PID=$!
+echo $PID > /var/lib/influxdb/influxd.pid
 
+PROTOCOL="http"
 BIND_ADDRESS=$(influxd config | grep -A5 "\[http\]" | grep '^  bind-address' | cut -d ' ' -f5 | tr -d '"')
+HTTPS_ENABLED_FOUND=$(influxd config | grep "https-enabled = true" | cut -d ' ' -f5)
+HTTPS_ENABLED=${HTTPS_ENABLED_FOUND:-"false"}
+if [ $HTTPS_ENABLED = "true" ]; then
+  HTTPS_CERT=$(influxd config | grep "https-certificate" | cut -d ' ' -f5 | tr -d '"')
+  if [ ! -f "${HTTPS_CERT}" ]; then
+    echo "${HTTPS_CERT} not found! Exiting..."
+    exit 1
+  fi
+  echo "$HTTPS_CERT found"
+  PROTOCOL="https"
+fi
 HOST=${BIND_ADDRESS%%:*}
 HOST=${HOST:-"localhost"}
 PORT=${BIND_ADDRESS##*:}
 
 set +e
-result=$(curl -s -o /dev/null http://$HOST:$PORT/health -w %{http_code})
+max_attempts=10
+url="$PROTOCOL://$HOST:$PORT/health"
+result=$(curl -k -s -o /dev/null $url -w %{http_code})
 while [ "$result" != "200" ]; do
   sleep 1
-  result=$(curl -s -o /dev/null http://$HOST:$PORT/health -w %{http_code})
+  result=$(curl -k -s -o /dev/null $url -w %{http_code})
+  max_attempts=$(($max_attempts-1))
+  if [ $max_attempts -le 0 ]; then
+    echo "Failed to reach influxdb $PROTOCOL endpoint at $url"
+    exit 1
+  fi
 done
 set -e
+
