918ffedc6865a4d9aff23c9f91326d6bda97e51c
Cody Shepherd <cody.shepherd@gmail.com>
2021-08-12 10:42:49 -0700

    chore: fix curl syntax for auth setup in pkg_run_test

--- scripts/ci/tests/03-auth-enabled.sh
+++ scripts/ci/tests/03-auth-enabled.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -eux
 
 service influxdb start
-result=$(curl -s -o /dev/null -H "Content-Type: application/json" -XPOST -d '{\"username\": \"default\", \"password\": \"thisisnotused\", \"retentionPeriodSeconds\": 0, \"org\": \"testorg\", \"bucket\": \"unusedbucket\", \"token\": \"thisisatesttoken\"}' http://localhost:8086/api/v2/setup -w %{http_code})
+result=$(curl -s -o /dev/null -H "Content-Type: application/json" -XPOST -d '{"username": "default", "password": "thisisnotused", "retentionPeriodSeconds": 0, "org": "testorg", "bucket": "unusedbucket", "token": "thisisatesttoken"}' http://localhost:8086/api/v2/setup -w %{http_code})
+if [ "$result" != "201" ]; then
+  exit 1
+fi
 service influxdb stop
 service influxdb start
 service influxdb stop
