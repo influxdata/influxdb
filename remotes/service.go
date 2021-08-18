4af5c7bf61a183e8a3126db76dbb6b91acaa38b2
Daniel Moran <danxmoran@gmail.com>
2021-08-18 15:25:40 -0400

    feat: implement metadata management for remote connections (#22254)

--- remotes/service.go
+++ remotes/service.go
@@ -2,55 +2,248 @@ package remotes
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 
+	sq "github.com/Masterminds/squirrel"
 	"github.com/influxdata/influxdb/v2"
 	"github.com/influxdata/influxdb/v2/kit/platform"
-	"github.com/influxdata/influxdb/v2/kit/platform/errors"
+	ierrors "github.com/influxdata/influxdb/v2/kit/platform/errors"
+	"github.com/influxdata/influxdb/v2/snowflake"
+	"github.com/influxdata/influxdb/v2/sqlite"
+	"go.uber.org/zap"
 )
 
 var (
-	errNotImplemented = &errors.Error{
-		Code: errors.ENotImplemented,
+	errNotImplemented = &ierrors.Error{
+		Code: ierrors.ENotImplemented,
 		Msg:  "remote connection APIs not yet implemented",
+	}
+	errRemoteNotFound = &ierrors.Error{
+		Code: ierrors.ENotFound,
+		Msg:  "remote connection not found",
 	}
 )
 
-func NewService() *service {
-	return &service{}
+type RemoteConnectionValidator interface {
+	ValidateRemoteConnectionHTTPConfig(context.Context, *influxdb.RemoteConnectionHTTPConfig) error
 }
 
-type service struct{}
+func NewService(logger *zap.Logger, store *sqlite.SqlStore) *service {
+	return &service{
+		store:       store,
+		log:         logger,
+		idGenerator: snowflake.NewIDGenerator(),
+		validator:   &stubValidator{},
+	}
+}
+
+type service struct {
+	store       *sqlite.SqlStore
+	log         *zap.Logger
+	idGenerator platform.IDGenerator
+	validator   RemoteConnectionValidator
+}
 
 var _ influxdb.RemoteConnectionService = (*service)(nil)
 
 func (s service) ListRemoteConnections(ctx context.Context, filter influxdb.RemoteConnectionListFilter) (*influxdb.RemoteConnections, error) {
-	return nil, errNotImplemented
+	q := sq.Select("id", "org_id", "name", "description", "remote_url", "remote_org_id", "allow_insecure_tls").
+		From("remotes").
+		Where(sq.Eq{"org_id": filter.OrgID})
+
+	if filter.Name != nil {
+		q = q.Where(sq.Eq{"name": *filter.Name})
+	}
+	if filter.RemoteURL != nil {
+		q = q.Where(sq.Eq{"remote_url": *filter.RemoteURL})
+	}
+
+	query, args, err := q.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var rcs influxdb.RemoteConnections
+	if err := s.store.DB.SelectContext(ctx, &rcs.Remotes, query, args...); err != nil {
+		return nil, err
+	}
+
+	return &rcs, nil
 }
 
 func (s service) CreateRemoteConnection(ctx context.Context, request influxdb.CreateRemoteConnectionRequest) (*influxdb.RemoteConnection, error) {
-	return nil, errNotImplemented
+	s.store.Mu.Lock()
+	defer s.store.Mu.Unlock()
+
+	q := sq.Insert("remotes").
+		SetMap(sq.Eq{
+			"id":                 s.idGenerator.ID(),
+			"org_id":             request.OrgID,
+			"name":               request.Name,
+			"description":        request.Description,
+			"remote_url":         request.RemoteURL,
+			"remote_api_token":   request.RemoteToken,
+			"remote_org_id":      request.RemoteOrgID,
+			"allow_insecure_tls": request.AllowInsecureTLS,
+			"created_at":         "datetime('now')",
+			"updated_at":         "datetime('now')",
+		}).
+		Suffix("RETURNING id, org_id, name, description, remote_url, remote_org_id, allow_insecure_tls")
+
+	query, args, err := q.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var rc influxdb.RemoteConnection
+	if err := s.store.DB.GetContext(ctx, &rc, query, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errRemoteNotFound
+		}
+		return nil, err
+	}
+	return &rc, nil
 }
 
 func (s service) ValidateNewRemoteConnection(ctx context.Context, request influxdb.CreateRemoteConnectionRequest) error {
-	return errNotImplemented
+	config := influxdb.RemoteConnectionHTTPConfig{
+		RemoteURL:        request.RemoteURL,
+		RemoteToken:      request.RemoteToken,
+		RemoteOrgID:      request.RemoteOrgID,
+		AllowInsecureTLS: request.AllowInsecureTLS,
+	}
+
+	return s.validator.ValidateRemoteConnectionHTTPConfig(ctx, &config)
 }
 
 func (s service) GetRemoteConnection(ctx context.Context, id platform.ID) (*influxdb.RemoteConnection, error) {
-	return nil, errNotImplemented
+	q := sq.Select("id", "org_id", "name", "description", "remote_url", "remote_org_id", "allow_insecure_tls").
+		From("remotes").
+		Where(sq.Eq{"id": id})
+
+	query, args, err := q.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var rc influxdb.RemoteConnection
+	if err := s.store.DB.GetContext(ctx, &rc, query, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errRemoteNotFound
+		}
+		return nil, err
+	}
+	return &rc, nil
 }
 
 func (s service) UpdateRemoteConnection(ctx context.Context, id platform.ID, request influxdb.UpdateRemoteConnectionRequest) (*influxdb.RemoteConnection, error) {
-	return nil, errNotImplemented
+	s.store.Mu.Lock()
+	defer s.store.Mu.Unlock()
+
+	updates := sq.Eq{"updated_at": sq.Expr("datetime('now')")}
+	if request.AllowInsecureTLS != nil {
+		updates["allow_insecure_tls"] = *request.AllowInsecureTLS
+	}
+	if request.RemoteOrgID != nil {
+		updates["remote_org_id"] = *request.RemoteOrgID
+	}
+	if request.RemoteURL != nil {
+		updates["remote_url"] = *request.RemoteURL
+	}
+	if request.RemoteToken != nil {
+		updates["remote_api_token"] = *request.RemoteToken
+	}
+	if request.Name != nil {
+		updates["name"] = *request.Name
+	}
+	if request.Description != nil {
+		updates["description"] = *request.Description
+	}
+
+	q := sq.Update("remotes").SetMap(updates).Where(sq.Eq{"id": id}).
+		Suffix("RETURNING id, org_id, name, description, remote_url, remote_org_id, allow_insecure_tls")
+
+	query, args, err := q.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var rc influxdb.RemoteConnection
+	if err := s.store.DB.GetContext(ctx, &rc, query, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errRemoteNotFound
+		}
+		return nil, err
+	}
+	return &rc, nil
 }
 
 func (s service) ValidateUpdatedRemoteConnection(ctx context.Context, id platform.ID, request influxdb.UpdateRemoteConnectionRequest) error {
-	return errNotImplemented
+	config, err := s.getConnectionHTTPConfig(ctx, id)
+	if err != nil {
+		return err
+	}
+	if request.AllowInsecureTLS != nil {
+		config.AllowInsecureTLS = *request.AllowInsecureTLS
+	}
+	if request.RemoteOrgID != nil {
+		config.RemoteOrgID = *request.RemoteOrgID
+	}
+	if request.RemoteToken != nil {
+		config.RemoteToken = *request.RemoteToken
+	}
+	if request.RemoteURL != nil {
+		config.RemoteURL = *request.RemoteURL
+	}
+
+	return s.validator.ValidateRemoteConnectionHTTPConfig(ctx, config)
 }
 
 func (s service) DeleteRemoteConnection(ctx context.Context, id platform.ID) error {
-	return errNotImplemented
+	s.store.Mu.Lock()
+	defer s.store.Mu.Unlock()
+
+	q := sq.Delete("remotes").Where(sq.Eq{"id": id}).Suffix("RETURNING id")
+	query, args, err := q.ToSql()
+	if err != nil {
+		return err
+	}
+
+	var d platform.ID
+	if err := s.store.DB.GetContext(ctx, &d, query, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return errRemoteNotFound
+		}
+		return err
+	}
+	return nil
 }
 
 func (s service) ValidateRemoteConnection(ctx context.Context, id platform.ID) error {
-	return errNotImplemented
+	config, err := s.getConnectionHTTPConfig(ctx, id)
+	if err != nil {
+		return err
+	}
+	return s.validator.ValidateRemoteConnectionHTTPConfig(ctx, config)
+}
+
+func (s service) getConnectionHTTPConfig(ctx context.Context, id platform.ID) (*influxdb.RemoteConnectionHTTPConfig, error) {
+	q := sq.Select("remote_url", "remote_api_token", "remote_org_id", "allow_insecure_tls").
+		From("remotes").
+		Where(sq.Eq{"id": id})
+
+	query, args, err := q.ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	var rc influxdb.RemoteConnectionHTTPConfig
+	if err := s.store.DB.GetContext(ctx, &rc, query, args...); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errRemoteNotFound
+		}
+		return nil, err
+	}
+	return &rc, nil
 }
---
+++ remotes/service_test.go
@@ -0,0 +1,322 @@
+package remotes
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/influxdata/influxdb/v2"
+	"github.com/influxdata/influxdb/v2/kit/platform"
+	"github.com/influxdata/influxdb/v2/mock"
+	"github.com/influxdata/influxdb/v2/sqlite"
+	"github.com/influxdata/influxdb/v2/sqlite/migrations"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap/zaptest"
+)
+
+var (
+	ctx        = context.Background()
+	initID     = platform.ID(1)
+	desc       = "testing testing"
+	connection = influxdb.RemoteConnection{
+		ID:               initID,
+		OrgID:            platform.ID(10), //createReq.OrgID,
+		Name:             "test",
+		Description:      &desc,
+		RemoteURL:        "https://influxdb.cloud",
+		RemoteOrgID:      platform.ID(20),
+		AllowInsecureTLS: true,
+	}
+	fakeToken = "abcdefghijklmnop"
+	createReq = influxdb.CreateRemoteConnectionRequest{
+		OrgID:            connection.OrgID,
+		Name:             connection.Name,
+		Description:      connection.Description,
+		RemoteURL:        connection.RemoteURL,
+		RemoteToken:      fakeToken,
+		RemoteOrgID:      connection.RemoteOrgID,
+		AllowInsecureTLS: connection.AllowInsecureTLS,
+	}
+	httpConfig = influxdb.RemoteConnectionHTTPConfig{
+		RemoteURL:        connection.RemoteURL,
+		RemoteToken:      fakeToken,
+		RemoteOrgID:      connection.RemoteOrgID,
+		AllowInsecureTLS: connection.AllowInsecureTLS,
+	}
+	fakeToken2 = "qrstuvwxyz"
+	fals       = false
+	updateReq  = influxdb.UpdateRemoteConnectionRequest{
+		RemoteToken:      &fakeToken2,
+		AllowInsecureTLS: &fals,
+	}
+	updatedConnection = influxdb.RemoteConnection{
+		ID:               connection.ID,
+		OrgID:            connection.OrgID,
+		Name:             connection.Name,
+		Description:      connection.Description,
+		RemoteURL:        connection.RemoteURL,
+		RemoteOrgID:      connection.RemoteOrgID,
+		AllowInsecureTLS: *updateReq.AllowInsecureTLS,
+	}
+	updatedHttpConfig = influxdb.RemoteConnectionHTTPConfig{
+		RemoteURL:        updatedConnection.RemoteURL,
+		RemoteToken:      fakeToken2,
+		RemoteOrgID:      updatedConnection.RemoteOrgID,
+		AllowInsecureTLS: updatedConnection.AllowInsecureTLS,
+	}
+)
+
+func TestCreateAndGetConnection(t *testing.T) {
+	t.Parallel()
+
+	svc, mockValidator, clean := newTestService(t)
+	defer clean(t)
+
+	// Getting or validating an invalid ID should return an error.
+	got, err := svc.GetRemoteConnection(ctx, initID)
+	require.ErrorIs(t, errRemoteNotFound, err)
+	require.Nil(t, got)
+	require.ErrorIs(t, errRemoteNotFound, svc.ValidateRemoteConnection(ctx, initID))
+
+	// Create a connection, check the results.
+	created, err := svc.CreateRemoteConnection(ctx, createReq)
+	require.NoError(t, err)
+	require.Equal(t, connection, *created)
+
+	// Read the created connection and assert it matches the creation response.
+	got, err = svc.GetRemoteConnection(ctx, initID)
+	require.NoError(t, err)
+	require.Equal(t, connection, *got)
+
+	// Validate the connection; this is mostly a no-op, for this test, but it allows
+	// us to assert that the auth token was properly persisted.
+	fakeErr := errors.New("O NO")
+	mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &httpConfig).Return(fakeErr)
+	require.ErrorIs(t, fakeErr, svc.ValidateRemoteConnection(ctx, initID))
+}
+
+func TestValidateConnectionWithoutPersisting(t *testing.T) {
+	t.Parallel()
+
+	t.Run("error", func(t *testing.T) {
+		t.Parallel()
+
+		svc, mockValidator, clean := newTestService(t)
+		defer clean(t)
+
+		fakeErr := errors.New("O NO")
+		mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &httpConfig).Return(fakeErr)
+		require.ErrorIs(t, fakeErr, svc.ValidateNewRemoteConnection(ctx, createReq))
+
+		got, err := svc.GetRemoteConnection(ctx, initID)
+		require.ErrorIs(t, err, errRemoteNotFound)
+		require.Nil(t, got)
+	})
+
+	t.Run("no error", func(t *testing.T) {
+		t.Parallel()
+
+		svc, mockValidator, clean := newTestService(t)
+		defer clean(t)
+
+		mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &httpConfig).Return(nil)
+		require.NoError(t, svc.ValidateNewRemoteConnection(ctx, createReq))
+
+		got, err := svc.GetRemoteConnection(ctx, initID)
+		require.ErrorIs(t, err, errRemoteNotFound)
+		require.Nil(t, got)
+	})
+}
+
+func TestUpdateAndGetConnection(t *testing.T) {
+	t.Parallel()
+
+	svc, mockValidator, clean := newTestService(t)
+	defer clean(t)
+
+	// Updating a nonexistent ID fails.
+	updated, err := svc.UpdateRemoteConnection(ctx, initID, updateReq)
+	require.ErrorIs(t, err, errRemoteNotFound)
+	require.Nil(t, updated)
+
+	// Create a connection.
+	created, err := svc.CreateRemoteConnection(ctx, createReq)
+	require.NoError(t, err)
+	require.Equal(t, connection, *created)
+
+	// Update the connection.
+	updated, err = svc.UpdateRemoteConnection(ctx, initID, updateReq)
+	require.NoError(t, err)
+	require.Equal(t, updatedConnection, *updated)
+
+	// Validate the updated connection to check that the new auth token persisted.
+	mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &updatedHttpConfig).Return(nil)
+	require.NoError(t, svc.ValidateRemoteConnection(ctx, initID))
+}
+
+func TestValidateUpdatedConnectionWithoutPersisting(t *testing.T) {
+	t.Parallel()
+
+	setup := func(t *testing.T, svc *service) {
+		// Validating an update to a nonexistent ID fails.
+		updated, err := svc.UpdateRemoteConnection(ctx, initID, updateReq)
+		require.ErrorIs(t, err, errRemoteNotFound)
+		require.Nil(t, updated)
+
+		// Create a connection.
+		created, err := svc.CreateRemoteConnection(ctx, createReq)
+		require.NoError(t, err)
+		require.Equal(t, connection, *created)
+	}
+	fakeErr := errors.New("O NO")
+
+	t.Run("update causes error", func(t *testing.T) {
+		t.Parallel()
+
+		svc, mockValidator, clean := newTestService(t)
+		defer clean(t)
+		setup(t, svc)
+
+		mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &updatedHttpConfig).Return(fakeErr)
+		mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &httpConfig).Return(nil)
+
+		require.ErrorIs(t, svc.ValidateUpdatedRemoteConnection(ctx, initID, updateReq), fakeErr)
+
+		// Ensure the update wasn't applied.
+		got, err := svc.GetRemoteConnection(ctx, initID)
+		require.NoError(t, err)
+		require.Equal(t, connection, *got)
+		require.NoError(t, svc.ValidateRemoteConnection(ctx, initID))
+	})
+
+	t.Run("update fixes error", func(t *testing.T) {
+		t.Parallel()
+
+		svc, mockValidator, clean := newTestService(t)
+		defer clean(t)
+		setup(t, svc)
+
+		mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &updatedHttpConfig).Return(nil)
+		mockValidator.EXPECT().ValidateRemoteConnectionHTTPConfig(gomock.Any(), &httpConfig).Return(fakeErr)
+
+		require.NoError(t, svc.ValidateUpdatedRemoteConnection(ctx, initID, updateReq))
+
+		// Ensure the update wasn't applied.
+		got, err := svc.GetRemoteConnection(ctx, initID)
+		require.NoError(t, err)
+		require.Equal(t, connection, *got)
+		require.ErrorIs(t, svc.ValidateRemoteConnection(ctx, initID), fakeErr)
+	})
+}
+
+func TestDeleteConnection(t *testing.T) {
+	t.Parallel()
+
+	svc, _, clean := newTestService(t)
+	defer clean(t)
+
+	// Deleting a nonexistent ID should return an error.
+	require.ErrorIs(t, svc.DeleteRemoteConnection(ctx, initID), errRemoteNotFound)
+
+	// Create a connection, then delete it.
+	created, err := svc.CreateRemoteConnection(ctx, createReq)
+	require.NoError(t, err)
+	require.Equal(t, connection, *created)
+	require.NoError(t, svc.DeleteRemoteConnection(ctx, initID))
+
+	// Looking up the ID should again produce an error.
+	got, err := svc.GetRemoteConnection(ctx, initID)
+	require.ErrorIs(t, err, errRemoteNotFound)
+	require.Nil(t, got)
+}
+
+func TestListConnections(t *testing.T) {
+	t.Parallel()
+
+	createReq2, createReq3 := createReq, createReq
+	createReq2.Name, createReq3.Name = "test2", "test3"
+	altURL := "https://other.influxdb"
+	createReq2.RemoteURL, createReq3.RemoteURL = altURL, altURL
+
+	setup := func(t *testing.T, svc *service) []influxdb.RemoteConnection {
+		var allConns []influxdb.RemoteConnection
+		for _, req := range []influxdb.CreateRemoteConnectionRequest{createReq, createReq2, createReq3} {
+			created, err := svc.CreateRemoteConnection(ctx, req)
+			require.NoError(t, err)
+			allConns = append(allConns, *created)
+		}
+		return allConns
+	}
+
+	t.Run("list all", func(t *testing.T) {
+		t.Parallel()
+
+		svc, _, clean := newTestService(t)
+		defer clean(t)
+		allConns := setup(t, svc)
+
+		listed, err := svc.ListRemoteConnections(ctx, influxdb.RemoteConnectionListFilter{OrgID: connection.OrgID})
+		require.NoError(t, err)
+		require.Equal(t, influxdb.RemoteConnections{Remotes: allConns}, *listed)
+	})
+
+	t.Run("list by name", func(t *testing.T) {
+		t.Parallel()
+
+		svc, _, clean := newTestService(t)
+		defer clean(t)
+		allConns := setup(t, svc)
+
+		listed, err := svc.ListRemoteConnections(ctx, influxdb.RemoteConnectionListFilter{
+			OrgID: connection.OrgID,
+			Name:  &createReq2.Name,
+		})
+		require.NoError(t, err)
+		require.Equal(t, influxdb.RemoteConnections{Remotes: allConns[1:2]}, *listed)
+	})
+
+	t.Run("list by URL", func(t *testing.T) {
+		t.Parallel()
+
+		svc, _, clean := newTestService(t)
+		defer clean(t)
+		allConns := setup(t, svc)
+
+		listed, err := svc.ListRemoteConnections(ctx, influxdb.RemoteConnectionListFilter{
+			OrgID:     connection.OrgID,
+			RemoteURL: &altURL,
+		})
+		require.NoError(t, err)
+		require.Equal(t, influxdb.RemoteConnections{Remotes: allConns[1:]}, *listed)
+	})
+
+	t.Run("list by other org ID", func(t *testing.T) {
+		t.Parallel()
+
+		svc, _, clean := newTestService(t)
+		defer clean(t)
+		setup(t, svc)
+
+		listed, err := svc.ListRemoteConnections(ctx, influxdb.RemoteConnectionListFilter{OrgID: platform.ID(1000)})
+		require.NoError(t, err)
+		require.Equal(t, influxdb.RemoteConnections{}, *listed)
+	})
+}
+
+func newTestService(t *testing.T) (*service, *mock.MockRemoteConnectionValidator, func(t *testing.T)) {
+	store, clean := sqlite.NewTestStore(t)
+	logger := zaptest.NewLogger(t)
+	sqliteMigrator := sqlite.NewMigrator(store, logger)
+	require.NoError(t, sqliteMigrator.Up(ctx, migrations.All))
+
+	mockValidator := mock.NewMockRemoteConnectionValidator(gomock.NewController(t))
+	svc := service{
+		store:       store,
+		log:         logger,
+		idGenerator: mock.NewIncrementingIDGenerator(platform.ID(1)),
+		validator:   mockValidator,
+	}
+
+	return &svc, mockValidator, clean
+}
