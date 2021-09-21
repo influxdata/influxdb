fb7299a169c092ffcd6e4fd24a67d13d9c6e2037
Marco Neumann <marco@crepererum.net>
2021-09-21 15:08:37 +0000

    fix: bubble up write errors (#2598)
    Fixes #2538.
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/influxdb_ioxd/rpc/error.rs
+++ src/influxdb_ioxd/rpc/error.rs
@@ -67,6 +67,9 @@ pub fn default_server_error_handler(error: server::Error) -> tonic::Status {
         Error::DatabaseInit { source } => {
             tonic::Status::invalid_argument(format!("Cannot initialize database: {}", source))
         }
+        e @ Error::StoreSequencedEntryFailures { .. } => {
+            tonic::Status::invalid_argument(e.to_string())
+        }
         error => {
             error!(?error, "Unexpected error");
             InternalError {}.into()
