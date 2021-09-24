641037c3286b99e58bdf67110f43446d882e70bd
Dan Moran <dmoran@influxdata.com>
2021-09-24 12:02:58 -0400

    fix: set proper user-agent in validation requests

--- replications/internal/validator.go
+++ replications/internal/validator.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"runtime"
 
 	"github.com/influxdata/influx-cli/v2/api"
+	"github.com/influxdata/influxdb/v2"
 	ierrors "github.com/influxdata/influxdb/v2/kit/platform/errors"
 )
 
@@ -16,6 +18,13 @@ func NewValidator() *noopWriteValidator {
 // noopWriteValidator checks if replication parameters are valid by attempting to write an empty payload
 // to the remote host using the configured information.
 type noopWriteValidator struct{}
+
+var userAgent = fmt.Sprintf(
+	"influxdb-oss/%s (%s) Sha/%s Date/%s",
+	influxdb.GetBuildInfo().Version,
+	runtime.GOOS,
+	influxdb.GetBuildInfo().Commit,
+	influxdb.GetBuildInfo().Date)
 
 func (s noopWriteValidator) ValidateReplication(ctx context.Context, config *ReplicationHTTPConfig) error {
 	u, err := url.Parse(config.RemoteURL)
@@ -28,7 +37,7 @@ func (s noopWriteValidator) ValidateReplication(ctx context.Context, config *Rep
 	}
 	params := api.ConfigParams{
 		Host:             u,
-		UserAgent:        "",
+		UserAgent:        userAgent,
 		Token:            &config.RemoteToken,
 		AllowInsecureTLS: config.AllowInsecureTLS,
 	}
