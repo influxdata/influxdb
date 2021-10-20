f75bd0771ad0a9859fab5f513e5a06090a95632b
Marco Neumann <marco@crepererum.net>
2021-10-20 08:56:03 +0200

    refactor: sort file list in build script

--- generated_types/build.rs
+++ generated_types/build.rs
@@ -24,35 +24,35 @@ fn main() -> Result<()> {
 /// - `influxdata.iox.write.v1.rs`
 /// - `influxdata.platform.storage.rs`
 fn generate_grpc_types(root: &Path) -> Result<()> {
-    let storage_path = root.join("influxdata/platform/storage");
-    let idpe_path = root.join("com/github/influxdata/idpe/storage/read");
     let catalog_path = root.join("influxdata/iox/catalog/v1");
+    let idpe_path = root.join("com/github/influxdata/idpe/storage/read");
     let management_path = root.join("influxdata/iox/management/v1");
+    let storage_path = root.join("influxdata/platform/storage");
     let write_path = root.join("influxdata/iox/write/v1");
 
     let proto_files = vec![
-        storage_path.join("test.proto"),
-        storage_path.join("predicate.proto"),
-        storage_path.join("storage_common.proto"),
-        storage_path.join("service.proto"),
-        storage_path.join("storage_common_idpe.proto"),
-        idpe_path.join("source.proto"),
         catalog_path.join("catalog.proto"),
         catalog_path.join("parquet_metadata.proto"),
         catalog_path.join("predicate.proto"),
-        management_path.join("database_rules.proto"),
+        idpe_path.join("source.proto"),
         management_path.join("chunk.proto"),
+        management_path.join("database_rules.proto"),
+        management_path.join("jobs.proto"),
         management_path.join("partition.proto"),
         management_path.join("server_config.proto"),
         management_path.join("service.proto"),
         management_path.join("shard.proto"),
-        management_path.join("jobs.proto"),
-        write_path.join("service.proto"),
-        root.join("influxdata/pbdata/v1/influxdb_pb_data_protocol.proto"),
-        root.join("grpc/health/v1/service.proto"),
         root.join("google/longrunning/operations.proto"),
         root.join("google/rpc/error_details.proto"),
         root.join("google/rpc/status.proto"),
+        root.join("grpc/health/v1/service.proto"),
+        root.join("influxdata/pbdata/v1/influxdb_pb_data_protocol.proto"),
+        storage_path.join("predicate.proto"),
+        storage_path.join("service.proto"),
+        storage_path.join("storage_common.proto"),
+        storage_path.join("storage_common_idpe.proto"),
+        storage_path.join("test.proto"),
+        write_path.join("service.proto"),
     ];
 
     // Tell cargo to recompile if any of these proto files are changed
