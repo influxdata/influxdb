90b932fab0b611e4735dff4ba55c9b475c8379a6
Daniel Moran <danxmoran@gmail.com>
2021-10-28 16:02:15 -0400

    feat: take a backup of KV/SQL metadata stores before applying migrations

--- sqlite/test_migrations/all.go
+++ sqlite/test_migrations/all.go
@@ -4,3 +4,9 @@ import "embed"
 
 //go:embed *.sql
 var All embed.FS
+
+//go:embed 0001_create_test_table_1.sql
+var First embed.FS
+
+//go:embed 0002_rename_test_table_id_1.sql 0003_create_test_table_2.sql
+var Rest embed.FS
