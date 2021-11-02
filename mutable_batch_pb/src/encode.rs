e444fa4cb265b72e1d13376a9c8252abfe97d312
Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>
2021-11-02 18:31:53 +0000

    feat: pbdata encode (#2724) (#3009)

---
+++ mutable_batch_pb/src/encode.rs
@@ -0,0 +1,122 @@
+//! Code to encode [`MutableBatch`] as pbdata protobuf
+
+use arrow_util::bitset::{iter_set_positions, BitSet};
+use generated_types::influxdata::pbdata::v1::column::SemanticType;
+use generated_types::influxdata::pbdata::v1::{
+    column::Values as PbValues, Column as PbColumn, DatabaseBatch, InternedStrings, PackedStrings,
+    TableBatch,
+};
+use mutable_batch::column::{Column, ColumnData};
+use mutable_batch::{DbWrite, MutableBatch};
+use schema::InfluxColumnType;
+
+/// Convert a [`DbWrite`] to a [`DatabaseBatch`]
+pub fn encode_write(db_name: &str, write: &DbWrite) -> DatabaseBatch {
+    DatabaseBatch {
+        database_name: db_name.to_string(),
+        table_batches: write
+            .tables()
+            .map(|(table_name, batch)| encode_batch(table_name, batch))
+            .collect(),
+    }
+}
+
+/// Convert a [`MutableBatch`] to [`TableBatch`]
+pub fn encode_batch(table_name: &str, batch: &MutableBatch) -> TableBatch {
+    TableBatch {
+        table_name: table_name.to_string(),
+        columns: batch
+            .columns()
+            .map(|(column_name, column)| encode_column(column_name, column))
+            .collect(),
+        row_count: batch.rows() as u32,
+    }
+}
+
+fn encode_column(column_name: &str, column: &Column) -> PbColumn {
+    let valid_mask = column.valid_mask().bytes();
+
+    let mut values = PbValues {
+        i64_values: vec![],
+        f64_values: vec![],
+        u64_values: vec![],
+        string_values: vec![],
+        bool_values: vec![],
+        bytes_values: vec![],
+        packed_string_values: None,
+        interned_string_values: None,
+    };
+
+    let semantic_type = match column.influx_type() {
+        InfluxColumnType::Tag => SemanticType::Tag,
+        InfluxColumnType::Field(_) => SemanticType::Field,
+        InfluxColumnType::Timestamp => SemanticType::Time,
+        InfluxColumnType::IOx(_) => SemanticType::Iox,
+    };
+
+    match column.data() {
+        ColumnData::F64(col_data, _) => {
+            values.f64_values = iter_set_positions(valid_mask)
+                .map(|idx| col_data[idx])
+                .collect();
+        }
+        ColumnData::I64(col_data, _) => {
+            values.i64_values = iter_set_positions(valid_mask)
+                .map(|idx| col_data[idx])
+                .collect();
+        }
+        ColumnData::U64(col_data, _) => {
+            values.u64_values = iter_set_positions(valid_mask)
+                .map(|idx| col_data[idx])
+                .collect();
+        }
+        ColumnData::String(col_data, _) => {
+            let (col_offsets, col_storage) = col_data.inner();
+
+            // Nulls are stored as empty strings which take up no space
+            let mut offsets = vec![0; 1];
+            for idx in iter_set_positions(valid_mask) {
+                offsets.push(col_offsets[idx + 1] as u32)
+            }
+
+            values.packed_string_values = Some(PackedStrings {
+                values: col_storage.to_string(),
+                offsets,
+            });
+        }
+        ColumnData::Bool(col_data, _) => {
+            values.bool_values = iter_set_positions(valid_mask)
+                .map(|idx| col_data.get(idx))
+                .collect();
+        }
+        ColumnData::Tag(col_data, dict, _) => {
+            let (val_offsets, val_storage) = dict.values().inner();
+
+            values.interned_string_values = Some(InternedStrings {
+                dictionary: Some(PackedStrings {
+                    values: val_storage.to_string(),
+                    offsets: val_offsets.iter().map(|idx| *idx as u32).collect(),
+                }),
+                values: iter_set_positions(valid_mask)
+                    .map(|idx| col_data[idx] as u32)
+                    .collect(),
+            });
+        }
+    };
+
+    PbColumn {
+        column_name: column_name.to_string(),
+        semantic_type: semantic_type as _,
+        values: Some(values),
+        null_mask: compute_null_mask(column.valid_mask()),
+    }
+}
+
+fn compute_null_mask(valid_mask: &BitSet) -> Vec<u8> {
+    let mut buffer: Vec<_> = valid_mask.bytes().iter().map(|x| !*x).collect();
+    let overrun = valid_mask.len() & 7;
+    if overrun > 0 {
+        *buffer.last_mut().unwrap() &= (1 << overrun) - 1;
+    }
+    buffer
+}
