48eaacf1a133fd9039a359194d536b9580c92ecf
Geoffrey Wossum <gwossum@influxdata.com>
2021-11-02 16:48:32 -0500

    chore: improve slowproxy with better error checking and propagation

--- tests/slowproxy_test.go
+++ tests/slowproxy_test.go
@@ -1,12 +1,15 @@
 package tests
 
 import (
+	"fmt"
 	"io"
 	"math/rand"
 	"net"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/influxdata/influxdb/pkg/errors"
 	"github.com/influxdata/influxdb/pkg/limiter"
 	"github.com/stretchr/testify/require"
 )
@@ -18,6 +21,9 @@ type slowProxy struct {
 
 	bytesPerSecond int // Target BPS rate
 	burstLimit     int // Maxmimum burst speed
+
+	muConnErrors sync.Mutex // muConnErrors protects connErrors
+	connErrors   []error    // List of connection errors since last check
 }
 
 // NewSlowProxy creates a slowProxy to a given server with the specified rate limits.
@@ -36,7 +42,7 @@ func NewSlowProxy(src, dest string, bytesPerSecond, burstLimit int) (*slowProxy,
 	}, nil
 }
 
-// Address returns the listening address of the slowProxy service.
+// Addr returns the listening address of the slowProxy service.
 func (s *slowProxy) Addr() net.Addr {
 	return s.listener.Addr()
 }
@@ -48,9 +54,25 @@ func (s *slowProxy) Serve() error {
 		if err != nil {
 			return err
 		}
-		// Run handleConnection async since server dial might block
-		go s.handleConnection(conn)
+		// Run handleConnection async since it blocks while its connection is open, collect errors.
+		go func() {
+			if err := s.handleConnection(conn); err != nil {
+				s.muConnErrors.Lock()
+				s.connErrors = append(s.connErrors, fmt.Errorf("handleConnection: %w", err))
+				s.muConnErrors.Unlock()
+			}
+		}()
 	}
+}
+
+// ConnectionErrors returns a slice of errors from closed connections since the last time Errors was called.
+func (s *slowProxy) ConnectionErrors() []error {
+	var errors []error
+	s.muConnErrors.Lock()
+	copy(errors, s.connErrors)
+	s.connErrors = nil
+	s.muConnErrors.Unlock()
+	return errors
 }
 
 // Close the proxy server and frees network resources. Already running connections are not stopped.
@@ -59,29 +81,47 @@ func (s *slowProxy) Close() error {
 }
 
 // handleConnection sets up and handles a single proxy connection.
-func (s *slowProxy) handleConnection(client_conn net.Conn) error {
+func (s *slowProxy) handleConnection(clientConn net.Conn) (err error) {
+	defer errors.Capture(&err, clientConn.Close)()
+
 	// Open up connection to destination server
-	server_conn, err := net.Dial("tcp", s.dest)
+	serverConn, err := net.Dial("tcp", s.dest)
 	if err != nil {
-		client_conn.Close()
-		return nil
+		return
+	}
+	defer errors.Capture(&err, serverConn.Close)()
+
+	slowServer := limiter.NewWriter(serverConn, s.bytesPerSecond, s.burstLimit)
+	slowClient := limiter.NewWriter(clientConn, s.bytesPerSecond, s.burstLimit)
+
+	errorChan := make(chan error, 2)
+	var wg sync.WaitGroup
+	shuttleData := func(dst io.WriteCloser, src io.ReadCloser) {
+		defer wg.Done()
+		errorChan <- func(dst io.WriteCloser, src io.ReadCloser) (err error) {
+			defer errors.Capture(&err, dst.Close)()
+			defer errors.Capture(&err, src.Close)()
+			_, err = io.Copy(dst, src)
+			return
+		}(dst, src)
 	}
 
-	slow_server := limiter.NewWriter(server_conn, s.bytesPerSecond, s.burstLimit)
-	slow_client := limiter.NewWriter(client_conn, s.bytesPerSecond, s.burstLimit)
+	wg.Add(2)
+	go shuttleData(slowServer, clientConn)
+	go shuttleData(slowClient, serverConn)
+	wg.Wait()
+	close(errorChan)
 
-	go s.shuttleData(slow_server, client_conn)
-	go s.shuttleData(slow_client, server_conn)
+	for shuttleErr := range errorChan {
+		// io.EOF is an expected condition, not an error for us
+		if shuttleErr != nil && shuttleErr != io.EOF {
+			if err == nil {
+				err = shuttleErr
+			}
+		}
+	}
 
-	return nil
-}
-
-// shuttleData shuttles data in one direction from a source to a sink.
-// It also closes both sides when an error occurs, which includes EOF.
-func (s *slowProxy) shuttleData(dst io.WriteCloser, src io.ReadCloser) {
-	_, _ = io.Copy(dst, src)
-	dst.Close()
-	src.Close()
+	return
 }
 
 func TestSlowProxy(t *testing.T) {
@@ -102,19 +142,31 @@ func TestSlowProxy(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, sp)
 	require.NotEqual(t, listener.Addr().String(), sp.Addr().String())
-	go sp.Serve()
+	defer sp.Close()
+
+	spErrCh := make(chan error, 1)
+	go func() {
+		spErrCh <- sp.Serve()
+	}()
 
 	// connectionBody runs the body of a test connection. Returns received data.
-	connectionBody := func(conn net.Conn, sender bool) []byte {
+	connectionBody := func(conn net.Conn, sender bool) ([]byte, error) {
 		recvbuf := make([]byte, dataSize*2)
 		var recvbufLen int
 		if sender {
 			count, err := conn.Write(sendbuf)
-			require.NoError(t, err)
-			require.Equal(t, len(sendbuf), count)
+			if err != nil {
+				return nil, err
+			}
+			if len(sendbuf) != count {
+				return nil, fmt.Errorf("incomplete write: %d of %d bytes", count, len(sendbuf))
+			}
 
 			// Read any data that might be there for us (mainly for sanity checks)
 			conn.SetReadDeadline(time.Now().Add(50 * time.Millisecond))
+			// We expect to get an i/o timeout error on the Read. If we don't get an
+			// error, we'll get data, which will cause a failure later on. So
+			// we'll ignore any that occurs here.
 			count, _ = conn.Read(recvbuf[recvbufLen:])
 			recvbufLen += count
 			conn.SetReadDeadline(time.Time{})
@@ -126,46 +178,61 @@ func TestSlowProxy(t *testing.T) {
 				if err == io.EOF {
 					break
 				}
-				require.NoError(t, err)
+				if err != nil {
+					return nil, err
+				}
 			}
 		}
-		return recvbuf[:recvbufLen]
+		return recvbuf[:recvbufLen], nil
 	}
 
-	singleServingServer := func(sender bool) []byte {
+	singleServingServer := func(sender bool) ([]byte, error) {
 		conn, err := listener.Accept()
-		require.NoError(t, err)
+		if err != nil {
+			return nil, err
+		}
 		defer conn.Close()
 		return connectionBody(conn, sender)
 	}
 
-	singleServingClient := func(sender bool) []byte {
+	singleServingClient := func(sender bool) ([]byte, error) {
 		conn, err := net.Dial("tcp", sp.Addr().String())
-		require.NoError(t, err)
+		if err != nil {
+			return nil, err
+		}
 		defer conn.Close()
 		return connectionBody(conn, sender)
 	}
 
+	type connResult struct {
+		recv []byte // Received data
+		err  error  // Any error from the connection
+	}
 	runTest := func(clientSends bool) {
 		// Spin the server up
-		serverRecv := make(chan []byte, 1)
-		go func(recv chan []byte) {
-			recv <- singleServingServer(!clientSends)
-		}(serverRecv)
+		serverResCh := make(chan connResult, 1)
+		go func(ch chan connResult) {
+			recv, err := singleServingServer(!clientSends)
+			ch <- connResult{recv: recv, err: err}
+		}(serverResCh)
 
 		// Wait for server connection to complete, then check results look correct.
 		t1 := time.Now()
-		clientRecvbuf := singleServingClient(clientSends)
-		serverRecvbuf := <-serverRecv
+		clientRecvbuf, clientErr := singleServingClient(clientSends)
+		serverResult := <-serverResCh
 		elapsed := time.Since(t1)
+
+		// Make sure neither the client nor the server encountered an error
+		require.NoError(t, clientErr)
+		require.NoError(t, serverResult.err)
 
 		// Now see if the data was sent properly
 		if clientSends {
 			require.Empty(t, clientRecvbuf)
-			require.Equal(t, sendbuf, serverRecvbuf)
+			require.Equal(t, sendbuf, serverResult.recv)
 		} else {
 			require.Equal(t, sendbuf, clientRecvbuf)
-			require.Empty(t, serverRecvbuf)
+			require.Empty(t, serverResult.recv)
 		}
 
 		// See if the throttling looks appropriate. We're looking for +/- 5% on duration
@@ -179,4 +246,10 @@ func TestSlowProxy(t *testing.T) {
 	// Test shuttling data in both directions, with close initiaited once by each side
 	runTest(true)
 	runTest(false)
+
+	require.NoError(t, sp.Close())
+	spErr := <-spErrCh
+	require.Error(t, spErr)
+	require.Contains(t, spErr.Error(), "use of closed network connection")
+	require.Empty(t, sp.ConnectionErrors())
 }
