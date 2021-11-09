00b328218089ee77c3ead2f0ccb912ee3f44117d
Marco Neumann <marco@crepererum.net>
2021-11-09 12:22:22 +0100

    refactor: partially split up HTTP implementation and tests
    This should make it easier to to wire up the HTTP write interface for
    the router. No functional change intended, this just moves code around.

--- influxdb_iox/src/influxdb_ioxd/server_type/router/http.rs
+++ influxdb_iox/src/influxdb_ioxd/server_type/router/http.rs
@@ -34,3 +34,37 @@ pub async fn route_request(
         }),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
+
+    use router::server::RouterServer;
+    use time::SystemProvider;
+
+    use crate::influxdb_ioxd::{
+        http::test_utils::{assert_health, assert_metrics, TestServer},
+        server_type::common_state::CommonServerState,
+    };
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_health() {
+        assert_health(test_server().await).await;
+    }
+
+    #[tokio::test]
+    async fn test_metrics() {
+        assert_metrics(test_server().await).await;
+    }
+
+    async fn test_server() -> TestServer<RouterServerType> {
+        let common_state = CommonServerState::for_testing();
+        let time_provider = Arc::new(SystemProvider::new());
+        let server =
+            Arc::new(RouterServer::new(None, common_state.trace_collector(), time_provider).await);
+        let server_type = Arc::new(RouterServerType::new(server, &common_state));
+        TestServer::new(server_type)
+    }
+}
