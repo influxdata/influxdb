3220970fea30535d341f2735333518d57e1a0f27
Marco Neumann <marco@crepererum.net>
2021-11-12 11:31:45 +0100

    fix: server IDs for routers on startup

--- influxdb_iox/tests/end_to_end_cases/deployment_api.rs
+++ influxdb_iox/tests/end_to_end_cases/deployment_api.rs
@@ -7,7 +7,7 @@ use influxdb_iox_client::{
 use test_helpers::assert_error;
 use tonic::Code;
 
-use crate::common::server_fixture::{ServerFixture, ServerType};
+use crate::common::server_fixture::{ServerFixture, ServerType, TestConfig};
 
 #[tokio::test]
 async fn test_serving_readiness_database() {
@@ -104,6 +104,28 @@ async fn test_set_get_server_id_router() {
     assert_set_get_server_id(ServerFixture::create_single_use(ServerType::Router).await).await;
 }
 
+#[tokio::test]
+async fn test_set_get_server_id_already_set_database() {
+    let server_id = NonZeroU32::new(42).unwrap();
+    let test_config = TestConfig::new(ServerType::Database).with_server_id(server_id);
+    assert_set_get_server_id_already_set(
+        ServerFixture::create_single_use_with_config(test_config).await,
+        server_id,
+    )
+    .await;
+}
+
+#[tokio::test]
+async fn test_set_get_server_id_already_set_router() {
+    let server_id = NonZeroU32::new(42).unwrap();
+    let test_config = TestConfig::new(ServerType::Router).with_server_id(server_id);
+    assert_set_get_server_id_already_set(
+        ServerFixture::create_single_use_with_config(test_config).await,
+        server_id,
+    )
+    .await;
+}
+
 async fn assert_set_get_server_id(server_fixture: ServerFixture) {
     let mut client = server_fixture.deployment_client();
 
@@ -116,6 +138,22 @@ async fn assert_set_get_server_id(server_fixture: ServerFixture) {
 
     let got = client.get_server_id().await.expect("get ID failed");
     assert_eq!(got, test_id);
+
+    // setting server ID a second time should fail
+    let result = client
+        .update_server_id(NonZeroU32::try_from(13).unwrap())
+        .await;
+    assert_error!(result, UpdateServerIdError::AlreadySet);
+}
+
+async fn assert_set_get_server_id_already_set(
+    server_fixture: ServerFixture,
+    server_id: NonZeroU32,
+) {
+    let mut client = server_fixture.deployment_client();
+
+    let got = client.get_server_id().await.expect("get ID failed");
+    assert_eq!(got, server_id);
 
     // setting server ID a second time should fail
     let result = client
