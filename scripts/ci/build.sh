d8e05c458fbcf045296c03ade14305f4e370ade8
Daniel Moran <danxmoran@gmail.com>
2021-11-15 13:03:04 -0500

    build: DRY up build script

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -3,55 +3,7 @@ set -exo pipefail
 
 declare -r SCRIPT_DIR=$(cd $(dirname $0) > /dev/null && pwd)
 
-function build_linux () {
-    local tags=osusergo,netgo,static_build,assets,sqlite_foreign_keys,sqlite_json
-    local cc
-    case $(go env GOARCH) in
-        amd64)
-            cc=musl-gcc
-            ;;
-        arm64)
-            cc=aarch64-unknown-linux-musl-gcc
-            TAGS="$TAGS,noasm"
-            ;;
-        *)
-            >&2 echo Error: Unknown arch $(go env GOARCH)
-            exit 1
-            ;;
-    esac
-
-    local -r commit=$(git rev-parse --short HEAD)
-    local -r build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-    local -r extld="-fno-PIC -static -Wl,-z,stack-size=8388608"
-    CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=${cc} go build \
-        -tags "$tags" \
-        -buildmode pie \
-        -ldflags "-s -w -X main.version=${2} -X main.commit=${commit} -X main.date=${build_date} -extldflags '${extld}'" \
-        -o "${1}/" \
-        "${3}"
-}
-
-function build_mac () {
-    local -r commit=$(git rev-parse --short HEAD)
-    local -r build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-    CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=x86_64-apple-darwin16-clang go build \
-        -tags assets,sqlite_foreign_keys,sqlite_json \
-        -buildmode pie \
-        -ldflags "-s -w -X main.version=${2} -X main.commit=${commit} -X main.date=${build_date}" \
-        -o "${1}/" \
-        "${3}"
-}
-
-function build_windows () {
-    local -r commit=$(git rev-parse --short HEAD)
-    local -r build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-    CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=x86_64-w64-mingw32-gcc go build \
-        -tags assets,sqlite_foreign_keys,sqlite_json,timetzdata \
-        -buildmode exe \
-        -ldflags "-s -w -X main.version=${2} -X main.commit=${commit} -X main.date=${build_date}" \
-        -o "${1}/" \
-        "${3}"
-}
+declare -r LINUX_EXTLD="-fno-PIC -static -Wl,-z,stack-size=8388608"
 
 function main () {
     if [[ $# != 3 ]]; then
@@ -61,23 +13,47 @@ function main () {
     local -r out_dir=$1 build_type=$2 pkg=$3
     local -r version="$(${SCRIPT_DIR}/build-version.sh "$build_type")"
 
-    rm -rf "$out_dir"
     mkdir -p "$out_dir"
-    case $(go env GOOS) in
-        linux)
-            build_linux "$out_dir" "$version" "$pkg"
+
+    local -r commit=$(git rev-parse --short HEAD)
+    local -r build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
+
+    local tags=assets,sqlite_foreign_keys,sqlite_json
+    local buildmode=pie
+    local ldflags="-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date}"
+    local cc
+
+    local -r os_arch="$(go env GOOS)_$(go env GOARCH)"
+    case "$os_arch" in
+        linux_amd64)
+            cc=musl-gcc
+            ldflags="$ldflags -extldflags '$LINUX_EXTLD'"
             ;;
-        darwin)
-            build_mac "$out_dir" "$version" "$pkg"
+        linux_arm64)
+            cc=aarch64-unknown-linux-musl-gcc
+            ldflags="$ldflags -extldflags '$LINUX_EXTLD'"
+            tags="$tags,noasm"
             ;;
-        windows)
-            build_windows "$out_dir" "$version" "$pkg"
+        darwin_amd64)
+            cc=x86_64-apple-darwin16-clang
+            ;;
+        windows_amd64)
+            cc=x86_64-w64-mingw32-gcc
+            tags="$tags,timetzdata"
+            buildmode=exe
             ;;
         *)
-            >&2 echo Error: unknown OS $(go env GOOS)
+            >&2 echo Error: unsupported OS_ARCH pair "'$os_arch'"
             exit 1
             ;;
     esac
+
+    CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=${cc} go build \
+        -tags "$tags" \
+        -buildmode "$buildmode" \
+        -ldflags "$ldflags" \
+        -o "$out_dir/" \
+        "$pkg"
 }
 
 main ${@}
