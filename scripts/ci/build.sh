1103813ca44e14feb9b54999c47eb0b7394b8ab0
Daniel Moran <danxmoran@gmail.com>
2021-11-16 09:57:46 -0500

    build: switch to build-version.sh from image, rewrite build.sh

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-declare -r SCRIPT_DIR=$(cd $(dirname $0) > /dev/null && pwd)
-
 declare -r LINUX_EXTLD="-fno-PIC -static -Wl,-z,stack-size=8388608"
 
 function main () {
@@ -11,50 +9,53 @@ function main () {
         exit 1
     fi
     local -r out_dir=$1 build_type=$2 pkg=$3
-    local -r version="$(${SCRIPT_DIR}/build-version.sh "$build_type")"
+    local -r version="$(build-version.sh "$build_type")"
 
     mkdir -p "$out_dir"
 
     local -r commit=$(git rev-parse --short HEAD)
     local -r build_date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 
-    local tags=assets,sqlite_foreign_keys,sqlite_json
-    local buildmode=pie
-    local ldflags="-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date}"
-    local cc
-
+    # NOTE: This code is purposefully repetitive, to enable easier copy-paste of individual build commands.
     local -r os_arch="$(go env GOOS)_$(go env GOARCH)"
     case "$os_arch" in
         linux_amd64)
-            cc=musl-gcc
-            ldflags="$ldflags -extldflags '$LINUX_EXTLD'"
-            tags="$tags,static_build"
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+                -tags assets,sqlite_foreign_keys,sqlite_json,static_build \
+                -buildmode pie \
+                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date} -extldflags '$LINUX_EXTLD'" \
+                -o "$out_dir/" \
+                "$pkg"
             ;;
         linux_arm64)
-            cc=aarch64-unknown-linux-musl-gcc
-            ldflags="$ldflags -extldflags '$LINUX_EXTLD'"
-            tags="$tags,static_build,noasm"
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+                -tags assets,sqlite_foreign_keys,sqlite_json,static_build,noasm \
+                -buildmode pie \
+                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date} -extldflags '$LINUX_EXTLD'" \
+                -o "$out_dir/" \
+                "$pkg"
             ;;
         darwin_amd64)
-            cc=x86_64-apple-darwin16-clang
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+                -tags assets,sqlite_foreign_keys,sqlite_json \
+                -buildmode pie \
+                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date}" \
+                -o "$out_dir/" \
+                "$pkg"
             ;;
         windows_amd64)
-            cc=x86_64-w64-mingw32-gcc
-            tags="$tags,timetzdata"
-            buildmode=exe
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+                -tags assets,sqlite_foreign_keys,sqlite_json,timetzdata \
+                -buildmode exe \
+                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date}" \
+                -o "$out_dir/" \
+                "$pkg"
             ;;
         *)
             >&2 echo Error: unsupported OS_ARCH pair "'$os_arch'"
             exit 1
             ;;
     esac
-
-    CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=${cc} go build \
-        -tags "$tags" \
-        -buildmode "$buildmode" \
-        -ldflags "$ldflags" \
-        -o "$out_dir/" \
-        "$pkg"
 }
 
 main ${@}
