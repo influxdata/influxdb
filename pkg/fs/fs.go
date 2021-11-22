c00076028112f02b13dbe5ed6f7205cda0df9a6a
Chris Pahl <sahib@online.de>
2021-11-22 13:46:59 +0100

    fix: use copy when a rename spans volumes
    When a file rename fails with EXDEV
    (cross device or volume error), copy the
    file and delete the original instead.
    
    closes https://github.com/influxdata/influxdb/issues/22890

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -2,6 +2,10 @@ package fs
 
 import (
 	"fmt"
+	"io"
+	"os"
+
+	"github.com/influxdata/influxdb/v2/pkg/errors"
 )
 
 // A FileExistsError is returned when an operation cannot be completed due to a
@@ -24,4 +28,39 @@ type DiskStatus struct {
 	Used  uint64
 	Free  uint64
 	Avail uint64
+}
+
+func copyFile(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	defer errors.Capture(&err, out.Close)()
+
+	defer errors.Capture(&err, in.Close)()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+
+	return out.Sync()
+}
+
+// MoveFileWithReplacement copies the file contents at `src` to `dst`.
+// and deletes `src` on success.
+//
+// If the file at `dst` already exists, it will be truncated and its contents
+// overwritten.
+func MoveFileWithReplacement(src, dst string) error {
+	if err := copyFile(src, dst); err != nil {
+		return fmt.Errorf("copy: %w", err)
+	}
+
+	return os.Remove(src)
 }
