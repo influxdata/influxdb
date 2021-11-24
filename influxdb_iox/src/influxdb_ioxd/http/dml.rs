539c08125b0dab8dccd00416ab03dfd4122c2cc3
Marco Neumann <marco@crepererum.net>
2021-11-24 09:43:12 +0100

    feat: HTTP delete support for router
    Closes #3154.

--- influxdb_iox/src/influxdb_ioxd/http/dml.rs
+++ influxdb_iox/src/influxdb_ioxd/http/dml.rs
@@ -4,11 +4,13 @@ use async_trait::async_trait;
 use chrono::Utc;
 use data_types::{
     names::{org_and_bucket_to_database, OrgBucketMappingError},
+    non_empty::NonEmptyString,
     DatabaseName,
 };
-use dml::{DmlMeta, DmlWrite};
+use dml::{DmlDelete, DmlMeta, DmlOperation, DmlWrite};
 use hyper::{Body, Method, Request, Response, StatusCode};
 use observability_deps::tracing::debug;
+use predicate::delete_predicate::{parse_delete_predicate, parse_http_delete_request};
 use serde::Deserialize;
 use snafu::{OptionExt, ResultExt, Snafu};
 
@@ -21,9 +23,21 @@ use super::{
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Snafu)]
-pub enum HttpWriteError {
+pub enum HttpDmlError {
     #[snafu(display("Internal error mapping org & bucket: {}", source))]
     BucketMappingError { source: OrgBucketMappingError },
+
+    #[snafu(display(
+        "User error writing points into org {}, bucket {}:  {}",
+        org,
+        bucket_name,
+        source
+    ))]
+    WritingPointsUser {
+        org: String,
+        bucket_name: String,
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
 
     #[snafu(display(
         "Internal error writing points into org {}, bucket {}:  {}",
@@ -31,7 +45,31 @@ pub enum HttpWriteError {
         bucket_name,
         source
     ))]
-    WritingPoints {
+    WritingPointsInternal {
+        org: String,
+        bucket_name: String,
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
+
+    #[snafu(display(
+        "User error writing deleting into org {}, bucket {}:  {}",
+        org,
+        bucket_name,
+        source
+    ))]
+    DeletingPointsUser {
+        org: String,
+        bucket_name: String,
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
+
+    #[snafu(display(
+        "Internal error deleting points into org {}, bucket {}:  {}",
+        org,
+        bucket_name,
+        source
+    ))]
+    DeletingPointsInternal {
         org: String,
         bucket_name: String,
         source: Box<dyn std::error::Error + Send + Sync>,
@@ -55,37 +93,66 @@ pub enum HttpWriteError {
     ParsingLineProtocol { source: mutable_batch_lp::Error },
 
     #[snafu(display("Database {} not found", db_name))]
-    DatabaseNotFound { db_name: String },
+    NotFoundDatabase { db_name: String },
+
+    #[snafu(display("Table {}:{} not found", db_name, table_name))]
+    NotFoundTable { db_name: String, table_name: String },
 
     #[snafu(display("Cannot parse body: {}", source))]
     ParseBody {
         source: crate::influxdb_ioxd::http::utils::ParseBodyError,
     },
+
+    #[snafu(display("Error parsing delete {}: {}", input, source))]
+    ParsingDelete {
+        source: predicate::delete_predicate::Error,
+        input: String,
+    },
+
+    #[snafu(display("Error building delete predicate {}: {}", input, source))]
+    BuildingDeletePredicate {
+        source: predicate::delete_predicate::Error,
+        input: String,
+    },
 }
 
-impl HttpApiErrorSource for HttpWriteError {
+impl HttpApiErrorSource for HttpDmlError {
     fn to_http_api_error(&self) -> HttpApiError {
         match self {
             e @ Self::BucketMappingError { .. } => e.internal_error(),
-            e @ Self::WritingPoints { .. } => e.internal_error(),
+            e @ Self::WritingPointsInternal { .. } => e.internal_error(),
+            e @ Self::WritingPointsUser { .. } => e.invalid(),
+            e @ Self::DeletingPointsInternal { .. } => e.internal_error(),
+            e @ Self::DeletingPointsUser { .. } => e.invalid(),
             e @ Self::ExpectedQueryString { .. } => e.invalid(),
             e @ Self::InvalidQueryString { .. } => e.invalid(),
             e @ Self::ReadingBodyAsUtf8 { .. } => e.invalid(),
             e @ Self::ParsingLineProtocol { .. } => e.invalid(),
-            e @ Self::DatabaseNotFound { .. } => e.not_found(),
+            e @ Self::NotFoundDatabase { .. } => e.not_found(),
+            e @ Self::NotFoundTable { .. } => e.not_found(),
             Self::ParseBody { source } => source.to_http_api_error(),
+            e @ Self::ParsingDelete { .. } => e.invalid(),
+            e @ Self::BuildingDeletePredicate { .. } => e.invalid(),
         }
     }
 }
 
 /// Write error when calling the underlying server type.
 #[derive(Debug, Snafu)]
-pub enum InnerWriteError {
+pub enum InnerDmlError {
     #[snafu(display("Database {} not found", db_name))]
-    NotFound { db_name: String },
+    DatabaseNotFound { db_name: String },
 
-    #[snafu(display("Error while writing: {}", source))]
-    OtherError {
+    #[snafu(display("Table {}:{} not found", db_name, table_name))]
+    TableNotFound { db_name: String, table_name: String },
+
+    #[snafu(display("User-provoked error while processing DML request: {}", source))]
+    UserError {
+        source: Box<dyn std::error::Error + Send + Sync>,
+    },
+
+    #[snafu(display("Internal error while processing DML request: {}", source))]
+    InternalError {
         source: Box<dyn std::error::Error + Send + Sync>,
     },
 }
@@ -102,7 +169,7 @@ pub enum RequestOrResponse {
 }
 
 #[async_trait]
-pub trait HttpDrivenWrite: ServerType {
+pub trait HttpDrivenDml: ServerType {
     /// Routes HTTP write requests.
     ///
     /// Returns `RequestOrResponse::Response` if the request was routed,
@@ -110,7 +177,7 @@ pub trait HttpDrivenWrite: ServerType {
     async fn route_write_http_request(
         &self,
         req: Request<Body>,
-    ) -> Result<RequestOrResponse, HttpWriteError> {
+    ) -> Result<RequestOrResponse, HttpDmlError> {
         if (req.method() != Method::POST) || (req.uri().path() != "/api/v2/write") {
             return Ok(RequestOrResponse::Request(req));
         }
@@ -149,7 +216,7 @@ pub trait HttpDrivenWrite: ServerType {
                         .unwrap(),
                 ));
             }
-            Err(source) => return Err(HttpWriteError::ParsingLineProtocol { source }),
+            Err(source) => return Err(HttpDmlError::ParsingLineProtocol { source }),
         };
 
         debug!(
@@ -164,7 +231,7 @@ pub trait HttpDrivenWrite: ServerType {
 
         let write = DmlWrite::new(tables, DmlMeta::unsequenced(span_ctx));
 
-        match self.write(&db_name, write).await {
+        match self.write(&db_name, DmlOperation::Write(write)).await {
             Ok(_) => {
                 lp_metrics.record_write(
                     &db_name,
@@ -180,14 +247,23 @@ pub trait HttpDrivenWrite: ServerType {
                         .unwrap(),
                 ))
             }
-            Err(InnerWriteError::NotFound { .. }) => {
+            Err(InnerDmlError::DatabaseNotFound { db_name }) => {
                 debug!(%db_name, ?stats, "database not found");
                 // Purposefully do not record ingest metrics
-                Err(HttpWriteError::DatabaseNotFound {
-                    db_name: db_name.to_string(),
+                Err(HttpDmlError::NotFoundDatabase { db_name })
+            }
+            Err(InnerDmlError::TableNotFound {
+                db_name,
+                table_name,
+            }) => {
+                debug!(%db_name, %table_name, ?stats, "table not found");
+                // Purposefully do not record ingest metrics
+                Err(HttpDmlError::NotFoundTable {
+                    db_name,
+                    table_name,
                 })
             }
-            Err(InnerWriteError::OtherError { source }) => {
+            Err(InnerDmlError::UserError { source }) => {
                 debug!(e=%source, %db_name, ?stats, "error writing lines");
                 lp_metrics.record_write(
                     &db_name,
@@ -196,12 +272,135 @@ pub trait HttpDrivenWrite: ServerType {
                     body.len(),
                     false,
                 );
-                Err(HttpWriteError::WritingPoints {
+                Err(HttpDmlError::WritingPointsUser {
                     org: write_info.org.clone(),
                     bucket_name: write_info.bucket.clone(),
                     source,
                 })
             }
+            Err(InnerDmlError::InternalError { source }) => {
+                debug!(e=%source, %db_name, ?stats, "error writing lines");
+                lp_metrics.record_write(
+                    &db_name,
+                    stats.num_lines,
+                    stats.num_fields,
+                    body.len(),
+                    false,
+                );
+                Err(HttpDmlError::WritingPointsInternal {
+                    org: write_info.org.clone(),
+                    bucket_name: write_info.bucket.clone(),
+                    source,
+                })
+            }
+        }
+    }
+
+    /// Routes HTTP delete requests.
+    ///
+    /// Returns `RequestOrResponse::Response` if the request was routed,
+    /// Returns `RequestOrResponse::Response` if the request did not match (and needs to be handled some other way)
+    async fn route_delete_http_request(
+        &self,
+        req: Request<Body>,
+    ) -> Result<RequestOrResponse, HttpDmlError> {
+        if (req.method() != Method::POST) || (req.uri().path() != "/api/v2/delete") {
+            return Ok(RequestOrResponse::Request(req));
+        }
+
+        let span_ctx = req.extensions().get().cloned();
+
+        let max_request_size = self.max_request_size();
+
+        // Extract the DB name from the request
+        // db_name = orrID_bucketID
+        let query = req.uri().query().context(ExpectedQueryString)?;
+        let delete_info: WriteInfo =
+            serde_urlencoded::from_str(query).context(InvalidQueryString {
+                query_string: String::from(query),
+            })?;
+        let db_name = org_and_bucket_to_database(&delete_info.org, &delete_info.bucket)
+            .context(BucketMappingError)?;
+
+        // Parse body
+        let body = parse_body(req, max_request_size).await.context(ParseBody)?;
+        let body = std::str::from_utf8(&body).context(ReadingBodyAsUtf8)?;
+
+        // Parse and extract table name (which can be empty), start, stop, and predicate
+        let parsed_delete =
+            parse_http_delete_request(body).context(ParsingDelete { input: body })?;
+
+        let table_name = parsed_delete.table_name;
+        let predicate = parsed_delete.predicate;
+        let start = parsed_delete.start_time;
+        let stop = parsed_delete.stop_time;
+        debug!(%table_name, %predicate, %start, %stop, body_size=body.len(), %db_name, org=%delete_info.org, bucket=%delete_info.bucket, "delete data from database");
+
+        // Build delete predicate
+        let predicate = parse_delete_predicate(&start, &stop, &predicate)
+            .context(BuildingDeletePredicate { input: body })?;
+
+        let delete = DmlDelete::new(
+            predicate,
+            NonEmptyString::new(table_name),
+            DmlMeta::unsequenced(span_ctx),
+        );
+
+        match self.write(&db_name, DmlOperation::Delete(delete)).await {
+            Ok(_) => Ok(RequestOrResponse::Response(
+                Response::builder()
+                    .status(StatusCode::NO_CONTENT)
+                    .body(Body::empty())
+                    .unwrap(),
+            )),
+            Err(InnerDmlError::DatabaseNotFound { db_name }) => {
+                debug!(%db_name, "database not found");
+                Err(HttpDmlError::NotFoundDatabase { db_name })
+            }
+            Err(InnerDmlError::TableNotFound {
+                db_name,
+                table_name,
+            }) => {
+                debug!(%db_name, %table_name, "table not found");
+                Err(HttpDmlError::NotFoundTable {
+                    db_name,
+                    table_name,
+                })
+            }
+            Err(InnerDmlError::UserError { source }) => {
+                debug!(e=%source, %db_name, "error issuing delete request");
+                Err(HttpDmlError::DeletingPointsUser {
+                    org: delete_info.org.clone(),
+                    bucket_name: delete_info.bucket.clone(),
+                    source,
+                })
+            }
+            Err(InnerDmlError::InternalError { source }) => {
+                debug!(e=%source, %db_name, "error issuing delete request");
+                Err(HttpDmlError::DeletingPointsInternal {
+                    org: delete_info.org.clone(),
+                    bucket_name: delete_info.bucket.clone(),
+                    source,
+                })
+            }
+        }
+    }
+
+    /// Routes HTTP DML requests.
+    ///
+    /// Combines:
+    /// - [`route_delete_http_request`](Self::route_delete_http_request)
+    /// - [`route_write_http_request`](Self::route_write_http_request)
+    ///
+    /// Returns `RequestOrResponse::Response` if the request was routed,
+    /// Returns `RequestOrResponse::Response` if the request did not match (and needs to be handled some other way)
+    async fn route_dml_http_request(
+        &self,
+        req: Request<Body>,
+    ) -> Result<RequestOrResponse, HttpDmlError> {
+        match self.route_delete_http_request(req).await? {
+            RequestOrResponse::Response(resp) => Ok(RequestOrResponse::Response(resp)),
+            RequestOrResponse::Request(req) => self.route_write_http_request(req).await,
         }
     }
 
@@ -211,16 +410,16 @@ pub trait HttpDrivenWrite: ServerType {
     /// Line protocol metrics.
     fn lp_metrics(&self) -> Arc<LineProtocolMetrics>;
 
-    /// Perform write.
+    /// Perform DML operation.
     async fn write(
         &self,
         db_name: &DatabaseName<'_>,
-        write: DmlWrite,
-    ) -> Result<(), InnerWriteError>;
+        op: DmlOperation,
+    ) -> Result<(), InnerDmlError>;
 }
 
 #[derive(Debug, Deserialize)]
-/// Body of the request to the /write endpoint
+/// Body of the request to the dml endpoints
 pub struct WriteInfo {
     pub org: String,
     pub bucket: String,
@@ -511,6 +710,90 @@ pub mod test_utils {
             assert_eq!(request_count_client_error.fetch(), 0);
             assert_eq!(request_count_server_error.fetch(), 1);
         }
+    }
+
+    pub async fn assert_delete_unknown_database<T>(test_server: TestServer<T>)
+    where
+        T: ServerType,
+    {
+        // delete from non-existing table
+        let client = Client::new();
+        let delete_line = r#"{"start":"2021-04-01T14:00:00Z","stop":"2021-04-02T14:00:00Z", "predicate":"_measurement=not_a_table and location=Boston"}"#;
+        let bucket_name = "MyBucket";
+        let org_name = "NotMyOrg";
+        let response = client
+            .post(&format!(
+                "{}/api/v2/delete?bucket={}&org={}",
+                test_server.url(),
+                bucket_name,
+                org_name
+            ))
+            .body(delete_line)
+            .send()
+            .await;
+        check_response(
+            "delete",
+            response,
+            StatusCode::NOT_FOUND,
+            Some("Database NotMyOrg_MyBucket not found"),
+        )
+        .await;
+    }
+
+    pub async fn assert_delete_unknown_table<T>(test_server: TestServer<T>)
+    where
+        T: ServerType,
+    {
+        // delete from non-existing table
+        let client = Client::new();
+        let delete_line = r#"{"start":"2021-04-01T14:00:00Z","stop":"2021-04-02T14:00:00Z", "predicate":"_measurement=not_a_table and location=Boston"}"#;
+        let bucket_name = "MyBucket";
+        let org_name = "MyOrg";
+        let response = client
+            .post(&format!(
+                "{}/api/v2/delete?bucket={}&org={}",
+                test_server.url(),
+                bucket_name,
+                org_name
+            ))
+            .body(delete_line)
+            .send()
+            .await;
+        check_response(
+            "delete",
+            response,
+            StatusCode::NOT_FOUND,
+            Some("Table MyOrg_MyBucket:not_a_table not found"),
+        )
+        .await;
+    }
+
+    pub async fn assert_delete_bad_request<T>(test_server: TestServer<T>)
+    where
+        T: ServerType,
+    {
+        // Not able to parse _measurement="not_a_table"  (it must be _measurement=\"not_a_table\" to work)
+        let client = Client::new();
+        let delete_line = r#"{"start":"2021-04-01T14:00:00Z","stop":"2021-04-02T14:00:00Z", "predicate":"_measurement="not_a_table" and location=Boston"}"#;
+        let bucket_name = "MyBucket";
+        let org_name = "MyOrg";
+        let response = client
+            .post(&format!(
+                "{}/api/v2/delete?bucket={}&org={}",
+                test_server.url(),
+                bucket_name,
+                org_name
+            ))
+            .body(delete_line)
+            .send()
+            .await;
+        check_response(
+            "delete",
+            response,
+            StatusCode::BAD_REQUEST,
+            Some("Unable to parse delete string"),
+        )
+        .await;
     }
 
     fn gzip_str(s: &str) -> Vec<u8> {
