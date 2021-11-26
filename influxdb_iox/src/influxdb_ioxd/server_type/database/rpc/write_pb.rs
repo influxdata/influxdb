0e06026fbdf68275a8f584d8ee5e7a56d56f395b
Marco Neumann <marco@crepererum.net>
2021-11-26 09:26:41 +0100

    refactor: remove write routing logic from `Server`
    API users should use `Database` instead, there's no need any longer to mirror
    this API in `Server`. Note that `Database` is better than `Db` in this
    case, because the former can also check if we're writing from a write
    buffer and can easily reject unsequenced writes. The `pub` modifiers
    were adjusted to make it impossible to write through `Db` directly.

--- influxdb_iox/src/influxdb_ioxd/server_type/database/rpc/write_pb.rs
+++ influxdb_iox/src/influxdb_ioxd/server_type/database/rpc/write_pb.rs
@@ -1,12 +1,12 @@
 use data_types::DatabaseName;
-use dml::{DmlMeta, DmlWrite};
+use dml::{DmlMeta, DmlOperation, DmlWrite};
 use generated_types::google::{FieldViolation, FieldViolationExt};
 use generated_types::influxdata::pbdata::v1::*;
 use server::{connection::ConnectionManager, Server};
 use std::fmt::Debug;
 use std::sync::Arc;
 
-use super::error::default_server_error_handler;
+use super::error::{default_database_write_error_handler, default_server_error_handler};
 
 struct PBWriteService<M: ConnectionManager> {
     server: Arc<Server<M>>,
@@ -27,9 +27,6 @@ where
             .database_batch
             .ok_or_else(|| FieldViolation::required("database_batch"))?;
 
-        let db_name = DatabaseName::new(&database_batch.database_name)
-            .scope("database_batch.database_name")?;
-
         let tables =
             mutable_batch_pb::decode::decode_database_batch(&database_batch).map_err(|e| {
                 FieldViolation {
@@ -40,10 +37,17 @@ where
 
         let write = DmlWrite::new(tables, DmlMeta::unsequenced(span_ctx));
 
-        self.server
-            .write(&db_name, write)
-            .await
+        let db_name = DatabaseName::new(&database_batch.database_name)
+            .scope("database_batch.database_name")?;
+        let database = self
+            .server
+            .active_database(&db_name)
             .map_err(default_server_error_handler)?;
+
+        database
+            .route_operation(&DmlOperation::Write(write))
+            .await
+            .map_err(default_database_write_error_handler)?;
 
         Ok(tonic::Response::new(WriteResponse {}))
     }
--- server/src/resolver.rs
+++ server/src/resolver.rs
@@ -14,10 +14,6 @@ impl RemoteTemplate {
         let template = template.into();
         Self { template }
     }
-
-    fn get(&self, id: &ServerId) -> GrpcConnectionString {
-        self.template.replace("{id}", &format!("{}", id.get_u32()))
-    }
 }
 
 /// A gRPC connection string.
@@ -54,38 +50,5 @@ impl Resolver {
     /// Delete remote server by ID.
     pub fn delete_remote(&mut self, id: ServerId) -> Option<GrpcConnectionString> {
         self.remotes.remove(&id)
-    }
-
-    /// Get remote server by ID.
-    pub fn resolve_remote(&self, id: ServerId) -> Option<GrpcConnectionString> {
-        self.remotes
-            .get(&id)
-            .cloned()
-            .or_else(|| self.remote_template.as_ref().map(|t| t.get(&id)))
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::num::NonZeroU32;
-
-    #[test]
-    fn resolve_remote() {
-        let resolver = Resolver::new(Some(RemoteTemplate::new("http://iox-query-{id}:8082")));
-
-        let server_id = ServerId::new(NonZeroU32::new(42).unwrap());
-        let remote = resolver.resolve_remote(server_id);
-        assert_eq!(
-            remote,
-            Some(GrpcConnectionString::from("http://iox-query-42:8082"))
-        );
-
-        let server_id = ServerId::new(NonZeroU32::new(24).unwrap());
-        let remote = resolver.resolve_remote(server_id);
-        assert_eq!(
-            remote,
-            Some(GrpcConnectionString::from("http://iox-query-24:8082"))
-        );
     }
 }
