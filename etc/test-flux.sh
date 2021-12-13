372b59096c27bc5e55df184f15f85e5c860b23ab
Sean Brickley <sean@wabr.io>
2021-12-13 16:12:46 -0500

    chore: use new cross-builder image and skip tests

--- etc/test-flux.sh
+++ etc/test-flux.sh
@@ -31,9 +31,14 @@ build_test_harness() {
   "$GO" build -o fluxtest ./internal/cmd/fluxtest-harness-influxdb
 }
 
+# Many tests targeting 3rd party databases are not yet supported in CI and should be filtered out.
+DB_INTEGRATION_WRITE_TESTS=integration_sqlite_write_to,integration_vertica_write_to,integration_mssql_write_to,integration_mysql_write_to,integration_mariadb_write_to,integration_pg_write_to
+DB_INTEGRATION_READ_TESTS=integration_sqlite_read_from_seed,integration_sqlite_read_from_nonseed,integration_vertica_read_from_seed,integration_vertica_read_from_nonseed,integration_mssql_read_from_seed,integration_mssql_read_from_nonseed,integration_mariadb_read_from_seed,integration_mariadb_read_from_nonseed,integration_mysql_read_from_seed,integration_mysql_read_from_nonseed,integration_pg_read_from_seed,integration_pg_read_from_nonseed
+DB_TESTS="${DB_INTEGRATION_WRITE_TESTS},${DB_INTEGRATION_READ_TESTS}"
+
 run_integration_tests() {
   log "Running integration tests..."
-  ./fluxtest -v -p flux.zip -p query/
+  ./fluxtest -v -p flux.zip -p query/ --skip "${DB_TESTS}",group_one_tag_first,group_all_filter_field_first,group_one_tag_filter_field_first,group_one_tag_last,group_all_filter_field_last,group_one_tag_filter_field_last
 }
 
 cleanup() {
