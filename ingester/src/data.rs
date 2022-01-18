23290fd2ff14f466aa15b2a843879e6e758925d2
NGA-TRAN <nga-tran@live.com>
2022-01-18 14:04:07 -0500

    fix: new data structures suggested by reviewers

--- ingester/src/data.rs
+++ ingester/src/data.rs
@@ -1,149 +1,123 @@
-
-
-//! Data for the lifecycle of the ingeter
+//! Data for the lifecycle of the Ingeter
 //! 
 
 use std::{sync::Arc, collections::BTreeMap};
 
-use mutable_batch::MutableBatch;
+use parking_lot::RwLock;
+use arrow::datatypes::DataType;
 
-//                                                 ┌──────────────┐                                                               
-//                                                 │Ingester Data │                                                               
-//                                                 │ (in memory)  │                                                               
-//                                                 └──────────────┘                                                               
-//                                                         │                                                                      
-//                                          ┌──────────────┼───────────────┐                                                      
-//                                          ▼              ▼               ▼                                                      
-//                                    ┌───────────┐                 ┌────────────┐                                                
-//                                    │Sequencer 1│       ...       │Sequencer m │                 Sequencers                     
-//                                    └───────────┘                 └────────────┘     a map of sequencer_id to Namespaces        
-//                                          │                              │                                                      
-//                           ┌──────────────┼─────────────┐                │                                                      
-//                           ▼              ▼             ▼                ▼                                                      
-//                     ┌────────────┐               ┌───────────┐                                   Namespaces                    
-//                     │Namespace 1 │     ...       │Namespace n│         ...           a map of namespace_name to Tables         
-//                     └────────────┘               └───────────┘                                                                 
-//                            │                           │                                                                       
-//             ┌──────────────┼──────────────┐            │                                                                       
-//             ▼              ▼              ▼            ▼                                                                       
-//      ┌────────────┐                ┌────────────┐                                                Tables                        
-//      │  Table 1   │       ...      │  Table p   │     ...                           a map of table_name to Partitions          
-//      └────────────┘                └────────────┘                                                                              
-//             │                             │                                                                                    
-//             │              ┌──────────────┼──────────────┐                                                                     
-//             ▼              ▼              ▼              ▼                                                                     
-//                     ┌────────────┐                ┌────────────┐                                   Partitions                  
-//            ...      │Partition 1 │       ...      │Partition q │                    a map of partition_key to PartitionData    
-//                     │(2021-12-10)│                │(2021-12-20)│                                                               
-//                     └────────────┘                └──────┬─────┘                                                               
-//                            │                             │                                                                     
-//       ┌───────────┬────────▼────┬─────────────┐          │                                                                     
-//       │           │             │             │          ▼                                                                     
-//       ▼           ▼             ▼             ▼                                                                                
-// ┌──────────┐┌───────────┐ ┌───────────┐ ┌───────────┐   ...                                                                    
-// │ Writing  ││  Snaphot  │ │Persisting │ │ Persisted │                        PartitionData: a struct of 4 items                
-// │Partition ││ Partition │ │ Partition │ │ Partition │                          . A `Writing Partition Batch`                   
-// │  Batch   ││  Batch 1  │ │  Batch 1  │ │  Batch 1  │                          . A vector of `Snapshot Partition Batches`      
-// └──────────┘├───────────┤ ├───────────┤ ├───────────┤                          . A vector of `Persisting Partition Batches`    
-//             │    ...    │ │    ...    │ │    ...    │                          . A vector of `Persisted Partition batches`     
-//             │           │ │           │ │           │                                                                          
-//             ├───────────┤ ├───────────┤ ├───────────┤                        1:1 map between `Snapshot`                        
-//             │ Snapshot  │ │Persisting │ │ Persisted │                        and `Persisting` Partition Batches                
-//             │ Partition │ │ Partition │ │ Partition │                                                                          
-//             │  Batch k  │ │  Batch k  │ │  Batch i  │                                                                          
-//             └───────────┘ └───────────┘ └───────────┘                                                                          
-
-// All sequencers aiisgned to this Ingester
-#[derive(Debug, Clone)]
-pub struct Sequencers {
-    // A map between a sequencer id to its corresponding Namespaces.
-    // A sequencer id is a `kafka_partittion`, a i32 defined in iox_catalog's Sequencer and 
-    // represents a shard of data of a Table of a Namesapce. Namespace is equivalent to 
-    // a customer db (aka an org's bucket). Depending on the comfiguration of sharding a table,
-    // either full data or set of rows of data of the table are included in a shard.
-    sequencers : BTreeMap<i32, Vec<Namespace>>,
+// Ingetser's setup: place to keep its Kafka Topic & Sequencer IDs
+struct IngesterProfile {
+    // kafka_topic: 
+    // sequencer_ids: 
 }
 
-// A Namespace and all of its tables of a sequencer
-#[derive(Debug, Clone)]
-pub struct Namespace {
-    // Name of the namespace which is unique and represents a customer db.
-    name: String,
+/// Ingester Data: a Mapp of Shard ID to its Data
+struct Sequencers {
+    // This map gets set up on initialization of the ingester so it won't ever be modified.
+    // The content of each SequenceData will get changed when more namespaces and tables 
+    // get ingested.
+    data: BTreeMap<i32, Arc<SequencerData>>,
+  }
 
-    // Tables of this namesapce
-    tables : Vec<Table>,
+impl Sequencers {
+    /// One time initilize Sequencers of this Ingester
+    pub fn initialize() -> Self {
+    }
 }
+  
+  /// Data of a Shard
+  struct SequencerData {
+    // New namespaces can come in at any time so we need to be able to add new ones
+    namespaces: RwLock<BTreeMap<i32, Arc<NamespaceData>>>,
+  }
 
-// A Table and all of its partittion
-#[derive(Debug, Clone)]
-pub struct Table {
-    // table name
-    name: String,
+  impl SequencerData {
+      pub fn new(seq_id: i32) -> Self {
 
-    // A map of partittion_key to its corresponding partition
-    partitions : BTreeMap<String, Partition>,
-}
+      }
+  }
+  
+  /// Data of a Namespace that belongs to a given Shard
+  struct NamespaceData {
+    tables: RwLock<BTreeMap<i64, Arc<TableData>>>,
+  }
+  
 
-// A Partittion and all of its in-memory data batches
-//
-// Stages of a batch of a partition:
-//  . A partition has only one `Writing Batch`. When is it big or 
-//    old enough, defined by IngesterPersistenceSettings, it will
-//    be put to `Snaphot Batch` and also copied to `Pesisting Batch`.
-//    The new and empty Wrtiting Batch will be created for accpeting new writes
-//  . Snapshot and Persisting batches are 1:1 mapped at all times. Snapshot ones are 
-//    immutable and used for querying. Persisting ones are modified to sort, 
-//    dedupilcate, and apply tombstone and then persited to parquet files. 
-//    While many batches can be persisted at the same time, a batch is only marked 
-//    in the catalog to be persisted after the batches before 
-//    its in the queue are marked persisted.
-//  . After the batch are marked persisted in the catalog, its will be removed 
-//    from Sanpshot and Persisting and put in Persisted. The Persisted ones 
-//    will get evicted based on IngesterPersistenceSettings.
-//                       ┌───────────────────┐                      
-//                       │    Persisting     │                      
-//                       │                   │                      
-//                       │ ┌───────────────┐ │                      
-// ┌────────────┐        │ │   Snapshot    │ │        ┌────────────┐
-// │  Writing   │───────▶│ └───────────────┘ │───────▶│ Persisted  │
-// └────────────┘        │ ┌───────────────┐ │        └────────────┘
-//                       │ │   Persiting   │ │                      
-//                       │ └───────────────┘ │                      
-//                       └───────────────────┘                      
-// 
-#[derive(Debug, Clone)]
-pub struct Partition {
+  /// Data of a Table in a given Namesapce that belongs to a given Shard
+  struct TableData {
+    partitions: RwLock<BTreeMap<i64, Arc<PartitionData>>>,
+  }
+
+  /// Data of an IOx Partition of a given Table of a Namesapce that belongs to a given Shard
+  struct PartitionData {
+    /// Key of this partition
     partition_key: String,
+    /// Data
+    inner: RwLock<DataBuffer>,
+  }
+  
+/// Data of an IOx partition split into batches
+//                       ┌────────────────────────┐            ┌────────────────────────┐
+//                       │       Snapshots        │            │       Persisting       │
+//                       │                        │            │                        │
+//                       │    ┌───────────────┐   │            │   ┌───────────────┐    │
+//                       │   ┌┴──────────────┐│   │            │   │  Persisting   │    │
+//                       │  ┌┴──────────────┐├┴───┼────────────┼──▶│     Data      │    │
+//                       │  │   Snapshot    ├┘    │            │   └───────────────┘    │
+//                       │  └───────────────┘     │            │                        │
+// ┌────────────┐        │                        │            │         ...            │
+// │   Buffer   │───────▶│          ...           │            │                        │
+// └────────────┘        │                        │            │                        │
+//                       │   ┌───────────────┐    │            │    ┌───────────────┐   │
+//                       │  ┌┴──────────────┐│    │            │    │  Persisting   │   │
+//                       │ ┌┴──────────────┐├┴────┼────────────┼───▶│     Data      │   │
+//                       │ │   Snapshot    ├┘     │            │    └───────────────┘   │
+//                       │ └───────────────┘      │            │                        │
+//                       │                        │            │                        │
+//                       └────────────────────────┘            └────────────────────────┘
+  struct DataBuffer {
 
-    // Writing batch that accepts writes to this partition
-    writing_batch: PartitionBatch,
+    /// Buffer of ingesting data
+    buffer: Vec<DataBatch>,
 
-    // Queue of batches that are immutable and used for querying only.
-    // The batches are queue contiguously in thier data arrival time
-    snapshot_batches: Vec<PartitionBatch>,  // todo: is Vec good enough for hanlding queue?
+    /// Data in `buffer` will be moved to a `snapshot` when one of these happens:
+    ///  . A background persist is called
+    ///  . A read request from Querier
+    /// The `buffer` will be empty when this happens.
+    snapshots: Vec<Arc<DataBatch>>,
 
-    // Queue of persisting batches which is a one on one mapping with the snapshot_batches.
-    // Data of these batches will be modified to sort, dedupilcate, and apply tombstone and then 
-    // persited to parquet files. While many batches can be persisted at the same time, 
-    // a batch is only marked in the catalog to be persisted after the batches before 
-    // its in the queue are marked persisted
-    pesisting_batched: Vec<PartitionBatch>,
+    /// When a persist is called, data in `buffer` will be moved to a `snapshot`
+    /// and then all `snapshots` will be moved to a `persisting`.
+    /// Both `buffer` and 'snaphots` will be empty when this happens.
+    persisting: Vec<PersistingData>,
 
-    // Persisted batches that are not yet evicted from the in-memory.
-    // These are batches moved from persiting_batches after they are fully persisted and marked 
-    // so in the catalog
-    pesisted_batched: Vec<PartitionBatch>,
-
-}
-
-// A PartitionBatch of contiguous in arrival time of writes
-// todo & question: do we want to call this Chunk instead?
-#[derive(Debug, Clone)]
-pub struct PartitionBatch {
-    // To keep the PartitionBtach in order of their 
-    // arrived data, we may need this auto created batch id
-    batch_id: i32,
-
-    // Data of this partition batch
-    data: Arc<MutableBatch>,
-}
+    // Extra Notes:
+    //  . Multiple perssiting operations may be happenning concurrently but
+    //    their persisted info must be added into the Catalog in thier data
+    //    ingesting order.
+    //  . When a read request comes from a Querier, all data from `snaphots` 
+    //    and `persisting` must be sent to the Querier.
+    //  . After the `persiting` data is persisted and successfully added 
+    //    into the Catalog, it will be removed from this Data Buffer.
+    //    This data might be added into an extra cache to serve up to 
+    //    Queriers that may not have loaded the parquet files from object 
+    //    storage yet. But this will be decided after MVP.
+  }
+  
+  struct PersistingData {
+    batches: Vec<Arc<DataBatch>>
+  }
+  
+  struct DataBatch {
+    // a map of the unique column name to its data. Every column
+    // must have the same number of values.
+    column_data: BTreeMap<i64, ColumnData<DataType>>,
+  }
+  
+  struct ColumnData<T> {
+    // it might be better to have the raw values and null markers, 
+    // but this will probably be easier and faster to get going.
+    values: Option<T>
+  }
+  
--- ingester/src/lib.rs
+++ ingester/src/lib.rs
@@ -2,4 +2,15 @@
 //! Design doc: https://docs.google.com/document/d/14NlzBiWwn0H37QxnE0k3ybTU58SKyUZmdgYpVw6az0Q/edit#
 //!
 
+#![deny(rustdoc::broken_intra_doc_links, rustdoc::bare_urls, rust_2018_idioms)] 
+#![warn( 
+    missing_copy_implementations, 
+    missing_debug_implementations, 
+    missing_docs, 
+    clippy::explicit_iter_loop, 
+    clippy::future_not_send, 
+    clippy::use_self, 
+    clippy::clone_on_ref_ptr 
+)] 
+
 pub mod data;
