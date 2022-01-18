b20d1757d0c6219f403910e020a15352479161b6
NGA-TRAN <nga-tran@live.com>
2022-01-18 17:43:03 -0500

    feat: initialize ingester data

--- ingester/src/data.rs
+++ ingester/src/data.rs
@@ -1,91 +1,100 @@
 //! Data for the lifecycle of the Ingeter
-//! 
+//!
 
-use std::{sync::Arc, collections::BTreeMap};
+use std::{collections::BTreeMap, sync::Arc};
 
 use crate::server::IngesterServer;
-use snafu::{OptionExt, Snafu};
-use iox_catalog::interface::{KafkaTopicId, SequencerId, RepoCollection, KafkaTopic, NamespaceId};
-use parking_lot::RwLock;
 use arrow::datatypes::DataType;
+use iox_catalog::interface::{NamespaceId, SequencerId, RepoCollection, KafkaPartition};
+use parking_lot::RwLock;
+use snafu::{Snafu, ResultExt};
 
 #[derive(Debug, Snafu)]
-//#[allow(missing_copy_implementations, missing_docs)]
+#[allow(missing_copy_implementations, missing_docs)]
 pub enum Error {
     #[snafu(display("Topic {} not found", name))]
-    TopicNotFound { name: String },
+    TopicNotFound { 
+        source: iox_catalog::interface::Error,
+        name: String},
+
+    #[snafu(display("Sequencer id {} not found", id.get()))]
+    SequencerNotFound { 
+        source: iox_catalog::interface::Error,
+        id: KafkaPartition},
+    
 }
 
 /// A specialized `Error` for Ingester Data errors
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-
 /// Ingester Data: a Mapp of Shard ID to its Data
 struct Sequencers {
     // This map gets set up on initialization of the ingester so it won't ever be modified.
-    // The content of each SequenceData will get changed when more namespaces and tables 
+    // The content of each SequenceData will get changed when more namespaces and tables
     // get ingested.
     data: BTreeMap<SequencerId, Arc<SequencerData>>,
-  }
+}
 
 impl Sequencers {
     /// One time initialize Sequencers of this Ingester
-    pub async fn initialize(ingester: &IngesterServer) -> Result<Self> {
-        // Get kafka topic
+    pub async fn initialize(ingester: &IngesterServer<'_>) -> Result<Self> {
+        // Get kafka topic from the catalog
+        let topic_name = ingester.get_topic();
         let kafka_topic_repro = ingester.iox_catalog.kafka_topic();
-        let topic = kafka_topic_repro.create_or_get(ingester.kafka_topic_name).await?;
+        let topic = kafka_topic_repro
+            .create_or_get(topic_name.as_str())
+            .await
+            .context(TopicNotFoundSnafu{name: topic_name})?;
 
-        // Get all namespaces of this topic
-        let namespace_repo = ingester.iox_catalog.namespace();
-        let x = namespace_repro.
-
-
-        // Get Sequencers
+        // Get sequencer ids from the catalog
         let sequencer_repro = ingester.iox_catalog.sequencer();
-        let sequencers = BTreeMap::default();
-        for shard in ingester.kafka_partitions {
-            let sequencer = sequencer_repro.create_or_get(&topic, shard).await?;
-
-            sequencers.insert(sequencer.id, )
+        let mut sequencers = BTreeMap::default();
+        for shard in ingester.get_kafka_partitions() {
+            let sequencer = sequencer_repro
+                .create_or_get(&topic, shard)
+                .await
+                .context(SequencerNotFoundSnafu{id: shard})?;
+            // Create empty buffer for each sequencer
+            sequencers.insert(sequencer.id, Arc::new(SequencerData::new()));
         }
 
-        Ok(Self {
-            data: BTreeMap::default(),
-        })
+        Ok(Self { data: sequencers })
     }
 }
-  
-  /// Data of a Shard
-  struct SequencerData {
+
+/// Data of a Shard
+struct SequencerData {
     // New namespaces can come in at any time so we need to be able to add new ones
     namespaces: RwLock<BTreeMap<NamespaceId, Arc<NamespaceData>>>,
-  }
+}
 
-  impl SequencerData {
-      pub fn new(seq_id: i32) -> Self {
+impl SequencerData {
+    /// Create an empty SequenceData
+    pub fn new() -> Self {
+        Self {
+            namespaces: RwLock::new(BTreeMap::default()),
+        }
+    }
+}
 
-      }
-  }
-  
-  /// Data of a Namespace that belongs to a given Shard
-  struct NamespaceData {
+/// Data of a Namespace that belongs to a given Shard
+struct NamespaceData {
     tables: RwLock<BTreeMap<i64, Arc<TableData>>>,
-  }
-  
+}
 
-  /// Data of a Table in a given Namesapce that belongs to a given Shard
-  struct TableData {
+/// Data of a Table in a given Namesapce that belongs to a given Shard
+struct TableData {
     partitions: RwLock<BTreeMap<i64, Arc<PartitionData>>>,
-  }
+}
 
-  /// Data of an IOx Partition of a given Table of a Namesapce that belongs to a given Shard
-  struct PartitionData {
+/// Data of an IOx Partition of a given Table of a Namesapce that belongs to a given Shard
+struct PartitionData {
     /// Key of this partition
     partition_key: String,
     /// Data
     inner: RwLock<DataBuffer>,
-  }
-  
+}
+
 /// Data of an IOx partition split into batches
 //                       ┌────────────────────────┐            ┌────────────────────────┐
 //                       │       Snapshots        │            │       Persisting       │
@@ -105,8 +114,7 @@ impl Sequencers {
 //                       │ └───────────────┘      │            │                        │
 //                       │                        │            │                        │
 //                       └────────────────────────┘            └────────────────────────┘
-  struct DataBuffer {
-
+struct DataBuffer {
     /// Buffer of ingesting data
     buffer: Vec<DataBatch>,
 
@@ -120,33 +128,31 @@ impl Sequencers {
     /// and then all `snapshots` will be moved to a `persisting`.
     /// Both `buffer` and 'snaphots` will be empty when this happens.
     persisting: Vec<PersistingData>,
-
     // Extra Notes:
     //  . Multiple perssiting operations may be happenning concurrently but
     //    their persisted info must be added into the Catalog in thier data
     //    ingesting order.
-    //  . When a read request comes from a Querier, all data from `snaphots` 
+    //  . When a read request comes from a Querier, all data from `snaphots`
     //    and `persisting` must be sent to the Querier.
-    //  . After the `persiting` data is persisted and successfully added 
+    //  . After the `persiting` data is persisted and successfully added
     //    into the Catalog, it will be removed from this Data Buffer.
-    //    This data might be added into an extra cache to serve up to 
-    //    Queriers that may not have loaded the parquet files from object 
+    //    This data might be added into an extra cache to serve up to
+    //    Queriers that may not have loaded the parquet files from object
     //    storage yet. But this will be decided after MVP.
-  }
-  
-  struct PersistingData {
-    batches: Vec<Arc<DataBatch>>
-  }
-  
-  struct DataBatch {
+}
+
+struct PersistingData {
+    batches: Vec<Arc<DataBatch>>,
+}
+
+struct DataBatch {
     // a map of the unique column name to its data. Every column
     // must have the same number of values.
     column_data: BTreeMap<i64, ColumnData<DataType>>,
-  }
-  
-  struct ColumnData<T> {
-    // it might be better to have the raw values and null markers, 
+}
+
+struct ColumnData<T> {
+    // it might be better to have the raw values and null markers,
     // but this will probably be easier and faster to get going.
-    values: Option<T>
-  }
-  
+    values: Option<T>,
+}
--- ingester/src/server.rs
+++ ingester/src/server.rs
@@ -1,22 +1,31 @@
 use std::sync::Arc;
 
-use iox_catalog::mem::MemCatalog;
+use iox_catalog::{mem::MemCatalog, interface::KafkaPartition};
 
-/// The [`IngesterServer`] manages the lifecycle and contains all state for 
+/// The [`IngesterServer`] manages the lifecycle and contains all state for
 /// an `ingester` server instance.
 #[derive(Debug)]
-struct IngesterServer<'a> {
+pub struct IngesterServer<'a> {
     pub kafka_topic_name: String,
-    pub kafka_partitions: Vec<i32>, // todo: use KafkaPartitionId when available
-    pub iox_catalog: &'a Arc<MemCatalog>
+    pub kafka_partitions: Vec<KafkaPartition>, // todo: use KafkaPartitionId when available
+    pub iox_catalog: &'a Arc<MemCatalog>,
 }
 
-impl<'a> IngesterServer<'a>{
-    pub fn new(topic_name: String, shard_ids: Vec<i32>, catalog: &'a Arc<MemCatalog>) -> Self {
+impl<'a> IngesterServer<'a> {
+    pub fn new(topic_name: String, shard_ids: Vec<KafkaPartition>, catalog: &'a Arc<MemCatalog>) -> Self {
         Self {
             kafka_topic_name: topic_name,
             kafka_partitions: shard_ids,
             iox_catalog: catalog,
         }
+    }
+
+    pub fn get_topic(&self) -> String {
+        self.kafka_topic_name.clone()
+    }
+
+
+    pub fn get_kafka_partitions(&self) -> Vec<KafkaPartition> {
+        self.kafka_partitions.clone()
     }
 }
