011b297f28c95d2d62921d1deb9b6f710332fd88
Luke Bond <lbond@influxdata.com>
2022-01-28 17:44:10 +0000

    feat: more benchmarks of router2 (#3575)

---
+++ router2/benches/e2e.rs
@@ -0,0 +1,88 @@
+use std::collections::BTreeSet;
+use std::sync::Arc;
+
+use criterion::measurement::WallTime;
+use criterion::{criterion_group, criterion_main, BenchmarkGroup, Criterion, Throughput};
+use hyper::{Body, Request};
+use router2::dml_handlers::ShardedWriteBuffer;
+use router2::sequencer::Sequencer;
+use router2::server::http::HttpDelegate;
+use router2::sharder::TableNamespaceSharder;
+use tokio::runtime::Runtime;
+use write_buffer::core::WriteBufferWriting;
+use write_buffer::mock::{MockBufferForWriting, MockBufferSharedState};
+
+// Init a mock write buffer with the given number of sequencers.
+fn init_write_buffer(
+    n_sequencers: u32,
+) -> ShardedWriteBuffer<TableNamespaceSharder<Arc<Sequencer>>> {
+    let time = time::MockProvider::new(time::Time::from_timestamp_millis(668563200000));
+    let write_buffer: Arc<dyn WriteBufferWriting> = Arc::new(
+        MockBufferForWriting::new(
+            MockBufferSharedState::empty_with_n_sequencers(
+                n_sequencers.try_into().expect("cannot have 0 sequencers"),
+            ),
+            None,
+            Arc::new(time),
+        )
+        .expect("failed to init mock write buffer"),
+    );
+
+    let shards: BTreeSet<_> = write_buffer.sequencer_ids();
+    ShardedWriteBuffer::new(
+        shards
+            .into_iter()
+            .map(|id| Sequencer::new(id as _, Arc::clone(&write_buffer)))
+            .map(Arc::new)
+            .collect::<TableNamespaceSharder<_>>(),
+    )
+}
+
+fn setup_server() -> HttpDelegate<ShardedWriteBuffer<TableNamespaceSharder<Arc<Sequencer>>>> {
+    let write_buffer = init_write_buffer(1);
+    HttpDelegate::new(1024, write_buffer)
+}
+
+fn runtime() -> Runtime {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_io()
+        .enable_time()
+        .build()
+        .unwrap()
+}
+
+fn e2e_benchmarks(c: &mut Criterion) {
+    let mut group = c.benchmark_group("e2e");
+
+    let delegate = setup_server();
+    benchmark_e2e(
+        &mut group,
+        &delegate,
+        "https://bananas.example/api/v2/write?org=bananas&bucket=test",
+        "platanos,tag1=A,tag2=B val=42i 123456",
+    );
+
+    group.finish();
+}
+
+fn benchmark_e2e(
+    group: &mut BenchmarkGroup<WallTime>,
+    http_delegate: &HttpDelegate<ShardedWriteBuffer<TableNamespaceSharder<Arc<Sequencer>>>>,
+    uri: &'static str,
+    body_str: &'static str,
+) {
+    group.throughput(Throughput::Bytes(body_str.len() as u64));
+    group.bench_function("e2e", |b| {
+        b.to_async(runtime()).iter(|| {
+            let request = Request::builder()
+                .uri(uri)
+                .method("POST")
+                .body(Body::from(body_str.as_bytes()))
+                .unwrap();
+            http_delegate.route(request)
+        })
+    });
+}
+
+criterion_group!(benches, e2e_benchmarks);
+criterion_main!(benches);
