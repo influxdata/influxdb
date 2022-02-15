5d3b40859954f71978550ee5aa6ee2911cd209cd
Jonathan A. Sternberg <jonathan@influxdata.com>
2022-02-15 14:58:38 -0600

    refactor(query): remove detailed query traces
    We're removing this functionality from flux because it doesn't work
    properly so remove the invocations here.

--- query/stdlib/influxdata/influxdb/source.go
+++ query/stdlib/influxdata/influxdb/source.go
@@ -47,14 +47,7 @@ type Source struct {
 func (s *Source) Run(ctx context.Context) {
 	labelValues := s.m.getLabelValues(ctx, s.orgID, s.op)
 	start := time.Now()
-	var err error
-	if flux.IsQueryTracingEnabled(ctx) {
-		span, ctxWithSpan := tracing.StartSpanFromContextWithOperationName(ctx, "source-"+s.op)
-		err = s.runner.run(ctxWithSpan)
-		span.Finish()
-	} else {
-		err = s.runner.run(ctx)
-	}
+	err := s.runner.run(ctx)
 	s.m.recordMetrics(labelValues, start)
 	for _, t := range s.ts {
 		t.Finish(s.id, err)
