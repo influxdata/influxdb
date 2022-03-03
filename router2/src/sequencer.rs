bb9b140f4b0c73bb7672cb55fc3d2595bf4a519c
Dom Dwyer <dom@itsallbroken.com>
2022-03-03 16:24:51 +0000

    refactor: sequencer metrics
    Records per-sequencer (kafka partition) enqueue latency / counts broken
    down by operation success/error.

--- router2/src/sequencer.rs
+++ router2/src/sequencer.rs
@@ -1,15 +1,21 @@
 //! A representation of a single operation sequencer.
 
-use std::{hash::Hash, sync::Arc};
+use std::{borrow::Cow, hash::Hash, sync::Arc};
 
 use dml::{DmlMeta, DmlOperation};
+use metric::{Metric, U64Histogram, U64HistogramOptions};
+use time::{SystemProvider, TimeProvider};
 use write_buffer::core::{WriteBufferError, WriteBufferWriting};
 
 /// A sequencer tags an write buffer with a sequencer ID.
 #[derive(Debug)]
-pub struct Sequencer {
+pub struct Sequencer<P = SystemProvider> {
     id: usize,
     inner: Arc<dyn WriteBufferWriting>,
+    time_provider: P,
+
+    enqueue_success: U64Histogram,
+    enqueue_error: U64Histogram,
 }
 
 impl Eq for Sequencer {}
@@ -28,8 +34,32 @@ impl Hash for Sequencer {
 
 impl Sequencer {
     /// Tag `inner` with the specified `id`.
-    pub fn new(id: usize, inner: Arc<dyn WriteBufferWriting>) -> Self {
-        Self { id, inner }
+    pub fn new(id: usize, inner: Arc<dyn WriteBufferWriting>, metrics: &metric::Registry) -> Self {
+        let buckets = || {
+            U64HistogramOptions::new([5, 10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, u64::MAX])
+        };
+        let write: Metric<U64Histogram> = metrics.register_metric_with_options(
+            "sequencer_enqueue_duration_ms",
+            "sequencer enqueue call duration in milliseconds",
+            buckets,
+        );
+
+        let enqueue_success = write.recorder([
+            ("shard_id", Cow::from(id.to_string())),
+            ("result", Cow::from("success")),
+        ]);
+        let enqueue_error = write.recorder([
+            ("shard_id", Cow::from(id.to_string())),
+            ("result", Cow::from("error")),
+        ]);
+
+        Self {
+            id,
+            inner,
+            enqueue_success,
+            enqueue_error,
+            time_provider: Default::default(),
+        }
     }
 
     /// Return the ID of this sequencer.
@@ -43,6 +73,17 @@ impl Sequencer {
     /// behaviour of the [`WriteBufferWriting::store_operation()`]
     /// implementation this [`Sequencer`] wraps.
     pub async fn enqueue<'a>(&self, op: DmlOperation) -> Result<DmlMeta, WriteBufferError> {
-        self.inner.store_operation(self.id as u32, &op).await
+        let t = self.time_provider.now();
+
+        let res = self.inner.store_operation(self.id as u32, &op).await;
+
+        if let Some(delta) = self.time_provider.now().checked_duration_since(t) {
+            match &res {
+                Ok(_) => self.enqueue_success.record(delta.as_millis() as _),
+                Err(_) => self.enqueue_error.record(delta.as_millis() as _),
+            }
+        }
+
+        res
     }
 }
