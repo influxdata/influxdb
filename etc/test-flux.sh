45aec477d0a99618df5623791b4070c338d27f46
Sam Arnold <sarnold@influxdata.com>
2022-03-14 15:47:01 -0400

    test: unskip flux group tests

--- etc/test-flux.sh
+++ etc/test-flux.sh
@@ -34,7 +34,8 @@ build_test_harness() {
 # Many tests targeting 3rd party databases are not yet supported in CI and should be filtered out.
 DB_INTEGRATION_WRITE_TESTS=integration_sqlite_write_to,integration_vertica_write_to,integration_mssql_write_to,integration_mysql_write_to,integration_mariadb_write_to,integration_pg_write_to,integration_hdb_write_to
 DB_INTEGRATION_READ_TESTS=integration_sqlite_read_from_seed,integration_sqlite_read_from_nonseed,integration_vertica_read_from_seed,integration_vertica_read_from_nonseed,integration_mssql_read_from_seed,integration_mssql_read_from_nonseed,integration_mariadb_read_from_seed,integration_mariadb_read_from_nonseed,integration_mysql_read_from_seed,integration_mysql_read_from_nonseed,integration_pg_read_from_seed,integration_pg_read_from_nonseed,integration_hdb_read_from_seed,integration_hdb_read_from_nonseed
-DB_TESTS="${DB_INTEGRATION_WRITE_TESTS},${DB_INTEGRATION_READ_TESTS}"
+DB_INTEGRATION_INJECTION_TESTS=integration_sqlite_injection,integration_hdb_injection,integration_pg_injection,integration_mysql_injection,integration_mariadb_injection,integration_mssql_injection
+DB_TESTS="${DB_INTEGRATION_WRITE_TESTS},${DB_INTEGRATION_READ_TESTS},${DB_INTEGRATION_INJECTION_TESTS}"
 
 run_integration_tests() {
   log "Running integration tests..."
@@ -42,7 +43,7 @@ run_integration_tests() {
       -v \
       -p flux.zip \
       -p query/ \
-      --skip "${DB_TESTS}",group_one_tag_first,group_all_filter_field_first,group_one_tag_filter_field_first,group_one_tag_last,group_all_filter_field_last,group_one_tag_filter_field_last,integration_hdb_injection,integration_pg_injection,integration_mysql_injection,integration_mariadb_injection,integration_mssql_injection
+      --skip "${DB_TESTS}"
 }
 
 cleanup() {
