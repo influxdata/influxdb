2c47d77a5b2bd0753f26134a276f63d2c96bd6ee
Marko Mikulicic <mkm@influxdata.com>
2022-03-30 18:31:26 +0200

    fix: Backfill namespace_id in schema migration (#4177)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- iox_catalog/migrations/20220324152729_add_namespace_id_to_parquet_file.sql
+++ iox_catalog/migrations/20220324152729_add_namespace_id_to_parquet_file.sql
@@ -1,6 +1,7 @@
 ALTER TABLE
   IF EXISTS parquet_file
-  ADD COLUMN namespace_id INT NOT NULL;
+  ADD COLUMN namespace_id INT;
+
 ALTER TABLE
   IF EXISTS parquet_file
   ADD FOREIGN KEY (namespace_id)
@@ -8,3 +9,11 @@ ALTER TABLE
   ON UPDATE NO ACTION
   ON DELETE NO ACTION
   NOT VALID;
+
+UPDATE parquet_file p
+  SET namespace_id=t.namespace_id
+  FROM table_name t
+  WHERE t.id=p.table_id;
+
+ALTER TABLE parquet_file
+  ALTER COLUMN namespace_id SET NOT NULL;
