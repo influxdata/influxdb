influxdata/influxdb

commit 380cd9bbff915d2fffd3813679e08e18f8ffc1b7

refactor: use a single flight client implementation (#4273)
"end-user -> querier" and "querier -> ingester" should use a single
Flight client implementation. The difference is just the request and
response metadata.

This changes our default Flight client to use protobuf instead of JSON
for the ticket format.

service_grpc_flight/src/lib.rs
@@ -14,11 +14,13 @@ use arrow_flight::{
     Action, ActionType, Criteria, Empty, FlightData, FlightDescriptor, FlightInfo,
     HandshakeRequest, HandshakeResponse, PutResult, SchemaAsIpc, SchemaResult, Ticket,
 };
+use bytes::{Bytes, BytesMut};
 use datafusion::physical_plan::ExecutionPlan;
 use futures::{SinkExt, Stream, StreamExt};
+use generated_types::influxdata::iox::querier::v1 as proto;
 use pin_project::{pin_project, pinned_drop};
+use prost::Message;
 use query::{QueryCompletedToken, QueryDatabase};
-use serde::Deserialize;
 use service_common::QueryDatabaseProvider;
 use snafu::{ResultExt, Snafu};
 use tokio::task::JoinHandle;
@@ -33,16 +35,8 @@ use service_common::planner::Planner;
 #[allow(clippy::enum_variant_names)]
 #[derive(Debug, Snafu)]
 pub enum Error {
-    #[snafu(display("Invalid ticket. Error: {:?} Ticket: {:?}", source, ticket))]
-    InvalidTicket {
-        source: std::string::FromUtf8Error,
-        ticket: Vec<u8>,
-    },
-    #[snafu(display("Invalid query, could not parse '{}': {}", query, source))]
-    InvalidQuery {
-        query: String,
-        source: serde_json::Error,
-    },
+    #[snafu(display("Invalid ticket. Error: {:?}", source))]
+    InvalidTicket { source: prost::DecodeError },
 
     #[snafu(display("Database {} not found", database_name))]
     DatabaseNotFound { database_name: String },
@@ -70,6 +64,9 @@ pub enum Error {
     Planning {
         source: service_common::planner::Error,
     },
+
+    #[snafu(display("Error during protobuf serialization: {}", source))]
+    Serialization { source: prost::EncodeError },
 }
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -83,13 +80,12 @@ impl From<Error> for tonic::Status {
         match err {
             Error::DatabaseNotFound { .. }
             | Error::InvalidTicket { .. }
-            | Error::InvalidQuery { .. }
             // TODO(edd): this should be `debug`. Keeping at info whilst IOx still in early development
             | Error::InvalidDatabaseName { .. } => info!(?err, msg),
             Error::Query { .. } => info!(?err, msg),
             Error::DictionaryError { .. }
             | Error::InvalidRecordBatch { .. }
-            | Error::Planning { .. } => warn!(?err, msg),
+            | Error::Planning { .. } | Error::Serialization { .. } => warn!(?err, msg),
         }
         err.to_status()
     }
@@ -102,26 +98,18 @@ impl Error {
         use tonic::Status;
         match &self {
             Self::InvalidTicket { .. } => Status::invalid_argument(self.to_string()),
-            Self::InvalidQuery { .. } => Status::invalid_argument(self.to_string()),
             Self::DatabaseNotFound { .. } => Status::not_found(self.to_string()),
             Self::Query { .. } => Status::internal(self.to_string()),
             Self::InvalidDatabaseName { .. } => Status::invalid_argument(self.to_string()),
             Self::InvalidRecordBatch { .. } => Status::internal(self.to_string()),
             Self::Planning { .. } => Status::invalid_argument(self.to_string()),
             Self::DictionaryError { .. } => Status::internal(self.to_string()),
+            Self::Serialization { .. } => Status::internal(self.to_string()),
         }
     }
 }
 
 type TonicStream<T> = Pin<Box<dyn Stream<Item = Result<T, tonic::Status>> + Send + Sync + 'static>>;
-
-#[derive(Deserialize, Debug)]
-/// Body of the `Ticket` serialized and sent to the do_get endpoint; this should
-/// be shared with the read API probably...
-struct ReadInfo {
-    database_name: String,
-    sql_query: String,
-}
 
 /// Concrete implementation of the gRPC Arrow Flight Service API
 #[derive(Debug)]
@@ -165,15 +153,11 @@ where
     ) -> Result<Response<Self::DoGetStream>, tonic::Status> {
         let span_ctx = request.extensions().get().cloned();
         let ticket = request.into_inner();
-        let json_str = String::from_utf8(ticket.ticket.to_vec()).context(InvalidTicketSnafu {
-            ticket: ticket.ticket,
-        })?;
-
-        let read_info: ReadInfo =
-            serde_json::from_str(&json_str).context(InvalidQuerySnafu { query: &json_str })?;
+        let read_info =
+            proto::ReadInfo::decode(Bytes::from(ticket.ticket)).context(InvalidTicketSnafu {})?;
 
         let database =
-            DatabaseName::new(&read_info.database_name).context(InvalidDatabaseNameSnafu)?;
+            DatabaseName::new(&read_info.namespace_name).context(InvalidDatabaseNameSnafu)?;
 
         let db =
             self.server.db(&database).await.ok_or_else(|| {
@@ -192,7 +176,7 @@ where
         let output = GetStream::new(
             ctx,
             physical_plan,
-            read_info.database_name,
+            read_info.namespace_name,
             query_completed_token,
         )
         .await?;
@@ -279,7 +263,14 @@ impl GetStream {
 
         // setup stream
         let options = arrow::ipc::writer::IpcWriteOptions::default();
-        let schema_flight_data = SchemaAsIpc::new(&schema, &options).into();
+        let mut schema_flight_data: FlightData = SchemaAsIpc::new(&schema, &options).into();
+
+        // Add response metadata
+        let mut bytes = BytesMut::new();
+        let app_metadata = proto::AppMetadata {};
+        prost::Message::encode(&app_metadata, &mut bytes).context(SerializationSnafu)?;
+        schema_flight_data.app_metadata = bytes.to_vec();
+
         let mut stream_record_batches = ctx
             .execute_stream(Arc::clone(&physical_plan))
             .await

generated_types/src/lib.rs
@@ -114,6 +114,16 @@ pub mod influxdata {
             }
         }
 
+        pub mod querier {
+            pub mod v1 {
+                include!(concat!(env!("OUT_DIR"), "/influxdata.iox.querier.v1.rs"));
+                include!(concat!(
+                    env!("OUT_DIR"),
+                    "/influxdata.iox.querier.v1.serde.rs"
+                ));
+            }
+        }
+
         pub mod remote {
             pub mod v1 {
                 include!(concat!(env!("OUT_DIR"), "/influxdata.iox.remote.v1.rs"));

ioxd_database/src/lib.rs
@@ -108,6 +108,7 @@ impl ServerType for DatabaseServerType {
 mod tests {
     use clap_blocks::run_config::RunConfig;
 
+    use influxdb_iox_client::flight::generated_types::ReadInfo;
     use ioxd_common::{grpc_listener, http_listener, serve};
 
     use crate::setup::{make_application, make_server};
@@ -542,19 +543,28 @@ mod tests {
         let mut flight = influxdb_iox_client::flight::Client::new(conn.clone());
         consume_query(
             flight
-                .perform_query(db_info.db_name(), "select * from cpu;")
+                .perform_query(ReadInfo {
+                    namespace_name: db_info.db_name().to_string(),
+                    sql_query: "select * from cpu;".to_string(),
+                })
                 .await
                 .unwrap(),
         )
         .await;
 
         flight
-            .perform_query("nonexistent", "select * from cpu;")
+            .perform_query(ReadInfo {
+                namespace_name: "nonexistent".to_string(),
+                sql_query: "select * from cpu;".to_string(),
+            })
             .await
             .unwrap_err();
 
         flight
-            .perform_query(db_info.db_name(), "select * from nonexistent;")
+            .perform_query(ReadInfo {
+                namespace_name: db_info.db_name().to_string(),
+                sql_query: "select * from nonexistent;".to_string(),
+            })
             .await
             .unwrap_err();
 

querier/src/lib.rs
@@ -14,8 +14,6 @@ pub mod cache;
 mod cache_system;
 pub mod chunk;
 mod database;
-/// Flight client to the ingester to request in-memory data.
-mod flight;
 mod handler;
 mod ingester;
 mod namespace;
@@ -27,7 +25,6 @@ mod table;
 mod tombstone;
 
 pub use database::QuerierDatabase;
-pub use flight::{Client as QuerierFlightClient, Error as QuerierFlightError};
 pub use handler::{QuerierHandler, QuerierHandlerImpl};
 pub use ingester::{
     create_ingester_connection, create_ingester_connection_for_testing, IngesterConnection,

generated_types/build.rs
@@ -24,6 +24,7 @@ fn main() -> Result<()> {
 /// - `influxdata.iox.management.v1.rs`
 /// - `influxdata.iox.namespace.v1.rs`
 /// - `influxdata.iox.preserved_catalog.v1.rs`
+/// - `influxdata.iox.querier.v1.rs`
 /// - `influxdata.iox.remote.v1.rs`
 /// - `influxdata.iox.router.v1.rs`
 /// - `influxdata.iox.schema.v1.rs`
@@ -38,6 +39,7 @@ fn generate_grpc_types(root: &Path) -> Result<()> {
     let namespace_path = root.join("influxdata/iox/namespace/v1");
     let predicate_path = root.join("influxdata/iox/predicate/v1");
     let preserved_catalog_path = root.join("influxdata/iox/preserved_catalog/v1");
+    let querier_path = root.join("influxdata/iox/querier/v1");
     let remote_path = root.join("influxdata/iox/remote/v1");
     let router_path = root.join("influxdata/iox/router/v1");
     let schema_path = root.join("influxdata/iox/schema/v1");
@@ -62,6 +64,7 @@ fn generate_grpc_types(root: &Path) -> Result<()> {
         predicate_path.join("predicate.proto"),
         preserved_catalog_path.join("catalog.proto"),
         preserved_catalog_path.join("parquet_metadata.proto"),
+        querier_path.join("flight.proto"),
         root.join("google/longrunning/operations.proto"),
         root.join("google/rpc/error_details.proto"),
         root.join("google/rpc/status.proto"),

generated_types/protos/influxdata/iox/querier/v1/flight.proto
@@ -1,0 +1,17 @@
+syntax = "proto3";
+package influxdata.iox.querier.v1;
+
+// Request body for ticket in "end-user to querier" flight requests.
+message ReadInfo {
+  // Namespace(/database) name.
+  string namespace_name = 1;
+
+  // SQL query.
+  string sql_query = 2;
+}
+
+// Response in "end-user to querier" flight response.
+//
+// Currently this does not contain any metadata, but IOx might provide metadata like data lineage information,
+// statistics or watermark information in the future.
+message AppMetadata {}

influxdb_iox/src/commands/database.rs
@@ -4,7 +4,7 @@ use crate::TABLE_STYLE_SINGLE_LINE_BORDERS;
 use comfy_table::{Cell, Table};
 use influxdb_iox_client::{
     connection::Connection,
-    flight,
+    flight::{self, generated_types::ReadInfo},
     format::QueryOutputFormat,
     management::{self, generated_types::database_status::DatabaseState, generated_types::*},
     write,
@@ -393,7 +393,12 @@ pub async fn command(connection: Connection, config: Config) -> Result<()> {
 
             let format = QueryOutputFormat::from_str(&format)?;
 
-            let mut query_results = client.perform_query(&name, query).await?;
+            let mut query_results = client
+                .perform_query(ReadInfo {
+                    namespace_name: name,
+                    sql_query: query,
+                })
+                .await?;
 
             // It might be nice to do some sort of streaming write
             // rather than buffering the whole thing.

influxdb_iox/src/commands/sql/observer.rs
@@ -1,6 +1,6 @@
 //! This module implements the "Observer" functionality of the SQL repl
 
-use influxdb_iox_client::connection::Connection;
+use influxdb_iox_client::{connection::Connection, flight::generated_types::ReadInfo};
 use snafu::{ResultExt, Snafu};
 
 use std::{collections::HashMap, sync::Arc, time::Instant};
@@ -134,7 +134,10 @@ async fn load_remote_system_tables(
                 tokio::task::spawn(async move {
                     let mut client = influxdb_iox_client::flight::Client::new(connection);
                     let mut query_results = client
-                        .perform_query(&db_name, &sql)
+                        .perform_query(ReadInfo {
+                            namespace_name: db_name.clone(),
+                            sql_query: sql,
+                        })
                         .await
                         .context(RunningRemoteQuerySnafu)?;
 

commit 3cd11f2ca07298055b1a00cd4e1e2c39b0d8764c

fix: accept legacy flight requests
This removes the SERVER-side breaking change introduced in #4273 by also
accepting JSON-based tickets.

Closes #4280.

service_grpc_flight/src/lib.rs
@@ -21,6 +21,7 @@ use generated_types::influxdata::iox::querier::v1 as proto;
 use pin_project::{pin_project, pinned_drop};
 use prost::Message;
 use query::{QueryCompletedToken, QueryDatabase};
+use serde::Deserialize;
 use service_common::QueryDatabaseProvider;
 use snafu::{ResultExt, Snafu};
 use tokio::task::JoinHandle;
@@ -37,6 +38,15 @@ use service_common::planner::Planner;
 pub enum Error {
     #[snafu(display("Invalid ticket. Error: {:?}", source))]
     InvalidTicket { source: prost::DecodeError },
+
+    #[snafu(display("Invalid legacy ticket. Error: {:?}", source))]
+    InvalidTicketLegacy { source: std::string::FromUtf8Error },
+
+    #[snafu(display("Invalid query, could not parse '{}': {}", query, source))]
+    InvalidQuery {
+        query: String,
+        source: serde_json::Error,
+    },
 
     #[snafu(display("Database {} not found", database_name))]
     DatabaseNotFound { database_name: String },
@@ -80,6 +90,8 @@ impl From<Error> for tonic::Status {
         match err {
             Error::DatabaseNotFound { .. }
             | Error::InvalidTicket { .. }
+            | Error::InvalidTicketLegacy { .. }
+            | Error::InvalidQuery { .. }
             // TODO(edd): this should be `debug`. Keeping at info whilst IOx still in early development
             | Error::InvalidDatabaseName { .. } => info!(?err, msg),
             Error::Query { .. } => info!(?err, msg),
@@ -98,6 +110,8 @@ impl Error {
         use tonic::Status;
         match &self {
             Self::InvalidTicket { .. } => Status::invalid_argument(self.to_string()),
+            Self::InvalidTicketLegacy { .. } => Status::invalid_argument(self.to_string()),
+            Self::InvalidQuery { .. } => Status::invalid_argument(self.to_string()),
             Self::DatabaseNotFound { .. } => Status::not_found(self.to_string()),
             Self::Query { .. } => Status::internal(self.to_string()),
             Self::InvalidDatabaseName { .. } => Status::invalid_argument(self.to_string()),
@@ -110,6 +124,34 @@ impl Error {
 }
 
 type TonicStream<T> = Pin<Box<dyn Stream<Item = Result<T, tonic::Status>> + Send + Sync + 'static>>;
+
+#[derive(Deserialize, Debug)]
+/// Body of the `Ticket` serialized and sent to the do_get endpoint.
+struct ReadInfo {
+    database_name: String,
+    sql_query: String,
+}
+
+impl ReadInfo {
+    fn decode_json(ticket: &[u8]) -> Result<Self> {
+        let json_str = String::from_utf8(ticket.to_vec()).context(InvalidTicketLegacySnafu {})?;
+
+        let read_info: ReadInfo =
+            serde_json::from_str(&json_str).context(InvalidQuerySnafu { query: &json_str })?;
+
+        Ok(read_info)
+    }
+
+    fn decode_protobuf(ticket: &[u8]) -> Result<Self> {
+        let read_info =
+            proto::ReadInfo::decode(Bytes::from(ticket.to_vec())).context(InvalidTicketSnafu {})?;
+
+        Ok(Self {
+            database_name: read_info.namespace_name,
+            sql_query: read_info.sql_query,
+        })
+    }
+}
 
 /// Concrete implementation of the gRPC Arrow Flight Service API
 #[derive(Debug)]
@@ -153,11 +195,18 @@ where
     ) -> Result<Response<Self::DoGetStream>, tonic::Status> {
         let span_ctx = request.extensions().get().cloned();
         let ticket = request.into_inner();
-        let read_info =
-            proto::ReadInfo::decode(Bytes::from(ticket.ticket)).context(InvalidTicketSnafu {})?;
+
+        // decode ticket
+        let read_info = match ReadInfo::decode_protobuf(&ticket.ticket) {
+            Ok(read_info) => read_info,
+            Err(_) => {
+                // try legacy json
+                ReadInfo::decode_json(&ticket.ticket)?
+            }
+        };
 
         let database =
-            DatabaseName::new(&read_info.namespace_name).context(InvalidDatabaseNameSnafu)?;
+            DatabaseName::new(&read_info.database_name).context(InvalidDatabaseNameSnafu)?;
 
         let db =
             self.server.db(&database).await.ok_or_else(|| {
@@ -176,7 +225,7 @@ where
         let output = GetStream::new(
             ctx,
             physical_plan,
-            read_info.namespace_name,
+            read_info.database_name,
             query_completed_token,
         )
         .await?;