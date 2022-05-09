cbe9eb4b1e46fe1dd7872cdb738d96eff11f76d2
YIXIAO SHI <aieruishi@gmail.com>
2022-05-09 19:24:12 +0800

    chore: fix comment typo (#4533)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- ingester/src/data.rs
+++ ingester/src/data.rs
@@ -993,7 +993,7 @@ impl PartitionData {
         self.data.get_persisting_data()
     }
 
-    /// Write the given mb in the bufer
+    /// Write the given mb in the buffer
     pub(crate) fn buffer_write(
         &mut self,
         sequencer_number: SequenceNumber,
