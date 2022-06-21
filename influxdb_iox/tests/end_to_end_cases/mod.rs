087dbd3eca3e3eeeb428d4b31d9fe648668b02db
Andrew Lamb <andrew@nerdnetworks.org>
2022-06-21 15:53:28 -0400

    fix: fix heappy + update docs (#4917)
    * docs: Update heap profiling documentation
    
    * fix: fix heappy builds
    
    * fix: do not run cli tests with heappy
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- influxdb_iox/tests/end_to_end_cases/mod.rs
+++ influxdb_iox/tests/end_to_end_cases/mod.rs
@@ -1,4 +1,7 @@
 mod all_in_one;
+// CLI errors when run with heappy (only works via `cargo run`):
+// loading shared libraries: libjemalloc.so.2: cannot open shared object file: No such file or directory"
+#[cfg(not(feature = "heappy"))]
 mod cli;
 mod debug;
 mod error;
