ee491cbbfce85ee767fc7a5a0769f76624db9985
Marco Neumann <marco@crepererum.net>
2022-08-02 08:37:30 +0000

    fix: re-enable querier read buffer cache (#5268)
    This reverts commit 82913743f15784fcef2a9fe2ad9e92f871ca55b1 / #5252.
    
    I misjudged the cache hit ratio for the RB, see
    https://github.com/influxdata/k8s-infra/pull/4548
    
    So let's bring back the RB cache until we have some form of parquet
    cache in place.

--- querier/src/chunk/mod.rs
+++ querier/src/chunk/mod.rs
@@ -183,7 +183,6 @@ impl From<Arc<RBChunk>> for ChunkStage {
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
 pub enum QuerierChunkLoadSetting {
     /// Only stay in parquet mode and never use read buffer data.
-    #[default]
     ParquetOnly,
 
     /// Only use read buffer data.
@@ -191,11 +190,12 @@ pub enum QuerierChunkLoadSetting {
     /// This forces the querier to load the read buffer for this chunk.
     ReadBufferOnly,
 
-    /// "On-demand" handling.
+    /// Default "on-demand" handling.
     ///
     /// When the chunk is created, it will look up if there is already read buffer data loaded (or loading is already in
     /// progress) and is that. Otherwise it will parquet. If the actual payload is request (via
     /// [`QueryChunk::read_filter`](iox_query::QueryChunk::read_filter)) then it will force-load the read buffer.
+    #[default]
     OnDemand,
 }
 
