58c3f27e3bf7d2d96afee3c8213157d424da3639
Brandon Pfeifer <bpfeifer@influxdata.com>
2022-08-11 23:06:55 -0400

    chore: fix build commands for new cross-builder

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
-declare -r LINUX_EXTLD="-fno-PIC -static -Wl,-z,stack-size=8388608"
+declare -r LINUX_EXTLD="-fno-PIC -static-pie -Wl,-z,stack-size=8388608"
 
 function main () {
     if [[ $# != 3 ]]; then
@@ -20,23 +20,26 @@ function main () {
     local -r os_arch="$(go env GOOS)_$(go env GOARCH)"
     case "$os_arch" in
         linux_amd64)
-            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
-                -tags assets,sqlite_foreign_keys,sqlite_json,static_build \
-                -buildmode pie \
-                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date} -extldflags '$LINUX_EXTLD'" \
+            export CC="$(xcc linux x86_64)"
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) go build \
+                -tags assets,sqlite_foreign_keys,sqlite_json,static_build,noasm \
+                -buildmode=pie \
+                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date} -linkmode=external -extld=${CC} -extldflags '${LINUX_EXTLD}'" \
                 -o "$out_dir/" \
                 "$pkg"
             ;;
         linux_arm64)
-            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+            export CC="$(xcc linux aarch64)"
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) go build \
                 -tags assets,sqlite_foreign_keys,sqlite_json,static_build,noasm \
-                -buildmode pie \
-                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date} -extldflags '$LINUX_EXTLD'" \
+                -buildmode=pie \
+                -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date} -linkmode=external -extld=${CC} -extldflags '${LINUX_EXTLD}'" \
                 -o "$out_dir/" \
                 "$pkg"
             ;;
         darwin_amd64)
-            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+            export CC="$(xcc darwin)"
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) go build \
                 -tags assets,sqlite_foreign_keys,sqlite_json \
                 -buildmode pie \
                 -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date}" \
@@ -44,7 +47,8 @@ function main () {
                 "$pkg"
             ;;
         windows_amd64)
-            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) CC=$(which xcc.sh) go build \
+            export CC="$(xcc windows)"
+            CGO_ENABLED=1 PKG_CONFIG=$(which pkg-config) go build \
                 -tags assets,sqlite_foreign_keys,sqlite_json,timetzdata \
                 -buildmode exe \
                 -ldflags "-s -w -X main.version=${version} -X main.commit=${commit} -X main.date=${build_date}" \
