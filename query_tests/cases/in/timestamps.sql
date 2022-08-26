283e90813294e0246e2c70564c5c18b8ab82ebb0
Nga Tran <nga-tran@live.com>
2022-08-26 16:52:12 -0400

    test: workaround for time > a number (#5477)
    * test: workaround for time > a number
    
    * chore: cargo update
    
    * chore: Revert "chore: cargo update"
    
    This reverts commit 0798e4e14674267ddd2308b12a25031fc35de8b6.

--- query_tests/cases/in/timestamps.sql
+++ query_tests/cases/in/timestamps.sql
@@ -10,3 +10,6 @@ SELECT * FROM cpu WHERE time  > to_timestamp('2021-07-20T19:28:50Z');
 --use cast workaround
 SELECT * FROM cpu WHERE
   CAST(time AS BIGINT) > CAST(to_timestamp('2021-07-20T19:28:50Z') AS BIGINT);
+-- work aroound for case time > 10 order by region, time, user;
+ SELECT * FROM cpu where cast(time as bigint) > 10  order by region, time, user;
+ -- this query does not work: SELECT * FROM cpu where time > 10
