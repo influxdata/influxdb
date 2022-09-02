485968cce4b5b101ebfcb6ccf073fe8e8e9d6a91
Jeffrey Smith II <jsmith@influxdata.com>
2022-09-02 10:54:05 -0400

    feat: unpin ui to point at latest (#23704)

--- scripts/fetch-ui-assets.sh
+++ scripts/fetch-ui-assets.sh
@@ -3,11 +3,11 @@
 # This script is used to download built UI assets from the "influxdata/ui"
 # repository. The built UI assets are attached to a release in "influxdata/ui",
 # which is linked here.
-# 
+#
 # The master branch of "influxdata/influxdb" (this repository) downloads from the
 # release tagged as "OSS-Master" in "influxdata/ui". That release is kept up-to-date
-# with the most recent changes in "influxdata/ui". 
-# 
+# with the most recent changes in "influxdata/ui".
+#
 # Feature branches of "influxdata/influxdb" (2.0, 2.1, etc) download from their
 # respective releases in "influxdata/ui" (OSS-2.0, OSS-2.1, etc). Those releases
 # are updated only when a bug fix needs included for the UI of that OSS release.
@@ -21,10 +21,10 @@ declare -r STATIC_DIR="$ROOT_DIR/static"
 # Download the SHA256 checksum attached to the release. To verify the integrity
 # of the download, this checksum will be used to check the download tar file
 # containing the built UI assets.
-curl -Ls https://github.com/influxdata/ui/releases/download/OSS-Master/sha256.txt --output sha256.txt
+curl -Ls https://github.com/influxdata/ui/releases/download/OSS-2022-09-01/sha256.txt --output sha256.txt
 
 # Download the tar file containing the built UI assets.
-curl -L https://github.com/influxdata/ui/releases/download/OSS-Master/build.tar.gz --output build.tar.gz
+curl -L https://github.com/influxdata/ui/releases/download/OSS-2022-09-01/build.tar.gz --output build.tar.gz
 
 # Verify the checksums match; exit if they don't.
 case "$(uname -s)" in
