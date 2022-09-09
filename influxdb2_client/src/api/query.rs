0d313a4579899cfcc913e6f9386a907adfb65157
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-09-09 10:44:35 -0400

    fix: Don't try to parse /api/v2/query response as JSON
    It's just plain text afaict.

--- influxdb2_client/src/api/query.rs
+++ influxdb2_client/src/api/query.rs
@@ -78,17 +78,7 @@ impl Client {
         match response.status() {
             StatusCode::OK => {
                 let bytes = response.bytes().await.context(ResponseBytesSnafu)?;
-
-                let json_result = serde_json::from_slice::<String>(&bytes);
-                match json_result {
-                    Ok(json) => Ok(json),
-                    Err(source) => {
-                        let text =
-                            String::from_utf8(bytes.to_vec()).context(ResponseStringSnafu)?;
-
-                        Err(RequestError::DeserializingJsonResponse { source, text })
-                    }
-                }
+                String::from_utf8(bytes.to_vec()).context(ResponseStringSnafu)
             }
             status => {
                 let text = response.text().await.context(ReqwestProcessingSnafu)?;
@@ -328,6 +318,32 @@ mod tests {
         let client = Client::new(&mockito::server_url(), token);
 
         let _result = client.query_ast(language_request).await;
+
+        mock_server.assert();
+    }
+
+    #[tokio::test]
+    async fn query_no_results() {
+        let token = "some-token";
+        let org = "some-org";
+        let query: Option<Query> = Some(Query::new("some-influx-query-string".to_string()));
+        let mock_server = mock("POST", "/api/v2/query")
+            .match_header("Authorization", format!("Token {}", token).as_str())
+            .match_header("Accepting-Encoding", "identity")
+            .match_header("Content-Type", "application/json")
+            .match_query(Matcher::UrlEncoded("org".into(), org.into()))
+            .match_body(
+                serde_json::to_string(&query.clone().unwrap_or_default())
+                    .unwrap()
+                    .as_str(),
+            )
+            .with_body("")
+            .create();
+
+        let client = Client::new(&mockito::server_url(), token);
+
+        let result = client.query(org, query).await.expect("request success");
+        assert_eq!(result, "");
 
         mock_server.assert();
     }
