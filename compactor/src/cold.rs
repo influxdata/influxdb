2aef7c7936f113bcc2e6f020c0ced6b940100be3
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-09-12 11:01:41 -0400

    feat: Temporarily disable cold full compaction

--- compactor/src/cold.rs
+++ compactor/src/cold.rs
@@ -101,20 +101,6 @@ pub async fn compact(compactor: Arc<Compactor>) -> usize {
     )
     .await;
 
-    // Compact level 1 files to level 2 in serial (for now)
-    for candidate in candidates {
-        let partition_id = candidate.id();
-        let compaction_result = full_compaction(&compactor, candidate, &Default::default()).await;
-        match compaction_result {
-            Err(e) => {
-                warn!(?e, ?partition_id, "full compaction failed");
-            }
-            Ok(_) => {
-                debug!(?partition_id, "full compaction complete");
-            }
-        }
-    }
-
     // Done compacting all candidates in the cycle, record its time
     if let Some(delta) = compactor
         .time_provider
@@ -156,6 +142,7 @@ pub(crate) enum Error {
 /// - Compact each group into a new level 2 file, no splitting
 ///
 /// Uses a hashmap of size overrides to allow mocking of file sizes.
+#[allow(dead_code)]
 async fn full_compaction(
     compactor: &Compactor,
     partition: Arc<PartitionCompactionCandidateWithInfo>,
@@ -787,12 +774,15 @@ mod tests {
 
         // Should have 1 non-soft-deleted files:
         //
-        // - the newly created file that was upgraded to level 1 then to level 2
+        // - the newly created file that was upgraded to level 1. Final compaction to level 2 is
+        //   currently disabled.
         let mut files = catalog.list_by_table_not_to_delete(table.table.id).await;
         assert_eq!(files.len(), 1);
         let file = files.pop().unwrap();
         assert_eq!(file.id.get(), 1); // ID doesn't change because the file doesn't get rewritten
-        assert_eq!(file.compaction_level, CompactionLevel::Final);
+
+        // Final compaction is currently disabled.
+        assert_eq!(file.compaction_level, CompactionLevel::FileNonOverlapped);
 
         // ------------------------------------------------
         // Verify the parquet file content
@@ -989,43 +979,17 @@ mod tests {
 
         compact(compactor).await;
 
-        // Should have 1 non-soft-deleted file:
-        //
-        // - the level 2 file created after combining all 3 level 1 files created by the first step
-        //   of compaction to compact remaining level 0 files
-        let mut files = catalog.list_by_table_not_to_delete(table.table.id).await;
-        assert_eq!(files.len(), 1);
+        // Full cold compaction to level 2 is currently disabled.
+        let files = catalog.list_by_table_not_to_delete(table.table.id).await;
+        assert_eq!(files.len(), 3);
         let files_and_levels: Vec<_> = files
             .iter()
             .map(|f| (f.id.get(), f.compaction_level))
             .collect();
-        assert_eq!(files_and_levels, vec![(9, CompactionLevel::Final),]);
-
-        // ------------------------------------------------
-        // Verify the parquet file content
-        let file = files.pop().unwrap();
-        let batches = table.read_parquet_file(file).await;
-        assert_batches_sorted_eq!(
-            &[
-                "+-----------+------+------+------+--------------------------------+",
-                "| field_int | tag1 | tag2 | tag3 | time                           |",
-                "+-----------+------+------+------+--------------------------------+",
-                "| 10        | VT   |      |      | 1970-01-01T00:00:00.000000020Z |",
-                "| 10        | VT   |      |      | 1970-01-01T00:00:00.000006Z    |",
-                "| 10        | VT   |      |      | 1970-01-01T00:00:00.000010Z    |",
-                "| 1000      | WA   |      |      | 1970-01-01T00:00:00.000000010Z |",
-                "| 1500      | WA   |      |      | 1970-01-01T00:00:00.000008Z    |",
-                "| 1600      |      | WA   | 10   | 1970-01-01T00:00:00.000028Z    |",
-                "| 1601      |      | PA   | 15   | 1970-01-01T00:00:00.000000009Z |",
-                "| 20        |      | VT   | 20   | 1970-01-01T00:00:00.000026Z    |",
-                "| 21        |      | OH   | 21   | 1970-01-01T00:00:00.000000025Z |",
-                "| 270       | UT   |      |      | 1970-01-01T00:00:00.000025Z    |",
-                "| 421       |      | OH   | 21   | 1970-01-01T00:00:00.000091Z    |",
-                "| 70        | UT   |      |      | 1970-01-01T00:00:00.000020Z    |",
-                "| 81601     |      | PA   | 15   | 1970-01-01T00:00:00.000090Z    |",
-                "+-----------+------+------+------+--------------------------------+",
-            ],
-            &batches
-        );
+        assert_eq!(files_and_levels, vec![
+            (6, CompactionLevel::FileNonOverlapped),
+            (7, CompactionLevel::FileNonOverlapped),
+            (8, CompactionLevel::FileNonOverlapped),
+        ]);
     }
 }
