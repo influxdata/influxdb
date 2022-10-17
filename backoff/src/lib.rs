b3b22cff3786b5a7bbe6776caef3674b81a34135
Marco Neumann <marco@crepererum.net>
2022-10-17 17:35:26 +0200

    feat: harden and polish backoff system (#5873)
    * refactor: `impl Iterator for Backoff`
    
    * feat: `Backoff::fade_to`
    
    * feat: impl `PartialEq` for `BackoffConfig`
    
    * fix: harden backoff logic
    
    * fix: harden backoff base handling
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- backoff/src/lib.rs
+++ backoff/src/lib.rs
@@ -20,7 +20,7 @@ use std::time::Duration;
 /// Exponential backoff with jitter
 ///
 /// See <https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/>
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 #[allow(missing_copy_implementations)]
 pub struct BackoffConfig {
     /// Initial backoff.
@@ -89,23 +89,40 @@ impl std::fmt::Debug for Backoff {
 }
 
 impl Backoff {
-    /// Create a new [`Backoff`] from the provided [`BackoffConfig`]
+    /// Create a new [`Backoff`] from the provided [`BackoffConfig`].
+    ///
+    /// # Pancis
+    /// Panics if [`BackoffConfig::base`] is not finite or < 1.0.
     pub fn new(config: &BackoffConfig) -> Self {
         Self::new_with_rng(config, None)
     }
 
-    /// Creates a new `Backoff` with the optional `rng`
+    /// Creates a new `Backoff` with the optional `rng`.
     ///
-    /// Used [`rand::thread_rng()`] if no rng provided
+    /// Used [`rand::thread_rng()`] if no rng provided.
+    ///
+    /// See [`new`](Self::new) for panic handling.
     pub fn new_with_rng(
         config: &BackoffConfig,
         rng: Option<Box<dyn RngCore + Sync + Send>>,
     ) -> Self {
-        let init_backoff = config.init_backoff.as_secs_f64();
+        assert!(
+            config.base.is_finite(),
+            "Backoff base ({}) must be finite.",
+            config.base,
+        );
+        assert!(
+            config.base >= 1.0,
+            "Backoff base ({}) must be greater or equal than 1.",
+            config.base,
+        );
+
+        let max_backoff = config.max_backoff.as_secs_f64();
+        let init_backoff = config.init_backoff.as_secs_f64().min(max_backoff);
         Self {
             init_backoff,
             next_backoff_secs: init_backoff,
-            max_backoff_secs: config.max_backoff.as_secs_f64(),
+            max_backoff_secs: max_backoff,
             base: config.base,
             total: 0.0,
             deadline: config.deadline.map(|d| d.as_secs_f64()),
@@ -113,26 +130,20 @@ impl Backoff {
         }
     }
 
-    /// Returns the next backoff duration to wait for, if any
-    fn next(&mut self) -> Option<Duration> {
-        let range = self.init_backoff..(self.next_backoff_secs * self.base);
+    /// Fade this backoff over to a different backoff config.
+    pub fn fade_to(&mut self, config: &BackoffConfig) {
+        // Note: `new` won't have the same RNG, but this doesn't matter
+        let new = Self::new(config);
 
-        let rand_backoff = match self.rng.as_mut() {
-            Some(rng) => rng.gen_range(range),
-            None => thread_rng().gen_range(range),
+        *self = Self {
+            init_backoff: new.init_backoff,
+            next_backoff_secs: self.next_backoff_secs,
+            max_backoff_secs: new.max_backoff_secs,
+            base: new.base,
+            total: self.total,
+            deadline: new.deadline,
+            rng: self.rng.take(),
         };
-
-        let next_backoff = self.max_backoff_secs.min(rand_backoff);
-        self.total += next_backoff;
-        if let Some(deadline) = self.deadline {
-            if self.total >= deadline {
-                return None;
-            }
-        }
-        Some(Duration::from_secs_f64(std::mem::replace(
-            &mut self.next_backoff_secs,
-            next_backoff,
-        )))
     }
 
     /// Perform an async operation that retries with a backoff
@@ -201,6 +212,40 @@ impl Backoff {
     }
 }
 
+impl Iterator for Backoff {
+    type Item = Duration;
+
+    /// Returns the next backoff duration to wait for, if any
+    fn next(&mut self) -> Option<Self::Item> {
+        let range = self.init_backoff..=(self.next_backoff_secs * self.base);
+
+        let rand_backoff = match self.rng.as_mut() {
+            Some(rng) => rng.gen_range(range),
+            None => thread_rng().gen_range(range),
+        };
+
+        let next_backoff = self.max_backoff_secs.min(rand_backoff);
+        self.total += next_backoff;
+        let res = std::mem::replace(&mut self.next_backoff_secs, next_backoff);
+        if let Some(deadline) = self.deadline {
+            if self.total >= deadline {
+                return None;
+            }
+        }
+        duration_try_from_secs_f64(res)
+    }
+}
+
+const MAX_F64_SECS: f64 = 1_000_000.0;
+
+/// Try to get `Duration` from `f64` secs.
+///
+/// This is required till <https://github.com/rust-lang/rust/issues/83400> is resolved.
+fn duration_try_from_secs_f64(secs: f64) -> Option<Duration> {
+    (secs.is_finite() && (0.0..=MAX_F64_SECS).contains(&secs))
+        .then(|| Duration::from_secs_f64(secs))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -260,5 +305,85 @@ mod tests {
             Some(rng),
         );
         assert_eq!(backoff.next(), None);
+    }
+
+    #[test]
+    fn test_overflow() {
+        let rng = Box::new(StepRng::new(u64::MAX, 0));
+        let cfg = BackoffConfig {
+            init_backoff: Duration::MAX,
+            max_backoff: Duration::MAX,
+            ..Default::default()
+        };
+        let mut backoff = Backoff::new_with_rng(&cfg, Some(rng));
+        assert_eq!(backoff.next(), None);
+    }
+
+    #[test]
+    fn test_duration_try_from_f64() {
+        for d in [-0.1, f64::INFINITY, f64::NAN, MAX_F64_SECS + 0.1] {
+            assert!(duration_try_from_secs_f64(d).is_none());
+        }
+
+        for d in [0.0, MAX_F64_SECS] {
+            assert!(duration_try_from_secs_f64(d).is_some());
+        }
+    }
+
+    #[test]
+    fn test_max_backoff_smaller_init() {
+        let rng = Box::new(StepRng::new(u64::MAX, 0));
+        let cfg = BackoffConfig {
+            init_backoff: Duration::from_secs(2),
+            max_backoff: Duration::from_secs(1),
+            ..Default::default()
+        };
+        let mut backoff = Backoff::new_with_rng(&cfg, Some(rng));
+        assert_eq!(backoff.next(), Some(Duration::from_secs(1)));
+        assert_eq!(backoff.next(), Some(Duration::from_secs(1)));
+    }
+
+    #[test]
+    #[should_panic(expected = "Backoff base (inf) must be finite.")]
+    fn test_panic_inf_base() {
+        let cfg = BackoffConfig {
+            base: f64::INFINITY,
+            ..Default::default()
+        };
+        Backoff::new(&cfg);
+    }
+
+    #[test]
+    #[should_panic(expected = "Backoff base (NaN) must be finite.")]
+    fn test_panic_nan_base() {
+        let cfg = BackoffConfig {
+            base: f64::NAN,
+            ..Default::default()
+        };
+        Backoff::new(&cfg);
+    }
+
+    #[test]
+    #[should_panic(expected = "Backoff base (0) must be greater or equal than 1.")]
+    fn test_panic_zero_base() {
+        let cfg = BackoffConfig {
+            base: 0.0,
+            ..Default::default()
+        };
+        Backoff::new(&cfg);
+    }
+
+    #[test]
+    fn test_constant_backoff() {
+        let rng = Box::new(StepRng::new(u64::MAX, 0));
+        let cfg = BackoffConfig {
+            init_backoff: Duration::from_secs(1),
+            max_backoff: Duration::from_secs(1),
+            base: 1.0,
+            ..Default::default()
+        };
+        let mut backoff = Backoff::new_with_rng(&cfg, Some(rng));
+        assert_eq!(backoff.next(), Some(Duration::from_secs(1)));
+        assert_eq!(backoff.next(), Some(Duration::from_secs(1)));
     }
 }
