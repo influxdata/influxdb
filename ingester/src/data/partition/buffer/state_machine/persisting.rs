c8fdd760335407579544cebf9f91ccac101a1fc7
Dom Dwyer <dom@itsallbroken.com>
2022-10-20 17:36:20 +0200

    feat(ingester): partition buffer state machine
    This commit introduces code that is intended to replace the current
    implicit state machine used by PartitionData. The existing code is still
    in use, the new code is NOT used in this commit. A follow-up commit will
    switch over to minimise the diff.
    
    This change has two main goals;
        * encapsulation & simplification for callers
        * robust implementation so developing correct additions is easier
    
    This is a significant refactor of the partition buffering logic to
    encapsulate the various states of data (buffering, snapshot, persisting
    and the mixed states between them) within the Partition. This alleviates
    the rest of the system from having to be concerned with the differences
    between "buffering" data, and "unpersisted data", "snapshot data",
    "persisting data", "persisting with snapshots" etc - callers now invoke
    a method called get_query_data() and they are provided with all the
    relevant data for a partition. This abstraction change alone
    significantly reduces code and test complexity in the rest of the
    ingester.
    
    For the second goal, the new implementation leverages an explicit state
    machine, encoded using typestates. Typestate ensures compile-time
    correctness of transitions and method calls, and the explicit FSM itself
    helps ensure the system progresses in the desired manner - this fixes
    and helps prevent bugs caused by implicit states such as:
    
        https://github.com/influxdata/influxdb_iox/issues/5805
    
    This state machine makes the system states explicit and
    self-descriptive, helping to reduce the cost of developer on-boarding
    (no prior knowledge of "how this bit works") and reduces ongoing
    developer burden. This explicit nature also de-risks adding new
    functionality - it should be relatively easy to add concurrent snapshot
    generation or incremental compaction without introducing bugs. The state
    transition logic is abstracted away from callers, minimising the
    overhead of this strategy.

---
+++ ingester/src/data/partition/buffer/state_machine/persisting.rs
@@ -0,0 +1,37 @@
+//! A writfield1 buffer, with one or more snapshots.
+
+use std::sync::Arc;
+
+use arrow::record_batch::RecordBatch;
+
+use crate::data::partition::buffer::traits::Queryable;
+
+use super::BufferState;
+
+/// An immutable set of [`RecordBatch`] in the process of being persisted.
+#[derive(Debug)]
+pub(crate) struct Persisting {
+    /// Snapshots generated from previous buffer contents to be persisted.
+    ///
+    /// INVARIANT: this array is always non-empty.
+    snapshots: Vec<Arc<RecordBatch>>,
+}
+
+impl Persisting {
+    pub(super) fn new(snapshots: Vec<Arc<RecordBatch>>) -> Self {
+        Self { snapshots }
+    }
+}
+
+impl Queryable for Persisting {
+    fn get_query_data(&self) -> &[Arc<RecordBatch>] {
+        &self.snapshots
+    }
+}
+
+impl BufferState<Persisting> {
+    /// Consume `self`, returning the data it holds as a set of [`RecordBatch`].
+    pub(super) fn into_data(self) -> Vec<Arc<RecordBatch>> {
+        self.state.snapshots
+    }
+}
---
+++ ingester/src/data/partition/buffer/state_machine/snapshot.rs
@@ -0,0 +1,49 @@
+//! A writfield1 buffer, with one or more snapshots.
+
+use std::sync::Arc;
+
+use arrow::record_batch::RecordBatch;
+
+use crate::data::partition::buffer::{state_machine::persisting::Persisting, traits::Queryable};
+
+use super::{buffering_with_snapshot::BufferingWithSnapshot, BufferState};
+
+/// An immutable, queryable FSM state containing at least one buffer snapshot.
+#[derive(Debug)]
+pub(crate) struct Snapshot {
+    /// Snapshots generated from previous buffer contents.
+    ///
+    /// INVARIANT: this array is always non-empty.
+    snapshots: Vec<Arc<RecordBatch>>,
+}
+
+impl Snapshot {
+    pub(super) fn new(snapshots: Vec<Arc<RecordBatch>>) -> Self {
+        assert!(!snapshots.is_empty());
+        Self { snapshots }
+    }
+}
+
+impl Queryable for Snapshot {
+    fn get_query_data(&self) -> &[Arc<RecordBatch>] {
+        &self.snapshots
+    }
+}
+
+impl BufferState<Snapshot> {
+    pub(crate) fn into_buffering(self) -> BufferState<BufferingWithSnapshot> {
+        assert!(!self.state.snapshots.is_empty());
+        BufferState {
+            state: BufferingWithSnapshot::new(self.state.snapshots),
+            sequence_range: self.sequence_range,
+        }
+    }
+
+    pub(crate) fn into_persisting(self) -> BufferState<Persisting> {
+        assert!(!self.state.snapshots.is_empty());
+        BufferState {
+            state: Persisting::new(self.state.snapshots),
+            sequence_range: self.sequence_range,
+        }
+    }
+}
