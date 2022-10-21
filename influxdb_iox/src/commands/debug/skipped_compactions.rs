68e310f45d1395091183d41b2e8b2ac9960adf9d
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2022-10-21 13:40:12 -0400

    feat: Display skipped compactions in a table instead of JSON

--- influxdb_iox/src/commands/debug/skipped_compactions.rs
+++ influxdb_iox/src/commands/debug/skipped_compactions.rs
@@ -1,6 +1,11 @@
 //! This module implements the `skipped-compactions` CLI command
 
-use influxdb_iox_client::{compactor, connection::Connection};
+use comfy_table::{Cell, Table};
+use influxdb_iox_client::{
+    compactor::{self, generated_types::SkippedCompaction},
+    connection::Connection,
+};
+use iox_time::Time;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -31,10 +36,46 @@ pub async fn command(connection: Connection, config: Config) -> Result<(), Error
     match config.command {
         Command::List => {
             let skipped_compactions = client.skipped_compactions().await?;
-            println!("{}", serde_json::to_string_pretty(&skipped_compactions)?);
+            println!("{}", create_table(&skipped_compactions));
         } // Deliberately not adding _ => so the compiler will direct people here to impl new
           // commands
     }
 
     Ok(())
+}
+
+/// Turn skipped compaction records into a table
+fn create_table(skipped_compactions: &[SkippedCompaction]) -> Table {
+    let mut table = Table::new();
+    table.load_preset("||--+-++|    ++++++");
+
+    let headers: Vec<_> = [
+        "partition_id",
+        "reason",
+        "skipped_at",
+        "estimated_bytes",
+        "limit_bytes",
+        "num_files",
+        "limit_num_files",
+    ]
+    .into_iter()
+    .map(Cell::new)
+    .collect();
+    table.set_header(headers);
+
+    for skipped_compaction in skipped_compactions {
+        let timestamp = Time::from_timestamp_nanos(skipped_compaction.skipped_at);
+
+        table.add_row(vec![
+            Cell::new(skipped_compaction.partition_id.to_string()),
+            Cell::new(&skipped_compaction.reason),
+            Cell::new(timestamp.to_rfc3339()),
+            Cell::new(skipped_compaction.estimated_bytes.to_string()),
+            Cell::new(skipped_compaction.limit_bytes.to_string()),
+            Cell::new(skipped_compaction.num_files.to_string()),
+            Cell::new(skipped_compaction.limit_num_files.to_string()),
+        ]);
+    }
+
+    table
 }
