ada347486c86d18b77325c368a45ff863297aba2
Brandon Pfeifer <bpfeifer@influxdata.com>
2022-10-26 17:17:43 -0400

    feat: upgrade flux to v0.179.0
    This removes all invocations of "flux.RegisterOpSpec". According
    to flux@e39096d5, "flux.RegisterOpSpec" does nothing in the
    current version of flux and was removed.

--- flux/stdlib/influxdata/influxdb/to.go
+++ flux/stdlib/influxdata/influxdb/to.go
@@ -40,7 +40,6 @@ type ToOpSpec struct {
 func init() {
 	toSignature := runtime.MustLookupBuiltinType("influxdata/influxdb", influxdb.ToKind)
 	runtime.ReplacePackageValue("influxdata/influxdb", "to", flux.MustValue(flux.FunctionValueWithSideEffect(ToKind, createToOpSpec, toSignature)))
-	flux.RegisterOpSpec(ToKind, func() flux.OperationSpec { return &ToOpSpec{} })
 	plan.RegisterProcedureSpecWithSideEffect(ToKind, newToProcedure, ToKind)
 	execute.RegisterTransformation(ToKind, createToTransformation)
 }
