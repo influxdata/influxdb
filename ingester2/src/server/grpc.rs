a0ab78298ffbf6694ea104725c269b0e31799f30
Dom Dwyer <dom@itsallbroken.com>
2022-11-25 11:58:32 +0100

    feat(ingester2): gRPC methods & type-erased init
    This commit implements the gRPC direct-write RPC interface (largely
    copied from the ingester crate), and adds a much improved RPC query
    handler.
    
    Compared to the ingester crate, the query API is now split into two
    defined halves - the API handler side, and types necessary to support it
    (server/grpc/query.rs) and the Ingester query execution side (a stub in
    query/exec.rs). These two halves maintain a separation of concerns, and
    are interfaced by an abstract QueryExec trait (in query/trait.rs).
    
    I also added the catalog RPC interface as it is currently exposed on the
    ingester, though I am unsure if it is used by anything.
    
    This commit also introduces the "init" module, and the
    IngesterRpcInterface trait within it. This trait forms the public
    ingester2 crate API, defining the complete set of methods external
    crates can expect to utilise in a stable, unchanging and decoupled way.
    
    The IngesterRpcInterface trait also serves as a method of type-erasure
    on the underlying handler implementations, avoiding the need to
    expose/pub the types, abstractions, and internal implementation details
    of the ingester to external crates.

---
+++ ingester2/src/server/grpc.rs
@@ -0,0 +1,87 @@
+//! gRPC service implementations for `ingester`.
+
+mod query;
+mod rpc_write;
+
+use std::{fmt::Debug, sync::Arc};
+
+use arrow_flight::flight_service_server::FlightServiceServer;
+use generated_types::influxdata::iox::{
+    catalog::v1::catalog_service_server::CatalogServiceServer,
+    ingester::v1::write_service_server::WriteServiceServer,
+};
+use iox_catalog::interface::Catalog;
+use service_grpc_catalog::CatalogService;
+
+use crate::{dml_sink::DmlSink, init::IngesterRpcInterface, query::QueryExec};
+
+use self::rpc_write::RpcWrite;
+
+/// This type is responsible for injecting internal dependencies that SHOULD NOT
+/// leak outside of the ingester crate into public gRPC handlers.
+///
+/// Configuration and external dependencies SHOULD be injected through the
+/// respective gRPC handler constructor method.
+#[derive(Debug)]
+pub(crate) struct GrpcDelegate<D, Q> {
+    dml_sink: Arc<D>,
+    query_exec: Arc<Q>,
+}
+
+impl<D, Q> GrpcDelegate<D, Q>
+where
+    D: DmlSink + 'static,
+    Q: QueryExec + 'static,
+{
+    /// Initialise a new [`GrpcDelegate`].
+    pub(crate) fn new(dml_sink: Arc<D>, query_exec: Arc<Q>) -> Self {
+        Self {
+            dml_sink,
+            query_exec,
+        }
+    }
+}
+
+/// Implement the type-erasure trait to hide internal types from crate-external
+/// callers.
+impl<D, Q> IngesterRpcInterface for GrpcDelegate<D, Q>
+where
+    D: DmlSink + 'static,
+    Q: QueryExec + 'static,
+{
+    type CatalogHandler = CatalogService;
+    type WriteHandler = RpcWrite<Arc<D>>;
+    type FlightHandler = query::FlightService<Arc<Q>>;
+
+    /// Acquire a [`CatalogService`] gRPC service implementation.
+    ///
+    /// [`CatalogService`]: generated_types::influxdata::iox::catalog::v1::catalog_service_server::CatalogService.
+    fn catalog_service(
+        &self,
+        catalog: Arc<dyn Catalog>,
+    ) -> CatalogServiceServer<Self::CatalogHandler> {
+        CatalogServiceServer::new(CatalogService::new(catalog))
+    }
+
+    /// Return a [`WriteService`] gRPC implementation.
+    ///
+    /// [`WriteService`]: generated_types::influxdata::iox::catalog::v1::write_service_server::WriteService.
+    fn write_service(&self) -> WriteServiceServer<Self::WriteHandler> {
+        WriteServiceServer::new(RpcWrite::new(Arc::clone(&self.dml_sink)))
+    }
+
+    /// Return an Arrow [`FlightService`] gRPC implementation.
+    ///
+    /// [`FlightService`]: arrow_flight::flight_service_server::FlightService
+    fn query_service(
+        &self,
+        max_simultaneous_requests: usize,
+        metrics: &metric::Registry,
+    ) -> FlightServiceServer<Self::FlightHandler> {
+        FlightServiceServer::new(query::FlightService::new(
+            Arc::clone(&self.query_exec),
+            max_simultaneous_requests,
+            metrics,
+        ))
+    }
+}
