e0ecacf6cc5d51b26eaee7d212879355ba3dd7c8
Andrew Lamb <alamb@influxdata.com>
2022-12-12 07:14:00 -0500

    chore: Update DataFusion (get median fix and automatic string to timestamp coercion) (#6363)
    * chore: Update DataFusion pin to get median fix
    
    * chore: Update for new Expr node
    
    * test: add test for median
    
    * test: add test for coercion of strings to timestamps
    
    * chore: Run cargo hakari tasks
    
    Co-authored-by: CircleCI[bot] <circleci@influxdata.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- query_tests/cases/in/basic.sql
+++ query_tests/cases/in/basic.sql
@@ -13,6 +13,12 @@ SELECT "user", region from cpu;
 -- predicate on CPU
 SELECT * from cpu where time > to_timestamp('1970-01-01T00:00:00.000000120+00:00');
 
+-- predicate on CPU with explicit coercion (cast string to timestamp)
+SELECT * from cpu where time > '1970-01-01T00:00:00'::timestamp ORDER BY time;
+
+-- predicate on CPU with automatic coercion (comparing time to string)
+SELECT * from cpu where time > '1970-01-01T00:00:00' ORDER BY time;
+
 -- projection and predicate
 -- expect that to get a subset of the columns and in the order specified
 SELECT "user", region from cpu where time > to_timestamp('1970-01-01T00:00:00.000000120+00:00');
@@ -20,6 +26,8 @@ SELECT "user", region from cpu where time > to_timestamp('1970-01-01T00:00:00.00
 -- basic grouping
 SELECT count(*) from cpu group by region;
 
-
 -- select from a different measurement
 SELECT * from disk;
+
+-- MEDIAN should work
+select MEDIAN("user"), region from cpu group by region;
