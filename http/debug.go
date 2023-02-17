23dba8b8a430a4e1d977738a439332ba2cb70d7f
wiedld <dlw405@gmail.com>
2023-02-17 14:50:37 -0800

    feat(monitor-ci/415): get oss-e2es working locally in UI repo

--- http/debug.go
+++ http/debug.go
@@ -2,7 +2,12 @@ package http
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+
+	"github.com/influxdata/influxdb/v2"
+	"github.com/influxdata/influxdb/v2/pkg/httpc"
+	"github.com/influxdata/influxdb/v2/tenant"
 )
 
 // Flusher flushes data from a store to reset; used for testing.
@@ -10,13 +15,39 @@ type Flusher interface {
 	Flush(ctx context.Context)
 }
 
-// DebugFlush clears all services for testing.
-func DebugFlush(ctx context.Context, next http.Handler, f Flusher) http.HandlerFunc {
+func Debug(ctx context.Context, next http.Handler, f Flusher) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path == "/debug/flush" {
+			// DebugFlush clears all services for testing.
 			f.Flush(ctx)
 			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			w.WriteHeader(http.StatusOK)
+			return
+		}
+		if r.URL.Path == "/debug/provision" {
+			client, err := httpc.New(httpc.WithAddr("http://localhost:8086"))
+			onboarding := tenant.OnboardClientService{Client: client}
+			data := &influxdb.OnboardingRequest{
+				User:     "dev_user",
+				Password: "password",
+				Org:      "InfluxData",
+				Bucket:   "project",
+			}
+			res, err := onboarding.OnboardInitialUser(ctx, data)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			body, err := json.Marshal(res)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write(body)
 			return
 		}
 		next.ServeHTTP(w, r)
