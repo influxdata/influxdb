f5c50094ca152d1e98b8e97fbb9e412e34393d71
Dom Dwyer <dom@itsallbroken.com>
2023-03-14 17:25:08 +0100

    refactor: owned persisted SequenceNumberSet copy
    Allow an owned SequenceNumberSet to be destructured from an owned
    CompletedPersist notification.
    
    Additionally allow an owned SequenceNumberSet to be obtained from a
    (potentially) shared Arc<CompletedPersist> in a memory efficient / no
    cloning way. A CompletedPersist notification is typically wrapped in an
    Arc, and at time of writing, typically referenced only once.

--- ingester2/src/persist/completion_observer.rs
+++ ingester2/src/persist/completion_observer.rs
@@ -68,6 +68,23 @@ impl CompletedPersist {
     pub(crate) fn sequence_numbers(&self) -> &SequenceNumberSet {
         &self.sequence_numbers
     }
+
+    /// Consume `self`, returning ownership of the inner [`SequenceNumberSet`].
+    pub(crate) fn into_sequence_numbers(self) -> SequenceNumberSet {
+        self.sequence_numbers
+    }
+
+    /// Obtain an owned inner [`SequenceNumberSet`] from an [`Arc`] wrapped
+    /// [`CompletedPersist`] in the most memory-efficient way possible at call
+    /// time.
+    ///
+    /// This method attempts to unwrap an [`Arc`]-wrapped [`CompletedPersist`]
+    /// if `self `is the only reference, otherwise the shared set is cloned.
+    pub(crate) fn owned_sequence_numbers(self: Arc<Self>) -> SequenceNumberSet {
+        Arc::try_unwrap(self)
+            .map(|v| v.into_sequence_numbers())
+            .unwrap_or_else(|v| v.sequence_numbers().clone())
+    }
 }
 
 /// A no-op implementation of the [`PersistCompletionObserver`] trait.
@@ -116,5 +133,47 @@ pub(crate) mod mock {
         async fn persist_complete(&self, note: Arc<CompletedPersist>) {
             self.calls.lock().push(Arc::clone(&note));
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use data_types::SequenceNumber;
+
+    use super::*;
+
+    #[test]
+    fn test_owned_sequence_numbers_only_ref() {
+        let orig_set = [SequenceNumber::new(42)]
+            .into_iter()
+            .collect::<SequenceNumberSet>();
+
+        let note = Arc::new(CompletedPersist::new(
+            NamespaceId::new(1),
+            TableId::new(2),
+            PartitionId::new(3),
+            orig_set.clone(),
+        ));
+
+        assert_eq!(orig_set, note.owned_sequence_numbers())
+    }
+
+    #[test]
+    fn test_owned_sequence_numbers_many_ref() {
+        let orig_set = [SequenceNumber::new(42)]
+            .into_iter()
+            .collect::<SequenceNumberSet>();
+
+        let note = Arc::new(CompletedPersist::new(
+            NamespaceId::new(1),
+            TableId::new(2),
+            PartitionId::new(3),
+            orig_set.clone(),
+        ));
+
+        let note2 = Arc::clone(&note);
+
+        assert_eq!(orig_set, note.owned_sequence_numbers());
+        assert_eq!(orig_set, note2.owned_sequence_numbers());
     }
 }
