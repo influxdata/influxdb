f834df004bb8c1c43d7fb96b116ec16b7d9fefa2
Joe-Blount <jblount@influxdata.com>
2023-03-28 10:23:57 -0500

    chore: insta churn

--- compactor2/src/components/split_or_compact/metrics.rs
+++ compactor2/src/components/split_or_compact/metrics.rs
@@ -161,7 +161,7 @@ mod tests {
             U64Histogram,
             METRIC_NAME_FILES_TO_SPLIT,
             samples = 1,
-            sum = 2,
+            sum = 3,
         );
         assert_counter!(
             registry,
