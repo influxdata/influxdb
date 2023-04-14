f0615f59d8dca4fe3e78b5e1753018e77baf9b07
Fraser Savage <fsavage@influxdata.com>
2023-04-14 09:55:36 +0100

    docs(router): Update documentation for change to NamespaceCache semantics
    The changes to `put_schema()`'s change the signature and introduces a
    side-effect transform. This needed to be documented at the trait level.

--- router/src/namespace_cache.rs
+++ router/src/namespace_cache.rs
@@ -30,8 +30,14 @@ pub trait NamespaceCache: Debug + Send + Sync {
         namespace: &NamespaceName<'static>,
     ) -> Result<Arc<NamespaceSchema>, Self::ReadError>;
 
-    /// Place `schema` in the cache, unconditionally overwriting any existing
-    /// [`NamespaceSchema`] mapped to `namespace`.
+    /// Place `schema` in the cache, including any missing columns present for the
+    /// pre-existing [`NamespaceSchema`] mapped to `namespace`, returning the
+    /// previous cache entry (if any) and the new entry complete with
+    /// modifications.
+    ///
+    /// If the entry in the cache for `namespace` resolves to a different ID than
+    /// the incoming `schema` the write unconditionally overwrites any pre-existing
+    /// entry for `namespace`.
     fn put_schema(
         &self,
         namespace: NamespaceName<'static>,
