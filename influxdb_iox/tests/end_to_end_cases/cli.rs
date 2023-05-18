c263585d942666a7c474ac20a37fe14ee45cc167
Fraser Savage <fsavage@influxdata.com>
2023-05-18 17:10:54 +0100

    feat(cli): Add table ID `debug wal regenerate-lp` command implementation
    This adds a command to `influxdb_iox` that can take a WAL segment file
    and regenerate all write operation entries, writing to stdout or namespaced
    files within a target directory, using table ID as the measurement name
    in the case where there is no catalog access at point of regeneration.

--- influxdb_iox/tests/end_to_end_cases/cli.rs
+++ influxdb_iox/tests/end_to_end_cases/cli.rs
@@ -1,15 +1,20 @@
 //! Tests CLI commands
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::sync::Arc;
+use std::time::{Duration, Instant};
 
-use super::get_object_store_id;
 use assert_cmd::Command;
+use assert_matches::assert_matches;
 use futures::FutureExt;
 use predicates::prelude::*;
-use std::time::{Duration, Instant};
 use tempfile::tempdir;
 use test_helpers_end_to_end::{
     maybe_skip_integration, AddAddrEnv, BindAddresses, MiniCluster, ServerType, Step, StepTest,
-    StepTestState,
+    StepTestState, TestConfig,
 };
+
+use super::get_object_store_id;
 
 #[tokio::test]
 async fn default_mode_is_run_all_in_one() {
@@ -1001,6 +1006,121 @@ async fn namespace_update_service_limit() {
                                 .and(predicate::str::contains(r#""maxTables": 1337"#))
                                 .and(predicate::str::contains(r#""maxColumnsPerTable": 42"#)),
                         );
+                }
+                .boxed()
+            })),
+        ],
+    )
+    .run()
+    .await
+}
+
+#[tokio::test]
+async fn write_lp_from_wal() {
+    use std::fs;
+
+    test_helpers::maybe_start_logging();
+    let database_url = maybe_skip_integration!();
+
+    let ingester_config = TestConfig::new_ingester_never_persist(&database_url);
+    let router_config = TestConfig::new_router(&ingester_config);
+    let wal_dir = Arc::new(std::path::PathBuf::from(
+        ingester_config
+            .wal_dir()
+            .as_ref()
+            .expect("missing WAL dir")
+            .path(),
+    ));
+
+    let mut cluster = MiniCluster::new()
+        .with_ingester(ingester_config)
+        .await
+        .with_router(router_config)
+        .await;
+
+    StepTest::new(
+        &mut cluster,
+        vec![
+            Step::Custom(Box::new(|state: &mut StepTestState| {
+                async {
+                    let router_addr = state.cluster().router().router_http_base().to_string();
+                    let namespace = state.cluster().namespace();
+
+                    Command::cargo_bin("influxdb_iox")
+                        .unwrap()
+                        .arg("-v")
+                        .arg("-h")
+                        .arg(&router_addr)
+                        .arg("write")
+                        .arg(namespace)
+                        .arg("../test_fixtures/lineproto/air_and_water.lp")
+                        .assert()
+                        .success()
+                        .stdout(predicate::str::contains("1243 Bytes OK"));
+                }
+                .boxed()
+            })),
+            Step::Custom(Box::new(move |_state: &mut StepTestState| {
+                let wal_dir = Arc::clone(&wal_dir);
+                async move {
+                    let mut reader =
+                        fs::read_dir(wal_dir.as_path()).expect("failed to read WAL directory");
+                    let segment_file_path = reader
+                        .next()
+                        .expect("no segment file found")
+                        .unwrap()
+                        .path();
+                    assert_matches!(reader.next(), None);
+
+                    let out_dir = test_helpers::tmp_dir()
+                        .expect("failed to create temp directory for line proto output");
+
+                    Command::cargo_bin("influxdb_iox")
+                        .unwrap()
+                        .arg("-vv")
+                        .arg("debug")
+                        .arg("wal")
+                        .arg("regenerate-lp")
+                        .arg("-o")
+                        .arg(
+                            out_dir
+                                .path()
+                                .to_str()
+                                .expect("should be able to get temp directory as string"),
+                        )
+                        .arg(
+                            segment_file_path
+                                .to_str()
+                                .expect("should be able to get segment file path as string"),
+                        )
+                        .assert()
+                        .success();
+
+                    // TODO(savage): Assert file size is the same after table-name aware implementation is done
+                    let mut reader =
+                        fs::read_dir(out_dir.path()).expect("failed to read output directory");
+                    let regenerated_file = BufReader::new(
+                        File::open(
+                            reader
+                                .next()
+                                .expect("no regenerated files found")
+                                .unwrap()
+                                .path(),
+                        )
+                        .expect("should be able to open regenerated line proto file"),
+                    );
+
+                    assert_matches!(reader.next(), None);
+
+                    let original_file = BufReader::new(
+                        File::open("../test_fixtures/lineproto/air_and_water.lp")
+                            .expect("should be able to read input line proto"),
+                    );
+
+                    assert_eq!(
+                        original_file.lines().count(),
+                        regenerated_file.lines().count()
+                    );
                 }
                 .boxed()
             })),
