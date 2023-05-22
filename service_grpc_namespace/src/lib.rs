9696b754761a8e4d8d286c8821525f9db46dc04d
Dom Dwyer <dom@itsallbroken.com>
2023-05-22 14:58:25 +0200

    ci: add missing lints to service_grpc_namespace
    This crate was missing some of the common lints we use everywhere else.

--- service_grpc_namespace/src/lib.rs
+++ service_grpc_namespace/src/lib.rs
@@ -1,4 +1,17 @@
 //! Implementation of the namespace gRPC service
+
+#![deny(rustdoc::broken_intra_doc_links, rust_2018_idioms)]
+#![warn(
+    clippy::clone_on_ref_ptr,
+    clippy::dbg_macro,
+    clippy::explicit_iter_loop,
+    // See https://github.com/influxdata/influxdb_iox/pull/1671
+    clippy::future_not_send,
+    clippy::todo,
+    clippy::use_self,
+    missing_debug_implementations,
+)]
+
 use std::sync::Arc;
 
 use data_types::{Namespace as CatalogNamespace, NamespaceName};
