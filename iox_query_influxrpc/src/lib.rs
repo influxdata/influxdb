adb135d47c5a02302a72c8293c512adbe6175b55
Dom Dwyer <dom@itsallbroken.com>
2023-05-22 13:58:40 +0200

    ci: add missing lints to iox_query_influxrpc
    This crate was missing some of the common lints we use everywhere else.

--- iox_query_influxrpc/src/lib.rs
+++ iox_query_influxrpc/src/lib.rs
@@ -1,5 +1,18 @@
 //! Query frontend for InfluxDB Storage gRPC requests
 
+#![deny(rustdoc::broken_intra_doc_links, rust_2018_idioms)]
+#![warn(
+    clippy::clone_on_ref_ptr,
+    clippy::dbg_macro,
+    clippy::explicit_iter_loop,
+    // See https://github.com/influxdata/influxdb_iox/pull/1671
+    clippy::future_not_send,
+    clippy::todo,
+    clippy::use_self,
+    missing_copy_implementations,
+    missing_debug_implementations,
+)]
+
 use arrow::datatypes::DataType;
 use data_types::ChunkId;
 use datafusion::{
