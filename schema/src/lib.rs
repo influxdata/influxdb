7bddba8eae6ede22ef774903c7f020d8989930b6
Dom Dwyer <dom@itsallbroken.com>
2023-05-22 14:41:07 +0200

    ci: add missing lints to schema
    This crate was missing some of the common lints we use everywhere else.

--- schema/src/lib.rs
+++ schema/src/lib.rs
@@ -1,4 +1,18 @@
 //! This module contains the schema definition for IOx
+
+#![deny(rustdoc::broken_intra_doc_links, rustdoc::bare_urls, rust_2018_idioms)]
+#![allow(clippy::clone_on_ref_ptr)]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    clippy::explicit_iter_loop,
+    // See https://github.com/influxdata/influxdb_iox/pull/1671
+    clippy::future_not_send,
+    clippy::clone_on_ref_ptr,
+    clippy::todo,
+    clippy::dbg_macro
+)]
+
 use std::{
     cmp::Ordering,
     collections::HashMap,
