4f84b2122cb8c23836d2d7514254c74bbb531181
Dom Dwyer <dom@itsallbroken.com>
2023-05-22 14:38:51 +0200

    refactor(lints): add missing lints to parquet_to_line_protocol
    Adds the standard lints to parquet_to_line_protocol and fixes any lint
    failures.
    
    Note this doesn't include the normal "document things" lint, because
    there's a load of missing docs

--- parquet_to_line_protocol/src/lib.rs
+++ parquet_to_line_protocol/src/lib.rs
@@ -1,5 +1,18 @@
 //! Code that can convert between parquet files and line protocol
 
+#![deny(rustdoc::broken_intra_doc_links, rustdoc::bare_urls, rust_2018_idioms)]
+#![allow(clippy::clone_on_ref_ptr)]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    clippy::explicit_iter_loop,
+    // See https://github.com/influxdata/influxdb_iox/pull/1671
+    clippy::future_not_send,
+    clippy::clone_on_ref_ptr,
+    clippy::todo,
+    clippy::dbg_macro,
+)]
+
 use datafusion::{
     arrow::datatypes::SchemaRef as ArrowSchemaRef,
     datasource::{
@@ -94,7 +107,7 @@ pub enum Error {
 /// protocol (aka lines are not split across the buffers)
 pub async fn convert_file<P>(path: P) -> Result<BoxStream<'static, Result<Vec<u8>>>>
 where
-    P: AsRef<Path>,
+    P: AsRef<Path> + Send,
 {
     let path = path.as_ref();
     let object_store_path =
@@ -170,6 +183,18 @@ pub struct ParquetFileReader {
 
     /// DataFusion configuration, such as the target batchsize, etc
     session_ctx: SessionContext,
+}
+
+impl std::fmt::Debug for ParquetFileReader {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ParquetFileReader")
+            .field("object_store", &self.object_store)
+            .field("object_store_url", &self.object_store_url)
+            .field("object_meta", &self.object_meta)
+            .field("schema", &self.schema)
+            .field("session_ctx", &"<CONTEXT>")
+            .finish()
+    }
 }
 
 impl ParquetFileReader {
