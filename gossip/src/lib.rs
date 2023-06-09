5c191ce6cfb75f500a4f6890901508ab1100119a
Dom Dwyer <dom@itsallbroken.com>
2023-06-09 14:42:46 +0200

    ci: enable standard lint set
    Adds the somewhat "standard" lint set we use to the gossip lib.

--- gossip/src/lib.rs
+++ gossip/src/lib.rs
@@ -1,1 +1,16 @@
+//! A work-in-progress, simple gossip primitive for metadata distribution
+//! between IOx nodes.
 
+#![deny(rustdoc::broken_intra_doc_links, rust_2018_idioms)]
+#![warn(
+    clippy::clone_on_ref_ptr,
+    clippy::dbg_macro,
+    clippy::explicit_iter_loop,
+    clippy::future_not_send,
+    clippy::todo,
+    clippy::use_self,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    unused_crate_dependencies,
+    missing_docs
+)]
