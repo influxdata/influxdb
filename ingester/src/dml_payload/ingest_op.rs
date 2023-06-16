198a47da5339ec7b3018fb109923ef30941d447a
Fraser Savage <fsavage@influxdata.com>
2023-06-16 10:42:43 +0100

    refactor(ingester): Implement `From<&WriteOperation>` for `DmlWrite`
    This commit implements the `From` trait to allow quick conversion from
    a `&WriteOperation` back to an owned `DmlWrite`. This conversion
    copies all the record batches and should be removed once WAL encoding
    can be done from `&WriteOperation`.

--- ingester/src/dml_payload/ingest_op.rs
+++ ingester/src/dml_payload/ingest_op.rs
@@ -1,5 +1,5 @@
 use data_types::{NamespaceId, PartitionKey, SequenceNumber, TableId};
-use dml::{DmlOperation, DmlWrite};
+use dml::{DmlMeta, DmlOperation, DmlWrite};
 use hashbrown::HashMap;
 use mutable_batch::MutableBatch;
 use trace::ctx::SpanContext;
@@ -29,10 +29,19 @@ impl IngestOp {
             Self::Write(w) => w.namespace,
         }
     }
+
+    // TODO(savage): Consider removing the use of these and requiring users to
+    //  match on the op type.
+    pub fn span_context(&self) -> Option<&SpanContext> {
+        match self {
+            Self::Write(w) => w.span_context.as_ref(),
+        }
+    }
 }
 
 /// A decoded representation of the data contained by an RPC write
 /// represented by an [`IngestOp::Write`]
+#[derive(Debug)]
 pub struct WriteOperation {
     namespace: NamespaceId,
 
@@ -65,20 +74,49 @@ impl WriteOperation {
             span_context,
         }
     }
+
+    pub fn partition_key(&self) -> &PartitionKey {
+        &self.partition_key
+    }
+
+    pub fn tables(&self) -> impl Iterator<Item = (&TableId, &TableData)> {
+        self.tables.iter()
+    }
+
+    pub fn into_tables(self) -> impl Iterator<Item = (TableId, TableData)> {
+        self.tables.into_iter()
+    }
 }
 
-// TODO(savage): Temporary [`From`] implementation to assist in switchover
+// TODO(savage): Temporary `From` implementation to assist in switchover
+// within ingester code. This is deeply inefficient.
+impl From<&WriteOperation> for DmlWrite {
+    fn from(value: &WriteOperation) -> Self {
+        Self::new(
+            value.namespace,
+            value
+                .tables
+                .iter()
+                .map(|(table_id, data)| (*table_id, data.partitioned_data.data.clone()))
+                .collect(),
+            value.partition_key.clone(),
+            DmlMeta::unsequenced(value.span_context.clone()),
+        )
+    }
+}
+
+// TODO(savage): Temporary `From` implementation to assist in switchover
 // within ingester code. This should be removed in favour of constructing all
 // [`WriteOperation`]s directly
 impl From<DmlWrite> for WriteOperation {
     fn from(dml_write: DmlWrite) -> Self {
         let namespace_id = dml_write.namespace_id();
-        let partition_key = dml_write.partition_key().to_owned();
+        let partition_key = dml_write.partition_key().clone();
         let sequence_number = dml_write
             .meta()
             .sequence()
             .expect("tried to create write operation from unsequenced DML write");
-        let span_context = dml_write.meta().span_context().map(SpanContext::to_owned);
+        let span_context = dml_write.meta().span_context().map(SpanContext::clone);
 
         Self::new(
             namespace_id,
@@ -105,6 +143,7 @@ impl From<DmlWrite> for WriteOperation {
 
 /// A container for all data for an individual table as part of a write
 /// operation
+#[derive(Debug)]
 pub struct TableData {
     table: TableId,
     /// The partitioned data for `table` in the write. Currently data is
@@ -120,10 +159,23 @@ impl TableData {
             partitioned_data,
         }
     }
+
+    pub fn table(&self) -> TableId {
+        self.table
+    }
+
+    pub fn partitioned_data(&self) -> &PartitionedData {
+        &self.partitioned_data
+    }
+
+    pub fn into_partitioned_data(self) -> PartitionedData {
+        self.partitioned_data
+    }
 }
 
 /// Partitioned data belonging to a write, sequenced individually from
 /// other [`PartitionedData`]
+#[derive(Debug)]
 pub struct PartitionedData {
     sequence_number: SequenceNumber,
     data: MutableBatch,
@@ -135,5 +187,13 @@ impl PartitionedData {
             sequence_number,
             data,
         }
+    }
+
+    pub fn sequence_number(&self) -> SequenceNumber {
+        self.sequence_number
+    }
+
+    pub fn data(self) -> MutableBatch {
+        self.data
     }
 }
