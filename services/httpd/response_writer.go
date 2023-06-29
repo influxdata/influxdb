2dc3dcb3d1795e0c6091e5b113e6297836ce5f2f
davidby-influx <72418212+davidby-influx@users.noreply.github.com>
2023-06-29 12:00:41 -0700

    fix: do not escape CSV output (#24311)
    CSV output is incorrectly escaped.
    Add a boolean flag to tag output
    functions to prevent this.
    
    closes https://github.com/influxdata/influxdb/issues/24309

--- services/httpd/response_writer.go
+++ services/httpd/response_writer.go
@@ -225,7 +225,7 @@ func (f *csvFormatter) WriteResponse(w io.Writer, resp Response) (err error) {
 			f.columns[0] = row.Name
 			f.columns[1] = ""
 			if len(row.Tags) > 0 {
-				hashKey := models.NewTags(row.Tags).HashKey()
+				hashKey := models.NewTags(row.Tags).HashKey(false)
 				if len(hashKey) > 0 {
 					f.columns[1] = string(hashKey[1:])
 				}
