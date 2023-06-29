2dacd0e7df4c63df88e9f3c4bc18a9d461cc9652
Joe-Blount <73478756+Joe-Blount@users.noreply.github.com>
2023-06-29 15:49:35 -0500

    feat: conditionally expand lookback for active partitions to avoid skipping any (#8112)
    * feat: conditionally expand lookback for active partitions to avoid skipping any
    
    * chore: comment adjustments
    
    * chore: rust fmt

--- compactor_scheduler/src/local_scheduler/partitions_source/catalog_to_compact.rs
+++ compactor_scheduler/src/local_scheduler/partitions_source/catalog_to_compact.rs
@@ -1,10 +1,10 @@
-use std::{fmt::Display, sync::Arc, time::Duration};
+use std::{fmt::Display, ops::Sub, sync::Arc, sync::Mutex, time::Duration};
 
 use async_trait::async_trait;
 use backoff::{Backoff, BackoffConfig};
 use data_types::PartitionId;
 use iox_catalog::interface::Catalog;
-use iox_time::TimeProvider;
+use iox_time::{Time, TimeProvider};
 
 use crate::PartitionsSource;
 
@@ -21,8 +21,19 @@ use crate::PartitionsSource;
 pub(crate) struct CatalogToCompactPartitionsSource {
     backoff_config: BackoffConfig,
     catalog: Arc<dyn Catalog>,
+
+    /// min_threshold is the duration subtracted from now to determine the start range of the query.
+    /// If its been a while since the last query, or this is the first query, this value will be
+    /// increased up to 3x.
     min_threshold: Duration,
+
+    /// max_threshold is the duration subtracted from now to determine the end range of the query.
     max_threshold: Option<Duration>,
+
+    /// last_maximum_time remembers the ending time of the last query.  If previous round of compaction took
+    /// too long, knowing the ending time of the last query helps us avoid skipping partitions with the next query.
+    last_maximum_time: Mutex<Time>,
+
     time_provider: Arc<dyn TimeProvider>,
 }
 
@@ -40,6 +51,7 @@ impl CatalogToCompactPartitionsSource {
             catalog,
             min_threshold,
             max_threshold,
+            last_maximum_time: Mutex::new(Time::from_timestamp_nanos(0)),
             time_provider,
         }
     }
@@ -54,8 +66,32 @@ impl Display for CatalogToCompactPartitionsSource {
 #[async_trait]
 impl PartitionsSource for CatalogToCompactPartitionsSource {
     async fn fetch(&self) -> Vec<PartitionId> {
-        let minimum_time = self.time_provider.now() - self.min_threshold;
-        let maximum_time = self.max_threshold.map(|max| self.time_provider.now() - max);
+        let mut minimum_time = self.time_provider.now() - self.min_threshold;
+        let maximum_time: Option<Time>;
+
+        // scope the locking to just maintenance of last_maximum_time, not the query
+        {
+            // we're going check the time range we'd like to query for against the end time of the last query.
+            let mut last = self.last_maximum_time.lock().unwrap();
+
+            // if the last query ended further back in time than this query starts, we're about to skip something.
+            if *last < minimum_time {
+                if minimum_time.sub(*last) < self.min_threshold * 3 {
+                    // the end of the last query says we're skipping less than 3x our configured lookback, so
+                    // back up and query everything since the last query.
+                    minimum_time = *last;
+                } else {
+                    // end of the last query says we're skiping a lot.  We should limit how far we lookback to avoid
+                    // returning all partitions, so we'll just backup 3x the configured lookback.
+                    // this might skip something (until cold compaction), but we need a limit in how far we look back.
+                    minimum_time = self.time_provider.now() - self.min_threshold * 3;
+                }
+            }
+            maximum_time = self.max_threshold.map(|max| self.time_provider.now() - max);
+
+            // save the maximum time used in this query to self.last_maximum_time
+            *last = maximum_time.unwrap_or(self.time_provider.now());
+        }
 
         Backoff::new(&self.backoff_config)
             .retry_all_errors("partitions_to_compact", || async {
@@ -86,7 +122,8 @@ mod tests {
         catalog: Arc<MemCatalog>,
         min_threshold: Duration,
         max_threshold: Option<Duration>,
-        expected_ids: &[i64],
+        first_expected_ids: &[i64], // expected values on first fetch, which does a 3x on min_threshold
+        second_expected_ids: &[i64], // expected values on second fetch, which uses min_threshold unmodified
     ) {
         let time_provider = catalog.time_provider();
 
@@ -103,9 +140,19 @@ mod tests {
 
         assert_eq!(
             actual_partition_ids,
-            partition_ids(expected_ids),
+            partition_ids(first_expected_ids),
             "CatalogToCompact source with min_threshold {min_threshold:?} and \
-            max_threshold {max_threshold:?} failed",
+            max_threshold {max_threshold:?} failed (first fetch, 3x lookback)",
+        );
+
+        let mut actual_partition_ids = partitions_source.fetch().await;
+        actual_partition_ids.sort();
+
+        assert_eq!(
+            actual_partition_ids,
+            partition_ids(second_expected_ids),
+            "CatalogToCompact source with min_threshold {min_threshold:?} and \
+            max_threshold {max_threshold:?} failed (second fetch, specified lookback)",
         );
     }
 
@@ -128,13 +175,13 @@ mod tests {
         }
 
         let one_minute = Duration::from_secs(60);
-        fetch_test(Arc::clone(&catalog), one_minute, None, &[]).await;
+        fetch_test(Arc::clone(&catalog), one_minute, None, &[], &[]).await;
 
         let four_hours = Duration::from_secs(60 * 60 * 4);
-        fetch_test(Arc::clone(&catalog), four_hours, None, &[1]).await;
+        fetch_test(Arc::clone(&catalog), four_hours, None, &[1, 2], &[1]).await;
 
         let seven_hours = Duration::from_secs(60 * 60 * 7);
-        fetch_test(Arc::clone(&catalog), seven_hours, None, &[1, 2]).await;
+        fetch_test(Arc::clone(&catalog), seven_hours, None, &[1, 2], &[1, 2]).await;
     }
 
     #[tokio::test]
@@ -165,16 +212,51 @@ mod tests {
         let four_hours = Duration::from_secs(60 * 60 * 4);
         let seven_hours = Duration::from_secs(60 * 60 * 7);
 
-        fetch_test(Arc::clone(&catalog), seven_hours, Some(four_hours), &[3]).await;
+        fetch_test(
+            Arc::clone(&catalog),
+            seven_hours,
+            Some(four_hours),
+            &[3],
+            &[3],
+        )
+        .await;
 
-        fetch_test(Arc::clone(&catalog), seven_hours, Some(one_hour), &[2, 3]).await;
+        fetch_test(
+            Arc::clone(&catalog),
+            seven_hours,
+            Some(one_hour),
+            &[2, 3],
+            &[2, 3],
+        )
+        .await;
 
-        fetch_test(Arc::clone(&catalog), seven_hours, Some(one_minute), &[2, 3]).await;
+        fetch_test(
+            Arc::clone(&catalog),
+            seven_hours,
+            Some(one_minute),
+            &[2, 3],
+            &[2, 3],
+        )
+        .await;
 
-        fetch_test(Arc::clone(&catalog), four_hours, Some(one_hour), &[2]).await;
+        fetch_test(
+            Arc::clone(&catalog),
+            four_hours,
+            Some(one_hour),
+            &[2, 3],
+            &[2],
+        )
+        .await;
 
-        fetch_test(Arc::clone(&catalog), four_hours, Some(one_minute), &[2]).await;
+        fetch_test(
+            Arc::clone(&catalog),
+            four_hours,
+            Some(one_minute),
+            &[2, 3],
+            &[2],
+        )
+        .await;
 
-        fetch_test(Arc::clone(&catalog), one_hour, Some(one_minute), &[]).await;
+        fetch_test(Arc::clone(&catalog), one_hour, Some(one_minute), &[], &[]).await;
     }
 }
