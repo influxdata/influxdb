bee1b45c1391c1f71ffc1063dcff3d5778943ed1
Dom Dwyer <dom@itsallbroken.com>
2023-07-10 13:48:01 +0200

    build: reuse path var
    DRY the path var.

--- gossip/build.rs
+++ gossip/build.rs
@@ -4,15 +4,13 @@ use prost_build::Config;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("proto");
+    let proto = root.join("gossip.proto");
 
-    println!(
-        "cargo:rerun-if-changed={}",
-        root.join("gossip.proto").display()
-    );
+    println!("cargo:rerun-if-changed={}", proto.display());
 
     Config::new()
         .bytes(["."])
-        .compile_protos(&[root.join("gossip.proto")], &[root])?;
+        .compile_protos(&[proto], &[root])?;
 
     Ok(())
 }
