ee063057b34e3bf6c6466f8eba6272d71d53a482
Dom Dwyer <dom@itsallbroken.com>
2023-08-17 17:37:39 +0200

    refactor(router): use gossip_schema
    Replace the bespoke schema gossip logic in the router with the reusable
    gossip_schema crate.

--- router/src/gossip/mock_schema_broadcast.rs
+++ router/src/gossip/mock_schema_broadcast.rs
@@ -1,10 +1,6 @@
 use std::{sync::Arc, time::Duration};
 
-use async_trait::async_trait;
-use generated_types::{
-    influxdata::iox::gossip::v1::{schema_message::Event, SchemaMessage},
-    prost::Message,
-};
+use generated_types::influxdata::iox::gossip::v1::schema_message::Event;
 use parking_lot::Mutex;
 use test_helpers::timeout::FutureTimeout;
 
@@ -12,33 +8,19 @@ use super::traits::SchemaBroadcast;
 
 #[derive(Debug, Default)]
 pub struct MockSchemaBroadcast {
-    payloads: Mutex<Vec<Vec<u8>>>,
+    payloads: Mutex<Vec<Event>>,
 }
 
-#[async_trait]
 impl SchemaBroadcast for Arc<MockSchemaBroadcast> {
-    async fn broadcast(&self, payload: Vec<u8>) {
+    fn broadcast(&self, payload: Event) {
         self.payloads.lock().push(payload);
     }
 }
 
 impl MockSchemaBroadcast {
-    /// Return the raw, serialised payloads.
-    pub fn raw_payloads(&self) -> Vec<Vec<u8>> {
-        self.payloads.lock().clone()
-    }
-
-    /// Return the deserialised [`Event`].
+    /// Return the broadcast [`Event`].
     pub fn messages(&self) -> Vec<Event> {
-        self.payloads
-            .lock()
-            .iter()
-            .map(|v| {
-                SchemaMessage::decode(v.as_slice())
-                    .map(|v| v.event.expect("no message in payload"))
-                    .expect("invalid gossip payload")
-            })
-            .collect()
+        self.payloads.lock().clone()
     }
 
     pub async fn wait_for_messages(&self, count: usize) {
