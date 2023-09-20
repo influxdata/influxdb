39768fa9896464648c7cf89ef1f73ed89d29d01a
Dom Dwyer <dom@itsallbroken.com>
2023-09-20 13:45:06 +0200

    feat(router): init anti-entropy merkle search tree
    Adds initialisation code to the routers to instantiate an
    AntiEntropyActor, pre-populate the Merkle Search Tree during schema
    warmup, and maintain it at runtime.

--- router/src/gossip/anti_entropy/mst/handle.rs
+++ router/src/gossip/anti_entropy/mst/handle.rs
@@ -97,11 +97,23 @@ impl AntiEntropyHandle {
         }
     }
 
+    /// Send `name` to the MST actor to observe a new schema state.
+    ///
+    /// This method is the blocking variant of the non-blocking
+    /// [`AntiEntropyHandle::observe_update()`] that waits for `name` to be
+    /// successfully enqueued (blocking if the queue is full).
+    pub async fn observe_update_blocking(&self, name: NamespaceName<'static>) {
+        self.schema_tx
+            .send(name)
+            .await
+            .expect("mst actor not running");
+    }
+
     /// Return the current content hash ([`RootHash`]) describing the set of
     /// [`NamespaceSchema`] observed so far.
     ///
     /// [`NamespaceSchema`]: data_types::NamespaceSchema
-    pub(crate) async fn content_hash(&self) -> RootHash {
+    pub async fn content_hash(&self) -> RootHash {
         let (tx, rx) = oneshot::channel();
 
         self.op_tx
