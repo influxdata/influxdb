f7b75a91a444599c61ff746c30561e1108d69853
davidby-influx <dbyrne@influxdata.com>
2023-12-18 11:36:32 -0800

    chore: rename imports for clarity

--- authorization/error.go
+++ authorization/error.go
@@ -1,67 +1,67 @@
 package authorization
 
 import (
-	go_errors "errors"
+	"errors"
 	"fmt"
 
-	"github.com/influxdata/influxdb/v2/kit/platform/errors"
+	errors2 "github.com/influxdata/influxdb/v2/kit/platform/errors"
 )
 
 var (
 	// ErrInvalidAuthID is used when the Authorization's ID cannot be encoded
-	ErrInvalidAuthID = &errors.Error{
-		Code: errors.EInvalid,
+	ErrInvalidAuthID = &errors2.Error{
+		Code: errors2.EInvalid,
 		Msg:  "authorization ID is invalid",
 	}
 
 	// ErrAuthNotFound is used when the specified auth cannot be found
-	ErrAuthNotFound = &errors.Error{
-		Code: errors.ENotFound,
+	ErrAuthNotFound = &errors2.Error{
+		Code: errors2.ENotFound,
 		Msg:  "authorization not found",
 	}
 
 	// NotUniqueIDError occurs when attempting to create an Authorization with an ID that already belongs to another one
-	NotUniqueIDError = &errors.Error{
-		Code: errors.EConflict,
+	NotUniqueIDError = &errors2.Error{
+		Code: errors2.EConflict,
 		Msg:  "ID already exists",
 	}
 
 	// ErrFailureGeneratingID occurs ony when the random number generator
 	// cannot generate an ID in MaxIDGenerationN times.
-	ErrFailureGeneratingID = &errors.Error{
-		Code: errors.EInternal,
+	ErrFailureGeneratingID = &errors2.Error{
+		Code: errors2.EInternal,
 		Msg:  "unable to generate valid id",
 	}
 
 	// ErrTokenAlreadyExistsError is used when attempting to create an authorization
 	// with a token that already exists
-	ErrTokenAlreadyExistsError = &errors.Error{
-		Code: errors.EConflict,
+	ErrTokenAlreadyExistsError = &errors2.Error{
+		Code: errors2.EConflict,
 		Msg:  "token already exists",
 	}
 )
 
 // ErrInvalidAuthIDError is used when a service was provided an invalid ID.
-func ErrInvalidAuthIDError(err error) *errors.Error {
-	return &errors.Error{
-		Code: errors.EInvalid,
+func ErrInvalidAuthIDError(err error) *errors2.Error {
+	return &errors2.Error{
+		Code: errors2.EInvalid,
 		Msg:  "auth id provided is invalid",
 		Err:  err,
 	}
 }
 
 // UnexpectedAuthIndexError is used when the error comes from an internal system.
-func UnexpectedAuthIndexError(err error) *errors.Error {
-	var e *errors.Error
-	if !go_errors.As(err, &e) {
-		e = &errors.Error{
+func UnexpectedAuthIndexError(err error) *errors2.Error {
+	var e *errors2.Error
+	if !errors.As(err, &e) {
+		e = &errors2.Error{
 			Msg:  fmt.Sprintf("unexpected error retrieving auth index; Err: %v", err),
-			Code: errors.EInternal,
+			Code: errors2.EInternal,
 			Err:  err,
 		}
 	}
 	if e.Code == "" {
-		e.Code = errors.EInternal
+		e.Code = errors2.EInternal
 	}
 	return e
 }
