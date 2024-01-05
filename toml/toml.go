99792fac6b6c0071e0f7419a842e758273571563
davidby-influx <dbyrne@influxdata.com>
2024-01-04 16:21:46 -0800

    chore: remove unnecessary change made for 1.X

--- toml/toml.go
+++ toml/toml.go
@@ -112,9 +112,8 @@ func (m *FileMode) UnmarshalText(text []byte) error {
 func (m FileMode) MarshalText() (text []byte, err error) {
 	if m != 0 {
 		return []byte(fmt.Sprintf("%04o", m)), nil
-	} else {
-		return []byte(""), nil
 	}
+	return nil, nil
 }
 
 type Group int
