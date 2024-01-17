0c2b9e7684444273616b3e2fedf9f8ff6930e4a3
Michael Gattozzi <mgattozzi@influxdata.com>
2024-01-17 16:13:02 -0500

    fix: Make paths work for ObjectStore

--- influxdb3_write/src/paths.rs
+++ influxdb3_write/src/paths.rs
@@ -1,81 +1,80 @@
 use crate::SegmentId;
 use chrono::prelude::*;
+use object_store::path::Path as ObjPath;
 use std::convert::AsRef;
 use std::ops::Deref;
 use std::path::Path;
 use std::path::PathBuf;
 
 /// File extension for catalog files
-const CATALOG_FILE_EXTENSION: &str = "json";
+pub const CATALOG_FILE_EXTENSION: &str = "json";
 
 /// File extension for parquet files
-const PARQUET_FILE_EXTENSION: &str = "parquet";
+pub const PARQUET_FILE_EXTENSION: &str = "parquet";
 
 /// File extension for segment info files
-const SEGMENT_INFO_FILE_EXTENSION: &str = "info.json";
+pub const SEGMENT_INFO_FILE_EXTENSION: &str = "info.json";
 
 /// File extension for segment wal files
-const SEGMENT_WAL_FILE_EXTENSION: &str = "wal";
+pub const SEGMENT_WAL_FILE_EXTENSION: &str = "wal";
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct CatalogFilePath(PathBuf);
+pub struct CatalogFilePath(ObjPath);
 
 impl CatalogFilePath {
-    pub fn new(prefix: impl Into<PathBuf>, sequence_number: u32) -> Self {
-        let mut path = prefix.into();
-        path.push("catalogs");
-        path.push(format!("{:010}", u32::MAX - sequence_number));
-        path.set_extension(CATALOG_FILE_EXTENSION);
+    pub fn new(segment_id: SegmentId) -> Self {
+        let path = ObjPath::from(format!(
+            "catalogs/{:010}.{}",
+            u32::MAX - segment_id.0,
+            CATALOG_FILE_EXTENSION
+        ));
         Self(path)
+    }
+
+    pub fn dir() -> Self {
+        Self(ObjPath::from("catalogs"))
     }
 }
 
 impl Deref for CatalogFilePath {
-    type Target = Path;
+    type Target = ObjPath;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl AsRef<Path> for CatalogFilePath {
-    fn as_ref(&self) -> &Path {
+impl AsRef<ObjPath> for CatalogFilePath {
+    fn as_ref(&self) -> &ObjPath {
         &self.0
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct ParquetFilePath(PathBuf);
+pub struct ParquetFilePath(ObjPath);
 
 impl ParquetFilePath {
-    pub fn new(
-        prefix: impl Into<PathBuf>,
-        db_name: &str,
-        table_name: &str,
-        date: DateTime<Utc>,
-        file_number: u32,
-    ) -> Self {
-        let mut path = prefix.into();
-        path.push("dbs");
-        path.push(db_name);
-        path.push(table_name);
-        path.push(format!("{}", date.format("%Y-%m-%d")));
-        path.push(format!("{:010}", u32::MAX - file_number));
-        path.set_extension(PARQUET_FILE_EXTENSION);
+    pub fn new(db_name: &str, table_name: &str, date: DateTime<Utc>, file_number: u32) -> Self {
+        let path = ObjPath::from(format!(
+            "dbs/{db_name}/{table_name}/{}/{:010}.{}",
+            date.format("%Y-%m-%d"),
+            u32::MAX - file_number,
+            PARQUET_FILE_EXTENSION
+        ));
         Self(path)
     }
 }
 
 impl Deref for ParquetFilePath {
-    type Target = Path;
+    type Target = ObjPath;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl AsRef<Path> for ParquetFilePath {
-    fn as_ref(&self) -> &Path {
+impl AsRef<ObjPath> for ParquetFilePath {
+    fn as_ref(&self) -> &ObjPath {
         &self.0
     }
 }
@@ -106,29 +105,30 @@ impl AsRef<Path> for SegmentWalFilePath {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SegmentInfoFilePath(ObjPath);
+
 impl SegmentInfoFilePath {
-    pub fn new(prefix: impl Into<PathBuf>, segment_id: SegmentId) -> Self {
-        let mut path = prefix.into();
-        path.push("segments");
-        path.push(format!("{:010}", u32::MAX - segment_id.0));
-        path.set_extension(SEGMENT_INFO_FILE_EXTENSION);
+    pub fn new(segment_id: SegmentId) -> Self {
+        let path = ObjPath::from(format!(
+            "segments/{:010}.{}",
+            u32::MAX - segment_id.0,
+            SEGMENT_INFO_FILE_EXTENSION
+        ));
         Self(path)
     }
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct SegmentInfoFilePath(PathBuf);
-
 impl Deref for SegmentInfoFilePath {
-    type Target = Path;
+    type Target = ObjPath;
 
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl AsRef<Path> for SegmentInfoFilePath {
-    fn as_ref(&self) -> &Path {
+impl AsRef<ObjPath> for SegmentInfoFilePath {
+    fn as_ref(&self) -> &ObjPath {
         &self.0
     }
 }
@@ -136,8 +136,8 @@ impl AsRef<Path> for SegmentInfoFilePath {
 #[test]
 fn catalog_file_path_new() {
     assert_eq!(
-        *CatalogFilePath::new("prefix/dir", 0),
-        PathBuf::from("prefix/dir/catalogs/4294967295.json").as_ref()
+        *CatalogFilePath::new(SegmentId::new(0)),
+        ObjPath::from("catalogs/4294967295.json")
     );
 }
 
@@ -145,21 +145,20 @@ fn catalog_file_path_new() {
 fn parquet_file_path_new() {
     assert_eq!(
         *ParquetFilePath::new(
-            "prefix/dir",
             "my_db",
             "my_table",
             Utc.with_ymd_and_hms(2038, 01, 19, 3, 14, 7).unwrap(),
             0
         ),
-        PathBuf::from("prefix/dir/dbs/my_db/my_table/2038-01-19/4294967295.parquet").as_ref()
+        ObjPath::from("dbs/my_db/my_table/2038-01-19/4294967295.parquet")
     );
 }
 
 #[test]
 fn segment_info_file_path_new() {
     assert_eq!(
-        *SegmentInfoFilePath::new("prefix/dir", SegmentId::new(0)),
-        PathBuf::from("prefix/dir/segments/4294967295.info.json").as_ref()
+        *SegmentInfoFilePath::new(SegmentId::new(0)),
+        ObjPath::from("segments/4294967295.info.json")
     );
 }
 
