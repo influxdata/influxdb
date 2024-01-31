f92cf98351591ec65282cc729445da662c3eb3b7
Brandon Pfeifer <bpfeifer@influxdata.com>
2024-01-31 12:57:24 -0500

    chore: download repository key to file

--- scripts/ci/run_perftest.sh
+++ scripts/ci/run_perftest.sh
@@ -27,7 +27,7 @@ install_influxdb() {
 
 install_telegraf() {
   # Install Telegraf
-  wget -qO- https://repos.influxdata.com/influxdata-archive_compat.key
+  curl -fLO https://repos.influxdata.com/influxdata-archive_compat.key
   echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
   echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 
