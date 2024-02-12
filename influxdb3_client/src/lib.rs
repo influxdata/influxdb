b1e65001bee95d61c0dea93ff75a92bf2329ed88
Trevor Hilton <thilton@influxdata.com>
2024-02-12 17:42:28 -0500

    docs: add rust docs for new influxdb3_client types
    The new Client and types related to the Client::api_v3_write_lp method
    have been documented
    
    The stub for the write_lp_from_file test was removed, to be done later

--- influxdb3_client/src/lib.rs
+++ influxdb3_client/src/lib.rs
@@ -3,6 +3,7 @@ use secrecy::{ExposeSecret, Secret};
 use serde::Serialize;
 use url::Url;
 
+/// Primary error type for the [`Client`]
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("base URL error: {0}")]
@@ -17,14 +18,24 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+/// The InfluxDB 3.0 Client
+///
+/// Convenience type for accessing the HTTP API of InfluxDB 3.0
 #[derive(Debug, Clone)]
 pub struct Client {
+    /// The base URL for making requests to a running InfluxDB 3.0 server
     base_url: Url,
+    /// The `Bearer` token to use for authenticating on each request to the server
     auth_header: Option<Secret<String>>,
+    /// A [`reqwest::Client`] for handling HTTP requests
     http_client: reqwest::Client,
 }
 
 impl Client {
+    /// Create a new [`Client`]
+    ///
+    /// A [`reqwest::Client`] is passed in order to allow for re-use of an existing
+    /// `reqwest::Client`.
     pub fn new<U: IntoUrl>(base_url: U, http_client: reqwest::Client) -> Result<Self> {
         Ok(Self {
             base_url: base_url.into_url().map_err(Error::BaseUrl)?,
@@ -33,11 +44,31 @@ impl Client {
         })
     }
 
+    /// Set the `Bearer` token that will be sent with each request to the server
     pub fn with_auth_header<S: Into<String>>(mut self, auth_header: S) -> Self {
         self.auth_header = Some(Secret::new(auth_header.into()));
         self
     }
 
+    /// Compose a request to the `/api/v3/write_lp` API
+    ///
+    /// # Example
+    /// ```no_run
+    /// # use influxdb3_client::Client;
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    /// let client = Client::new("localhost:8181", reqwest::Client::new())?;
+    /// let _ = client
+    ///     .api_v3_write_lp("db_name")
+    ///     .precision_milli()
+    ///     .accept_partial(true)
+    ///     .body("cpu,host=s1 usage=0.5")
+    ///     .send()
+    ///     .await
+    ///     .expect("send write_lp request");
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn api_v3_write_lp<S: Into<String>>(&self, db: S) -> WriteRequestBuilder<NoBody> {
         WriteRequestBuilder {
             client: self.clone(),
@@ -49,6 +80,8 @@ impl Client {
     }
 }
 
+/// The body of the request to the `/api/v3/write_lp` API
+// TODO - this should re-use the type defined in the server code.
 #[derive(Debug, Serialize)]
 struct WriteRequest<'a> {
     db: &'a str,
@@ -66,6 +99,8 @@ impl<'a, B> From<&'a WriteRequestBuilder<B>> for WriteRequest<'a> {
     }
 }
 
+/// Time series precision
+// TODO - this should re-use type from server code.
 #[derive(Debug, Copy, Clone, Serialize)]
 #[serde(rename_all = "snake_case")]
 enum Precision {
@@ -75,6 +110,9 @@ enum Precision {
     Nano,
 }
 
+/// Builder type for composing a request to `/api/v3/write_lp`
+///
+/// Produced by [`Client::api_v3_write_lp`]
 #[derive(Debug)]
 pub struct WriteRequestBuilder<B> {
     client: Client,
@@ -85,26 +123,31 @@ pub struct WriteRequestBuilder<B> {
 }
 
 impl<B> WriteRequestBuilder<B> {
+    /// Use seconds precision
     pub fn precision_seconds(mut self) -> Self {
         self.precision = Some(Precision::Second);
         self
     }
 
+    /// Use milliseconds precision
     pub fn precision_milli(mut self) -> Self {
         self.precision = Some(Precision::Milli);
         self
     }
 
+    /// Use microseconds precision
     pub fn precision_micro(mut self) -> Self {
         self.precision = Some(Precision::Micro);
         self
     }
 
+    /// Use nanoseconds precision
     pub fn precision_nano(mut self) -> Self {
         self.precision = Some(Precision::Nano);
         self
     }
 
+    /// Set the `accept_partial` parameter
     pub fn accept_partial(mut self, set_to: bool) -> Self {
         self.accept_partial = Some(set_to);
         self
@@ -112,6 +155,10 @@ impl<B> WriteRequestBuilder<B> {
 }
 
 impl WriteRequestBuilder<NoBody> {
+    /// Set the body of the request to the `/api/v3/write_lp` API
+    ///
+    /// This essentially wraps `reqwest`'s [`body`][reqwest::RequestBuilder::body]
+    /// method, and puts the responsibility on the caller for now.
     pub fn body<T: Into<Body>>(self, body: T) -> WriteRequestBuilder<Body> {
         WriteRequestBuilder {
             client: self.client,
@@ -124,6 +171,7 @@ impl WriteRequestBuilder<NoBody> {
 }
 
 impl WriteRequestBuilder<Body> {
+    /// Send the request to the server
     pub async fn send(self) -> Result<()> {
         let url = self.client.base_url.join("/api/v3/write_lp")?;
         let req = WriteRequest::from(&self);
@@ -137,6 +185,8 @@ impl WriteRequestBuilder<Body> {
     }
 }
 
+#[doc(hidden)]
+/// Typestate type for [`WriteRequestBuilder`]
 #[derive(Debug, Copy, Clone)]
 pub struct NoBody;
 
@@ -181,10 +231,5 @@ cpu,host=s1,region=us-west usage=0.7";
             .expect("send write_lp request");
 
         mock.assert_async().await;
-    }
-
-    #[tokio::test]
-    async fn api_v3_write_lp_from_file() {
-        todo!("invoke /api/v3/write_lp API by streaming an .lp file in the body");
     }
 }
