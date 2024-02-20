de102bc92726aa85358da8caf3a79d147565b49e
Michael Gattozzi <mgattozzi@influxdata.com>
2024-02-20 15:34:39 -0500

    feat: Add All or Nothing Bearer token auth support (#24666)
    This commit adds basic authorization support to Edge. Up to this point
    we didn't need have authorization at all and so the server would
    receive and accept requests from anyone. This isn't exactly secure or
    ideal for a deployment and so we add a basic form of authentication.
    
    The way this works is that a user passes in a hex encoded sha256 hash of
    a given token to the '--bearer-token' flag of the serve command. When
    the server starts with this flag it will now check a header of the form
    'Authorization: Bearer <token>' by making sure it is valid in the sense
    that it is not malformed and that when token is hashed it matches the
    value passed in on the command line. The request is denied with either a
    400 Bad Request if the header is malformed or a 401 Unauthorized if the
    hash does not match or the header is missing.
    
    The user is provided a new subcommand of the form: 'influxdb3 create
    token <token>' where the output contains the command to run the server
    with and what the header should look like to make requests.
    
    I can see future work including multiple tokens and rotating between
    them or adding new ones to a live service, but for now this shall
    suffice.
    
    As part of the commit end-to-end tests are included to run the server
    and make requests against the HTTP API and to make sure that requests
    are denied for being unauthorized, accepted for having the right header,
    or denied for being malformed.
    
    Also as part of this commit a small fix is included for 'Accept: */*'
    headers. We were not checking for them and if this header was included
    we were denying it instead of sending back the default payload return
    value.

---
+++ influxdb3/tests/auth.rs
@@ -0,0 +1,152 @@
+use parking_lot::Mutex;
+use reqwest::StatusCode;
+use std::env;
+use std::mem;
+use std::panic;
+use std::process::Child;
+use std::process::Command;
+use std::process::Stdio;
+
+struct DropCommand {
+    cmd: Option<Child>,
+}
+
+impl DropCommand {
+    const fn new(cmd: Child) -> Self {
+        Self { cmd: Some(cmd) }
+    }
+
+    fn kill(&mut self) {
+        let mut cmd = self.cmd.take().unwrap();
+        cmd.kill().unwrap();
+        mem::drop(cmd);
+    }
+}
+
+static COMMAND: Mutex<Option<DropCommand>> = parking_lot::const_mutex(None);
+
+#[tokio::test]
+async fn auth() {
+    // The binary is made before testing so we have access to it
+    let bin_path = {
+        let mut bin_path = env::current_exe().unwrap();
+        bin_path.pop();
+        bin_path.pop();
+        bin_path.join("influxdb3")
+    };
+    let server = DropCommand::new(
+        Command::new(bin_path)
+            .args([
+                "serve",
+                "--bearer-token",
+                "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae", // foo as a sha256
+            ])
+            .stderr(Stdio::null())
+            .stdout(Stdio::null())
+            .spawn()
+            .expect("Was able to spawn a server"),
+    );
+
+    *COMMAND.lock() = Some(server);
+
+    let current_hook = panic::take_hook();
+    panic::set_hook(Box::new(move |info| {
+        COMMAND.lock().take().unwrap().kill();
+        current_hook(info);
+    }));
+
+    let client = reqwest::Client::new();
+
+    // Wait for the server to come up
+    while client
+        .get("http://127.0.0.1:8181/health")
+        .bearer_auth("foo")
+        .send()
+        .await
+        .is_err()
+    {}
+
+    assert_eq!(
+        client
+            .post("http://127.0.0.1:8181/api/v3/write_lp?db=foo")
+            .body("cpu,host=a val=1i 123")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::UNAUTHORIZED
+    );
+    assert_eq!(
+        client
+            .get("http://127.0.0.1:8181/api/v3/query_sql?db=foo&q=select+*+from+cpu")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::UNAUTHORIZED
+    );
+    assert_eq!(
+        client
+            .post("http://127.0.0.1:8181/api/v3/write_lp?db=foo")
+            .body("cpu,host=a val=1i 123")
+            .bearer_auth("foo")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::OK
+    );
+    assert_eq!(
+        client
+            .get("http://127.0.0.1:8181/api/v3/query_sql?db=foo&q=select+*+from+cpu")
+            .bearer_auth("foo")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::OK
+    );
+    // Malformed Header Tests
+    // Test that there is an extra string after the token foo
+    assert_eq!(
+        client
+            .get("http://127.0.0.1:8181/api/v3/query_sql?db=foo&q=select+*+from+cpu")
+            .header("Authorization", "Bearer foo whee")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::BAD_REQUEST
+    );
+    assert_eq!(
+        client
+            .get("http://127.0.0.1:8181/api/v3/query_sql?db=foo&q=select+*+from+cpu")
+            .header("Authorization", "bearer foo")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::BAD_REQUEST
+    );
+    assert_eq!(
+        client
+            .get("http://127.0.0.1:8181/api/v3/query_sql?db=foo&q=select+*+from+cpu")
+            .header("Authorization", "Bearer")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::BAD_REQUEST
+    );
+    assert_eq!(
+        client
+            .get("http://127.0.0.1:8181/api/v3/query_sql?db=foo&q=select+*+from+cpu")
+            .header("Authorizon", "Bearer foo")
+            .send()
+            .await
+            .unwrap()
+            .status(),
+        StatusCode::UNAUTHORIZED
+    );
+    COMMAND.lock().take().unwrap().kill();
+}
