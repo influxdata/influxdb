da4ee1b28caeb6beaf11abbcafda0d65051bdbff
Trevor Hilton <thilton@influxdata.com>
2024-03-14 15:48:47 -0400

    test: add test for non-chunked v1 query api

--- influxdb3/tests/server/query.rs
+++ influxdb3/tests/server/query.rs
@@ -1,6 +1,7 @@
 use crate::TestServer;
 use influxdb3_client::Precision;
 use pretty_assertions::assert_eq;
+use serde_json::{json, Value};
 
 #[tokio::test]
 async fn api_v3_query_sql() {
@@ -264,6 +265,179 @@ async fn api_v3_query_influxql() {
             .unwrap();
         println!("\n{q}", q = t.query);
         println!("{resp}");
+        assert_eq!(t.expected, resp, "query failed: {q}", q = t.query);
+    }
+}
+
+#[tokio::test]
+async fn api_v1_query() {
+    let server = TestServer::spawn().await;
+
+    server
+        .write_lp_to_db(
+            "foo",
+            "cpu,host=a usage=0.9 1\n\
+            cpu,host=a usage=0.89 2\n\
+            cpu,host=a usage=0.85 3\n\
+            mem,host=a usage=0.5 4\n\
+            mem,host=a usage=0.6 5\n\
+            mem,host=a usage=0.7 6",
+            Precision::Second,
+        )
+        .await
+        .unwrap();
+
+    struct TestCase<'a> {
+        database: Option<&'a str>,
+        epoch: Option<&'a str>,
+        query: &'a str,
+        expected: Value,
+    }
+
+    let test_cases = [
+        // Basic Query:
+        TestCase {
+            database: Some("foo"),
+            epoch: None,
+            query: "SELECT * FROM cpu",
+            expected: json!({
+              "results": [
+                {
+                  "series": [
+                    {
+                      "columns": [
+                        "time",
+                        "host",
+                        "usage"
+                      ],
+                      "name": "cpu",
+                      "values": [
+                        ["1970-01-01T00:00:01", "a", 0.9],
+                        ["1970-01-01T00:00:02", "a", 0.89],
+                        ["1970-01-01T00:00:03", "a", 0.85]
+                      ]
+                    }
+                  ],
+                  "statement_id": 0
+                }
+              ]
+            }),
+        },
+        // Basic Query with multiple measurements:
+        TestCase {
+            database: Some("foo"),
+            epoch: None,
+            query: "SELECT * FROM cpu, mem",
+            expected: json!({
+              "results": [
+                {
+                  "series": [
+                    {
+                      "columns": [
+                        "time",
+                        "host",
+                        "usage"
+                      ],
+                      "name": "mem",
+                      "values": [
+                        ["1970-01-01T00:00:04", "a", 0.5],
+                        ["1970-01-01T00:00:05", "a", 0.6],
+                        ["1970-01-01T00:00:06", "a", 0.7]
+                      ]
+                    },
+                    {
+                      "columns": [
+                        "time",
+                        "host",
+                        "usage"
+                      ],
+                      "name": "cpu",
+                      "values": [
+                        ["1970-01-01T00:00:01", "a", 0.9],
+                        ["1970-01-01T00:00:02", "a", 0.89],
+                        ["1970-01-01T00:00:03", "a", 0.85]
+                      ]
+                    }
+                  ],
+                  "statement_id": 0
+                }
+              ]
+            }),
+        },
+        // Basic Query with db in query string:
+        TestCase {
+            database: None,
+            epoch: None,
+            query: "SELECT * FROM foo.autogen.cpu",
+            expected: json!({
+              "results": [
+                {
+                  "series": [
+                    {
+                      "columns": [
+                        "time",
+                        "host",
+                        "usage"
+                      ],
+                      "name": "cpu",
+                      "values": [
+                        ["1970-01-01T00:00:01", "a", 0.9],
+                        ["1970-01-01T00:00:02", "a", 0.89],
+                        ["1970-01-01T00:00:03", "a", 0.85]
+                      ]
+                    }
+                  ],
+                  "statement_id": 0
+                }
+              ]
+            }),
+        },
+        // Basic Query epoch parameter set:
+        TestCase {
+            database: Some("foo"),
+            epoch: Some("s"),
+            query: "SELECT * FROM cpu",
+            expected: json!({
+              "results": [
+                {
+                  "series": [
+                    {
+                      "columns": [
+                        "time",
+                        "host",
+                        "usage"
+                      ],
+                      "name": "cpu",
+                      "values": [
+                        [1, "a", 0.9],
+                        [2, "a", 0.89],
+                        [3, "a", 0.85]
+                      ]
+                    }
+                  ],
+                  "statement_id": 0
+                }
+              ]
+            }),
+        },
+    ];
+
+    for t in test_cases {
+        let mut params = vec![("q", t.query)];
+        if let Some(db) = t.database {
+            params.push(("db", db));
+        }
+        if let Some(epoch) = t.epoch {
+            params.push(("epoch", epoch));
+        }
+        let resp = server
+            .api_v1_query(&params)
+            .await
+            .json::<Value>()
+            .await
+            .unwrap();
+        println!("\n{q}", q = t.query);
+        println!("{resp:#}");
         assert_eq!(t.expected, resp, "query failed: {q}", q = t.query);
     }
 }
