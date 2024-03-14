5858411af5d5727b380a9e3da8d5ecbff680f72f
Michael Gattozzi <mgattozzi@influxdata.com>
2024-03-14 12:06:54 -0400

    feat: build stuff with ci

--- influxdb3_server/src/lib.rs
+++ influxdb3_server/src/lib.rs
@@ -28,7 +28,7 @@ use hyper::service::service_fn;
 use influxdb3_write::{Persister, WriteBuffer};
 use iox_query::QueryNamespaceProvider;
 use iox_time::TimeProvider;
-use observability_deps::tracing::{error, info};
+use observability_deps::tracing::error;
 use service::hybrid;
 use std::convert::Infallible;
 use std::fmt::Debug;
@@ -203,6 +203,7 @@ where
 /// This method returns if either are signalled
 #[cfg(unix)]
 pub async fn wait_for_signal() {
+    use observability_deps::tracing::info;
     use tokio::signal::unix::{signal, SignalKind};
     let mut term = signal(SignalKind::terminate()).expect("failed to register signal handler");
     let mut int = signal(SignalKind::interrupt()).expect("failed to register signal handler");
