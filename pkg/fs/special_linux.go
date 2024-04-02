81fd82e636aeb877a100d5a55de155a78ea7a4e6
Geoffrey Wossum <gwossum@influxdata.com>
2024-04-02 14:44:34 -0500

    fix: add special file exception for tmpfs

--- pkg/fs/special_linux.go
+++ pkg/fs/special_linux.go
@@ -3,6 +3,7 @@ package fs
 import (
 	"errors"
 	"io/fs"
+	"os"
 	"syscall"
 
 	"golang.org/x/sys/unix"
@@ -17,14 +18,46 @@ func IsSpecialFSFromFileInfo(st fs.FileInfo) (bool, error) {
 	// will always have a major device number of 0 per the kernels
 	// Documentation/devices.txt file.
 
-	st_sys_any := st.Sys()
-	if st_sys_any == nil {
-		return false, errors.New("nil returned by fs.FileInfo.Sys")
+	getDevId := func(st fs.FileInfo) (uint64, error) {
+		st_sys_any := st.Sys()
+		if st_sys_any == nil {
+			return 0, errors.New("nil returned by fs.FileInfo.Sys")
+		}
+
+		st_sys, ok := st_sys_any.(*syscall.Stat_t)
+		if !ok {
+			return 0, errors.New("could not convert st.sys() to a *syscall.Stat_t")
+		}
+		return st_sys.Dev, nil
 	}
 
-	st_sys, ok := st_sys_any.(*syscall.Stat_t)
-	if !ok {
-		return false, errors.New("could not convert st.sys() to a *syscall.Stat_t")
+	devId, err := getDevId(st)
+	if err != nil {
+		return false, err
 	}
-	return unix.Major(st_sys.Dev) == 0, nil
+	if unix.Major(devId) != 0 {
+		// This file is definitely not on a special file system.
+		return false, nil
+	}
+
+	// We know the file is in a special file system, but we'll make an
+	// exception for tmpfs, which some distros use for /tmp.
+	// Since the minor IDs are assigned dynamically, we'll find the device ID
+	// for /tmp. If /tmp's device ID matches this st's, then it is safe to assume
+	// the file is in tmpfs. Otherwise, it is a special device that is not tmpfs.
+	// Note that if /tmp is not tmpfs, then the device IDs won't match since
+	// tmp would be a standard file system.
+	tmpSt, err := os.Stat(os.TempDir())
+	if err != nil {
+		// We got an error getting stats on /tmp, but that just means we can't
+		// say the file is in tmpfs. We'll still go ahead and call it a special file.
+		return true, nil
+	}
+	tmpDevId, err := getDevId(tmpSt)
+	if err != nil {
+		// See above for why we're returning an error here.
+		return true, nil
+	}
+	// It's a special file unless the device ID matches /tmp's ID.
+	return devId != tmpDevId, nil
 }
