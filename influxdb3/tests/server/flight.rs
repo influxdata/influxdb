5c1b2d40369a9418e1701ce4c2425c1081b1fcd8
Trevor Hilton <thilton@influxdata.com>
2024-05-10 16:34:21 -0400

    fix: gRPC test broken after addition of system.queries table

--- influxdb3/tests/server/flight.rs
+++ influxdb3/tests/server/flight.rs
@@ -117,6 +117,7 @@ async fn flight() -> Result<(), influxdb3_client::Error> {
                 "| public       | information_schema | tables      | VIEW       |",
                 "| public       | information_schema | views       | VIEW       |",
                 "| public       | iox                | cpu         | BASE TABLE |",
+                "| public       | system             | queries     | BASE TABLE |",
                 "+--------------+--------------------+-------------+------------+",
             ],
             &batches
