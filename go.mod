9db62ccbf13d35b399b5943ec61e928c51ad246d
Martin Hilton <mhilton@influxdata.com>
2024-06-10 15:56:02 +0100

    fix: update CI script
    The scripts/ci/check-system-go-matches-go-mod.sh is failing because
    newer go toolchains include the bugfix version in go.mod's go
    directive. Update the script to check the major and minor versions
    reported by both tools match.

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/influxdata/influxdb/v2
 
-go 1.21
+go 1.21.0
 
 toolchain go1.21.9
 
--- scripts/ci/check-system-go-matches-go-mod.sh
+++ scripts/ci/check-system-go-matches-go-mod.sh
@@ -4,10 +4,11 @@ set -eo pipefail
 # Check that the same major/minor version of go is used in the mod file as on CI to prevent
 # the two from accidentally getting out-of-sync.
 function main () {
-    local -r version_diff=$(go mod edit -go=$(go version | sed -n 's/^.*go\([0-9]*.[0-9]*\).*$/\1/p') -print | diff - go.mod)
-    if [ -n "$version_diff" ]; then
+    local -r go_version=$(go version | sed -n 's/^.*go\([0-9]*.[0-9]*\).*$/\1/p')
+    local -r go_mod_go_version=$(sed -n 's/^go \([0-9]*.[0-9]*\).*$/\1/p' < go.mod)
+    if [ "$go_version" != "$go_mod_go_version" ]; then
         >&2 echo Error: unexpected difference in go version:
-        >&2 echo "$version_diff"
+        >&2 echo "go version: $go_version, go.mod version: $go_mod_go_version"
         exit 1
     fi
 }
