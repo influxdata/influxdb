4f56f53a2835595c8eadb0536aefcfe263b03837
Michael Gattozzi <mgattozzi@influxdata.com>
2024-09-27 12:36:05 -0400

    feat: Add TableId and ColumnId

--- influxdb3_id/src/lib.rs
+++ influxdb3_id/src/lib.rs
@@ -37,3 +37,60 @@ impl From<u32> for DbId {
         Self(value)
     }
 }
+
+#[derive(Debug, Copy, Clone, Eq, PartialOrd, Ord, PartialEq, Serialize, Deserialize)]
+pub struct TableId(u32);
+
+static NEXT_TABLE_ID: AtomicU32 = AtomicU32::new(0);
+
+impl TableId {
+    pub fn new() -> Self {
+        Self(NEXT_TABLE_ID.fetch_add(1, Ordering::SeqCst))
+    }
+
+    pub fn next_id() -> Self {
+        Self(NEXT_TABLE_ID.load(Ordering::SeqCst))
+    }
+
+    pub fn set_next_id(&self) {
+        NEXT_TABLE_ID.store(self.0, Ordering::SeqCst)
+    }
+
+    pub fn as_u32(&self) -> u32 {
+        self.0
+    }
+}
+
+impl Default for TableId {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl From<u32> for TableId {
+    fn from(value: u32) -> Self {
+        Self(value)
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialOrd, Ord, PartialEq, Serialize, Deserialize)]
+pub struct ColumnId(u16);
+
+impl ColumnId {
+    pub fn new(id: u16) -> Self {
+        Self(id)
+    }
+
+    pub fn next_id(&self) -> Self {
+        Self(self.0 + 1)
+    }
+
+    pub fn as_u16(&self) -> u16 {
+        self.0
+    }
+}
+impl From<u16> for ColumnId {
+    fn from(value: u16) -> Self {
+        Self(value)
+    }
+}
