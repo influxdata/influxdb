0c30cc3ab9d066889868d5ca168a23e10c633fc8
Trevor Hilton <thilton@influxdata.com>
2024-10-23 11:56:11 -0400

    fix: last cache cli parser bug
    Correctly parse key and value column args to create last cache command

--- influxdb3/src/commands/common.rs
+++ influxdb3/src/commands/common.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use clap::Parser;
 use secrecy::Secret;
 use url::Url;
@@ -20,4 +22,33 @@ pub struct InfluxDb3Config {
     /// The token for authentication with the InfluxDB 3.0 server
     #[clap(long = "token", env = "INFLUXDB3_AUTH_TOKEN")]
     pub auth_token: Option<Secret<String>>,
+}
+
+/// A clap argument privided as a list of items separated by `SEPARATOR`, which by default is a ','
+#[derive(Debug, Clone)]
+pub struct SeparatedList<T, const SEPARATOR: char = ','>(pub Vec<T>);
+
+impl<T, const SEPARATOR: char> FromStr for SeparatedList<T, SEPARATOR>
+where
+    T: FromStr<Err: Into<anyhow::Error>>,
+{
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(
+            s.split(SEPARATOR)
+                .map(|s| s.parse::<T>().map_err(Into::into))
+                .collect::<Result<Vec<T>, Self::Err>>()?,
+        ))
+    }
+}
+
+impl<T, const SEPARATOR: char> IntoIterator for SeparatedList<T, SEPARATOR> {
+    type Item = T;
+
+    type IntoIter = std::vec::IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
 }
