37be6a68b26fcfaa0664e03243a73e869a17b38e
Jackson Newhouse <jnewhouse@influxdata.com>
2025-01-11 13:26:52 -0800

    feat(docker): Rework docker builds to push to quay and be multi-arch.

--- .circleci/scripts/docker_build_release.bash
+++ .circleci/scripts/docker_build_release.bash
@@ -5,6 +5,8 @@ set -euo pipefail
 readonly PACKAGE="$1"
 readonly FEATURES="$2"
 readonly TAG="$3"
+readonly ARCH="${4:-amd64}"  # Default to amd64 if not specified
+readonly PROFILE="${5:-release}"       # Default to release if not specified
 
 RUST_VERSION="$(sed -E -ne 's/channel = "(.*)"/\1/p' rust-toolchain.toml)"
 COMMIT_SHA="$(git rev-parse HEAD)"
@@ -12,12 +14,17 @@ COMMIT_TS="$(env TZ=UTC0 git show --quiet --date='format-local:%Y-%m-%dT%H:%M:%S
 NOW="$(date --utc --iso-8601=seconds)"
 REPO_URL="https://github.com/influxdata/influxdb"
 
+# Convert arch to platform
+PLATFORM="linux/${ARCH}"
+
 exec docker buildx build \
   --build-arg CARGO_INCREMENTAL="no" \
   --build-arg CARGO_NET_GIT_FETCH_WITH_CLI="true" \
   --build-arg FEATURES="$FEATURES" \
   --build-arg RUST_VERSION="$RUST_VERSION" \
   --build-arg PACKAGE="$PACKAGE" \
+  --build-arg PROFILE="$PROFILE" \
+  --platform "$PLATFORM" \
   --label org.opencontainers.image.created="$NOW" \
   --label org.opencontainers.image.url="$REPO_URL" \
   --label org.opencontainers.image.revision="$COMMIT_SHA" \
