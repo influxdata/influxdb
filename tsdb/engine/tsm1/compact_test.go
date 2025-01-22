8b83edd69e1428751700e5e49697ca6bae0347df
davidby-influx <dbyrne@influxdata.com>
2025-01-22 11:14:17 -0800

    chore: update imports after cherry-pick

--- tsdb/engine/tsm1/compact_test.go
+++ tsdb/engine/tsm1/compact_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/influxdata/influxdb/v2/tsdb"
 	"github.com/influxdata/influxdb/v2/tsdb/engine/tsm1"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
 
