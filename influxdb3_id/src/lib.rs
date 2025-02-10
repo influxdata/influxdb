f5b561b6c9ad2540a288ddccb508f8129cb8120e
Trevor Hilton <thilton@influxdata.com>
2025-02-10 14:30:17 -0500

    refactor: use macro to define id types (#25986)
    * refactor: use macro to define catalog id types
    
    * refactor: make catalog table limit test faster

--- influxdb3_id/src/lib.rs
+++ influxdb3_id/src/lib.rs
@@ -8,138 +8,59 @@ use std::sync::atomic::Ordering;
 mod serialize;
 pub use serialize::SerdeVecMap;
 
-#[derive(Debug, Copy, Clone, Eq, PartialOrd, Ord, PartialEq, Serialize, Deserialize, Hash)]
-pub struct DbId(u32);
+macro_rules! catalog_identifier_type {
+    ($name:ident, $atomic:ident) => {
+        #[derive(
+            Debug, Copy, Clone, Eq, PartialOrd, Ord, PartialEq, Serialize, Deserialize, Hash,
+        )]
+        pub struct $name(u32);
 
-static NEXT_DB_ID: AtomicU32 = AtomicU32::new(0);
+        static $atomic: AtomicU32 = AtomicU32::new(0);
 
-impl DbId {
-    pub fn new() -> Self {
-        Self(
-            NEXT_DB_ID
-                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |n| n.checked_add(1))
-                .expect("Overflowed with DB IDs"),
-        )
-    }
+        impl $name {
+            pub fn new() -> Self {
+                Self(
+                    $atomic
+                        .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |n| n.checked_add(1))
+                        .expect("overflowed u32 when generating new identifier"),
+                )
+            }
 
-    pub fn next_id() -> DbId {
-        Self(NEXT_DB_ID.load(Ordering::SeqCst))
-    }
+            pub fn next_id() -> $name {
+                Self($atomic.load(Ordering::SeqCst))
+            }
 
-    pub fn set_next_id(&self) {
-        NEXT_DB_ID.store(self.0, Ordering::SeqCst)
-    }
+            pub fn set_next_id(&self) {
+                $atomic.store(self.0, Ordering::SeqCst)
+            }
 
-    pub fn as_u32(&self) -> u32 {
-        self.0
-    }
+            pub fn as_u32(&self) -> u32 {
+                self.0
+            }
+        }
+
+        impl Default for $name {
+            fn default() -> Self {
+                Self::new()
+            }
+        }
+
+        impl From<u32> for $name {
+            fn from(value: u32) -> Self {
+                Self(value)
+            }
+        }
+        impl Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+    };
 }
 
-impl Default for DbId {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl From<u32> for DbId {
-    fn from(value: u32) -> Self {
-        Self(value)
-    }
-}
-impl Display for DbId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialOrd, Ord, PartialEq, Serialize, Deserialize, Hash)]
-pub struct TableId(u32);
-
-static NEXT_TABLE_ID: AtomicU32 = AtomicU32::new(0);
-
-impl TableId {
-    pub fn new() -> Self {
-        Self(
-            NEXT_TABLE_ID
-                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |n| n.checked_add(1))
-                .expect("Overflowed with Table IDs"),
-        )
-    }
-
-    pub fn next_id() -> Self {
-        Self(NEXT_TABLE_ID.load(Ordering::SeqCst))
-    }
-
-    pub fn set_next_id(&self) {
-        NEXT_TABLE_ID.store(self.0, Ordering::SeqCst)
-    }
-
-    pub fn as_u32(&self) -> u32 {
-        self.0
-    }
-}
-
-impl Default for TableId {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-impl From<u32> for TableId {
-    fn from(value: u32) -> Self {
-        Self(value)
-    }
-}
-
-impl Display for TableId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-#[derive(Debug, Copy, Clone, Eq, PartialOrd, Ord, PartialEq, Serialize, Deserialize, Hash)]
-pub struct ColumnId(u32);
-
-static NEXT_COLUMN_ID: AtomicU32 = AtomicU32::new(0);
-
-impl ColumnId {
-    pub fn new() -> Self {
-        Self(
-            NEXT_COLUMN_ID
-                .fetch_update(Ordering::SeqCst, Ordering::SeqCst, |n| n.checked_add(1))
-                .expect("Overflowed with Column IDs"),
-        )
-    }
-
-    pub fn next_id() -> Self {
-        Self(NEXT_COLUMN_ID.load(Ordering::SeqCst))
-    }
-
-    pub fn set_next_id(&self) {
-        NEXT_COLUMN_ID.store(self.0, Ordering::SeqCst)
-    }
-
-    pub fn as_u32(&self) -> u32 {
-        self.0
-    }
-}
-impl From<u32> for ColumnId {
-    fn from(value: u32) -> Self {
-        Self(value)
-    }
-}
-
-impl Display for ColumnId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl Default for ColumnId {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+catalog_identifier_type!(DbId, NEXT_DB_ID);
+catalog_identifier_type!(TableId, NEXT_TABLE_ID);
+catalog_identifier_type!(ColumnId, NEXT_COLUMN_ID);
 
 /// The next file id to be used when persisting `ParquetFile`s
 pub static NEXT_FILE_ID: AtomicU64 = AtomicU64::new(0);
@@ -179,5 +100,17 @@ impl From<u64> for ParquetFileId {
 impl Default for ParquetFileId {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::DbId;
+
+    #[test]
+    #[should_panic]
+    fn test_overflow_error() {
+        DbId::from(u32::MAX).set_next_id();
+        DbId::new();
     }
 }
