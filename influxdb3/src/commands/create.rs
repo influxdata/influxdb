e33eb69d50645adf679187e743d7a5b64e85ab0e
Jackson Newhouse <jnewhouse@influxdata.com>
2025-02-28 14:41:04 -0800

    feat(processing_engine): error handling for triggers.

--- influxdb3/src/commands/create.rs
+++ influxdb3/src/commands/create.rs
@@ -5,7 +5,7 @@ use base64::Engine as _;
 use base64::engine::general_purpose::URL_SAFE_NO_PAD as B64;
 use hashbrown::HashMap;
 use influxdb3_client::Client;
-use influxdb3_wal::TriggerSpecificationDefinition;
+use influxdb3_wal::{ErrorBehavior, TriggerSettings, TriggerSpecificationDefinition};
 use rand::RngCore;
 use rand::rngs::OsRng;
 use secrecy::ExposeSecret;
@@ -15,6 +15,7 @@ use sha2::Sha512;
 use std::error::Error;
 use std::num::NonZeroUsize;
 use std::str;
+use std::str::FromStr;
 use std::time::Duration;
 use url::Url;
 
@@ -231,6 +232,11 @@ pub struct TriggerConfig {
     disabled: bool,
     #[clap(long)]
     run_asynchronous: bool,
+    #[clap(long,
+        value_parser = ErrorBehavior::from_str,
+        default_value = "log",
+    )]
+    error_behavior: ErrorBehavior,
     /// Name for the new trigger
     trigger_name: String,
 }
@@ -354,12 +360,18 @@ pub async fn command(config: Config) -> Result<(), Box<dyn Error>> {
             trigger_arguments,
             disabled,
             run_asynchronous,
+            error_behavior,
         }) => {
             let trigger_arguments: Option<HashMap<String, String>> = trigger_arguments.map(|a| {
                 a.into_iter()
                     .map(|SeparatedKeyValue((k, v))| (k, v))
                     .collect::<HashMap<String, String>>()
             });
+
+            let trigger_settings = TriggerSettings {
+                run_async: run_asynchronous,
+                error_behavior,
+            };
 
             match client
                 .api_v3_configure_processing_engine_trigger_create(
@@ -369,7 +381,7 @@ pub async fn command(config: Config) -> Result<(), Box<dyn Error>> {
                     trigger_specification.string_rep(),
                     trigger_arguments,
                     disabled,
-                    run_asynchronous,
+                    trigger_settings,
                 )
                 .await
             {
