f3e24e2c6cfa838d899dc8ce7e88aeeadcfb3317
davidby-influx <dbyrne@influxdata.com>
2025-03-03 14:59:26 -0800

    chore: add information to log messages

--- tsdb/engine/tsm1/file_store.go
+++ tsdb/engine/tsm1/file_store.go
@@ -857,7 +857,7 @@ func (f *FileStore) replace(oldFiles, newFiles []string, updatedFn func(r []TSMF
 			// The new TSM files have a tmp extension.  First rename them.
 			newName = file[:len(file)-4]
 			if err := os.Rename(oldName, newName); err != nil {
-				return err
+				return fmt.Errorf("failed renaming %s to %s: %w", oldName, newName, err)
 			}
 		}
 
@@ -866,9 +866,10 @@ func (f *FileStore) replace(oldFiles, newFiles []string, updatedFn func(r []TSMF
 		// remove it.
 		fd, err := os.Open(newName)
 		if err != nil {
+			err = fmt.Errorf("failed opening %s: %w", newName, err)
 			if newName != oldName {
 				if err1 := os.Rename(newName, oldName); err1 != nil {
-					return err1
+					return errors.Join(err, fmt.Errorf("failed renaming %s to %s: %w", oldName, newName, err1))
 				}
 			}
 			return err
@@ -883,9 +884,10 @@ func (f *FileStore) replace(oldFiles, newFiles []string, updatedFn func(r []TSMF
 
 		tsm, err := NewTSMReader(fd, f.readerOptions...)
 		if err != nil {
+			err = fmt.Errorf("failed creating TSMReader for %s: %w", newName, err)
 			if newName != oldName {
 				if err1 := os.Rename(newName, oldName); err1 != nil {
-					return err1
+					return errors.Join(err, fmt.Errorf("failed renaming %s to %s: %w", oldName, newName, err1))
 				}
 			}
 			return err
@@ -948,14 +950,14 @@ func (f *FileStore) replace(oldFiles, newFiles []string, updatedFn func(r []TSMF
 					// Rename the TSM file used by this reader
 					tempPath := fmt.Sprintf("%s.%s", file.Path(), TmpTSMFileExtension)
 					if err := file.Rename(tempPath); err != nil {
-						return err
+						return fmt.Errorf("failed renaming open TSM file to %s: %w", tempPath, err)
 					}
 
 					// Remove the old file and tombstones.  We can't use the normal TSMReader.Remove()
 					// because it now refers to our temp file which we can't remove.
 					for _, f := range deletes {
 						if err := os.Remove(f); err != nil {
-							return err
+							return fmt.Errorf("failed removing old file %s: %w", f, err)
 						}
 					}
 
@@ -964,11 +966,11 @@ func (f *FileStore) replace(oldFiles, newFiles []string, updatedFn func(r []TSMF
 				}
 
 				if err := file.Close(); err != nil {
-					return err
+					return fmt.Errorf("failed to close TSM file %s: %w", file.Path(), err)
 				}
 
 				if err := file.Remove(); err != nil {
-					return err
+					return fmt.Errorf("failed to remove TSM file %s: %w", file.Path(), err)
 				}
 				break
 			}
@@ -980,7 +982,7 @@ func (f *FileStore) replace(oldFiles, newFiles []string, updatedFn func(r []TSMF
 	}
 
 	if err := file.SyncDir(f.dir); err != nil {
-		return err
+		return fmt.Errorf("failed to sync directory %s: %w", f.dir, err)
 	}
 
 	// Tell the purger about our in-use files we need to remove
@@ -1566,22 +1568,26 @@ type purger struct {
 }
 
 func (p *purger) add(files []TSMFile) {
+	var fileNames []string
 	p.mu.Lock()
 	for _, f := range files {
-		p.files[f.Path()] = f
+		fileName := f.Path()
+		fileNames = append(fileNames, fileName)
+		p.files[fileName] = f
 	}
 	p.mu.Unlock()
-	p.purge(len(files))
+	p.purge(fileNames)
 }
 
-func (p *purger) purge(added int) {
+func (p *purger) purge(fileNames []string) {
 	logger, logEndOp := logger.NewOperation(p.logger, "Purge held files", "filestore_purger")
 
-	logger.Info("Purge started", zap.Int("files", added))
+	logger.Info("added", zap.Int("count", len(fileNames)))
+	logger.Debug("purging", zap.Strings("files", fileNames))
 	p.mu.Lock()
 	if p.running {
 		p.mu.Unlock()
-		logger.Info("Purge already running, files added to previous operation")
+		logger.Info("already running, files added to previous operation")
 		logEndOp()
 		return
 	}
@@ -1591,7 +1597,7 @@ func (p *purger) purge(added int) {
 	go func() {
 		var purgeCount int
 		defer func() {
-			logger.Info("Purge complete", zap.Int("files", purgeCount))
+			logger.Info("removed", zap.Int("files", purgeCount))
 			logEndOp()
 		}()
 		for {
@@ -1604,14 +1610,15 @@ func (p *purger) purge(added int) {
 				// we allow calls to Ref and Unref under the read lock and no lock at all respectively.
 				if !v.InUse() {
 					if err := v.Close(); err != nil {
-						logger.Info("Purge: close file", zap.String("file", k), zap.Error(err))
+						logger.Error("close file failed", zap.String("file", k), zap.Error(err))
 						continue
 					}
 
 					if err := v.Remove(); err != nil {
-						logger.Info("Purge: remove file", zap.String("file", k), zap.Error(err))
+						logger.Error("remove file failed", zap.String("file", k), zap.Error(err))
 						continue
 					}
+					logger.Debug("successfully removed", zap.String("file", k))
 					delete(p.files, k)
 					purgeCount++
 				}
