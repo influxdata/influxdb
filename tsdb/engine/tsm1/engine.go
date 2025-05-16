148e6dc673ab10413823bf9a003ea22c5f6d0cbc
Geoffrey Wossum <gwossum@influxdata.com>
2025-05-16 18:38:43 -0500

    fix: fix when dry running scheduler to determine if level 5 should be planned

--- tsdb/engine/tsm1/engine.go
+++ tsdb/engine/tsm1/engine.go
@@ -2374,7 +2374,7 @@ func (e *Engine) PlanCompactions(planType PlanType) (
 
 	if planType == PT_SmartOptimize {
 		level, runnable := e.scheduler.nextByQueueDepths([TotalCompactionLevels]int{int(len1), int(len2), int(len3), int(len4), 0})
-		if runnable && level <= 3 {
+		if runnable && level <= 4 {
 			// We know that the compaction loop will pull a compaction group from levels 1-3, so no need to refine level 4 or plan level 5.
 			return level1Groups, level2Groups, level3Groups, nil, nil
 		}
