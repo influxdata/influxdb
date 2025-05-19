b615e5c37007d9aaf2cb7673f075d9c6fff9872b
peterbarnett03 <peter.barnett03@gmail.com>
2025-05-19 11:27:34 -0400

    chore: update install_influxdb.sh (#26428)

--- install_influxdb.sh
+++ install_influxdb.sh
@@ -16,7 +16,7 @@ INSTALL_LOC=~/.influxdb
 BINARY_NAME="influxdb3"
 PORT=8181
 
-INFLUXDB_VERSION="3.0.2"
+INFLUXDB_VERSION="3.0.3"
 EDITION="Core"
 EDITION_TAG="core"
 if [ "$1" = "enterprise" ]; then
