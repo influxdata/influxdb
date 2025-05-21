4a917c5a9fd8db864c2de8a9b1549b444f4892e8
Trevor Hilton <thilton@influxdata.com>
2025-05-21 15:45:23 -0400

    refactor: remove variants for unused series key type (#26443)
    * refactor: remove unused Key type from write buffer
    
    The write buffer had a Key variant for handling the experimental v3
    write API that was phased out and removed from an earlier iteration
    of influxdb3.
    
    * refactor: remove key column type from last cache

--- influxdb3_write/src/write_buffer/table_buffer.rs
+++ influxdb3_write/src/write_buffer/table_buffer.rs
@@ -264,22 +264,6 @@ impl MutableTableChunk {
                             panic!("unexpected field type");
                         }
                     }
-                    FieldData::Key(v) => {
-                        if let Entry::Vacant(e) = self.data.entry(f.id) {
-                            let key_builder = StringDictionaryBuilder::new();
-                            if self.row_count > 0 {
-                                panic!(
-                                    "series key columns must be passed in the very first write for a table"
-                                );
-                            }
-                            e.insert(Builder::Key(key_builder));
-                        }
-                        let b = self.data.get_mut(&f.id).expect("key builder should exist");
-                        let Builder::Key(b) = b else {
-                            panic!("unexpected field type");
-                        };
-                        b.append_value(v);
-                    }
                     FieldData::String(v) => {
                         let b = self.data.entry(f.id).or_insert_with(|| {
                             let mut string_builder = StringBuilder::new();
@@ -359,7 +343,7 @@ impl MutableTableChunk {
                         Builder::I64(b) => b.append_null(),
                         Builder::U64(b) => b.append_null(),
                         Builder::String(b) => b.append_null(),
-                        Builder::Tag(b) | Builder::Key(b) => {
+                        Builder::Tag(b) => {
                             // NOTE: we use an empty string "" for tags that are omitted
                             b.append_value("");
                         }
@@ -513,9 +497,6 @@ pub(super) enum Builder {
     U64(UInt64Builder),
     String(StringBuilder),
     Tag(StringDictionaryBuilder<Int32Type>),
-    // For now we use a string dict to be consistent with tags, but in future
-    // keys, like fields may support different data types.
-    Key(StringDictionaryBuilder<Int32Type>),
     Time(TimestampNanosecondBuilder),
 }
 
@@ -528,7 +509,6 @@ impl Builder {
             Self::U64(b) => Arc::new(b.finish_cloned()),
             Self::String(b) => Arc::new(b.finish_cloned()),
             Self::Tag(b) => Arc::new(b.finish_cloned()),
-            Self::Key(b) => Arc::new(b.finish_cloned()),
             Self::Time(b) => Arc::new(b.finish_cloned()),
         }
     }
@@ -556,7 +536,6 @@ impl Builder {
                 Arc::new(b.finish()),
             ),
             Self::Tag(mut b) => (InfluxColumnType::Tag, Arc::new(b.finish())),
-            Self::Key(mut b) => (InfluxColumnType::Tag, Arc::new(b.finish())),
             Self::Time(mut b) => (InfluxColumnType::Timestamp, Arc::new(b.finish())),
         }
     }
@@ -578,7 +557,7 @@ impl Builder {
                     + b.offsets_slice().len()
                     + b.validity_slice().map(|s| s.len()).unwrap_or(0)
             }
-            Self::Tag(b) | Self::Key(b) => {
+            Self::Tag(b) => {
                 let b = b.finish_cloned();
                 b.keys().len() * size_of::<i32>() + b.values().get_array_memory_size()
             }
@@ -724,7 +703,7 @@ mod tests {
         table_buffer.buffer_chunk(0, &rows);
 
         let size = table_buffer.computed_size();
-        assert_eq!(size, 17763);
+        assert_eq!(size, 17739);
     }
 
     #[test]
