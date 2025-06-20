940a2b86260ef49356c5c91bbba46c98739e8de6
Praveen Kumar <pkumar@influxdata.com>
2025-06-20 17:33:26 +0100

    refactor: remove builder completely and initialize `Server` directly
    closes: https://github.com/influxdata/influxdb/issues/25903

--- influxdb3_write/src/write_buffer/mod.rs
+++ influxdb3_write/src/write_buffer/mod.rs
@@ -1407,6 +1407,9 @@ mod tests {
     /// generated snapshot files use the next sequence number.
     #[tokio::test]
     async fn new_snapshots_use_correct_sequence() {
+        // Reset the global file ID counter for consistent test results
+        ParquetFileId::from(0).set_next_id();
+        
         // set up a local file system object store:
         let object_store: Arc<dyn ObjectStore> =
             Arc::new(LocalFileSystem::new_with_prefix(test_helpers::tmp_dir().unwrap()).unwrap());
@@ -1515,6 +1518,9 @@ mod tests {
 
     #[tokio::test]
     async fn next_id_is_correct_number() {
+        // Reset the global file ID counter for consistent test results
+        ParquetFileId::from(0).set_next_id();
+        
         // set up a local file system object store:
         let object_store: Arc<dyn ObjectStore> =
             Arc::new(LocalFileSystem::new_with_prefix(test_helpers::tmp_dir().unwrap()).unwrap());
