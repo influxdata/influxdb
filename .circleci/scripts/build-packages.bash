bc57828c9e6e434a80e21fd9a788b664ce811dc5
Charles Thomas <ch@rlesthom.as>
2025-07-09 16:22:07 -0400

    test(ci): try mangling perms ourselves

---
+++ .circleci/scripts/build-packages.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get install --no-install-recommends --yes \
+asciidoc        \
+build-essential \
+git             \
+python3         \
+python3-venv    \
+rpm             \
+ruby-dev        \
+xmlto
+gem install fpm
+python3 -m venv /tmp/venv
+/tmp/venv/bin/pip install -r .circleci/scripts/package/requirements.txt
+/tmp/venv/bin/python .circleci/scripts/package/build.py
