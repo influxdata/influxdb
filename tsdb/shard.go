13c96af89617263498594a161d523289f0203e22
devanbenz <devandbenz@gmail.com>
2025-07-23 14:16:05 -0500

    feat: remove setlastmodified

--- tsdb/shard.go
+++ tsdb/shard.go
@@ -479,15 +479,6 @@ func (s *Shard) LastModifiedOrErr() (time.Time, error) {
 	return engine.LastModified(), nil
 }
 
-// SetLastModified updates the last modified time for this shard.
-func (s *Shard) SetLastModified(t time.Time) error {
-	engine, err := s.Engine()
-	if err != nil {
-		return err
-	}
-	return engine.SetLastModified(t)
-}
-
 // Index returns a reference to the underlying index. It returns an error if
 // the index is nil.
 func (s *Shard) Index() (Index, error) {
