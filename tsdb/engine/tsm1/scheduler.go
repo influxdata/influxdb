d2c17d0faf392d40de5246fb8476ab6570dfb004
davidby-influx <dbyrne@influxdata.com>
2025-08-01 11:21:39 -0700

    chore: add trace logging to compaction scheduler

--- tsdb/engine/tsm1/scheduler.go
+++ tsdb/engine/tsm1/scheduler.go
@@ -1,6 +1,7 @@
 package tsm1
 
 import (
+	"go.uber.org/zap"
 	"sync/atomic"
 )
 
@@ -12,8 +13,9 @@ type Scheduler struct {
 	stats          *EngineStatistics
 
 	// queues is the depth of work pending for each compaction level
-	queues  [TotalCompactionLevels]int
-	weights [TotalCompactionLevels]float64
+	queues      [TotalCompactionLevels]int
+	weights     [TotalCompactionLevels]float64
+	traceLogger *zap.Logger
 }
 
 func newScheduler(stats *EngineStatistics, maxConcurrency int) *Scheduler {
@@ -21,7 +23,16 @@ func newScheduler(stats *EngineStatistics, maxConcurrency int) *Scheduler {
 		stats:          stats,
 		maxConcurrency: maxConcurrency,
 		weights:        defaultWeights,
+		traceLogger:    zap.NewNop(),
 	}
+}
+
+func (s *Scheduler) WithLogger(logger *zap.Logger) *Scheduler {
+	if logger == nil {
+		logger = zap.NewNop()
+	}
+	s.traceLogger = logger.With(zap.String("service", "compaction_scheduler"))
+	return s
 }
 
 func (s *Scheduler) SetDepth(level, depth int) {
@@ -40,7 +51,16 @@ func (s *Scheduler) nextByQueueDepths(depths [TotalCompactionLevels]int) (int, b
 	level4Running := int(atomic.LoadInt64(&s.stats.TSMFullCompactionsActive))
 	level5Running := int(atomic.LoadInt64(&s.stats.TSMOptimizeCompactionsActive))
 
+	if level1Running+level2Running+level3Running+level4Running+level5Running > 0 {
+		s.traceLogger.Debug("running compactions",
+			zap.Int("level1", level1Running),
+			zap.Int("level2", level2Running),
+			zap.Int("level3", level3Running),
+			zap.Int("level4", level4Running),
+			zap.Int("level5", level5Running))
+	}
 	if level1Running+level2Running+level3Running+level4Running+level5Running >= s.maxConcurrency {
+		s.traceLogger.Debug("max compaction concurrency reached", zap.Int("maxConcurrency", s.maxConcurrency))
 		return 0, false
 	}
 
@@ -53,6 +73,7 @@ func (s *Scheduler) nextByQueueDepths(depths [TotalCompactionLevels]int) (int, b
 
 	end := len(depths)
 	if level3Running+level4Running+level5Running >= loLimit && s.maxConcurrency-(level1Running+level2Running) == 0 {
+		s.traceLogger.Debug("levels 3 through 5 compactions greater than low limit and levels 1 and 2 equal max concurrency, restricting to levels 1 and 2", zap.Int("loLimit", loLimit))
 		end = 2
 	}
 
@@ -62,6 +83,9 @@ func (s *Scheduler) nextByQueueDepths(depths [TotalCompactionLevels]int) (int, b
 			level, runnable = i+1, true
 			weight = float64(depths[i]) * s.weights[i]
 		}
+	}
+	if runnable {
+		s.traceLogger.Debug("selected runnable compaction level", zap.Int("level", level))
 	}
 	return level, runnable
 }
