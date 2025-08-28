29919257b9e661de9cc3446e7e1e6e8a730c85ba
devanbenz <devandbenz@gmail.com>
2025-08-28 13:23:21 -0500

    feat: Functionality is currently working, added some comments regarding the algorithm to be used
    the expected output is the following
    
    ❯ curl -s -XPOST 'http://localhost:8086/debug/vars' | grep "compact-throughput-usage"
    "stats": {"compact-throughput-usage":42.1},
    
    where 42.1 is a number in percentage use

--- monitor/stats.go
+++ monitor/stats.go
@@ -17,11 +17,24 @@ type compactThroughputStats struct {
 	burst   int
 }
 
+// CompactThroughputUsage calculates the percentage of burst capacity currently consumed by compaction.
+//
+// available = current tokens in the rate limiter bucket (can be negative when in debt)
+// burst = maximum tokens the bucket can hold
+// usage percentage = ((burst - available) / burst) * 100
 func (s *stats) CompactThroughputUsage() float64 {
 	if s.comp.burst == 0 {
 		return 0.0
 	}
 	available := s.comp.limiter.Tokens()
+
+	// Clamp available tokens
+	if available < 0 {
+		available = 0
+	} else if available > float64(s.comp.burst) {
+		available = float64(s.comp.burst)
+	}
+
 	percentage := ((float64(s.comp.burst) - available) / float64(s.comp.burst)) * 100
 	return math.Round(percentage*10) / 10
 }
