7143187c6411c54ae5f0ab35bee62ce5fd95c577
peterbarnett03 <peter.barnett03@gmail.com>
2025-09-15 11:58:45 -0400

    chore: Update InfluxDB version to 3.4.2 (#26801)

--- install_influxdb.sh
+++ install_influxdb.sh
@@ -17,7 +17,7 @@ INSTALL_LOC=~/.influxdb
 BINARY_NAME="influxdb3"
 PORT=8181
 
-INFLUXDB_VERSION="3.4.1"
+INFLUXDB_VERSION="3.4.2"
 EDITION="Core"
 EDITION_TAG="core"
 if [ "$1" = "enterprise" ]; then
