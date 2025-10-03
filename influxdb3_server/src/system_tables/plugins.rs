f7021ff6ecba58eb6c1e07f1457fb2536b9cf865
Michael Gattozzi <mgattozzi@influxdata.com>
2025-10-03 16:16:01 -0400

    feat: Add plugin file system table (#26875)
    This commit adds a system table that can be queried in order to show all
    of the files for a plugin via a system table under _internal. We also
    add some tests to make sure the tables contain what we would expect if
    we delete plugins, add plugins, or have no plugins set.
    
    This is part of the multi file plugin updates and lays the foundation
    for being able to test that those changes are working.

---
+++ influxdb3_server/src/system_tables/plugins.rs
@@ -0,0 +1,77 @@
+use arrow_array::{ArrayRef, Int64Array, RecordBatch, StringArray};
+use arrow_schema::{DataType, Field, Schema, SchemaRef};
+use async_trait::async_trait;
+use datafusion::common::Result;
+use datafusion::logical_expr::Expr;
+use influxdb3_processing_engine::ProcessingEngineManagerImpl;
+use iox_system_tables::IoxSystemTable;
+use std::sync::Arc;
+
+#[derive(Debug)]
+pub(super) struct PluginsTable {
+    schema: SchemaRef,
+    processing_engine: Option<Arc<ProcessingEngineManagerImpl>>,
+}
+
+impl PluginsTable {
+    pub(super) fn new(processing_engine: Option<Arc<ProcessingEngineManagerImpl>>) -> Self {
+        Self {
+            schema: plugins_schema(),
+            processing_engine,
+        }
+    }
+}
+
+fn plugins_schema() -> SchemaRef {
+    let columns = vec![
+        Field::new("plugin_name", DataType::Utf8, false),
+        Field::new("file_name", DataType::Utf8, false),
+        Field::new("file_path", DataType::Utf8, false),
+        Field::new("size_bytes", DataType::Int64, false),
+        Field::new("last_modified", DataType::Int64, false),
+    ];
+    Schema::new(columns).into()
+}
+
+#[async_trait]
+impl IoxSystemTable for PluginsTable {
+    fn schema(&self) -> SchemaRef {
+        Arc::clone(&self.schema)
+    }
+
+    async fn scan(
+        &self,
+        _filters: Option<Vec<Expr>>,
+        _limit: Option<usize>,
+    ) -> Result<RecordBatch> {
+        let Some(processing_engine) = &self.processing_engine else {
+            return Ok(RecordBatch::new_empty(Arc::clone(&self.schema)));
+        };
+
+        let plugin_files = processing_engine.list_plugin_files().await;
+
+        let mut plugin_names = Vec::new();
+        let mut file_names = Vec::new();
+        let mut file_paths = Vec::new();
+        let mut sizes = Vec::new();
+        let mut last_modifieds = Vec::new();
+
+        for file_info in plugin_files {
+            plugin_names.push(Some(file_info.plugin_name.to_string()));
+            file_names.push(Some(file_info.file_name.to_string()));
+            file_paths.push(Some(file_info.file_path.to_string()));
+            sizes.push(Some(file_info.size_bytes));
+            last_modifieds.push(Some(file_info.last_modified_millis));
+        }
+
+        let columns: Vec<ArrayRef> = vec![
+            Arc::new(StringArray::from(plugin_names)),
+            Arc::new(StringArray::from(file_names)),
+            Arc::new(StringArray::from(file_paths)),
+            Arc::new(Int64Array::from(sizes)),
+            Arc::new(Int64Array::from(last_modifieds)),
+        ];
+
+        Ok(RecordBatch::try_new(Arc::clone(&self.schema), columns)?)
+    }
+}
