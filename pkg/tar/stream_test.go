d16734597e7c59aa429015d3d5860cf775717575
Devan <devandbenz@gmail.com>
2025-11-07 10:18:54 -0600

    feat: Use StreamRenameFile for our buffer testing

--- pkg/tar/stream_test.go
+++ pkg/tar/stream_test.go
@@ -13,23 +13,32 @@ import (
 	pkgtar "github.com/influxdata/influxdb/pkg/tar"
 )
 
-func TestStreamWithBufSize(t *testing.T) {
+func TestStreamRenameWithBufSize(t *testing.T) {
 	dir := t.TempDir()
-	testFile := filepath.Join(dir, "test.txt")
+	testFile := filepath.Join(dir, "test2.txt.tar")
 	testData := []byte("test data for buffer size")
+
+	testFileRename := "test.txt.tar"
 
 	require.NoError(t, os.WriteFile(testFile, testData, 0644))
 
 	var buf bytes.Buffer
-	bufSize := uint64(8192)
+	bufSize := uint64(1024 * 1024)
 
-	require.NoError(t, pkgtar.Stream(&buf, dir, "shard", bufSize, nil))
-	require.True(t, buf.Len() > 0)
+	tw := tar.NewWriter(&buf)
+
+	f, err := os.Open(testFile)
+	require.NoError(t, err, "error opening testFile")
+	info, err := f.Stat()
+	require.NoError(t, err, "error stat testFile")
+
+	require.NoError(t, pkgtar.StreamRenameFile(info, testFileRename, "", testFile, tw, bufSize))
+	require.NoError(t, tw.Close())
 
 	tr := tar.NewReader(&buf)
 	hdr, err := tr.Next()
 	require.NoError(t, err)
-	require.Equal(t, "shard/test.txt", hdr.Name)
+	require.Equal(t, testFileRename, hdr.Name)
 
 	content, err := io.ReadAll(tr)
 	require.NoError(t, err)
