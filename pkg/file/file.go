8c9b85043bffe3a337ae3d28346d99b29ab40a3a
Geoffrey Wossum <gwossum@influxdata.com>
2025-12-23 14:29:30 -0600

    feat: add TLS certificate reloading on SIGHUP (#26994)
    * feat: add TLS certificate reloading on SIGHUP
    
    Add TLS certificate reloading on SIGHUP. For httpd service certificates,
    the configuration is reloaded and certificate and key file locations are
    updated accordingly. For opentsdb service certificates, certificates and
    keys at the existing locations are reloaded. If reloading a certificate
    fails, the currently loaded certificate continues to be used.
    
    Also adds file permission checking for TLS certificates and private
    keys.
    
    * chore: rewrite some tests to get certificates without tricks
    
    Rewrite some tests so that tricks are not used to get a hold of the TLS
    certificate in-use. Instead, an HTTP client is used to get the
    certificate presented by the http service.
    
    * fix: correct race conditions in httpd.Service
    
    - Correct race conditions present in httpd.Service.
    - Improve error messages.
    - Rename some methods for clarity.
    - Fix issues in tests.
    - Fix comments.

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -1,11 +1,17 @@
 package file
 
 import (
+	goerrors "errors"
 	"fmt"
 	"io"
 	"os"
 
 	"github.com/influxdata/influxdb/pkg/errors"
+)
+
+var (
+	ErrPermissionsTooOpen = goerrors.New("file permissions are too open")
+	ErrNilParam           = goerrors.New("got nil parameter")
 )
 
 func copyFile(src, dst string) (err error) {
@@ -41,4 +47,42 @@ func MoveFileWithReplacement(src, dst string) error {
 	}
 
 	return os.Remove(src)
+}
+
+// VerifyFilePermissivenessF checks if permissions on f are as restrictive
+// or more restrictive than maxPerms. if not, then an error is returned.
+// For security reasons, there is no VerifyFilePermissiveness function
+// that allows passing a path. This is to prevent TOCTOU (Time-of-Check-Time-of-Use)
+// issues because of race conditions on checking file permissions versus
+// opening the file.
+func VerifyFilePermissivenessF(f *os.File, maxPerms os.FileMode) error {
+	if f == nil {
+		return fmt.Errorf("VerifyFilePermissivenessF: %w", ErrNilParam)
+	}
+
+	info, err := f.Stat()
+	if err != nil {
+		return fmt.Errorf("stat failed for %q: %w", f.Name(), err)
+	}
+	return VerifyFileInfoPermissiveness(info, maxPerms, f.Name())
+}
+
+// VerifyFileInfoPermissiveness checks if permissions on info are as restrictive
+// or more restrictive than maxPerms. If not, then an error is returned. path is
+// only used to provide better messages. If path is empty, then info.Name() is used
+// for error messages.
+func VerifyFileInfoPermissiveness(info os.FileInfo, maxPerms os.FileMode, path string) error {
+	if info == nil {
+		return fmt.Errorf("VerifyFileInfoPermissiveness: %w", ErrNilParam)
+	}
+	perms := info.Mode().Perm()
+	extraPerms := perms & ^maxPerms
+	if extraPerms != 0 {
+		if len(path) == 0 {
+			path = info.Name()
+		}
+		return fmt.Errorf("%w: for %q, maximum is %04o (%s) but found %04o (%s); extra permissions: %04o (%s)",
+			ErrPermissionsTooOpen, path, maxPerms, maxPerms, perms, perms, extraPerms, extraPerms)
+	}
+	return nil
 }
